8d92a8d1c396596348c96c1502922e80e3e24f26
noga <35369933+noga-aviator@users.noreply.github.com>
2020-08-25 00:56:25 +0200

    Add files via upload

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/EICAS/ECAM/A320_Neo_LowerECAM_BLEED.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/EICAS/ECAM/A320_Neo_LowerECAM_BLEED.js
@@ -15,10 +15,41 @@ var A320_Neo_LowerECAM_BLEED;
         }
         init() {
             this.isInitialised = true;
+            // finding all html element for the display, first element of array is always the open on, the second is the closed one
+            this.apuBleedIndication = [this.querySelector("#apu-open"), this.querySelector("#apu-closed")];
+            this.left_hp = [this.querySelector("#left-hp-open"), this.querySelector("#left-hp-closed")];
+            this.left_ip = [this.querySelector("#left-ip-open"), this.querySelector("#left-ip-closed")];
+            this.right_ip = [this.querySelector("#right-ip-open"), this.querySelector("#right-ip-closed")];
+            this.right_hp = [this.querySelector("#right-hp-open"), this.querySelector("#right-hp-closed")]
         }
         update(_deltaTime) {
             if (!this.isInitialised) {
                 return;
+            }
+            // find if engine one and two are providing bleed air
+            var currentEngineGenState = [SimVar.GetSimVarValue("BLEED AIR ENGINE:1", "Bool",), SimVar.GetSimVarValue("BLEED AIR ENGINE:2", "Bool")]
+
+            if (currentEngineGenState[0] === 1) {
+                this.left_hp[0].setAttribute("visibility", 'visible')
+                this.left_hp[1].setAttribute("visibility", 'hidden')
+                this.right_ip[0].setAttribute("visibility", 'visible')
+                this.right_ip[1].setAttribute("visibility", 'hiddeb')
+            } else {
+                this.left_hp[0].setAttribute("visibility", 'hidden')
+                this.left_hp[1].setAttribute("visibility", 'visible')
+                this.right_ip[0].setAttribute("visibility", 'hidden')
+                this.right_ip[1].setAttribute("visibility", 'visible')
+            }
+            if (currentEngineGenState[1] === 1) {
+                this.right_hp[0].setAttribute("visibility", 'visible')
+                this.right_hp[1].setAttribute("visibility", 'hidden')
+                this.right_ip[0].setAttribute("visibility", 'visible')
+                this.right_ip[1].setAttribute("visibility", 'hiddeb')
+            } else {
+                this.right_hp[0].setAttribute("visibility", 'hidden')
+                this.right_hp[1].setAttribute("visibility", 'visible')
+                this.right_ip[0].setAttribute("visibility", 'hidden')
+                this.right_ip[1].setAttribute("visibility", 'visible')
             }
         }
     }
