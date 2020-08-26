1d3ca4d1112131a004757292ba7322dd34acc24f
Ninjo <ninjomcs@gmail.com>
2020-08-26 17:20:56 +0200

    Fixed a bug that made the displays go off when starting on the runway at certain airports
    This in turn disabled the functionality I added for making the screens also go off when shutting down the aircraft, but it's better than having the displays off when not starting cold and dark until I work on a solution.

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/MFD/A320_Neo_MFD.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/MFD/A320_Neo_MFD.js
@@ -93,6 +93,7 @@ class A320_Neo_MFD_MainPage extends NavSystemPage {
         this.selfTestTimer = -1;
         this.selfTestTimerStarted = false;
         this.electricity = this.gps.getChildById("Electricity")
+        this.displaysAbleToTurnOff = true;
     }
     onUpdate(_deltaTime) {
         super.onUpdate(_deltaTime);
@@ -117,9 +118,11 @@ class A320_Neo_MFD_MainPage extends NavSystemPage {
 
         var externalPower = SimVar.GetSimVarValue("EXTERNAL POWER ON", "bool");
         var engineOn = SimVar.GetSimVarValue("GENERAL ENG STARTER:1", "bool");
-        var apuOn = SimVar.GetSimVarValue("APU SWITCH", "bool");   
+        var apuOn = SimVar.GetSimVarValue("APU SWITCH", "bool");
+        var onRunway = SimVar.GetSimVarValue("ON ANY RUNWAY", "bool");
+        var isOnGround = SimVar.GetSimVarValue("SIM ON GROUND", "bool")
         
-        this.updateScreenState(externalPower, engineOn, apuOn);
+        this.updateScreenState(externalPower, engineOn, apuOn, onRunway, isOnGround);
 
         if (engineOn) {
             this.selfTestDiv.style.display = "none";
@@ -140,11 +143,12 @@ class A320_Neo_MFD_MainPage extends NavSystemPage {
         
     }
 
-    updateScreenState(externalPowerOn, engineOn, apuOn) {
-        if (!externalPowerOn && !engineOn && !apuOn) {
+    updateScreenState(externalPowerOn, engineOn, apuOn, onRunway, isOnGround) {
+        if (!externalPowerOn && !apuOn && !engineOn && !onRunway && isOnGround && this.displaysAbleToTurnOff) {
             this.electricity.style.display = "none";
         } else {
             this.electricity.style.display = "block";
+            this.displaysAbleToTurnOff = false;
         }
     }
 
