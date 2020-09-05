f1807bb84fee0e0cc587e01c0ec500810bf9a502
Curtis English <curtis@voxelloop.co.uk>
2020-09-05 21:23:13 +0100

    Simplified code.

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/PFD/ILSIndicator.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/PFD/ILSIndicator.js
@@ -13,8 +13,6 @@ class Jet_PFD_ILSIndicator extends HTMLElement {
         this.gsVisible = false;
         this.infoVisible = false;
         this._aircraft = Aircraft.A320_NEO;
-        this.locCaptured = false;
-        this.gsCaptured = false;
     }
     get aircraft() {
         return this._aircraft;
@@ -254,15 +252,11 @@ class Jet_PFD_ILSIndicator extends HTMLElement {
                     let gsi = -SimVar.GetSimVarValue("NAV GSI:" + localizer.id, "number") / 127.0;
                     let delta = (gsi + 1.0) * 0.5;
                     let y = this.gs_cursorMinY + (this.gs_cursorMaxY - this.gs_cursorMinY) * delta;
-                    if (y >= 220 && y <= 230 || this.gsCaptured) {
+                    if (y >= 220 && y <= 230 || SimVar.GetSimVarValue("L:A32NX_OFF_GS", "bool") == 0) {
                         SimVar.SetSimVarValue("L:A32NX_OFF_GS", "bool", 0);
-                        this.gsCaptured = true;
-                    }
-                    else {
-                        SimVar.SetSimVarValue("L:A32NX_OFF_GS", "bool", 1);
                     }
                     if (y < 105 || y > 340) {
-                        this.gsCaptured = false;
+                        SimVar.SetSimVarValue("L:A32NX_OFF_GS", "bool", 1);
                     }
                     y = Math.min(this.gs_cursorMinY, Math.max(this.gs_cursorMaxY, y));
                     this.gs_cursorGroup.setAttribute("transform", "translate(" + this.gs_cursorPosX + ", " + y + ")");
@@ -285,15 +279,11 @@ class Jet_PFD_ILSIndicator extends HTMLElement {
                     let gsi = -SimVar.GetSimVarValue("GPS VERTICAL ERROR", "meters");
                     let delta = 0.5 + (gsi / 150.0) / 2;
                     let y = this.gs_cursorMinY + (this.gs_cursorMaxY - this.gs_cursorMinY) * delta;
-                    if (y >= 220 && y <= 230 || this.gsCaptured) {
+                    if (y >= 220 && y <= 230 || SimVar.GetSimVarValue("L:A32NX_OFF_GS", "bool") == 0) {
                         SimVar.SetSimVarValue("L:A32NX_OFF_GS", "bool", 0);
-                        this.gsCaptured = true;
-                    }
-                    else {
-                        SimVar.SetSimVarValue("L:A32NX_OFF_GS", "bool", 1);
                     }
                     if (y < 105 || y > 340) {
-                        this.gsCaptured = false;
+                        SimVar.SetSimVarValue("L:A32NX_OFF_GS", "bool", 1);
                     }
                     y = Math.min(this.gs_cursorMinY, Math.max(this.gs_cursorMaxY, y));
                     this.gs_cursorGroup.setAttribute("transform", "translate(" + this.gs_cursorPosX + ", " + y + ")");
@@ -324,15 +314,11 @@ class Jet_PFD_ILSIndicator extends HTMLElement {
                     let cdi = SimVar.GetSimVarValue("NAV CDI:" + localizer.id, "number") / 127.0;
                     let delta = (cdi + 1.0) * 0.5;
                     let x = this.loc_cursorMinX + (this.loc_cursorMaxX - this.loc_cursorMinX) * delta;
-                    if (x >= 245 && x <= 255 || this.locCaptured) {
+                    if (x >= 245 && x <= 255 || SimVar.GetSimVarValue("L:A32NX_OFF_LOC", "bool") == 0) {
                         SimVar.SetSimVarValue("L:A32NX_OFF_LOC", "bool", 0);
-                        this.locCaptured = true;
-                    }
-                    else {
-                        SimVar.SetSimVarValue("L:A32NX_OFF_LOC", "bool", 1);
                     }
                     if (x < 115 || x > 375) {
-                        this.locCaptured = false;
+                        SimVar.SetSimVarValue("L:A32NX_OFF_LOC", "bool", 1);
                     }
                     x = Math.max(this.loc_cursorMinX, Math.min(this.loc_cursorMaxX, x));
                     this.loc_cursorGroup.setAttribute("transform", "translate(" + x + ", " + this.loc_cursorPosY + ")");
