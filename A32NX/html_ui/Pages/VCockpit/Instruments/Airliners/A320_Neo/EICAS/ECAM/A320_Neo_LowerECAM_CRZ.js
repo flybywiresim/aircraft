7abc0e26afd4fcc2731c72f52b4314bd8d8a8971
Jonathan Schostak <jonathan@schostak.org>
2020-09-06 18:34:45 +0200

    JS Sytle improvements

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/EICAS/ECAM/A320_Neo_LowerECAM_CRZ.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/EICAS/ECAM/A320_Neo_LowerECAM_CRZ.js
@@ -14,23 +14,23 @@ var A320_Neo_LowerECAM_CRZ;
             TemplateElement.call(this, this.init.bind(this));
         }
         init() {
-            this.FuelUsedTotal = this.querySelector("#FuelUsedTotal"); //
-            this.FuelUsedLeft = this.querySelector("#FuelUsedLeft"); //
-            this.FuelUsedRight = this.querySelector("#FuelUsedRight"); //
+            this.FuelUsedTotal = this.querySelector("#FuelUsedTotal");
+            this.FuelUsedLeft = this.querySelector("#FuelUsedLeft");
+            this.FuelUsedRight = this.querySelector("#FuelUsedRight");
 
-            this.OilQuantityLeft = this.querySelector("#OilQuantityLeft"); //
+            this.OilQuantityLeft = this.querySelector("#OilQuantityLeft");
             this.OilQuantityLeftDecimal = this.querySelector("#OilQuantityLeftDecimal");
-            this.OilQuantityRight = this.querySelector("#OilQuantityRight"); //
+            this.OilQuantityRight = this.querySelector("#OilQuantityRight");
             this.OilQuantityRightDecimal = this.querySelector("#OilQuantityRightDecimal");
 
-            this.VibN1Left = this.querySelector("#VibN1Left"); //
+            this.VibN1Left = this.querySelector("#VibN1Left");
             this.VibN1LeftDecimal = this.querySelector("#VibN1LeftDecimal");
-            this.VibN1Right = this.querySelector("#VibN1Right"); //
+            this.VibN1Right = this.querySelector("#VibN1Right");
             this.VibN1RightDecimal = this.querySelector("#VibN1RightDecimal");
 
-            this.VibN2Left = this.querySelector("#VibN2Left"); //
+            this.VibN2Left = this.querySelector("#VibN2Left");
             this.VibN2LeftDecimal = this.querySelector("#VibN2LeftDecimal");
-            this.VibN2Right = this.querySelector("#VibN2Right"); //
+            this.VibN2Right = this.querySelector("#VibN2Right");
             this.VibN2RightDecimal = this.querySelector("#VibN2RightDecimal");
 
             this.LandingElevationMode = this.querySelector("#LandingElevationMode");
@@ -39,14 +39,12 @@ var A320_Neo_LowerECAM_CRZ;
             this.DeltaPressure = this.querySelector("#DeltaPressure");
             this.DeltaPressureDecimal = this.querySelector("#DeltaPressureDecimal");
 
-            this.CabinVerticalSpeed = this.querySelector("#CabinVerticalSpeed"); //
-            this.CabinAltitude = this.querySelector("#CabinAltitude"); //
+            this.CabinVerticalSpeed = this.querySelector("#CabinVerticalSpeed");
+            this.CabinAltitude = this.querySelector("#CabinAltitude");
 
             this.CockpitTemp = this.querySelector("#CockpitTemp");
             this.ForwardTemp = this.querySelector("#ForwardTemp");
             this.AftTemp = this.querySelector("#AftTemp");
-
-            //this.querySelector("#LandingElevationMode").textValue 
 
             this.isInitialised = true;
         }
@@ -57,8 +55,9 @@ var A320_Neo_LowerECAM_CRZ;
             
             // Fuel
             var unitFactor = SimVar.GetSimVarValue("FUEL WEIGHT PER GALLON", "lbs");
-            if (BaseAirliners.unitIsMetric(Aircraft.A320_NEO))
+            if (BaseAirliners.unitIsMetric(Aircraft.A320_NEO)) {
                 unitFactor = SimVar.GetSimVarValue("FUEL WEIGHT PER GALLON", "kilogram");
+            }
             let leftConsumption = SimVar.GetSimVarValue("GENERAL ENG FUEL USED SINCE START:1", "gallon") * unitFactor * 0.001;
             let rightConsumption = SimVar.GetSimVarValue("GENERAL ENG FUEL USED SINCE START:2", "gallon") * unitFactor * 0.001;
 
@@ -67,7 +66,7 @@ var A320_Neo_LowerECAM_CRZ;
             this.FuelUsedRight.textContent = fastToFixed(rightConsumption, 0);
 
             // Oil
-            var value = SimVar.GetSimVarValue("ENG OIL QUANTITY:1", "percent") * 0.01 * 25;
+            let value = SimVar.GetSimVarValue("ENG OIL QUANTITY:1", "percent") * 0.01 * 25;
             this.OilQuantityLeft.textContent = Math.floor(value) + ".";
             this.OilQuantityLeftDecimal.textContent = value.toFixed(1).split(".", 2)[1];
 
@@ -103,12 +102,12 @@ var A320_Neo_LowerECAM_CRZ;
             value = SimVar.GetSimVarValue("PRESSURIZATION CABIN ALTITUDE", "feet");
             this.CabinAltitude.textContent = fastToFixed(value, 0);
 
-            var cabinAltMetres = value * 0.3048;
-            var cabinPressurePascal = 101325 * Math.exp(-0.00011857591*cabinAltMetres);
-            var cabinPressurePSI = cabinPressurePascal * 0.000145038;
-            var outsidePressureINHG = SimVar.GetSimVarValue("AMBIENT PRESSURE", "inHg");
-            var outsidePressurePSI = outsidePressureINHG * 0.491154;
-            var pressureDiff = cabinPressurePSI - outsidePressurePSI;
+            let cabinAltMetres = value * 0.3048;
+            let cabinPressurePascal = 101325 * Math.exp(-0.00011857591 * cabinAltMetres);
+            let cabinPressurePSI = cabinPressurePascal * 0.000145038;
+            let outsidePressureINHG = SimVar.GetSimVarValue("AMBIENT PRESSURE", "inHg");
+            let outsidePressurePSI = outsidePressureINHG * 0.491154;
+            let pressureDiff = cabinPressurePSI - outsidePressurePSI;
 
             this.DeltaPressure.textContent = Math.floor(pressureDiff) + ".";
             this.DeltaPressureDecimal.textContent = value.toFixed(1).split(".", 2)[1];
