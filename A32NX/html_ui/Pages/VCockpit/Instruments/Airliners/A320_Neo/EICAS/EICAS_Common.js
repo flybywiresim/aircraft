6178bbbd22f7b6b3b9e7652f4caea830edec834f
David Valachovic <David.E.Valachovic@gmail.com>
2020-09-07 01:04:35 -0500

    show GW in increments of 100

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/EICAS/EICAS_Common.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/EICAS/EICAS_Common.js
@@ -77,25 +77,30 @@ class EICASCommonDisplay extends Airliners.EICASTemplateElement {
         }
     }
     refreshGrossWeight(_force = false) {
-        let isInMetric = BaseAirliners.unitIsMetric(Aircraft.A320_NEO);
+        const isInMetric = BaseAirliners.unitIsMetric(Aircraft.A320_NEO);
         const unit = isInMetric ? "kg" : "lbs";
-        let payloadWeight = 0;
-        let payloadCount = SimVar.GetSimVarValue("PAYLOAD STATION COUNT", "number");
-        let fuelWeight = Math.round(SimVar.GetSimVarValue("FUEL TOTAL QUANTITY WEIGHT", unit));
-        let emptyWeight = Math.round(SimVar.GetSimVarValue("EMPTY WEIGHT", unit));
-        for (let i = 1; i <= payloadCount; i++) {
-            payloadWeight += Math.round(SimVar.GetSimVarValue(`PAYLOAD STATION WEIGHT:${i}`, unit));
-        }
+        const fuelWeight = SimVar.GetSimVarValue("FUEL TOTAL QUANTITY WEIGHT", unit);
+        const emptyWeight = SimVar.GetSimVarValue("EMPTY WEIGHT", unit);
+        const payloadWeight = this.getPayloadWeight(unit);
         const gw = Math.round(emptyWeight + fuelWeight + payloadWeight);
         if ((gw != this.currentGW) || _force) {
             this.currentGW = gw;
             if (this.gwValue != null) {
-                this.gwValue.textContent = this.currentGW.toString();
+                // Lower EICAS displays GW in increments of 100
+                this.gwValue.textContent = (Math.floor(this.currentGW / 100) * 100).toString();
             }
             if (this.gwUnit) {
                 this.gwUnit.textContent = unit.toUpperCase();
             }
         }
+    }
+    getPayloadWeight(unit) {
+        const payloadCount = SimVar.GetSimVarValue("PAYLOAD STATION COUNT", "number");
+        let payloadWeight = 0;
+        for (let i = 1; i <= payloadCount; i++) {
+            payloadWeight += SimVar.GetSimVarValue(`PAYLOAD STATION WEIGHT:${i}`, unit);
+        }
+        return payloadWeight;
     }
     refreshADIRS() {
         if (this.tatText != null && this.satText != null) {
