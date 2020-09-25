870b1e1eae6a77bd0f1c9e072b688f2f22128543
lousybyte <lousybyte@users.noreply.github.com>
2020-09-25 16:13:53 +0300

    [CDU] Add support for UI magenta color text

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/FMC/A32NX_FMCMainDisplay.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -90,7 +90,7 @@ class FMCMainDisplay extends BaseAirliners {
             color = "white";
         }
         this._title = content.split("[color]")[0];
-        this._titleElement.classList.remove("white", "blue", "yellow", "green", "red");
+        this._titleElement.classList.remove("white", "blue", "yellow", "green", "red", "magenta");
         this._titleElement.classList.add(color);
         this._titleElement.textContent = this._title;
     }
@@ -162,7 +162,7 @@ class FMCMainDisplay extends BaseAirliners {
                 color = "white";
             }
             let e = this._labelElements[row][col];
-            e.classList.remove("white", "blue", "yellow", "green", "red");
+            e.classList.remove("white", "blue", "yellow", "green", "red", "magenta");
             e.classList.add(color);
             label = label.split("[color]")[0];
         }
@@ -208,7 +208,7 @@ class FMCMainDisplay extends BaseAirliners {
                 color = "white";
             }
             let e = this._lineElements[row][col];
-            e.classList.remove("white", "blue", "yellow", "green", "red");
+            e.classList.remove("white", "blue", "yellow", "green", "red", "magenta");
             e.classList.add(color);
             content = content.split("[color]")[0];
         }
