02395221000243c72b5829d4e75f38732a3493c8
nikotina91 <43645128+nikotina91@users.noreply.github.com>
2020-10-14 15:06:34 +0200

    gauges now work

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/EICAS/ECAM/A320_Neo_LowerECAM_PRESS.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/EICAS/ECAM/A320_Neo_LowerECAM_PRESS.js
@@ -18,11 +18,43 @@ var A320_Neo_LowerECAM_PRESS;
         }
         init() {
             this.isInitialised = true;
+
+            this.htmlPsiInt = this.querySelector("#psi-gauge-int");
+            this.htmlPsiDecimal = this.querySelector("#psi-gauge-decimal");
+            this.htmlPsiIndicator = this.querySelector("#delta-psi-indicator");
+
+            this.htmlCabinVSValue = this.querySelector("#v-s-value");
+            this.htmlCabinVSIndicator = this.querySelector("#v-s-indicator");
+
+            this.htmlCabinAltValue = this.querySelector("#cabin-altitude-value");
+            this.htmlCabinAltIndicator = this.querySelector("#cabin-alt-indicator");
         }
         update(_deltaTime) {
             if (!this.isInitialised) {
                 return;
             }
+
+            //psi delta gauge
+            const pressureDelta = SimVar.GetSimVarValue("PRESSURIZATION PRESSURE DIFFERENTIAL", "Pounds per square foot") / 144;
+            const pressureDeltaInt = parseInt(pressureDelta);
+            const pressureDeltaDecimal = parseInt((pressureDelta - pressureDeltaInt) * 10);
+
+            this.htmlPsiInt.textContent = pressureDeltaInt + ".";
+            this.htmlPsiDecimal.textContent = pressureDeltaDecimal;
+            this.htmlPsiIndicator.setAttribute("style", "transform-origin: 100px 152.5px; transform: rotate("+ (pressureDelta * 19.375) +"deg); stroke-width: 3px; stroke-linecap: round;");
+            
+            //cabin v/s gauge
+            const cabinVSValue = SimVar.GetSimVarValue("PRESSURIZATION CABIN ALTITUDE RATE", "feet per second") * 60;
+
+            this.htmlCabinVSValue.textContent = parseInt(cabinVSValue);
+            this.htmlCabinVSIndicator.setAttribute("style", "transform-origin: 100px 152.5px; transform: rotate("+ (cabinVSValue * 0.045) +"deg); stroke-width: 3px; stroke-linecap: round;");
+
+            //cabin alt gauge
+            const cabinAltitude = SimVar.GetSimVarValue("PRESSURIZATION CABIN ALTITUDE", "feet");
+
+            this.htmlCabinAltValue.textContent = parseInt(cabinAltitude);
+            this.htmlCabinAltIndicator.setAttribute("style", "transform-origin: 100px 152.5px; transform: rotate("+ (cabinAltitude * 0.0164) +"deg); stroke-width: 3px; stroke-linecap: round;");
+
         }
     }
     A320_Neo_LowerECAM_PRESS.Page = Page;
