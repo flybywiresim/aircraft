431c380492a20436582b0fadc6bcf85a780310c8
Gus Caplan <me@gus.host>
2020-10-30 17:30:27 -0500

    fix indent

--- src/instruments/template/template.js
+++ src/instruments/template/template.js
@@ -7,53 +7,53 @@
 
 // eslint-disable-next-line camelcase
 class A32NX_INSTRUMENT_NAME_Logic extends BaseInstrument {
-  get templateID() {
-    return 'A32NX_INSTRUMENT_NAME_TEMPLATE';
-  }
-
-  get isInteractive() {
-    // eslint-disable-next-line
-    return INSTRUMENT_IS_INTERACTIVE;
-  }
-
-  get IsGlassCockpit() {
-    return true;
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-
-    Include.addScript('/JS/debug.js', () => {
-      g_modDebugMgr.AddConsole(null);
-    });
-
-    // This is big hack, see `template.html`.
-    {
-      const code = document.getElementById('A32NX_BUNDLED_STYLE').innerHTML;
-      const style = document.createElement('style');
-      style.innerHTML = code;
-      document.head.appendChild(style);
+    get templateID() {
+        return 'A32NX_INSTRUMENT_NAME_TEMPLATE';
     }
-    {
-      const code = document.getElementById('A32NX_BUNDLED_LOGIC').innerHTML;
-      const script = document.createElement('script');
-      script.innerHTML = code;
-      document.body.appendChild(script);
-    }
-  }
 
-  Update() {
-    super.Update();
-    if (this.CanUpdate()) {
-      this.dispatchEvent(new CustomEvent('update', { detail: this.deltaTime }));
+    get isInteractive() {
+        // eslint-disable-next-line
+        return INSTRUMENT_IS_INTERACTIVE;
     }
-  }
 
-  onInteractionEvent(event) {
-    const eventName = String(event);
-    this.dispatchEvent(new CustomEvent(eventName));
-    this.dispatchEvent(new CustomEvent('*', { detail: eventName }));
-  }
+    get IsGlassCockpit() {
+        return true;
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        Include.addScript('/JS/debug.js', () => {
+            g_modDebugMgr.AddConsole(null);
+        });
+
+        // This is big hack, see `template.html`.
+        {
+            const code = document.getElementById('A32NX_BUNDLED_STYLE').innerHTML;
+            const style = document.createElement('style');
+            style.innerHTML = code;
+            document.head.appendChild(style);
+        }
+        {
+            const code = document.getElementById('A32NX_BUNDLED_LOGIC').innerHTML;
+            const script = document.createElement('script');
+            script.innerHTML = code;
+            document.body.appendChild(script);
+        }
+    }
+
+    Update() {
+        super.Update();
+        if (this.CanUpdate()) {
+            this.dispatchEvent(new CustomEvent('update', { detail: this.deltaTime }));
+        }
+    }
+
+    onInteractionEvent(event) {
+        const eventName = String(event);
+        this.dispatchEvent(new CustomEvent(eventName));
+        this.dispatchEvent(new CustomEvent('*', { detail: eventName }));
+    }
 }
 
 registerInstrument('a32nx-INSTRUMENT_NAME_LOWER-element', A32NX_INSTRUMENT_NAME_Logic);
