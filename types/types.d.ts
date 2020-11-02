030468d73156b1f5a47b4c2230ad54a607751c9a
David Walschots <davidwalschots@users.noreply.github.com>
2020-11-02 19:22:28 +0100

    Add dataStorage.js

--- types/types.d.ts
+++ types/types.d.ts
@@ -1574,6 +1574,28 @@ declare global {
     }
 }
 
+// fs-base-ui/html_ui/JS/dataStorage.js
+declare global {
+    interface DataStorage {
+        getData(key: string): any;
+        searchData(key: string): DataStorageSearchData[] | null;
+        setData(key: string, data: any): any;
+        deleteData(key: string): any;
+    }
+
+    interface DataStorageSearchData {
+        key: any;
+        data: any;
+    }
+
+    function OnDataStorageReady(): void;
+    function GetDataStorage(): DataStorage | void;
+    function GetStoredData(key: string): any;
+    function SearchStoredData(key: string): any;
+    function SetStoredData(key: string, data: any): any;
+    function DeleteStoredData(key: string): any;
+}
+
 // fs-base-ui/html_ui/JS/SimPlane.js
 declare global {
     namespace Simplane {
