79898e56bd41abb2e3713aa6f4415f33a3ab0e71
Niklas Steiner <niklas@sbg.at>
2020-11-03 21:11:06 +0100

    Port ATSU to new API

--- A32NX/html_ui/Pages/A32NX_Core/A32NX_ATSU.js
+++ A32NX/html_ui/Pages/A32NX_Core/A32NX_ATSU.js
@@ -42,20 +42,18 @@ const getMETAR = async (icaos, lines, store, updateView) => {
     const storedMetarSrc = NXDataStore.get("CONFIG_METAR_SRC", "MSFS");
     for (const icao of icaos) {
         if (icao !== "") {
-            await fetch(`${endpoint}metar?source=${srcMap[storedMetarSrc]}&icao=${icao}`)
-                .then((response) => response.text())
+            await fetch(`${endpoint}metar/{icao}?source=${srcMap[storedMetarSrc]}`)
+                .then((response) => response.json())
                 .then((data) => {
-                    const error = data.slice(0, 9) == "FBW_ERROR";
-                    if (!error) {
-                        lines.push(`METAR ${icao}[color]blue`);
-                        const newLines = wordWrapToStringList(data, 25);
-                        newLines.forEach(l => lines.push(l.concat("[color]green")));
-                        lines.push(msgSep);
-                    } else {
-                        lines.push(`METAR ${icao}[color]blue`);
-                        lines.push('STATION NOT AVAILABLE[color]red');
-                        lines.push(msgSep);
-                    }
+                    lines.push(`METAR ${icao}[color]blue`);
+                    const newLines = wordWrapToStringList(data.metar, 25);
+                    newLines.forEach(l => lines.push(l.concat("[color]green")));
+                    lines.push(msgSep);
+                })
+                .catch(() => {
+                    lines.push(`METAR ${icao}[color]blue`);
+                    lines.push('STATION NOT AVAILABLE[color]red');
+                    lines.push(msgSep);
                 });
         }
     }
@@ -67,20 +65,18 @@ const getTAF = async (icaos, lines, store, updateView) => {
     const storedTafSrc = NXDataStore.get("CONFIG_TAF_SRC", "NOAA");
     for (const icao of icaos) {
         if (icao !== "") {
-            await fetch(`${endpoint}taf?source=${srcMap[storedTafSrc]}&icao=${icao}`)
-                .then((response) => response.text())
+            await fetch(`${endpoint}taf/${icao}?source=${srcMap[storedTafSrc]}`)
+                .then((response) => response.json())
                 .then((data) => {
-                    const error = data.slice(0, 9) == "FBW_ERROR";
-                    if (!error) {
-                        lines.push(`TAF ${icao}[color]blue`);
-                        const newLines = wordWrapToStringList(data, 25);
-                        newLines.forEach(l => lines.push(l.concat("[color]green")));
-                        lines.push(msgSep);
-                    } else {
-                        lines.push(`TAF ${icao}[color]blue`);
-                        lines.push('STATION NOT AVAILABLE[color]red');
-                        lines.push(msgSep);
-                    }
+                    lines.push(`TAF ${icao}[color]blue`);
+                    const newLines = wordWrapToStringList(data.taf, 25);
+                    newLines.forEach(l => lines.push(l.concat("[color]green")));
+                    lines.push(msgSep);
+                })
+                .catch(() => {
+                    lines.push(`TAF ${icao}[color]blue`);
+                    lines.push('STATION NOT AVAILABLE[color]red');
+                    lines.push(msgSep);
                 });
         }
     }
@@ -91,39 +87,37 @@ const getTAF = async (icaos, lines, store, updateView) => {
 const getATIS = async (icao, lines, type, store, updateView) => {
     const storedAtisSrc = NXDataStore.get("CONFIG_ATIS_SRC", "FAA");
     if (icao !== "") {
-        await fetch(`${endpoint}atis?source=${srcMap[storedAtisSrc]}&icao=${icao}`)
+        await fetch(`${endpoint}atis/${icao}?source=${srcMap[storedAtisSrc]}`)
             .then((response) => response.json())
             .then((data) => {
-                const error = "error" in data;
-                if (!error) {
-                    let atisData;
-                    switch (type) {
-                        case 0:
-                            if ("arr" in data) {
-                                atisData = data.arr;
-                            } else {
-                                atisData = data.combined;
-                            }
-                            break;
-                        case 1:
-                            if ("dep" in data) {
-                                atisData = data.dep;
-                            } else {
-                                atisData = data.combined;
-                            }
-                            break;
-                        default:
+                let atisData;
+                switch (type) {
+                    case 0:
+                        if ("arr" in data) {
+                            atisData = data.arr;
+                        } else {
                             atisData = data.combined;
-                    }
-                    lines.push(`ATIS ${icao}[color]blue`);
-                    const newLines = wordWrapToStringList(atisData, 25);
-                    newLines.forEach(l => lines.push(l.concat("[color]green")));
-                    lines.push(msgSep);
-                } else {
-                    lines.push(`ATIS ${icao}[color]blue`);
-                    lines.push('D-ATIS NOT AVAILABLE[color]red');
-                    lines.push(msgSep);
+                        }
+                        break;
+                    case 1:
+                        if ("dep" in data) {
+                            atisData = data.dep;
+                        } else {
+                            atisData = data.combined;
+                        }
+                        break;
+                    default:
+                        atisData = data.combined;
                 }
+                lines.push(`ATIS ${icao}[color]blue`);
+                const newLines = wordWrapToStringList(atisData, 25);
+                newLines.forEach(l => lines.push(l.concat("[color]green")));
+                lines.push(msgSep);
+            })
+            .catch(() => {
+                lines.push(`ATIS ${icao}[color]blue`);
+                lines.push('D-ATIS NOT AVAILABLE[color]red');
+                lines.push(msgSep);
             });
     }
     store["sendStatus"] = "SENT";
