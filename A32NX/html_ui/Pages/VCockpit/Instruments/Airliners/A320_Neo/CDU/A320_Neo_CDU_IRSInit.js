47e25b1a73032530776aa2dbf4d42b2de51b87d5
paul92ilm <paul92ilm@users.noreply.github.com>
2020-11-11 15:45:55 +0100

    Fix space (#1858)
    Co-authored-by: Benjamin Dupont <benjozorkfr@gmail.com>
    Co-authored-by: lukecologne <39596827+lukecologne@users.noreply.github.com>

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/A320_Neo_CDU_IRSInit.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/A320_Neo_CDU_IRSInit.js
@@ -1,12 +1,12 @@
 class CDUIRSInit {
-    static ShowPage(mcdu, lon, originAirportLat, originAirportLon, referenceName, originAirportCoordinates, alignMsg = "ALIGN ON REF  }[color]blue") {
+    static ShowPage(mcdu, lon, originAirportLat, originAirportLon, referenceName, originAirportCoordinates, alignMsg = "ALIGN ON REF}[color]blue") {
         mcdu.clearDisplay();
         mcdu.page.Current = mcdu.page.IRSInit;
         mcdu.setTitle('IRS INIT');
         const checkAligned = SimVar.GetSimVarValue("L:A320_Neo_ADIRS_STATE", "Number");
         if (checkAligned === 0) {
             SimVar.SetSimVarValue("L:A32XN_Neo_ADIRS_ALIGN_TYPE_REF", "Enum", 0);
-            alignMsg = "ALIGN ON REF }[color]blue";
+            alignMsg = "ALIGN ON REF}[color]blue";
         }
         const emptyIRSGpsString = "--°--.--/---°--.--";
         const arrowupdwn = "↑↓";
