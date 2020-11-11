f561677777f5287e02fc2b838f542ac759f55c91
lousybyte <lousybyte@users.noreply.github.com>
2020-11-11 16:42:38 +0200

    [FMS] Fix F-PLN for identical FROM/DEST (#1856)

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/A320_Neo_CDU_FlightPlanPage.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/A320_Neo_CDU_FlightPlanPage.js
@@ -90,7 +90,7 @@ class CDUFlightPlanPage {
             waypointsWithDiscontinuities.push(destination);
         }
         if (mcdu.flightPlanManager.decelWaypoint) {
-            const idx = waypointsWithDiscontinuities.findIndex((e) => e.wp.cumulativeDistanceInFP > mcdu.flightPlanManager.decelWaypoint.cumulativeDistanceInFP);
+            const idx = waypointsWithDiscontinuities.findIndex((e) => e.wp.cumulativeDistanceInFP >= mcdu.flightPlanManager.decelWaypoint.cumulativeDistanceInFP);
             if (idx >= 0 && idx < waypointsWithDiscontinuities.length) {
                 waypointsWithDiscontinuities.splice(idx, 0, {
                     wp: mcdu.flightPlanManager.decelWaypoint,
