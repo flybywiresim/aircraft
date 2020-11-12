33674a778b5cedb31f6fc59adf40a90fb61c31c1
Pessi Päivärinne <pessip@hotmail.fi>
2020-11-12 21:47:36 +0200

    Order the waypoints by distance and add colors
    Revert

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/A320_Neo_CDU_SelectWptPage.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/A320_Neo_CDU_SelectWptPage.js
@@ -16,25 +16,29 @@ class A320_Neo_CDU_SelectWptPage {
             [""],
             ["<RETURN"]
         ];
-        for (let i = 0; i < 5; i++) {
-            function calculateDistance(w) {
-                const planeLla = new LatLongAlt(SimVar.GetSimVarValue("PLANE LATITUDE", "degree latitude"), SimVar.GetSimVarValue("PLANE LONGITUDE", "degree longitude"));
-                return Avionics.Utils.computeGreatCircleDistance(planeLla, w.infos.coordinates);
+        function calculateDistance(w) {
+            const planeLla = new LatLongAlt(SimVar.GetSimVarValue("PLANE LATITUDE", "degree latitude"), SimVar.GetSimVarValue("PLANE LONGITUDE", "degree longitude"));
+            return Avionics.Utils.computeGreatCircleDistance(planeLla, w.infos.coordinates);
+        }
 
-            }
-            const w = waypoints[i + 5 * page];
+        const orderedWaypoints = [...waypoints].filter(Boolean).sort((a, b) => {
+            return calculateDistance(a) - calculateDistance(b);
+        });
+
+        for (let i = 0; i < 5; i++) {
+            const w = orderedWaypoints[i + 5 * page];
             if (w) {
                 let t = "";
                 let freq = 0;
                 if (w.icao[0] === "V") {
                     t = " VOR";
-                    freq = fastToFixed(w.infos.frequencyMHz, 3);
+                    freq = (w.infos.frequencyMHz) ? fastToFixed(w.infos.frequencyMHz, 3).toString() : " ";
                 } else if (w.icao[0] === "N") {
                     t = " NDB";
-                    freq = fastToFixed(w.infos.frequencyMHz, 3);
+                    freq = (w.infos.frequencyMHz) ? fastToFixed(w.infos.frequencyMHz, 3).toString() : " ";
                 } else if (w.icao[0] === "A") {
                     t = " AIRPORT";
-                    freq = fastToFixed(w.infos.frequencies[0].mhValue, 3);
+                    freq = (w.infos.frequencies[0]) ? fastToFixed(w.infos.frequencies[0].mhValue, 3).toString() : " ";
                 }
 
                 const latString = (w.infos.coordinates.lat.toFixed(0) >= 0) ? `${w.infos.coordinates.lat.toFixed(0).toString().padStart(2, "0")}N` : `${Math.abs(w.infos.coordinates.lat.toFixed(0)).toString().padStart(2, "0")}S`;
@@ -42,7 +46,7 @@ class A320_Neo_CDU_SelectWptPage {
 
                 const dist = calculateDistance(w);
 
-                rows[2 * i].splice(0, 1, dist.toFixed(0) + "NM");
+                rows[2 * i].splice(0, 1, "{green}" + dist.toFixed(0) + "{end}NM");
                 rows[2 * i + 1] = ["*" + w.ident + "[color]blue", freq + "[color]green", `${latString}/${longString}[color]green`];
                 mcdu.onLeftInput[i] = () => {
                     callback(w);
@@ -56,18 +60,18 @@ class A320_Neo_CDU_SelectWptPage {
             }
         }
         mcdu.setTemplate([
-            ["DUPLICATE NAMES", (page + 1).toFixed(0), (waypoints.length / 5).toFixed(0)],
+            ["DUPLICATE NAMES", (page + 1).toFixed(0), (orderedWaypoints.length / 5).toFixed(0)],
             ...rows,
             [""]
         ]);
         mcdu.onPrevPage = () => {
             if (page > 0) {
-                A320_Neo_CDU_SelectWptPage.ShowPage(mcdu, waypoints, callback, page - 1);
+                A320_Neo_CDU_SelectWptPage.ShowPage(mcdu, orderedWaypoints, callback, page - 1);
             }
         };
         mcdu.onNextPage = () => {
             if (page < Math.floor(waypoints.length / 5)) {
-                A320_Neo_CDU_SelectWptPage.ShowPage(mcdu, waypoints, callback, page + 1);
+                A320_Neo_CDU_SelectWptPage.ShowPage(mcdu, orderedWaypoints, callback, page + 1);
             }
         };
     }
