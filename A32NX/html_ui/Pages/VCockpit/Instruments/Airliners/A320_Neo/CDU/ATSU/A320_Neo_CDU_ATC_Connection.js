8c6b5effb2dc1317e5827d988b89f4bd16f2f851
Chloe Rodrigues <chloe.1192@outlook.com>
2020-11-19 19:10:38 -0300

    Connection page added

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/ATSU/A320_Neo_CDU_ATC_Connection.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/ATSU/A320_Neo_CDU_ATC_Connection.js
@@ -1,59 +1,19 @@
 class CDUAtcConnection {
-    static ShowPage1(mcdu) {
+    static ShowPage(mcdu) {
         mcdu.clearDisplay();
         mcdu.setTemplate([
-            ["ATC MENU", "1", "2"],
+            ["CONNECTION"],
             [""],
-            ["<LAT REQ", "VERT REQ>"],
-            ["", ""],
-            ["<WHEN CAN WE", "OTHER REQ>"],
-            [""],
-            ["", "TEXT>"],
-            [""],
-            ["<MSG RECORD", "REPORTS>"],
-            [""],
-            ["<CONNECTION", ""],
-            ["ATSU DLK"],
-            ["<RETURN", "EMERGENCY>[color]red"]
-        ]);
-
-        mcdu.leftInputDelay[5] = () => {
-            return mcdu.getDelaySwitchPage();
-        };
-        mcdu.onLeftInput[5] = () => {
-            CDUAtcConnection.ShowPage(mcdu);
-        };
-
-        mcdu.leftInputDelay[5] = () => {
-            return mcdu.getDelaySwitchPage();
-        };
-        mcdu.onLeftInput[5] = () => {
-            CDUAtsuMenu.ShowPage(mcdu);
-        };
-
-        mcdu.onPrevPage = () => {
-            CDUAtcMenu.ShowPage2(mcdu);
-        };
-        mcdu.onNextPage = () => {
-            CDUAtcMenu.ShowPage2(mcdu);
-        };
-    }
-
-    static ShowPage2(mcdu) {
-        mcdu.clearDisplay();
-        mcdu.setTemplate([
-            ["ATC MENU", "2", "2"],
-            [""],
-            ["<DEPART REQ", "ATIS>"],
-            ["", ""],
-            ["<OCEANIC REQ", ""],
+            ["<NOTIFICATION"],
             [""],
             [""],
+            ["CONNECTION"],
+            ["<STATUS"],
             [""],
             [""],
-            [""],
-            [""],
-            ["ATSU DLK"],
+            ["", "MAX UPLINK"],
+            ["", "DELAY>"],
+            ["ATC MENU"],
             ["<RETURN"]
         ]);
 
@@ -61,20 +21,6 @@ class CDUAtcConnection {
             return mcdu.getDelaySwitchPage();
         };
         mcdu.onLeftInput[5] = () => {
-            CDUAtsuMenu.ShowPage(mcdu);
-        };
-
-        mcdu.rightInputDelay[0] = () => {
-            return mcdu.getDelaySwitchPage();
-        };
-        mcdu.onRightInput[0] = () => {
-            CDUAocRequestsAtis.ShowPage(mcdu);
-        };
-
-        mcdu.onPrevPage = () => {
-            CDUAtcMenu.ShowPage1(mcdu);
-        };
-        mcdu.onNextPage = () => {
             CDUAtcMenu.ShowPage1(mcdu);
         };
     }
