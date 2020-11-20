5a6056a85ed87ea4bc26405a0c44db4d344a480d
benjozork <benjamin.dupont.984@gmail.com>
2020-11-20 13:41:10 -0500

    refactor: add back status area to ECAM

---
+++ src/instruments/src/SD/StatusArea/StatusArea.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './StatusArea.scss';
+
+export const StatusArea = () => (
+    <>
+        <path className="sd-status-line" d="M 0   510 h 600" />
+        <path className="sd-status-line" d="M 200 510 v 100" />
+        <path className="sd-status-line" d="M 400 510 v 100" />
+    </>
+);
