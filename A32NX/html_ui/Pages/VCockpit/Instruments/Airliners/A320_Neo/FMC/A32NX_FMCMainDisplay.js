a04a5cdb493e654d2ac8901458e5aecc7aacafc9
Alex Mcbean <alex.mcbean@sainsburys.co.uk>
2020-11-20 09:40:46 +0000

    Reduced fuel pred delay - 21 seconds delay should be about.. 6 - 7 seconds

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/FMC/A32NX_FMCMainDisplay.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -3059,7 +3059,7 @@ class FMCMainDisplay extends BaseAirliners {
      * @returns {number} dynamic delay in ms between 2000ms and 4000ms
      */
     getDelayFuelPred() {
-        return 640 * this.flightPlanManager.getWaypointsCount() + this.flightPlanManager.getDestination().cumulativeDistanceInFP;
+        return 225 * this.flightPlanManager.getWaypointsCount() + this.flightPlanManager.getDestination().cumulativeDistanceInFP;
     }
 
     /**
