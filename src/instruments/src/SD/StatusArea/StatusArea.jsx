bd01334aa88d2e2c80c1b69980327b807f4f6aaa
benjozork <benjamin.dupont.984@gmail.com>
2020-11-21 14:47:42 -0500

    refactor: add back clock

--- src/instruments/src/SD/StatusArea/StatusArea.jsx
+++ src/instruments/src/SD/StatusArea/StatusArea.jsx
@@ -1,18 +1,28 @@
 import './StatusArea.scss';
-import { useState } from 'react';
 import { Text } from '../Text/Text.jsx';
-import { getSimVar, useUpdate } from '../../util.mjs';
+import { useGlobalVar, useSimVar } from '../../util.mjs';
 
 export const StatusArea = () => {
-    const [gw, setGw] = useState(0);
+    const gw = useSimVar('TOTAL WEIGHT', 'kg');
+    const zulu = useGlobalVar('ZULU TIME', 'seconds');
 
-    useUpdate(() => setGw(getSimVar('TOTAL WEIGHT', 'kg')));
+    const seconds = Math.floor(zulu);
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds - (hours * 3600)) / 60);
 
     return (
         <>
             <path className="sd-status-line" d="M 0   510 h 600" />
             <path className="sd-status-line" d="M 200 510 v 100" />
             <path className="sd-status-line" d="M 400 510 v 100" />
+
+            {/* Time */}
+
+            <Text bigValue x={251} y={560} alignStart>{hours}</Text>
+            <Text unit x={290} y={560} alignStart>H</Text>
+            <Text value x={316} y={560} alignStart>{minutes}</Text>
+
+            {/* Gross weight */}
 
             <Text title x={415} y={534} alignStart>
                 GW
