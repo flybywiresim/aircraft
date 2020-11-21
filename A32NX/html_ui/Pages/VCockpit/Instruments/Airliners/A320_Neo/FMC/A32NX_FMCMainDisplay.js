f3534b7167643e03476432e5d9195caa039c4e8f
Kimbyeoungjang <kim9431237@naver.com>
2020-11-21 11:28:52 +0900

    Update A32NX_FMCMainDisplay.js

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/FMC/A32NX_FMCMainDisplay.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -2217,12 +2217,11 @@ class FMCMainDisplay extends BaseAirliners {
         if (s < transALT) {
             this.showErrorMessage("NOT ALLOWED");
             return false;
-        } else {
-            if (isFinite(v) && v > 0) {
-                this.transitionAltitude = v;
-                SimVar.SetSimVarValue("L:AIRLINER_APPR_TRANS_ALT", "Number", v);
-                return true;
-            }
+        }
+        if (isFinite(v) && v > 0) {
+            this.transitionAltitude = v;
+            SimVar.SetSimVarValue("L:AIRLINER_APPR_TRANS_ALT", "Number", v);
+            return true;
         }
         this.showErrorMessage(this.defaultInputErrorMessage);
         return false;
