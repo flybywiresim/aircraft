d6f6fac5a40547bfa67cb468f2fd70fc925f3149
benjozork <benjamin.dupont.984@gmail.com>
2020-11-20 21:55:29 -0500

    refactor: add GW display

---
+++ src/instruments/src/SD/Text/Text.jsx
@@ -0,0 +1,27 @@
+import './Text.scss';
+
+export const Text = (props) => {
+    const {
+        x, y,
+        unit, value, title,
+        alignEnd, alignStart,
+        children,
+    } = props;
+
+    // eslint-disable-next-line no-nested-ternary
+    const produceTextWithClass = (className) => <text x={x} y={y} textAnchor={alignEnd ? 'end' : (alignStart ? 'start' : 'middle')} className={`text-${className}`}>{children}</text>;
+
+    if (unit) {
+        return produceTextWithClass('unit');
+    }
+
+    if (value) {
+        return produceTextWithClass('value');
+    }
+
+    if (title) {
+        return produceTextWithClass('title');
+    }
+
+    return produceTextWithClass('value');
+};
