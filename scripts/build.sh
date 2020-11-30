a9baf216547a182ed9cdc7657eb82ba98adb8fdc
devsnek <me@gus.host>
2020-11-30 11:53:37 -0600

    fix build (#2139)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# get directory of this script relative to root
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 set -ex
 
-npm run build
+# various build commands here:
+npm run build:instruments
 
+"${DIR}/../src/fbw/build.sh"
+
+node "${DIR}/build.js"
+
+# create build_info.json
 if [ -z "${GITHUB_ACTOR}" ]; then
     GITHUB_ACTOR="$(git log -1 --pretty=format:'%an <%ae>')"
 fi
-
 if [ -z "${GITHUB_EVENT_NAME}" ]; then
     GITHUB_EVENT_NAME="manual"
 fi
-
 jq -n \
     --arg built "$(date -u -Iseconds)" \
     --arg ref "$(git show-ref HEAD | awk '{print $2}')" \
@@ -19,4 +26,4 @@ jq -n \
     --arg actor "${GITHUB_ACTOR}" \
     --arg event_name "${GITHUB_EVENT_NAME}" \
     '{ built: $built, ref: $ref, sha: $sha, actor: $actor, event_name: $event_name }' \
-    > ./A32NX/build_info.json
+    > "${DIR}/../A32NX/build_info.json"
