ff8641981ebb58cac781bd8922cc2073278b290d
devsnek <me@gus.host>
2020-11-30 11:53:37 -0600

    fix build (#2139)

--- src/fbw/build.sh
+++ src/fbw/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-OUTPUT=../../A32NX/SimObjects/AirPlanes/Asobo_A320_NEO/panel/airbus-fly-by-wire.wasm
+# get directory of this script relative to root
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+OUTPUT="${DIR}/../../A32NX/SimObjects/AirPlanes/Asobo_A320_NEO/panel/airbus-fly-by-wire.wasm"
 
 set -ex
 
@@ -20,15 +23,15 @@ clang++ \
   -mexec-model=reactor \
   -I "${MSFS_SDK}/WASM/include" \
   -I "${MSFS_SDK}/SimConnect SDK/include" \
-  -I ./src/model \
-  -I ./src/inih \
-  ./src/model/FlyByWire.cpp \
-  ./src/model/FlyByWire_data.cpp \
-  -I ./src/interface \
-  ./src/interface/SimConnectInterface.cpp \
-  ./src/FlyByWireInterface.cpp \
-  ./src/InterpolatingLookupTable.cpp \
-  ./src/main.cpp \
+  -I "${DIR}/src/inih" \
+  -I "${DIR}/src/model" \
+  "${DIR}/src/model/FlyByWire.cpp" \
+  "${DIR}/src/model/FlyByWire_data.cpp" \
+  -I "${DIR}/src/interface" \
+  "${DIR}/src/interface/SimConnectInterface.cpp" \
+  "${DIR}/src/FlyByWireInterface.cpp" \
+  "${DIR}/src/InterpolatingLookupTable.cpp" \
+  "${DIR}/src/main.cpp" \
   -o $OUTPUT
 
 wasm-opt -O4 --strip-debug --strip-producers $OUTPUT -o $OUTPUT
