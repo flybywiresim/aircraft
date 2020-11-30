6daf48e32850c79486b828e46dc7520a30d58229
Andreas Guther <andreas@guther.family>
2020-11-30 07:52:56 +0100

    Latest changes from wasm repository (#2137)
    * Latest changes from wasm repository
    
    - includes fix for throttle issues by handling them directly
    - allows configuration of throttle to map to detents correctly
    - adds support of proper sidestick position display using l-vars
    - description of throttle configuration added
    
    * Fixed comments for copyright

--- src/fbw/src/FlyByWireInterface.h
+++ src/fbw/src/FlyByWireInterface.h
@@ -23,8 +23,9 @@
 
 #include "SimConnectInterface.h"
 #include "FlyByWire.h"
+#include "InterpolatingLookupTable.h"
 
-class FlyByWireInterface
+    class FlyByWireInterface
 {
 public:
   bool connect();
@@ -36,12 +37,26 @@ public:
   );
 
 private:
+  const std::string THROTTLE_CONFIGURATION_FILEPATH = "\\work\\ThrottleConfiguration.ini";
+
+  bool isThrottleHandlingEnabled;
+  bool useReverseOnAxis;
+  double idleThrottleInput;
+
   SimConnectInterface simConnectInterface;
   FlyByWireModelClass model;
+  InterpolatingLookupTable throttleLookupTable;
+
+  ID sideStickPositionX;
+  ID sideStickPositionY;
 
   bool getModelInputDataFromSim(
     double sampleTime
   );
 
   bool writeModelOuputDataToSim();
+
+  void initializeThrottles();
+
+  bool processThrottles();
 };
