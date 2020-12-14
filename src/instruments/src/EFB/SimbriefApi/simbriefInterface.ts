633124fcd0cdca15d77c1843afa1f0588599b4f6
Aaryan Kapoor <myself.aaryankapoor@gmail.com>
2020-12-14 13:34:58 -0500

    a few changes to flight widget

--- src/instruments/src/EFB/SimbriefApi/simbriefInterface.ts
+++ src/instruments/src/EFB/SimbriefApi/simbriefInterface.ts
@@ -1,3 +1,21 @@
+/*
+ * A32NX
+ * Copyright (C) 2020 FlyByWire Simulations and its contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 export interface ISimbriefData {
     origin: IAirportInfo,
     destination: IAirportInfo,
@@ -69,10 +87,10 @@ interface ITimes {
     orig_timezone: number,
     reserve_time: number,
     sched_block: number,
-    sched_in: number,
+    sched_in: string,
     sched_off: number,
     sched_on: number,
-    sched_out: number,
+    sched_out: string,
     sched_time_enroute: number,
     taxi_in: number,
     taxi_out: number,
