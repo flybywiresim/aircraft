944ef578c257c56fc96a42453641de210df033fc
pareil6 <73158172+pareil6@users.noreply.github.com>
2020-12-18 20:32:28 +0000

    [CDU] Allow SimBrief user ID instead of username

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/OPTIONS/A320_Neo_CDU_OPTIONS_AOC_SIMBRIEF.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/OPTIONS/A320_Neo_CDU_OPTIONS_AOC_SIMBRIEF.js
@@ -2,9 +2,13 @@ class CDU_OPTIONS_SIMBRIEF {
     static ShowPage(mcdu) {
         mcdu.clearDisplay();
 
-        const simbriefUsername = NXDataStore.get("CONFIG_SIMBRIEF_USERNAME", "").replace('_', ' ');
+        const simbriefUsername = NXDataStore.get("CONFIG_SIMBRIEF_USERNAME", "").replace(/_/g, ' ');
 
-        const simbriefUsernameString = simbriefUsername != "" ? simbriefUsername : "[ ]";
+        const simbriefUsernameString = simbriefUsername ? simbriefUsername : "[ ]";
+
+        const simbriefUserId = NXDataStore.get("CONFIG_SIMBRIEF_USERID", "");
+
+        const simbriefUserIdString = simbriefUserId ? simbriefUserId : "[ ]";
 
         mcdu.setTemplate([
             ["A32NX OPTIONS"],
@@ -12,8 +16,8 @@ class CDU_OPTIONS_SIMBRIEF {
             [""],
             ["USERNAME"],
             [`${simbriefUsernameString}[color]blue`],
-            [""],
-            [""],
+            ["USER ID"],
+            [`${simbriefUserIdString}[color]blue`],
             [""],
             [""],
             [""],
@@ -29,7 +33,20 @@ class CDU_OPTIONS_SIMBRIEF {
             if (value === FMCMainDisplay.clrValue) {
                 NXDataStore.set("CONFIG_SIMBRIEF_USERNAME", "");
             } else {
-                NXDataStore.set("CONFIG_SIMBRIEF_USERNAME", value.replace(' ', '_'));
+                NXDataStore.set("CONFIG_SIMBRIEF_USERNAME", value.replace(/ /g, '_'));
+                NXDataStore.set("CONFIG_SIMBRIEF_USERID", "");
+            }
+            CDU_OPTIONS_SIMBRIEF.ShowPage(mcdu);
+        };
+
+        mcdu.onLeftInput[2] = (value) => {
+            if (value === FMCMainDisplay.clrValue) {
+                NXDataStore.set("CONFIG_SIMBRIEF_USERID", "");
+            } else if (!/^\d$/.test(value)) {
+                mcdu.showErrorMessage("NOT ALLOWED");
+            } else {
+                NXDataStore.set("CONFIG_SIMBRIEF_USERID", value);
+                NXDataStore.set("CONFIG_SIMBRIEF_USERNAME", "");
             }
             CDU_OPTIONS_SIMBRIEF.ShowPage(mcdu);
         };
