7a3776a83d14c3f99495b62e589e5d4dc6e4f641
benjozork <benjamin.dupont.984@gmail.com>
2020-12-20 18:58:35 -0500

    feat: WASM implementation of flight warning computer

--- src/fwc/src/lib.rs
+++ src/fwc/src/lib.rs
@@ -17,17 +17,66 @@
  */
 
 use msfs::MSFSEvent;
+use msfs::legacy::{NamedVariable, execute_calculator_code};
 
 #[msfs::gauge(name=fwc)]
 async fn demo(mut gauge: msfs::Gauge) -> Result<(), Box<dyn std::error::Error>> {
+    let mut fwc = Fwc {
+        inputs: FwcInputs {
+            flight_phase: 7f64
+        },
+        outputs: FwcOutput {
+            ecam_message: "Hello".to_string()
+        }
+    };
+
     while let Some(event) = gauge.next_event().await {
         match event {
             MSFSEvent::PostUpdate => {
-                println!("update");
+                fwc.update();
+
+                let ecam_message = NamedVariable::from("A32NX_FWC_OUT_MESSAGE");
+                ecam_message.set_value(fwc.inputs.flight_phase);
+
+                execute_calculator_code::<()>("(>H:A32NX_DCDU_FWC)");
             }
             _ => {}
         }
     }
 
     Ok(())
+}
+
+
+struct FwcInputs {
+    flight_phase: f64
+}
+
+struct FwcOutput {
+    ecam_message: String
+}
+
+struct Fwc {
+    inputs: FwcInputs,
+    outputs: FwcOutput
+}
+
+impl Fwc {
+    fn update(&mut self) {
+        self.acquire();
+        self.concentrate();
+        self.output();
+    }
+
+    fn acquire(&self) {
+
+    }
+
+    fn concentrate(&self) {
+
+    }
+
+    fn output(&mut self) {
+        self.outputs.ecam_message = self.inputs.flight_phase.to_string();
+    }
 }
