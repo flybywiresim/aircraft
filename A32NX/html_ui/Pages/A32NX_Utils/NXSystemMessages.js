5eae0b689d5a2325154546535142c286eef81e34
Leon Beeser <leon.beeser@yahoo.de>
2020-12-21 17:44:51 +0100

    separated type I and typeII messages

--- A32NX/html_ui/Pages/A32NX_Utils/NXSystemMessages.js
+++ A32NX/html_ui/Pages/A32NX_Utils/NXSystemMessages.js
@@ -2,21 +2,25 @@
  NXSystemMessages only holds real messages
  */
 const NXSystemMessages = {
-    aocActFplnUplink:       {text: "AOC ACT F-PLN UPLINK", isAmber: false},
-    awyWptMismatch:         {text: "AWY/WPT MISMATCH"},
-    checkMinDestFob:        {text: "CHECK MIN DEST FOB", isAmber: false},
-    destEfobBelowMin:       {text: "DEST EFOB BELOW MIN", isAmber: true},
-    enterDestData:          {text: "ENTER DEST DATA", isAmber: true},
-    entryOutOfRange:        {text: "ENTRY OUT OF RANGE"},
-    formatError:            {text: "FORMAT ERROR"},
-    gpsPrimary:             {text: "GPS PRIMARY", isAmber: false},
-    gpsPrimaryLost:         {text: "GPS PRIMARY LOST", isAmber: true},
-    newCrzAlt:              {text: "NEW CRZ ALT-", isAmber: false},
-    noIntersectionFound:    {text: "NO INTERSECTION FOUND"},
-    notAllowed:             {text: "NOT ALLOWED"},
-    notInDatabase:          {text: "NOT IN DATABASE"},
-    selectDesiredSystem:    {text: "SELECT DESIRED SYSTEM"},
-    uplinkInsertInProg:     {text: "UPLINK INSERT IN PROG", isAmber: false},
-    toSpeedsDisagree:       {text: "V1/VR/V2 DISAGREE", isAmber: true},
-    waitForSystemResponse:  {text: "WAIT FOR SYSTEM RESPONSE"},
+    typeOne: {
+        awyWptMismatch:         {text: "AWY/WPT MISMATCH", isAmber: false},
+        entryOutOfRange:        {text: "ENTRY OUT OF RANGE", isAmber: false},
+        formatError:            {text: "FORMAT ERROR", isAmber: false},
+        noIntersectionFound:    {text: "NO INTERSECTION FOUND", isAmber: false},
+        notAllowed:             {text: "NOT ALLOWED", isAmber: false},
+        notInDatabase:          {text: "NOT IN DATABASE", isAmber: false},
+        selectDesiredSystem:    {text: "SELECT DESIRED SYSTEM", isAmber: false},
+        waitForSystemResponse:  {text: "WAIT FOR SYSTEM RESPONSE", isAmber: false}
+    },
+    typeTwo: {
+        aocActFplnUplink:       {text: "AOC ACT F-PLN UPLINK", isAmber: false},
+        checkMinDestFob:        {text: "CHECK MIN DEST FOB", isAmber: false},
+        destEfobBelowMin:       {text: "DEST EFOB BELOW MIN", isAmber: true},
+        enterDestData:          {text: "ENTER DEST DATA", isAmber: true},
+        gpsPrimary:             {text: "GPS PRIMARY", isAmber: false},
+        gpsPrimaryLost:         {text: "GPS PRIMARY LOST", isAmber: true},
+        newCrzAlt:              {text: "NEW CRZ ALT-", isAmber: false},
+        uplinkInsertInProg:     {text: "UPLINK INSERT IN PROG", isAmber: false},
+        toSpeedsDisagree:       {text: "V1/VR/V2 DISAGREE", isAmber: true}
+    }
 };
