0d66fd38b746ee04a1d05ef520e6d50e4b685864
Leon Beeser <leon.beeser@yahoo.de>
2020-12-21 18:03:15 +0100

    created key for message types

--- A32NX/html_ui/Pages/A32NX_Utils/NXSystemMessages.js
+++ A32NX/html_ui/Pages/A32NX_Utils/NXSystemMessages.js
@@ -2,25 +2,21 @@
  NXSystemMessages only holds real messages
  */
 const NXSystemMessages = {
-    typeOne: {
-        awyWptMismatch:         {text: "AWY/WPT MISMATCH", isAmber: false},
-        entryOutOfRange:        {text: "ENTRY OUT OF RANGE", isAmber: false},
-        formatError:            {text: "FORMAT ERROR", isAmber: false},
-        noIntersectionFound:    {text: "NO INTERSECTION FOUND", isAmber: false},
-        notAllowed:             {text: "NOT ALLOWED", isAmber: false},
-        notInDatabase:          {text: "NOT IN DATABASE", isAmber: false},
-        selectDesiredSystem:    {text: "SELECT DESIRED SYSTEM", isAmber: false},
-        waitForSystemResponse:  {text: "WAIT FOR SYSTEM RESPONSE", isAmber: false}
-    },
-    typeTwo: {
-        aocActFplnUplink:       {text: "AOC ACT F-PLN UPLINK", isAmber: false},
-        checkMinDestFob:        {text: "CHECK MIN DEST FOB", isAmber: false},
-        destEfobBelowMin:       {text: "DEST EFOB BELOW MIN", isAmber: true},
-        enterDestData:          {text: "ENTER DEST DATA", isAmber: true},
-        gpsPrimary:             {text: "GPS PRIMARY", isAmber: false},
-        gpsPrimaryLost:         {text: "GPS PRIMARY LOST", isAmber: true},
-        newCrzAlt:              {text: "NEW CRZ ALT-", isAmber: false},
-        uplinkInsertInProg:     {text: "UPLINK INSERT IN PROG", isAmber: false},
-        toSpeedsDisagree:       {text: "V1/VR/V2 DISAGREE", isAmber: true}
-    }
+    aocActFplnUplink:       {text: "AOC ACT F-PLN UPLINK", isAmber: false, isTypeTwo: true},
+    awyWptMismatch:         {text: "AWY/WPT MISMATCH", isAmber: false, isTypeTwo: false},
+    checkMinDestFob:        {text: "CHECK MIN DEST FOB", isAmber: false, isTypeTwo: true},
+    destEfobBelowMin:       {text: "DEST EFOB BELOW MIN", isAmber: true, isTypeTwo: true},
+    enterDestData:          {text: "ENTER DEST DATA", isAmber: true, isTypeTwo: true},
+    entryOutOfRange:        {text: "ENTRY OUT OF RANGE", isAmber: false, isTypeTwo: false},
+    formatError:            {text: "FORMAT ERROR", isAmber: false, isTypeTwo: false},
+    gpsPrimary:             {text: "GPS PRIMARY", isAmber: false, isTypeTwo: true},
+    gpsPrimaryLost:         {text: "GPS PRIMARY LOST", isAmber: true, isTypeTwo: true},
+    newCrzAlt:              {text: "NEW CRZ ALT-", isAmber: false, isTypeTwo: true},
+    noIntersectionFound:    {text: "NO INTERSECTION FOUND", isAmber: false, isTypeTwo: false},
+    notAllowed:             {text: "NOT ALLOWED", isAmber: false, isTypeTwo: false},
+    notInDatabase:          {text: "NOT IN DATABASE", isAmber: false, isTypeTwo: false},
+    selectDesiredSystem:    {text: "SELECT DESIRED SYSTEM", isAmber: false, isTypeTwo: false},
+    uplinkInsertInProg:     {text: "UPLINK INSERT IN PROG", isAmber: false, isTypeTwo: true},
+    toSpeedsDisagree:       {text: "V1/VR/V2 DISAGREE", isAmber: true, isTypeTwo: true},
+    waitForSystemResponse:  {text: "WAIT FOR SYSTEM RESPONSE", isAmber: false, isTypeTwo: false},
 };
