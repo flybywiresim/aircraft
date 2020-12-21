84b24cd0b35e33942889658f9a97475b3f0a5047
Kimbyeoungjang <kim9431237@naver.com>
2020-12-21 20:06:35 +0900

    Add files via upload

---
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/CDU/OPTIONS/A320_Neo_CDU_OPTIONS_TRANSITION.js
@@ -0,0 +1,87 @@
+class CDU_OPTIONS_TRANSITION {
+    static ShowPage(mcdu) {
+        mcdu.clearDisplay();
+
+        const storedTransAltSettings = NXDataStore.get("CONFIG_TRANSALT", "AUTO");
+        const storedDepartTransAlt = NXDataStore.get("CONFIG_DEPART_TRANSALT", "10000");
+        const storedArrivalTransAlt = NXDataStore.get("CONFIG_ARRIVAL_TRANSALT", "10000");
+
+        let Auto = "*AUTO[color]cyan";
+        let Manual = "*MANUAL[color]cyan";
+
+        switch (storedTransAltSettings) {
+            case "MANUAL":
+                Manual = "MANUAL[color]green";
+                break;
+            default:
+                Auto = "AUTO[color]green";
+        }
+
+        mcdu.setTemplate([
+            ["A32NX OPTIONS REALISM"],
+            ["", "", "TRANS ALT MODE"],
+            [Auto],
+            [""],
+            [Manual],
+            ["", "", "DEFAULT TRANS ALT"],
+            ["DEP TRANS ALT", `{cyan}[${storedDepartTransAlt}]{end}`],
+            [""],
+            ["ARR TRANS ALT", `{cyan}[${storedArrivalTransAlt}]{end}`],
+            [""],
+            [""],
+            [""],
+            ["<RETURN"]
+        ]);
+
+        mcdu.leftInputDelay[0] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[0] = () => {
+            if (storedTransAltSettings != "AUTO") {
+                NXDataStore.set("CONFIG_TRANSALT", "AUTO");
+                CDU_OPTIONS_TRANSITION.ShowPage(mcdu);
+            }
+        };
+        mcdu.leftInputDelay[1] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[1] = () => {
+            if (storedTransAltSettings != "MANUAL") {
+                NXDataStore.set("CONFIG_TRANSALT", "MANUAL");
+                CDU_OPTIONS_TRANSITION.ShowPage(mcdu);
+            }
+        };
+
+        mcdu.onRightInput[2] = (value) => {
+            if (isNaN(value) || parseInt(value) > 50000 || parseInt(value) <= 0) {
+                mcdu.showErrorMessage("NOT ALLOWED");
+            } else {
+                NXDataStore.set("CONFIG_DEPART_TRANSALT", value);
+            }
+            CDU_OPTIONS_TRANSITION.ShowPage(mcdu);
+        };
+
+        mcdu.rightInputDelay[2] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onRightInput[3] = (value) => {
+            if (isNaN(value) || parseInt(value) > 50000 || parseInt(value) <= 0) {
+                mcdu.showErrorMessage("NOT ALLOWED");
+            } else {
+                NXDataStore.set("CONFIG_ARRIVAL_TRANSALT", value);
+            }
+            CDU_OPTIONS_TRANSITION.ShowPage(mcdu);
+        };
+
+        mcdu.rightInputDelay[3] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+
+        mcdu.leftInputDelay[5] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[5] = () => {
+            CDU_OPTIONS_REALISM.ShowPage(mcdu);
+        };
+    }
+}
