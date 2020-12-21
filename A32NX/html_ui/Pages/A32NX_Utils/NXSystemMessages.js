e654f572d324aaa8919851d1b79b39199e0f4681
Leon Beeser <leon.beeser@yahoo.de>
2020-12-21 16:34:39 +0100

    created definition for currently used messages

---
+++ A32NX/html_ui/Pages/A32NX_Utils/NXSystemMessages.js
@@ -0,0 +1,22 @@
+/**
+ NXSystemMessages only holds real messages
+ */
+const NXSystemMessages = {
+    "AOCACTFPLNUPLINK":         {"text": "AOC ACT F-PLN UPLINK", "isAmber": false},
+    "AWYWPTMISMATCH":           {"text": "AWY/WPT MISMATCH"},
+    "CHECKMINDESTFOB":          {"text": "CHECK MIN DEST FOB", "isAmber": false},
+    "DESTEFOBBELOWMIN":         {"text": "DEST EFOB BELOW MIN", "isAmber": true},
+    "ENTERDESTDATA":            {"text": "ENTER DEST DATA", "isAmber": true},
+    "ENTRYOUTOFRANGE":          {"text": "ENTRY OUT OF RANGE"},
+    "FORMATERROR":              {"text": "FORMAT ERROR"},
+    "GPSPRIMARY":               {"text": "GPS PRIMARY", "isAmber": false},
+    "GPSPRIMARYLOST":           {"text": "GPS PRIMARY LOST", "isAmber": true},
+    "NEWCRZALT":                {"text": "NEW CRZ ALT-", "isAmber": false},
+    "NOINTERSECTIONFOUND":      {"text": "NO INTERSECTION FOUND"},
+    "NOTALLOWED":               {"text": "NOT ALLOWED"},
+    "NOTINDATABASE":            {"text": "NOT IN DATABASE"},
+    "SELECTDESIREDSYSTEM":      {"text": "SELECT DESIRED SYSTEM"},
+    "UPLINKINSERTINPROG":       {"text": "UPLINK INSERT IN PROG", "isAmber": false},
+    "V1VRV2DISAGREE":           {"text": "V1/VR/V2 DISAGREE", "isAmber": true},
+    "WAITFORSYSTEMRESPONSE":    {"text": "WAIT FOR SYSTEM RESPONSE"},
+};
