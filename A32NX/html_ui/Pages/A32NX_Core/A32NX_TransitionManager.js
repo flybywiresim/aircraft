58a936f00b52f7965f9c24d3db64d8d1143d29f2
Kimbyeoungjang <kim9431237@naver.com>
2020-12-23 14:32:31 +0900

    Update A32NX_TransitionManager.js

--- A32NX/html_ui/Pages/A32NX_Core/A32NX_TransitionManager.js
+++ A32NX/html_ui/Pages/A32NX_Core/A32NX_TransitionManager.js
@@ -1,8 +1,26 @@
 class A32NX_TransitionManager {
     init() {
-
+        console.log('A32NX_TransitionManager init');
+        const mode = NXDataStore.get("CONFIG_TRANSALT", "AUTO");
+        if (mode!=AUTO) {
+            this.transitionManual();
+        }
     }
     update(_deltaTime, _core) {
+        this.transitionSelector(deltaTime);
+    }
+    transitionSelector(deltaTime) {
+        const mode = NXDataStore.get("CONFIG_TRANSALT", "AUTO");
 
+        if (mode == "AUTO") {
+            
+        }
+    }
+    transitionManual() {
+        const storedDepartTransAlt = NXDataStore.get("CONFIG_DEPART_TRANSALT", "10000");
+        const storedArrivalTransAlt = NXDataStore.get("CONFIG_ARRIVAL_TRANSALT", "10000");
+
+        SimVar.SetSimVarValue("L:AIRLINER_TRANS_ALT", "Number", storedDepartTransAlt);
+        SimVar.SetSimVarValue("L:AIRLINER_APPR_TRANS_ALT", "Number", storedArrivalTransAlt);
     }
 }
