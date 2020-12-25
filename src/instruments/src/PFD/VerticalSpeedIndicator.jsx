ada7a4a663114e05742f4e67a9a44cbd543149ae
lukecologne <lukasmichels00@gmail.com>
2020-12-26 00:38:56 +0100

    Add react vertical speed indicator

---
+++ src/instruments/src/PFD/VerticalSpeedIndicator.jsx
@@ -0,0 +1,88 @@
+export const VerticalSpeedIndicator = ({ radioAlt }) => {
+    // This represents inertial vertical speed
+    const verticalSpeed = SimVar.GetSimVarValue('VELOCITY WORLD Y', 'feet per minute');
+
+    let isAmber = false;
+    if ((Math.abs(verticalSpeed) > 6000 || (radioAlt < 2500 && radioAlt > 1000 && verticalSpeed < -2000) || (radioAlt < 1000 && verticalSpeed < -1200))) {
+        isAmber = true;
+    }
+
+    const absVSpeed = Math.abs(verticalSpeed);
+    const sign = Math.sign(verticalSpeed);
+
+    let yOffset = 0;
+
+    if (absVSpeed < 1000) {
+        yOffset = verticalSpeed / 1000 * -25.5255;
+    } else if (absVSpeed < 2000) {
+        yOffset = (verticalSpeed - sign * 1000) / 1000 * -10.14 - sign * 25.5255;
+    } else if (absVSpeed < 6000) {
+        yOffset = (verticalSpeed - sign * 2000) / 4000 * -10.14 - sign * 35.6655;
+    } else {
+        yOffset = sign * -45.8055;
+    }
+
+    return (
+        <g>
+            <path className="TapeBackground" d="m151.84 131.72 4.1301-15.623v-70.556l-4.1301-15.623h-5.5404v101.8z" />
+            <g id="VerticalSpeedGroup">
+                <g className="Fill White">
+                    <path d="m151.84 54.566v1.4615h-1.914v-1.4615z" />
+                    <path d="m151.84 44.426v1.4615h-1.914v-1.4615z" />
+                    <path d="m149.93 35.685v-1.2095h1.914v1.2095z" />
+                    <path d="m151.84 107.08v-1.4615h-1.914v1.4615z" />
+                    <path d="m151.84 117.22v-1.4615h-1.914v1.4615z" />
+                    <path d="m151.84 125.96h-1.914v1.2095h1.914z" />
+                </g>
+                <g className="NormalStroke White">
+                    <path d="m149.93 68.122h1.7125h0" />
+                    <path d="m151.84 50.221h-1.914h0" />
+                    <path d="m151.84 40.069h-1.914h0" />
+                    <path d="m149.93 93.522h1.7125h0" />
+                    <path d="m151.84 111.42h-1.914h0" />
+                    <path d="m151.84 121.58h-1.914h0" />
+                </g>
+                <g className="FontSmallest MiddleAlign Fill White">
+                    <text x="148.07108" y="108.12872">1</text>
+                    <text x="148.14471" y="118.18059">2</text>
+                    <text x="148.07108" y="128.30609">6</text>
+                    <text x="148.09711" y="56.916523">1</text>
+                    <text x="148.06529" y="46.855984">2</text>
+                    <text x="148.11371" y="36.795124">6</text>
+                </g>
+                <path className="Fill Yellow" d="m145.8 81.679v-1.7135h6.0441v1.7135z" />
+                <VSpeedNeedle isAmber={isAmber} yOffset={yOffset} />
+                <VSpeedText yOffset={yOffset} isAmber={isAmber} VSpeed={verticalSpeed} />
+            </g>
+        </g>
+    );
+};
+
+const VSpeedNeedle = ({ yOffset, isAmber }) => {
+    const className = `NormalStroke ${isAmber ? 'Amber' : 'Green'}`;
+
+    return (
+        <path className={className} id="VSpeedIndicator" d={`m162.74 80.822 l -12.125 ${yOffset}`} />
+    );
+};
+
+const VSpeedText = ({ VSpeed, yOffset, isAmber }) => {
+    const absVSpeed = Math.abs(VSpeed);
+    const sign = Math.sign(VSpeed);
+
+    if (absVSpeed < 200) {
+        return null;
+    }
+
+    const textOffset = yOffset - sign * 2.4;
+
+    const text = (Math.round(absVSpeed / 100) < 10 ? '0' : '') + Math.round(absVSpeed / 100).toString();
+    const className = `FontSmallest MiddleAlign ${isAmber ? 'Amber' : 'Green'}`;
+
+    return (
+        <g id="VSpeedTextGroup" transform={`translate(0 ${textOffset})`}>
+            <path className="BackgroundFill" d="m158.4 83.011h-7.0514v-4.3989h7.0514z" />
+            <text id="VSpeedText" className={className} x="154.84036" y="82.554581">{text}</text>
+        </g>
+    );
+};
--- src/instruments/src/PFD/index.jsx
+++ src/instruments/src/PFD/index.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import { useState } from 'react';
 import { Horizon } from './AttitudeIndicatorHorizon.jsx';
+import { VerticalSpeedIndicator } from './VerticalSpeedIndicator.jsx';
 import {
     renderTarget,
     useUpdate,
@@ -38,6 +39,7 @@ function PFD() {
                 className="BackgroundFill"
                 d="m32.138 145.34h73.536v10.382h-73.536zm0-44.092c7.4164 13.363 21.492 21.652 36.768 21.652 15.277 0 29.352-8.2886 36.768-21.652v-40.859c-7.4164-13.363-21.492-21.652-36.768-21.652-15.277 0-29.352 8.2886-36.768 21.652zm-32.046 57.498h158.66v-158.75h-158.66zm115.14-35.191v-85.473h15.609v85.473zm-113.33 0v-85.473h27.548v85.473z"
             />
+            <VerticalSpeedIndicator radioAlt={radioAlt} />
         </svg>
     );
 }
