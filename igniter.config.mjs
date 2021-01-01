7fd5d08506d7c258879d36102f845a1d6647bbb8
benjozork <benjamin.dupont.984@gmail.com>
2020-12-31 20:19:29 -0500

    feat: improved flight plan manager based on WorkingTitle implementation
    Co-authored-by: Benedict Etzel <developer@beheh.de>

--- igniter.config.mjs
+++ igniter.config.mjs
@@ -7,6 +7,7 @@ export default new TaskOfTasks('a32nx', [
         new ExecTask('failures','npm run build:failures', ['src/failures', 'flybywire-aircraft-a320-neo/html_ui/JS/generated/failures.js']),
         new ExecTask('behavior','node src/behavior/build.js', ['src/behavior', 'flybywire-aircraft-a320-neo/ModelBehaviorDefs/A32NX/generated']),
         new ExecTask('model','node src/model/build.js', ['src/model', 'flybywire-aircraft-a320-neo/SimObjects/AirPlanes/FlyByWire_A320_NEO/model']),
+        new ExecTask('fmgc','npm run build:fmgc', ['src/fmgc', 'flybywire-aircraft-a320-neo/html_ui/JS/fmgc']),
         new ExecTask('systems', [
             'cargo build --target wasm32-wasi --release',
             'wasm-opt -O3 -o flybywire-aircraft-a320-neo/SimObjects/AirPlanes/FlyByWire_A320_NEO/panel/systems.wasm target/wasm32-wasi/release/systems.wasm',
