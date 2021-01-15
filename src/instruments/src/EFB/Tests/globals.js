899cfb99dd144273a9892c8861f78ff278171443
benjozork <benjamin.dupont.984@gmail.com>
2021-01-15 18:58:09 -0500

    fix: disable ESLint in shim code

--- src/instruments/src/EFB/Tests/globals.js
+++ src/instruments/src/EFB/Tests/globals.js
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* eslint-disable */
+
 const simvars = {
     'ATC FLIGHT NUMBER': 'AB123',
     'GPS POSITION LAT': 0,
