b7a0fc6246967e700bb91165238de1acd66e0388
Saschl <sascharudolf46@gmail.com>
2021-02-01 03:05:32 +0100

    One more whitespcace

--- src/instruments/src/EFB/Ground/Ground.tsx
+++ src/instruments/src/EFB/Ground/Ground.tsx
@@ -77,7 +77,7 @@ class Ground extends React.Component<GroundProps, GroundState> {
             <div className="wrapper flex-grow flex flex-col">
                 <div className="pushback control-grid">
                     <h1 className="text-white font-medium text-xl">Pushback</h1>
-                    <div onClick={() => this.state.tugStatus?this.togglePushback():{}} className="stop"><IconHandStop/></div>
+                    <div onClick={() => this.state.tugStatus ? this.togglePushback() : {}} className="stop"><IconHandStop/></div>
                     <div onClick={this.setTugHeading(GroundServices.PUSHBACK_TURN, 90, "number")} className="down-left"><IconCornerDownLeft/></div>
                     <div onClick={this.setTugHeading(GroundServices.PUSHBACK_TURN, 0, "number")} className="down selected"><IconArrowDown /></div>
                     <div onClick={this.setTugHeading(GroundServices.PUSHBACK_TURN, 270, "number")} className="down-right"><IconCornerDownRight/></div>
