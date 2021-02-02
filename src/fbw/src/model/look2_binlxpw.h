dfa09b2ec4771a65b415452c30ae091c2bd90fb4
Andreas Guther <andreas@guther.family>
2021-02-02 16:57:07 +0100

    feat: added custom fly-by-wire, autopilot and autothrust system
    see docs/README.md for further details

---
+++ src/fbw/src/model/look2_binlxpw.h
@@ -0,0 +1,9 @@
+#ifndef RTW_HEADER_look2_binlxpw_h_
+#define RTW_HEADER_look2_binlxpw_h_
+#include "rtwtypes.h"
+
+extern real_T look2_binlxpw(real_T u0, real_T u1, const real_T bp0[], const real_T bp1[], const real_T table[], const
+  uint32_T maxIndex[], uint32_T stride);
+
+#endif
+
