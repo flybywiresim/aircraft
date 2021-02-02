4f5b36423b6ab6fddd4c2beed57e79722a038f01
Kimbyeoungjang <kim9431237@naver.com>
2021-02-03 01:34:23 +0900

    move to FWC

--- A32NX/html_ui/Pages/A32NX_Core/A32NX_FWC.js
+++ A32NX/html_ui/Pages/A32NX_Core/A32NX_FWC.js
@@ -56,6 +56,16 @@ class A32NX_FWC {
 
         // ESDL 1. 0.320
         this.memoLdgInhibit_conf01 = new NXLogic_ConfirmNode(3, true); // CONF 01
+
+        // master warning & caution buttons
+        this.warningPressed = false;
+        this.cautionPressed = false;
+
+        // altitude warning
+        this.previousTargetAltitude = NaN;
+        this._wasBellowThreshold = false;
+        this._wasAboveThreshold = false;
+        this._wasInRange = false;
     }
 
     update(_deltaTime, _core) {
@@ -65,6 +75,8 @@ class A32NX_FWC {
         this._updateButtons(_deltaTime);
         this._updateTakeoffMemo(_deltaTime);
         this._updateLandingMemo(_deltaTime);
+
+        this.altitudeWarning(_deltaTime);
     }
 
     _resetPulses() {
@@ -89,6 +101,17 @@ class A32NX_FWC {
 
         const inhibOverride = this.memoFlightPhaseInhibOvrd_memo.write(recall, this.flightPhaseEndedPulse);
         SimVar.SetSimVarValue("L:A32NX_FWC_INHIBOVRD", "Bool", inhibOverride);
+
+        if (SimVar.GetSimVarValue("L:PUSH_AUTOPILOT_MASTERAWARN_L", "Bool") || SimVar.GetSimVarValue("L:PUSH_AUTOPILOT_MASTERAWARN_R", "Bool")) {
+            this.warningPressed = true;
+        } else {
+            this.warningPressed = false;
+        }
+        if (SimVar.GetSimVarValue("L:PUSH_AUTOPILOT_MASTERCAUT_L", "Bool") || SimVar.GetSimVarValue("L:PUSH_AUTOPILOT_MASTERCAUT_R", "Bool")) {
+            this.cautionPressed = true;
+        } else {
+            this.cautionPressed = false;
+        }
     }
 
     _updateFlightPhase(_deltaTime) {
@@ -285,5 +308,81 @@ class A32NX_FWC {
 
         this.ldgMemo = showInApproach || invalidRadioMemo || this.flightPhase === 8 || this.flightPhase === 7;
         SimVar.SetSimVarValue("L:A32NX_FWC_LDGMEMO", "Bool", this.ldgMemo);
+    }
+
+    altitudeWarning(_deltaTime) {
+        let indicatedAltitude = Simplane.getAltitude();
+        SimVar.SetSimVarValue("L:A32NX_ALT_DEVIATION_SHORT", "Bool", false);
+        SimVar.SetSimVarValue("L:A32NX_ALT_DEVIATION", "Bool", false);
+
+        // Exit when:
+        // - Landing gear down
+        // - Glide slope captured
+        const landingGearIsDown = !SimVar.GetSimVarValue("IS GEAR RETRACTABLE", "Boolean") || SimVar.GetSimVarValue("GEAR HANDLE POSITION", "Boolean");
+        const glideSlopeCaptured = SimVar.GetSimVarValue("L:GLIDE_SLOPE_CAPTURED", "bool") === 1;
+        if (landingGearIsDown || glideSlopeCaptured) {
+            return;
+        }
+
+        const currentAltitudeConstraint = SimVar.GetSimVarValue("L:A32NX_AP_CSTN_ALT", "feet");
+        // Use the constraint altitude if provided otherwise use selected altitude lock value
+        const targetAltitude = currentAltitudeConstraint && !this.getAutopilotMode() ? currentAltitudeConstraint : Simplane.getAutoPilotSelectedAltitudeLockValue();
+
+        // Exit when selected altitude is being changed
+        if (this.previousTargetAltitude !== targetAltitude) {
+            this.previousTargetAltitude = targetAltitude;
+            this._wasBellowThreshold = false;
+            this._wasAboveThreshold = false;
+            this._wasInRange = false;
+            return;
+        }
+
+        if (warningLeft === 1 || warningRight === 1) {
+            this._wasBellowThreshold = false;
+            this._wasInRange = false;
+            SimVar.SetSimVarValue("L:A32NX_ALT_DEVIATION", "Bool", false);
+            return;
+        }
+
+        const delta = Math.abs(indicatedAltitude - targetAltitude);
+
+        if (delta < 250) {
+            this._wasBellowThreshold = true;
+            this._wasAboveThreshold = false;
+        }
+
+        if (750 < delta) {
+            this._wasAboveThreshold = true;
+            this._wasBellowThreshold = false;
+        }
+
+        if (250 <= delta && delta <= 750) {
+            this._wasInRange = true;
+        }
+
+        if (250 <= delta) {
+            if (this._wasBellowThreshold) {
+                SimVar.SetSimVarValue("L:A32NX_ALT_DEVIATION", "Bool", 1);
+            } else if (this._wasAboveThreshold && delta <= 750) {
+                if (SimVar.GetSimVarValue("L:XMLVAR_Autopilot_1_Status", "Bool") === true && SimVar.GetSimVarValue("L:XMLVAR_Autopilot_2_Status", "Bool") === true) {
+                    SimVar.SetSimVarValue("L:A32NX_ALT_DEVIATION_SHORT", "Bool", true);
+                }
+            } else if (750 < delta && this._wasInRange) {
+                SimVar.SetSimVarValue("L:A32NX_ALT_DEVIATION", "Bool", 1);
+            } else if (this._wasInRange) {
+                if (SimVar.GetSimVarValue("L:XMLVAR_Autopilot_1_Status", "Bool") === true && SimVar.GetSimVarValue("L:XMLVAR_Autopilot_2_Status", "Bool") === true) {
+                    SimVar.SetSimVarValue("L:A32NX_ALT_DEVIATION_SHORT", "Bool", true);
+                }
+            }
+        }
+    }
+
+    getAutopilotMode() {
+        if (this.aircraft == Aircraft.A320_NEO) {
+            if (Simplane.getAutoPilotAltitudeManaged() && SimVar.GetSimVarValue("L:AP_CURRENT_TARGET_ALTITUDE_IS_CONSTRAINT", "number") != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
