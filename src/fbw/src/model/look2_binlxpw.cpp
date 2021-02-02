d36c42da3b63648fa341f2ccc2b6b525a4a97734
Andreas Guther <andreas@guther.family>
2021-02-02 16:57:07 +0100

    feat: added custom fly-by-wire and autopilot system
    see docs/README.md for further details

---
+++ src/fbw/src/model/look2_binlxpw.cpp
@@ -0,0 +1,66 @@
+#include "rtwtypes.h"
+#include "look2_binlxpw.h"
+
+real_T look2_binlxpw(real_T u0, real_T u1, const real_T bp0[], const real_T bp1[], const real_T table[], const uint32_T
+                     maxIndex[], uint32_T stride)
+{
+  real_T frac;
+  uint32_T bpIndices[2];
+  real_T fractions[2];
+  real_T yL_1d;
+  uint32_T iRght;
+  uint32_T bpIdx;
+  uint32_T iLeft;
+  if (u0 <= bp0[0U]) {
+    iLeft = 0U;
+    frac = (u0 - bp0[0U]) / (bp0[1U] - bp0[0U]);
+  } else if (u0 < bp0[maxIndex[0U]]) {
+    bpIdx = maxIndex[0U] >> 1U;
+    iLeft = 0U;
+    iRght = maxIndex[0U];
+    while (iRght - iLeft > 1U) {
+      if (u0 < bp0[bpIdx]) {
+        iRght = bpIdx;
+      } else {
+        iLeft = bpIdx;
+      }
+
+      bpIdx = (iRght + iLeft) >> 1U;
+    }
+
+    frac = (u0 - bp0[iLeft]) / (bp0[iLeft + 1U] - bp0[iLeft]);
+  } else {
+    iLeft = maxIndex[0U] - 1U;
+    frac = (u0 - bp0[maxIndex[0U] - 1U]) / (bp0[maxIndex[0U]] - bp0[maxIndex[0U] - 1U]);
+  }
+
+  fractions[0U] = frac;
+  bpIndices[0U] = iLeft;
+  if (u1 <= bp1[0U]) {
+    iLeft = 0U;
+    frac = (u1 - bp1[0U]) / (bp1[1U] - bp1[0U]);
+  } else if (u1 < bp1[maxIndex[1U]]) {
+    bpIdx = maxIndex[1U] >> 1U;
+    iLeft = 0U;
+    iRght = maxIndex[1U];
+    while (iRght - iLeft > 1U) {
+      if (u1 < bp1[bpIdx]) {
+        iRght = bpIdx;
+      } else {
+        iLeft = bpIdx;
+      }
+
+      bpIdx = (iRght + iLeft) >> 1U;
+    }
+
+    frac = (u1 - bp1[iLeft]) / (bp1[iLeft + 1U] - bp1[iLeft]);
+  } else {
+    iLeft = maxIndex[1U] - 1U;
+    frac = (u1 - bp1[maxIndex[1U] - 1U]) / (bp1[maxIndex[1U]] - bp1[maxIndex[1U] - 1U]);
+  }
+
+  bpIdx = iLeft * stride + bpIndices[0U];
+  yL_1d = (table[bpIdx + 1U] - table[bpIdx]) * fractions[0U] + table[bpIdx];
+  bpIdx += stride;
+  return (((table[bpIdx + 1U] - table[bpIdx]) * fractions[0U] + table[bpIdx]) - yL_1d) * frac + yL_1d;
+}
