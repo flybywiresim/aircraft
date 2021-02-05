73fc0c9c184d739e48e4df89a3f9f46507792033
Kimbyeoungjang <kim9431237@naver.com>
2021-02-05 19:55:07 +0900

    Update NXLogic.js

--- A32NX/html_ui/Pages/A32NX_Utils/NXLogic.js
+++ A32NX/html_ui/Pages/A32NX_Utils/NXLogic.js
@@ -1,254 +1,140 @@
-class NXApi {
-    static getMetar(icao, source) {
-        if (!icao) {
-            throw ("No ICAO provided");
-        }
+/*
+ * This file contains various nodes that can be used for logical processing. Systems like the FWC may use them to
+ * accurately implement their functionality.
+ */
 
-        return fetch(`${NXApi.url}/metar/${icao}?source=${source}`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw (response);
-                }
-
-                return response.json();
-            });
+/**
+ * The following class represents a monostable circuit. It is inspired by the MTRIG nodes as described in the ESLD and
+ * used by the FWC.
+ * When it detects either a rising or a falling edge (depending on it's type) it will emit a signal for a certain time t
+ * after the detection. It is not retriggerable, so a rising/falling edge within t will not reset the timer.
+ */
+class NXLogic_TriggeredMonostableNode {
+    constructor(t, risingEdge = true) {
+        this.t = t;
+        this.risingEdge = risingEdge;
+        this._timer = 0;
+        this._previousValue = null;
     }
-
-    static getAirport(icao) {
-        if (!icao) {
-            throw ("No ICAO provided");
+    write(value, _deltaTime) {
+        if (this._previousValue === null && SimVar.GetSimVarValue("L:A32NX_FWC_SKIP_STARTUP", "Bool")) {
+            this._previousValue = value;
         }
-
-        return fetch(`${NXApi.url}/api/v1/airport/${icao}`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw (response);
-                }
-
-                return response.json();
-            });
-    }
-
-    static getTaf(icao, source) {
-        if (!icao) {
-            throw ("No ICAO provided");
+        if (this.risingEdge) {
+            if (this._timer > 0) {
+                this._timer = Math.max(this._timer - _deltaTime / 1000, 0);
+                this._previousValue = value;
+                return true;
+            } else if (!this._previousValue && value) {
+                this._timer = this.t;
+                this._previousValue = value;
+                return true;
+            }
+        } else {
+            if (this._timer > 0) {
+                this._timer = Math.max(this._timer - _deltaTime / 1000, 0);
+                this._previousValue = value;
+                return true;
+            } else if (this._previousValue && !value) {
+                this._timer = this.t;
+                this._previousValue = value;
+                return true;
+            }
         }
-
-        return fetch(`${NXApi.url}/taf/${icao}?source=${source}`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw (response);
-                }
-
-                return response.json();
-            });
-    }
-
-    static getAtis(icao, source) {
-        if (!icao) {
-            throw ("No ICAO provided");
-        }
-
-        return fetch(`${NXApi.url}/atis/${icao}?source=${source}`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw (response);
-                }
-
-                return response.json();
-            });
-    }
-
-    static connectTelex(flightNo) {
-        // TELEX disabled
-        if (NXDataStore.get("CONFIG_ONLINE_FEATURES_STATUS", "DISABLED") !== "ENABLED") {
-            return Promise.reject(NXApi.disabledError);
-        }
-
-        if (!flightNo) {
-            return Promise.reject(NXApi.disabledError);
-        }
-
-        // Do not reconnect when using the same FLT NBR
-        if (flightNo === NXApi.activeFlight) {
-            return Promise.resolve({
-                flight: NXApi.activeFlight
-            });
-        }
-
-        const oldToken = NXApi.accessToken;
-
-        const connectBody = NXApi.buildTelexBody(flightNo);
-        const headers = {"Content-Type": "application/json"};
-
-        return fetch(`${NXApi.url}/txcxn`, {method: "POST", body: JSON.stringify(connectBody), headers})
-            .then((response) => {
-                if (!response.ok) {
-                    throw (response);
-                }
-
-                // Delete old connection using an override token
-                // in case the new one is successful
-                if (!!oldToken) {
-                    this.disconnectTelex(oldToken);
-                }
-
-                return response.json()
-                    .then((data) => {
-                        NXApi.accessToken = data.accessToken;
-                        NXApi.activeFlight = data.flight;
-                        return data;
-                    });
-            });
-    }
-
-    static updateTelex() {
-        // TELEX disabled
-        if (NXDataStore.get("CONFIG_ONLINE_FEATURES_STATUS", "DISABLED") !== "ENABLED") {
-            return Promise.reject(NXApi.disabledError);
-        }
-
-        // No connection
-        if (!NXApi.hasTelexConnection()) {
-            return Promise.reject(NXApi.disconnectedError);
-        }
-
-        const updateBody = NXApi.buildTelexBody();
-        const headers = {
-            "Content-Type": "application/json",
-            Authorization: NXApi.buildToken()
-        };
-
-        return fetch(`${NXApi.url}/txcxn`, {method: "PUT", body: JSON.stringify(updateBody), headers})
-            .then((response) => {
-                if (!response.ok) {
-                    throw (response);
-                }
-
-                return response.json();
-            });
-    }
-
-    static disconnectTelex(tokenOverride) {
-        // No connection
-        if (!NXApi.hasTelexConnection()) {
-            return Promise.reject(NXApi.disconnectedError);
-        }
-
-        const headers = {
-            Authorization: NXApi.buildToken(tokenOverride)
-        };
-
-        return fetch(`${NXApi.url}/txcxn`, {method: "DELETE", headers})
-            .then((response) => {
-                if (!response.ok) {
-                    throw (response);
-                }
-
-                NXApi.accessToken = "";
-                NXApi.activeFlight = "";
-            });
-    }
-
-    static getTelexMessages() {
-        // TELEX disabled
-        if (NXDataStore.get("CONFIG_ONLINE_FEATURES_STATUS", "DISABLED") !== "ENABLED") {
-            return Promise.reject(NXApi.disabledError);
-        }
-
-        // No connection
-        if (!NXApi.hasTelexConnection()) {
-            return Promise.reject(NXApi.disconnectedError);
-        }
-
-        const headers = {
-            Authorization: NXApi.buildToken()
-        };
-
-        return fetch(`${NXApi.url}/txmsg`, {method: "GET", headers})
-            .then((response) => {
-                if (!response.ok) {
-                    throw (response);
-                }
-
-                return response.json();
-            });
-    }
-
-    static sendTelexMessage(recipient, message) {
-        // TELEX disabled
-        if (NXDataStore.get("CONFIG_ONLINE_FEATURES_STATUS", "DISABLED") !== "ENABLED") {
-            return Promise.reject(NXApi.disabledError);
-        }
-
-        // No connection
-        if (!NXApi.hasTelexConnection()) {
-            return Promise.reject(NXApi.disconnectedError);
-        }
-
-        // No recipient
-        if (!recipient) {
-            return Promise.reject(NXApi.noRecipientError);
-        }
-
-        const body = {
-            to: recipient,
-            message
-        };
-        const headers = {
-            "Content-Type": "application/json",
-            Authorization: NXApi.buildToken()
-        };
-
-        return fetch(`${NXApi.url}/txmsg`, {method: "POST", body: JSON.stringify(body), headers})
-            .then((response) => {
-                if (!response.ok) {
-                    throw (response);
-                }
-            });
-    }
-
-    static hasTelexConnection() {
-        return !!NXApi.accessToken && !!NXApi.activeFlight;
-    }
-
-    static buildToken(tokenOverride) {
-        return `Bearer ${!!tokenOverride ? tokenOverride : NXApi.accessToken}`;
-    }
-
-    static buildTelexBody(flightNo) {
-        const lat = SimVar.GetSimVarValue("PLANE LATITUDE", "degree latitude");
-        const long = SimVar.GetSimVarValue("PLANE LONGITUDE", "degree longitude");
-        const alt = SimVar.GetSimVarValue("PLANE ALTITUDE", "feet");
-        const heading = SimVar.GetSimVarValue("PLANE HEADING DEGREES TRUE", "degree");
-        const acType = SimVar.GetSimVarValue("TITLE", "string");
-        const origin = NXDataStore.get("PLAN_ORIGIN", "");
-        const destination = NXDataStore.get("PLAN_DESTINATION", "");
-        const freetext = NXDataStore.get("CONFIG_ONLINE_FEATURES_STATUS", "DISABLED") === "ENABLED";
-
-        return {
-            location: {
-                x: long,
-                y: lat,
-            },
-            trueAltitude: alt,
-            heading: heading,
-            origin: origin,
-            destination: destination,
-            freetextEnabled: freetext,
-            flight: flightNo,
-            aircraftType: acType,
-        };
+        this._previousValue = value;
+        return false;
     }
 }
 
-NXApi.url = "https://api.flybywiresim.com";
-NXApi.disabledError = "TELEX DISABLED";
-NXApi.disconnectedError = "TELEX DISCONNECTED";
-NXApi.noRecipientError = "NO RECIPIENT";
-NXApi.accessToken = "";
-NXApi.activeFlight = "";
-NXApi.updateRate = 15000;
+/**
+ * The following class represents a "confirmation" circuit, which only passes a signal once it has been stable for a
+ * certain amount of time. It is inspired by the CONF nodes as described in the ESLD and used by the FWC.
+ * When it detects either a rising or falling edge (depending on it's type) it will wait for up to time t and emit the
+ * incoming signal if it was stable throughout t. If at any point the signal reverts during t the state is fully reset,
+ * and the original signal will be emitted again.
+ */
+class NXLogic_ConfirmNode {
+    constructor(t, risingEdge = true) {
+        this.t = t;
+        this.risingEdge = risingEdge;
+        this._timer = 0;
+        this._previousInput = null;
+        this._previousOutput = null;
+    }
+    write(value, _deltaTime) {
+        if (this._previousInput === null && SimVar.GetSimVarValue("L:A32NX_FWC_SKIP_STARTUP", "Bool")) {
+            this._previousInput = value;
+            this._previousOutput = value;
+        }
+        if (this.risingEdge) {
+            if (!value) {
+                this._timer = 0;
+            } else if (this._timer > 0) {
+                this._timer = Math.max(this._timer - _deltaTime / 1000, 0);
+                this._previousInput = value;
+                this._previousOutput = !value;
+                return !value;
+            } else if (!this._previousInput && value) {
+                this._timer = this.t;
+                this._previousInput = value;
+                this._previousOutput = !value;
+                return !value;
+            }
+        } else {
+            if (value) {
+                this._timer = 0;
+            } else if (this._timer > 0) {
+                this._timer = Math.max(this._timer - _deltaTime / 1000, 0);
+                this._previousInput = value;
+                this._previousOutput = !value;
+                return !value;
+            } else if (this._previousInput && !value) {
+                this._timer = this.t;
+                this._previousInput = value;
+                this._previousOutput = !value;
+                return !value;
+            }
+        }
+        this._previousInput = value;
+        this._previousOutput = value;
+        return value;
+    }
+    read() {
+        return this._previousOutput;
+    }
+}
 
-NXDataStore.set("PLAN_ORIGIN", "");
-NXDataStore.set("PLAN_DESTINATION", "");
+/**
+ * The following class represents a flip-flop or memory circuit that can be used to store a single bit. It is inspired
+ * by the S+R nodes as described in the ESLD.
+ * It has two inputs: Set and Reset. At first it will always emit a falsy value, until it receives a signal on the set
+ * input, at which point it will start emitting a truthy value. This will continue until a signal is received on the
+ * reset input, at which point it reverts to the original falsy output. It a signal is sent on both set and reset at the
+ * same time, the input with a star will have precedence.
+ * The NVM flag is not implemented right now but can be used to indicate non-volatile memory storage, which means the
+ * value will persist even when power is lost and subsequently restored.
+ */
+class NXLogic_MemoryNode {
+    /**
+     * @param setStar Whether set has precedence over reset if both are applied simultaneously.
+     * @param nvm Whether the is non-volatile and will be kept even when power is lost.
+     */
+    constructor(setStar = true, nvm = false) {
+        this.setStar = setStar;
+        this.nvm = nvm; // TODO in future, reset non-nvm on power cycle
+        this._value = false;
+    }
+    write(set, reset) {
+        if (set && reset) {
+            this._value = this.setStar;
+        } else if (set && !this._value) {
+            this._value = true;
+        } else if (reset && this._value) {
+            this._value = false;
+        }
+        return this._value;
+    }
+    read() {
+        return this._value;
+    }
+}
