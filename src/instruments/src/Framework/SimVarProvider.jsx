e6d37bdb3b55f6cd92866b7d855fb78b18439176
Benedict Etzel <developer@beheh.de>
2021-02-06 15:41:01 +0100

    Update documentation

--- src/instruments/src/Framework/SimVarProvider.jsx
+++ src/instruments/src/Framework/SimVarProvider.jsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 import { customElement } from '../util.mjs';
 
-const errorCallback = () => {throw Error("useSimVar called outside of SimVarProvider context.")};
-const context = React.createContext({get: errorCallback, set: errorCallback});
+const errorCallback = () => {
+    throw Error('useSimVar was called in a React tree with no SimVarProvider');
+};
+const context = React.createContext({
+    get: errorCallback,
+    set: errorCallback,
+});
 
+/**
+ * This component provides the basic functionality for the useSimVar hooks.
+ * By keeping the last known SimVar values inside this provider, we're
+ * effectively caching each SimVar, so that there is no additional overhead when
+ * using multiple useSimVar hooks for the same SimVar.
+ * For improved performance, this component will only trigger renders when the
+ * "update" custom event is emitted through an instrument.
+ */
 export class SimVarProvider extends React.Component {
     constructor(props) {
         super(props);
@@ -18,14 +31,14 @@ export class SimVarProvider extends React.Component {
         customElement.addEventListener('update', this.onUpdateEvent);
     }
 
-    componentWillMount() {
-        customElement.removeEventListener('update', this.onUpdateEvent);
-    }
-
     shouldComponentUpdate(nextProps, nextState) {
         return Object.entries(nextState.cache).some(([key, {value}]) => {
             return !this.state.cache[key] || this.state.cache[key].value !== value;
         });
+    }
+
+    componentWillUnmount() {
+        customElement.removeEventListener('update', this.onUpdateEvent);
     }
 
     onUpdateEvent(event) {
@@ -116,16 +129,60 @@ export class SimVarProvider extends React.Component {
     }
 }
 
-export const useSimVar = (name, unit, minInterval = 0) => {
+/**
+ * The useSimVar hook provides an easy way to read and write SimVars from React.
+ *
+ * It's signature is similar to useState and it regularly refreshes the SimVar
+ * to ensure your React component stays in sync with the SimVar being modified
+ * from outside your component (like from other components, XML or SimConnect).
+ *
+ * You may optionally specify the maximum refresh interval. If the same SimVar
+ * is used in multiple places, this hook will automatically deduplicate those
+ * for maximum performance, rather than fetching the SimVar multiple times.
+ * Setting the SimVar will instantly cause it to be updated in all other places
+ * within the same React tree.
+ *
+ * @param name The name of the SimVar.
+ * @param unit The unit of the SimVar.
+ * @param maxInterval The maximum time in milliseconds that may elapse before
+ * the next render will cause a SimVar refresh from the simulator. This
+ * parameter is only an upper bound! If another hook requests the same SimVar
+ * with a lower maxInterval, this hook will also benefit from that and refresh
+ * the value more often.
+ *
+ * @example
+ * // the return value is the value itself and a setter, similar to useSate
+ * const [v1, setV1] = useSimVar('L:AIRLINER_V1_SPEED', 'Knots');
+ *
+ * @example
+ * // only refresh the SimVar every 500ms
+ * const [lightsTest] = useSimVar('L:XMLVAR_LTS_Test', 'Bool', 500);
+ *
+ * @returns {[*, (function(*): void)]}
+ */
+export const useSimVar = (name, unit, maxInterval = 0) => {
     const contextValue = React.useContext(context);
 
     React.useEffect(() => {
-        contextValue.register(name, unit, minInterval);
-        return () => contextValue.unregister(name, unit, minInterval);
-    }, [name, unit, minInterval]);
+        // This part of useEffect will be called whenever either:
+        // - the component has just mounted, or
+        // - one the parameters below (name, unit, maxInterval) has changed.
+        // In these cases, we want to register our current parameters with the
+        // SimVarProvider that we access through the context.
+        contextValue.register(name, unit, maxInterval);
+        return () => {
+            // This part of useEffect will be called whenever either:
+            // - one of the parameters below (name, unit, maxInterval) is about
+            //   to change, or
+            // - the component is about to unmount
+            // In these cases, we want to unregister our current parameters from
+            // the SimVar provider, that we again access through the context.
+            contextValue.unregister(name, unit, maxInterval);
+        };
+    }, [name, unit, maxInterval]);
 
     return [
-        contextValue.get(name, unit),
-        (value) => contextValue.set(name, unit, value),
+        contextValue.get(name, unit), // SimVar value
+        (value) => contextValue.set(name, unit, value), // SimVar setter
     ];
-}
+};
