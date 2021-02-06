9e0a8504b1091208de71c851aee4b85b6364e605
Benedict Etzel <developer@beheh.de>
2021-02-06 23:12:24 +0100

    Refactor SimVarProvider

--- src/instruments/src/RMP/index.jsx
+++ src/instruments/src/RMP/index.jsx
@@ -1,20 +1,20 @@
 import ReactDOM from 'react-dom';
-import { renderTarget } from '../util.mjs';
+import { renderTarget } from '../util.js';
 import './style.scss';
 
 import { SevenSegmentDisplay } from './Components/SevenSegmentDisplay.jsx';
-import { SimVarProvider, useSimVar } from '../Framework/SimVarProvider.jsx';
-import { useInteractionEvent } from '../util.mjs';
+import SimVarProvider, { useSimVar } from '../Framework/SimVarProvider.tsx';
 
 function RootInstrumentDisplay() {
-    console.log("render");
     const [lightsTest] = useSimVar('L:XMLVAR_LTS_Test', 'Bool');
     const [toggleSwitch] = useSimVar('L:A32NX_RMP_L_TOGGLE_SWITCH', 'Bool');
 
+    console.log("lightsTest: " + lightsTest);
+
     return (
         <div className="rmp-wrapper">
-            <SevenSegmentDisplay type="string" value={toggleSwitch ? "123.455" : ""} lightsTest={lightsTest} />
-            <SevenSegmentDisplay type="string" value={toggleSwitch ? "123.455" : ""} lightsTest={lightsTest} />
+            <SevenSegmentDisplay type="string" value={toggleSwitch ? '123.455' : ''} lightsTest={lightsTest} />
+            <SevenSegmentDisplay type="string" value={toggleSwitch ? '123.455' : ''} lightsTest={lightsTest} />
             {/* <RadioManagementPanel side="L" lightsTest={lightsTestStatefulSimVar.value} /> */}
             {/* <RadioManagementPanel side="R" lightsTest={lightsTestStatefulSimVar.value} /> */}
         </div>
