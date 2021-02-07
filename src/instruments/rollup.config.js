2afa7cdee144e0fdb701f6f4e0000a57c8ae5997
benjozork <benjamin.dupont.984@gmail.com>
2021-02-06 20:50:21 -0500

    feat: allow converting individual pages to ReactJS

--- src/instruments/rollup.config.js
+++ src/instruments/rollup.config.js
@@ -27,24 +27,65 @@ const commonjs = require('@rollup/plugin-commonjs');
 const replace = require('@rollup/plugin-replace');
 const postcss = require('rollup-plugin-postcss');
 const tailwindcss = require('tailwindcss');
-const template = require('./template/rollup.js');
+
+const instrumentTemplate = require('./template/rollup.js');
+const ecamPageTemplate = require('./ecam-page-template/rollup.js');
 
 const TMPDIR = `${os.tmpdir()}/a32nx-instruments-gen`;
 
 const extensions = ['.ts', '.tsx', '.js', '.jsx', '.mjs'];
 
-function makePostcssPluginList(instrumentName) {
-    const usesTailwind = fs.existsSync(`${__dirname}/src/${instrumentName}/tailwind.config.js`);
+const extraInstruments = [
+    {
+        name: 'door-page',
+        path: 'SD/Pages/Door',
+    },
+];
 
-    return [tailwindcss(usesTailwind ? `${__dirname}/src/${instrumentName}/tailwind.config.js` : undefined)];
+function makePostcssPluginList(instrumentPath) {
+    const usesTailwind = fs.existsSync(`${__dirname}/src/${instrumentPath}/tailwind.config.js`);
+
+    return [tailwindcss(usesTailwind ? `${__dirname}/src/${instrumentPath}/tailwind.config.js` : undefined)];
 }
 
-module.exports = fs.readdirSync(`${__dirname}/src`, { withFileTypes: true })
-    .filter((d) => d.isDirectory() && fs.existsSync(`${__dirname}/src/${d.name}/config.json`))
-    .map(({ name }) => {
-        const config = JSON.parse(fs.readFileSync(`${__dirname}/src/${name}/config.json`));
+function getInstrumentsToCompile() {
+    const baseInstruments = fs.readdirSync(`${__dirname}/src`, { withFileTypes: true })
+        .filter((d) => d.isDirectory() && fs.existsSync(`${__dirname}/src/${d.name}/config.json`));
+
+    return [
+        ...baseInstruments.map(({ name }) => ({ path: name, name, isInstrument: true })),
+        ...extraInstruments.map((def) => ({ ...def, isInstrument: false })),
+    ];
+}
+
+function getTemplatePlugin({ name, config, isInstrument }) {
+    if (isInstrument) {
+        return instrumentTemplate({
+            name,
+            config,
+            getCssBundle() {
+                return fs.readFileSync(`${TMPDIR}/${name}-gen.css`).toString();
+            },
+            outputDir: `${__dirname}/../../A32NX/html_ui/Pages/VCockpit/Instruments/generated`,
+        });
+        // eslint-disable-next-line no-else-return
+    } else {
+        return ecamPageTemplate({
+            name,
+            getCssBundle() {
+                return fs.readFileSync(`${TMPDIR}/${name}-gen.css`).toString();
+            },
+            outputDir: `${__dirname}/../../A32NX/html_ui/Pages/VCockpit/Instruments/generated/EcamPages`,
+        });
+    }
+}
+
+module.exports = getInstrumentsToCompile()
+    .map(({ path, name, isInstrument }) => {
+        const config = JSON.parse(fs.readFileSync(`${__dirname}/src/${path}/config.json`));
+
         return {
-            input: `${__dirname}/src/${name}/${config.index}`,
+            input: `${__dirname}/src/${path}/${config.index}`,
             output: {
                 file: `${TMPDIR}/${name}-gen.js`,
                 format: 'iife',
@@ -81,16 +122,11 @@ module.exports = fs.readdirSync(`${__dirname}/src`, { withFileTypes: true })
                     use: {
                         sass: {},
                     },
-                    plugins: makePostcssPluginList(name),
+                    plugins: makePostcssPluginList(path),
                     extract: `${TMPDIR}/${name}-gen.css`,
                 }),
-                template({
-                    name,
-                    config,
-                    getCssBundle() {
-                        return fs.readFileSync(`${TMPDIR}/${name}-gen.css`).toString();
-                    },
-                    outputDir: `${__dirname}/../../A32NX/html_ui/Pages/VCockpit/Instruments/generated`,
+                getTemplatePlugin({
+                    name, path, config, isInstrument,
                 }),
             ],
         };
