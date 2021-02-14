41fadce9bae888ef1c62cc4a14a9ff82432c7c9d
Akhilesh Anand <akhileshmalini@gmail.com>
2021-02-14 12:15:55 -0800

    Lint Error

--- src/instruments/src/EFB/tailwind.config.js
+++ src/instruments/src/EFB/tailwind.config.js
@@ -61,8 +61,8 @@ module.exports = {
                 '2xl-light': '0 0 50px -20px rgba(255, 255, 255, 0.15)',
             },
             borderWidth: {
-               '6': '6px',
-              }
+               	'6': '6px',
+            },
         },
     },
     variants: {
