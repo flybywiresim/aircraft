arturkuma/a32nx

implement TOD calculator

src/instruments/src/EFB/Service/TODCalculator.tsx
@@ -1,0 +1,105 @@
+/*
+ * A32NX
+ * Copyright (C) 2020-2021 FlyByWire Simulations and its contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {get, orderBy, min, max, inRange, set, last, head} from 'lodash';
+
+type groundSpeed = {from: number, groundSpeed: number};
+type groundSpeedRange= {groundSpeed: number, range: [number, number]};
+
+export default class TODCalculator {
+    private readonly avgGroundSpeed;
+
+    public constructor(private from: number, private to: number, groundSpeeds: groundSpeed[]) {
+        const groundSpeedRanges = this.findGroundSpeedRanges(groundSpeeds);
+        const filteredGroundSpeedRanges = this.filterGroundSpeedRanges(from, to, groundSpeedRanges);
+
+        this.avgGroundSpeed = this.findAverageGroundSpeed(filteredGroundSpeedRanges);
+    }
+
+    public calculateVS(distance: number): number {
+        const time = (distance / this.avgGroundSpeed) * 60;
+
+        return (this.to - this.from) / time;
+    }
+
+    public calculateDistance(vs: number): number {
+        const verticalDistance = Math.abs(this.from - this.to);
+        const verticalDistanceTime = verticalDistance / Math.abs(vs);
+
+        return (this.avgGroundSpeed / 60) * verticalDistanceTime;
+    }
+
+    private findAverageGroundSpeed(groundSpeedsRanges: groundSpeedRange[]) {
+        let sum = 0;
+
+        groundSpeedsRanges.forEach(({groundSpeed, range: [bottom, top]}) => {
+            sum += groundSpeed * (top - bottom);
+        });
+
+        const totalVerticalDistance = get(last(groundSpeedsRanges), ['range', 1]) - get(head(groundSpeedsRanges), ['range', 0]);
+
+        return sum / totalVerticalDistance;
+    }
+
+    private findGroundSpeedRanges(groundSpeeds: groundSpeed[]): groundSpeedRange[] {
+        groundSpeeds = orderBy(groundSpeeds, 'from', 'asc');
+
+        const ranges: groundSpeedRange[] = [];
+
+        for (let i = 0; i < groundSpeeds.length; i++) {
+            const groundSpeed = get(groundSpeeds, [i, 'groundSpeed']);
+            const rangeStart = get(groundSpeeds, [i, 'from']);
+            const rangeEnd = get(groundSpeeds, [i + 1, 'from'], 1000000); // Unrealistic default value to avoid many IFs later
+
+            ranges.push({groundSpeed, range: [rangeStart, rangeEnd]});
+        }
+
+        return ranges;
+    }
+
+    private filterGroundSpeedRanges(from: number, to: number, groundSpeeds: groundSpeedRange[]) {
+        const bottom = min([from, to]);
+        const top = max([from, to]);
+
+        let rangeStartElementIndex;
+        let rangeEndElementIndex;
+
+        for (let i = 0; i < groundSpeeds.length; i++) {
+            const rangeStart = get(groundSpeeds, [i, 'range', 0]);
+            const rangeEnd = get(groundSpeeds, [i, 'range', 1]);
+
+            if (inRange(bottom, rangeStart, rangeEnd)) {
+                rangeStartElementIndex = i;
+            }
+
+            if (inRange(top, rangeStart, rangeEnd)) {
+                rangeEndElementIndex = i;
+            }
+        }
+
+        const rangeStartElement = set({...groundSpeeds[rangeStartElementIndex]}, ['range', 0], bottom);
+
+        if (rangeStartElementIndex !== rangeEndElementIndex) {
+            const rangeEndElement = set({...groundSpeeds[rangeEndElementIndex]}, ['range', 1], top);
+
+            return [rangeStartElement, ...groundSpeeds.slice(rangeStartElementIndex + 1, rangeEndElementIndex), rangeEndElement];
+        }
+
+        return [set({...rangeStartElement}, ['range', 1], top)];
+    }
+}

src/instruments/src/EFB/TODCalculator/Data/Data.tsx
@@ -1,0 +1,78 @@
+/*
+ * A32NX
+ * Copyright (C) 2020-2021 FlyByWire Simulations and its contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { connect } from 'react-redux';
+import Input from "../../Components/Form/Input/Input";
+import React from "react";
+import Card from "../../Components/Card/Card";
+import Divider from "../../Components/Divider/Divider";
+import {TOD_CALCULATOR_REDUCER} from "../../Store";
+import {setTodData} from "../../Store/action-creator/tod-calculator";
+import {TOD_CALCULATION_TYPE} from "../../Enum/TODCalculationType.enum";
+
+function Data({
+    currentAltitude,
+    targetAltitude,
+    calculation: { type: calculationType, input: calculationInput },
+    setTodData,
+    ...props
+}) {
+    const calculationTypes = [
+        {label: 'Distance', rightLabel: 'M', type: TOD_CALCULATION_TYPE.DISTANCE},
+        {label: 'Vertical speed', rightLabel: 'FT/s', type: TOD_CALCULATION_TYPE.VERTICAL_SPEED}
+    ];
+
+    return (
+        <Card {...props} title={'Data'}>
+            <Input
+                label={'Current altitude'}
+                type={'number'}
+                className={'dark-option mb-4'}
+                rightComponent={<span className={'text-lg'}>FT</span>}
+                value={currentAltitude}
+                onChange={(currentAltitude) => setTodData({ currentAltitude })}
+            />
+
+            <Input
+                label={'Target altitude'}
+                type={'number'}
+                className={'dark-option mb-6'}
+                rightComponent={<span className={'text-lg'}>FT</span>}
+                value={targetAltitude}
+                onChange={(targetAltitude) => setTodData({ targetAltitude })}
+            />
+
+            <Divider className={'mb-6'} />
+
+            {calculationTypes.map(({ label, rightLabel, type }) => (!calculationInput || calculationType === type) && (
+                <Input
+                    label={label}
+                    type={'number'}
+                    className={'dark-option mb-4'}
+                    rightComponent={<span className={'text-lg'}>{rightLabel}</span>}
+                    onChange={(input) => setTodData({ calculation: {input, type: input !== '' ? type : undefined }})}
+                />
+            ))}
+        </Card>
+    );
+}
+
+export default connect(
+    ({ [TOD_CALCULATOR_REDUCER]: { currentAltitude, targetAltitude, calculation } }) => ({ currentAltitude, targetAltitude, calculation }),
+    { setTodData }
+)(Data);

flybywiresim/aircraft

src/instruments/src/EFB/Service/TODCalculator.tsx
@@ -1,0 +1,107 @@
+/*
+ * A32NX
+ * Copyright (C) 2020-2021 FlyByWire Simulations and its contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {get, orderBy, min, max, inRange, set, last, head} from 'lodash';
+
+type groundSpeed = {from: number, groundSpeed: number};
+type groundSpeedRange= {groundSpeed: number, range: [number, number]};
+
+export default class TODCalculator {
+    private readonly avgGroundSpeed;
+
+    public constructor(private from: number, private to: number, groundSpeeds: groundSpeed[]) {
+        const groundSpeedRanges = this.findGroundSpeedRanges(groundSpeeds);
+        const filteredGroundSpeedRanges = this.filterGroundSpeedRanges(from, to, groundSpeedRanges);
+
+        this.avgGroundSpeed = this.findAverageGroundSpeed(filteredGroundSpeedRanges);
+    }
+
+    public calculateVS(distance: number): number {
+        const time = (distance / this.avgGroundSpeed) * 60;
+
+        return (this.to - this.from) / time;
+    }
+
+    public calculateDistance(vs: number): number {
+        const verticalDistance = Math.abs(this.from - this.to);
+        const verticalDistanceTime = verticalDistance / Math.abs(vs);
+
+        return (this.avgGroundSpeed / 60) * verticalDistanceTime;
+    }
+
+    private findAverageGroundSpeed(groundSpeedsRanges: groundSpeedRange[]) {
+        let sum = 0;
+
+        groundSpeedsRanges.forEach(({groundSpeed, range: [bottom, top]}) => {
+            sum += groundSpeed * (top - bottom);
+        });
+
+        const totalVerticalDistance = get(last(groundSpeedsRanges), ['range', 1]) - get(head(groundSpeedsRanges), ['range', 0]);
+
+        return sum / totalVerticalDistance;
+    }
+
+    private findGroundSpeedRanges(groundSpeeds: groundSpeed[]): groundSpeedRange[] {
+        groundSpeeds = orderBy(groundSpeeds, 'from', 'asc');
+
+        const ranges: groundSpeedRange[] = [];
+
+        for (let i = 0; i < groundSpeeds.length; i++) {
+            const groundSpeed = get(groundSpeeds, [i, 'groundSpeed']);
+            const rangeStart = get(groundSpeeds, [i, 'from']);
+            const rangeEnd = get(groundSpeeds, [i + 1, 'from'], 1000000); // Unrealistic default value to avoid many IFs later
+
+            ranges.push({groundSpeed, range: [rangeStart, rangeEnd]});
+        }
+
+        return ranges;
+    }
+
+    private filterGroundSpeedRanges(from: number, to: number, groundSpeedsArg: groundSpeedRange[]) {
+        const groundSpeeds = [...groundSpeedsArg];
+
+        const bottom = min([from, to]);
+        const top = max([from, to]);
+
+        let rangeStartElementIndex;
+        let rangeEndElementIndex;
+
+        for (let i = 0; i < groundSpeeds.length; i++) {
+            const rangeStart = get(groundSpeeds, [i, 'range', 0]);
+            const rangeEnd = get(groundSpeeds, [i, 'range', 1]);
+
+            if (inRange(bottom, rangeStart, rangeEnd)) {
+                rangeStartElementIndex = i;
+            }
+
+            if (inRange(top, rangeStart, rangeEnd)) {
+                rangeEndElementIndex = i;
+            }
+        }
+
+        const rangeStartElement = set({...groundSpeeds[rangeStartElementIndex]}, ['range', 0], bottom);
+
+        if (rangeStartElementIndex !== rangeEndElementIndex) {
+            const rangeEndElement = set({...groundSpeeds[rangeEndElementIndex]}, ['range', 1], top);
+
+            return [rangeStartElement, ...groundSpeeds.slice(rangeStartElementIndex + 1, rangeEndElementIndex), rangeEndElement];
+        }
+
+        return [set({...rangeStartElement}, ['range', 1], top)];
+    }
+}

src/instruments/src/EFB/TODCalculator/Data/Data.tsx
@@ -1,0 +1,78 @@
+/*
+ * A32NX
+ * Copyright (C) 2020-2021 FlyByWire Simulations and its contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { connect } from 'react-redux';
+import Input from "../../Components/Form/Input/Input";
+import React from "react";
+import Card from "../../Components/Card/Card";
+import Divider from "../../Components/Divider/Divider";
+import {TOD_CALCULATOR_REDUCER} from "../../Store";
+import {setTodData} from "../../Store/action-creator/tod-calculator";
+import {TOD_CALCULATION_TYPE} from "../../Enum/TODCalculationType.enum";
+
+const Data = ({
+    currentAltitude,
+    targetAltitude,
+    calculation: { type: calculationType, input: calculationInput },
+    setTodData,
+    ...props
+}) => {
+    const calculationTypes = [
+        {label: 'Distance', rightLabel: 'NM', type: TOD_CALCULATION_TYPE.DISTANCE},
+        {label: 'Vertical speed', rightLabel: 'ft/min', type: TOD_CALCULATION_TYPE.VERTICAL_SPEED}
+    ];
+
+    return (
+        <Card {...props} title={'Data'}>
+            <Input
+                label={'Current altitude'}
+                type={'number'}
+                className={'dark-option mb-4'}
+                rightComponent={<span className={'text-2xl'}>ft</span>}
+                value={currentAltitude}
+                onChange={(currentAltitude) => setTodData({ currentAltitude })}
+            />
+
+            <Input
+                label={'Target altitude'}
+                type={'number'}
+                className={'dark-option mb-6'}
+                rightComponent={<span className={'text-2xl'}>ft</span>}
+                value={targetAltitude}
+                onChange={(targetAltitude) => setTodData({ targetAltitude })}
+            />
+
+            <Divider className={'mb-6'} />
+
+            {calculationTypes.map(({ label, rightLabel, type }) => (!calculationInput || calculationType === type) && (
+                <Input
+                    label={label}
+                    type={'number'}
+                    className={'dark-option mb-4'}
+                    rightComponent={<span className={'text-2xl'}>{rightLabel}</span>}
+                    onChange={(input) => setTodData({ calculation: {input, type: input !== '' ? type : undefined }})}
+                />
+            ))}
+        </Card>
+    );
+};
+
+export default connect(
+    ({ [TOD_CALCULATOR_REDUCER]: { currentAltitude, targetAltitude, calculation } }) => ({ currentAltitude, targetAltitude, calculation }),
+    { setTodData }
+)(Data);