c5eb0714e0311ac0379da89d5cfb7b6a40c1d84c
lukecologne <lukasmichels00@gmail.com>
2021-02-23 15:53:00 +0100

    feat: clock elapsed time indicator

--- src/instruments/src/Clock/Components/ElapsedTime.tsx
+++ src/instruments/src/Clock/Components/ElapsedTime.tsx
@@ -1,3 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { useInteractionSimVar, useSimVar } from '../../Common/simVars';
+
+const getDisplayString = (seconds: number, running: boolean) : string => (seconds === 0 && !running ? ''
+    : `${Math.floor(Math.min(seconds, 359940) / 3600).toString().padStart(2, '0')}${running ? ':' : ' '}${(Math.floor((Math.min(seconds, 359940) % 3600)) / 60).toString().padStart(2, '0')}`);
+
 export const ElapsedTime = () => {
-    return null;
+    const [ltsTest] = useSimVar('L:XMLVAR_LTS_Test', 'bool', 250);
+    const [elapsedKnobPos] = useInteractionSimVar('', '', '');
+    const [absTime] = useSimVar('E:ABSOLUTE TIME', 'Seconds', 1000);
+    const [prevTime, setPrevTime] = useState(absTime);
+
+    const [elapsedTime, setElapsedTime] = useState(0);
+
+    useEffect(() => {
+        if (elapsedKnobPos === 0) {
+            setElapsedTime(elapsedTime + absTime - prevTime);
+        } else if (elapsedKnobPos === 2) {
+            setElapsedTime(0);
+        }
+        setPrevTime(absTime);
+    }, [absTime, elapsedKnobPos]);
+
+    return (
+        <text x="47" y="247" className="fontBig">{ltsTest ? '88:88' : getDisplayString(elapsedTime, elapsedKnobPos === 0)}</text>
+    );
 };
