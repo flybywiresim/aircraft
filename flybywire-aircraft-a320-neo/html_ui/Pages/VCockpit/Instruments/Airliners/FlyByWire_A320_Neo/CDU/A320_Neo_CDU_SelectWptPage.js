b50994970d2075c0498bdbb2db3591cb63333bab
Michael Corcoran <tracer@outlook.co.nz>
2021-02-28 16:51:41 +1300

    fix(mcdu): show max dist 9999 on wp select page
    Per Honeywell manual:
    "When the distance to the duplicate identifier is greater
    than 9,999 NM, 9999 is displayed as the distance--to--go to the
    waypoint."

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_SelectWptPage.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_SelectWptPage.js
@@ -44,7 +44,7 @@ class A320_Neo_CDU_SelectWptPage {
                 const latString = (w.infos.coordinates.lat.toFixed(0) >= 0) ? `${w.infos.coordinates.lat.toFixed(0).toString().padStart(2, "0")}N` : `${Math.abs(w.infos.coordinates.lat.toFixed(0)).toString().padStart(2, "0")}S`;
                 const longString = (w.infos.coordinates.long.toFixed(0) >= 0) ? `${w.infos.coordinates.long.toFixed(0).toString().padStart(3, "0")}E` : `${Math.abs(w.infos.coordinates.long.toFixed(0)).toString().padStart(3, "0")}W`;
 
-                const dist = calculateDistance(w);
+                const dist = Math.min(calculateDistance(w), 9999);
 
                 rows[2 * i].splice(0, 1, "{green}" + dist.toFixed(0) + "{end}NM");
                 rows[2 * i + 1] = ["*" + w.ident + "[color]cyan", freq + "[color]green", `${latString}/${longString}[color]green`];
