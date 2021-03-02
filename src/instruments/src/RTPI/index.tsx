d4d08c8ee4922cc0138faba7811b5455b9946023
lukecologne <lukasmichels00@gmail.com>
2021-03-02 20:41:06 +0100

    refactor: React rudder trim indicator

---
+++ src/instruments/src/RTPI/index.tsx
@@ -0,0 +1,37 @@
+import './style.scss';
+import React from 'react';
+import { render } from '../Common';
+import { useSimVar } from '../Common/simVars';
+
+const RTPIDisplay = () => {
+    const [ltsTest] = useSimVar('L:XMLVAR_LTS_Test', 'Bool', 400);
+    const [rudderTrim] = useSimVar('RUDDER TRIM', 'degrees', 100);
+
+    let directionText = '';
+    if (rudderTrim <= 0) {
+        directionText = 'L';
+    } else {
+        directionText = 'R';
+    }
+
+    return (
+        <>
+            <text x="0" y="115" className="direction">{ltsTest ? '8' : directionText}</text>
+            <text x="330" y="115" className="value">{ltsTest ? '88.8' : `${Math.abs(rudderTrim).toFixed(1)}`}</text>
+        </>
+    );
+};
+
+const RTPIRoot = () => {
+    const [powerAvailable] = useSimVar('L:DCPowerAvailable', 'Boolean', 250);
+
+    if (!powerAvailable) return null;
+
+    return (
+        <svg className="rtpi-svg" viewBox="0 0 338 128">
+            <RTPIDisplay />
+        </svg>
+    );
+};
+
+render(<RTPIRoot />);
