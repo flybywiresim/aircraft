7cdad7836f5a924e0c29c831e8079aec9cf43120
Aaryan Kapoor <myself.aaryankapoor@gmail.com>
2021-03-08 00:48:42 +0530

    mini fixes 🛴

--- src/instruments/src/EFB/Dashboard/Dashboard.tsx
+++ src/instruments/src/EFB/Dashboard/Dashboard.tsx
@@ -20,6 +20,7 @@ import React from 'react';
 import FlightWidget from "./Widgets/FlightWidget";
 import WeatherWidget from "./Widgets/WeatherWidget";
 import { CurrentFlight, Map } from '@flybywiresim/map';
+import { useSimVar } from '../../Common/simVars';
 
 type DashboardProps = {
     currentFlight: string,
@@ -41,14 +42,18 @@ type DashboardProps = {
     costInd: string
 }
 
-type DashboardState = {}
+const Dashboard: React.FC<DashboardProps> = (props) => {
+    const [flightNumber] = useSimVar('ATC FLIGHT NUMBER', 'String', 1_000);
+    const [aircraftType] = useSimVar('TITLE', 'String', 1_000);
+    const [altitude] = useSimVar('PLANE ALTITUDE', 'feet', 500);
+    const [heading] = useSimVar('PLANE HEADING DEGREES TRUE', 'degrees', 500);
+    const [latitude] = useSimVar('PLANE LATITUDE', 'degree latitude', 500);
+    const [longitude] = useSimVar('PLANE LONGITUDE', 'degree longitude', 500);
 
-class Dashboard extends React.Component<DashboardProps, DashboardState> {
-
-    calculateFlightTime(flightETAInSeconds: string): string {
+    const calculateFlightTime = (flightETAInSeconds: string): string => {
         const timeInMinutes: number = parseInt(flightETAInSeconds) * 0.0166;
-        if (timeInMinutes.toString() === "NaN") {
-            return "00:00";
+        if (timeInMinutes.toString() === 'NaN') {
+            return '00:00';
         }
 
         const hours = (timeInMinutes / 60);
@@ -56,48 +61,57 @@ class Dashboard extends React.Component<DashboardProps, DashboardState> {
         const minutes = (hours - roundedHours) * 60;
         const roundedMinutes = Math.round(minutes);
 
-        return (roundedHours <= 9 ? "0" : "") + roundedHours + ":" + (roundedMinutes <= 9 ? "0" : "") + roundedMinutes;
-    }
+        return `${(roundedHours <= 9 ? '0' : '') + roundedHours}:${roundedMinutes <= 9 ? '0' : ''}${roundedMinutes}`;
+    };
 
-    render() {
-        return (
-            <div className="w-full">
-                <h1 className="text-3xl text-white">Dashboard</h1>
-                <div className="flex w-full mt-6 h-efb">
-                    <FlightWidget
-                    name="todays"
-                    airline={this.props.airline}
-                    flightNum={this.props.flightNum}
-                    aircraftReg={this.props.aircraftReg}
-                    dep={this.props.departingAirport}
-                    depIata={this.props.depIata}
-                    arrIata={this.props.arrIata}
-                    arr={this.props.arrivingAirport}
-                    route={this.props.route}
-                    distance={this.props.flightDistance}
-                    eta={this.calculateFlightTime(this.props.flightETAInSeconds)}
-                    timeSinceStart={this.props.timeSinceStart}
-                    sta={this.props.schedIn}
-                    std={this.props.schedOut}
-                    fetchSimbrief={this.props.fetchSimbrief}
-                    altIcao={this.props.altIcao}
-                    costInd={this.props.costInd} />
+    const handleGettingCurrentFlightData = (): CurrentFlight => ({
+        flightNumber,
+        aircraftType,
+        altitude,
+        heading,
+        origin: '',
+        destination: '',
+        latitude,
+        longitude,
+    });
 
-                    <div className="flex flex-col w-3/5">
-                        <div className="h-2/5 bg-navy-medium rounded-xl ml-3 mb-3 shadow-lg p-6">
-                            <div className="h-full rounded-lg overflow-hidden">
+    return (
+        <div className="w-full">
+            <h1 className="text-3xl text-white">Dashboard</h1>
+            <div className="flex w-full mt-6 h-efb">
+                <FlightWidget
+                name="todays"
+                airline={props.airline}
+                flightNum={props.flightNum}
+                aircraftReg={props.aircraftReg}
+                dep={props.departingAirport}
+                depIata={props.depIata}
+                arrIata={props.arrIata}
+                arr={props.arrivingAirport}
+                route={props.route}
+                distance={props.flightDistance}
+                eta={calculateFlightTime(props.flightETAInSeconds)}
+                timeSinceStart={props.timeSinceStart}
+                sta={props.schedIn}
+                std={props.schedOut}
+                fetchSimbrief={props.fetchSimbrief}
+                altIcao={props.altIcao}
+                costInd={props.costInd} />
 
-                            </div>
+                <div className="flex flex-col w-3/5">
+                    <div className="h-2/5 bg-navy-medium rounded-xl ml-3 mb-3 shadow-lg p-6">
+                        <div className="h-full rounded-lg overflow-hidden">
+
                         </div>
+                    </div>
 
-                        <div className="h-3/5 ml-3 mt-3 rounded-xl overflow-hidden shadow-lg">
-                            <Map disableMenu={true} hideOthers={true} />
-                        </div>
+                    <div className="h-3/5 ml-3 mt-3 rounded-xl overflow-hidden shadow-lg">
+                        <Map currentFlight={handleGettingCurrentFlightData} disableMenu hideOthers />
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Dashboard;
