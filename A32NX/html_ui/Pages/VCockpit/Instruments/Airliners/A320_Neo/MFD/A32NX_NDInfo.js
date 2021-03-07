661121a39c3375ce7ee5e1d9007f43f0a81de5b3
sidnov <sidnov@yahoo.co.in>
2021-03-08 00:11:29 +0530

    decimal with 20 fixed

--- A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/MFD/A32NX_NDInfo.js
+++ A32NX/html_ui/Pages/VCockpit/Instruments/Airliners/A320_Neo/MFD/A32NX_NDInfo.js
@@ -192,7 +192,7 @@ class Jet_MFD_NDInfo extends HTMLElement {
                         this.currentWaypointDistance = _distance;
                         if (this.waypointDistance != null) {
                             if (this.currentWaypointDistance < 10000) {
-                                if (this.currentWaypointDistance > 20) {
+                                if (this.currentWaypointDistance > 19.9) {
                                     this.currentWaypointDistance = Math.round(this.currentWaypointDistance);
                                     this.waypointDistance.textContent = fastToFixed(this.currentWaypointDistance, 1);
                                 } else {
