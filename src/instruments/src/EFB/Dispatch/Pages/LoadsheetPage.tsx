6f538f857c752fa8ac0232fe4bdfba65bee0bc24
Akhilesh Anand <akhileshmalini@gmail.com>
2021-03-13 11:25:41 -0800

    Add EFB Changes

--- src/instruments/src/EFB/Dispatch/Pages/LoadsheetPage.tsx
+++ src/instruments/src/EFB/Dispatch/Pages/LoadsheetPage.tsx
@@ -16,18 +16,116 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import './Loadsheet.scss';
 
 type LoadsheetPageProps = {
-    loadsheet: string
+    loadsheet: string,
 };
 
-const LoadSheetWidget = (props: LoadsheetPageProps) => (
-    <div className="px-6">
-        <div className="w-full">
-            <div className="bg-gray-800 rounded-xl p-6 text-white shadow-lg mr-4 overflow-y-scroll" dangerouslySetInnerHTML={{ __html: props.loadsheet }} />
-        </div>
-    </div>
-);
+const LoadSheetWidget = (props: LoadsheetPageProps) => {
+    const position = useRef({ top: 0, y: 0 });
+    const ref = useRef<HTMLDivElement>(null);
 
+    const [fontSize, setFontSize] = useState(14);
+    const [imageSize, setImageSize] = useState(60);
+
+    const mouseDownHandler = (event) => {
+        position.current.top = ref.current ? ref.current.scrollTop : 0;
+        position.current.y = event.clientY;
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = (event) => {
+        const dy = event.clientY - position.current.y;
+        if (ref.current) {
+            ref.current.scrollTop = position.current.top - dy;
+        }
+    };
+
+    const mouseUpHandler = () => {
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    const fontIncreaseHandler = () => {
+        let cFontSize = fontSize;
+        let cImageSize = imageSize;
+
+        if (cFontSize < 26) {
+            cFontSize += 2;
+            cImageSize += 5;
+            handleScaling(cFontSize);
+            setImageSize(cImageSize);
+        }
+    };
+
+    const fontDecreaseHandler = () => {
+        let cFontSize = fontSize;
+        let cImageSize = imageSize;
+
+        if (cFontSize > 14) {
+            cFontSize -= 2;
+            cImageSize -= 5;
+            handleScaling(cFontSize);
+            setImageSize(cImageSize);
+        }
+    };
+
+    const handleScaling = (cFontSize) => {
+        const pLoadsheet = ref.current?.firstChild as HTMLElement;
+
+        const pImages = ref.current?.getElementsByTagName('img');
+
+        if (pLoadsheet) {
+            pLoadsheet.style.fontSize = `${cFontSize}px`;
+            pLoadsheet.style.lineHeight = `${cFontSize}px`;
+            setFontSize(cFontSize);
+            if (pImages) {
+                for (let i = 0; i < pImages.length; i++) {
+                    pImages[i].style.width = `${imageSize}%`;
+                }
+            }
+        }
+    };
+
+    return (
+        <div className="px-6">
+            <div className="w-full">
+                <div className="relative bg-gray-800 rounded-xl p-6 text-white shadow-lg mr-4">
+                    {props.loadsheet !== 'N/A' ? (
+                        <>
+                            <div className="flex flex-col justify-end absolute bottom-5 right-16">
+                                <button
+								    type='button'
+                                    onClick={fontIncreaseHandler}
+                                    className="font-size-button"
+                                >
+                                    +
+                                </button>
+                                <button
+									type='button'
+                                    onClick={fontDecreaseHandler}
+                                    className="font-size-button"
+                                >
+                                    -
+                                </button>
+                            </div>
+                            <div
+                                ref={ref}
+                                className="loadsheet-container grabbable show-scrollbar overflow-y-scroll"
+                                onMouseDown={mouseDownHandler}
+                                dangerouslySetInnerHTML={{__html: props.loadsheet}}
+                            />
+                        </>
+                    ) : (
+                        'N/A'
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
 export default LoadSheetWidget;
