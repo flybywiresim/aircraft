aa016e5c0df53695bfbe8893dcbaf50804b5b93b
David Walschots <davidwalschots@users.noreply.github.com>
2021-03-30 15:06:25 +0200

    refactor: infinite battery

--- A32NX/html_ui/Pages/A32NX_Core/A32NX_Electricity.js
+++ A32NX/html_ui/Pages/A32NX_Core/A32NX_Electricity.js
@@ -18,20 +18,27 @@
 
 class A32NX_Electricity {
     constructor() {
-        console.log('A32NX_Electricity constructed');
-    }
+        /**
+         * Initialises the infinite battery to which buses that are powered are connected
+         * to simulate availability of electricity.
+         *
+         * This function cannot be called on init, as the sim's default ELEC state seemingly hasn't
+         * fully initialised by that point.
+         *
+         * Internally the function ensures it's cheap to execute by only retrieving sim variables
+         * the first time it is called.
+         */
+        this.initialiseInfiniteBattery = () => {
+            const infiniteBatteryNumber = 1;
+            if (!SimVar.GetSimVarValue("A:ELECTRICAL MASTER BATTERY:" + infiniteBatteryNumber, "Bool")) {
+                SimVar.SetSimVarValue("K:TOGGLE_MASTER_BATTERY", "Number", infiniteBatteryNumber);
+            }
 
-    init() {
-        console.log('A32NX_Electricity init');
+            this.initialiseInfiniteBattery = () => {};
+        };
     }
 
     update() {
-        this.toggleMasterBattery(1);
-    }
-
-    toggleMasterBattery(number) {
-        if (!SimVar.GetSimVarValue("A:ELECTRICAL MASTER BATTERY:" + number, "Bool")) {
-            SimVar.SetSimVarValue("K:TOGGLE_MASTER_BATTERY", "Number", number);
-        }
+        this.initialiseInfiniteBattery();
     }
 }
