9a643e761df8e719178c59d60672e5866ceac409
ChruutvoLuzi <70195216+ChruutvoLuzi@users.noreply.github.com>
2021-04-04 22:54:45 +0200

    Update A320_Neo_MFD.js

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/MFD/A320_Neo_MFD.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/MFD/A320_Neo_MFD.js
@@ -853,6 +853,8 @@ class A320_Neo_MFD_NDInfo extends NavSystemElement {
         }
         const ADIRSState = SimVar.GetSimVarValue("L:A320_Neo_ADIRS_STATE", "Enum");
         const groundSpeed = Math.round(Simplane.getGroundSpeed());
+        const trueAirSpeed = SimVar.GetSimVarValue("AIRSPEED TRUE", "knots");
+        const windStrength = SimVar.GetSimVarValue("AMBIENT WIND VELOCITY", "knots");
         const gs = this.ndInfo.querySelector("#GS_Value");
         const tas = this.ndInfo.querySelector("#TAS_Value");
         const wd = this.ndInfo.querySelector("#Wind_Direction");
@@ -860,9 +862,12 @@ class A320_Neo_MFD_NDInfo extends NavSystemElement {
         const wa = this.ndInfo.querySelector("#Wind_Arrow");
         const wptg = this.ndInfo.querySelector("#Waypoint_Group");
         const wptPPOS = this.ndInfo.querySelector("#Waypoint_PPOS");
-        if (ADIRSState != 2 || groundSpeed <= 100) {
-            //Hide TAS, and wind info
+        if (ADIRSState != 2 || trueAirSpeed < 60) {
+            //TAS info Conditions
             tas.textContent = "---";
+        }
+        if (ADIRSState != 2 || trueAirSpeed < 100 || windStrength < 2) {
+            //Wind Arrow info Conditions
             wd.textContent = "---";
             ws.textContent = "---";
             wa.setAttribute("visibility", "hidden");
