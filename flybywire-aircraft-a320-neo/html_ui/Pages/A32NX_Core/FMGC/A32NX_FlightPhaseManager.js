7052d13cc08f2f35cf36affda123ca44df53f7ca
Leon Beeser <leon.beeser@yahoo.de>
2021-04-06 22:55:40 +0200

    fix: early descent initiation

--- flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/FMGC/A32NX_FlightPhaseManager.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/FMGC/A32NX_FlightPhaseManager.js
@@ -104,8 +104,8 @@ class A32NX_FlightPhaseManager {
 
             // Try initiate early descent
             if (
-                !!dest && dest.liveDistanceTo > 200 &&
-                fl > 200 &&
+                !!dest && dest.liveDistanceTo >= 200 &&
+                fl > fcuSelFl &&
                 fcuSelFl < 200
             ) {
                 this.changeFlightPhase(FmgcFlightPhases.DESCENT);
