be26deec66133455d0a3f11403e58bf3f525a2db
David Walschots <davidwalschots@users.noreply.github.com>
2021-04-07 12:50:49 +0200

    feat: power EWD with AC ESS bus

--- src/instruments/src/EWD/index.jsx
+++ src/instruments/src/EWD/index.jsx
@@ -8,16 +8,8 @@ import {
 } from '../util.js';
 import './style.scss';
 
-// TODO: Move anything dependent on ac power change to A32NX_Core
 function powerAvailable() {
-    // These are inlined so they're only evaluated if prior conditions return false.
-    return (
-        Simplane.getEngineActive(0) === 1 || Simplane.getEngineActive(1) === 1
-    ) || (
-        getSimVar('L:APU_GEN_ONLINE')
-    ) || (
-        getSimVar('EXTERNAL POWER AVAILABLE:1') && getSimVar('EXTERNAL POWER ON')
-    );
+    return getSimVar('L:A32NX_ELEC_AC_ESS_BUS_IS_POWERED', 'Bool');
 }
 
 function SelfTest() {
