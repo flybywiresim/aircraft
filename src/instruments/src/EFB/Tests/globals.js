373e0ab2243fae7fb09dc37d8ceae7a530646757
David Walschots <davidwalschots@users.noreply.github.com>
2021-04-07 13:08:43 +0200

    chore: remove unused L:APU_GEN_ONLINE

--- src/instruments/src/EFB/Tests/globals.js
+++ src/instruments/src/EFB/Tests/globals.js
@@ -3,7 +3,6 @@
 const simvars = {
     'ATC FLIGHT NUMBER': 'AB123',
     'GPS POSITION LAT': 0,
-    'L:APU_GEN_ONLINE': false,
     'EXTERNAL POWER AVAILABLE:1': false,
     'EXTERNAL POWER ON': false,
     'L:A32NX_COLD_AND_DARK_SPAWN': true,
