7fedd47298b930911332cf06766f45f0eb6d41e6
Vinicius Fontoura <viniciusbfont@gmail.com>
2021-04-11 17:20:59 -0300

    Add min and max props to Slider component

--- src/instruments/src/EFB/Components/Form/Slider.tsx
+++ src/instruments/src/EFB/Components/Form/Slider.tsx
@@ -2,20 +2,35 @@ import React, { useRef } from 'react';
 
 import './Slider.scss';
 
-export type SliderProps = { className?: string, value?: number, onInput?: (v: number) => void; };
+export type SliderProps = {
+    className?: string,
+    value?: number,
+    onInput?: (v: number) => void,
+    min?: number,
+    max?: number,
+};
 
 export const Slider = (props: SliderProps) => {
     const sliderRef = useRef<HTMLInputElement>(null);
+    const { min, max, onInput, className } = props;
 
     const handleInput = () => {
         const newValue = Number(sliderRef?.current?.value ?? 0);
 
-        if (props.onInput) {
-            props.onInput(newValue);
+        if (onInput) {
+            onInput(newValue);
         }
     };
 
     return (
-        <input value={props.value} ref={sliderRef} onChange={handleInput} type="range" min="1" max="100" className={`slider slider h-1 pb-1.5 pt-0.5 rounded-full ${props.className}`} />
+        <input
+            value={props.value}
+            ref={sliderRef}
+            onChange={handleInput}
+            type="range"
+            min={min ?? 1}
+            max={max ?? 100}
+            className={`slider slider h-1 pb-1.5 pt-0.5 rounded-full ${className}`}
+        />
     );
 };
