6cb9c1d628d243a712c47c8e83dd36a575f28a72
lukecologne <lukasmichels00@gmail.com>
2021-04-16 00:49:01 +0200

    refactor: Convert PFD to new framework and tsx part 2

--- src/instruments/src/PFD/index.tsx
+++ src/instruments/src/PFD/index.tsx
@@ -1,184 +1,126 @@
-import { Component } from 'react';
-import { Horizon } from './AttitudeIndicatorHorizon.jsx';
-import { AttitudeIndicatorFixedUpper, AttitudeIndicatorFixedCenter } from './AttitudeIndicatorFixed.jsx';
-import { LandingSystem } from './LandingSystemIndicator.jsx';
-import { VerticalSpeedIndicator } from './VerticalSpeedIndicator.jsx';
-import { HeadingOfftape, HeadingTape } from './HeadingIndicator.jsx';
-import { AltitudeIndicatorOfftape, AltitudeIndicator } from './AltitudeIndicator.jsx';
-import { AirspeedIndicatorOfftape, AirspeedIndicator } from './SpeedIndicator.jsx';
-import { FMA } from './FMA.jsx';
-import { getSimVar, setSimVar, renderTarget, createDeltaTimeCalculator } from '../util.js';
-import { SmoothSin, LagFilter, RateLimiter } from './PFDUtils.jsx';
-import { DisplayUnit } from '../Common/displayUnit';
+import React, { useEffect, useState } from 'react';
+import { Horizon } from './AttitudeIndicatorHorizon';
+import { AttitudeIndicatorFixedUpper, AttitudeIndicatorFixedCenter } from './AttitudeIndicatorFixed';
+import { LandingSystem } from './LandingSystemIndicator';
+import { VerticalSpeedIndicator } from './VerticalSpeedIndicator';
+import { HeadingOfftape, HeadingTape } from './HeadingIndicator';
+import { AltitudeIndicatorOfftape, AltitudeIndicator } from './AltitudeIndicator';
+import { AirspeedIndicatorOfftape, AirspeedIndicator } from './SpeedIndicator';
+import { FMA } from './FMA';
+import { getSimVar, setSimVar } from '../util.js';
+import { LagFilter, RateLimiter } from './PFDUtils';
+import { useSimVar } from '../Common/simVars';
 import { render } from '../Common';
 import './style.scss';
+import { useInteractionEvent, useUpdate } from '../Common/hooks';
+import { DisplayUnit } from '../Common/displayUnit';
 
+const airspeedAccFilter = new LagFilter(1.2);
+const airspeedAccRateLimiter = new RateLimiter(1.2, -1.2);
 /* eslint-disable max-len */
-// eslint-disable-next-line react/prefer-stateless-function
-class PFD extends Component {
-    constructor(props) {
-        super(props);
 
-        const url = document.getElementsByTagName('a32nx-pfd-element')[0].getAttribute('url');
-        this.dispIndex = parseInt(url.substring(url.length - 1), 10);
+const PFD = () => {
+    const [dispIndex, setDispIndex] = useState(0);
+    const [lsButtonPressed, setLsPressed] = useState(false);
+    const [filteredAirspeedAcc, setAirspeedAcc] = useState(0);
+    const [isAttExcessive, setAttExcessive] = useState(false);
+    const [prevAirspeed, setPrevAirspeed] = useState(0);
 
-        this.deltaTime = 0;
-        this.GetDeltaTime = createDeltaTimeCalculator();
-        this.prevAirspeed = 0;
+    const [negPitch] = useSimVar('PLANE PITCH DEGREES', 'degrees');
+    const pitch = -negPitch;
+    const [roll] = useSimVar('PLANE BANK DEGREES', 'degrees');
 
-        this.VLs = 0;
+    const [unclampedAirspeed] = useSimVar('AIRSPEED INDICATED', 'knots');
+    const clampedAirspeed = Math.max(unclampedAirspeed, 30);
 
-        this.barTimer = 10;
+    useEffect(() => {
+        const url = document.getElementsByTagName('a32nx-pfd-element')[0].getAttribute('url') || '';
+        setDispIndex(parseInt(url.substring(url.length - 1), 10));
 
-        this.smoothFactor = 0.5;
-
-        this.isAttExcessive = false;
-
-        this.AirspeedAccFilter = new LagFilter(1.2);
-        this.AirspeedAccRateLimiter = new RateLimiter(1.2, -1.2);
-
-        this.LSButtonPressed = false;
+        setLsPressed(!!getSimVar(`L:BTN_LS_${dispIndex}_FILTER_ACTIVE`, 'bool'));
 
         // for testing only
         setSimVar('L:A32NX_MachPreselVal', -1, 'mach');
         setSimVar('L:A32NX_SpeedPreselVal', -1, 'knots');
-    }
+    }, []);
 
-    componentDidMount() {
-        renderTarget.parentElement.addEventListener('update', () => {
-            this.update(this.GetDeltaTime());
-        });
-        renderTarget.parentElement.addEventListener(`A320_Neo_PFD_BTN_LS_${this.dispIndex}`, () => {
-            this.onLSButtonPressed();
-        });
-    }
-
-    onLSButtonPressed() {
-        this.LSButtonPressed = !this.LSButtonPressed;
-        setSimVar(`L:BTN_LS_${this.dispIndex}_FILTER_ACTIVE`, this.LSButtonPressed, 'Bool');
-    }
-
-    smoothSpeeds(_dTime, _vls) {
-        const seconds = _dTime / 1000;
-        this.VLs = SmoothSin(this.VLs, _vls, this.smoothFactor, seconds);
-    }
-
-    update(_deltaTime) {
-        this.deltaTime = _deltaTime;
-        this.forceUpdate();
-    }
-
-    render() {
-        const pitch = -getSimVar('PLANE PITCH DEGREES', 'degrees');
-        const roll = getSimVar('PLANE BANK DEGREES', 'degrees');
-        const heading = getSimVar('PLANE HEADING DEGREES MAGNETIC', 'degrees');
-
-        if (!this.isAttExcessive && (pitch > 25 || pitch < -13 || Math.abs(roll) > 45)) {
-            this.isAttExcessive = true;
-        } else if (this.isAttExcessive && pitch < 22 && pitch > -10 && Math.abs(roll) < 40) {
-            this.isAttExcessive = false;
+    useEffect(() => {
+        if (!isAttExcessive && (pitch > 25 || pitch < -13 || Math.abs(roll) > 45)) {
+            setAttExcessive(true);
+        } else if (isAttExcessive && pitch < 22 && pitch > -10 && Math.abs(roll) < 40) {
+            setAttExcessive(false);
         }
+    }, [pitch, roll]);
 
-        const groundTrack = getSimVar('GPS GROUND MAGNETIC TRACK', 'degrees');
+    useInteractionEvent(`A320_Neo_PFD_BTN_LS_${dispIndex}`, () => {
+        setSimVar(`L:BTN_LS_${dispIndex}_FILTER_ACTIVE`, !lsButtonPressed, 'bool');
+        setLsPressed(!lsButtonPressed);
+    });
 
-        const isOnGround = getSimVar('SIM ON GROUND', 'Bool');
+    useUpdate((deltaTime) => {
+        const airspeedAcc = (clampedAirspeed - prevAirspeed) / deltaTime * 1000;
+        setPrevAirspeed(clampedAirspeed);
+        const rateLimitedAirspeedAcc = airspeedAccRateLimiter.step(airspeedAcc, deltaTime / 1000);
+        setAirspeedAcc(airspeedAccFilter.step(rateLimitedAirspeedAcc, deltaTime / 1000));
+    });
 
-        const radioAlt = getSimVar('PLANE ALT ABOVE GROUND MINUS CG', 'feet');
-        const decisionHeight = getSimVar('L:AIRLINER_DECISION_HEIGHT', 'feet');
+    // We shouldn't use the normal simvar getters, and simplane here. But it should be fine for no, until we have a better
+    // system for the target altitude/speeds etc. The Component will be updated anyways.
+    const [armedVerticalBitmask] = useSimVar('L:A32NX_FMA_VERTICAL_ARMED', 'number');
+    const [activeVerticalMode] = useSimVar('L:A32NX_FMA_VERTICAL_MODE', 'enum');
+    const isManaged = ((armedVerticalBitmask >> 1) & 1) || activeVerticalMode === 21 || activeVerticalMode === 20;
+    const [targetAlt] = useSimVar(isManaged ? 'L:A32NX_AP_CSTN_ALT' : 'AUTOPILOT ALTITUDE LOCK VAR:3', 'feet');
 
-        const baroAlt = getSimVar('INDICATED ALTITUDE', 'feet');
-        const mda = getSimVar('L:AIRLINER_MINIMUM_DESCENT_ALTITUDE', 'feet');
+    const [speedMode] = useSimVar('AUTOPILOT SPEED SLOT INDEX', 'number');
+    const isSelected = speedMode === 1;
+    const [targetSpeed] = useSimVar(isSelected ? 'AUTOPILOT AIRSPEED HOLD VAR' : 'L:A32NX_SPEEDS_MANAGED_PFD', 'knots');
 
-        const FlightPhase = getSimVar('L:A32NX_FWC_FLIGHT_PHASE', 'Enum');
+    const [FDActive] = useSimVar(`AUTOPILOT FLIGHT DIRECTOR ACTIVE:${dispIndex}`, 'Bool');
 
-        // eslint-disable-next-line no-undef
-        const pressureMode = Simplane.getPressureSelectedMode(Aircraft.A320_NEO);
-
-        const clampedAirspeed = Math.max(getSimVar('AIRSPEED INDICATED', 'knots'), 30);
-        const airspeedAcc = (clampedAirspeed - this.prevAirspeed) / this.deltaTime * 1000;
-        this.prevAirspeed = clampedAirspeed;
-
-        const rateLimitedAirspeedAcc = this.AirspeedAccRateLimiter.step(airspeedAcc, this.deltaTime / 1000);
-        const filteredAirspeedAcc = this.AirspeedAccFilter.step(rateLimitedAirspeedAcc, this.deltaTime / 1000);
-
-        const mach = getSimVar('AIRSPEED MACH', 'mach');
-
-        const VMax = getSimVar('L:A32NX_SPEEDS_VMAX', 'number');
-        const VLs = getSimVar('L:A32NX_SPEEDS_VLS', 'number');
-
-        let showSpeedBars = true;
-        if (isOnGround) {
-            showSpeedBars = false;
-            this.barTimer = 0;
-        } else if (this.barTimer < 10) {
-            showSpeedBars = false;
-            this.barTimer += this.deltaTime / 1000;
-        }
-
-        this.smoothSpeeds(this.deltaTime, VLs);
-
-        const armedVerticalBitmask = getSimVar('L:A32NX_FMA_VERTICAL_ARMED', 'number');
-        const activeVerticalMode = getSimVar('L:A32NX_FMA_VERTICAL_MODE', 'enum');
-        const isManaged = ((armedVerticalBitmask >> 1) & 1) || activeVerticalMode === 21 || activeVerticalMode === 20;
-        const targetAlt = isManaged ? getSimVar('L:A32NX_AP_CSTN_ALT', 'feet') : Simplane.getAutoPilotDisplayedAltitudeLockValue();
-
-        let targetSpeed;
-        const isSelected = Simplane.getAutoPilotAirspeedSelected();
-        const isMach = Simplane.getAutoPilotMachModeActive();
-        if (isSelected) {
-            if (isMach) {
-                targetSpeed = SimVar.GetGameVarValue('FROM MACH TO KIAS', 'number', Simplane.getAutoPilotMachHoldValue());
-            } else {
-                targetSpeed = Simplane.getAutoPilotAirspeedHoldValue();
-            }
-        } else {
-            targetSpeed = getSimVar('L:A32NX_SPEEDS_MANAGED_PFD', 'knots') || NaN;
-        }
-
-        const FDActive = getSimVar(`AUTOPILOT FLIGHT DIRECTOR ACTIVE:${this.dispIndex}`, 'Bool');
-
-        let selectedHeading = NaN;
-        if (getSimVar('L:A320_FCU_SHOW_SELECTED_HEADING', 'number')) {
-            selectedHeading = Simplane.getAutoPilotSelectedHeadingLockValue(false);
-        }
-
-        let ILSCourse = NaN;
-        if (this.LSButtonPressed) {
-            ILSCourse = getSimVar('NAV LOCALIZER:3', 'degrees');
-        }
-
-        return (
-            <DisplayUnit
-                electricitySimvar={this.dispIndex === 1 ? 'L:A32NX_ELEC_AC_ESS_BUS_IS_POWERED' : 'L:A32NX_ELEC_AC_2_BUS_IS_POWERED'}
-                potentiometerIndex={this.dispIndex === 1 ? 88 : 90}
-            >
-                <svg className="pfd-svg" version="1.1" viewBox="0 0 158.75 158.75" xmlns="http://www.w3.org/2000/svg">
-                    <Horizon pitch={pitch} roll={roll} heading={heading} FDActive={FDActive} selectedHeading={selectedHeading} isOnGround={isOnGround} radioAlt={radioAlt} decisionHeight={decisionHeight} isAttExcessive={this.isAttExcessive} deltaTime={this.deltaTime} />
-                    <path
-                        id="Mask1"
-                        className="BackgroundFill"
-                        d="m32.138 101.25c7.4164 13.363 21.492 21.652 36.768 21.652 15.277 0 29.352-8.2886 36.768-21.652v-40.859c-7.4164-13.363-21.492-21.652-36.768-21.652-15.277 0-29.352 8.2886-36.768 21.652zm-32.046 57.498h158.66v-158.75h-158.66z"
-                    />
-                    <HeadingTape heading={heading} ILSCourse={ILSCourse} />
-                    <AltitudeIndicator altitude={baroAlt} FWCFlightPhase={FlightPhase} />
-                    <AirspeedIndicator airspeed={clampedAirspeed} airspeedAcc={filteredAirspeedAcc} FWCFlightPhase={FlightPhase} altitude={baroAlt} VLs={this.VLs} VMax={VMax} showBars={showSpeedBars} />
-                    <path
-                        id="Mask2"
-                        className="BackgroundFill"
-                        d="m32.138 145.34h73.536v10.382h-73.536zm0-44.092c7.4164 13.363 21.492 21.652 36.768 21.652 15.277 0 29.352-8.2886 36.768-21.652v-40.859c-7.4164-13.363-21.492-21.652-36.768-21.652-15.277 0-29.352 8.2886-36.768 21.652zm-32.046 57.498h158.66v-158.75h-158.66zm115.14-35.191v-85.473h15.609v85.473zm-113.33 0v-85.473h27.548v85.473z"
-                    />
-                    <LandingSystem LSButtonPressed={this.LSButtonPressed} deltaTime={this.deltaTime} />
-                    <AttitudeIndicatorFixedUpper />
-                    <AttitudeIndicatorFixedCenter isOnGround={isOnGround} FDActive={FDActive} isAttExcessive={this.isAttExcessive} />
-                    <VerticalSpeedIndicator radioAlt={radioAlt} />
-                    <HeadingOfftape ILSCourse={ILSCourse} groundTrack={groundTrack} heading={heading} selectedHeading={selectedHeading} />
-                    <AltitudeIndicatorOfftape altitude={baroAlt} radioAlt={radioAlt} MDA={mda} targetAlt={targetAlt} altIsManaged={isManaged} mode={pressureMode} />
-                    <AirspeedIndicatorOfftape airspeed={clampedAirspeed} mach={mach} airspeedAcc={filteredAirspeedAcc} targetSpeed={targetSpeed} speedIsManaged={!isSelected} />
-                    <FMA isAttExcessive={this.isAttExcessive} />
-                </svg>
-            </DisplayUnit>
-        );
+    const [showSelectedHeading] = useSimVar('L:A320_FCU_SHOW_SELECTED_HEADING', 'number');
+    const [selectedHeadingVal] = useSimVar('AUTOPILOT HEADING LOCK DIR:1', 'degrees');
+    let selectedHeading = NaN;
+    if (showSelectedHeading) {
+        selectedHeading = selectedHeadingVal;
     }
-}
+
+    const [ILSCourseVal] = useSimVar('NAV LOCALIZER:3', 'degrees');
+    let ILSCourse = NaN;
+    if (lsButtonPressed) {
+        ILSCourse = ILSCourseVal;
+    }
+
+    return (
+        <DisplayUnit
+            electricitySimvar={dispIndex === 1 ? 'L:A32NX_ELEC_AC_ESS_BUS_IS_POWERED' : 'L:A32NX_ELEC_AC_2_BUS_IS_POWERED'}
+            potentiometerIndex={dispIndex === 1 ? 88 : 90}
+        >
+            <svg className="pfd-svg" version="1.1" viewBox="0 0 158.75 158.75" xmlns="http://www.w3.org/2000/svg">
+                <Horizon FDActive={FDActive} selectedHeading={selectedHeading} isAttExcessive={isAttExcessive} />
+                <path
+                    id="Mask1"
+                    className="BackgroundFill"
+                    d="m32.138 101.25c7.4164 13.363 21.492 21.652 36.768 21.652 15.277 0 29.352-8.2886 36.768-21.652v-40.859c-7.4164-13.363-21.492-21.652-36.768-21.652-15.277 0-29.352 8.2886-36.768 21.652zm-32.046 57.498h158.66v-158.75h-158.66z"
+                />
+                <HeadingTape />
+                <AltitudeIndicator />
+                <AirspeedIndicator airspeed={clampedAirspeed} airspeedAcc={filteredAirspeedAcc} />
+                <path
+                    id="Mask2"
+                    className="BackgroundFill"
+                    d="m32.138 145.34h73.536v10.382h-73.536zm0-44.092c7.4164 13.363 21.492 21.652 36.768 21.652 15.277 0 29.352-8.2886 36.768-21.652v-40.859c-7.4164-13.363-21.492-21.652-36.768-21.652-15.277 0-29.352 8.2886-36.768 21.652zm-32.046 57.498h158.66v-158.75h-158.66zm115.14-35.191v-85.473h15.609v85.473zm-113.33 0v-85.473h27.548v85.473z"
+                />
+                <LandingSystem LSButtonPressed={lsButtonPressed} />
+                <AttitudeIndicatorFixedUpper />
+                <AttitudeIndicatorFixedCenter FDActive={FDActive} isAttExcessive={isAttExcessive} />
+                <VerticalSpeedIndicator />
+                <HeadingOfftape ILSCourse={ILSCourse} selectedHeading={selectedHeading} />
+                <AltitudeIndicatorOfftape targetAlt={targetAlt} altIsManaged={isManaged} />
+                <AirspeedIndicatorOfftape airspeed={clampedAirspeed} airspeedAcc={filteredAirspeedAcc} targetSpeed={targetSpeed} speedIsManaged={!isSelected} />
+                <FMA isAttExcessive={isAttExcessive} />
+            </svg>
+        </DisplayUnit>
+    );
+};
 
 render(<PFD />);
