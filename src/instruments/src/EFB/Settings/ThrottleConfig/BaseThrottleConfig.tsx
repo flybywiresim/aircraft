31b8bc7962a5664dd87338391ca0398431b37cfa
Saschl <sascharudolf46@gmail.com>
2021-04-21 17:23:11 +0200

    feat: simplified throttle calibration (#4158)
    * feat: simplified detent config
    
    * feat: store deadzone in localstore
    
    * fix: display values + cleanup
    
    * fix: removed outcommented code and reducer
    
    * feat: set immediately via text field, add validation, add bar for visualization
    
    * fix: lint + refactoring
    
    * fix: cleanup + small design improvements
    
    * feat: mirrored config, enabled manual entry
    
    * feat: show detents on diagram
    
    * feat: layout changes
    
    * fix: button placement
    
    * feat: improved layout for input/button
    
    * fix: fuel page progress bar
    
    * fix: layout when error message is displayed, minor refactoring
    
    * fix: reset button renamed, minor refactoring

--- src/instruments/src/EFB/Settings/ThrottleConfig/BaseThrottleConfig.tsx
+++ src/instruments/src/EFB/Settings/ThrottleConfig/BaseThrottleConfig.tsx
@@ -1,126 +1,66 @@
-import React from 'react';
+import { IconEdit } from '@tabler/icons';
+import React, { useState } from 'react';
 import { useSimVar } from '../../../Common/simVars';
 import DetentConfig from './DetentConfig';
+import { ThrottleSimvar } from './ThrottleSimVar';
 
 interface Props {
     throttleNumber: number,
     throttleCount: number,
+    mappingsAxisOne: ThrottleSimvar[],
+    mappingsAxisTwo?: ThrottleSimvar[]
     activeIndex: number,
-    disabled: boolean
-}
-
-class ThrottleSimvar {
-    readableName: string;
-
-    technicalName: string;
-
-    hiSetter: Array<any>;
-
-    hiGetter: Array<number>;
-
-    lowSetter: Array<any>;
-
-    lowGetter: Array<number>;
-
-    constructor(readableName: string, technicalName: string) {
-        this.readableName = readableName;
-        this.technicalName = technicalName;
-        this.hiSetter = [];
-        this.hiGetter = [];
-        this.lowGetter = [];
-        this.lowSetter = [];
-    }
+    disabled: boolean,
 }
 
 const BaseThrottleConfig: React.FC<Props> = (props: Props) => {
-    const mappings: Array<ThrottleSimvar> = [
-        new ThrottleSimvar('Reverse', 'L:A32NX_THROTTLE_MAPPING_REVERSE_'),
-        new ThrottleSimvar('Rev. Idle', 'L:A32NX_THROTTLE_MAPPING_REVERSE_IDLE_'),
-        new ThrottleSimvar('Idle', 'L:A32NX_THROTTLE_MAPPING_IDLE_'),
-        new ThrottleSimvar('Climb', 'L:A32NX_THROTTLE_MAPPING_CLIMB_'),
-        new ThrottleSimvar('Flex', 'L:A32NX_THROTTLE_MAPPING_FLEXMCT_'),
-        new ThrottleSimvar('TO/GA', 'L:A32NX_THROTTLE_MAPPING_TOGA_'),
-    ];
-
-    const [throttlePosition] = useSimVar(`L:A32NX_THROTTLE_MAPPING_INPUT:${props.throttleNumber}`, 'number', 50);
-
-    for (const mapped of mappings) {
-        for (let index = 1; index <= props.throttleCount; index++) {
-            const throttleNumber: number = props.throttleCount > 1 ? index : props.throttleNumber;
-
-            mapped.hiGetter.push(useSimVar(`${mapped.technicalName}HIGH:${throttleNumber}`, 'number', 100)[0]);
-            mapped.hiSetter.push(useSimVar(`${mapped.technicalName}HIGH:${throttleNumber}`, 'number', 100)[1]);
-
-            mapped.lowGetter.push(useSimVar(`${mapped.technicalName}LOW:${throttleNumber}`, 'number', 100)[0]);
-            mapped.lowSetter.push(useSimVar(`${mapped.technicalName}LOW:${throttleNumber}`, 'number', 100)[1]);
-        }
-    }
+    const [throttlePosition] = useSimVar(`L:A32NX_THROTTLE_MAPPING_INPUT:${props.throttleNumber}`, 'number', 30);
+    const [expertMode, setExpertMode] = useState(false);
 
     const currentDetent = (
-        <div className={`flex flex-row ${props.disabled ? 'opacity-30' : ''}`}>
-            <DetentConfig
-                throttlePosition={throttlePosition}
-                text={`${mappings[props.activeIndex].readableName} - Start`}
-                detentSetting={mappings[props.activeIndex].lowSetter}
-                detentValue={mappings[props.activeIndex].lowGetter[props.throttleCount - 1]}
-                throttleNumber={props.throttleNumber}
-                disabled={props.disabled}
-            />
-            <DetentConfig
-                throttlePosition={throttlePosition}
-                text={`${mappings[props.activeIndex].readableName} - End`}
-                detentSetting={mappings[props.activeIndex].hiSetter}
-                detentValue={mappings[props.activeIndex].hiGetter[props.throttleCount - 1]}
-                throttleNumber={props.throttleNumber}
-                disabled={props.disabled}
-            />
-        </div>
+        <DetentConfig
+            key={props.activeIndex}
+            index={props.activeIndex}
+            barPosition={props.throttleNumber === 1 ? 'right' : 'left'}
+            throttlePosition={throttlePosition}
+            upperBoundDetentSetter={props.mappingsAxisTwo
+                ? [props.mappingsAxisOne[props.activeIndex].getHiSetter(), props.mappingsAxisTwo[props.activeIndex].getHiSetter()]
+                : [props.mappingsAxisOne[props.activeIndex].getHiSetter()]}
+            lowerBoundDetentSetter={props.mappingsAxisTwo
+                ? [props.mappingsAxisOne[props.activeIndex].getLowSetter(), props.mappingsAxisTwo[props.activeIndex].getLowSetter()]
+                : [props.mappingsAxisOne[props.activeIndex].getLowSetter()]}
+            lowerBoundDetentGetter={props.mappingsAxisOne[props.activeIndex].getLowGetter()}
+            upperBoundDetentGetter={props.mappingsAxisOne[props.activeIndex].getHiGetter()}
+            detentValue={props.mappingsAxisOne[props.activeIndex].getLowGetter()}
+            throttleNumber={props.throttleNumber}
+            expertMode={expertMode}
+        />
     );
 
     return (
-        <div>
+        <div className="w-50">
             <h1 className="text-white mb-4 text-3xl ">
                 Axis
                 {' '}
                 {props.throttleNumber}
                 {' '}
             </h1>
-            <div className="bg-gray-800 rou px-4 pt-5 pb-4 sm:p-6 sm:pb-4 mt-4 mb-4 rounded-lg">
-                <h1 className="text-white mb-8 text-xl ">
-                    Current Value:
-                    {' '}
-                    {throttlePosition.toFixed(2)}
-                </h1>
+            <div className="bg-gray-800 px-4 pt-5 mt-4 rounded-lg">
+                <div className="flex flew-row justify-center">
+                    <h1 className="text-white mb-8 text-xl ">
+                        Current Value:
+                        {' '}
+                        {throttlePosition.toFixed(2)}
+                        {' '}
+
+                    </h1>
+                    <div className="ml-2 text-blue-500"><IconEdit onMouseDown={() => setExpertMode(!expertMode)} size="1.5rem" stroke="1.5" /></div>
+                </div>
 
                 <div className="flex flex-row">
-                    {/*                     <Bar d={throttlePosition} barHeight={20} />
- */}
-                    <div className="justify-between items-center flex flex-col ">
-
-                        {/* mappings.map((m) => (
-                            <div className="flex flex-row ">
-
-                                <DetentConfig
-                                    throttlePosition={throttlePosition}
-                                    text={`Set ${m.readableName} - Start`}
-                                    detentSetting={m.lowSetter}
-                                    detentValue={m.lowGetter[props.throttleCount - 1]}
-                                    throttleNumber={props.throttleNumber}
-                                />
-                                <DetentConfig
-                                    throttlePosition={throttlePosition}
-                                    text={`Set ${m.readableName} - End`}
-                                    detentSetting={m.hiSetter}
-                                    detentValue={m.hiGetter[props.throttleCount - 1]}
-                                    throttleNumber={props.throttleNumber}
-                                />
-                            </div>
-                        )) */}
-
+                    <div className="justify-between items-center flex flex-col">
                         {currentDetent}
-
                     </div>
-
                 </div>
             </div>
         </div>
