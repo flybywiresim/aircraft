dd3906f770668c14d9a2ff777122606c7a7e41e4
David Walschots <davidwalschots@users.noreply.github.com>
2021-04-22 14:09:38 +0200

    fix: infinibat on flight start not always ON

--- flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/A32NX_Electricity.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/A32NX_Electricity.js
@@ -1,26 +1,40 @@
 class A32NX_Electricity {
     constructor() {
+        this.batteryOnDuration = 0;
         /**
          * Initialises the infinite battery to which buses that are powered are connected
          * to simulate availability of electricity.
          *
-         * This function cannot be called on init, as the sim's default ELEC state seemingly hasn't
-         * fully initialised by that point.
-         *
-         * Internally the function ensures it's cheap to execute by only retrieving sim variables
-         * the first time it is called.
+         * Depending on the loading order in the simulator, this function is sometimes
+         * called too early, causing the battery to not be on despite turning it on.
+         * Therefore, we check if the infinite battery is on for at least a whole minute
+         * before stopping this initialisation.
          */
-        this.initialiseInfiniteBattery = () => {
+        this.initialiseInfiniteBattery = (deltaTime) => {
             const infiniteBatteryNumber = 1;
-            if (!SimVar.GetSimVarValue("A:ELECTRICAL MASTER BATTERY:" + infiniteBatteryNumber, "Bool")) {
-                SimVar.SetSimVarValue("K:TOGGLE_MASTER_BATTERY", "Number", infiniteBatteryNumber);
+            if (!this.batteryIsOn(infiniteBatteryNumber)) {
+                this.batteryOnDuration = 0;
+                this.turnBatteryOn(infiniteBatteryNumber);
+            } else {
+                this.batteryOnDuration += deltaTime;
             }
 
-            this.initialiseInfiniteBattery = () => {};
+            const oneMinute = 60000;
+            if (this.batteryOnDuration >= oneMinute) {
+                this.initialiseInfiniteBattery = () => {};
+            }
         };
     }
 
-    update() {
-        this.initialiseInfiniteBattery();
+    update(deltaTime) {
+        this.initialiseInfiniteBattery(deltaTime);
+    }
+
+    batteryIsOn(number) {
+        return !!SimVar.GetSimVarValue("A:ELECTRICAL MASTER BATTERY:" + number, "Bool");
+    }
+
+    turnBatteryOn(number) {
+        SimVar.SetSimVarValue("K:TOGGLE_MASTER_BATTERY", "Number", number);
     }
 }
