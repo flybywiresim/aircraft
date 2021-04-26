39c25f60c073ede86787338f0716b34ae601e283
benjozork <benjamin.dupont.984@gmail.com>
2021-04-26 14:31:51 -0400

    refactor(fmgc): re-organizing

---
+++ src/fmgc/src/index.ts
@@ -0,0 +1,11 @@
+import { FlightPlanManager } from './flightplanning/FlightPlanManager';
+import { FlightPlanAsoboSync } from './flightplanning/FlightPlanAsoboSync';
+import { GuidanceManager } from './guidance/GuidanceManager';
+import { ManagedFlightPlan } from './flightplanning/ManagedFlightPlan';
+
+export {
+    FlightPlanManager,
+    ManagedFlightPlan,
+    FlightPlanAsoboSync,
+    GuidanceManager,
+};
