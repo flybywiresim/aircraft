fd2dd18380bd03c3dc80b1088f697c34263ad912
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2021-04-29 17:25:07 +0200

    remove comment

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/ATSU/A320_Neo_CDU_AOC_RequestsWeather.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/ATSU/A320_Neo_CDU_AOC_RequestsWeather.js
@@ -93,7 +93,7 @@ class CDUAocRequestsWeather {
         mcdu.onRightInput[5] = async () => {
             const icaos = [store["arpt1"], store["arpt2"], store["arpt3"], store["arpt4"]];
             if (icaos[0] == "" && icaos[1] == "" && icaos[2] == "" && icaos[3] == "") {
-                // mcdu.addNewMessage(NXFictionalMessages.noAiportSpecified);
+                mcdu.addNewMessage(NXFictionalMessages.noAiportSpecified);
                 return;
             }
             store["sendStatus"] = "QUEUED";
