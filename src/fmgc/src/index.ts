8a62c0fb5537e49ac4b4f5a037472911f71c487f
benjozork <benjamin.dupont.984@gmail.com>
2021-04-30 00:32:37 -0400

    refactor(fmgc/guidance): introduce GuidanceController, LnavDriver
    This re-architectures the guidance subsystem to consist of a main guidance controller and a controller for LNAV. NXFlightGuidance is replaced by LnavDriver.

--- src/fmgc/src/index.ts
+++ src/fmgc/src/index.ts
@@ -2,10 +2,12 @@ import { FlightPlanManager } from './flightplanning/FlightPlanManager';
 import { FlightPlanAsoboSync } from './flightplanning/FlightPlanAsoboSync';
 import { GuidanceManager } from './guidance/GuidanceManager';
 import { ManagedFlightPlan } from './flightplanning/ManagedFlightPlan';
+import { GuidanceController } from './guidance/GuidanceController';
 
 export {
     FlightPlanManager,
     ManagedFlightPlan,
     FlightPlanAsoboSync,
     GuidanceManager,
+    GuidanceController,
 };
