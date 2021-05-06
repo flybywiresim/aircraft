80f938cae24f8580fd77d90cf52c6f3d4629b099
benjozork <benjamin.dupont.984@gmail.com>
2021-05-06 02:32:43 -0400

    refactor(nd): rewrite in ReactJS

---
+++ src/instruments/src/ND/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { render } from '../Common';
+import { DisplayUnit } from '../Common/displayUnit';
+import { ArcMode } from './ArcMode';
+import { DebugInfo } from './DebugInfo';
+import { FlightPlanProvider } from '../Common/flightplan';
+
+import './styles.scss';
+
+const NavigationDisplay: React.FC = () => {
+    const [displayIndex] = useState(() => {
+        const url = document.getElementsByTagName('a32nx-nd-element')[0].getAttribute('url');
+
+        return parseInt(url?.substring(url.length - 1) ?? '1', 10);
+    });
+
+    return (
+        <DisplayUnit
+            electricitySimvar={displayIndex === 1 ? 'L:A32NX_ELEC_AC_ESS_BUS_IS_POWERED' : 'L:A32NX_ELEC_AC_2_BUS_IS_POWERED'}
+            potentiometerIndex={displayIndex === 1 ? 89 : 91}
+        >
+            <FlightPlanProvider>
+                <svg className="pfd-svg" version="1.1" viewBox="0 0 768 768">
+                    <ArcMode />
+
+                    <DebugInfo />
+                </svg>
+            </FlightPlanProvider>
+        </DisplayUnit>
+    );
+};
+
+render(<NavigationDisplay />);
