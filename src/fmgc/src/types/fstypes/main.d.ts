d544529fb129b10dcf0fe190f6b9d6faf1b642ec
benjozork <benjamin.dupont.984@gmail.com>
2021-05-06 02:32:16 -0400

    refactor(fmgc): WTDataStore -> NXDataStore

--- src/fmgc/src/types/fstypes/main.d.ts
+++ src/fmgc/src/types/fstypes/main.d.ts
@@ -23,28 +23,3 @@
  */
 
 declare const LZUTF8;
-
-declare module 'WorkingTitle' {
-  export class WTDataStore {
-      /**
-     * Retrieves a key from the datastore, possibly returning the default value
-     * @param key The name of the key to retrieve
-     * @param defaultValue The default value to use if the key does not exist
-     * @returns Either the stored value of the key, or the default value
-     */
-      static get(key: string, defaultValue: string | number | boolean): any;
-
-      /**
-     * Stores a key in the datastore
-     * @param key The name of the value to store
-     * @param The value to store
-     */
-      static set(key: string, value: string | number | boolean): any;
-  }
-
-  export class LZUTF8 {
-      static compress(input: string, options?: any): any;
-
-      static decompress(input: string, options?: any): any;
-  }
-}
