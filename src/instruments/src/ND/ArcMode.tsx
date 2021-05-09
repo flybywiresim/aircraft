2d0b5ad46e7bc4ac1f84d6510ccfb6d8c5546597
benjozork <benjamin.dupont.984@gmail.com>
2021-05-09 19:29:57 -0400

    refactor(nd): add basic flight path

--- src/instruments/src/ND/ArcMode.tsx
+++ src/instruments/src/ND/ArcMode.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSimVar } from '@instruments/common/simVars';
+import { MathUtils } from '@shared/MathUtils';
+import { FlightPath } from './FlightPath';
+import { MapParameters } from './utils/MapParameters';
 
-export const ArcMode: React.FC = () => (
-    <>
-    </>
-);
+export const ArcMode: React.FC = () => {
+    const [lat] = useSimVar('PLANE LATITUDE', 'degree latitude');
+    const [long] = useSimVar('PLANE LONGITUDE', 'degree longitude');
+    const [heading] = useSimVar('PLANE HEADING DEGREES MAGNETIC', 'degrees');
+
+    const [mapParams] = useState(() => {
+        const params = new MapParameters();
+        params.compute({ lat, long }, 100, heading);
+
+        return params;
+    });
+
+    useEffect(() => {
+        console.log('params update!');
+        mapParams.compute({ lat, long }, 100, heading);
+    }, [lat, long, heading].map((n) => MathUtils.fastToFixed(n, 3)));
+
+    return (
+        <FlightPath mapParams={mapParams} />
+    );
+};
