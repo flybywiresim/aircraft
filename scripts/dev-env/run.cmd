2f5cd951a5f76b9ef2bd24ca1323ad07df2b5b88
David Walschots <davidwalschots@users.noreply.github.com>
2021-05-17 12:21:54 +0200

    chore: update dev environment

--- scripts/dev-env/run.cmd
+++ scripts/dev-env/run.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set image="ghcr.io/flybywiresim/dev-env@sha256:0bf0844b914cbd45a8a0a866a71c7a07fa09308f48cb961708be479f0d63aa0f"
+set image="ghcr.io/flybywiresim/dev-env@sha256:07a2514a84d030b01d59e47df80223ce75f08817c775469e9a286fba3e1bb8d9"
 
 docker image inspect %image% 1> nul || docker system prune --filter label=flybywiresim=true -f
 docker run --rm -it -v "%cd%:/external" %image% %*
--- scripts/dev-env/run.sh
+++ scripts/dev-env/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE="ghcr.io/flybywiresim/dev-env@sha256:0bf0844b914cbd45a8a0a866a71c7a07fa09308f48cb961708be479f0d63aa0f"
+IMAGE="ghcr.io/flybywiresim/dev-env@sha256:07a2514a84d030b01d59e47df80223ce75f08817c775469e9a286fba3e1bb8d9"
 
 # only set `-it` if there is a tty
 if [ -t 0 ] && [ -t 1 ];
