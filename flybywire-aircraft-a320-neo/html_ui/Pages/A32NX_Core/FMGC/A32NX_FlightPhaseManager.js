flybywiresim/aircraft

commit 503fb5946e683929f2845df06430a15c82e86fe1

feat(fmgc): des init via fcu vs knob (#4687)
* feat(fmgc): des init via vs fcu knob

Signed-off-by: Leon Beeser <leon.beeser@yahoo.de>

* refactored variable declaration

Signed-off-by: Leon Beeser <leon.beeser@yahoo.de>

* multiple improvements

Signed-off-by: Leon Beeser <leon.beeser@yahoo.de>

* fixed logic error

Signed-off-by: Leon Beeser <leon.beeser@yahoo.de>

* fixed flight phase change on fcu action

Signed-off-by: Leon Beeser <leon.beeser@yahoo.de>

* small fix to correct behaviour

Signed-off-by: Leon Beeser <leon.beeser@yahoo.de>

Co-authored-by: Andreas Guther <andreas@guther.family>

flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/FMGC/A32NX_FlightPhaseManager.js
@@ -1,4 +1,32 @@
 /**
+ * The following functions are used to determine whether a flight phase can be changed or not
+ */
+function canInitiateTO(_fmc) {
+    const ra = Simplane.getAltitudeAboveGround();
+    return Math.round(ra / 100) !== Math.round(Simplane.getAltitude() / 100) && ra > 1.5 ||
+    (
+        Math.max(SimVar.GetSimVarValue("L:A32NX_AUTOTHRUST_TLA:1", "number"), SimVar.GetSimVarValue("L:A32NX_AUTOTHRUST_TLA:2", "number")) >= 35 &&
+        !isNaN(_fmc.v2Speed) &&
+        (
+            (
+                SimVar.GetSimVarValue("ENG N1 RPM:1", "percent") > .85 &&
+                SimVar.GetSimVarValue("ENG N1 RPM:2", "percent") > .85
+            ) ||
+            Math.abs(Simplane.getGroundSpeed()) > 80
+        )
+    );
+}
+
+function canInitiateDes(_fmc, _ignoreAlt = false) {
+    const fl = Math.round(Simplane.getAltitude() / 100);
+    const fcuSelFl = Simplane.getAutoPilotDisplayedAltitudeLockValue("feet") / 100;
+    const dest = _fmc.flightPlanManager.getDestination();
+    // Can initiate descent? OR Can initiate early descent?
+    return ((!!dest && dest.liveDistanceTo < 200 || !dest) && (fcuSelFl < _fmc.cruiseFlightLevel || (_ignoreAlt && fcuSelFl < fl)))
+        || (!!dest && dest.liveDistanceTo >= 200 && fl > 200 && fcuSelFl <= 200);
+}
+
+/**
  * The A32NX_FlightPhaseManager handles the FMGCs flight phases.
  * It does so by creating a unique flight phase object for every FMGC flight phase.
  *
@@ -81,35 +109,41 @@ class A32NX_FlightPhaseManager {
         }
     }
 
-    handleFcuInput() {
-        const fcuSelFl = Simplane.getAutoPilotDisplayedAltitudeLockValue("feet") / 100;
-        const fl = Math.round(Simplane.getAltitude() / 100);
-
+    handleFcuAltKnobPushPull() {
         // Try Initiate climb
         if (this.fmc.currentFlightPhase === FmgcFlightPhases.TAKEOFF) {
             this.changeFlightPhase(FmgcFlightPhases.CLIMB);
             return;
         }
 
-        if (this.fmc.currentFlightPhase === FmgcFlightPhases.CLIMB || this.fmc.currentFlightPhase === FmgcFlightPhases.CRUISE) {
-            const dest = this.fmc.flightPlanManager.getDestination();
-            // Try initiate descent
-            if (
-                (!!dest && dest.liveDistanceTo < 200 || !dest) &&
-                fcuSelFl < this.fmc.cruiseFlightLevel
-            ) {
-                this.changeFlightPhase(FmgcFlightPhases.DESCENT);
-                return;
-            }
+        if ((this.fmc.currentFlightPhase === FmgcFlightPhases.CLIMB || this.fmc.currentFlightPhase === FmgcFlightPhases.CRUISE) && canInitiateDes(this.fmc)) {
+            this.changeFlightPhase(FmgcFlightPhases.DESCENT);
+        }
+    }
 
-            // Try initiate early descent
-            if (
-                !!dest && dest.liveDistanceTo >= 200 &&
-                fl > 200 &&
-                fcuSelFl < 200
-            ) {
-                this.changeFlightPhase(FmgcFlightPhases.DESCENT);
+    handleFcuAltKnobTurn() {
+        if (this.fmc.currentFlightPhase === FmgcFlightPhases.CRUISE) {
+            const activeVerticalMode = SimVar.GetSimVarValue('L:A32NX_FMA_VERTICAL_MODE', 'enum');
+            if (canInitiateDes(this.fmc, true) && (activeVerticalMode === 13 || activeVerticalMode === 14 || activeVerticalMode === 15 || activeVerticalMode === 23)) {
+                this.fmc.flightPhaseManager.changeFlightPhase(FmgcFlightPhases.DESCENT);
             }
+        }
+    }
+
+    handleFcuVSKnob() {
+        if (this.fmc.currentFlightPhase === FmgcFlightPhases.CLIMB || this.fmc.currentFlightPhase === FmgcFlightPhases.CRUISE) {
+            /** a timeout of 100ms is required in order to receive the updated autopilot vertical mode */
+            setTimeout(() => {
+                const activeVerticalMode = SimVar.GetSimVarValue('L:A32NX_FMA_VERTICAL_MODE', 'enum');
+                const VS = SimVar.GetSimVarValue('L:A32NX_AUTOPILOT_VS_SELECTED', 'feet per minute');
+                if (activeVerticalMode === 14 && VS < 0) {
+                    if (canInitiateDes(this.fmc, true)) {
+                        this.changeFlightPhase(FmgcFlightPhases.DESCENT);
+                    } else {
+                        this.fmc._onStepClimbDescent();
+                    }
+                }
+            }, 100);
         }
     }
 
@@ -142,24 +176,7 @@ class A32NX_FlightPhase_PreFlight {
     }
 
     check(_deltaTime, _fmc) {
-        const ra = Simplane.getAltitudeAboveGround();
-
-        return this.takeoffConfirmation.write(
-            // we try to detect a false lift off (during terrain loading) from a true liftoff e.g. during takeoff. (temporary solution only)
-            Math.round(ra / 100) !== Math.round(Simplane.getAltitude() / 100) && ra > 1.5 ||
-            (
-                Math.max(SimVar.GetSimVarValue("L:A32NX_AUTOTHRUST_TLA:1", "number"), SimVar.GetSimVarValue("L:A32NX_AUTOTHRUST_TLA:2", "number")) >= 35 &&
-                !isNaN(_fmc.v2Speed) &&
-                (
-                    (
-                        SimVar.GetSimVarValue("ENG N1 RPM:1", "percent") > .85 &&
-                        SimVar.GetSimVarValue("ENG N1 RPM:2", "percent") > .85
-                    ) ||
-                    Math.abs(Simplane.getGroundSpeed()) > 80
-                )
-            ),
-            _deltaTime
-        );
+        return this.takeoffConfirmation.write(canInitiateTO(_fmc), _deltaTime);
     }
 }
 
@@ -231,7 +248,6 @@ class A32NX_FlightPhase_Climb {
     }
 }
 
-//TODO: implement ability to initiate descent with V/S knob
 class A32NX_FlightPhase_Cruise {
     constructor() {
         this.nextFmgcFlightPhase = FmgcFlightPhases.DONE;
@@ -347,22 +363,6 @@ class A32NX_FlightPhase_Done {
     }
 
     check(_deltaTime, _fmc) {
-        const ra = Simplane.getAltitudeAboveGround();
-
-        return this.takeoffConfirmation.write(
-            Math.round(ra / 100) !== Math.round(Simplane.getAltitude() / 100) && ra > 1.5 ||
-            (
-                Math.max(SimVar.GetSimVarValue("L:A32NX_AUTOTHRUST_TLA:1", "number"), SimVar.GetSimVarValue("L:A32NX_AUTOTHRUST_TLA:2", "number")) >= 35 &&
-                !isNaN(_fmc.v2Speed) &&
-                (
-                    (
-                        SimVar.GetSimVarValue("ENG N1 RPM:1", "percent") > .85 &&
-                        SimVar.GetSimVarValue("ENG N1 RPM:2", "percent") > .85
-                    ) ||
-                    Math.abs(Simplane.getGroundSpeed()) > 80
-                )
-            ),
-            _deltaTime
-        );
+        return this.takeoffConfirmation.write(canInitiateTO(_fmc), _deltaTime);
     }
 }

flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -1034,22 +1034,17 @@ class FMCMainDisplay extends BaseAirliners {
             this._onModeManagedHeading();
         }
         if (_event === "MODE_SELECTED_ALTITUDE") {
-            this.flightPhaseManager.handleFcuInput();
+            this.flightPhaseManager.handleFcuAltKnobPushPull();
             this._onModeSelectedAltitude();
-
-            if (this.currentFlightPhase === FmgcFlightPhases.CRUISE) {
-                this._onStepClimbDescent(Simplane.getAutoPilotDisplayedAltitudeLockValue() / 100);
-            }
+            this._onStepClimbDescent();
         }
         if (_event === "MODE_MANAGED_ALTITUDE") {
-            this.flightPhaseManager.handleFcuInput();
+            this.flightPhaseManager.handleFcuAltKnobPushPull();
             this._onModeManagedAltitude();
-
-            if (this.currentFlightPhase === FmgcFlightPhases.CRUISE) {
-                this._onStepClimbDescent(Simplane.getAutoPilotDisplayedAltitudeLockValue() / 100);
-            }
+            this._onStepClimbDescent();
         }
         if (_event === "AP_DEC_ALT" || _event === "AP_INC_ALT") {
+            this.flightPhaseManager.handleFcuAltKnobTurn();
             if (this.currentFlightPhase === FmgcFlightPhases.CLIMB) {
                 const fcuFl = Simplane.getAutoPilotDisplayedAltitudeLockValue() / 100;
                 if (this._activeCruiseFlightLevelDefaulToFcu || fcuFl >= this._cruiseFlightLevel) {
@@ -1066,6 +1061,9 @@ class FMCMainDisplay extends BaseAirliners {
                 Coherent.call("HEADING_BUG_SET", 1, currentHeading);
             }
             SimVar.SetSimVarValue("L:A320_FCU_SHOW_SELECTED_HEADING", "number", 1);
+        }
+        if (_event === "VS") {
+            this.flightPhaseManager.handleFcuVSKnob();
         }
     }
 
@@ -1110,9 +1108,17 @@ class FMCMainDisplay extends BaseAirliners {
         }
     }
 
-    _onStepClimbDescent(_targetFl) {
-        // Only show the message on step climbs
-        if (_targetFl > this.cruiseFlightLevel) {
+    _onStepClimbDescent() {
+        if (!(this.currentFlightPhase === FmgcFlightPhases.CLIMB || this.currentFlightPhase === FmgcFlightPhases.CRUISE)) {
+            return;
+        }
+
+        const _targetFl = Simplane.getAutoPilotDisplayedAltitudeLockValue() / 100;
+
+        if (
+            (this.currentFlightPhase === FmgcFlightPhases.CLIMB && _targetFl >= this.cruiseFlightLevel) ||
+            (this.currentFlightPhase === FmgcFlightPhases.CRUISE && _targetFl !== this.cruiseFlightLevel)
+        ) {
             this.addNewMessage(NXSystemMessages.newCrzAlt.getSetMessage(_targetFl * 100));
         }
         this.cruiseFlightLevel = _targetFl;

src/behavior/src/A32NX_Interior_FCU.xml
@@ -288,6 +288,7 @@
                     } els{
                         (&gt;H:A320_Neo_FCU_VS_INC_VS)
                     }
+                    (&gt;H:A320_Neo_CDU_VS)
                 </CLOCKWISE_CODE>
                 <ANTICLOCKWISE_CODE>
                     (L:A32NX_TRK_FPA_MODE_ACTIVE, bool) 1 == if{
@@ -295,12 +296,15 @@
                     } els{
                         (&gt;H:A320_Neo_FCU_VS_DEC_VS)
                     }
+                    (&gt;H:A320_Neo_CDU_VS)
                 </ANTICLOCKWISE_CODE>
                 <PULL_CODE>
                     (&gt;H:A320_Neo_FCU_VS_PULL)
+                    (&gt;H:A320_Neo_CDU_VS)
                 </PULL_CODE>
                 <PUSH_CODE>
                     (&gt;H:A320_Neo_FCU_VS_PUSH)
+                    (&gt;H:A320_Neo_CDU_VS)
                 </PUSH_CODE>
                 <CENTER_RADIUS>0</CENTER_RADIUS>
                 <WWISE_EVENT_1>QNHknob</WWISE_EVENT_1>

commit a0f27cf8515012b032ab1d939f53b2ad9cb37fe5

fixed issues and improved functionalities introduced by #4687
Signed-off-by: Leon Beeser <leon.beeser@yahoo.de>

flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/FMGC/A32NX_FlightPhaseManager.js
@@ -17,12 +17,12 @@ function canInitiateTO(_fmc) {
     );
 }
 
-function canInitiateDes(_fmc, _ignoreAlt = false) {
+function canInitiateDes(_fmc) {
     const fl = Math.round(Simplane.getAltitude() / 100);
     const fcuSelFl = Simplane.getAutoPilotDisplayedAltitudeLockValue("feet") / 100;
     const dest = _fmc.flightPlanManager.getDestination();
     // Can initiate descent? OR Can initiate early descent?
-    return ((!!dest && dest.liveDistanceTo < 200 || !dest) && (fcuSelFl < _fmc.cruiseFlightLevel || (_ignoreAlt && fcuSelFl < fl)))
+    return (((!!dest && dest.liveDistanceTo < 200) || !dest || fl < 200) && fcuSelFl < _fmc.cruiseFlightLevel && fcuSelFl < fl)
         || (!!dest && dest.liveDistanceTo >= 200 && fl > 200 && fcuSelFl <= 200);
 }
 
@@ -124,7 +124,9 @@ class A32NX_FlightPhaseManager {
     handleFcuAltKnobTurn() {
         if (this.fmc.currentFlightPhase === FmgcFlightPhases.CRUISE) {
             const activeVerticalMode = SimVar.GetSimVarValue('L:A32NX_FMA_VERTICAL_MODE', 'enum');
-            if (canInitiateDes(this.fmc, true) && (activeVerticalMode === 13 || activeVerticalMode === 14 || activeVerticalMode === 15 || activeVerticalMode === 23)) {
+            const VS = SimVar.GetSimVarValue('L:A32NX_AUTOPILOT_VS_SELECTED', 'feet per minute');
+            const FPA = SimVar.GetSimVarValue('L:A32NX_AUTOPILOT_FPA_SELECTED', 'Degrees');
+            if ((activeVerticalMode === 13 || (activeVerticalMode === 14 && VS < 0) || (activeVerticalMode === 15 && FPA < 0) || activeVerticalMode === 23) && canInitiateDes(this.fmc)) {
                 this.fmc.flightPhaseManager.changeFlightPhase(FmgcFlightPhases.DESCENT);
             }
         }
@@ -136,8 +138,9 @@ class A32NX_FlightPhaseManager {
             setTimeout(() => {
                 const activeVerticalMode = SimVar.GetSimVarValue('L:A32NX_FMA_VERTICAL_MODE', 'enum');
                 const VS = SimVar.GetSimVarValue('L:A32NX_AUTOPILOT_VS_SELECTED', 'feet per minute');
-                if (activeVerticalMode === 14 && VS < 0) {
-                    if (canInitiateDes(this.fmc, true)) {
+                const FPA = SimVar.GetSimVarValue('L:A32NX_AUTOPILOT_FPA_SELECTED', 'Degrees');
+                if ((activeVerticalMode === 14 && VS < 0) || (activeVerticalMode === 15 && FPA < 0)) {
+                    if (canInitiateDes(this.fmc)) {
                         this.changeFlightPhase(FmgcFlightPhases.DESCENT);
                     } else {
                         this.fmc._onStepClimbDescent();

flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -187,6 +187,7 @@ class FMCMainDisplay extends BaseAirliners {
         this.managedSpeedDescendIsPilotEntered = false;
         this.managedSpeedDescendMach = .78;
         // this.managedSpeedDescendMachIsPilotEntered = false;
+        this.cruiseFlightLevelTimeOut = undefined;
     }
 
     Init() {
@@ -1100,15 +1101,7 @@ class FMCMainDisplay extends BaseAirliners {
         }
         if (_event === "AP_DEC_ALT" || _event === "AP_INC_ALT") {
             this.flightPhaseManager.handleFcuAltKnobTurn();
-            if (this.currentFlightPhase === FmgcFlightPhases.CLIMB) {
-                const fcuFl = Simplane.getAutoPilotDisplayedAltitudeLockValue() / 100;
-                if (this._activeCruiseFlightLevelDefaulToFcu || fcuFl >= this._cruiseFlightLevel) {
-                    this.cruiseFlightLevel = fcuFl;
-                    if (this.page.Current === this.page.ProgressPage) {
-                        CDUProgressPage.ShowPage(this);
-                    }
-                }
-            }
+            this._onTrySetCruiseFlightLevel();
         }
         if (_event === "AP_DEC_HEADING" || _event === "AP_INC_HEADING") {
             if (SimVar.GetSimVarValue("L:A320_FCU_SHOW_SELECTED_HEADING", "number") === 0) {
@@ -1171,13 +1164,50 @@ class FMCMainDisplay extends BaseAirliners {
         const _targetFl = Simplane.getAutoPilotDisplayedAltitudeLockValue() / 100;
 
         if (
-            (this.currentFlightPhase === FmgcFlightPhases.CLIMB && _targetFl >= this.cruiseFlightLevel) ||
+            (this.currentFlightPhase === FmgcFlightPhases.CLIMB && _targetFl > this.cruiseFlightLevel) ||
             (this.currentFlightPhase === FmgcFlightPhases.CRUISE && _targetFl !== this.cruiseFlightLevel)
         ) {
             this.addNewMessage(NXSystemMessages.newCrzAlt.getSetMessage(_targetFl * 100));
         }
         this.cruiseFlightLevel = _targetFl;
         this._cruiseFlightLevel = _targetFl;
+    }
+
+    _onTrySetCruiseFlightLevel() {
+        if (!(this.currentFlightPhase === FmgcFlightPhases.CLIMB || this.currentFlightPhase === FmgcFlightPhases.CRUISE)) {
+            return;
+        }
+
+        const activeVerticalMode = SimVar.GetSimVarValue('L:A32NX_FMA_VERTICAL_MODE', 'enum');
+
+        if ((activeVerticalMode >= 11 && activeVerticalMode <= 15) || (activeVerticalMode >= 21 && activeVerticalMode <= 23)) {
+            const fcuFl = Simplane.getAutoPilotDisplayedAltitudeLockValue() / 100;
+
+            if (this.currentFlightPhase === FmgcFlightPhases.CLIMB && fcuFl > this.cruiseFlightLevel ||
+                this.currentFlightPhase === FmgcFlightPhases.CRUISE && fcuFl !== this.cruiseFlightLevel
+            ) {
+                if (this.cruiseFlightLevelTimeOut) {
+                    clearTimeout(this.cruiseFlightLevelTimeOut);
+                    this.cruiseFlightLevelTimeOut = undefined;
+                }
+
+                this.cruiseFlightLevelTimeOut = setTimeout(() => {
+                    if (fcuFl === Simplane.getAutoPilotDisplayedAltitudeLockValue() / 100 &&
+                        (
+                            this.currentFlightPhase === FmgcFlightPhases.CLIMB && fcuFl > this.cruiseFlightLevel ||
+                            this.currentFlightPhase === FmgcFlightPhases.CRUISE && fcuFl !== this.cruiseFlightLevel
+                        )
+                    ) {
+                        this.addNewMessage(NXSystemMessages.newCrzAlt.getSetMessage(fcuFl * 100));
+                        this.cruiseFlightLevel = fcuFl;
+                        this._cruiseFlightLevel = fcuFl;
+                        if (this.page.Current === this.page.ProgressPage) {
+                            CDUProgressPage.ShowPage(this);
+                        }
+                    }
+                }, 3000);
+            }
+        }
     }
 
     /* END OF FMS EVENTS */