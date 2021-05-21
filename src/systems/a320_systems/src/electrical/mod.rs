69abf5d7a799ea6a9ec3bebafc3869fb05b91898
crocket63 <38904654+crocket63@users.noreply.github.com>
2021-05-21 12:20:11 +0200

    Update src/systems/a320_systems/src/electrical/mod.rs
    Co-authored-by: David Walschots <davidwalschots@users.noreply.github.com>

--- src/systems/a320_systems/src/electrical/mod.rs
+++ src/systems/a320_systems/src/electrical/mod.rs
@@ -370,7 +370,7 @@ impl A320EmergencyElectricalOverheadPanel {
         self.gen_1_line.is_on()
     }
 
-    pub fn rat_and_emer_gen_man_on(&self) -> bool {
+    fn rat_and_emer_gen_man_on(&self) -> bool {
         self.rat_and_emer_gen_man_on.is_pressed()
     }
 }
