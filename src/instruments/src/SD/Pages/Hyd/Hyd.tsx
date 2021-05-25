f8565d0df8e1335ceaa63142ee013784ea233f3f
Richard Pilbery <email@pilbery.co.uk>
2021-05-25 08:27:42 +0100

    rat stowed, the sequel

--- src/instruments/src/SD/Pages/Hyd/Hyd.tsx
+++ src/instruments/src/SD/Pages/Hyd/Hyd.tsx
@@ -210,7 +210,7 @@ const RAT = ({ x, y }: RATProps) => {
         <>
             <text className="RatPtuElec FillWhite" x={x - 42} y={y + 180} alignmentBaseline="central">RAT</text>
             <line className={`GreenLine ${RatStowed > 0.1 ? '' : 'Hide'}`} x1={x} y1={y + 180} x2={x + 10} y2={y + 180} />
-            <Triangle x={x} y={y + 180} colour={RatStowed > 0.1 ? 'Green' : 'White'} fill={RatStowed === 1 ? 1 : 0} orientation={90} />
+            <Triangle x={x} y={y + 180} colour={RatStowed > 0.1 ? 'Green' : 'White'} fill={RatStowed > 0.1 ? 1 : 0} orientation={90} />
         </>
     );
 };
