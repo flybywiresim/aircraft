7dcdc4087eaf1997797f1ebade009f4d52b60b66
Richard Pilbery <email@pilbery.co.uk>
2021-05-25 08:27:08 +0100

    rat code change

--- src/instruments/src/SD/Pages/Hyd/Hyd.tsx
+++ src/instruments/src/SD/Pages/Hyd/Hyd.tsx
@@ -209,8 +209,8 @@ const RAT = ({ x, y }: RATProps) => {
     return (
         <>
             <text className="RatPtuElec FillWhite" x={x - 42} y={y + 180} alignmentBaseline="central">RAT</text>
-            <line className={`GreenLine ${RatStowed === 1 ? '' : 'Hide'}`} x1={x} y1={y + 180} x2={x + 10} y2={y + 180} />
-            <Triangle x={x} y={y + 180} colour={RatStowed === 1 ? 'Green' : 'White'} fill={RatStowed === 1 ? 1 : 0} orientation={90} />
+            <line className={`GreenLine ${RatStowed > 0.1 ? '' : 'Hide'}`} x1={x} y1={y + 180} x2={x + 10} y2={y + 180} />
+            <Triangle x={x} y={y + 180} colour={RatStowed > 0.1 ? 'Green' : 'White'} fill={RatStowed === 1 ? 1 : 0} orientation={90} />
         </>
     );
 };
