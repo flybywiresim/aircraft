a32f738231eace7080a2c3d142c26f67ff672c5a
Andreas Guther <andreas@guther.family>
2021-05-26 00:12:13 +0200

    feat: implemented missing conditions for anti-droop

--- src/fbw/src/AnimationAileronHandler.cpp
+++ src/fbw/src/AnimationAileronHandler.cpp
@@ -6,19 +6,23 @@ AnimationAileronHandler::AnimationAileronHandler() {
   rateLimiterRight.setRate(RATE);
 }
 
-void AnimationAileronHandler::update(bool groundSpoilersActive, double flapsPosition, double position, double dt) {
-  isGroundSpoilersActive = groundSpoilersActive;
-
+void AnimationAileronHandler::update(bool autopilotActive,
+                                     bool groundSpoilersActive,
+                                     double pitchAttitudeDegrees,
+                                     double flapsHandleIndex,
+                                     double flapsPosition,
+                                     double position,
+                                     double dt) {
   double flapsBias = fmax(0.0, fmin(1.0, (flapsPosition - FLAPS_ANGLE_REFERENCE) / FLAPS_ANGLE_REFERENCE));
   droopBiasLeft = flapsBias * BIAS_DROOP_LEFT;
   droopBiasRight = flapsBias * BIAS_DROOP_RIGHT;
 
-  if (!isGroundSpoilersActive) {
-    targetValueLeft = fmax(-1.0, fmin(1.0, droopBiasLeft + position));
-    targetValueRight = fmax(-1.0, fmin(1.0, droopBiasRight + position));
-  } else {
+  if (groundSpoilersActive && !autopilotActive && (-pitchAttitudeDegrees) < PITCH_ATTITUDE_REFERENCE && flapsHandleIndex > 0) {
     targetValueLeft = POSITION_ANTI_DROOP_LEFT;
     targetValueRight = POSITION_ANTI_DROOP_RIGHT;
+  } else {
+    targetValueLeft = fmax(-1.0, fmin(1.0, droopBiasLeft + position));
+    targetValueRight = fmax(-1.0, fmin(1.0, droopBiasRight + position));
   }
 
   rateLimiterLeft.update(targetValueLeft, dt);
--- src/fbw/src/AnimationAileronHandler.h
+++ src/fbw/src/AnimationAileronHandler.h
@@ -6,11 +6,18 @@ class AnimationAileronHandler {
  public:
   AnimationAileronHandler();
 
-  void update(bool groundSpoilersActive, double flapsPosition, double position, double dt);
+  void update(bool autopilotActive,
+              bool groundSpoilersActive,
+              double pitchAttitudeDegrees,
+              double flapsHandleIndex,
+              double flapsPosition,
+              double position,
+              double dt);
   double getPositionLeft();
   double getPositionRight();
 
  private:
+  static constexpr double PITCH_ATTITUDE_REFERENCE = 2.5;
   static constexpr double FLAPS_ANGLE_REFERENCE = 5.0;
   static constexpr double BIAS_DROOP_LEFT = 0.2;
   static constexpr double BIAS_DROOP_RIGHT = -0.2;
@@ -18,7 +25,6 @@ class AnimationAileronHandler {
   static constexpr double POSITION_ANTI_DROOP_RIGHT = 1.0;
   static constexpr double RATE = 2.00;
 
-  bool isGroundSpoilersActive = false;
   double droopBiasLeft = 0;
   double droopBiasRight = 0;
   double targetValueLeft = 0;
