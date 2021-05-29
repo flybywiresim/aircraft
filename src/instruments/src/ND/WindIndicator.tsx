87722887365afc64e04fe0bf5918af07d34ce6f7
Andreas Guther <andreas@guther.family>
2021-05-30 00:33:07 +0200

    fix: show wind direction arrow on ND depending on plane heading

--- src/instruments/src/ND/WindIndicator.tsx
+++ src/instruments/src/ND/WindIndicator.tsx
@@ -2,9 +2,13 @@ import React, { FC } from 'react';
 import { useSimVar } from '@instruments/common/simVars';
 import { Layer } from '@instruments/common/utils';
 
+const mod = (x: number, n: number) => x - Math.floor(x / n) * n;
+
 export const WindIndicator: FC = () => {
     const [windDirection] = useSimVar('AMBIENT WIND DIRECTION', 'degrees', 500);
     const [windSpeed] = useSimVar('AMBIENT WIND VELOCITY', 'knots', 500);
+    const [planeHeading] = useSimVar('PLANE HEADING DEGREES MAGNETIC', 'degrees', 500);
+    const windDirectionArrow = mod(windDirection - planeHeading + 180, 360);
 
     return (
         <Layer x={17} y={50}>
@@ -13,7 +17,7 @@ export const WindIndicator: FC = () => {
             <text x={46} y={0} fontSize={20} className="Green">{Math.round(windSpeed)}</text>
 
             {windSpeed > 0 && (
-                <path className="Green" strokeWidth={2.25} strokeLinecap="round" d="M 0 30 v -20 m -7 8 l 7 -8 l 7 8" transform={`rotate(${windDirection} 0 20)`} />
+                <path className="Green" strokeWidth={2.25} strokeLinecap="round" d="M 0 30 v -20 m -7 8 l 7 -8 l 7 8" transform={`rotate(${windDirectionArrow} 0 20)`} />
             )}
         </Layer>
     );
