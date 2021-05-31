3de025b36fff52d20b4cfec4757aa2cebaedcda1
benjozork <benjamin.dupont.984@gmail.com>
2021-05-31 18:05:43 -0400

    fix(nd): improved wind & speed indicators

--- src/instruments/src/ND/SpeedIndicator.tsx
+++ src/instruments/src/ND/SpeedIndicator.tsx
@@ -1,17 +1,29 @@
 import React, { FC } from 'react';
 import { useSimVar } from '@instruments/common/simVars';
 import { Layer } from '@instruments/common/utils';
+import { AdirsTasDrivenIndicatorProps } from './index';
 
-export const SpeedIndicator: FC = () => {
-    const [gs] = useSimVar('GPS GROUND SPEED', 'knots');
-    const [tas] = useSimVar('AIRSPEED TRUE', 'knots');
+export const SpeedIndicator: FC<AdirsTasDrivenIndicatorProps> = ({ adirsState, tas }) => {
+    const [gs] = useSimVar('GPS GROUND SPEED', 'knots', 500);
 
     return (
-        <Layer x={2} y={22}>
-            <text x={0} y={0} fontSize={18} className="White">GS</text>
-            <text x={74} y={0} fontSize={22} textAnchor="end" className="Green">{Math.round(gs)}</text>
-            <text x={80} y={0} fontSize={18} className="White">TAS</text>
-            <text x={165} y={0} fontSize={22} textAnchor="end" className="Green">{Math.round(tas)}</text>
+        <Layer x={2} y={24}>
+            <text x={0} y={0} fontSize={20} className="White">GS</text>
+            <text x={74} y={0} fontSize={25} textAnchor="end" className="Green">
+                {(adirsState === 2) ? (
+                    Math.round(gs).toString().padStart(3)
+                ) : (
+                    '---'
+                )}
+            </text>
+            <text x={82} y={0} fontSize={20} className="White">TAS</text>
+            <text x={181} y={0} fontSize={25} textAnchor="end" className="Green">
+                {(adirsState === 2 && tas >= 60) ? (
+                    Math.round(tas).toString().padStart(3, '0')
+                ) : (
+                    '---'
+                )}
+            </text>
         </Layer>
     );
 };
