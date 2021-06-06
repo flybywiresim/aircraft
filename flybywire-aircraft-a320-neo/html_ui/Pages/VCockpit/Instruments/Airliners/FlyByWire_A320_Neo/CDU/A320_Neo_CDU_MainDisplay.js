176cc0e4650c7e6e769e6ead1a2bd020c1ce8bc3
2hwk <reasonstouninstall@tutanota.com>
2021-06-06 15:15:00 +0800

    KeyCode

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_MainDisplay.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_MainDisplay.js
@@ -915,42 +915,42 @@ class A320_Neo_CDU_MainDisplay extends FMCMainDisplay {
             if (this.inFocus) {
                 let keycode = e.keyCode;
                 this.lastInput = new Date().getTime() / 1000;
-                if (keycode >= 96 && keycode <= 105) {
+                if (keycode >= KeyCode.KEY_NUMPAD0 && keycode <= KeyCode.KEY_NUMPAD9) {
                     keycode -= 48;  // numpad support
                 }
                 // Note: tried using H-events, worse performance. Reverted to direct input.
-                if (keycode >= 48 && keycode <= 57 || keycode >= 65 && keycode <= 90) { // alphanumerics
+                if (keycode >= KeyCode.KEY_0 && keycode <= KeyCode.KEY_9 || keycode >= KeyCode.KEY_A && keycode <= KeyCode.KEY_Z) {
                     const letter = String.fromCharCode(keycode)
                     this.onLetterInput(letter);
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_1_"+ letter.toUpperCase(), "Number", 1); // TODO: L/R [1/2] side MCDU Split
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_2_"+ letter.toUpperCase(), "Number", 1);
                 }
-                else if (keycode === 190 || keycode === 110) { // .
+
+                else if (keycode === KeyCode.KEY_PERIOD || keycode === KeyCode.KEY_DECIMAL) {
                     this.onDot();
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_1_DOT", "Number", 1);
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_2_DOT", "Number", 1);
                 }
-                else if (keycode === 191) { // Fwd Slash
+                else if (keycode === KeyCode.KEY_SLASH) {
                     this.onDiv();
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_1_SLASH", "Number", 1);
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_2_SLASH", "Number", 1);
                 }
-                else if (keycode === 8) { // Backspace
+                else if (keycode === KeyCode.KEY_BACK_SPACE) {
                     this.onClr();
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_1_CLR", "Number", 1);
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_2_CLR", "Number", 1);
                 }
-                else if (keycode === 32) { // Space
+                else if (keycode === KeyCode.KEY_SPACE) {
                     this.onSp();
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_1_SP", "Number", 1);
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_2_SP", "Number", 1);
                 }
-                else if (keycode === 187 || keycode === 189 || keycode === 107 || keycode === 109) {    // Plus/Minus Keys
+                else if (keycode === 187 || keycode === 189 || keycode === KeyCode.KEY_ADD || keycode === KeyCode.KEY_SUBTRACT) {
                     this.onPlusMinus();
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_1_PLUSMINUS", "Number", 1);
                     SimVar.SetSimVarValue("L:A32NX_MCDU_PUSH_ANIM_2_PLUSMINUS", "Number", 1);
                 }
-
             }
         });
     }
