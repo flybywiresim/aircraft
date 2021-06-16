5a1497fca0070158f463b0bb0702d187b2eaa02d
Andreas Guther <andreas@guther.net>
2021-06-16 20:39:06 +0200

    Update src/fbw/src/FlyByWireInterface.cpp
    Co-authored-by: Benedict Etzel <developer@beheh.de>

--- src/fbw/src/FlyByWireInterface.cpp
+++ src/fbw/src/FlyByWireInterface.cpp
@@ -1259,7 +1259,7 @@ bool FlyByWireInterface::updateAutothrust(double sampleTime) {
 
   // update warnings
   auto fwcFlightPhase = idFwcFlightPhase->get();
-  if (fwcFlightPhase == 2 || fwcFlightPhase == 3 || fwcFlightPhase == 9) {
+  if (fwcFlightPhase == 2 || fwcFlightPhase == 3 || fwcFlightPhase == 4 || fwcFlightPhase == 8 || fwcFlightPhase == 9) {
     idAutothrustThrustLeverWarningFlex->set(autoThrustOutput.thrust_lever_warning_flex);
     idAutothrustThrustLeverWarningToga->set(autoThrustOutput.thrust_lever_warning_toga);
   } else {
