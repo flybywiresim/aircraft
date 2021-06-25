6ddfb3998b89fdebb889913dd60370c4e087d6c5
Michael Corcoran <tracer@outlook.co.nz>
2021-06-25 22:07:13 +1200

    feat(nd): display approach name at top

--- src/instruments/src/Common/flightplan.tsx
+++ src/instruments/src/Common/flightplan.tsx
@@ -37,3 +37,43 @@ export const useCurrentFlightPlan = (): ManagedFlightPlan => {
 
     return currentFlightPlan;
 };
+
+export type ApproachNameComponents = {
+    // the approach type, e.g. ILS or RNAV
+    type: string,
+
+    // the runway
+    runway: string,
+
+    // alphanumeric designator when multiple approaches of the same type exist for the same runway
+    designator: string | undefined,
+};
+
+export const parseApproachName = (name: string): ApproachNameComponents | undefined => {
+    // L(eft), C(entre), R(ight), T(true North) are the possible runway designators (ARINC424)
+    // If there are multiple procedures for the same type of approach, an alphanumeric suffix is added to their names (last subpattern)
+    // We are a little more lenient than ARINC424 in an effort to match non-perfect navdata, so we allow dashes, spaces, or nothing before the suffix
+    const match = name.trim().match(/^(ILS|LOC|RNAV|NDB|VOR|GPS) (RW)?([0-9]{1,2}[LCRT]?)([\s\-]*([A-Z0-9]))?$/);
+    if (!match) {
+        return;
+    }
+    return {
+        type: match[1],
+        runway: match[3],
+        designator: match[5]
+    };
+};
+
+/**
+ *
+ * @param name approach name from the nav database
+ * @returns max 9 digit name in the format <approach type><runway with leading zero><option -designator><spaces if needed>
+ */
+export const normaliseApproachName = (name: string): string => {
+    const appr = parseApproachName(name);
+    if (!appr) {
+        return '';
+    }
+    const suffix = appr.designator ? '-' + appr.designator : '';
+    return `${appr.type}${Avionics.Utils.formatRunway(appr.runway)}${suffix}`;
+}
