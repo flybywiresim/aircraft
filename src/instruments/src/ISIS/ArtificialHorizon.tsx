6b1f3504772d7390d73239556232345d69ac4d03
BlueberryKing <22713769+BlueberryKing@users.noreply.github.com>
2021-07-04 13:30:20 +0200

    Implement basic ATT RST function

--- src/instruments/src/ISIS/ArtificialHorizon.tsx
+++ src/instruments/src/ISIS/ArtificialHorizon.tsx
@@ -3,6 +3,7 @@ import { useSimVar } from '@instruments/common/simVars';
 import { PitchScale } from './PitchScale';
 import { RollScale } from './RollScale';
 import { RollIndex } from './RollIndex';
+import { Att10sFlag } from './Att10sFlag';
 
 const Sky: React.FC = () => <rect x={-256} y={-512} width={1024} height={768} className="sky" />;
 const Earth: React.FC = () => <rect x={-256} y={256} width={1024} height={768} className="earth" />;
@@ -17,19 +18,21 @@ export const ArtificialHorizon: React.FC<{ maskWidth: number }> = ({ maskWidth }
     const pitchShift = -pitch * pitchDegPixels;
 
     return (
-        <g id="ArtificialHorizon">
-            <g id="RollGroup" transform={`rotate(${roll} 256 256)`}>
-                <g id="PitchGroup" transform={`translate(0 ${pitchShift})`}>
-                    <Sky />
-                    <Earth />
-                    <PitchScale pitchDegPixels={pitchDegPixels} />
+        <Att10sFlag>
+            <g id="ArtificialHorizon">
+                <g id="RollGroup" transform={`rotate(${roll} 256 256)`}>
+                    <g id="PitchGroup" transform={`translate(0 ${pitchShift})`}>
+                        <Sky />
+                        <Earth />
+                        <PitchScale pitchDegPixels={pitchDegPixels} />
+                    </g>
+                    {/* TODO: Add actual delta time */}
+                    {/* TODO: Figure out if isOnGround is needed (does the lateral acceleration shown on ISIS differ from sideslip shown on PFD?) */}
+                    <RollIndex roll={roll} isOnGround={isOnGround} deltaTime={33} />
                 </g>
-                {/* TODO: Add actual delta time */}
-                {/* TODO: Figure out if isOnGround is needed (does the lateral acceleration shown on ISIS differ from sideslip shown on PFD?) */}
-                <RollIndex roll={roll} isOnGround={isOnGround} deltaTime={33} />
+                <RollScale />
+                <path id="Mask" className="mask" d="M 0 0 h 512 v 512 h -512 z M 108 120.5 c 50 -30 246 -30 296 0 v 271 c -50 30 -246 30 -296 0 z" />
             </g>
-            <RollScale />
-            <path id="Mask" className="mask" d="M 0 0 h 512 v 512 h -512 z M 108 120.5 c 50 -30 246 -30 296 0 v 271 c -50 30 -246 30 -296 0 z" />
-        </g>
+        </Att10sFlag>
     );
 };
