f1a2c0970f3de5b605a153da58ada5b467a0a3db
Michael Corcoran <tracer@outlook.co.nz>
2021-07-10 00:14:29 +1200

    feat(nd): add heading/track/course bugs

--- src/instruments/src/PFD/AttitudeIndicatorHorizon.tsx
+++ src/instruments/src/PFD/AttitudeIndicatorHorizon.tsx
@@ -4,7 +4,6 @@ import { useUpdate } from '@instruments/common/hooks';
 import {
     calculateHorizonOffsetFromPitch,
     calculateVerticalOffsetFromRoll,
-    getSmallestAngle,
     HorizontalTape,
     LagFilter,
 } from './PFDUtils';
