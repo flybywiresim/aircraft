f9ccd0c026b887052d0a573390ebbf6261d047c6
Michael Corcoran <tracer@outlook.co.nz>
2021-07-10 00:14:29 +1200

    feat(nd): add heading/track/course bugs

--- src/instruments/src/Common/utils.tsx
+++ src/instruments/src/Common/utils.tsx
@@ -7,3 +7,19 @@ export const Layer: React.FC<LayerProps> = ({ x = 0, y = 0, children }) => (
         {children}
     </g>
 );
+
+/**
+ * Gets the smallest angle between two angles
+ * @param angle1 First angle in degrees
+ * @param angle2 Second angle in degrees
+ * @returns {number} Smallest angle between angle1 and angle2 in degrees
+ */
+ export const getSmallestAngle = (angle1: number, angle2: number) : number => {
+    let smallestAngle = angle1 - angle2;
+    if (smallestAngle > 180) {
+        smallestAngle -= 360;
+    } else if (smallestAngle < -180) {
+        smallestAngle += 360;
+    }
+    return smallestAngle;
+};
