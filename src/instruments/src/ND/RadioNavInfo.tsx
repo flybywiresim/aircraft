49e2fc5f1ca79ce04f9f2b2021d2231bd555799f
benjozork <benjamin.dupont.984@gmail.com>
2021-07-10 02:16:23 -0400

    style: formatting and eslint fixes

--- src/instruments/src/ND/RadioNavInfo.tsx
+++ src/instruments/src/ND/RadioNavInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSimVar } from '@instruments/common/simVars';
 import { TuningMode } from '@fmgc/radionav';
+import { EfisSide } from './index';
 
 export enum NavAidMode {
     Off = 0,
@@ -8,31 +9,38 @@ export enum NavAidMode {
     VOR,
 }
 
-export type RadioNavInfoProps = { index: 1 | 2, side: 'L' | 'R' }
+export type RadioNavInfoProps = { index: 1 | 2, side: EfisSide }
 
 const TuningModeIndicator: React.FC<{ index: 1 | 2, frequency: number }> = ({ index, frequency }) => {
     const [tuningMode] = useSimVar(`L:A32NX_FMGC_RADIONAV_${index}_TUNING_MODE`, 'enum');
 
     return (
         frequency > 1 && tuningMode !== TuningMode.Auto && (
-            <text x={index === 1 ? 138 : 616} y={720} fontSize={20} textDecoration="underline" fill="#ffffff">{tuningMode === TuningMode.Manual ? "M" : "R"}</text>
+            <text x={index === 1 ? 138 : 616} y={720} fontSize={20} textDecoration="underline" fill="#ffffff">{tuningMode === TuningMode.Manual ? 'M' : 'R'}</text>
         ) || null
     );
-}
+};
 
 const VorInfo: React.FC<{index: 1 | 2}> = ({ index }) => {
     const [vorIdent] = useSimVar(`NAV IDENT:${index}`, 'string');
     const [vorFrequency] = useSimVar(`NAV ACTIVE FREQUENCY:${index}`, 'megahertz');
     const [vorHasDme] = useSimVar(`NAV HAS DME:${index}`, 'bool');
     const [dmeDistance] = useSimVar(`NAV DME:${index}`, 'nautical miles');
-    const [vorAvailable] = useSimVar(`NAV HAS NAV:${index}`, "boolean");
-
+    const [vorAvailable] = useSimVar(`NAV HAS NAV:${index}`, 'boolean');
 
     const x = index === 1 ? 37 : 668;
 
     const bigLittle = (value: number, digits: number) => {
-        const [intPart, decimalPart] = value.toFixed(digits).split(".", 2);
-        return <>{intPart}<tspan fontSize={20}>.{decimalPart}</tspan></>;
+        const [intPart, decimalPart] = value.toFixed(digits).split('.', 2);
+        return (
+            <>
+                {intPart}
+                <tspan fontSize={20}>
+                    .
+                    {decimalPart}
+                </tspan>
+            </>
+        );
     };
 
     const freqText = bigLittle(vorFrequency, 2);
@@ -45,7 +53,7 @@ const VorInfo: React.FC<{index: 1 | 2}> = ({ index }) => {
         }
     }
 
-    const path = index === 1 ? 'M25,675 L25,680 L37,696 L13,696 L25,680 M25,696 L25,719': 'M749,719 L749,696 L755,696 L743,680 L731,696 L737,696 L737,719 M743,680 L743,675';
+    const path = index === 1 ? 'M25,675 L25,680 L37,696 L13,696 L25,680 M25,696 L25,719' : 'M749,719 L749,696 L755,696 L743,680 L731,696 L737,696 L737,719 M743,680 L743,675';
 
     return (
         <g className="GtLayer">
@@ -56,7 +64,10 @@ const VorInfo: React.FC<{index: 1 | 2}> = ({ index }) => {
                 strokeLinejoin="round"
                 strokeLinecap="round"
             />
-            <text x={x} y={692} fontSize={24} className="White">VOR{index}</text>
+            <text x={x} y={692} fontSize={24} className="White">
+                VOR
+                {index}
+            </text>
             {(vorAvailable || vorHasDme) && vorFrequency > 1 && (
                 <text x={x} y={722} fontSize={24} className="White">{vorIdent}</text>
             )}
@@ -74,12 +85,12 @@ const VorInfo: React.FC<{index: 1 | 2}> = ({ index }) => {
 
 const AdfInfo: React.FC<{index: 1 | 2}> = ({ index }) => {
     const [adfIdent] = useSimVar(`ADF IDENT:${index}`, 'string');
-    const [adfFrequency] = useSimVar(`ADF ACTIVE FREQUENCY:${index}`, "kilohertz");
+    const [adfFrequency] = useSimVar(`ADF ACTIVE FREQUENCY:${index}`, 'kilohertz');
     const [adfAvailable] = useSimVar(`ADF SIGNAL:${index}`, 'boolean');
 
     const x = index === 1 ? 37 : 668;
 
-    const path = index === 1 ? 'M31,686 L25,680 L19,686 M25,680 L25,719': 'M749,719 L749,696 L743,690 L737,696 L737,719 M743,690 L743,675';
+    const path = index === 1 ? 'M31,686 L25,680 L19,686 M25,680 L25,719' : 'M749,719 L749,696 L743,690 L737,696 L737,719 M743,690 L743,675';
 
     return (
         <g className="GtLayer">
@@ -90,7 +101,10 @@ const AdfInfo: React.FC<{index: 1 | 2}> = ({ index }) => {
                 strokeLinejoin="round"
                 strokeLinecap="round"
             />
-            <text x={x} y={692} fontSize={24} className="Green">ADF{index}</text>
+            <text x={x} y={692} fontSize={24} className="Green">
+                ADF
+                {index}
+            </text>
             <text x={x} y={722} fontSize={24} className="Green">{adfAvailable ? adfIdent : adfFrequency.toFixed(0)}</text>
             {adfAvailable && (
                 <text x={x} y={722} fontSize={24} className="Green">{adfIdent}</text>
