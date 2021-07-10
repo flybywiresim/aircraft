ca08e571b12a1ed4fce0802a3247d4b996837d6f
benjozork <benjamin.dupont.984@gmail.com>
2021-07-10 02:20:22 -0400

    refactor(nd): re-organize files

--- src/instruments/src/ND/elements/RadioNeedles.tsx
+++ src/instruments/src/ND/elements/RadioNeedles.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSimVar } from '@instruments/common/simVars';
 import { NavAidMode } from './RadioNavInfo';
-import { EfisSide, Mode } from './index';
+import { EfisSide, Mode } from '../index';
 
 type RadioNavPointerProps = { index: 1 | 2, side: EfisSide, displayMode: Mode, centreHeight: number };
 
