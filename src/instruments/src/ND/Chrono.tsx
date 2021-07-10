d216c6f3298230c5c3269f7e06c090e7d01dbf77
benjozork <benjamin.dupont.984@gmail.com>
2021-07-10 02:16:23 -0400

    style: formatting and eslint fixes

--- src/instruments/src/ND/Chrono.tsx
+++ src/instruments/src/ND/Chrono.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useInteractionEvent } from '@instruments/common/hooks';
 import { useSimVar } from '@instruments/common/simVars';
+import { EfisSide } from './index';
 
 // TODO need a font with the right H ' and " chars
 
 const getDisplayString = (seconds: number) : string => {
     if (seconds >= 3600) {
-        return `${Math.floor(seconds / 3600).toString().padStart(2, "0")}H${Math.floor((seconds % 3600) / 60).toString().padStart(2, "0")}'`;
-    } else {
-        return `${Math.floor(seconds / 60).toString().padStart(2, "0")}'${Math.floor(seconds % 60).toString().padStart(2, "0")}"`;
+        return `${Math.floor(seconds / 3600).toString().padStart(2, '0')}H${Math.floor((seconds % 3600) / 60).toString().padStart(2, '0')}'`;
     }
+    return `${Math.floor(seconds / 60).toString().padStart(2, '0')}'${Math.floor(seconds % 60).toString().padStart(2, '0')}"`;
 };
 
-export const Chrono: React.FC<{ side: 'L' | 'R' }> = ({ side }) => {
+export const Chrono: React.FC<{ side: EfisSide }> = ({ side }) => {
     const [absTime] = useSimVar('E:ABSOLUTE TIME', 'Seconds', 200);
     const [prevTime, setPrevTime] = useState(absTime);
 
@@ -29,26 +29,27 @@ export const Chrono: React.FC<{ side: 'L' | 'R' }> = ({ side }) => {
 
     useInteractionEvent(`A32NX_EFIS_${side}_CHRONO_PUSHED`, () => {
         switch (state) {
-            case 'HIDDEN':
-                setPrevTime(absTime);
-                setElapsedTime(0);
-                setState('RUNNING');
-                break;
-            case 'RUNNING':
-                setState('STOPPED');
-                break;
-            case 'STOPPED':
-                setState('HIDDEN');
-                break;
+        case 'HIDDEN':
+            setPrevTime(absTime);
+            setElapsedTime(0);
+            setState('RUNNING');
+            break;
+        case 'RUNNING':
+            setState('STOPPED');
+            break;
+        case 'STOPPED':
+            setState('HIDDEN');
+            break;
         }
     });
 
     if (state === 'HIDDEN') {
         return <></>;
-    } else {
-        return <g className="chrono">
+    }
+    return (
+        <g className="chrono">
             <rect x={0} y={632} width={104} height={30} className="Grey Fill" />
             <text x={8} y={652} fontSize={24} className="Green">{getDisplayString(elapsedTime)}</text>
-        </g>;
-    }
+        </g>
+    );
 };
