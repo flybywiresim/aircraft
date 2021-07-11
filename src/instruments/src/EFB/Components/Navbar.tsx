3c77813399191586a6ee9d3cf36c6e827429e431
Benjamin Dupont <benjozorkfr@gmail.com>
2021-07-11 14:48:35 -0400

    Update src/instruments/src/EFB/Components/Navbar.tsx

--- src/instruments/src/EFB/Components/Navbar.tsx
+++ src/instruments/src/EFB/Components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export type NavbarProps = {
     tabs: string[],
     onSelected: (index: number) => void;
-    activeIndex: number
+    activeIndex: number,
 };
 
 const c = {
