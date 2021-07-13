64e94c9cbfd075014685cdccd4a86c90d01e9935
BlueberryKing <22713769+BlueberryKing@users.noreply.github.com>
2021-07-13 23:53:08 +0200

    Implement automatic switch away from bugs after inactivity

--- src/instruments/src/ISIS/index.tsx
+++ src/instruments/src/ISIS/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useInteractionSimVar, useSimVar } from '@instruments/common/simVars';
 import { useInteractionEvent } from '@instruments/common/hooks';
 import { render } from '../Common';
@@ -11,23 +11,27 @@ import { AutoBrightness } from './AutoBrightness';
 import './style.scss';
 
 export const ISISDisplay: React.FC = () => {
-    const [ias] = useSimVar('AIRSPEED INDICATED', 'knots');
-    const [bugsActive] = useInteractionSimVar('L:A32NX_ISIS_BUGS_ACTIVE', 'Boolean', 'H:A32NX_ISIS_BUGS_PRESSED');
+    const [ias] = useSimVar('AIRSPEED INDICATED', 'knots', 200);
+    const [bugsActive, setBugsActive] = useInteractionSimVar('L:A32NX_ISIS_BUGS_ACTIVE', 'Boolean', 'H:A32NX_ISIS_BUGS_PRESSED');
     const isBrightnessUpPressed = useRef(false);
     const isBrightnessDownPressed = useRef(false);
 
+    const lastPilotInput = useRef(0);
     const [bugs, bugSetters] = useBugs();
 
-    // console.log(JSON.stringify(bugs));
     const [selectedIndex, setSelectedIndex] = useState(5);
     const selectedBug = bugs[selectedIndex];
 
     useInteractionEvent('A32NX_ISIS_KNOB_PRESSED', () => {
+        lastPilotInput.current = Date.now();
+
         selectedBug.isActive = !selectedBug.isActive;
         bugSetters[selectedIndex](selectedBug);
     });
 
     useInteractionEvent('A32NX_ISIS_KNOB_CLOCKWISE', () => {
+        lastPilotInput.current = Date.now();
+
         if (selectedBug.value >= selectedBug.max) {
             return;
         }
@@ -37,6 +41,8 @@ export const ISISDisplay: React.FC = () => {
     });
 
     useInteractionEvent('A32NX_ISIS_KNOB_ANTI_CLOCKWISE', () => {
+        lastPilotInput.current = Date.now();
+
         if (selectedBug.value <= selectedBug.min) {
             return;
         }
@@ -46,6 +52,8 @@ export const ISISDisplay: React.FC = () => {
     });
 
     useInteractionEvent('A32NX_ISIS_PLUS_PRESSED', () => {
+        lastPilotInput.current = Date.now();
+
         isBrightnessUpPressed.current = !isBrightnessUpPressed.current;
         if (!isBrightnessUpPressed.current) {
             return;
@@ -55,6 +63,8 @@ export const ISISDisplay: React.FC = () => {
     });
 
     useInteractionEvent('A32NX_ISIS_MINUS_PRESSED', () => {
+        lastPilotInput.current = Date.now();
+
         isBrightnessDownPressed.current = !isBrightnessDownPressed.current;
         if (!isBrightnessDownPressed.current) {
             return;
@@ -63,13 +73,25 @@ export const ISISDisplay: React.FC = () => {
         setSelectedIndex((5 + selectedIndex) % 6);
     });
 
+    useEffect(() => {
+        if (bugsActive) {
+            lastPilotInput.current = Date.now();
+        }
+    }, [bugsActive]);
+
+    useEffect(() => {
+        if (bugsActive && Date.now() - lastPilotInput.current > 15000) {
+            setBugsActive(false);
+        }
+    });
+
     return (
         <AutoBrightness bugsActive={bugsActive}>
-            <ISISDisplayUnit indicatedAirspeed={ias}>
+            <ISISDisplayUnit indicatedAirspeed={ias.toFixed(2)}>
                 <svg id="ISIS" className="ISIS" version="1.1" viewBox="0 0 512 512">
                     {bugsActive
                         ? <BugSetupDisplay bugs={bugs} selectedIndex={selectedIndex} />
-                        : <ArtificialHorizonDisplay indicatedAirspeed={ias} bugs={bugs} />}
+                        : <ArtificialHorizonDisplay indicatedAirspeed={ias.toFixed(2)} bugs={bugs} />}
                 </svg>
             </ISISDisplayUnit>
         </AutoBrightness>
