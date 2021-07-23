bc09d461bf1449619e6124146553ab675a25d410
ErickSharp <titaninmc@gmail.com>
2021-07-23 13:26:36 -0500

    replace elements and plug in values using hooks

--- src/instruments/src/SD/Pages/Eng/Eng.tsx
+++ src/instruments/src/SD/Pages/Eng/Eng.tsx
@@ -1,7 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { render } from '@instruments/common/index';
 import { setIsEcamPage } from '@instruments/common/defaults';
 import { Arc, Needle } from '@instruments/common/gauges';
+import { usePersistentProperty } from '@instruments/common/persistence';
+import { useSimVar } from '@instruments/common/simVars';
 import { PageTitle } from '../../Common/PageTitle';
 import { EcamPage } from '../../Common/EcamPage';
 
@@ -10,39 +12,44 @@ import { SvgGroup } from '../../Common/SvgGroup';
 
 setIsEcamPage('eng_page');
 
-export const EngPage: FC = () => (
-    <EcamPage name="main-eng">
-        <PageTitle x={0} y={18} text="ENGINE" />
+export const EngPage: FC = () => {
+    const [weightUnit] = usePersistentProperty('CONFIG_USING_METRIC_UNIT', '1');
 
-        <EngineColumn x={50} y={50} engineNumber={1} />
-        {/* TODO: attach this to simvar */}
+    return (
+        <EcamPage name="main-eng">
+            <PageTitle x={6} y={18} text="ENGINE" />
 
-        <line className="Indicator" x1={250} y1={75} x2={220} y2={77} />
-        <text x={300} y={70} className="FillWhite FontSmall">F.USED</text>
-        <text x={300} y={92} className="FillCyan FontSmall">KG</text>
-        <line className="Indicator" x1={350} y1={75} x2={380} y2={77} />
+            <EngineColumn x={90} y={40} engineNumber={1} />
 
-        <text x={300} y={135} className="FillWhite FontSmall">OIL</text>
-        <text x={300} y={165} className="FillCyan FontSmall">QT</text>
-        <text x={300} y={265} className="FillCyan FontSmall">PSI</text>
+            <line className="Indicator" x1={250} y1={75} x2={225} y2={77} />
+            <text x={300} y={75} className="FillWhite FontMedium TextCenter">F.USED</text>
+            <text x={300} y={97} className="FillCyan FontSmall TextCenter">{parseInt(weightUnit) === 1 ? 'KG' : 'LBS'}</text>
+            <line className="Indicator" x1={350} y1={75} x2={375} y2={77} />
 
-        <line className="Indicator" x1={250} y1={312} x2={220} y2={314} />
-        <text x={300} y={310} className="FillCyan FontSmall">&deg;C</text>
-        <line className="Indicator" x1={350} y1={312} x2={380} y2={314} />
+            <text x={300} y={135} className="FillWhite FontMedium TextCenter">OIL</text>
+            <text x={300} y={160} className="FillCyan FontSmall TextCenter">QT</text>
+            <text x={300} y={235} className="FillCyan FontSmall TextCenter">PSI</text>
 
-        <line className="Indicator" x1={250} y1={360} x2={220} y2={362} />
-        <text x={300} y={360} className="FillCyan FontSmall">VIB N1</text>
-        <line className="Indicator" x1={350} y1={360} x2={380} y2={362} />
+            <line className="Indicator" x1={250} y1={292} x2={225} y2={294} />
+            <text x={300} y={290} className="FillCyan FontSmall TextCenter">&deg;C</text>
+            <line className="Indicator" x1={350} y1={292} x2={375} y2={294} />
 
-        <line className="Indicator" x1={250} y1={390} x2={220} y2={392} />
-        <text x={300} y={390} className="FillCyan FontSmall">N2</text>
-        <line className="Indicator" x1={350} y1={390} x2={380} y2={392} />
+            <line className="Indicator" x1={250} y1={340} x2={225} y2={342} />
+            <text x={300} y={340} className="FillWhite FontSmall TextCenter">VIB N1</text>
+            <line className="Indicator" x1={350} y1={340} x2={375} y2={342} />
 
-        <line className="Indicator" x1={250} y1={488} x2={220} y2={490} />
-        <text x={300} y={490} className="FillCyan FontSmall">PSI</text>
-        <line className="Indicator" x1={350} y1={488} x2={380} y2={490} />
-    </EcamPage>
-);
+            <line className="Indicator" x1={250} y1={370} x2={225} y2={372} />
+            <text x={300} y={370} className="FillWhite FontSmall TextCenter">N2</text>
+            <line className="Indicator" x1={350} y1={370} x2={375} y2={372} />
+
+            <line className="Indicator" x1={250} y1={488} x2={225} y2={490} />
+            <text x={300} y={485} className="FillCyan FontSmall TextCenter">PSI</text>
+            <line className="Indicator" x1={350} y1={488} x2={375} y2={490} />
+
+            <EngineColumn x={210} y={40} engineNumber={2} />
+        </EcamPage>
+    );
+};
 
 interface EngineColumnProps {
     x: number,
@@ -51,26 +58,102 @@ interface EngineColumnProps {
     // TODO: add simvars for engine parameters
 }
 
-const EngineColumn = ({ x, y, engineNumber }: EngineColumnProps) => (
-    <SvgGroup x={x} y={y}>
-        <text x={x} y={y} className="FillGreen">81</text>
-        {/* use a variable here for fuel used */}
+const EngineColumn = ({ x, y, engineNumber }: EngineColumnProps) => {
+    const [weightUnit] = usePersistentProperty('CONFIG_USING_METRIC_UNIT', '1');
 
-        <Arc x={x} y={y + 20} toValue={100} scaleMax={100} className="WhiteLine NoFill" />
-        <text x={x - 40} y={y + 20} className="FillWhite FontLarge">0</text>
-        <text x={x + 40} y={y + 20} className="FillWhite FontLarge">25</text>
-        <Needle x={x} y={y + 20} scaleMax={100} value={0} className="GreenLine NoFill" />
-        <text x={x + (x / 2)} className="FillGreen">25</text>
-        {/* use a variable value for both the text and the needle */}
+    const [flightPhase] = useSimVar('L:A32NX_FWC_FLIGHT_PHASE', 'number', 2000);
+    const [pressureClassName, setPressureClassName] = useState('');
 
-        <Arc x={x} y={y + 120} toValue={100} scaleMax={100} className="WhiteLine NoFill" />
-        <Needle x={x} y={y + 120} scaleMax={100} value={0} className="GreenLine NoFill" />
-        <text x={x} y={y + 120} className="FillGreen FontLarge">285</text>
-        <text x={x} y={y + 135} className="FillGreen FontLarge">108</text>
-        <text x={x} y={y + 150} className="FillGreen FontLarge">2.1</text>
-        <text x={x} y={y + 165} className="FillGreen FontLarge">2.1</text>
-        {/* use a variable value for both the text and the needle */}
-    </SvgGroup>
-);
+    const [fuelUsed] = useSimVar(`L:A32NX_FUEL_USED:${engineNumber}`, 'number', 500);
+    // Fuel used has a step of 10 when in Kilograms and 20 when in imperial pounds
+    const displayedFuelUsed = parseInt(weightUnit) === 1 ? Math.round(fuelUsed / 10) * 10 : Math.round(fuelUsed / 20) * 20;
+
+    const [engineOilQuantity] = useSimVar(`ENG OIL QUANTITY:${engineNumber}`, 'percent', 150); //* 0.01
+    const displayedEngineOilQuantity = Math.round(engineOilQuantity / 0.5) * 0.5; // Engine oil quantity has a step of 0.2
+    const OIL_QTY_MAX = 17.1;
+
+    const [engineOilPressure] = useSimVar(`ENG OIL PRESSURE:${engineNumber}`, 'psi', 200);
+    const displayedEngineOilPressure = Math.round(engineOilPressure / 2) * 2; // Engine oil pressure has a step of 2
+    const OIL_PSI_MAX = 130;
+    const OIL_PSI_VLOW_LIMIT = 17;
+
+    const [engineOilTemperature] = useSimVar(`GENERAL ENG OIL TEMPERATURE:${engineNumber}`, 'celsius', 200);
+    const displayedEngineOilTemperature = Math.round(engineOilTemperature / 5) * 5; // Engine oil temperature has a step of 5
+
+    const [n1Vibration] = useSimVar(`TURB ENG VIBRATION:${engineNumber}`, 'Number');
+
+    const [n2Vibration] = useSimVar(`TURB ENG VIBRATION:${engineNumber}`, 'Number'); // FIXME: should have a different value than N1, currently API limited
+
+    const [isValveOpen, setIsValveOpen] = useState(false);
+
+    useEffect(() => {
+        if (flightPhase === 2 || flightPhase === 6 || engineOilPressure <= OIL_PSI_VLOW_LIMIT) {
+            if (engineOilPressure >= OIL_PSI_MAX - 1) {
+                setPressureClassName('animate-pulse');
+            }
+            if (engineOilPressure <= OIL_PSI_VLOW_LIMIT) {
+                setPressureClassName('animate-pulse RedLine');
+            }
+            if (pressureClassName.length > 0) {
+                if (engineOilPressure <= OIL_PSI_MAX - 4 || engineOilPressure >= OIL_PSI_VLOW_LIMIT + 0.5) {
+                    setPressureClassName('');
+                }
+            }
+        } else {
+            setPressureClassName('');
+        }
+    }, [flightPhase, engineOilPressure]);
+
+    function getNeedleValue(value: any, max: number): number {
+        const numberValue = (Number)(value);
+        if (numberValue < max) {
+            return (numberValue / max) * 100;
+        }
+        return 100;
+    }
+
+    return (
+        <SvgGroup x={x} y={y}>
+            <text x={x} y={y} className="FillGreen FontLarge TextCenter">{displayedFuelUsed}</text>
+
+            <line className="GaugeMarking" x1={x - 51} y1={y + 85} x2={x - 45} y2={y + 85} />
+            <line className="GaugeMarking" x1={x} y1={y + 35} x2={x} y2={y + 40} />
+            <line className="GaugeMarking" x1={x + 45} y1={y + 85} x2={x + 51} y2={y + 85} />
+            <Arc x={x} y={y + 85} radius={50} toValue={100} scaleMax={100} className="WhiteLine NoFill" />
+            <Needle x={x} y={y + 85} length={60} scaleMax={100} value={getNeedleValue(engineOilQuantity, OIL_QTY_MAX)} className="GreenLine NoFill" dashOffset={-40} />
+            <text x={x + 5} y={y + 85} className="FillGreen FontLarge TextCenter">
+                <tspan className="FontLarge">{displayedEngineOilQuantity.toString().split('.')[0]}</tspan>
+                <tspan className="FontSmall">.</tspan>
+                <tspan className="FontSmall">{displayedEngineOilQuantity.toString().split('.')[1]}</tspan>
+            </text>
+
+            <line className="GaugeMarking" x1={x} y1={y + 110} x2={x} y2={y + 115} />
+            <line className="GaugeMarking" x1={x + 45} y1={y + 160} x2={x + 51} y2={y + 160} />
+            <Arc x={x} y={y + 160} radius={50} toValue={100} scaleMax={100} className="WhiteLine NoFill" />
+            <Arc x={x} y={y + 160} radius={50} toValue={13} scaleMax={100} className="RedLine NoFill" />
+            <Needle x={x} y={y + 160} length={60} scaleMax={100} value={getNeedleValue(engineOilPressure, OIL_PSI_MAX)} className={`GreenLine NoFill ${pressureClassName}`} dashOffset={-40} />
+            <text x={x} y={y + 155} className={`FillGreen FontLarge TextCenter ${pressureClassName}`}>{displayedEngineOilPressure}</text>
+
+            <text x={x} y={y + 220} className="FillGreen FontLarge TextCenter">{displayedEngineOilTemperature}</text>
+            <text x={x} y={y + 270} className="FillGreen TextCenter">
+                <tspan className="FontLarge">{n1Vibration.toFixed(1).toString().split('.')[0]}</tspan>
+                <tspan className="FontSmall">.</tspan>
+                <tspan className="FontSmall">{n1Vibration.toFixed(1).toString().split('.')[1]}</tspan>
+            </text>
+            <text x={x} y={y + 300} className="FillGreen TextCenter">
+                <tspan className="FontLarge">{n2Vibration.toFixed(1).toString().split('.')[0]}</tspan>
+                <tspan className="FontSmall">.</tspan>
+                <tspan className="FontSmall">{n2Vibration.toFixed(1).toString().split('.')[1]}</tspan>
+            </text>
+
+            <g>
+                <circle r={12} cx={420} cy={456} />
+                <line id="StartValveRight_CLOSED" x1={408} y1={456} x2={432} y2={456} className={`${isValveOpen && 'hidden'}`} />
+                <line id="StartValveRight_OPEN" x1={420} y1={436} x2={420} y2={468} className={`${!isValveOpen && 'hidden'}`} />
+                <line id="ValveRightBleed" x1={420} y1={468} x2={420} y2={476} />
+            </g>
+        </SvgGroup>
+    );
+};
 
 render(<EngPage />);
