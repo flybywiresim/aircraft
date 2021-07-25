e6dd45f9e84147148e4aa4a4eb9bdb85bab489ea
Benjamin Dupont <benjozorkfr@gmail.com>
2021-07-25 01:01:55 -0400

    feat(fpm, lnav): refactoring of flight path building / rendering. add support for VM legs (#5362)
    * feat(fpm, lnav): refactoring of flight path building / rendering. add support for VM legs
    
    * fix: apply suggestions from code review
    
    * fix(ND): missing transitions, def Tf->TF
    
    * fix: VM leg heading not taken into account in flight plan syncing and multipleLegGeometry
    
    * fix: do not sequence discontinuity if there is a VM leg after
    
    * fix: getActiveLegPathGeometry VM leg generation
    
    * fix: respect when vector is last wp in segment
    
    * fix: mag var issue on VM legs
    
    * fix(nd): recompute geometry every 2 seconds
    
    * fix(fmgc): incorrect spacing, discont. msg when clr attempt, modification of tmp. fpln fixed
    
    * fix: VM guidance mag var
    
    * fix: use magnetic heading for to wp info
    
    * fix: draw transitions >180 degrees
    
    * fix: round heading for bug
    
    For some reason it sometimes becomes a non-integral value...
    
    * fix(nd): adjust map masks
    
    Should be very close to realistic. Will need a final pass of all
    geometry later for consistency.
    
    * feat(nd): constraints display
    
    * fix(nd): wind flicker 0 -> 360 -> 0
    
    * fix(nd): alt constraint circle
    
    Alt constrained waypoints are displayed with a circle around them. The
    circle should be white when the constraint is not taken into account/NAV
    is not engaged, magenta when it will be met, and amber when it will be
    missed. The met/missed predictions are not available yet (VNAV), so for
    now we just display them in white.
    
    * fix(cdu): cumulative distance/time calculations
    
    Co-authored-by: 2hwk <reasonstouninstall@tutanota.com>
    Co-authored-by: Michael Corcoran <tracer@outlook.co.nz>

---
+++ src/fmgc/src/guidance/lnav/legs/TF.ts
@@ -0,0 +1,131 @@
+import { Degrees, NauticalMiles } from '@typings/types';
+import { ControlLaw, GuidanceParameters } from '@fmgc/guidance/ControlLaws';
+import { LatLongData } from '@typings/fs-base-ui/html_ui/JS/Types';
+import { MathUtils } from '@shared/MathUtils';
+import { EARTH_RADIUS_NM } from '@fmgc/guidance/Geometry';
+import { Leg } from '@fmgc/guidance/lnav/legs';
+
+export class TFLeg implements Leg {
+    public from: WayPoint;
+
+    public to: WayPoint;
+
+    constructor(from: WayPoint, to: WayPoint) {
+        this.from = from;
+        this.to = to;
+    }
+
+    get bearing(): Degrees {
+        return Avionics.Utils.computeGreatCircleHeading(
+            this.from.infos.coordinates,
+            this.to.infos.coordinates,
+        );
+    }
+
+    getGuidanceParameters(ppos: LatLongData, trueTrack: Degrees): GuidanceParameters | null {
+        const fromLatLongAlt = this.from.infos.coordinates;
+
+        const desiredTrack = this.bearing;
+        const trackAngleError = MathUtils.mod(desiredTrack - trueTrack + 180, 360) - 180;
+
+        // crosstrack error
+        const bearingAC = Avionics.Utils.computeGreatCircleHeading(fromLatLongAlt, ppos);
+        const bearingAB = desiredTrack;
+        const distanceAC = Avionics.Utils.computeDistance(fromLatLongAlt, ppos);
+
+        const desiredOffset = 0;
+        const actualOffset = (
+            Math.asin(
+                Math.sin(Avionics.Utils.DEG2RAD * (distanceAC / EARTH_RADIUS_NM))
+                * Math.sin(Avionics.Utils.DEG2RAD * (bearingAC - bearingAB)),
+            ) / Avionics.Utils.DEG2RAD
+        ) * EARTH_RADIUS_NM;
+        const crossTrackError = desiredOffset - actualOffset;
+
+        return {
+            law: ControlLaw.LATERAL_PATH,
+            trackAngleError,
+            crossTrackError,
+            phiCommand: 0,
+        };
+    }
+
+    /**
+     * Calculates the angle between the leg and the aircraft PPOS.
+     *
+     * This effectively returns the angle ABC in the figure shown below:
+     *
+     * ```
+     * * A
+     * |
+     * * B (TO)
+     * |\
+     * | \
+     * |  \
+     * |   \
+     * |    \
+     * |     \
+     * |      \
+     * * FROM  * C (PPOS)
+     * ```
+     *
+     * @param ppos {LatLong} the current position of the aircraft
+     */
+    getAircraftToLegBearing(ppos: LatLongData): number {
+        const aircraftToTerminationBearing = Avionics.Utils.computeGreatCircleHeading(ppos, this.to.infos.coordinates);
+
+        // Rotate frame of reference to 0deg
+        let correctedLegBearing = this.bearing - aircraftToTerminationBearing;
+        if (correctedLegBearing < 0) {
+            correctedLegBearing = 360 + correctedLegBearing;
+        }
+
+        let aircraftToLegBearing = 180 - correctedLegBearing;
+        if (aircraftToLegBearing < 0) {
+            // if correctedLegBearing was greater than 180 degrees, then its supplementary angle is negative.
+            // In this case, we can subtract it from 360 degrees to obtain the bearing.
+
+            aircraftToLegBearing = 360 + aircraftToLegBearing;
+        }
+
+        return aircraftToLegBearing;
+    }
+
+    getDistanceToGo(ppos: LatLongData): NauticalMiles {
+        const aircraftLegBearing = this.getAircraftToLegBearing(ppos);
+
+        const absDtg = Avionics.Utils.computeGreatCircleDistance(ppos, this.to.infos.coordinates);
+
+        // @todo should be abeam distance
+        if (aircraftLegBearing >= 90 && aircraftLegBearing <= 270) {
+            // Since a line perpendicular to the leg is formed by two 90 degree angles, an aircraftLegBearing outside
+            // (North - 90) and (North + 90) is in the lower quadrants of a plane centered at the TO fix. This means
+            // the aircraft is NOT past the TO fix, and DTG must be positive.
+
+            return absDtg;
+        }
+
+        return -absDtg;
+    }
+
+    get distance(): NauticalMiles {
+        return Avionics.Utils.computeGreatCircleDistance(this.from.infos.coordinates, this.to.infos.coordinates);
+    }
+
+    isAbeam(ppos: LatLongAlt): boolean {
+        const bearingAC = Avionics.Utils.computeGreatCircleHeading(this.from.infos.coordinates, ppos);
+        const headingAC = Math.abs(MathUtils.diffAngle(this.bearing, bearingAC));
+        if (headingAC > 90) {
+            // if we're even not abeam of the starting point
+            return false;
+        }
+        const distanceAC = Avionics.Utils.computeDistance(this.from.infos.coordinates, ppos);
+        const distanceAX = Math.cos(headingAC * Avionics.Utils.DEG2RAD) * distanceAC;
+        // if we're too far away from the starting point to be still abeam of the ending point
+        return distanceAX <= this.distance;
+    }
+
+    toString(): string {
+        return `<TFLeg from=${this.from} to=${this.to}>`;
+    }
+}
