8d7bc4c28d6272dca0da76d016b9d881a9330d46
Will Pine <wpine215@gmail.com>
2021-07-27 02:20:02 -0700

    Added constraint data to Legs + fixed some ESLint warnings

--- src/fmgc/src/guidance/lnav/legs/index.ts
+++ src/fmgc/src/guidance/lnav/legs/index.ts
@@ -1,9 +1,38 @@
-import { Degrees, NauticalMiles } from '@typings/types';
+import { Degrees, NauticalMiles, Feet, Knots } from '@typings/types';
 import { Guidable } from '@fmgc/guidance/Geometry';
 import { LatLongData } from '@typings/fs-base-ui/html_ui/JS/Types';
+import { WayPoint } from '@fmgc/types/fstypes/FSTypes';
+
+export enum AltitudeConstraintType {
+    at,
+    atOrAbove,
+    atOrBelow,
+    range,
+}
+
+export enum SpeedConstraintType {
+    at,
+    atOrAbove,
+    atOrBelow,
+}
+
+export interface AltitudeConstraint {
+    type: AltitudeConstraintType,
+    altitude1: Feet,
+    altitude2: Feet | undefined,
+}
+
+export interface SpeedConstraint {
+    type: SpeedConstraintType,
+    speed: Knots,
+}
 
 export interface Leg extends Guidable {
     get bearing(): Degrees;
+
+    get distance(): NauticalMiles;
+
+    get speedConstraint(): SpeedConstraint | undefined;
 
     getGuidanceParameters(ppos: LatLongAlt, trueTrack: Degrees);
 
@@ -15,4 +44,45 @@ export interface Leg extends Guidable {
     getDistanceToGo(ppos: LatLongData): NauticalMiles;
 
     isAbeam(ppos);
+}
+
+export interface AltitudeConstrainedLeg extends Leg {
+    get altitudeConstraint(): AltitudeConstraint | undefined;
+}
+
+export function getAltitudeConstraintFromWaypoint(wp: WayPoint): AltitudeConstraint | undefined {
+    if (wp.altDesc && wp.altitude1) {
+        let ac: AltitudeConstraint;
+        ac.altitude1 = wp.altitude1;
+        ac.altitude2 = undefined;
+        switch (wp.altDesc) {
+        case 1:
+            ac.type = AltitudeConstraintType.at;
+            break;
+        case 2:
+            ac.type = AltitudeConstraintType.atOrAbove;
+            break;
+        case 3:
+            ac.type = AltitudeConstraintType.atOrBelow;
+            break;
+        case 4:
+            ac.type = AltitudeConstraintType.range;
+            ac.altitude2 = wp.altitude2;
+            break;
+        default:
+            break;
+        }
+        return ac;
+    }
+    return undefined;
+}
+
+export function getSpeedConstraintFromWaypoint(wp: WayPoint): SpeedConstraint | undefined {
+    if (wp.speedConstraint) {
+        let sc: SpeedConstraint;
+        sc.type = SpeedConstraintType.at;
+        sc.speed = wp.speedConstraint;
+        return sc;
+    }
+    return undefined;
 }
