1b08851e4c63ec68648afe372493217d12a6d401
Michael Corcoran <tracer@outlook.co.nz>
2021-07-28 21:53:29 +1200

    fix: selected heading display

--- src/instruments/src/ND/pages/ArcMode.tsx
+++ src/instruments/src/ND/pages/ArcMode.tsx
@@ -465,7 +465,7 @@ const Overlay: React.FC<OverlayProps> = memo(({ heading, track, rangeSetting, si
 
             <TrackBug heading={heading} track={track} />
             { lsDisplayed && <IlsCourseBug heading={heading} ilsCourse={ilsCourse} /> }
-            <SelectedHeadingBug heading={Math.round(heading)} selected={selectedHeading} />
+            <SelectedHeadingBug heading={heading} selected={selectedHeading} />
         </g>
 
         <RadioNeedle index={1} side={side} displayMode={Mode.ARC} centreHeight={620} />
@@ -633,7 +633,7 @@ const SelectedHeadingBug: React.FC<{heading: number, selected: number}> = ({ hea
             className="Cyan"
             fontSize={22}
         >
-            {`${selected}`}
+            {`${("" + Math.round(selected)).padStart(3, "0")}`}
         </text>
     );
 };
