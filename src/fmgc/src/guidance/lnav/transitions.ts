d15e0130a459c5d39252091ce808b205347801bf
Michael Corcoran <tracer@outlook.co.nz>
2021-08-01 21:26:10 +1200

    feat(fmgc): roll anticipation initial impl.

--- src/fmgc/src/guidance/lnav/transitions.ts
+++ src/fmgc/src/guidance/lnav/transitions.ts
@@ -1,12 +1,18 @@
 import { Guidable } from '@fmgc/guidance/Geometry';
-import { NauticalMiles } from '@typings/types';
+import { Degrees, NauticalMiles } from '@typings/types';
 
 export abstract class Transition implements Guidable {
     abstract isAbeam(ppos: LatLongAlt): boolean;
 
-    abstract getGuidanceParameters(ppos, trueTrack);
+    abstract getGuidanceParameters(ppos: LatLongAlt, trueTrack: Degrees);
 
-    abstract getDistanceToGo(ppos);
+    abstract getNominalRollAngle(gs): Degrees;
+
+    abstract get isCircularArc(): boolean;
+
+    abstract getDistanceToGo(ppos: LatLongAlt);
 
     abstract getTrackDistanceToTerminationPoint(ppos: LatLongAlt): NauticalMiles;
+
+    abstract getTurningPoints(): [LatLongAlt, LatLongAlt]
 }
