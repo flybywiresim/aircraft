2175e80c27b3d144138664139a8aba58e413a61d
Will Pine <wpine215@gmail.com>
2021-08-04 22:11:06 -0700

    Fixed discontinuity waypoint drawing (again) and added temporary flight plan display on ND

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -1704,6 +1704,7 @@ class FMCMainDisplay extends BaseAirliners {
     }
 
     setApproachTransitionIndex(transitionIndex, callback = EmptyCallback.Boolean) {
+        // console.log("FMCMainDisplay: setApproachTransitionIndex = ", transitionIndex);
         const arrivalIndex = this.flightPlanManager.getArrivalProcIndex();
         this.ensureCurrentFlightPlanIsTemporary(() => {
             this.flightPlanManager.setApproachTransitionIndex(transitionIndex, () => {
@@ -1715,6 +1716,7 @@ class FMCMainDisplay extends BaseAirliners {
     }
 
     setArrivalProcIndex(arrivalIndex, callback = EmptyCallback.Boolean) {
+        // console.log("FMCMainDisplay: setArrivalProcIndex = ", arrivalIndex);
         this.ensureCurrentFlightPlanIsTemporary(() => {
             this.flightPlanManager.setArrivalProcIndex(arrivalIndex, () => {
                 callback(true);
@@ -1723,6 +1725,7 @@ class FMCMainDisplay extends BaseAirliners {
     }
 
     setArrivalIndex(arrivalIndex, transitionIndex, callback = EmptyCallback.Boolean) {
+        // console.log("FMCMainDisplay: setArrivalIndex: arrivalIndex=", arrivalIndex, " transitionIndex=", transitionIndex);
         this.ensureCurrentFlightPlanIsTemporary(() => {
             this.flightPlanManager.setArrivalEnRouteTransitionIndex(transitionIndex, () => {
                 this.flightPlanManager.setArrivalProcIndex(arrivalIndex, () => {
@@ -1733,6 +1736,7 @@ class FMCMainDisplay extends BaseAirliners {
     }
 
     setApproachIndex(approachIndex, callback = EmptyCallback.Boolean) {
+        // console.log("FMCMainDisplay: setApproachIndex = ", approachIndex);
         this.ensureCurrentFlightPlanIsTemporary(() => {
             this.flightPlanManager.setApproachIndex(approachIndex, () => {
                 const approach = this.flightPlanManager.getApproach();
