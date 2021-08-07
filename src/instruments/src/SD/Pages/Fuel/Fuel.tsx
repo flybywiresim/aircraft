9c251b48ab89c024f1273cd985ed2e74c8525de2
Richard Pilbery <email@pilbery.co.uk>
2021-08-07 22:54:48 +0100

    refactor pump component

--- src/instruments/src/SD/Pages/Fuel/Fuel.tsx
+++ src/instruments/src/SD/Pages/Fuel/Fuel.tsx
@@ -6,46 +6,18 @@ import { SimVarProvider, useSimVar } from '@instruments/common/simVars';
 import { getRenderTarget, setIsEcamPage } from '@instruments/common/defaults';
 import { usePersistentProperty } from '../../../Common/persistence';
 import { fuelForDisplay, fuelInTanksForDisplay } from '../../Common/FuelFunctions';
+import { Triangle } from '../../Common/Shapes';
 
 setIsEcamPage('fuel_page');
 
 export const FuelPage = () => {
     const [crossFeedPosition] = useSimVar('FUELSYSTEM VALVE OPEN:3', 'number', 500);
 
-    const [pumpLeftInner1] = useSimVar('FUELSYSTEM PUMP ACTIVE:2', 'boolean', 500);
-    const [pumpLeftInner2] = useSimVar('FUELSYSTEM PUMP ACTIVE:5', 'boolean', 500);
-    const [pumpCenter1] = useSimVar('FUELSYSTEM PUMP ACTIVE:1', 'boolean', 500);
-    const [pumpCenter2] = useSimVar('FUELSYSTEM PUMP ACTIVE:4', 'boolean', 500);
-    const [pumpRightInner1] = useSimVar('FUELSYSTEM PUMP ACTIVE:3', 'boolean', 500);
-    const [pumpRightInner2] = useSimVar('FUELSYSTEM PUMP ACTIVE:6', 'boolean', 500);
-
     const [tankLeftOuter] = useSimVar('FUEL TANK LEFT AUX QUANTITY', 'gallons', 500);
     const [tankLeftInner] = useSimVar('FUEL TANK LEFT MAIN QUANTITY', 'gallons', 500);
     const [tankCenter] = useSimVar('FUEL TANK CENTER QUANTITY', 'gallons', 500);
     const [tankRightInner] = useSimVar('FUEL TANK RIGHT MAIN QUANTITY', 'gallons', 500);
     const [tankRightOuter] = useSimVar('FUEL TANK RIGHT AUX QUANTITY', 'gallons', 500);
-
-    const [pump1] = useSimVar('FUEL TRANSFER PUMP ON:1', 'bool', 500);
-    const [pump2] = useSimVar('FUEL TRANSFER PUMP ON:2', 'bool', 500);
-    const [pump3] = useSimVar('FUEL TRANSFER PUMP ON:3', 'bool', 500);
-    const [pump5] = useSimVar('FUEL TRANSFER PUMP ON:5', 'bool', 500);
-    const [pump7] = useSimVar('FUEL TRANSFER PUMP ON:7', 'bool', 500);
-    const [pump4] = useSimVar('FUEL TRANSFER PUMP ON:4', 'bool', 500);
-    const [pump6] = useSimVar('FUEL TRANSFER PUMP ON:6', 'bool', 500);
-    const [pump8] = useSimVar('FUEL TRANSFER PUMP ON:8', 'bool', 500);
-    const [pump9] = useSimVar('FUEL TRANSFER PUMP ON:9', 'bool', 500);
-    useEffect(() => {
-        console.log(`pump1 valve is ${pump1}`);
-        console.log(`pump2 valve is ${pump2}`);
-        console.log(`pump3 valve is ${pump3}`);
-        console.log(`pump5 valve is ${pump5}`);
-        console.log(`pump7 valve is ${pump7}`);
-        console.log(`pump4 valve is ${pump4}`);
-        console.log(`pump6 valve is ${pump6}`);
-        console.log(`pump8 valve is ${pump8}`);
-        console.log(`pump9 valve is ${pump9}`);
-        console.log(' ----------- ');
-    }, [pump1, pump2, pump3, pump5, pump7, pump4, pump6, pump8, pump9]);
 
     const [unit] = usePersistentProperty('CONFIG_USING_METRIC_UNIT');
 
@@ -54,6 +26,9 @@ export const FuelPage = () => {
 
     const leftFuelUsed = fuelForDisplay(leftConsumption, unit);
     const rightFuelUsed = fuelForDisplay(rightConsumption, unit);
+
+    const leftOuterFuelTransferInProgress = 0;
+    const rightOuterFuelTransferInProgress = 0;
 
     const [fuelWeightPerGallon] = useSimVar('FUEL WEIGHT PER GALLON', 'kilogram', 60_000);
 
@@ -98,12 +73,14 @@ export const FuelPage = () => {
                 <line className="FlowShape" x1={195} y1={190} x2={195} y2={215} />
 
                 {/* Pumps */}
-                <Pump x={145} y={215} active={!!pumpLeftInner1} />
-                <Pump x={180} y={215} onBus="DC_2" active={!!pumpLeftInner2} />
+                <Pump x={145} y={215} pumpNumber={2} />
+                <Pump x={180} y={215} onBus="DC_2" pumpNumber={5} />
 
                 {/* Quantities */}
                 <text className="TankQuantity" x={74} y={285}>{fuelInTanksForDisplay(tankLeftOuter, unit, fuelWeightPerGallon)}</text>
                 <text className="TankQuantity" x={190} y={285}>{fuelInTanksForDisplay(tankLeftInner, unit, fuelWeightPerGallon)}</text>
+
+                { leftOuterFuelTransferInProgress ? <Triangle x={77} y={319} colour="Green" fill={0} orientation={90} /> : null }
 
                 <text className="UnitTemp" x="70" y="355">°C</text>
             </>
@@ -131,8 +108,8 @@ export const FuelPage = () => {
                 <line className="FlowShape" x1="326" y1="150" x2="326" y2="205" />
 
                 {/* Pumps */}
-                <Pump x={259} y={205} active={!!pumpCenter1} />
-                <Pump x={311} y={205} active={!!pumpCenter2} />
+                <Pump x={259} y={205} pumpNumber={1} />
+                <Pump x={311} y={205} pumpNumber={4} />
 
                 {/* Quantities */}
                 <text className="TankQuantity" x={335} y={275}>{fuelInTanksForDisplay(tankCenter, unit, fuelWeightPerGallon)}</text>
@@ -152,12 +129,13 @@ export const FuelPage = () => {
                 <line className="FlowShape" x1={405} y1={190} x2={405} y2={215} />
 
                 {/* Pumps */}
-                <Pump x={390} y={215} active={!!pumpRightInner1} />
-                <Pump x={425} y={215} onBus="DC_2" active={!!pumpRightInner2} />
+                <Pump x={390} y={215} pumpNumber={3} />
+                <Pump x={425} y={215} onBus="DC_2" pumpNumber={6} />
 
                 {/* Quantities */}
                 <text className="TankQuantity" x={472} y={285}>{fuelInTanksForDisplay(tankRightInner, unit, fuelWeightPerGallon)}</text>
                 <text className="TankQuantity" x={579} y={285}>{fuelInTanksForDisplay(tankRightOuter, unit, fuelWeightPerGallon)}</text>
+                { rightOuterFuelTransferInProgress ? <Triangle x={522} y={319} colour="Green" fill={0} orientation={-90} /> : null }
 
                 <text className="UnitTemp" x="510" y="355">°C</text>
             </>
@@ -181,8 +159,8 @@ const Apu = () => {
 
     return (
         <g id="APU">
-            <text className="White" x="126" y="150" textAnchor="end" alignmentBaseline="central">APU</text>
-            <path className="FlowShape" d="M 132, 150 l 15, 9 l 0,-18 Z" />
+            <text className="White" x="122" y="150" textAnchor="end" alignmentBaseline="central">APU</text>
+            <Triangle x={129} y={150} colour="Green" fill={0} orientation={-90} />
             <path id="apuFuelLine" className="FlowShape" d="M147,150 l13,0" />
         </g>
     );
@@ -326,11 +304,22 @@ const CrossFeedValve = ({ x, y }) => {
     );
 };
 
-const Pump = ({ x, y, onBus = 'DC_ESS', active = true }) => {
+const Pump = ({ x, y, onBus = 'DC_ESS', pumpNumber }) => {
+    const [active] = useSimVar(`FUELSYSTEM PUMP ACTIVE:${pumpNumber}`, 'boolean', 500);
+    const [pushButtonPressed] = pumpNumber === 1 || pumpNumber === 4
+        ? pumpNumber === 1
+            ? useSimVar('L:XMLVAR_Momentary_PUSH_OVHD_FUEL_LTKPUMPS1_Pressed', 'bool', 500)
+            : useSimVar('L:XMLVAR_Momentary_PUSH_OVHD_FUEL_LTKPUMPS2_Pressed', 'bool', 500)
+        : [null];
+
+    useEffect(() => {
+        console.log(`Push buytton pressed for ${pumpNumber} is ${pushButtonPressed}`);
+    }, [pushButtonPressed]);
+
     const [busIsPowered] = useSimVar(`L:A32NX_ELEC_${onBus}_BUS_IS_POWERED`, 'Bool', 1000);
 
     return (
-        <g className={(active && busIsPowered) ? 'ThickShape PumpActive' : 'ThickShape PumpInactive'}>
+        <g className={(pumpNumber === 1 || pumpNumber === 4 ? pushButtonPressed && busIsPowered : active && busIsPowered) ? 'ThickShape PumpActive' : 'ThickShape PumpInactive'}>
             <rect x={x} y={y} width="30" height="30" />
             {active
                 ? (busIsPowered
