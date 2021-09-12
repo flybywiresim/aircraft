3a58e0373e170b7bef35f88a8fb60eb2436d261d
crocket63 <38904654+crocket63@users.noreply.github.com>
2021-09-12 08:59:09 +0200

    Update src/systems/a320_systems/src/hydraulic.rs
    Co-authored-by: BBK <22713769+BlueberryKing@users.noreply.github.com>

--- src/systems/a320_systems/src/hydraulic.rs
+++ src/systems/a320_systems/src/hydraulic.rs
@@ -687,7 +687,7 @@ impl A320HydraulicLoopController {
 }
 impl HydraulicLoopController for A320HydraulicLoopController {
     fn should_open_fire_shutoff_valve(&self, _pump_idx: usize) -> bool {
-        // A320 only has one main pump per pump section thus index not usefull
+        // A320 only has one main pump per pump section thus index not useful
         self.should_open_fire_shutoff_valve
     }
 }
