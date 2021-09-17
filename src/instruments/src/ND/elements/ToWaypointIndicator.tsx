5b86c777e6ca6cbc24132739f3aff8c9c7ea29bc
Michael Corcoran <tracer@outlook.co.nz>
2021-09-17 16:06:32 +1200

    fix(nd): to wp when data invalid

--- src/instruments/src/ND/elements/ToWaypointIndicator.tsx
+++ src/instruments/src/ND/elements/ToWaypointIndicator.tsx
@@ -44,24 +44,30 @@ export const ToWaypointIndicator: FC<ToWaypointIndicatorProps> = memo(({ info })
             {/* EIS2 can only display 9 characters for this ident */}
             <text x={-9} y={0} fontSize={25} className="White" textAnchor="end">{info.ident.substring(0, 8)}</text>
 
-            <text x={54} y={0} fontSize={25} className="Green" textAnchor="end">{(Math.round(heading)).toString().padStart(3, '0')}</text>
-            <text x={73} y={2} fontSize={25} className="Cyan" textAnchor="end">&deg;</text>
+            {isFinite(heading) && (<>
+                <text x={54} y={0} fontSize={25} className="Green" textAnchor="end">{(Math.round(heading)).toString().padStart(3, '0')}</text>
+                <text x={73} y={2} fontSize={25} className="Cyan" textAnchor="end">&deg;</text>
+            </>)}
 
-            {info.distanceFromPpos < 20 ? (
-                <>
-                    <text x={8} y={30} fontSize={24} className="Green" textAnchor="end">{distanceIntegralPart}</text>
-                    <text x={8} y={30} fontSize={23} className="Green" textAnchor="start">.</text>
-                    <text x={22} y={30} fontSize={19} className="Green" textAnchor="start">{distanceDecimalPart}</text>
-                </>
-            ) : (
-                <>
-                    <text x={34} y={30} fontSize={24} className="Green" textAnchor="end">{distanceFixed}</text>
-                </>
+            {isFinite(info.distanceFromPpos) && (<>
+                {info.distanceFromPpos < 20 ? (
+                    <>
+                        <text x={8} y={30} fontSize={24} className="Green" textAnchor="end">{distanceIntegralPart}</text>
+                        <text x={8} y={30} fontSize={23} className="Green" textAnchor="start">.</text>
+                        <text x={22} y={30} fontSize={19} className="Green" textAnchor="start">{distanceDecimalPart}</text>
+                    </>
+                ) : (
+                    <>
+                        <text x={34} y={30} fontSize={24} className="Green" textAnchor="end">{distanceFixed}</text>
+                    </>
+                )}
+
+                <text x={72} y={30} fontSize={18} className="Cyan" textAnchor="end">NM</text>
+            </>)}
+
+            {isFinite(info.etaFromPpos) && (
+                <text x={72} y={62} fontSize={25} className="Green" textAnchor="end">{timeText}</text>
             )}
-
-            <text x={72} y={30} fontSize={18} className="Cyan" textAnchor="end">NM</text>
-
-            <text x={72} y={62} fontSize={25} className="Green" textAnchor="end">{timeText}</text>
         </Layer>
     );
 });
