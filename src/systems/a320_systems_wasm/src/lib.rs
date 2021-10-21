9d7c953082c3faf76c0c9a48e1abd3b9de135e56
BlueberryKing <22713769+BlueberryKing@users.noreply.github.com>
2021-10-21 22:08:52 +0200

    Adapt to changes from master

--- src/systems/a320_systems_wasm/src/lib.rs
+++ src/systems/a320_systems_wasm/src/lib.rs
@@ -124,14 +124,14 @@ async fn systems(mut gauge: msfs::Gauge) -> Result<(), Box<dyn std::error::Error
             "BLEED AIR ENGINE",
             "Bool",
             1,
-            &vec!["OVHD_PNEU_ENG_1_BLEED_PB_IS_AUTO"],
+            vec!["OVHD_PNEU_ENG_1_BLEED_PB_IS_AUTO"],
         )?
         .provides_aircraft_variable_with_additional_names(
             "BLEED AIR ENGINE",
             "Bool",
             2,
-            &vec!["OVHD_PNEU_ENG_2_BLEED_PB_IS_AUTO"],
-        )
+            vec!["OVHD_PNEU_ENG_2_BLEED_PB_IS_AUTO"],
+        )?
         .build();
 
     let mut simulation = Simulation::new(|electricity| A320::new(electricity));
