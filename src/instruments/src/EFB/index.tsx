8e54e51a7cb74f5c2ddf10989db9c4121e3e952f
ErickSharp <titaninmc@gmail.com>
2021-10-25 21:54:28 -0500

    fix: redux store issues and fix reducers

--- src/instruments/src/EFB/index.tsx
+++ src/instruments/src/EFB/index.tsx
@@ -1,82 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { MemoryRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { FailuresOrchestratorProvider } from './failures-orchestrator-provider';
 import Efb from './Efb';
-import logo from './Assets/fbw-logo.svg';
 
 import './Assets/Reset.scss';
 import './Assets/Efb.scss';
 import { render } from '../Common/index';
 import { readSettingsFromPersistentStorage } from './Settings/sync';
-import { useInteractionEvent } from '../util';
-import { clearEfbState, useAppDispatch } from './Store/store';
+import { store } from './Store/store';
 
-const ScreenLoading = () => (
-    <div className="loading-screen">
-        <div className="center">
-            <div className="placeholder">
-                <img src={logo} className="fbw-logo" alt="logo" />
-                {' '}
-                flyPad
-            </div>
-            <div className="loading-bar">
-                <div className="loaded" />
-            </div>
-        </div>
-    </div>
+const EFBLoad = () => (
+    <Router>
+        <Provider store={store}>
+            <Efb />
+        </Provider>
+    </Router>
 );
-
-export enum ContentState {
-    OFF,
-    LOADING,
-    LOADED
-}
-
-interface PowerContextInterface {
-    content: ContentState,
-    setContent: (ContentState) => void
-}
-
-export const PowerContext = React.createContext<PowerContextInterface>(undefined as any);
-export const usePower = () => React.useContext(PowerContext);
-
-const EFBLoad = () => {
-    const [content, setContent] = useState<ContentState>(ContentState.LOADED);
-    const dispatch = useAppDispatch();
-
-    function offToLoaded() {
-        setContent(ContentState.LOADING);
-        setTimeout(() => {
-            setContent(ContentState.LOADED);
-        }, 100);
-    }
-
-    useInteractionEvent('A32NX_EFB_POWER', () => {
-        if (content === ContentState.OFF) {
-            offToLoaded();
-        } else {
-            setContent(ContentState.OFF);
-            dispatch(clearEfbState());
-        }
-    });
-
-    switch (content) {
-    case ContentState.OFF:
-        return <div className="w-screen h-screen" onClick={() => offToLoaded()} />;
-    case ContentState.LOADING:
-        return <ScreenLoading />;
-    case ContentState.LOADED:
-        return (
-            <Router>
-                <PowerContext.Provider value={{ content, setContent }}>
-                    <Efb />
-                </PowerContext.Provider>
-            </Router>
-        );
-    default:
-        throw new Error('Invalid content state provided');
-    }
-};
 
 readSettingsFromPersistentStorage();
 
