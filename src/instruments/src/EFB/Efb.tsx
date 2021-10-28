09850b0320600718a6fed812be9c77d82a7d9448
ErickSharp <titaninmc@gmail.com>
2021-10-27 20:46:48 -0500

    refactor: remove unused import

--- src/instruments/src/EFB/Efb.tsx
+++ src/instruments/src/EFB/Efb.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { UIMessagesProvider, useUIMessages } from './UIMessages/Provider';
 
