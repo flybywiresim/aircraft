03999eb6a14ee02f9abc2f23f1d33698131be7b8
davydecorps <38904654+crocket63@users.noreply.github.com>
2021-11-09 20:10:29 +0100

    Update hydraulic.rs

--- src/systems/a320_systems/src/hydraulic.rs
+++ src/systems/a320_systems/src/hydraulic.rs
@@ -1698,9 +1698,9 @@ impl A320DoorController {
                 self.control_position_request = if self.position_requested > Ratio::new::<ratio>(0.)
                     || self.duration_in_hyd_control < Self::UP_CONTROL_TIME_BEFORE_DOWN_CONTROL
                 {
-                    Ratio::new::<ratio>(1.2)
+                    Ratio::new::<ratio>(1.0)
                 } else {
-                    Ratio::new::<ratio>(-0.2)
+                    Ratio::new::<ratio>(-0.1)
                 }
             }
             DoorControlState::UpLocked => {
