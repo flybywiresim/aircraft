d827864f1cdfc8d67ed30f0dc1195f1ccd402900
Holger Schmid <holgerschmid@atron.de>
2021-11-30 11:26:46 +0100

    just formatting with eslint

--- src/instruments/src/EFB/Efb.tsx
+++ src/instruments/src/EFB/Efb.tsx
@@ -1,41 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Redirect, Route, Switch } from 'react-router-dom';
-import { useSimVar } from '@instruments/common/simVars';
-import { useInteractionEvent } from '@instruments/common/hooks';
-import { UIMessagesProvider, useUIMessages } from './UIMessages/Provider';
-import { usePersistentNumberProperty, usePersistentProperty } from '../Common/persistence';
-import NavigraphClient, { NavigraphContext } from './ChartsApi/Navigraph';
+import { Redirect, Route, Switch } from "react-router-dom";
+import { useSimVar } from "@instruments/common/simVars";
+import { useInteractionEvent } from "@instruments/common/hooks";
+import { UIMessagesProvider, useUIMessages } from "./UIMessages/Provider";
+import { usePersistentNumberProperty, usePersistentProperty } from "../Common/persistence";
+import NavigraphClient, { NavigraphContext } from "./ChartsApi/Navigraph";
 
-import { StatusBar } from './StatusBar/StatusBar';
-import { ToolBar } from './ToolBar/ToolBar';
+import { StatusBar } from "./StatusBar/StatusBar";
+import { ToolBar } from "./ToolBar/ToolBar";
 
-import { Dashboard } from './Dashboard/Dashboard';
-import { Dispatch } from './Dispatch/Dispatch';
-import { Ground } from './Ground/Ground';
-import { Performance } from './Performance/Performance';
-import { Navigation } from './Navigation/Navigation';
-import { ATC } from './ATC/ATC';
-import { Settings } from './Settings/Settings';
-import { Failures } from './Failures/Failures';
+import { Dashboard } from "./Dashboard/Dashboard";
+import { Dispatch } from "./Dispatch/Dispatch";
+import { Ground } from "./Ground/Ground";
+import { Performance } from "./Performance/Performance";
+import { Navigation } from "./Navigation/Navigation";
+import { ATC } from "./ATC/ATC";
+import { Settings } from "./Settings/Settings";
+import { Failures } from "./Failures/Failures";
 
-import { clearEfbState, useAppDispatch, useAppSelector } from './Store/store';
+import { clearEfbState, useAppDispatch, useAppSelector } from "./Store/store";
 
-import { fetchSimbriefDataAction, initialState as simbriefInitialState } from './Store/features/simbrief';
+import { fetchSimbriefDataAction, initialState as simbriefInitialState } from "./Store/features/simbrief";
 
-import { NotificationsContainer, NotificationTypes, Notification } from './UIMessages/Notification';
-import { FbwLogo } from './Assets/FbwLogo';
+import { NotificationsContainer, NotificationTypes, Notification } from "./UIMessages/Notification";
+import { FbwLogo } from "./Assets/FbwLogo";
 
 const navigraph = new NavigraphClient();
 
 const ApplicationNotifications = () => {
     const firstNotification = useUIMessages().notifications[0];
 
-    return (
-        <NotificationsContainer>
-            {firstNotification}
-        </NotificationsContainer>
-    );
+    return <NotificationsContainer>{firstNotification}</NotificationsContainer>;
 };
 
 const ScreenLoading = () => (
@@ -52,8 +48,8 @@ export enum PowerStates {
 }
 
 interface PowerContextInterface {
-    powerState: PowerStates,
-    setPowerState: (PowerState) => void
+    powerState: PowerStates;
+    setPowerState: (PowerState) => void;
 }
 
 export const PowerContext = React.createContext<PowerContextInterface>(undefined as any);
@@ -64,34 +60,36 @@ const Efb = () => {
 
     const [powerState, setPowerState] = useState<PowerStates>(PowerStates.SHUTOFF);
 
-    const [currentLocalTime] = useSimVar('E:LOCAL TIME', 'seconds', 3000);
-    const [, setBrightness] = useSimVar('L:A32NX_EFB_BRIGHTNESS', 'number');
-    const [brightnessSetting] = usePersistentNumberProperty('EFB_BRIGHTNESS', 0);
-    const [usingAutobrightness] = useSimVar('L:A32NX_EFB_USING_AUTOBRIGHTNESS', 'bool', 300);
-    const [dayOfYear] = useSimVar('E:ZULU DAY OF YEAR', 'number');
-    const [latitude] = useSimVar('PLANE LATITUDE', 'degree latitude');
+    const [currentLocalTime] = useSimVar("E:LOCAL TIME", "seconds", 3000);
+    const [, setBrightness] = useSimVar("L:A32NX_EFB_BRIGHTNESS", "number");
+    const [brightnessSetting] = usePersistentNumberProperty("EFB_BRIGHTNESS", 0);
+    const [usingAutobrightness] = useSimVar("L:A32NX_EFB_USING_AUTOBRIGHTNESS", "bool", 300);
+    const [dayOfYear] = useSimVar("E:ZULU DAY OF YEAR", "number");
+    const [latitude] = useSimVar("PLANE LATITUDE", "degree latitude");
 
     const dispatch = useAppDispatch();
     const simbriefData = useAppSelector((state) => state.simbrief.data);
-    const [simbriefUserId] = usePersistentProperty('CONFIG_SIMBRIEF_USERID');
-    const [autoSimbriefImport] = usePersistentProperty('CONFIG_AUTO_SIMBRIEF_IMPORT');
+    const [simbriefUserId] = usePersistentProperty("CONFIG_SIMBRIEF_USERID");
+    const [autoSimbriefImport] = usePersistentProperty("CONFIG_AUTO_SIMBRIEF_IMPORT");
 
     useEffect(() => {
         if (powerState === PowerStates.SHUTOFF) {
             dispatch(clearEfbState());
         } else if (powerState === PowerStates.LOADED) {
-            if ((!simbriefData || simbriefData === simbriefInitialState.data) && autoSimbriefImport === 'ENABLED') {
-                fetchSimbriefDataAction(simbriefUserId ?? '').then((action) => {
-                    dispatch(action);
-                }).catch(() => {
-                    uiMessages.pushNotification(
-                        <Notification
-                            type={NotificationTypes.ERROR}
-                            title="SimBrief Error"
-                            message="An error occurred when trying to fetch your SimBrief data."
-                        />,
-                    );
-                });
+            if ((!simbriefData || simbriefData === simbriefInitialState.data) && autoSimbriefImport === "ENABLED") {
+                fetchSimbriefDataAction(simbriefUserId ?? "")
+                    .then((action) => {
+                        dispatch(action);
+                    })
+                    .catch(() => {
+                        uiMessages.pushNotification(
+                            <Notification
+                                type={NotificationTypes.ERROR}
+                                title="SimBrief Error"
+                                message="An error occurred when trying to fetch your SimBrief data."
+                            />
+                        );
+                    });
             }
         }
     }, [powerState]);
@@ -103,7 +101,7 @@ const Efb = () => {
         }, 2500);
     };
 
-    useInteractionEvent('A32NX_EFB_POWER', () => {
+    useInteractionEvent("A32NX_EFB_POWER", () => {
         if (powerState === PowerStates.SHUTOFF) {
             offToLoaded();
         } else {
@@ -119,79 +117,82 @@ const Efb = () => {
      */
     const calculateBrightness = (latitude: number, dayOfYear: number, timeOfDay: number) => {
         const solarTime = timeOfDay + (dayOfYear - 1) * 24;
-        const solarDeclination = 0.409 * Math.sin(2 * Math.PI * (284 + dayOfYear) / 365);
+        const solarDeclination = 0.409 * Math.sin((2 * Math.PI * (284 + dayOfYear)) / 365);
         const solarAltitude = Math.asin(
-            Math.sin(latitude * Math.PI / 180) * Math.sin(solarDeclination) + Math.cos(latitude * Math.PI / 180) * Math.cos(solarDeclination) * Math.cos(2 * Math.PI * solarTime / 24),
+            Math.sin((latitude * Math.PI) / 180) * Math.sin(solarDeclination) +
+                Math.cos((latitude * Math.PI) / 180) *
+                    Math.cos(solarDeclination) *
+                    Math.cos((2 * Math.PI * solarTime) / 24)
         );
         const solarZenith = 90 - (latitude - solarDeclination);
 
-        return Math.min(Math.max((-solarAltitude * (180 / Math.PI)) / solarZenith * 100, 0), 100);
+        return Math.min(Math.max(((-solarAltitude * (180 / Math.PI)) / solarZenith) * 100, 0), 100);
     };
 
     // handle setting brightness if user is using autobrightness
     useEffect(() => {
         if (usingAutobrightness) {
             const localTime = currentLocalTime / 3600;
-            setBrightness((calculateBrightness(latitude, dayOfYear, localTime)));
+            setBrightness(calculateBrightness(latitude, dayOfYear, localTime));
         } else {
             setBrightness(brightnessSetting);
         }
     }, [currentLocalTime, usingAutobrightness]);
 
     switch (powerState) {
-    case PowerStates.SHUTOFF:
-        return <div className="w-screen h-screen" onClick={() => offToLoaded()} />;
-    case PowerStates.LOADING:
-        return <ScreenLoading />;
-    case PowerStates.LOADED:
-        return (
-            <NavigraphContext.Provider value={navigraph}>
-                <PowerContext.Provider value={{ powerState, setPowerState }}>
-                    <UIMessagesProvider>
-                        <div className="bg-theme-body">
-                            <ApplicationNotifications />
-                            <StatusBar />
-                            <div className="flex flex-row">
-                                <ToolBar />
-                                <div className="pt-14 pr-6 w-screen h-screen text-gray-700">
-                                    <Switch>
-                                        <Route exact path="/">
-                                            <Redirect to="/dashboard" />
-                                        </Route>
-                                        <Route path="/dashboard">
-                                            <Dashboard />
-                                        </Route>
-                                        <Route path="/dispatch">
-                                            <Dispatch />
-                                        </Route>
-                                        <Route path="/ground">
-                                            <Ground />
-                                        </Route>
-                                        <Route path="/performance">
-                                            <Performance />
-                                        </Route>
-                                        <Route path="/navigation">
-                                            <Navigation />
-                                        </Route>
-                                        <Route path="/atc">
-                                            <ATC />
-                                        </Route>
-                                        <Route path="/failures">
-                                            <Failures />
-                                        </Route>
-                                        <Route path="/settings">
-                                            <Settings />
-                                        </Route>
-                                    </Switch>
+        case PowerStates.SHUTOFF:
+            return <div className="w-screen h-screen" onClick={() => offToLoaded()} />;
+        case PowerStates.LOADING:
+            return <ScreenLoading />;
+        case PowerStates.LOADED:
+            return (
+                <NavigraphContext.Provider value={navigraph}>
+                    <PowerContext.Provider value={{ powerState, setPowerState }}>
+                        <UIMessagesProvider>
+                            <div className="bg-theme-body">
+                                <ApplicationNotifications />
+                                <StatusBar />
+                                <div className="flex flex-row">
+                                    <ToolBar />
+                                    <div className="pt-14 pr-6 w-screen h-screen text-gray-700">
+                                        <Switch>
+                                            <Route exact path="/">
+                                                <Redirect to="/dashboard" />
+                                            </Route>
+                                            <Route path="/dashboard">
+                                                <Dashboard />
+                                            </Route>
+                                            <Route path="/dispatch">
+                                                <Dispatch />
+                                            </Route>
+                                            <Route path="/ground">
+                                                <Ground />
+                                            </Route>
+                                            <Route path="/performance">
+                                                <Performance />
+                                            </Route>
+                                            <Route path="/navigation">
+                                                <Navigation />
+                                            </Route>
+                                            <Route path="/atc">
+                                                <ATC />
+                                            </Route>
+                                            <Route path="/failures">
+                                                <Failures />
+                                            </Route>
+                                            <Route path="/settings">
+                                                <Settings />
+                                            </Route>
+                                        </Switch>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </UIMessagesProvider>
-                </PowerContext.Provider>
-            </NavigraphContext.Provider>
-        );
-    default:
-        throw new Error('Invalid content state provided');
+                        </UIMessagesProvider>
+                    </PowerContext.Provider>
+                </NavigraphContext.Provider>
+            );
+        default:
+            throw new Error("Invalid content state provided");
     }
 };
 
--- src/instruments/src/EFB/StatusBar/StatusBar.tsx
+++ src/instruments/src/EFB/StatusBar/StatusBar.tsx
@@ -1,47 +1,53 @@
-import React from 'react';
-import { Wifi2, BatteryFull, Power } from 'react-bootstrap-icons';
-import { useSimVar } from '@instruments/common/simVars';
-import { usePersistentProperty } from '@instruments/common/persistence';
-import { usePower, PowerStates } from '../Efb';
+import React from "react";
+import { Wifi2, BatteryFull, Power } from "react-bootstrap-icons";
+import { useSimVar } from "@instruments/common/simVars";
+import { usePersistentProperty } from "@instruments/common/persistence";
+import { usePower, PowerStates } from "../Efb";
 
 export const StatusBar = () => {
-    const [currentUTC] = useSimVar('E:ZULU TIME', 'seconds');
-    const [currentLocalTime] = useSimVar('E:LOCAL TIME', 'seconds');
-    const [dayOfWeek] = useSimVar('E:ZULU DAY OF WEEK', 'number');
-    const [monthOfYear] = useSimVar('E:ZULU MONTH OF YEAR', 'number');
-    const [dayOfMonth] = useSimVar('E:ZULU DAY OF MONTH', 'number');
+    const [currentUTC] = useSimVar("E:ZULU TIME", "seconds");
+    const [currentLocalTime] = useSimVar("E:LOCAL TIME", "seconds");
+    const [dayOfWeek] = useSimVar("E:ZULU DAY OF WEEK", "number");
+    const [monthOfYear] = useSimVar("E:ZULU MONTH OF YEAR", "number");
+    const [dayOfMonth] = useSimVar("E:ZULU DAY OF MONTH", "number");
 
-    const [timeDisplayed] = usePersistentProperty('EFB_TIME_DISPLAYED', 'utc');
-    const [timeFormat] = usePersistentProperty('EFB_TIME_FORMAT');
+    const [timeDisplayed] = usePersistentProperty("EFB_TIME_DISPLAYED", "utc");
+    const [timeFormat] = usePersistentProperty("EFB_TIME_FORMAT");
 
     const power = usePower();
 
-    const getDayName = (day: number) => ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'][day];
+    const getDayName = (day: number) => ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat", "Sun"][day];
 
-    const getMonthName = (month: number) => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][month - 1];
+    const getMonthName = (month: number) =>
+        ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][month - 1];
 
-    const getZuluFormattedTime = (seconds: number) => `${Math.floor(seconds / 3600).toString().padStart(2, '0')}:${Math.floor((seconds % 3600) / 60).toString().padStart(2, '0')}z`;
+    const getZuluFormattedTime = (seconds: number) =>
+        `${Math.floor(seconds / 3600)
+            .toString()
+            .padStart(2, "0")}:${Math.floor((seconds % 3600) / 60)
+            .toString()
+            .padStart(2, "0")}z`;
     const getLocalFormattedTime = (seconds: number) => {
-        if (timeFormat === '24') {
-            return `${Math.floor(seconds / 3600).toString().padStart(2, '0')}:${Math.floor((seconds % 3600) / 60).toString().padStart(2, '0')}`;
+        if (timeFormat === "24") {
+            return `${Math.floor(seconds / 3600)
+                .toString()
+                .padStart(2, "0")}:${Math.floor((seconds % 3600) / 60)
+                .toString()
+                .padStart(2, "0")}`;
         }
         const hours = Math.floor(seconds / 3600) % 12;
         const minutes = Math.floor((seconds % 3600) / 60);
-        const ampm = Math.floor(seconds / 3600) >= 12 ? 'pm' : 'am';
-        return `${hours === 0 ? 12 : hours}:${minutes.toString().padStart(2, '0')}${ampm}`;
+        const ampm = Math.floor(seconds / 3600) >= 12 ? "pm" : "am";
+        return `${hours === 0 ? 12 : hours}:${minutes.toString().padStart(2, "0")}${ampm}`;
     };
 
     return (
         <div className="flex fixed justify-between items-center px-6 w-full h-10 text-lg font-medium leading-none text-theme-text bg-theme-statusbar">
             <p>{`${getDayName(dayOfWeek)} ${getMonthName(monthOfYear)} ${dayOfMonth}`}</p>
             <div className="flex absolute inset-x-0 flex-row justify-center items-center mx-auto space-x-4 w-min">
-                {(timeDisplayed === 'utc' || timeDisplayed === 'both') && (
-                    <p>{getZuluFormattedTime(currentUTC)}</p>
-                )}
-                {timeDisplayed === 'both' && (
-                    <p>/</p>
-                )}
-                {(timeDisplayed === 'local' || timeDisplayed === 'both') && (
+                {(timeDisplayed === "utc" || timeDisplayed === "both") && <p>{getZuluFormattedTime(currentUTC)}</p>}
+                {timeDisplayed === "both" && <p>/</p>}
+                {(timeDisplayed === "local" || timeDisplayed === "both") && (
                     <p>{getLocalFormattedTime(currentLocalTime)}</p>
                 )}
             </div>
