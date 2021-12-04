a3ea1f8f23c6e513dbd45c0119879ea0bc4bdf68
Holger Schmid <schmid_holger@hotmail.com>
2021-12-04 16:31:33 +0100

    make battery level dependant on local time (not working yet)

--- src/instruments/src/EFB/Efb.tsx
+++ src/instruments/src/EFB/Efb.tsx
@@ -1,41 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import { Redirect, Route, Switch } from "react-router-dom";
-import { useSimVar } from "@instruments/common/simVars";
-import { useInteractionEvent } from "@instruments/common/hooks";
-import { UIMessagesProvider, useUIMessages } from "./UIMessages/Provider";
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { useSimVar } from '@instruments/common/simVars';
+import { useInteractionEvent } from '@instruments/common/hooks';
+import { IconBatteryCharging, IconBatteryOff } from '@tabler/icons';
+import { UIMessagesProvider, useUIMessages } from './UIMessages/Provider';
 import {
     usePersistentNumberProperty,
     usePersistentProperty,
-} from "../Common/persistence";
-import NavigraphClient, { NavigraphContext } from "./ChartsApi/Navigraph";
+} from '../Common/persistence';
+import NavigraphClient, { NavigraphContext } from './ChartsApi/Navigraph';
 
-import { StatusBar } from "./StatusBar/StatusBar";
-import { ToolBar } from "./ToolBar/ToolBar";
+import { StatusBar } from './StatusBar/StatusBar';
+import { ToolBar } from './ToolBar/ToolBar';
 
-import { Dashboard } from "./Dashboard/Dashboard";
-import { Dispatch } from "./Dispatch/Dispatch";
-import { Ground } from "./Ground/Ground";
-import { Performance } from "./Performance/Performance";
-import { Navigation } from "./Navigation/Navigation";
-import { ATC } from "./ATC/ATC";
-import { Settings } from "./Settings/Settings";
-import { Failures } from "./Failures/Failures";
+import { Dashboard } from './Dashboard/Dashboard';
+import { Dispatch } from './Dispatch/Dispatch';
+import { Ground } from './Ground/Ground';
+import { Performance } from './Performance/Performance';
+import { Navigation } from './Navigation/Navigation';
+import { ATC } from './ATC/ATC';
+import { Settings } from './Settings/Settings';
+import { Failures } from './Failures/Failures';
 
-import { clearEfbState, useAppDispatch, useAppSelector } from "./Store/store";
+import { clearEfbState, useAppDispatch, useAppSelector } from './Store/store';
 
 import {
     fetchSimbriefDataAction,
     initialState as simbriefInitialState,
-} from "./Store/features/simbrief";
+} from './Store/features/simbrief';
 
 import {
     NotificationsContainer,
     NotificationTypes,
     Notification,
-} from "./UIMessages/Notification";
-import { FbwLogo } from "./Assets/FbwLogo";
-import { IconBatteryCharging, IconBatteryOff } from "@tabler/icons";
+} from './UIMessages/Notification';
+import { FbwLogo } from './Assets/FbwLogo';
 
 const BATTERY_DURATION_MIN = 1;
 
@@ -55,10 +55,10 @@ const ScreenLoading = () => (
 
 const ScreenEmpty = (props: { charging: boolean }) => (
     <div className="flex justify-center items-center w-screen h-screen bg-theme-statusbar">
-        {false === props.charging && (
+        {props.charging === false && (
             <IconBatteryOff size={128} className="text-red-500" />
         )}
-        {true === props.charging && (
+        {props.charging === true && (
             <IconBatteryCharging size={128} className="text-red-500" />
         )}
     </div>
@@ -78,8 +78,14 @@ interface PowerContextInterface {
 }
 
 export const PowerContext = React.createContext<PowerContextInterface>(
-    undefined as any
+    undefined as any,
 );
+
+type BatteryLevel = {
+    level: number;
+    lastChangeTimestamp: number;
+    lastChargeSwitchLevel: number;
+};
 
 export const usePower = () => React.useContext(PowerContext);
 
@@ -87,39 +93,50 @@ const Efb = () => {
     const uiMessages = useUIMessages();
 
     const [powerState, setPowerState] = useState<PowerStates>(
-        PowerStates.SHUTOFF
+        PowerStates.SHUTOFF,
     );
 
-    const [currentLocalTime] = useSimVar("E:LOCAL TIME", "seconds", 3000);
-    const [, setBrightness] = useSimVar("L:A32NX_EFB_BRIGHTNESS", "number");
+    const [currentLocalTime] = useSimVar('E:LOCAL TIME', 'seconds', 3000);
+    const [zuluTime] = useSimVar('E:ZULU TIME', 'seconds', 3000);
+    const [, setBrightness] = useSimVar('L:A32NX_EFB_BRIGHTNESS', 'number');
     const [brightnessSetting] = usePersistentNumberProperty(
-        "EFB_BRIGHTNESS",
-        0
+        'EFB_BRIGHTNESS',
+        0,
     );
     const [usingAutobrightness] = useSimVar(
-        "L:A32NX_EFB_USING_AUTOBRIGHTNESS",
-        "bool",
-        300
+        'L:A32NX_EFB_USING_AUTOBRIGHTNESS',
+        'bool',
+        300,
     );
-    const [dayOfYear] = useSimVar("E:ZULU DAY OF YEAR", "number");
-    const [latitude] = useSimVar("PLANE LATITUDE", "degree latitude");
+    const [dayOfYear] = useSimVar('E:ZULU DAY OF YEAR', 'number');
+    const [latitude] = useSimVar('PLANE LATITUDE', 'degree latitude');
 
     const dispatch = useAppDispatch();
     const simbriefData = useAppSelector((state) => state.simbrief.data);
-    const [simbriefUserId] = usePersistentProperty("CONFIG_SIMBRIEF_USERID");
-    const [autoSimbriefImport] = usePersistentProperty(
-        "CONFIG_AUTO_SIMBRIEF_IMPORT"
-    );
+    const [simbriefUserId] = usePersistentProperty('CONFIG_SIMBRIEF_USERID');
+    const [autoSimbriefImport] = usePersistentProperty('CONFIG_AUTO_SIMBRIEF_IMPORT');
 
-    const [batteryLevel, setBatteryLevel] = useState(100);
+    const [batteryLevel, setBatteryLevel] = useState<BatteryLevel>({ level: 100, lastChangeTimestamp: zuluTime, lastChargeSwitchLevel: zuluTime });
     const [timer, setTimer] = useState<NodeJS.Timer | undefined>(undefined);
-    const [dc2BusIsPowered] = useSimVar(
-        "L:A32NX_ELEC_DC_2_BUS_IS_POWERED",
-        "bool"
-    );
+    const [dc2BusIsPowered] = useSimVar('L:A32NX_ELEC_DC_2_BUS_IS_POWERED', 'bool');
 
     useEffect(() => {
-        if (timer != undefined) {
+        setBatteryLevel((oldLevel:BatteryLevel) => {
+            const deltaTs = zuluTime - oldLevel.lastChangeTimestamp;
+            const deltaLevel = deltaTs * (BATTERY_DURATION_MIN * 60 * 1000) / 100;
+            if (dc2BusIsPowered) {
+                oldLevel.level += deltaLevel;
+            } else {
+                oldLevel.level -= deltaLevel;
+            }
+            oldLevel.lastChangeTimestamp = zuluTime;
+            return oldLevel;
+        });
+    }, [zuluTime, dc2BusIsPowered]);
+
+    /*
+    useEffect(() => {
+        if (timer !== undefined) {
             clearInterval(timer);
         }
         const newTimer = setInterval(() => {
@@ -132,13 +149,13 @@ const Efb = () => {
         }, (BATTERY_DURATION_MIN * 60 * 1000) / 100);
         setTimer(newTimer);
     }, [dc2BusIsPowered]);
-
+*/
     useEffect(() => {
-        if (batteryLevel === 0) {
+        if (batteryLevel.level === 0) {
             setPowerState(PowerStates.EMPTY);
         }
 
-        if (batteryLevel > 1 && powerState === PowerStates.EMPTY) {
+        if (batteryLevel.level > 1 && powerState === PowerStates.EMPTY) {
             offToLoaded();
         }
     }, [batteryLevel, powerState]);
@@ -148,10 +165,10 @@ const Efb = () => {
             dispatch(clearEfbState());
         } else if (powerState === PowerStates.LOADED) {
             if (
-                (!simbriefData || simbriefData === simbriefInitialState.data) &&
-                autoSimbriefImport === "ENABLED"
+                (!simbriefData || simbriefData === simbriefInitialState.data)
+                && autoSimbriefImport === 'ENABLED'
             ) {
-                fetchSimbriefDataAction(simbriefUserId ?? "")
+                fetchSimbriefDataAction(simbriefUserId ?? '')
                     .then((action) => {
                         dispatch(action);
                     })
@@ -161,7 +178,7 @@ const Efb = () => {
                                 type={NotificationTypes.ERROR}
                                 title="SimBrief Error"
                                 message="An error occurred when trying to fetch your SimBrief data."
-                            />
+                            />,
                         );
                     });
             }
@@ -175,7 +192,7 @@ const Efb = () => {
         }, 2500);
     };
 
-    useInteractionEvent("A32NX_EFB_POWER", () => {
+    useInteractionEvent('A32NX_EFB_POWER', () => {
         if (powerState === PowerStates.SHUTOFF) {
             offToLoaded();
         } else {
@@ -192,25 +209,24 @@ const Efb = () => {
     const calculateBrightness = (
         latitude: number,
         dayOfYear: number,
-        timeOfDay: number
+        timeOfDay: number,
     ) => {
         const solarTime = timeOfDay + (dayOfYear - 1) * 24;
-        const solarDeclination =
-            0.409 * Math.sin((2 * Math.PI * (284 + dayOfYear)) / 365);
+        const solarDeclination = 0.409 * Math.sin((2 * Math.PI * (284 + dayOfYear)) / 365);
         const solarAltitude = Math.asin(
-            Math.sin((latitude * Math.PI) / 180) * Math.sin(solarDeclination) +
-                Math.cos((latitude * Math.PI) / 180) *
-                    Math.cos(solarDeclination) *
-                    Math.cos((2 * Math.PI * solarTime) / 24)
+            Math.sin((latitude * Math.PI) / 180) * Math.sin(solarDeclination)
+                + Math.cos((latitude * Math.PI) / 180)
+                    * Math.cos(solarDeclination)
+                    * Math.cos((2 * Math.PI * solarTime) / 24),
         );
         const solarZenith = 90 - (latitude - solarDeclination);
 
         return Math.min(
             Math.max(
                 ((-solarAltitude * (180 / Math.PI)) / solarZenith) * 100,
-                0
+                0,
             ),
-            100
+            100,
         );
     };
 
@@ -225,71 +241,71 @@ const Efb = () => {
     }, [currentLocalTime, usingAutobrightness]);
 
     switch (powerState) {
-        case PowerStates.SHUTOFF:
-            return (
-                <div
-                    className="w-screen h-screen"
-                    onClick={() => offToLoaded()}
-                />
-            );
-        case PowerStates.EMPTY:
-            return <ScreenEmpty charging={dc2BusIsPowered === 1} />;
-        case PowerStates.LOADING:
-            return <ScreenLoading />;
-        case PowerStates.LOADED:
-            return (
-                <NavigraphContext.Provider value={navigraph}>
-                    <PowerContext.Provider
-                        value={{ powerState, setPowerState }}
-                    >
-                        <UIMessagesProvider>
-                            <div className="bg-theme-body">
-                                <ApplicationNotifications />
-                                <StatusBar
-                                    batteryLevel={batteryLevel}
-                                    isCharging={dc2BusIsPowered === 1}
-                                />
-                                <div className="flex flex-row">
-                                    <ToolBar />
-                                    <div className="pt-14 pr-6 w-screen h-screen text-gray-700">
-                                        <Switch>
-                                            <Route exact path="/">
-                                                <Redirect to="/dashboard" />
-                                            </Route>
-                                            <Route path="/dashboard">
-                                                <Dashboard />
-                                            </Route>
-                                            <Route path="/dispatch">
-                                                <Dispatch />
-                                            </Route>
-                                            <Route path="/ground">
-                                                <Ground />
-                                            </Route>
-                                            <Route path="/performance">
-                                                <Performance />
-                                            </Route>
-                                            <Route path="/navigation">
-                                                <Navigation />
-                                            </Route>
-                                            <Route path="/atc">
-                                                <ATC />
-                                            </Route>
-                                            <Route path="/failures">
-                                                <Failures />
-                                            </Route>
-                                            <Route path="/settings">
-                                                <Settings />
-                                            </Route>
-                                        </Switch>
-                                    </div>
+    case PowerStates.SHUTOFF:
+        return (
+            <div
+                className="w-screen h-screen"
+                onClick={() => offToLoaded()}
+            />
+        );
+    case PowerStates.EMPTY:
+        return <ScreenEmpty charging={dc2BusIsPowered === 1} />;
+    case PowerStates.LOADING:
+        return <ScreenLoading />;
+    case PowerStates.LOADED:
+        return (
+            <NavigraphContext.Provider value={navigraph}>
+                <PowerContext.Provider
+                    value={{ powerState, setPowerState }}
+                >
+                    <UIMessagesProvider>
+                        <div className="bg-theme-body">
+                            <ApplicationNotifications />
+                            <StatusBar
+                                batteryLevel={batteryLevel.level}
+                                isCharging={dc2BusIsPowered === 1}
+                            />
+                            <div className="flex flex-row">
+                                <ToolBar />
+                                <div className="pt-14 pr-6 w-screen h-screen text-gray-700">
+                                    <Switch>
+                                        <Route exact path="/">
+                                            <Redirect to="/dashboard" />
+                                        </Route>
+                                        <Route path="/dashboard">
+                                            <Dashboard />
+                                        </Route>
+                                        <Route path="/dispatch">
+                                            <Dispatch />
+                                        </Route>
+                                        <Route path="/ground">
+                                            <Ground />
+                                        </Route>
+                                        <Route path="/performance">
+                                            <Performance />
+                                        </Route>
+                                        <Route path="/navigation">
+                                            <Navigation />
+                                        </Route>
+                                        <Route path="/atc">
+                                            <ATC />
+                                        </Route>
+                                        <Route path="/failures">
+                                            <Failures />
+                                        </Route>
+                                        <Route path="/settings">
+                                            <Settings />
+                                        </Route>
+                                    </Switch>
                                 </div>
                             </div>
-                        </UIMessagesProvider>
-                    </PowerContext.Provider>
-                </NavigraphContext.Provider>
-            );
-        default:
-            throw new Error("Invalid content state provided");
+                        </div>
+                    </UIMessagesProvider>
+                </PowerContext.Provider>
+            </NavigraphContext.Provider>
+        );
+    default:
+        throw new Error('Invalid content state provided');
     }
 };
 
