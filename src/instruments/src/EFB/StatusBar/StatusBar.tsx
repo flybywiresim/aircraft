8efebc0d0d8cd9f7f1fdbcacf61e7b401117df07
Holger Schmid <schmid_holger@hotmail.com>
2021-12-14 14:50:17 +0100

    no code change, just formatting (eslint)

--- src/instruments/src/EFB/StatusBar/StatusBar.tsx
+++ src/instruments/src/EFB/StatusBar/StatusBar.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { Wifi2, Power } from "react-bootstrap-icons";
-import { useSimVar } from "@instruments/common/simVars";
-import { usePersistentProperty } from "@instruments/common/persistence";
-import { usePower, PowerStates } from "../Efb";
+import React, { useEffect, useState } from 'react';
+import { Wifi2, Power } from 'react-bootstrap-icons';
+import { useSimVar } from '@instruments/common/simVars';
+import { usePersistentProperty } from '@instruments/common/persistence';
 import {
     IconBattery,
     IconBattery1,
@@ -10,7 +9,8 @@ import {
     IconBattery3,
     IconBattery4,
     IconBatteryCharging,
-} from "@tabler/icons";
+} from '@tabler/icons';
+import { usePower, PowerStates } from '../Efb';
 
 type StatusBarProps = {
     batteryLevel: number;
@@ -20,69 +20,69 @@ type StatusBarProps = {
 export const StatusBar = (props: StatusBarProps) => {
     const { batteryLevel, isCharging } = props;
 
-    const [currentUTC] = useSimVar("E:ZULU TIME", "seconds");
-    const [currentLocalTime] = useSimVar("E:LOCAL TIME", "seconds");
-    const [dayOfWeek] = useSimVar("E:ZULU DAY OF WEEK", "number");
-    const [monthOfYear] = useSimVar("E:ZULU MONTH OF YEAR", "number");
-    const [dayOfMonth] = useSimVar("E:ZULU DAY OF MONTH", "number");
+    const [currentUTC] = useSimVar('E:ZULU TIME', 'seconds');
+    const [currentLocalTime] = useSimVar('E:LOCAL TIME', 'seconds');
+    const [dayOfWeek] = useSimVar('E:ZULU DAY OF WEEK', 'number');
+    const [monthOfYear] = useSimVar('E:ZULU MONTH OF YEAR', 'number');
+    const [dayOfMonth] = useSimVar('E:ZULU DAY OF MONTH', 'number');
 
-    const [timeDisplayed] = usePersistentProperty("EFB_TIME_DISPLAYED", "utc");
-    const [timeFormat] = usePersistentProperty("EFB_TIME_FORMAT");
+    const [timeDisplayed] = usePersistentProperty('EFB_TIME_DISPLAYED', 'utc');
+    const [timeFormat] = usePersistentProperty('EFB_TIME_FORMAT');
 
     const power = usePower();
 
-    const getDayName = (day: number) =>
-        ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat", "Sun"][day];
+    const getDayName = (day: number) => ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'][day];
 
-    const getMonthName = (month: number) =>
-        [
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "May",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct",
-            "Nov",
-            "Dec",
-        ][month - 1];
+    const getMonthName = (month: number) => [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+    ][month - 1];
 
-    const getZuluFormattedTime = (seconds: number) =>
-        `${Math.floor(seconds / 3600)
-            .toString()
-            .padStart(2, "0")}:${Math.floor((seconds % 3600) / 60)
-            .toString()
-            .padStart(2, "0")}z`;
+    const getZuluFormattedTime = (seconds: number) => `${Math.floor(seconds / 3600)
+        .toString()
+        .padStart(2, '0')}:${Math.floor((seconds % 3600) / 60)
+        .toString()
+        .padStart(2, '0')}z`;
     const getLocalFormattedTime = (seconds: number) => {
-        if (timeFormat === "24") {
+        if (timeFormat === '24') {
             return `${Math.floor(seconds / 3600)
                 .toString()
-                .padStart(2, "0")}:${Math.floor((seconds % 3600) / 60)
+                .padStart(2, '0')}:${Math.floor((seconds % 3600) / 60)
                 .toString()
-                .padStart(2, "0")}`;
+                .padStart(2, '0')}`;
         }
         const hours = Math.floor(seconds / 3600) % 12;
         const minutes = Math.floor((seconds % 3600) / 60);
-        const ampm = Math.floor(seconds / 3600) >= 12 ? "pm" : "am";
+        const ampm = Math.floor(seconds / 3600) >= 12 ? 'pm' : 'am';
         return `${hours === 0 ? 12 : hours}:${minutes
             .toString()
-            .padStart(2, "0")}${ampm}`;
+            .padStart(2, '0')}${ampm}`;
     };
 
     return (
         <div className="flex fixed justify-between items-center px-6 w-full h-10 text-lg font-medium leading-none text-theme-text bg-theme-statusbar">
-            <p>{`${getDayName(dayOfWeek)} ${getMonthName(
-                monthOfYear
-            )} ${dayOfMonth}`}</p>
+            <p>
+                {`${getDayName(dayOfWeek)} ${getMonthName(
+                    monthOfYear,
+                )} ${dayOfMonth}`}
+
+            </p>
             <div className="flex absolute inset-x-0 flex-row justify-center items-center mx-auto space-x-4 w-min">
-                {(timeDisplayed === "utc" || timeDisplayed === "both") && (
+                {(timeDisplayed === 'utc' || timeDisplayed === 'both') && (
                     <p>{getZuluFormattedTime(currentUTC)}</p>
                 )}
-                {timeDisplayed === "both" && <p>/</p>}
-                {(timeDisplayed === "local" || timeDisplayed === "both") && (
+                {timeDisplayed === 'both' && <p>/</p>}
+                {(timeDisplayed === 'local' || timeDisplayed === 'both') && (
                     <p>{getLocalFormattedTime(currentLocalTime)}</p>
                 )}
             </div>
@@ -116,7 +116,7 @@ const BatteryStatus = (props: BatteryStatusProps) => {
 
     const { batteryLevel, isCharging } = props;
     const batteryStatusIcon = () => {
-        if (true === isCharging) {
+        if (isCharging === true) {
             return <IconBatteryCharging size={SIZE} color="green" />;
         }
 
@@ -124,7 +124,7 @@ const BatteryStatus = (props: BatteryStatusProps) => {
             return (
                 <IconBattery
                     size={SIZE}
-                    color={batteryLevel < 8 ? "red" : "white"}
+                    color={batteryLevel < 8 ? 'red' : 'white'}
                 />
             );
         }
@@ -149,11 +149,12 @@ const BatteryStatus = (props: BatteryStatusProps) => {
             <div className="flex items-center space-x-4">
                 <text
                     className={
-                        "w-12 text-right " +
-                        (batteryLevel < 8 ? "text-red-500" : "text-white")
+                        `w-12 text-right ${
+                            batteryLevel < 8 ? 'text-red-500' : 'text-white'}`
                     }
                 >
-                    {batteryLevel}%
+                    {batteryLevel}
+                    %
                 </text>
                 {batteryStatusIcon()}
             </div>
