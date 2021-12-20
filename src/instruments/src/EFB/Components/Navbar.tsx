cc5e42628f852ca3a3220ed82105c7d02725489b
Holger Schmid <schmid_holger@hotmail.com>
2021-12-20 10:48:44 +0100

    just formatting - no code change

--- src/instruments/src/EFB/Components/Navbar.tsx
+++ src/instruments/src/EFB/Components/Navbar.tsx
@@ -9,22 +9,13 @@ export type NavbarProps = {
 
 const c = {
     active: 'flex items-center px-6 py-2 bg-theme-accent bg-opacity-100',
-    inactive:
-        'flex items-center px-6 py-2 bg-opacity-0 hover:bg-opacity-100 transition duration-300',
+    inactive: 'flex items-center px-6 py-2 bg-opacity-0 hover:bg-opacity-100 transition duration-300',
 };
 
-export const Navbar = ({
-    tabs,
-    onSelected,
-    className,
-    selectedTabIndex,
-}: NavbarProps) => {
-    const [activeIndex, setActiveIndex] = useState(
-        selectedTabIndex === undefined ? 0 : selectedTabIndex,
-    );
+export const Navbar = ({ tabs, onSelected, className, selectedTabIndex }: NavbarProps) => {
+    const [activeIndex, setActiveIndex] = useState(selectedTabIndex === undefined ? 0 : selectedTabIndex);
 
     useEffect(() => {
-        console.log(`activeIndex=${activeIndex} - ${selectedTabIndex}`);
         setActiveIndex(selectedTabIndex || 0);
     }, [activeIndex, selectedTabIndex]);
 
@@ -38,9 +29,7 @@ export const Navbar = ({
             <div className="flex overflow-hidden rounded-md border divide-x divide-theme-accent border-theme-accent">
                 {tabs.map((tab, index) => (
                     <a
-                        className={
-                            index === activeIndex ? c.active : c.inactive
-                        }
+                        className={index === activeIndex ? c.active : c.inactive}
                         key={tab}
                         onClick={() => handleClick(index)}
                     >
