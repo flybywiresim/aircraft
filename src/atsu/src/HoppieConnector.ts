becff87fb3980ed76d159d29bacfecc8f9e00583
Sven Czarnian <devel@svcz.de>
2022-01-03 23:30:31 +0100

    prepare the Hoppie connector

---
+++ src/atsu/src/HoppieConnector.ts
@@ -0,0 +1,64 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 FlyByWire Simulations
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { NXDataStore } from '@shared/persistence';
+
+/**
+ * Defines the connector to the hoppies network
+ */
+export class HoppieConnector {
+    private static corsProxyUrl = 'http://127.0.0.1:65512/';
+
+    private static hoppieUrl = 'http://www.hoppie.nl/acars/system/connect.html?';
+
+    private callsign = '';
+
+    private static createBaseUrl(type: string, from: string, to: string) {
+        // validate the configuration
+        const system = NXDataStore.get('CONFIG_HOPPIE_SYSTEM', 'NONE');
+        const logon = NXDataStore.get('CONFIG_HOPPIE_USERID', '');
+        if (system === 'NONE' || logon === '') {
+            return '';
+        }
+
+        return `${HoppieConnector.corsProxyUrl + HoppieConnector.hoppieUrl}logon=${logon}&type${type}&from=${from}&to=${to}`;
+    }
+
+    public getAllAtcStations() {
+        const url = HoppieConnector.createBaseUrl('ping', this.callsign, 'ALL-CALLSIGNS');
+        if (url.length === 0) {
+            return [];
+        }
+
+        return [];
+    }
+
+    public setCallsign(callsign: string) {
+        this.callsign = callsign;
+    }
+
+    public getCallsign() {
+        return this.callsign;
+    }
+}
