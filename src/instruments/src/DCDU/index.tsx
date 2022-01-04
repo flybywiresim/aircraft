e0b5b0d9b2d0e88f8f0b378e8a935ad1d00f3357
Sven Czarnian <devel@svcz.de>
2022-01-04 13:02:15 +0100

    refactor the DCDU to allow a flexible message and view handling

---
+++ src/instruments/src/DCDU/index.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { useSimVar } from '@instruments/common/simVars';
+import { render } from '../Common';
+import { SelfTest } from './pages/SelfTest';
+import { Standby } from './pages/Standby';
+import { WaitingForData } from './pages/WaitingForData';
+import { getSimVar, useUpdate } from '../util.js';
+
+import './style.scss';
+
+enum DcduState {
+    Default,
+    Off,
+    On,
+    Waiting,
+    Standby
+}
+
+function powerAvailable() {
+    // Each DCDU is powered by a different DC BUS. Sadly the cockpit only contains a single DCDU emissive.
+    // Once we have two DCDUs running, the capt. DCDU should be powered by DC 1, and F/O by DC 2.
+    return getSimVar('L:A32NX_ELEC_DC_1_BUS_IS_POWERED', 'Bool') || getSimVar('L:A32NX_ELEC_DC_2_BUS_IS_POWERED', 'Bool');
+}
+
+const DCDU: React.FC = () => {
+    const [isColdAndDark] = useSimVar('L:A32NX_COLD_AND_DARK_SPAWN', 'Bool', 200);
+    const [state, setState] = useState(isColdAndDark ? DcduState.Off : DcduState.Standby);
+
+    useUpdate((_deltaTime) => {
+        if (state === DcduState.Off) {
+            if (powerAvailable()) {
+                setState(DcduState.On);
+            }
+        } else if (!powerAvailable()) {
+            setState(DcduState.Off);
+        }
+    });
+
+    switch (state) {
+    case DcduState.Off:
+        return <></>;
+    case DcduState.On:
+        setTimeout(() => {
+            if (powerAvailable()) {
+                setState(DcduState.Waiting);
+            }
+        }, 8000);
+        return (
+            <>
+                <div className="BacklightBleed" />
+                <SelfTest />
+            </>
+        );
+    case DcduState.Waiting:
+        setTimeout(() => {
+            if (powerAvailable()) {
+                setState(DcduState.Standby);
+            }
+        }, 12000);
+        return (
+            <>
+                <div className="BacklightBleed" />
+                <WaitingForData />
+            </>
+        );
+    case DcduState.Standby:
+        return (
+            <>
+                <div className="BacklightBleed" />
+                <Standby />
+            </>
+        );
+    default:
+        throw new RangeError();
+    }
+};
+
+render(<DCDU />);
