24f74f24a4acab9d900084e7a9dc87ec0d8b6303
Sven Czarnian <devel@svcz.de>
2022-01-04 13:00:26 +0100

    introduce the base view for all messages

---
+++ src/instruments/src/DCDU/elements/BaseView.tsx
@@ -0,0 +1,11 @@
+export const BaseView: React.FC = () => (
+    <>
+        <svg className="dcdu-lines">
+            <g>
+                <path d="m 21 236 h 450" />
+                <path d="m 130 246 v 124" />
+                <path d="m 362 246 v 124" />
+            </g>
+        </svg>
+    </>
+);
