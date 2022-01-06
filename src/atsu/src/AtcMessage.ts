323b2f7522383ff20366fb50cfd13fc33dbab9e2
Sven Czarnian <devel@svcz.de>
2022-01-06 13:33:17 +0100

    introduce a UID for later communication

--- src/atsu/src/AtcMessage.ts
+++ src/atsu/src/AtcMessage.ts
@@ -44,6 +44,8 @@ export enum AtcMessageStatus {
  * Defines the generic ATC message
  */
 export class AtcMessage {
+    public UniqueMessageID = undefined;
+
     public Timestamp = undefined;
 
     public Type = undefined;
@@ -52,12 +54,23 @@ export class AtcMessage {
 
     public Status = undefined;
 
+    constructor() {
+        let datetime = new Date().getTime();
+
+        this.UniqueMessageID = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+            const r = (datetime + Math.random() * 16) % 16 | 0;
+            datetime = Math.floor(datetime / 16);
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    }
+
     public serialize() {
         throw new Error('No valid implementation');
     }
 
     // used to deserialize event data
     public deserialize(jsonData) {
+        this.UniqueMessageID = jsonData.UniqueMessageID;
         this.Timestamp = jsonData.Timestamp;
         this.Type = jsonData.Type;
         this.Direction = jsonData.Direction;
