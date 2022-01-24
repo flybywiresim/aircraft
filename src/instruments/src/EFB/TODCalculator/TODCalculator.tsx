f56aa670b73a17ad2f471169744b1530480e2fc9
ErickSharp <titaninmc@gmail.com>
2022-01-23 23:51:14 -0600

    feat: allow landing calculator unit override along with utilcomponent folder rename

--- src/instruments/src/EFB/TODCalculator/TODCalculator.tsx
+++ src/instruments/src/EFB/TODCalculator/TODCalculator.tsx
@@ -3,20 +3,18 @@ import GroundSpeed from './GroundSpeed/GroundSpeed';
 import Data from './Data/Data';
 import Result from './Result/Result';
 
-const TODCalculator = () => (
-    <div className="flex w-full mt-6">
-        <div className="w-4/12 mr-4">
-            <GroundSpeed className="h-full flex flex-col" />
+export const TODCalculator = () => (
+    <div className="flex mt-6 w-full">
+        <div className="mr-4 w-4/12">
+            <GroundSpeed className="flex flex-col h-full" />
         </div>
 
-        <div className="w-4/12 mr-4">
-            <Data className="h-full flex flex-col" />
+        <div className="mr-4 w-4/12">
+            <Data className="flex flex-col h-full" />
         </div>
 
         <div className="w-4/12">
-            <Result className="h-full flex flex-col" />
+            <Result className="flex flex-col h-full" />
         </div>
     </div>
 );
-
-export default TODCalculator;
