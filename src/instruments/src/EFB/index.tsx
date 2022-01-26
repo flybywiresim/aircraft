b97b746666154a18780690f3f6af708b5acf2190
ErickSharp <titaninmc@gmail.com>
2022-01-26 00:32:26 -0600

    refactor: better adherence to theming

--- src/instruments/src/EFB/index.tsx
+++ src/instruments/src/EFB/index.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { usePersistentProperty } from '@instruments/common/persistence';
 import { FailuresOrchestratorProvider } from './failures-orchestrator-provider';
 import Efb from './Efb';
 
@@ -11,33 +10,13 @@ import { render } from '@instruments/common/index';
 import { readSettingsFromPersistentStorage } from './Settings/sync';
 import { store } from './Store/store';
 
-const EFBLoad = () => {
-    const [theme] = usePersistentProperty('EFB_THEME');
-
-    useEffect(() => {
-        let themeToApply;
-        switch (theme) {
-        case 'dark':
-            themeToApply = ' theme-dark';
-            break;
-        case 'light':
-            themeToApply = ' theme-light';
-            break;
-        default:
-            themeToApply = ' theme-blue';
-        }
-        document.documentElement.className = document.documentElement.className.replace(/\btheme-\S+/g, ' ');
-        document.documentElement.className += themeToApply;
-    }, [theme]);
-
-    return (
-        <Router>
-            <Provider store={store}>
-                <Efb />
-            </Provider>
-        </Router>
-    );
-};
+const EFBLoad = () => (
+    <Router>
+        <Provider store={store}>
+            <Efb />
+        </Provider>
+    </Router>
+);
 
 readSettingsFromPersistentStorage();
 
