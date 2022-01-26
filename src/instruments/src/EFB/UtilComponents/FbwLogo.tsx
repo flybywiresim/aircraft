02957dfd6d90c0b6e897a27842cf90a1c6b59e44
ErickSharp <titaninmc@gmail.com>
2022-01-26 00:32:26 -0600

    refactor: better adherence to theming

--- src/instruments/src/EFB/UtilComponents/FbwLogo.tsx
+++ src/instruments/src/EFB/UtilComponents/FbwLogo.tsx
@@ -7,7 +7,13 @@ interface LogoProps {
 }
 
 export const FbwLogo = ({ width, height, className }: LogoProps) => (
-    <svg width={width} height={height} className={className} viewBox="0 0 32 30" xmlns="http://www.w3.org/2000/svg">
-        <path d="M32 0H23.1872C22.4553 0 21.7819 0.399775 21.4314 1.0423L10 22C6.5 28 2.86913 30 0 30H26L32 0Z" fill="currentColor" />
+    <svg width={width} height={height} viewBox="0 0 214 200" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M213.333 0H162.497C152.738 0 143.759 5.33034 139.086 13.8973L66.6667 146.667C44.8001 186.667 19.1275 200 0 200H173.334L213.333 0Z" fill="url(#paint0_linear_1679_669)" />
+        <defs>
+            <linearGradient id="paint0_linear_1679_669" x1="170" y1="93.3333" x2="107" y2="75.4898" gradientUnits="userSpaceOnUse">
+                <stop stopColor="#00E0FE" />
+                <stop offset="1" stopColor="#02CCFF" />
+            </linearGradient>
+        </defs>
     </svg>
 );
