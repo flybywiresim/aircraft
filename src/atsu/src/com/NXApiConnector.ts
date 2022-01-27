6beb1f3bbfaaa8fe8f8a5797ac3171b5e5b3e2f1
Sven Czarnian <devel@svcz.de>
2022-01-27 20:19:11 +0100

    use the api-client library

--- src/atsu/src/com/NXApiConnector.ts
+++ src/atsu/src/com/NXApiConnector.ts
@@ -1,7 +1,7 @@
 //  Copyright (c) 2022 FlyByWire Simulations
 //  SPDX-License-Identifier: GPL-3.0
 
-import { NXApi } from '@shared/nxapi';
+import { Atis, Metar, Taf, Telex, AircraftStatus } from '@flybywiresim/api-client';
 import { NXDataStore } from '@shared/persistence';
 import { AtsuStatusCodes } from '../AtsuStatusCodes';
 import { AtsuMessage, AtsuMessageComStatus, AtsuMessageNetwork, AtsuMessageDirection } from '../messages/AtsuMessage';
@@ -23,20 +23,23 @@ const WeatherMap = {
  */
 export class NXApiConnector {
     public static async sendTelexMessage(message: FreetextMessage): Promise<AtsuStatusCodes> {
-        const content = message.Message.replace('\n', ';');
-        return NXApi.sendTelexMessage(message.Station, content).then(() => {
-            message.ComStatus = AtsuMessageComStatus.Sent;
-            return AtsuStatusCodes.Ok;
-        }).catch(() => {
-            message.ComStatus = AtsuMessageComStatus.Failed;
-            return AtsuStatusCodes.ComFailed;
-        });
+        if (NXApiConnector.connected) {
+            const content = message.Message.replace('\n', ';');
+            return Telex.sendMessage(message.Station, content).then(() => {
+                message.ComStatus = AtsuMessageComStatus.Sent;
+                return AtsuStatusCodes.Ok;
+            }).catch(() => {
+                message.ComStatus = AtsuMessageComStatus.Failed;
+                return AtsuStatusCodes.ComFailed;
+            });
+        }
+        return AtsuStatusCodes.NoTelexConnection;
     }
 
     public static async receiveMetar(icao: string, message: WeatherMessage): Promise<AtsuStatusCodes> {
         const storedMetarSrc = NXDataStore.get('CONFIG_METAR_SRC', 'MSFS');
 
-        return NXApi.getMetar(icao, WeatherMap[storedMetarSrc])
+        return Metar.get(icao, WeatherMap[storedMetarSrc])
             .then((data) => {
                 const newLines = data.metar;
                 message.Reports.push({ airport: icao, report: newLines });
@@ -47,7 +50,7 @@ export class NXApiConnector {
     public static async receiveTaf(icao: string, message: WeatherMessage): Promise<AtsuStatusCodes> {
         const storedTafSrc = NXDataStore.get('CONFIG_TAF_SRC', 'NOAA');
 
-        return NXApi.getTaf(icao, WeatherMap[storedTafSrc])
+        return Taf.get(icao, WeatherMap[storedTafSrc])
             .then((data) => {
                 const newLines = data.taf;
                 message.Reports.push({ airport: icao, report: newLines });
@@ -58,7 +61,7 @@ export class NXApiConnector {
     public static async receiveAtis(icao: string, message: AtisMessage): Promise<AtsuStatusCodes> {
         const storedAtisSrc = NXDataStore.get('CONFIG_ATIS_SRC', 'FAA');
 
-        await NXApi.getAtis(icao, WeatherMap[storedAtisSrc])
+        await Atis.get(icao, WeatherMap[storedAtisSrc])
             .then((data) => {
                 const newLines = data.combined;
                 message.Reports.push({ airport: icao, report: newLines });
@@ -72,29 +75,29 @@ export class NXApiConnector {
     public static async poll(): Promise<[AtsuStatusCodes, AtsuMessage[]]> {
         const retval: AtsuMessage[] = [];
 
-        // Update connection
-        NXApi.updateTelex()
-            .catch(() => {});
-
-        // Fetch new messages
-        NXApi.getTelexMessages()
-            .then((data) => {
-                for (const msg of data) {
-                    const message = new FreetextMessage();
-                    message.Network = AtsuMessageNetwork.FBW;
-                    message.Direction = AtsuMessageDirection.Input;
-                    message.Station = msg.from.flight;
-                    message.Message = msg.message.replace(/;/i, ' ');
-
-                    retval.push(message);
+        if (NXApiConnector.connected) {
+            if (NXApiConnector.updateCounter++ % 4 === 0) {
+                const status = NXApiConnector.createAircraftStatus();
+                if (status !== undefined) {
+                    await Telex.update(status);
                 }
-            })
-            .catch((err) => {
-                if (err.status === 404 || err === NXApi.disabledError || err === NXApi.disconnectedError) {
-                    return retval;
-                }
-                return [AtsuStatusCodes.ComFailed, retval];
-            });
+            }
+
+            // Fetch new messages
+            Telex.fetchMessages()
+                .then((data) => {
+                    for (const msg of data) {
+                        const message = new FreetextMessage();
+                        message.Network = AtsuMessageNetwork.FBW;
+                        message.Direction = AtsuMessageDirection.Input;
+                        message.Station = msg.from.flight;
+                        message.Message = msg.message.replace(/;/i, ' ');
+
+                        retval.push(message);
+                    }
+                })
+                .catch(() => [AtsuStatusCodes.ComFailed, retval]);
+        }
 
         return [AtsuStatusCodes.Ok, retval];
     }
