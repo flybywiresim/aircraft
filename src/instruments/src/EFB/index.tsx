a7318a55180727931adab4a9ee0e5864a0a422bd
ErickSharp <titaninmc@gmail.com>
2022-01-30 10:51:59 -0600

    refactor: hoist modalprovider up in tree and show battery warning when battery reaches 20

--- src/instruments/src/EFB/index.tsx
+++ src/instruments/src/EFB/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { render } from '@instruments/common/index';
+import { ModalProvider } from './UtilComponents/Modals/Modals';
 import { FailuresOrchestratorProvider } from './failures-orchestrator-provider';
 import Efb from './Efb';
 
@@ -13,9 +14,11 @@ import { store } from './Store/store';
 
 const EFBLoad = () => (
     <Router>
-        <Provider store={store}>
-            <Efb />
-        </Provider>
+        <ModalProvider>
+            <Provider store={store}>
+                <Efb />
+            </Provider>
+        </ModalProvider>
     </Router>
 );
 
