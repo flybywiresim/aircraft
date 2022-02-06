08d3e2ab10f0f09f8853584baf732ca67236f322
Andre Brito <felipe.brito016@gmail.com>
2022-02-06 13:04:38 -0400

    update

--- src/instruments/src/ND/elements/messages/TcasWxrMessages.tsx
+++ src/instruments/src/ND/elements/messages/TcasWxrMessages.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { Layer } from '@instruments/common/utils';
-import { useSimVar } from '@instruments/common/simVars';
 import { Mode } from '@shared/NavigationDisplay';
+import { useSimVar } from '@instruments/common/simVars';
 
 /*
 Messages in priority order, from 1-12 (full set with ATSAW and nice weather radar)
@@ -18,6 +18,12 @@ Messages in priority order, from 1-12 (full set with ATSAW and nice weather rada
 [                 |     ADS-B (amber)     ]
 */
 
+enum TcasPosition {
+    Standby = 0,
+    Ta = 1,
+    TaRa = 2,
+}
+
 interface TcasWxrMessage {
     text: string;
     color: 'White' | 'Amber';
@@ -30,30 +36,36 @@ export const TcasWxrMessages: FC<{ modeIndex: Mode }> = ({ modeIndex }) => {
     let rightMessage: TcasWxrMessage | undefined;
     let centerMessage: TcasWxrMessage | undefined;
 
-    const [tcasOnly] = useSimVar('L:A32NX_TCAS_TA_ONLY', 'boolean', 200);
-    const [tcasFault] = useSimVar('L:A32NX_TCAS_FAULT', 'boolean', 200);
-
-    if (tcasFault) {
-        leftMessage = { text: 'TCAS', color: 'Amber' };
-    } else if (tcasOnly) {
-        centerMessage = { text: 'TA ONLY', color: 'White' };
+    // TODO use logic in TCAS when it's implemented
+    const [tcasPosition] = useSimVar('L:A32NX_SWITCH_TCAS_Position', 'enum', 500);
+    const [radioAlt] = useSimVar('PLANE ALT ABOVE GROUND MINUS CG', 'feet', 500);
+    if (tcasPosition === TcasPosition.Ta || (tcasPosition === TcasPosition.TaRa && radioAlt < 1000)) {
+        leftMessage = { text: 'TA ONLY', color: 'White' };
     }
 
-    if ((modeIndex !== Mode.ARC && modeIndex !== Mode.ROSE_NAV && modeIndex !== Mode.ROSE_VOR && modeIndex !== Mode.ROSE_ILS) || (!leftMessage && !rightMessage && !centerMessage)) {
+    if (modeIndex !== Mode.ARC && modeIndex !== Mode.ROSE_NAV && modeIndex !== Mode.ROSE_VOR && modeIndex !== Mode.ROSE_ILS || (!leftMessage && !rightMessage && !centerMessage)) {
         return null;
     }
 
-    const y = modeIndex === Mode.ROSE_VOR || modeIndex === Mode.ROSE_ILS ? 713 : 684;
+    const y = (modeIndex === Mode.ROSE_VOR || modeIndex === Mode.ROSE_ILS) ? 713 : 684;
 
     return (
         <Layer x={164} y={y}>
-            {/* we fill/mask the map under both message boxes, per IRL refs */}
-            {(modeIndex === Mode.ARC || modeIndex === Mode.ROSE_NAV) && <rect x={0} y={0} width={440} height={59} className="BackgroundFill" stroke="none" />}
+            { /* we fill/mask the map under both message boxes, per IRL refs */ }
+            { (modeIndex === Mode.ARC || modeIndex === Mode.ROSE_NAV) && (
+                <rect x={0} y={0} width={440} height={59} className="BackgroundFill" stroke="none" />
+            )}
 
             <rect x={0} y={0} width={440} height={30} className="White BackgroundFill" strokeWidth={1.75} />
 
-            {leftMessage && (
-                <text x={8} y={25} className={`${leftMessage.color}`} textAnchor="start" fontSize={25}>
+            { (leftMessage) && (
+                <text
+                    x={8}
+                    y={25}
+                    className={`${leftMessage.color}`}
+                    textAnchor="start"
+                    fontSize={25}
+                >
                     {leftMessage.text}
                 </text>
             )}
@@ -63,8 +75,15 @@ export const TcasWxrMessages: FC<{ modeIndex: Mode }> = ({ modeIndex }) => {
                     {centerMessage.text}
                 </text>
             )}
-            {rightMessage && (
-                <text x={425} y={25} className={`${rightMessage.color}`} textAnchor="end" fontSize={25}>
+
+            { (rightMessage) && (
+                <text
+                    x={425}
+                    y={25}
+                    className={`${rightMessage.color}`}
+                    textAnchor="end"
+                    fontSize={25}
+                >
                     {rightMessage.text}
                 </text>
             )}
