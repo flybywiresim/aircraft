c98e04b0568647af73b530975ea5da5c50af0c62
Richard Pilbery <email@pilbery.co.uk>
2022-02-07 20:48:41 +0000

    Added PACKS/NAI/WAI logic

--- src/instruments/src/EWD/elements/PacksNaiWai.tsx
+++ src/instruments/src/EWD/elements/PacksNaiWai.tsx
@@ -8,12 +8,33 @@ type PacksNaiWaiProps = {
 };
 
 const PacksNaiWai: React.FC<PacksNaiWaiProps> = ({ x, y, flightPhase }) => {
-    const textString = 'PACKS';
+    const [autoThrustMode] = useSimVar('L:A32NX_AUTOTHRUST_MODE', 'enum', 500);
+    const [packs1Supplying] = useSimVar('L:A32NX_PACKS_1_IS_SUPPLYING', 'bool', 500);
+    const [packs2Supplying] = useSimVar('L:A32NX_PACKS_2_IS_SUPPLYING', 'bool', 500);
+    const [engine1AntiIce] = useSimVar('L:XMLVAR_Momentary_PUSH_OVHD_ANTIICE_ENG1_Pressed', 'number', 500);
+    const [engine2AntiIce] = useSimVar('L:XMLVAR_Momentary_PUSH_OVHD_ANTIICE_ENG2_Pressed', 'number', 500);
+    const [wingAntiIce] = useSimVar('L:XMLVAR_Momentary_PUSH_OVHD_ANTIICE_WING_Pressed', 'bool', 500);
+
+    const messageStrings = [
+        { name: 'PACKS', show: packs1Supplying || packs2Supplying },
+        { name: 'NAI', show: engine1AntiIce || engine2AntiIce },
+        { name: 'WAI', show: wingAntiIce },
+    ];
+
+    const finalMessageString = messageStrings.filter((item) => item.show).map((item) => item.name).join('/');
+
+    const showMessage = !!(flightPhase === 2
+    || (autoThrustMode >= 1 && autoThrustMode <= 4)
+    || (flightPhase >= 5 && flightPhase <= 7 && autoThrustMode === 5));
 
     return (
         <>
-            <text className="Green Large End" x={x} y={y}>{textString}</text>
+            {showMessage
+        && (
+            <text className="Green Large End" x={x} y={y}>{finalMessageString}</text>
+        )}
         </>
+
     );
 };
 
