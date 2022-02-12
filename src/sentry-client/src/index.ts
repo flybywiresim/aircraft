cb0253c5be85dcb6d36fa3e5b938a2e66944819e
Benjamin Dupont <benjozorkfr@gmail.com>
2022-02-12 14:52:55 -0500

    fix config

--- src/sentry-client/src/index.ts
+++ src/sentry-client/src/index.ts
@@ -5,4 +5,6 @@ declare const process: any;
 new FbwAircraftSentryClient().onInstrumentLoaded({
     dsn: process.env.SENTRY_DSN,
     buildInfoFilePrefix: 'a32nx',
+    root: false,
+    enableTracing: false,
 });
