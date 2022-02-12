e0c1ebff70aaf8e26be7af608e8f5934a3aab7f6
Frank Kopp <frank@familie-kopp.de>
2022-02-12 13:22:40 +0100

    Merged with main branch

--- src/instruments/src/EFB/Dashboard/Dashboard.tsx
+++ src/instruments/src/EFB/Dashboard/Dashboard.tsx
@@ -4,17 +4,20 @@
 import React from 'react';
 import { FlightWidget } from './Widgets/FlightWidget';
 import { RemindersWidget } from './Widgets/RemindersWidget';
+import { WeatherWidget } from './Widgets/WeatherWidget';
+import { useAppSelector } from '../Store/store';
 
 export const Dashboard = () => {
     const { departingAirport, arrivingAirport } = useAppSelector((state) => state.simbrief.data);
     const { userDepartureIcao, userDestinationIcao } = useAppSelector((state) => state.dashboard);
 
     return (
-    <div className="w-full">
-        <h1 className="font-bold">Dashboard</h1>
+        <div className="w-full">
+            <h1 className="font-bold">Dashboard</h1>
 
-        <div className="flex mt-4 w-full h-efb">
-            <FlightWidget />
+            <div className="flex mt-4 w-full h-efb">
+                <FlightWidget />
+
                 <div className="flex flex-col w-3/5">
                     <div className="p-6 mb-3 ml-3 h-2/5 rounded-lg border-2 border-theme-accent shadow-md">
                         <div className="flex items-center h-full">
@@ -28,9 +31,11 @@ export const Dashboard = () => {
                         </div>
                     </div>
 
-            <div className="overflow-hidden p-6 w-1/2 h-full rounded-lg border-2 border-theme-accent">
-                <RemindersWidget />
+                    <div className="overflow-hidden p-6 ml-3 h-3/5 rounded-lg border-2 border-theme-accent">
+                        <RemindersWidget />
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
