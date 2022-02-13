34c616c8703d57bc83354bf8649cff82e7214a01
ErickSharp <titaninmc@gmail.com>
2022-02-12 20:09:42 -0600

    feat: pinned charts base

--- src/instruments/src/EFB/Dashboard/Widgets/RemindersWidget.tsx
+++ src/instruments/src/EFB/Dashboard/Widgets/RemindersWidget.tsx
@@ -1,33 +1,72 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { IconArrowRight } from '@tabler/icons';
 import { A320Failure } from '@flybywiresim/failures';
 import { Link } from 'react-router-dom';
+import { usePersistentProperty } from '@instruments/common/persistence';
+import { setChartId, setChartLinks, setChartName, setIcao, setTabIndex, setChartRotation, setChartDimensions } from '../../Store/features/navigationPage';
+import { useNavigraph } from '../../ChartsApi/Navigraph';
 import { useFailuresOrchestrator } from '../../failures-orchestrator-provider';
+import { useAppDispatch, useAppSelector } from '../../Store/store';
+import { ScrollableContainer } from '../../UtilComponents/ScrollableContainer';
 
-export const RemindersWidget: FC = () => {
+export const RemindersWidget = () => {
     const { allFailures, activeFailures } = useFailuresOrchestrator();
+    const { pinnedCharts } = useAppSelector((state) => state.navigationTab);
+    const navigraph = useNavigraph();
+    const dispatch = useAppDispatch();
+    const [, setChartSource] = usePersistentProperty('EFB_CHART_SOURCE');
 
     return (
-        <>
-            <h2 className="font-bold">Reminders</h2>
-
-            <RemindersSection title="Pinned Charts" pageLinkPath="/navigation" />
-            <RemindersSection title="Maintenance" pageLinkPath="/failures">
-                <div className="flex flex-row flex-wrap">
-                    {Array
-                        .from(activeFailures)
-                        // Sorts the failures by name length, greatest to least
-                        .sort((a, b) => (allFailures.find((f) => f.identifier === b)?.name ?? '').length - (allFailures.find((f) => f.identifier === a)?.name ?? '').length)
-                        .map((failure) => (
-                            <ActiveFailureReminder name={allFailures.find((it) => it.identifier === failure)?.name ?? '<unknown>'} />
-                        ))}
-                    {activeFailures.size === 0 && (
-                        <h1 className="m-auto my-4 font-bold opacity-60">No Active Failures</h1>
+        <div className="p-6 w-full rounded-lg border-2 border-theme-accent">
+            <ScrollableContainer height={51}>
+                <div className="flex flex-col space-y-4">
+                    {navigraph.hasToken && (
+                        <RemindersSection title="Pinned Charts" pageLinkPath="/navigation">
+                            {pinnedCharts.map((chart) => (
+                                <Link
+                                    to="/navigation"
+                                    className="flex flex-col flex-wrap p-2 mt-4 mr-4 rounded-md border-2 bg-theme-highlight border-theme-highlight"
+                                    onClick={() => {
+                                        setChartSource('NAVIGRAPH');
+                                        dispatch(setChartDimensions({ width: undefined, height: undefined }));
+                                        dispatch(setChartLinks({ light: '', dark: '' }));
+                                        dispatch(setChartName(chart.chartName));
+                                        dispatch(setChartId(chart.chartId));
+                                        dispatch(setIcao(chart.icao));
+                                        dispatch(setTabIndex(chart.tabIndex));
+                                        dispatch(setChartRotation(0));
+                                    }}
+                                >
+                                    <h3 className="font-bold text-black">{chart.icao}</h3>
+                                    <span className="mt-2 text-black font-inter">{chart.title}</span>
+                                    <span className="ml-auto text-black">
+                                        <IconArrowRight />
+                                    </span>
+                                </Link>
+                            ))}
+                            {!pinnedCharts.length && (
+                                <h1 className="m-auto my-4 font-bold opacity-60">No Pinned Charts</h1>
+                            )}
+                        </RemindersSection>
                     )}
+                    <RemindersSection title="Maintenance" pageLinkPath="/failures">
+                        <div className="flex flex-row flex-wrap">
+                            {Array
+                                .from(activeFailures)
+                            // Sorts the failures by name length, greatest to least
+                                .sort((a, b) => (allFailures.find((f) => f.identifier === b)?.name ?? '').length - (allFailures.find((f) => f.identifier === a)?.name ?? '').length)
+                                .map((failure) => (
+                                    <ActiveFailureReminder name={allFailures.find((it) => it.identifier === failure)?.name ?? '<unknown>'} />
+                                ))}
+                            {!activeFailures.size && (
+                                <h1 className="m-auto my-4 font-bold opacity-60">No Active Failures</h1>
+                            )}
+                        </div>
+                    </RemindersSection>
+                    <RemindersSection title="Checklists" pageLinkPath="/checklists" />
                 </div>
-            </RemindersSection>
-            <RemindersSection title="Checklists" pageLinkPath="/checklists" />
-        </>
+            </ScrollableContainer>
+        </div>
     );
 };
 
@@ -38,7 +77,7 @@ interface RemindersSectionProps {
 
 const RemindersSection: FC<RemindersSectionProps> = ({ title, children, pageLinkPath }) => (
     <div className="flex flex-col pb-6 border-b-2 border-gray-700">
-        <div className="flex flex-row justify-between items-center mt-5 mb-2">
+        <div className="flex flex-row justify-between items-center mb-2">
             <h2 className="font-medium">{title}</h2>
 
             <Link to={pageLinkPath} className="flex items-center border-b-2 opacity-80 hover:opacity-100 transition duration-100 text-theme-highlight border-theme-highlight">
