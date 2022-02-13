8682a3dab177717340c1bc6abeef2de35b7f80cc
Sven Czarnian <devel@svcz.de>
2022-02-13 13:09:05 +0100

    add a function to set the value and fix some visualization issues

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/ATSU/A320_Neo_CDU_ATC_MaxUplinkDelay.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/ATSU/A320_Neo_CDU_ATC_MaxUplinkDelay.js
@@ -7,21 +7,52 @@ class CDUAtcMaxUplinkDelay {
             activeAtc = mcdu.atsuManager.atc.currentStation();
         }
 
+        let currentDelay = "\xa0NONE[color]cyan";
+        if (mcdu.atsuManager.atc.maxUplinkDelay !== -1) {
+            currentDelay = `\xa0${mcdu.atsuManager.atc.maxUplinkDelay}[color]cyan`;
+        }
+
         mcdu.setTemplate([
             ["MAX UPLINK DELAY"],
             [""],
             [""],
-            ["\xa0MODIFY ONLY ON DEMAND OF"],
-            [`ACTIVE ATC : ${activeAtc}`],
+            ["MODIFY ONLY ON DEMAND OF"],
+            [`ACTIVE ATC : {green}${activeAtc}{end}`],
             [""],
             [""],
             ["\xa0MAX UPLINK DELAY"],
-            ["\xa0NONE[color]inop"],
+            [currentDelay],
             [""],
             [""],
             ["\xa0ATC MENU"],
             ["<RETURN"]
         ]);
+
+        mcdu.leftInputDelay[3] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[3] = (value) => {
+            if (mcdu.atsuManager.atc.currentStation() === "") {
+                mcdu.addNewMessage(NXFictionalMessages.noAtc);
+            } else {
+                if (value === FMCMainDisplay.clrValue) {
+                    mcdu.atsuManager.atc.maxUplinkDelay = -1;
+                    CDUAtcMaxUplinkDelay.ShowPage(mcdu);
+                } else if (value) {
+                    if (/^[0-9]{3}(S)*$/.test(value)) {
+                        const delay = parseInt(value.replace("S", ""));
+                        if (delay < 5 || delay > 999) {
+                            mcdu.addNewMessage(NXSystemMessages.entryOutOfRange);
+                        } else {
+                            mcdu.atsuManager.atc.maxUplinkDelay = delay;
+                            CDUAtcMaxUplinkDelay.ShowPage(mcdu);
+                        }
+                    } else {
+                        mcdu.addNewMessage(NXSystemMessages.formatError);
+                    }
+                }
+            }
+        };
 
         mcdu.leftInputDelay[5] = () => {
             return mcdu.getDelaySwitchPage();
