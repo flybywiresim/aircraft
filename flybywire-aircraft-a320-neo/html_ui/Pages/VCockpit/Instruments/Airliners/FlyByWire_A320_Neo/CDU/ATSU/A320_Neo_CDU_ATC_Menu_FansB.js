e2cd15c85f9015a338185efe00798de626e33619
Sven Czarnian <devel@svcz.de>
2022-02-14 21:46:18 +0100

    introduce ATC menu for FANS A and FANS B

---
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/ATSU/A320_Neo_CDU_ATC_Menu_FansB.js
@@ -0,0 +1,71 @@
+class CDUAtcMenuFansB {
+    static ShowPage(mcdu) {
+        mcdu.clearDisplay();
+        mcdu.page.Current = mcdu.page.ATCMenu;
+
+        mcdu.setTemplate([
+            ["ATC MENU", "1", "2"],
+            [""],
+            ["<REQUEST[color]inop", "EDIT>[color]inop"],
+            [""],
+            ["", "TEXT>[color]inop"],
+            [""],
+            ["", ""],
+            [""],
+            ["<MSG RECORD", ""],
+            ["", "FREQUENCY\xa0"],
+            ["<CONNECTION", "MONITORING>"],
+            ["\xa0ATSU DLK"],
+            ["<RETURN"]
+        ]);
+
+        mcdu.leftInputDelay[0] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[0] = () => {
+            //CDUAtcRequest.ShowPage(mcdu);
+        };
+
+        mcdu.leftInputDelay[3] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[3] = () => {
+            CDUAtcMessagesRecord.ShowPage(mcdu);
+        };
+
+        mcdu.leftInputDelay[4] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[4] = () => {
+            CDUAtcConnection.ShowPage(mcdu);
+        };
+
+        mcdu.leftInputDelay[5] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onLeftInput[5] = () => {
+            CDUAtsuMenu.ShowPage(mcdu);
+        };
+
+        mcdu.rightInputDelay[3] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onRightInput[3] = () => {
+            //CDUAtcReports.ShowPage(mcdu);
+        };
+
+        mcdu.rightInputDelay[5] = () => {
+            return mcdu.getDelaySwitchPage();
+        };
+        mcdu.onRightInput[5] = () => {
+            //CDUAtcEmergency.ShowPage(mcdu);
+        };
+
+        mcdu.onPrevPage = () => {
+            CDUAtcMenu.ShowPage2(mcdu);
+        };
+        mcdu.onNextPage = () => {
+            CDUAtcMenu.ShowPage2(mcdu);
+        };
+    }
+}
