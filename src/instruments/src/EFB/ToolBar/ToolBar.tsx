6ffcc0f99309b9ede640bc4bc2b2459433171465
ErickSharp <titaninmc@gmail.com>
2022-02-18 01:13:19 -0600

    add rudimentary reminder sorting and change red marking on checklist behavior

--- src/instruments/src/EFB/ToolBar/ToolBar.tsx
+++ src/instruments/src/EFB/ToolBar/ToolBar.tsx
@@ -7,16 +7,15 @@ import {
     ExclamationDiamond,
     Gear,
     Calculator,
-    JournalCheck,
+    JournalCheck, HouseDoor,
 } from 'react-bootstrap-icons';
 import { Link, useHistory } from 'react-router-dom';
-import { FbwLogo } from '../UtilComponents/FbwLogo';
 
 export const ToolBar = () => (
     <nav className="flex overflow-hidden flex-col flex-shrink-0 justify-between w-32">
         <div className="flex flex-col items-center mt-11">
             <ToolBarButton to="/dashboard">
-                <FbwLogo width={40} height={40} />
+                <HouseDoor size={35} />
             </ToolBarButton>
             <ToolBarButton to="/dispatch">
                 <Clipboard size={35} />
