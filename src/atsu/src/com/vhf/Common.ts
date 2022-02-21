489681ff3dbb466e252eb27c898bb774f914ff68
Sven Czarnian <devel@svcz.de>
2022-02-21 19:00:44 +0100

    introduce a dataprovider configuration for dynamic data setups

--- src/atsu/src/com/vhf/Common.ts
+++ src/atsu/src/com/vhf/Common.ts
@@ -1,6 +1,12 @@
 //  Copyright (c) 2021 FlyByWire Simulations
 //  SPDX-License-Identifier: GPL-3.0
 
+export enum DatalinkProviders {
+    ARINC = 0,
+    SITA = 1,
+    ProviderCount = 2
+}
+
 export class Aircraft {
     public Latitude = 0.0;
 
@@ -19,3 +25,9 @@ export class OwnAircraft extends Aircraft {
 export const MaxSearchRange = 400;
 // maximum datarate under optimal conditions: 31.5 kb/s
 export const VdlMaxDatarate = 31500;
+
+// dataprovider configuration
+export const DatalinkConfiguration: number[] = [
+    137.275, // ARINC
+    137.975, // SITA
+];
