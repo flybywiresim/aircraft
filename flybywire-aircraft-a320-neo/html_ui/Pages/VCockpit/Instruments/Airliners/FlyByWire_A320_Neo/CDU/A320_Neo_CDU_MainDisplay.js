0e2cd2b4dbde6f593e8718cb39ec104f61bb6bf5
Sven Czarnian <devel@svcz.de>
2022-02-25 11:16:40 +0100

    add a translation to NX-messages

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_MainDisplay.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_MainDisplay.js
@@ -983,6 +983,12 @@ class A320_Neo_CDU_MainDisplay extends FMCMainDisplay {
             case Atsu.AtsuStatusCodes.SystemBusy:
                 this.addNewMessage(NXSystemMessages.systemBusy);
                 break;
+            case Atsu.AtsuStatusCodes.EntryOutOfRange:
+                this.addNewMessage(NXSystemMessages.entryOutOfRange);
+                break;
+            case Atsu.AtsuStatusCodes.FormatError:
+                this.addNewMessage(NXSystemMessages.formatError);
+                break;
             default:
                 break;
         }
