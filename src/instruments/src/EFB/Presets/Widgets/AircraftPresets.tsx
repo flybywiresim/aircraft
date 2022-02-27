a9cf8bfee797fca34422fe729eae11cc48b58774
Frank Kopp <frank@familie-kopp.de>
2022-02-27 13:33:29 +0100

    Added prototype pages

--- src/instruments/src/EFB/Presets/Widgets/AircraftPresets.tsx
+++ src/instruments/src/EFB/Presets/Widgets/AircraftPresets.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 FlyByWire Simulations
+// SPDX-License-Identifier: GPL-3.0
+
 import React from 'react';
 import { ScrollableContainer } from '../../UtilComponents/ScrollableContainer';
 
@@ -9,7 +12,7 @@ export const AircraftPresets = () => (
         <div className="p-4 mt-4 rounded-lg border-2 border-theme-accent">
             <ScrollableContainer height={52}>
                 <div className="grid grid-cols-4 grid-rows-4 grid-flow-row gap-4">
-                    TEST
+                    Not yet implemented
                 </div>
             </ScrollableContainer>
         </div>
--- src/instruments/src/EFB/Presets/Widgets/LightPresets.tsx
+++ src/instruments/src/EFB/Presets/Widgets/LightPresets.tsx
@@ -1,5 +1,13 @@
+// Copyright (c) 2022 FlyByWire Simulations
+// SPDX-License-Identifier: GPL-3.0
+
+/* eslint-disable max-len */
 import React from 'react';
 import { ScrollableContainer } from '../../UtilComponents/ScrollableContainer';
+
+function handleClick(number: number) {
+    console.log(`Set Preset ${number}`);
+}
 
 export const LightPresets = () => (
     <div className="w-full">
@@ -7,11 +15,20 @@ export const LightPresets = () => (
             <h1 className="font-bold">Light Presets</h1>
         </div>
         <div className="p-4 mt-4 rounded-lg border-2 border-theme-accent">
-            <ScrollableContainer height={52}>
-                <div className="grid grid-cols-4 grid-rows-4 grid-flow-row gap-4">
-                    TEST
+            <div className="flex flex-row space-x-6 h-content-section-reduced">
+                <div className="flex flex-col flex-shrink-0 justify-between w-1/2">
+                    <ScrollableContainer height={52}>
+                        <div className="space-y-4">
+                            <div
+                                className="flex justify-center items-center w-full h-12 text-theme-text hover:text-theme-body bg-theme-accent hover:bg-theme-highlight rounded-md border-2 border-theme-accent transition duration-100"
+                                onClick={() => handleClick(1)}
+                            >
+                                Preset 1
+                            </div>
+                        </div>
+                    </ScrollableContainer>
                 </div>
-            </ScrollableContainer>
+            </div>
         </div>
     </div>
 );
