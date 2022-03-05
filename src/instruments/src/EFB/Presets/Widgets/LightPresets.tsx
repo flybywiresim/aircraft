950c5599ef3ed8bda50d1bec46d8957f2436ae4e
Frank Kopp <frank@familie-kopp.de>
2022-03-05 01:51:45 +0100

    Changed to 3 Presets with load and save buttons

--- src/instruments/src/EFB/Presets/Widgets/LightPresets.tsx
+++ src/instruments/src/EFB/Presets/Widgets/LightPresets.tsx
@@ -2,16 +2,13 @@
 // SPDX-License-Identifier: GPL-3.0
 
 /* eslint-disable max-len */
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSimVar } from '@instruments/common/simVars';
-import { usePersistentNumberProperty } from '@instruments/common/persistence';
 import { ScrollableContainer } from '../../UtilComponents/ScrollableContainer';
 
 export const LightPresets = () => {
-    const [loadPreset, setLoadPreset] = useSimVar('L:A32NX_LOAD_LIGHTING_PRESET', 'number', 200);
-    const [testWASMVar] = useSimVar('L:A32NX_TEST_VAR', 'number', 10);
-
-    const [efbBrightness] = useSimVar('L:A32NX_EFB_BRIGHTNESS', 'number', 200);
+    const [, setLoadPreset] = useSimVar('L:A32NX_LOAD_LIGHTING_PRESET', 'number', 200);
+    const [, setSavePreset] = useSimVar('L:A32NX_SAVE_LIGHTING_PRESET', 'number', 200);
 
     return (
         <div className="w-full">
@@ -19,34 +16,46 @@ export const LightPresets = () => {
                 <h1 className="font-bold">Light Presets</h1>
             </div>
             <div className="p-4 mt-4 rounded-lg border-2 border-theme-accent">
-                <div className="flex flex-row space-x-6 h-content-section-reduced">
-                    <div className="flex flex-col flex-shrink-0 justify-between w-1/2">
-                        <ScrollableContainer height={52}>
-                            <div className="space-y-4">
-                                <div
-                                    className="flex justify-center items-center w-full h-12 text-theme-text hover:text-theme-body bg-theme-accent hover:bg-theme-highlight rounded-md border-2 border-theme-accent transition duration-100"
-                                    onClick={() => setLoadPreset(1)}
-                                >
-                                    Load Preset 1
-                                </div>
-                                <div
-                                    className="flex justify-center items-center w-full h-12 text-theme-text hover:text-theme-body bg-theme-accent hover:bg-theme-highlight rounded-md border-2 border-theme-accent transition duration-100"
-                                    onClick={() => setLoadPreset(2)}
-                                >
-                                    Load Preset 2
-                                </div>
-                            </div>
-                            <br />
-                            loadPreset =
-                            {' '}
-                            {loadPreset}
-                            <br />
-                            efbBrightness =
-                            {' '}
-                            {efbBrightness}
-                        </ScrollableContainer>
+                <ScrollableContainer height={52}>
+                    <div className="grid grid-cols-2 grid-rows-2 grid-flow-row gap-4">
+                        <div
+                            className="flex justify-center items-center my-1 mx-1 w-full h-24 text-theme-text hover:text-theme-body bg-theme-accent hover:bg-theme-highlight rounded-md border-2 border-theme-accent transition duration-100"
+                            onClick={() => setLoadPreset(1)}
+                        >
+                            Load Preset 1
+                        </div>
+                        <div
+                            className="flex justify-center items-center my-1 mx-2 w-full h-24 text-white bg-red-500 hover:bg-red-600 rounded-md border-2 border-red-500 hover:border-red-600 transition duration-100"
+                            onClick={() => setSavePreset(1)}
+                        >
+                            Save Preset 1
+                        </div>
+                        <div
+                            className="flex justify-center items-center my-2 mx-1 w-full h-24 text-theme-text hover:text-theme-body bg-theme-accent hover:bg-theme-highlight rounded-md border-2 border-theme-accent transition duration-100"
+                            onClick={() => setLoadPreset(2)}
+                        >
+                            Load Preset 2
+                        </div>
+                        <div
+                            className="flex justify-center items-center my-1 mx-2 w-full h-24 text-white bg-red-500 hover:bg-red-600 rounded-md border-2 border-red-500 hover:border-red-600 transition duration-100"
+                            onClick={() => setSavePreset(2)}
+                        >
+                            Save Preset 2
+                        </div>
+                        <div
+                            className="flex justify-center items-center my-2 mx-1 w-full h-24 text-theme-text hover:text-theme-body bg-theme-accent hover:bg-theme-highlight rounded-md border-2 border-theme-accent transition duration-100"
+                            onClick={() => setLoadPreset(3)}
+                        >
+                            Load Preset 3
+                        </div>
+                        <div
+                            className="flex justify-center items-center my-1 mx-2 w-full h-24 text-white bg-red-500 hover:bg-red-600 rounded-md border-2 border-red-500 hover:border-red-600 transition duration-100"
+                            onClick={() => setSavePreset(3)}
+                        >
+                            Save Preset 3
+                        </div>
                     </div>
-                </div>
+                </ScrollableContainer>
             </div>
         </div>
     );
