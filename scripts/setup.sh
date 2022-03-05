9838e6a4c62383be058bef32a28b31e31ee325c2
Saschl <sascharudolf46@gmail.com>
2022-03-05 22:40:13 +0100

    chore: changed how msfs-avionics-mirror is used

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,10 +2,17 @@
 
 set -ex
 
+pwd
+cd msfs-avionics-mirror/src/sdk
+rm -rf node_modules
+rm -rf build
+npm install
+npm run build
+cp package.json build/
+cd build
+npm pack
+
+cd /external
 rm -f package-lock.json
 rm -rf node_modules
 npm install --no-optional
-cd src/PFDV2
-rm -f package-lock.json
-rm -rf node_modules
-npm install
