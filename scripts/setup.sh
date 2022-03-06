52365438ddd4273052a0f62c1280f973bebd59da
Saschl <sascharudolf46@gmail.com>
2022-03-06 14:27:55 +0100

    list node_modules contents

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,12 +7,14 @@ cd msfs-avionics-mirror/src/sdk
 rm -rf node_modules
 rm -rf build
 npm install
+cd node_modules
+ls -la
 npm run build
 cp package.json build/
 cd build
 npm pack
 
-cd /external
-rm -f package-lock.json
-rm -rf node_modules
-npm install --no-optional
+#cd /external
+#rm -f package-lock.json
+#rm -rf node_modules
+#npm install --no-optional
