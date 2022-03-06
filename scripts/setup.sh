2b32329af95f0bae876b35a3b0f2ad951117eaf6
Saschl <sascharudolf46@gmail.com>
2022-03-06 14:46:17 +0100

    fix: try bundling types as tgz

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,20 +2,18 @@
 
 set -ex
 
-pwd
-cd msfs-avionics-mirror/src/sdk
+cd msfs-avionics-mirror/src/msfstypes
+npm pack
+cd ../sdk
 rm -rf node_modules
 rm -rf build
 npm install
-cd node_modules
-cd msfstypes
-ls -la
 npm run build
 cp package.json build/
 cd build
 npm pack
 
-#cd /external
-#rm -f package-lock.json
-#rm -rf node_modules
-#npm install --no-optional
+cd /external
+rm -f package-lock.json
+rm -rf node_modules
+npm install --no-optional
