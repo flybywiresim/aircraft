dac6a29cc04c987fd2b2caef11f50f45fdc95d9d
Richard Pilbery <email@pilbery.co.uk>
2022-03-13 18:39:07 +0000

    Initial commit

---
+++ src/instruments/src/SD/Pages/Bleed/Bleed.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { render } from '@instruments/common/index';
+import { setIsEcamPage } from '@instruments/common/defaults';
+import { PageTitle } from '../../Common/PageTitle';
+import { EcamPage } from '../../Common/EcamPage';
+
+import './Bleed.scss';
+
+setIsEcamPage('bleed_page');
+
+export const BleedPage: FC = () => {
+    console.log('Bleed Page');
+
+    return (
+        <EcamPage name="main-bleed">
+            <PageTitle x={6} y={18} text="BLEED" />
+
+        </EcamPage>
+    );
+};
+
+render(<BleedPage />);
