f58838333c5f0170005ce00ff770407bf3f970fb
ErickSharp <titaninmc@gmail.com>
2022-03-16 15:47:26 -0500

    persistence of failure subpage routes

--- src/instruments/src/EFB/Failures/Failures.tsx
+++ src/instruments/src/EFB/Failures/Failures.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { AtaChaptersDescription, AtaChaptersTitle, AtaChapterNumber } from '@shared/ata';
-import { Route } from 'react-router';
 import { Link } from 'react-router-dom';
-import { pathify } from '../Utils/routing';
+import { InfoCircleFill } from 'react-bootstrap-icons';
+import { PageLink, PageRedirect, pathify, TabRoutes } from '../Utils/routing';
 import { ScrollableContainer } from '../UtilComponents/ScrollableContainer';
 import { useFailuresOrchestrator } from '../failures-orchestrator-provider';
 import { AtaChapterPage } from './Pages/AtaChapterPage';
@@ -53,32 +53,51 @@ const ATAChapterCard = ({ ataNumber, description, title }: ATAFailureCardProps) 
     );
 };
 
-export const Failures = () => {
+const FailuresHome = () => {
     const { allFailures } = useFailuresOrchestrator();
     const chapters = Array.from(new Set(allFailures.map((it) => it.ata))).sort((a, b) => a - b);
 
     return (
-        <div className="w-full">
-            <Route exact path="/failures">
-                <div className="flex flex-row items-end space-x-4">
-                    <h1 className="font-bold">Failures</h1>
-                    <h2>Full simulation of the failures below isn't yet guaranteed.</h2>
+        <>
+            <div className="flex flex-row justify-between space-x-4">
+                <h1 className="font-bold">Failures</h1>
+
+                <div className="flex flex-row items-center py-1 px-4 space-x-2 rounded-md bg-colors-yellow-400">
+                    <InfoCircleFill className="text-theme-body" />
+                    <p className="text-theme-body">Full simulation of the failures below isn't yet guaranteed.</p>
                 </div>
-                <div className="p-4 mt-4 rounded-lg border-2 border-theme-accent h-content-section-reduced">
-                    <ScrollableContainer height={52}>
-                        <div className="flex flex-col space-y-1">
-                            {chapters.map((chapter) => (
-                                <ATAChapterCard
-                                    ataNumber={chapter}
-                                    title={AtaChaptersTitle[chapter]}
-                                    description={AtaChaptersDescription[chapter]}
-                                />
-                            ))}
-                        </div>
-                    </ScrollableContainer>
-                </div>
-            </Route>
-            <Route path="/failures/:ataNumber" component={AtaChapterPage} />
-        </div>
+            </div>
+
+            <div className="p-4 mt-4 rounded-lg border-2 border-theme-accent h-content-section-reduced">
+                <ScrollableContainer height={52}>
+                    <div className="flex flex-col space-y-1">
+                        {chapters.map((chapter) => (
+                            <ATAChapterCard
+                                ataNumber={chapter}
+                                title={AtaChaptersTitle[chapter]}
+                                description={AtaChaptersDescription[chapter]}
+                            />
+                        ))}
+                    </div>
+                </ScrollableContainer>
+            </div>
+        </>
+    );
+};
+
+export const Failures = () => {
+    const { allFailures } = useFailuresOrchestrator();
+    const chapters = Array.from(new Set(allFailures.map((it) => it.ata))).sort((a, b) => a - b);
+
+    const tabs: PageLink[] = [
+        { name: 'Home', component: <FailuresHome /> },
+        ...chapters.map((chapter) => ({ name: chapter.toString(), component: <AtaChapterPage chapter={chapter} /> })),
+    ];
+
+    return (
+        <>
+            <TabRoutes basePath="/failures" tabs={tabs} />
+            <PageRedirect basePath="/failures" tabs={tabs} />
+        </>
     );
 };
--- src/instruments/src/EFB/Failures/Pages/AtaChapterPage.tsx
+++ src/instruments/src/EFB/Failures/Pages/AtaChapterPage.tsx
@@ -1,31 +1,34 @@
 import React from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ArrowLeft } from 'react-bootstrap-icons';
-import { AtaChaptersTitle } from '@shared/ata';
+import { AtaChapterNumber, AtaChaptersTitle } from '@shared/ata';
 import { FailureButton } from './Failure';
 import { useFailuresOrchestrator } from '../../failures-orchestrator-provider';
 import { ScrollableContainer } from '../../UtilComponents/ScrollableContainer';
 
-export const AtaChapterPage = () => {
-    const { ataNumber } = useParams<{ataNumber: string}>();
+interface AtaChapterPageProps {
+    chapter: AtaChapterNumber;
+}
+
+export const AtaChapterPage = ({ chapter }: AtaChapterPageProps) => {
     const { allFailures, activeFailures, changingFailures, activate, deactivate } = useFailuresOrchestrator();
 
     return (
         <div>
-            <Link to="/failures" className="inline-block">
+            <Link to="/failures/home" className="inline-block">
                 <div className="flex flex-row items-center space-x-3 transition duration-100 hover:text-theme-highlight">
                     <ArrowLeft size={30} />
                     <h1 className="font-bold text-current">
                         Failures
                         {' > '}
-                        {AtaChaptersTitle[ataNumber]}
+                        {AtaChaptersTitle[chapter]}
                     </h1>
                 </div>
             </Link>
             <div className="p-4 mt-4 rounded-lg border-2 border-theme-accent h-content-section-reduced">
                 <ScrollableContainer height={44}>
                     <div className="grid grid-cols-4 auto-rows-auto">
-                        {allFailures.filter((failure) => failure.ata === parseInt(ataNumber)).map((failure, index) => (
+                        {allFailures.filter((failure) => failure.ata === chapter).map((failure, index) => (
                             <FailureButton
                                 name={failure.name}
                                 isActive={activeFailures.has(failure.identifier)}
