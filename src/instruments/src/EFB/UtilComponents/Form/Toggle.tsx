ff924a079b35b5ed39637bb7b2e96469162a415d
ErickSharp <titaninmc@gmail.com>
2022-03-17 23:18:59 -0500

    fix toggle color when active

--- src/instruments/src/EFB/UtilComponents/Form/Toggle.tsx
+++ src/instruments/src/EFB/UtilComponents/Form/Toggle.tsx
@@ -7,6 +7,6 @@ interface ToggleProps {
 
 export const Toggle = ({ value, onToggle }: ToggleProps) => (
     <div className="flex items-center w-14 h-8 rounded-full cursor-pointer bg-theme-accent" onClick={() => onToggle(!value)}>
-        <div className={`w-6 h-6 bg-white rounded-full transition mx-1.5 duration-200 transform ${value && 'translate-x-5 bg-theme-highlight'}`} />
+        <div className={`w-6 h-6 bg-white rounded-full transition mx-1.5 duration-200 transform ${value && 'translate-x-5 !bg-theme-highlight'}`} />
     </div>
 );
