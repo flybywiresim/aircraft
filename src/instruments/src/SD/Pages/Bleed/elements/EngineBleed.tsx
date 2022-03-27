f9d5eef13628e3147cc6b2b424d6412ffd5b6639
Richard Pilbery <email@pilbery.co.uk>
2022-03-27 17:12:58 +0100

    Separate packs component

--- src/instruments/src/SD/Pages/Bleed/elements/EngineBleed.tsx
+++ src/instruments/src/SD/Pages/Bleed/elements/EngineBleed.tsx
@@ -1,6 +1,6 @@
-import { GaugeComponent, GaugeMarkerComponent } from '@instruments/common/gauges';
 import { useSimVar } from '@instruments/common/simVars';
 import React, { FC } from 'react';
+import BleedGauge from './BleedGauge';
 import Valve from './Valve';
 
 interface EngineBleedProps {
@@ -17,7 +17,6 @@ const EngineBleed: FC<EngineBleedProps> = ({ x, y, engine, sdacDatum, enginePRVa
     const [engineN1Idle] = useSimVar('L:A32NX_ENGINE_IDLE_N1', 'percent', 500);
     const engineN1BelowIdle = (engineN1 + 2) < engineN1Idle;
     const [engineHPValveOpen] = useSimVar(`L:A32NX_PNEU_ENG_${engine}_HP_VALVE_OPEN`, 'bool', 500);
-    // const [enginePRValveOpen] = useSimVar(`L:A32NX_PNEU_ENG_${engine}_PR_VALVE_OPEN`, 'bool', 500);
     const [precoolerOutletTemp] = useSimVar(`L:A32NX_PNEU_ENG_${engine}_PRECOOLER_OUTLET_TEMPERATURE`, 'celsius', 100);
     const precoolerOutletTempFive = Math.round(precoolerOutletTemp / 5) * 5;
     const [precoolerInletPress] = useSimVar(`L:A32NX_PNEU_ENG_${engine}_PRECOOLER_INLET_PRESSURE`, 'psi', 10);
@@ -78,111 +77,3 @@ const EngineBleed: FC<EngineBleedProps> = ({ x, y, engine, sdacDatum, enginePRVa
 };
 
 export default EngineBleed;
-
-interface BleedGaugeProps {
-    x: number,
-    y: number,
-    engine: number,
-    sdacDatum: boolean,
-    packFlowValveOpen: boolean,
-}
-
-const BleedGauge: FC<BleedGaugeProps> = ({ x, y, engine, sdacDatum, packFlowValveOpen }) => {
-    // TODO
-    // Pack precpper outlet temp, pack inlet flow rate, pack bypass valve and pack outlet temp should be revised once the packs are modelled
-
-    const [precoolerOutletTemp] = useSimVar(`L:A32NX_PNEU_ENG_${engine}_PRECOOLER_OUTLET_TEMPERATURE`, 'celsius', 500);
-    const compressorOutletTemp = Math.round(precoolerOutletTemp / 5) * 5;
-
-    const [packInletFlowPercentage] = useSimVar('L:A32NX_COND_PACK_FLOW', 'percent', 500);
-
-    const [fwdCondSelectorKnob] = useSimVar('L:A32NX_OVHD_COND_FWD_SELECTOR_KNOB', 'number', 1000); // 0 to 300
-    const packBypassValve = Math.round(fwdCondSelectorKnob / 300 * 100);
-    const [fwdCabinTemp] = useSimVar('L:A32NX_COND_FWD_TEMP', 'celsius', 1000);
-    const packOutletTemp = Math.round(fwdCabinTemp / 5) * 5;
-
-    const radius = 38;
-    const startAngle = -63;
-    const endAngle = 63;
-    const min = 80;
-    const max = 120;
-    const minBypass = 0;
-    const maxBypass = 100;
-
-    return (
-        <g id={`Engine${engine}AirCond`}>
-            {/* Pack Outlet Temp */}
-            <text className="Large End Green" x={x + 15} y={y - 117}>{packOutletTemp}</text>
-            <text x={x + 20} y={y - 117} className="Cyan Standard">°C</text>
-
-            {/* Bypass valve */}
-            <GaugeComponent x={x} y={y - 69} radius={radius} startAngle={startAngle} endAngle={endAngle} visible className="GaugeComponent Gauge">
-                <GaugeMarkerComponent
-                    value={50}
-                    x={x}
-                    y={y - 69}
-                    min={minBypass}
-                    max={maxBypass}
-                    radius={radius}
-                    startAngle={startAngle}
-                    endAngle={endAngle}
-                    className="WhiteStroke Stroke2"
-                    showValue={false}
-                    outer
-                    multiplierOuter={1.1}
-                />
-                <GaugeMarkerComponent
-                    value={packBypassValve}
-                    x={x}
-                    y={y - 69}
-                    min={minBypass}
-                    max={maxBypass}
-                    radius={radius}
-                    startAngle={startAngle}
-                    endAngle={endAngle}
-                    className="GaugeIndicator Gauge LineCapRound"
-                    indicator
-                    multiplierOuter={1.1}
-                />
-            </GaugeComponent>
-
-            {/* Compressor Outlet Temp */}
-            <text className={`Large End ${compressorOutletTemp > 230 || !sdacDatum ? 'Amber' : 'Green'}`} x={x + 20} y={y - 47}>{sdacDatum ? compressorOutletTemp : 'XX'}</text>
-            <text x={x + 20} y={y - 47} className="Cyan Standard">°C</text>
-
-            {/* Pack inlet flow */}
-            <GaugeComponent x={x} y={y} radius={radius} startAngle={startAngle} endAngle={endAngle} visible className="GaugeComponent Gauge">
-                <GaugeMarkerComponent
-                    value={100}
-                    x={x}
-                    y={y}
-                    min={min}
-                    max={max}
-                    radius={radius}
-                    startAngle={startAngle}
-                    endAngle={endAngle}
-                    className="WhiteStroke Stroke2"
-                    showValue={false}
-                    outer
-                    multiplierOuter={1.1}
-                />
-                <GaugeMarkerComponent
-                    value={packInletFlowPercentage < 80 ? 80 : packInletFlowPercentage}
-                    x={x}
-                    y={y}
-                    min={min}
-                    max={max}
-                    radius={radius}
-                    startAngle={startAngle}
-                    endAngle={endAngle}
-                    className={`${packFlowValveOpen ? 'GaugeIndicator' : 'AmberLine'} Gauge LineCapRound`}
-                    indicator
-                    multiplierOuter={1.1}
-                />
-            </GaugeComponent>
-
-            {/* Flow control valve */}
-            <Valve x={x} y={y} radius={15} css="GreenLine BackgroundFill" position={packFlowValveOpen ? 'V' : 'H'} sdacDatum={sdacDatum} />
-        </g>
-    );
-};
