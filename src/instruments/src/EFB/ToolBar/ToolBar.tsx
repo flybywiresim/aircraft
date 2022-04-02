b54e6ff429fb02f17476562c9278ac8b8bae462f
ErickSharp <titaninmc@gmail.com>
2022-04-02 12:30:07 -0500

    use translations for toolbar tooltips

--- src/instruments/src/EFB/ToolBar/ToolBar.tsx
+++ src/instruments/src/EFB/ToolBar/ToolBar.tsx
@@ -11,6 +11,7 @@ import {
     Sliders,
 } from 'react-bootstrap-icons';
 import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { TooltipWrapper } from '../UtilComponents/TooltipWrapper';
 
 // @ts-ignore
@@ -33,43 +34,47 @@ const ToolBarButton: FC<ToolBarButtonProps> = ({ to, tooltipText, children }) =>
     </TooltipWrapper>
 );
 
-export const ToolBar = () => (
-    <nav className="flex flex-col flex-shrink-0 justify-between py-6 w-32">
-        <div className="flex flex-col items-center mt-9 space-y-4">
-            <ToolBarButton to="/dashboard" tooltipText="Dashboard">
-                <img className="w-[35px]" src={FbwTail} alt="FbwTail" />
-            </ToolBarButton>
-            <ToolBarButton to="/dispatch" tooltipText="Dispatch">
-                <Clipboard size={35} />
-            </ToolBarButton>
-            <ToolBarButton to="/ground" tooltipText="Ground">
-                <Truck size={35} />
-            </ToolBarButton>
-            <ToolBarButton to="/performance" tooltipText="Performance">
-                <Calculator size={35} />
-            </ToolBarButton>
-            <ToolBarButton to="/navigation" tooltipText="Navigation & Charts">
-                <Compass size={35} />
-            </ToolBarButton>
-            <ToolBarButton to="/atc" tooltipText="Air Traffic Control">
-                <BroadcastPin size={35} />
-            </ToolBarButton>
-            <ToolBarButton to="/failures" tooltipText="Failures">
-                <ExclamationDiamond size={35} />
-            </ToolBarButton>
-            <ToolBarButton to="/checklists" tooltipText="Checklists">
-                <JournalCheck size={35} />
-            </ToolBarButton>
-            <ToolBarButton to="/presets" tooltipText="Presets">
-                <Sliders size={35} />
-            </ToolBarButton>
-        </div>
+export const ToolBar = () => {
+    const { t } = useTranslation();
 
-        <div className="flex flex-col items-center">
-            <div className="my-4 w-14 h-1.5 rounded-full bg-theme-accent" />
-            <ToolBarButton to="/settings" tooltipText="Settings">
-                <Gear color="currentColor" size={35} />
-            </ToolBarButton>
-        </div>
-    </nav>
-);
+    return (
+        <nav className="flex flex-col flex-shrink-0 justify-between py-6 w-32">
+            <div className="flex flex-col items-center mt-9 space-y-4">
+                <ToolBarButton to="/dashboard" tooltipText="Dashboard">
+                    <img className="w-[35px]" src={FbwTail} alt="FbwTail" />
+                </ToolBarButton>
+                <ToolBarButton to="/dispatch" tooltipText={t('Dispatch.Title')}>
+                    <Clipboard size={35} />
+                </ToolBarButton>
+                <ToolBarButton to="/ground" tooltipText={t('Ground.Title')}>
+                    <Truck size={35} />
+                </ToolBarButton>
+                <ToolBarButton to="/performance" tooltipText={t('Performance.Title')}>
+                    <Calculator size={35} />
+                </ToolBarButton>
+                <ToolBarButton to="/navigation" tooltipText={t('NavigationAndCharts.Title')}>
+                    <Compass size={35} />
+                </ToolBarButton>
+                <ToolBarButton to="/atc" tooltipText={t('AirTrafficControl.Title')}>
+                    <BroadcastPin size={35} />
+                </ToolBarButton>
+                <ToolBarButton to="/failures" tooltipText={t('Failures.Title')}>
+                    <ExclamationDiamond size={35} />
+                </ToolBarButton>
+                <ToolBarButton to="/checklists" tooltipText={t('Checklists.Title')}>
+                    <JournalCheck size={35} />
+                </ToolBarButton>
+                <ToolBarButton to="/presets" tooltipText={t('Presets.Title')}>
+                    <Sliders size={35} />
+                </ToolBarButton>
+            </div>
+
+            <div className="flex flex-col items-center">
+                <div className="my-4 w-14 h-1.5 rounded-full bg-theme-accent" />
+                <ToolBarButton to="/settings" tooltipText="Settings">
+                    <Gear color="currentColor" size={35} />
+                </ToolBarButton>
+            </div>
+        </nav>
+    );
+};
