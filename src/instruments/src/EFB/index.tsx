5a2b813dbf5c746a3bf3db0149ed29847a24d54b
ErickSharp <titaninmc@gmail.com>
2022-04-03 07:38:23 -0500

    show sentry session id on error page

--- src/instruments/src/EFB/index.tsx
+++ src/instruments/src/EFB/index.tsx
@@ -7,6 +7,7 @@ import { usePersistentProperty } from '@instruments/common/persistence';
 import { Provider } from 'react-redux';
 import { render } from '@instruments/common/index';
 import { ErrorBoundary } from 'react-error-boundary';
+import { SentryConsentState, SENTRY_CONSENT_KEY } from 'sentry-client/src/FbwAircraftSentryClient';
 import { ModalProvider } from './UtilComponents/Modals/Modals';
 import { FailuresOrchestratorProvider } from './failures-orchestrator-provider';
 import Efb from './Efb';
@@ -42,40 +43,56 @@ interface ErrorFallbackProps {
     resetErrorBoundary: (...args: Array<unknown>) => void;
 }
 
-export const ErrorFallback = ({ resetErrorBoundary }: ErrorFallbackProps) => (
-    <div className="flex justify-center items-center w-full h-screen bg-theme-body">
-        <div className="max-w-4xl">
-            <Error />
-            <div className="mt-6 space-y-12">
-                <h1 className="text-4xl font-bold">A critical error has been encountered.</h1>
+export const ErrorFallback = ({ resetErrorBoundary }: ErrorFallbackProps) => {
+    const [sessionId] = usePersistentProperty('A32NX_SENTRY_SESSION_ID');
+    const [sentryEnabled] = usePersistentProperty(SENTRY_CONSENT_KEY, SentryConsentState.Refused);
 
-                <h2 className="text-3xl">You are able to reset this tablet to recover from this error.</h2>
+    return (
+        <div className="flex justify-center items-center w-full h-screen bg-theme-body">
+            <div className="max-w-4xl">
+                <Error />
+                <div className="mt-6 space-y-12">
+                    <h1 className="text-4xl font-bold">A critical error has been encountered.</h1>
 
-                <h2 className="text-3xl leading-relaxed">
-                    You have opted into anonymous error reporting and this issue has been relayed to us. If you want immediate support, please share the following code to a member of staff in the #support channel on the FlyByWire Discord server:
-                </h2>
-                {/* TODO: Replace this with the actual sessionID */}
-                <h1 className="text-4xl font-extrabold tracking-wider text-center">ZX96CHUNGHOLBOR</h1>
+                    <h2 className="text-3xl">You are able to reset this tablet to recover from this error.</h2>
 
-                <div className="py-4 px-8 w-full rounded-md border-2 transition duration-100 border-utility-red text-theme-body bg-utility-red hover:bg-theme-body hover:text-utility-red" onClick={resetErrorBoundary}>
-                    <h2 className="font-bold text-center text-current">Reset Display</h2>
+                    {sentryEnabled === SentryConsentState.Given && (
+                        <>
+                            <h2 className="text-3xl leading-relaxed">
+                                You have opted into anonymous error reporting and this issue has been relayed to us. If you want immediate support, please share the following code to a member of staff in the #support channel on the FlyByWire Discord server:
+                            </h2>
+
+                            <h1 className="text-4xl font-extrabold tracking-wider text-center">{sessionId}</h1>
+                        </>
+                    )}
+
+                    <div className="py-4 px-8 w-full rounded-md border-2 transition duration-100 border-utility-red text-theme-body bg-utility-red hover:bg-theme-body hover:text-utility-red" onClick={resetErrorBoundary}>
+                        <h2 className="font-bold text-center text-current">Reset Display</h2>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
-const ALPHABET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const SESSION_ID_LENGTH = 14;
-const nanoid = customAlphabet(ALPHABET, SESSION_ID_LENGTH);
-const generatedSessionID = nanoid();
+const setSessionId = () => {
+    const ALPHABET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const SESSION_ID_LENGTH = 14;
+    const nanoid = customAlphabet(ALPHABET, SESSION_ID_LENGTH);
+    const generatedSessionID = nanoid();
 
-NXDataStore.set('A32NX_SENTRY_SESSION_ID', generatedSessionID);
+    NXDataStore.set('A32NX_SENTRY_SESSION_ID', generatedSessionID);
+};
+
+const setup = () => {
+    readSettingsFromPersistentStorage();
+    setSessionId();
+};
 
 if (process.env.VITE_BUILD) {
-    window.addEventListener('AceInitialized', () => readSettingsFromPersistentStorage());
+    window.addEventListener('AceInitialized', setup);
 } else {
-    readSettingsFromPersistentStorage();
+    setup();
 }
 
 render(
