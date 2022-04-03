a2b572ecfa831f37b59299f3ce870ed4c4817449
ErickSharp <titaninmc@gmail.com>
2022-04-03 07:04:38 -0500

    add ability to recover display

--- src/instruments/src/EFB/index.tsx
+++ src/instruments/src/EFB/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { customAlphabet } from 'nanoid';
 import { NXDataStore } from '@shared/persistence';
@@ -23,31 +23,43 @@ const EFBLoad = () => {
     const [, setSessionId] = usePersistentProperty('A32NX_SENTRY_SESSION_ID');
     useEffect(() => () => setSessionId(''), []);
 
+    const [err, setErr] = useState(false);
+
     return (
-        <Router>
-            <ModalProvider>
-                <Provider store={store}>
-                    <Efb />
-                </Provider>
-            </ModalProvider>
-        </Router>
+        <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => setErr(false)} resetKeys={[err]}>
+            <Router>
+                <ModalProvider>
+                    <Provider store={store}>
+                        <Efb />
+                    </Provider>
+                </ModalProvider>
+            </Router>
+        </ErrorBoundary>
     );
 };
 
-export const ErrorBoundaryMessage = () => (
+interface ErrorFallbackProps {
+    resetErrorBoundary: (...args: Array<unknown>) => void;
+}
+
+export const ErrorFallback = ({ resetErrorBoundary }: ErrorFallbackProps) => (
     <div className="flex justify-center items-center w-full h-screen bg-theme-body">
         <div className="max-w-4xl">
             <Error />
             <div className="mt-6 space-y-12">
                 <h1 className="text-4xl font-bold">A critical error has been encountered.</h1>
 
-                <h2 className="text-3xl">You must restart your flight to use this tablet.</h2>
+                <h2 className="text-3xl">You are able to reset this tablet to recover from this error.</h2>
 
                 <h2 className="text-3xl leading-relaxed">
                     You have opted into anonymous error reporting and this issue has been relayed to us. If you want immediate support, please share the following code to a member of staff in the #support channel on the FlyByWire Discord server:
                 </h2>
                 {/* TODO: Replace this with the actual sessionID */}
                 <h1 className="text-4xl font-extrabold tracking-wider text-center">ZX96CHUNGHOLBOR</h1>
+
+                <div className="py-4 px-8 w-full rounded-md border-2 transition duration-100 border-utility-red text-theme-body bg-utility-red hover:bg-theme-body hover:text-utility-red" onClick={resetErrorBoundary}>
+                    <h2 className="font-bold text-center text-current">Reset Display</h2>
+                </div>
             </div>
         </div>
     </div>
@@ -67,8 +79,6 @@ if (process.env.VITE_BUILD) {
 }
 
 render(
-    <ErrorBoundary FallbackComponent={ErrorBoundaryMessage}>
-        <FailuresOrchestratorProvider><EFBLoad /></FailuresOrchestratorProvider>
-    </ErrorBoundary>,
+    <FailuresOrchestratorProvider><EFBLoad /></FailuresOrchestratorProvider>,
     true, true,
 );
