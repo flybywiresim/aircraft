86c1bd5f691cfacd072660e30956275420f6c981
Michael Corcoran <tracer@outlook.co.nz>
2022-04-15 01:08:55 +1200

    doc: correct copyright claim

--- src/fmgc/src/guidance/vnav/CoarsePredictions.ts
+++ src/fmgc/src/guidance/vnav/CoarsePredictions.ts
@@ -1,5 +1,4 @@
-// Copyright (c) 2021-2022 FlyByWire Simulations
-// Copyright (c) 2021-2022 Synaptic Simulations
+// Copyright (c) 2022 FlyByWire Simulations
 //
 // SPDX-License-Identifier: GPL-3.0
 
