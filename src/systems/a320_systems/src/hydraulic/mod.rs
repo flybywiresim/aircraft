99774679bb757b53e06e300053dac4f6faa99bcb
Andreas Guther <andreas@guther.family>
2022-04-23 15:17:01 +0200

    fix: linter issue

--- src/systems/a320_systems/src/hydraulic/mod.rs
+++ src/systems/a320_systems/src/hydraulic/mod.rs
@@ -3290,7 +3290,7 @@ impl A320AutobrakeController {
             deceleration_governor: AutobrakeDecelerationGovernor::new(),
             target: Acceleration::new::<meter_per_second_squared>(0.),
             mode: AutobrakeMode::NONE,
-            arming_is_allowed_by_bcu: if context.is_in_flight() { true } else { false },
+            arming_is_allowed_by_bcu: context.is_in_flight(),
             left_brake_pedal_input: Ratio::new::<percent>(0.),
             right_brake_pedal_input: Ratio::new::<percent>(0.),
             ground_spoilers_are_deployed: false,
