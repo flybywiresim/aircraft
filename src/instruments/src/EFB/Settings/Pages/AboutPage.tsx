95c81485e3aea88f6d82c89ff4b2acbe379f1b2f
2hwk <15316958+2hwk@users.noreply.github.com>
2022-04-24 03:39:37 +0800

    feat: sim version in about page (#7099)
    * feat: sim version in about page
    
    * Update src/instruments/src/EFB/Settings/Pages/AboutPage.tsx
    
    Co-authored-by: Erick Torres <30361843+ErickSharp@users.noreply.github.com>

--- src/instruments/src/EFB/Settings/Pages/AboutPage.tsx
+++ src/instruments/src/EFB/Settings/Pages/AboutPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { usePersistentProperty } from '@instruments/common/persistence';
+import { usePersistentProperty, useSessionStorage } from '@instruments/common/persistence';
 import { SentryConsentState, SENTRY_CONSENT_KEY } from '../../../../../sentry-client/src/FbwAircraftSentryClient';
 import { SettingsPage } from '../Settings';
 
@@ -20,6 +20,18 @@ interface BuildInfoEntryProps {
     underline?: number;
 }
 
+interface CommunityPanelPlayerData {
+    bCanSignOut: boolean;
+    bDisable: boolean;
+    sAvatar: string;
+    sBuildVersion: string;
+    sMoney: string;
+    sCurrency: string;
+    sName: string;
+    sRichPresence: string;
+    sStatus: string;
+}
+
 const SPACE_BETWEEN = 28;
 
 const BuildInfoEntry = ({ title, value, underline = 0 }: BuildInfoEntryProps) => {
@@ -30,7 +42,7 @@ const BuildInfoEntry = ({ title, value, underline = 0 }: BuildInfoEntryProps) =>
         <div className="flex flex-row mt-2 font-mono">
             <p>{title + '\u00A0'.repeat(Math.abs(SPACE_BETWEEN - title.length))}</p>
             <p className="ml-4">
-                <span className="text-theme-highlight underline">{first}</span>
+                <span className="underline text-theme-highlight">{first}</span>
                 {last}
             </p>
         </div>
@@ -40,9 +52,17 @@ const BuildInfoEntry = ({ title, value, underline = 0 }: BuildInfoEntryProps) =>
 export const AboutPage = () => {
     const [buildInfo, setBuildInfo] = useState<BuildInfo | undefined>(undefined);
     const [sessionId] = usePersistentProperty('A32NX_SENTRY_SESSION_ID');
+    const [version, setVersion] = useSessionStorage('SIM_VERSION', '');
     const [sentryEnabled] = usePersistentProperty(SENTRY_CONSENT_KEY, SentryConsentState.Refused);
+    const [listener] = useState(RegisterViewListener('JS_LISTENER_COMMUNITY', undefined, false));
+
+    const onSetPlayerData = (data: CommunityPanelPlayerData) => {
+        setVersion(data.sBuildVersion);
+    };
 
     useEffect(() => {
+        listener.on('SetGamercardInfo', onSetPlayerData, null);
+        
         fetch('/VFS/build_info.json').then((response) => response.json()).then((json) => setBuildInfo({
             built: json.built,
             ref: json.ref,
@@ -51,6 +71,12 @@ export const AboutPage = () => {
             eventName: json.event_name,
         }));
     }, []);
+
+    useEffect(() => {
+        if (version) {
+            listener.unregister();
+        }
+    }, [version]);
 
     return (
         <SettingsPage name="About">
@@ -78,6 +104,7 @@ export const AboutPage = () => {
                 <div className="mt-16">
                     <h1 className="font-bold">Build Info</h1>
                     <div className="mt-4">
+                        <BuildInfoEntry title="Sim Version" value={version} />
                         <BuildInfoEntry title="Built" value={buildInfo?.built} />
                         <BuildInfoEntry title="Ref" value={buildInfo?.ref} />
                         <BuildInfoEntry title="SHA" value={buildInfo?.sha} underline={8} />
