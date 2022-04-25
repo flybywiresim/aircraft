b4ba109382b508bdba36b1aa1f647712e6e63c43
Frank Kopp <frank@familie-kopp.de>
2022-04-26 00:39:54 +0200

    Added additional keyboard layouts

--- src/instruments/src/EFB/UtilComponents/KeyboardWrapper.tsx
+++ src/instruments/src/EFB/UtilComponents/KeyboardWrapper.tsx
@@ -14,42 +14,27 @@ interface KeyboardWrapperProps {
 }
 
 export const keyboardLayoutOptions = [
-    {
-        name: 'english',
-        alias: 'English',
-    },
-    {
-        name: 'spanish',
-        alias: 'Español',
-    },
-    {
-        name: 'german',
-        alias: 'Deutsch',
-    },
-    {
-        name: 'french',
-        alias: 'Français',
-    },
-    {
-        name: 'arabic',
-        alias: 'العربية',
-    },
-    {
-        name: 'italian',
-        alias: 'Italiano',
-    },
-    {
-        name: 'korean',
-        alias: '한국어',
-    },
-    {
-        name: 'japanese',
-        alias: '日本語',
-    },
-    {
-        name: 'thai',
-        alias: 'ไทย',
-    },
+    // Standard
+    { name: 'english', alias: 'English' },
+    // Alternatives
+    { name: 'arabic', alias: 'العربية' },
+    { name: 'chinese', alias: '中文键盘' },
+    { name: 'czech', alias: 'Čeština' },
+    { name: 'french', alias: 'Français' },
+    { name: 'german', alias: 'Deutsch' },
+    { name: 'greek', alias: 'Ελληνικά' },
+    { name: 'hebrew', alias: 'עִבְרִית' },
+    { name: 'hindi', alias: 'हिंदी' },
+    { name: 'italian', alias: 'Italiano' },
+    { name: 'japanese', alias: '日本語' },
+    { name: 'korean', alias: '한국어' },
+    { name: 'norwegian', alias: 'Norsk' },
+    { name: 'polish', alias: 'Polski' },
+    { name: 'russian', alias: 'Русский' },
+    { name: 'spanish', alias: 'Español' },
+    { name: 'swedish', alias: 'Svenska' },
+    { name: 'thai', alias: 'ไทย' },
+    { name: 'turkish', alias: 'Türkçe' },
 ];
 
 export const KeyboardWrapper = ({ onChangeAll, keyboardRef, setOpen, blurInput, onKeyDown }: KeyboardWrapperProps) => {
