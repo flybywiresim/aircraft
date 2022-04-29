2b93ae8af812d9dfbdb0d4b6048b357d6741f291
Michael Corcoran <tracer@outlook.co.nz>
2022-04-30 00:11:47 +1200

    fix(fmgc): use current bank for active df trans

--- src/fmgc/src/guidance/lnav/transitions/DirectToFixTransition.ts
+++ src/fmgc/src/guidance/lnav/transitions/DirectToFixTransition.ts
@@ -99,7 +99,7 @@ export class DirectToFixTransition extends Transition {
         return this.computedPath;
     }
 
-    recomputeWithParameters(_isActive: boolean, tas: Knots, gs: Knots, _ppos: Coordinates, _trueTrack: DegreesTrue) {
+    recomputeWithParameters(isActive: boolean, tas: Knots, gs: Knots, _ppos: Coordinates, _trueTrack: DegreesTrue) {
         if (this.isFrozen) {
             return;
         }
@@ -123,7 +123,7 @@ export class DirectToFixTransition extends Transition {
         const turnDirectionSign = trackChange > 0 ? 1 : -1;
         const turnDirection = turnDirectionSign > 0 ? TurnDirection.Right : TurnDirection.Left;
 
-        const currentRollAngle = 0; // TODO: if active leg, current aircraft roll, else 0
+        const currentRollAngle = isActive ? -SimVar.GetSimVarValue('PLANE BANK DEGREES', 'degrees') : 0;
         const rollAngleChange = Math.abs(turnDirectionSign * maxBank(tas, true) - currentRollAngle);
         const rollAnticipationDistance = Geometry.getRollAnticipationDistance(gs, 0, rollAngleChange);
 
