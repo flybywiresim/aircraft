2142bcfca658857d21f694aa76ce12c538073224
Andreas Guther <andreas@guther.family>
2022-04-30 22:54:29 +0200

    fix: typo

--- flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/A32NX_TipsManager.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/A32NX_TipsManager.js
@@ -47,7 +47,7 @@ class A32NX_TipsManager {
         const assistenceLandingEnabled = SimVar.GetSimVarValue("ASSISTANCE LANDING ENABLED", "Bool");
         const assistenceAutotrimActive = SimVar.GetSimVarValue("AI AUTOTRIM ACTIVE", "Bool");
         if (assistenceTakeOffEnabled || assistenceLandingEnabled || assistenceAutotrimActive) {
-            this.notif.showNotification({message: "Ensure you have turned off the following assistance functions:\n- ASSISTET TAKEOFF\n- ASSISTET LANDING\n- AI AUTO-TRIM\n\nThey cause serious incompatibility!", timeout: 20000});
+            this.notif.showNotification({message: "Ensure you have turned off the following assistance functions:\n- ASSISTED TAKEOFF\n- ASSISTED LANDING\n- AI AUTO-TRIM\n\nThey cause serious incompatibility!", timeout: 20000});
         }
     }
 
