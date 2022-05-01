1102ca695ce5cc8a0cc588c6a7528d87a5cc6708
ErickSharp <titaninmc@gmail.com>
2022-04-30 20:03:49 -0500

    add map rotation

--- src/instruments/src/EFB/UtilComponents/BingMap.tsx
+++ src/instruments/src/EFB/UtilComponents/BingMap.tsx
@@ -1,39 +1,41 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 interface BingMapProps {
     configFolder: string;
     mapId: string;
     centerLla: { lat: number; long: number };
     range?: number;
+    rotation?: number;
 }
 
 const RANGE_CONSTANT = 1852;
 const DEFAULT_RANGE = 80;
 
-export const BingMap: React.FC<BingMapProps> = ({ configFolder, mapId, range = DEFAULT_RANGE, centerLla }) => {
+export const BingMap: React.FC<BingMapProps> = ({ configFolder, mapId, range = DEFAULT_RANGE, centerLla, rotation = 0 }) => {
     const mapRef = useRef<BingMapElement>();
+    const mapContainerRef = useRef<HTMLDivElement>(null);
+    const [mapSize, setMapSize] = useState(100);
 
     useEffect(() => {
-        if (mapRef.current) {
-            const svgMapConfig = new SvgMapConfig();
+        const svgMapConfig = new SvgMapConfig();
 
-            svgMapConfig.load(configFolder, () => {
-                console.log(`[ReactBingMap (${mapId})] NetBingMap config loaded`);
+        svgMapConfig.load(configFolder, () => {
+            console.log(`[ReactBingMap (${mapId})] NetBingMap config loaded`);
+            if (!mapRef.current) return;
 
-                svgMapConfig.generateBingMap(mapRef.current);
-                mapRef.current.setConfig(0);
+            svgMapConfig.generateBingMap(mapRef.current);
+            mapRef.current.setConfig(0);
 
-                mapRef.current.setBingId(mapId);
-                mapRef.current.setVisible(true);
+            mapRef.current.setBingId(mapId);
+            mapRef.current.setVisible(true);
 
-                const lla = new LatLongAlt(centerLla.lat, centerLla.long);
-                const radius = range * RANGE_CONSTANT;
+            const lla = new LatLongAlt(centerLla.lat, centerLla.long);
+            const radius = range * RANGE_CONSTANT;
 
-                mapRef.current.setParams({ lla, radius });
+            mapRef.current.setParams({ lla, radius });
 
-                console.log(`[ReactBingMap (${mapId})] NetBingMap initialized and configured with config id # ${mapRef.current.m_configId} out of ${mapRef.current.m_configs.length} configs`);
-            });
-        }
+            console.log(`[ReactBingMap (${mapId})] NetBingMap initialized and configured with config id # ${mapRef.current.m_configId} out of ${mapRef.current.m_configs.length} configs`);
+        });
     }, [mapRef]);
 
     useEffect(() => {
@@ -45,10 +47,29 @@ export const BingMap: React.FC<BingMapProps> = ({ configFolder, mapId, range = D
         }
     }, [range, centerLla]);
 
+    useEffect(() => {
+        if (mapContainerRef.current) {
+            const newSize = mapContainerRef.current.clientWidth * Math.sqrt(2);
+            setMapSize(newSize);
+        }
+    }, [mapContainerRef.current]);
+
     return (
-        <>
-            {/* @ts-ignore */}
-            <bing-map ref={mapRef} />
-        </>
+        <div ref={mapContainerRef} style={{ position: 'relative', width: '100%', height: '100%' }}>
+            <div
+                style={{
+                    transform: `rotateZ(${rotation}deg) translateX(-50%) translateY(-50%)`,
+                    transformOrigin: '0 0',
+                    width: mapSize,
+                    height: mapSize,
+                    position: 'absolute',
+                    left: '50%',
+                    top: '50%',
+                }}
+            >
+                {/* @ts-ignore */}
+                <bing-map ref={mapRef} />
+            </div>
+        </div>
     );
 };
