9246dc4da985bc0811bea35d7d9a73c0900edf09
FoxtrotSierra6829 <github@florian-scheuner.de>
2022-05-03 20:15:37 +0200

    fix: missing space

--- scripts/install-source.js
+++ scripts/install-source.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 
 const source = process.env['BUILD_DIR_NAME'] ? 'external/a32nx/' + process.env['BUILD_DIR_NAME'] : 'external/a32nx';
-console.log('installManifest source is:' + source);
+console.log('installManifest source is: ' + source);
 
 const installManifest = fs.readJSONSync('./flybywire-aircraft-a320-neo/install.json');
 installManifest.source = source;
