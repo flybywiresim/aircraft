df7ebb12502a38a4ab0d61380809dd00c2df06dd
Florian Scheuner <github@florian-scheuner.de>
2022-05-08 20:00:12 +0200

    ci: recognise external downloads in installer (#7062)
    * ci: recognise external downloads in installer
    
    * ci: zip artifacts only once
    
    * feat: add source to installManifest to select the right version
    
    * fix: missing space
    
    * fix: keep top folder in PR artifact
    
    Co-authored-by: Andreas Guther <andreas@guther.family>

---
+++ scripts/install-source.js
@@ -0,0 +1,8 @@
+const fs = require('fs-extra');
+
+const source = process.env['BUILD_DIR_NAME'] ? 'external/a32nx/' + process.env['BUILD_DIR_NAME'] : 'external/a32nx';
+console.log('installManifest source is: ' + source);
+
+const installManifest = fs.readJSONSync('./flybywire-aircraft-a320-neo/install.json');
+installManifest.source = source;
+fs.writeJSONSync('./flybywire-aircraft-a320-neo/install.json', installManifest);
