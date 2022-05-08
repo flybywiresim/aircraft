ae6fad948ac1e8f944a0841972f5c10ea3656b8b
Benjamin Dupont <benjozorkfr@gmail.com>
2022-05-08 00:52:40 -0400

    add navaid modes to ECP bus publisher

--- src/instruments/src/MsfsAvionicsCommon/providers/EcpBusSimVarPublisher.ts
+++ src/instruments/src/MsfsAvionicsCommon/providers/EcpBusSimVarPublisher.ts
@@ -1,11 +1,13 @@
 import { EventBus, SimVarValueType, Subject } from 'msfssdk';
-import { EfisNdMode, EfisOption } from '@shared/NavigationDisplay';
+import { EfisNdMode, EfisOption, NavAidMode } from '@shared/NavigationDisplay';
 import { SwitchableSimVarProvider } from './SwitchableProvider';
 
 export interface EcpSimVars {
     ndRangeSetting: number,
     ndMode: EfisNdMode,
     option: EfisOption,
+    navaidMode1: NavAidMode,
+    navaidMode2: NavAidMode,
 }
 
 export class EcpBusSimVarPublisher extends SwitchableSimVarProvider<EcpSimVars, 'L' | 'R'> {
@@ -17,6 +19,8 @@ export class EcpBusSimVarPublisher extends SwitchableSimVarProvider<EcpSimVars, 
             ['ndRangeSetting', { name: (side) => `L:A32NX_EFIS_${side}_ND_RANGE`, type: SimVarValueType.Enum }],
             ['ndMode', { name: (side) => `L:A32NX_EFIS_${side}_ND_MODE`, type: SimVarValueType.Enum }],
             ['option', { name: (side) => `L:A32NX_EFIS_${side}_OPTION`, type: SimVarValueType.Enum }],
+            ['navaidMode1', { name: (side) => `L:A32NX_EFIS_${side}_NAVAID_1_MODE`, type: SimVarValueType.Enum }],
+            ['navaidMode2', { name: (side) => `L:A32NX_EFIS_${side}_NAVAID_2_MODE`, type: SimVarValueType.Enum }],
         ]), stateSubject, bus);
     }
 }
