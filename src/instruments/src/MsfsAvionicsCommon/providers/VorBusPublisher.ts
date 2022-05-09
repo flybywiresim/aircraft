60543f1e46ab303662ff2b8a21249a3ae6789d9d
Benjamin Dupont <benjozorkfr@gmail.com>
2022-05-09 19:26:55 -0400

    add relative bearing to vor busses

--- src/instruments/src/MsfsAvionicsCommon/providers/VorBusPublisher.ts
+++ src/instruments/src/MsfsAvionicsCommon/providers/VorBusPublisher.ts
@@ -6,6 +6,7 @@ export interface VorSimVars {
     nav1Frequency: number,
     nav1HasDme: boolean,
     nav1DmeDistance: NauticalMiles,
+    nav1RelativeBearing: Degrees,
     nav1Available: boolean,
     nav1TuningMode: TuningMode,
 
@@ -13,6 +14,7 @@ export interface VorSimVars {
     nav2Frequency: number,
     nav2HasDme: boolean,
     nav2DmeDistance: NauticalMiles,
+    nav2RelativeBearing: Degrees,
     nav2Available: boolean,
     nav2TuningMode: TuningMode,
 }
@@ -24,6 +26,7 @@ export class VorBusPublisher extends SimVarPublisher<VorSimVars> {
             ['nav1Frequency', { name: 'NAV ACTIVE FREQUENCY:1', type: SimVarValueType.MHz }],
             ['nav1HasDme', { name: 'NAV HAS DME:1', type: SimVarValueType.Bool }],
             ['nav1DmeDistance', { name: 'NAV DME:1', type: SimVarValueType.NM }],
+            ['nav1RelativeBearing', { name: 'NAV RELATIVE BEARING TO STATION:1', type: SimVarValueType.Degree }],
             ['nav1Available', { name: 'NAV HAS NAV:1', type: SimVarValueType.Bool }],
             ['nav1TuningMode', { name: 'L:A32NX_FMGC_RADIONAV_1_TUNING_MODE', type: SimVarValueType.Enum }],
 
@@ -31,6 +34,7 @@ export class VorBusPublisher extends SimVarPublisher<VorSimVars> {
             ['nav2Frequency', { name: 'NAV ACTIVE FREQUENCY:2', type: SimVarValueType.MHz }],
             ['nav2HasDme', { name: 'NAV HAS DME:2', type: SimVarValueType.Bool }],
             ['nav2DmeDistance', { name: 'NAV DME:2', type: SimVarValueType.NM }],
+            ['nav2RelativeBearing', { name: 'NAV RELATIVE BEARING TO STATION:2', type: SimVarValueType.Degree }],
             ['nav2Available', { name: 'NAV HAS NAV:2', type: SimVarValueType.Bool }],
             ['nav2TuningMode', { name: 'L:A32NX_FMGC_RADIONAV_2_TUNING_MODE', type: SimVarValueType.Enum }],
         ]), bus);
