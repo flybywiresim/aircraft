676deb435e37b0358e72e786b65e35a1263c72af
Frank Kopp <frank@familie-kopp.de>
2022-05-12 00:01:58 +0200

    Fixed Failure page after new chapters have been added and page broke

--- src/shared/src/ata.ts
+++ src/shared/src/ata.ts
@@ -96,6 +96,7 @@ export const AtaChaptersDescription = Object.freeze({
     24: 'All things related to the electrical system. The electrical system supplies power from the engines, APU, batteries, or emergency generator to all cockpit instruments.',
     29: 'The hydraulic system connects to the flight controls, flaps and landing gear to provide pressure to these surfaces. Failing these can cause loss of control over some flight surfaces.',
     31: 'The cockpit displays give critical flight information to the pilots. In a failure where displays are lost, the pilots must deal with a lack of flight data given to them.',
+    32: 'The landing gear components are responsible for supporting and steering the aircraft on the ground, and make it possible to retract and store the landing gear in flight. Includes the functioning and maintenance aspects of the landing gear doors.',
     34: 'The navigation systems provide data about the position, speed, heading, and altitude of the aircraft. Failures in a system such as the ADIRS can cause a loss of data sent to instrumentation.',
 });
 
