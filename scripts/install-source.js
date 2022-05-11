75421ef7185ae57b5848c6b058ee9f7a2c4f7409
Florian Scheuner <github@florian-scheuner.de>
2022-05-11 21:00:10 +0200

    fix(ci): installer version detection (#7184)

--- scripts/install-source.js
+++ scripts/install-source.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const fs = require('fs-extra');
 
-const source = process.env['BUILD_DIR_NAME'] ? 'external/a32nx/' + process.env['BUILD_DIR_NAME'] : 'external/a32nx';
+const source = process.env.BUILD_DIR_NAME ? 'external/a32nx/' + process.env.BUILD_DIR_NAME : 'external/a32nx';
 console.log('installManifest source is: ' + source);
 
 const installManifest = fs.readJSONSync('./flybywire-aircraft-a320-neo/install.json');
