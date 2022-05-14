c8377b31251d741f23db7897c530d004d112f44b
Frank Kopp <frank@familie-kopp.de>
2022-05-14 11:32:21 +0200

    Renamed console output prefix to FLYPAD_BACKEND

--- src/flypad-backend/src/Pushback/Pushback.cpp
+++ src/flypad-backend/src/Pushback/Pushback.cpp
@@ -1,12 +1,10 @@
 // Copyright (c) 2022 FlyByWire Simulations
 // SPDX-License-Identifier: GPL-3.0
 
-//
-// Created by frank on 13.05.2022.
-//
+#include <iostream>
 
-#include "Pushback.h"
 #include "InertialDampener.h"
+#include "Pushback.h"
 
 Pushback::Pushback() {
   inertialDampenerPtr = std::make_unique<InertialDampener>();
@@ -16,7 +14,7 @@ Pushback::~Pushback() = default;
 
 void Pushback::initialize() {
   isInitialized = true;
-  std::cout << "PRESETS: LightPresets initialized" << std::endl;
+  std::cout << "FLYPAD_BACKEND: Pushback initialized" << std::endl;
 }
 
 void Pushback::onUpdate(double deltaTime) {
