5f0d8c5bf0a36a5480bfabd26b60782947764c31
Siddharth S <sidnov@yahoo.co.in>
2022-05-29 12:56:44 +0530

    revert: conflicting changes

--- src/instruments/src/EFB/Dispatch/Pages/FuelPage.tsx
+++ src/instruments/src/EFB/Dispatch/Pages/FuelPage.tsx
@@ -30,7 +30,7 @@ export const FuelPage = () => {
         if (usingMetrics === '1') {
             return 1;
         }
-        return 2.204625;
+        return 2.204617615;
     };
 
     const [galToKg] = useSimVar('FUEL WEIGHT PER GALLON', 'kilograms', 1_000);
