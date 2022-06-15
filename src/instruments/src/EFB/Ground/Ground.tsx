4c934c8266c6fdea362259b59989709f1f81dc36
Frank Kopp <frank@familie-kopp.de>
2022-06-15 23:09:16 +0200

    feat: improving flypadOS 3 Ground Service page (#7229)

--- src/instruments/src/EFB/Ground/Ground.tsx
+++ src/instruments/src/EFB/Ground/Ground.tsx
@@ -5,45 +5,6 @@ import { Navbar } from '../UtilComponents/Navbar';
 import { ServicesPage } from './Pages/ServicesPage';
 import { PushbackPage } from './Pages/PushbackPage';
 import { FuelPage } from './Pages/FuelPage';
-import { store, RootState } from '../Store/store';
-
-export interface StatefulButton {
-    id: string,
-    state: string,
-    callBack,
-    value: number,
-}
-
-const BUTTON_STYLE_INACTIVE = ' hover:bg-theme-highlight text-theme-text hover:text-theme-secondary transition duration-200 disabled:bg-grey-600';
-const BUTTON_STYLE_ACTIVE = ' text-white bg-green-600 border-green-600';
-
-/**
- * Applies highlighting of an activated service based on SimVars
- * This ensures the displayed state is in sync with the active services
- */
-export const applySelectedWithSync = (className: string, id: string, gameSync: number, disabledId?: string) => {
-    const activeButtons = (store.getState() as RootState).buttons.activeButtons;
-    const disabledButtons = (store.getState() as RootState).buttons.disabledButtons;
-
-    const index = activeButtons.map((b: StatefulButton) => b.id).indexOf(id);
-    const disabledIndex = disabledButtons.indexOf(disabledId ?? '');
-
-    if (gameSync > 0.5 && (index !== -1 || disabledIndex !== -1)) {
-        return `${className} ${BUTTON_STYLE_ACTIVE}`;
-    }
-    return className + (activeButtons.map((b: StatefulButton) => b.id).includes(id) ? ' text-white bg-gray-600'
-        : BUTTON_STYLE_INACTIVE);
-};
-
-export const applySelected = (className: string, id?: string) => {
-    const activeButtons = (store.getState() as RootState).buttons.activeButtons;
-
-    if (id) {
-        return className + (activeButtons.map((b: StatefulButton) => b.id).includes(id) ? BUTTON_STYLE_ACTIVE
-            : BUTTON_STYLE_INACTIVE);
-    }
-    return className;
-};
 
 export const Ground = () => {
     const tabs: PageLink[] = [
