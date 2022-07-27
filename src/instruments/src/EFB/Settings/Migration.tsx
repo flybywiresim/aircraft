850fd7fb11607c1f2291ce283c05412165f501ec
Alex Mcbean <alex.mcbean@sainsburys.co.uk>
2022-07-27 16:58:01 +0100

    refactoring of migration

--- src/instruments/src/EFB/Settings/Migration.tsx
+++ src/instruments/src/EFB/Settings/Migration.tsx
@@ -1,16 +1,27 @@
 import { NXDataStore } from '@shared/persistence';
 
-type SimVar = [name: string, defaultValue: string]
+type SimVarProp = {name: string, defaultValue: string}
+type migrationSet = [oldSimvar: SimVarProp, newSimvar: string]
 
-const migrateSetting = (oldSimvar: Simvar, newSimvar: string) => {
-    NXDataStore.set(newSimvar, NXDataStore.get(oldSimvar[0], oldSimvar[1]));
+const migrateSetting = (oldSimvar: SimVarProp, newSimvar: string) => {
+    NXDataStore.set(newSimvar, NXDataStore.get(oldSimvar.name, oldSimvar.defaultValue));
 };
 
-const settingsToMigrate: Map<SimVar, string> = new Map([
-    [['CONFIG_EXTERNAL_MCDU_PORT', '8380'], 'CONFIG_SIMBRIDGE_PORT'],
-    [['CONFIG_EXTERNAL_MCDU_SERVER_ENABLED', 'AUTO ON'], 'CONFIG_SIMBRIDGE_ENABLED'],
+// Object is set so that a list of simvars will be migrated when the migrated flag is false.
+const settingsToMigrate: Map<string, migrationSet[]> = new Map([
+    ['SIMBRIDGE_MIGRATED', [
+        [{ name: 'CONFIG_EXTERNAL_MCDU_PORT', defaultValue: '8380' }, 'CONFIG_SIMBRIDGE_PORT'],
+        [{ name: 'CONFIG_EXTERNAL_MCDU_SERVER_ENABLED', defaultValue: 'AUTO ON' }, 'CONFIG_SIMBRIDGE_ENABLED'],
+    ]],
 ]);
 
 export function migrateSettings() {
-    settingsToMigrate.forEach((newSimvar, oldSimvar) => migrateSetting(oldSimvar, newSimvar));
+    settingsToMigrate.forEach((migrations, migrationCheck) => {
+        if (NXDataStore.get(migrationCheck, 'false') === 'false') {
+            migrations.forEach((value) => {
+                migrateSetting(value[0], value[1]);
+            });
+            NXDataStore.set(migrationCheck, 'true');
+        }
+    });
 }
