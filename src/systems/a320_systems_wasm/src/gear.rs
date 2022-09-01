71699f0bd7860e1e61430193d842f37987622e9d
davydecorps <38904654+crocket63@users.noreply.github.com>
2022-09-01 18:06:58 +0200

    freeze plane position

--- src/systems/a320_systems_wasm/src/gear.rs
+++ src/systems/a320_systems_wasm/src/gear.rs
@@ -62,6 +62,18 @@ pub(super) fn gear(builder: &mut MsfsAspectBuilder) -> Result<(), Box<dyn Error>
         right_position: 1.,
     }));
 
+    // STATIC PLANE POSITION
+    builder.variables_to_object(Box::new(PlaneStaticPosition {
+        // longitude: -0.027933959772,
+        // latitude: 0.82302885833,
+        accx: 0.,
+        accy: 0.,
+        accz: 0.,
+        velx: 0.,
+        vely: 0.,
+        velz: 0.,
+    }));
+
     Ok(())
 }
 
@@ -93,6 +105,66 @@ impl VariablesToObject for GearPosition {
         self.nose_position = values[0] / 100.;
         self.left_position = values[1] / 100.;
         self.right_position = values[2] / 100.;
+
+        ObjectWrite::default()
+    }
+
+    set_data_on_sim_object!();
+}
+
+#[sim_connect::data_definition]
+struct PlaneStaticPosition {
+    // #[name = "PLANE LONGITUDE"]
+    // #[unit = "Radians"]
+    // longitude: f64,
+
+    // #[name = "PLANE LATITUDE"]
+    // #[unit = "Radians"]
+    // latitude: f64,
+    #[name = "ACCELERATION WORLD X"]
+    #[unit = "Feet per second squared"]
+    accx: f64,
+
+    #[name = "ACCELERATION WORLD Y"]
+    #[unit = "Feet per second squared"]
+    accy: f64,
+
+    #[name = "ACCELERATION WORLD Z"]
+    #[unit = "Feet per second squared"]
+    accz: f64,
+
+    #[name = "VELOCITY BODY X"]
+    #[unit = "Feet per second"]
+    velx: f64,
+
+    #[name = "VELOCITY BODY Y"]
+    #[unit = "Feet per second"]
+    vely: f64,
+
+    #[name = "VELOCITY BODY Z"]
+    #[unit = "Feet per second"]
+    velz: f64,
+}
+
+impl VariablesToObject for PlaneStaticPosition {
+    fn variables(&self) -> Vec<Variable> {
+        vec![
+            Variable::named("GEARSWING_LATITUDE"),
+            Variable::named("GEARSWING_LONGITUDE"),
+        ]
+    }
+
+    fn write(&mut self, values: Vec<f64>) -> ObjectWrite {
+        // self.latitude = values[0];
+        // self.longitude = values[1];
+
+        self.accx = 0.;
+        self.accy = 0.;
+        self.accz = 0.;
+
+        self.velx = 0.;
+        self.vely = 0.;
+        self.velz = 0.;
 
         ObjectWrite::default()
     }
