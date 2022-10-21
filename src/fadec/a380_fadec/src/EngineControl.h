6f7140ae34330551bebcf1c78c8fb00020ed71cd
Taz5150 <roldan5150@gmail.com>
2022-10-21 22:48:18 +0200

    fix: several code warnings in EngineControl

--- src/fadec/a380_fadec/src/EngineControl.h
+++ src/fadec/a380_fadec/src/EngineControl.h
@@ -165,7 +165,6 @@ class EngineControl {
       engineState = engineState + 10;
       simPaused = true;
     } else if (deltaTimeDiff == 0 && engineState >= 10) {
-      engineState = engineState;
       simPaused = true;
     } else {
       simPaused = false;
