29bda40bf0a5f24915db8064cecaa7753e6331cf
Taz5150 <roldan5150@gmail.com>
2022-10-21 22:59:06 +0200

    Update build.sh

--- src/fadec/build.sh
+++ src/fadec/build.sh
@@ -3,8 +3,7 @@
 # get directory of this script relative to root
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-OUTPUT_A32NX="${DIR}/../../flybywire-aircraft-a320-neo/SimObjects/AirPlanes/FlyByWire_A320_NEO/panel/fadec.wasm"
-OUTPUT_A380="${DIR}/../../flybywire-aircraft-a380-841/SimObjects/AirPlanes/fbw_a380X/panel/fadec.wasm"
+OUTPUT="${DIR}/../../flybywire-aircraft-a320-neo/SimObjects/AirPlanes/FlyByWire_A320_NEO/panel/fadec.wasm"
 
 if [ "$1" == "--debug" ]; then
   CLANG_ARGS="-g"
@@ -62,52 +61,5 @@ wasm-ld \
   -O3 --lto-O3 \
   -lc++ -lc++abi \
   ${DIR}/obj/*.o \
-  -o $OUTPUT_A32NX
+  -o $OUTPUT
 
-pushd "${DIR}/obj"
-
-# compile c++ code for the A380
-clang++ \
-  -c \
-  ${CLANG_ARGS} \
-  -Wno-unused-command-line-argument \
-  -Wno-ignored-attributes \
-  -Wno-macro-redefined \
-  --sysroot "${MSFS_SDK}/WASM/wasi-sysroot" \
-  -target wasm32-unknown-wasi \
-  -flto \
-  -D_MSFS_WASM=1 \
-  -D__wasi__ \
-  -D_LIBCPP_HAS_NO_THREADS \
-  -D_WINDLL \
-  -D_MBCS \
-  -mthread-model single \
-  -fno-exceptions \
-  -fms-extensions \
-  -fvisibility=hidden \
-  -O3 \
-  -I "${MSFS_SDK}/WASM/include" \
-  -I "${MSFS_SDK}/SimConnect SDK/include" \
-  -I "${DIR}/../fbw/src/inih" \
-  "${DIR}/a380_fadec/src/FadecGauge.cpp"
-
-# restore directory
-popd
-
-wasm-ld \
-  --no-entry \
-  --allow-undefined \
-  -L "${MSFS_SDK}/WASM/wasi-sysroot/lib/wasm32-wasi" \
-  -lc "${MSFS_SDK}/WASM/wasi-sysroot/lib/wasm32-wasi/libclang_rt.builtins-wasm32.a" \
-  --export __wasm_call_ctors \
-  ${WASMLD_ARGS} \
-  --export-dynamic \
-  --export malloc \
-  --export free \
-  --export __wasm_call_ctors \
-  --export-table \
-  --gc-sections \
-  -O3 --lto-O3 \
-  -lc++ -lc++abi \
-  ${DIR}/obj/*.o \
-  -o $OUTPUT_A380
