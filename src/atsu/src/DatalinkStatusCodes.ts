b8e3f1b371e6148aa5f169534b81e678883c4142
peter skaarup <peterskaarup@gmail.com>
2022-12-17 21:33:01 +0100

    Added datalink status code

---
+++ src/atsu/src/DatalinkStatusCodes.ts
@@ -0,0 +1,13 @@
+//  Copyright (c) 2022 FlyByWire Simulations
+//  SPDX-License-Identifier: GPL-3.0
+
+export enum DatalinkStatusCodes {
+    DlkAvail,
+    DlkNotAvail,
+    Inop,
+    NotInstalled,
+    AtcAoc,
+    AtcOnly,
+    AocOnly
+
+}
