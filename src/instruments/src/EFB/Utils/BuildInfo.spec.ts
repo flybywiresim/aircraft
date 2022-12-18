8cb209890694bf26126c44d38f00a5f63db8878d
Frank Kopp <frank@familie-kopp.de>
2022-12-18 17:28:57 +0100

    Added Util class for retrieving and handling of build_info and versions.

---
+++ src/instruments/src/EFB/Utils/BuildInfo.spec.ts
@@ -0,0 +1,59 @@
+// Copyright (c) 2022 FlyByWire Simulations
+// SPDX-License-Identifier: GPL-3.0
+
+import { BuildInfo } from './BuildInfo';
+
+describe('BuildInfo.getVersionInfo()', () => {
+    it('should throw error on malformed version info strings.', () => {
+        expect(() => BuildInfo.getVersionInfo('v0.0.0master.00000000')).toThrow();
+        expect(() => BuildInfo.getVersionInfo('v0.0.0-master.012345678')).toThrow();
+        expect(() => BuildInfo.getVersionInfo('v0.0-master.01234567')).toThrow();
+        expect(() => BuildInfo.getVersionInfo('version0.0.0-master.01234567')).toThrow();
+    });
+    it('should not throw error on well formed version info strings.', () => {
+        expect(() => BuildInfo.getVersionInfo('v0.0.0-master.00000000')).not.toThrow();
+        expect(() => BuildInfo.getVersionInfo('0.10.0-version-selfcheck.9e483278')).not.toThrow();
+        expect(() => BuildInfo.getVersionInfo('v0.10.0-version-selfcheck.9e483278')).not.toThrow();
+    });
+    it('should return correct version info.', () => {
+        expect(BuildInfo.getVersionInfo('v0.0.0-master.00000000')).toStrictEqual({
+            version: '0.0.0',
+            major: 0,
+            minor: 0,
+            patch: 0,
+            branch: 'master',
+            commit: '00000000',
+        });
+        expect(BuildInfo.getVersionInfo('v0.10.0-version-selfcheck.9e483278')).toStrictEqual({
+            version: '0.10.0',
+            major: 0,
+            minor: 10,
+            patch: 0,
+            branch: 'version-selfcheck',
+            commit: '9e483278',
+        });
+    });
+});
+
+describe('BuildInfo.BuildInfo.versionCompare()', () => {
+    it('should not throw error on well formed version strings.', () => {
+        expect(() => BuildInfo.versionCompare('v0.0.0', 'v0.0.0')).not.toThrow();
+        expect(() => BuildInfo.versionCompare('0.0.0', 'v1.0.0')).not.toThrow();
+        expect(() => BuildInfo.versionCompare('v0.1.0', '0.0.1')).not.toThrow();
+    });
+    it('should throw error when malformed version strings are provided.', () => {
+        expect(() => BuildInfo.versionCompare('', '')).toThrow();
+        expect(() => BuildInfo.versionCompare('v1.0.0', '')).toThrow();
+        expect(() => BuildInfo.versionCompare('', 'v1.0.0')).toThrow();
+        expect(() => BuildInfo.versionCompare('v.1.0.0', 'v1.0.0')).toThrow();
+    });
+    it('should return correct version comparison.', () => {
+        expect(BuildInfo.versionCompare('v0.0.0', 'v0.0.0')).toBe(0);
+        expect(BuildInfo.versionCompare('v0.0.0', 'v0.0.1')).toBe(-1);
+        expect(BuildInfo.versionCompare('v0.0.1', 'v0.0.0')).toBe(1);
+        expect(BuildInfo.versionCompare('v0.0.0', 'v0.1.0')).toBe(-1);
+        expect(BuildInfo.versionCompare('v0.1.0', 'v0.0.0')).toBe(1);
+        expect(BuildInfo.versionCompare('v0.0.0', 'v1.0.0')).toBe(-1);
+        expect(BuildInfo.versionCompare('v1.0.0', 'v0.0.0')).toBe(1);
+    });
+});
---
+++ src/instruments/src/EFB/Utils/BuildInfo.ts
@@ -0,0 +1,132 @@
+// Copyright (c) 2022 FlyByWire Simulations
+// SPDX-License-Identifier: GPL-3.0
+
+/**
+ * Contains the {type}_build_info.json file's information in a structured way.
+ */
+export interface BuildInfo {
+    built: string;
+    ref: string;
+    sha: string;
+    actor: string;
+    eventName: string;
+    prettyReleaseName: string;
+    version: string;
+}
+
+/**
+ * Structure of the version info.
+ */
+export interface VersionInfoData {
+    version: string
+    major: number;
+    minor: number;
+    patch: number;
+    branch: string;
+    commit: string;
+}
+
+/**
+ *  Provides functions to the {type}_build_info.json file and provide version functions.
+ */
+export class BuildInfo {
+    /**
+    * Reads the {type}_build_info.json file and returns the data a BuildInfo object.
+    */
+    public static async getBuildInfo(): Promise<BuildInfo> {
+        let buildInfo;
+        await fetch('/VFS/a32nx_build_info.json').then((response) => {
+            response.json().then((json) => {
+                buildInfo = ({
+                    built: json.built,
+                    ref: json.ref,
+                    sha: json.sha,
+                    actor: json.actor,
+                    eventName: json.event_name,
+                    prettyReleaseName: json.pretty_release_name,
+                    version: json.version,
+                });
+            });
+        });
+        return buildInfo;
+    }
+
+    /**
+     * Parses the version string and returns the version info as VersionInfoData object.
+     * @param versionString as provided by the {type}_build_info.json file.
+     */
+    public static getVersionInfo(versionString: string): VersionInfoData {
+        const matchBuildInfo = versionString.match(/^v?((\d+)\.(\d+)\.(\d+))-(.*)\.(.{7})$/);
+        if (matchBuildInfo) {
+            const version = matchBuildInfo[1];
+            const major = parseInt(matchBuildInfo[2], 10);
+            const minor = parseInt(matchBuildInfo[3], 10);
+            const patch = parseInt(matchBuildInfo[4], 10);
+            const branch = matchBuildInfo[5];
+            const commit = matchBuildInfo[6];
+            return {
+                version,
+                major,
+                minor,
+                patch,
+                branch,
+                commit,
+            };
+        }
+        throw new Error('Version is not in the correct format');
+    }
+
+    /**
+     * Compare two version strings. Returns 1 if v1 is newer, -1 if v2 is newer and 0 if they are equal.
+     * TODO: Use semver package. semver package did not work for some reason. This is a temporary solution.
+     * @param v1
+     * @param v2
+     */
+    public static versionCompare(v1: string, v2: string): number {
+        let major1;
+        let minor1;
+        let patch1;
+        let major2;
+        let minor2;
+        let patch2;
+
+        const matchBuildInfo = v1.match(/^v?(\d+)\.(\d+)\.(\d+).*$/);
+        if (matchBuildInfo) {
+            major1 = parseInt(matchBuildInfo[1], 10);
+            minor1 = parseInt(matchBuildInfo[2], 10);
+            patch1 = parseInt(matchBuildInfo[3], 10);
+            console.debug(`Version 1 (parsed): ${major1} ${minor1} ${patch1}`);
+        } else {
+            throw new Error('Version 1 is not in the correct format');
+        }
+        const matchReleaseInfo = v2.match(/^v?(\d+)\.(\d+)\.(\d+).*$/);
+        if (matchReleaseInfo) {
+            major2 = parseInt(matchReleaseInfo[1], 10);
+            minor2 = parseInt(matchReleaseInfo[2], 10);
+            patch2 = parseInt(matchReleaseInfo[3], 10);
+            console.debug(`Version 2 (parsed): ${major2} ${minor2} ${patch2}`);
+        } else {
+            throw new Error('Version 2 is not in the correct format');
+        }
+
+        if (major1 > major2) {
+            return 1;
+        }
+        if (major1 < major2) {
+            return -1;
+        }
+        if (minor1 > minor2) {
+            return 1;
+        }
+        if (minor1 < minor2) {
+            return -1;
+        }
+        if (patch1 > patch2) {
+            return 1;
+        }
+        if (patch1 < patch2) {
+            return -1;
+        }
+        return 0;
+    }
+}
