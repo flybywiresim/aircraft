08a00a8334fcd7f305e1593e601991fff14517da
peter skaarup <peterskaarup@gmail.com>
2022-12-19 13:27:33 +0100

    Changed enum values from string to int.

--- src/atsu/src/DatalinkStatusCodes.ts
+++ src/atsu/src/DatalinkStatusCodes.ts
@@ -2,16 +2,16 @@
 //  SPDX-License-Identifier: GPL-3.0
 
 export enum DatalinkStatusCode {
-    DlkAvail = '{green}DLK AVAIL{end}',
-    DlkNotAvail = '{small}DLK NOT AVAIL{end}',
-    Inop = '{red}INOP{end}',
-    NotInstalled = '{small}NOT INSTALLED{end}'
+    DlkAvail = 2,
+    DlkNotAvail = 1,
+    Inop = -1,
+    NotInstalled = 0
 
 }
 
 export enum DatalinkModeCode {
-    AtcAoc = 'ATC/AOC',
-    Aoc = 'AOC',
-    Atc = 'ATC',
-    None = ''
+    AtcAoc = 1,
+    Aoc = 2,
+    Atc = 3,
+    None = 0
 }
