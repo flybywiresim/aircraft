077180494768bdadc3c101b347e1bebca3bfaea6
BBK <22713769+BlueberryKing@users.noreply.github.com>
2022-12-25 22:43:25 +0100

    Connect all four engines

--- fbw-a380x/src/wasm/systems/a380_systems_wasm/src/lib.rs
+++ fbw-a380x/src/wasm/systems/a380_systems_wasm/src/lib.rs
@@ -209,8 +209,12 @@ async fn systems(mut gauge: msfs::Gauge) -> Result<(), Box<dyn Error>> {
     .provides_aircraft_variable("TRAILING EDGE FLAPS RIGHT PERCENT", "Percent", 0)?
     .provides_aircraft_variable("TURB ENG CORRECTED N1", "Percent", 1)?
     .provides_aircraft_variable("TURB ENG CORRECTED N1", "Percent", 2)?
+    .provides_aircraft_variable("TURB ENG CORRECTED N1", "Percent", 3)?
+    .provides_aircraft_variable("TURB ENG CORRECTED N1", "Percent", 4)?
     .provides_aircraft_variable("TURB ENG CORRECTED N2", "Percent", 1)?
     .provides_aircraft_variable("TURB ENG CORRECTED N2", "Percent", 2)?
+    .provides_aircraft_variable("TURB ENG CORRECTED N2", "Percent", 3)?
+    .provides_aircraft_variable("TURB ENG CORRECTED N2", "Percent", 4)?
     .provides_aircraft_variable("TURB ENG IGNITION SWITCH EX1", "Enum", 1)?
     .provides_aircraft_variable("UNLIMITED FUEL", "Bool", 0)?
     .provides_aircraft_variable("VELOCITY BODY X", "feet per second", 0)?
