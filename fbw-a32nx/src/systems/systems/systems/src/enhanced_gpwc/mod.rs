03c3977593acbed35bb76f15cedebbde9e666f6c
Sven Czarnian <devel@svcz.de>
2023-01-05 20:31:36 +0100

    get information about the destination

--- fbw-a32nx/src/systems/systems/systems/src/enhanced_gpwc/mod.rs
+++ fbw-a32nx/src/systems/systems/systems/src/enhanced_gpwc/mod.rs
@@ -1,10 +1,12 @@
 use std::vec::Vec;
 use crate::{
     enhanced_gpwc::navigation_display::NavigationDisplay,
-    shared::AdirsMeasurementOutputs,
-    simulation::{InitContext, SimulationElement, SimulationElementVisitor},
+    shared::{
+        arinc429::{Arinc429Word, SignStatus},
+        AdirsMeasurementOutputs,
+    },
+    simulation::{InitContext, Read, SimulationElement, SimulationElementVisitor, VariableIdentifier, SimulatorReader},
 };
-// use self::navigation_display::NavigationDisplay;
 use uom::si::{
     angle::degree,
     f64::{Angle, Length, Velocity},
@@ -16,6 +18,12 @@ pub mod navigation_display;
 // pub mod terrain;
 
 pub struct EnhancedGPWC {
+    fm1_destination_longitude_ssm_id: VariableIdentifier,
+    fm1_destination_longitude_id: VariableIdentifier,
+    fm1_destination_latitude_ssm_id: VariableIdentifier,
+    fm1_destination_latitude_id: VariableIdentifier,
+    destination_longitude: Arinc429Word<Angle>,
+    destination_latitude: Arinc429Word<Angle>,
     adiru_data_valid: bool,
     latitude: Angle,
     longitude: Angle,
@@ -34,6 +42,12 @@ impl EnhancedGPWC {
         range_lookup: Vec<Length>,
     ) -> Self {
         EnhancedGPWC {
+            fm1_destination_longitude_ssm_id: context.get_identifier("FM1_DEST_LONG_SSM".to_owned()),
+            fm1_destination_longitude_id: context.get_identifier("FM1_DEST_LONG".to_owned()),
+            fm1_destination_latitude_ssm_id: context.get_identifier("FM1_DEST_LAT_SSM".to_owned()),
+            fm1_destination_latitude_id: context.get_identifier("FM1_DEST_LAT".to_owned()),
+            destination_longitude: Arinc429Word::new(Angle::new::<degree>(0.0), SignStatus::FailureWarning),
+            destination_latitude: Arinc429Word::new(Angle::new::<degree>(0.0), SignStatus::FailureWarning),
             adiru_data_valid: false,
             latitude: Angle::new::<degree>(0.0),
             longitude: Angle::new::<degree>(0.0),
@@ -88,9 +102,27 @@ impl EnhancedGPWC {
     pub fn vertical_speed(&self) -> Velocity {
         self.vertical_speed
     }
+
+    pub fn destination_longitude(&self) -> Arinc429Word<Angle> {
+        self.destination_longitude
+    }
+
+    pub fn destination_latitude(&self) -> Arinc429Word<Angle> {
+        self.destination_latitude
+    }
 }
 
 impl SimulationElement for EnhancedGPWC {
+    fn read(&mut self, reader: &mut SimulatorReader) {
+        let destination_long: f64 = reader.read(&self.fm1_destination_longitude_id);
+        let destination_lat: f64 = reader.read(&self.fm1_destination_latitude_id);
+        let destination_long_ssm: u32 = reader.read(&self.fm1_destination_longitude_ssm_id);
+        let destination_lat_ssm: u32 = reader.read(&self.fm1_destination_latitude_ssm_id);
+
+        self.destination_longitude = Arinc429Word::new(Angle::new::<degree>(destination_long), SignStatus::from(destination_long_ssm));
+        self.destination_latitude = Arinc429Word::new(Angle::new::<degree>(destination_lat), SignStatus::from(destination_lat_ssm));
+    }
+
     fn accept<T: SimulationElementVisitor>(&mut self, visitor: &mut T) {
         self.navigation_displays
             .iter_mut()
