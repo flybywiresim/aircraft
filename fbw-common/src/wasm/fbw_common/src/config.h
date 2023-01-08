1ae9444ea81825d449553ab98dc7fa7300520e27
Sven Czarnian <devel@svcz.de>
2023-01-08 18:43:21 +0100

    introduce a configuration to configure the built binaries

---
+++ fbw-common/src/wasm/fbw_common/src/config.h
@@ -0,0 +1,46 @@
+#pragma once
+
+enum TerrainRenderingMode {
+  ArcMode = 0,
+  VerticalMode = 1,
+};
+
+#ifdef BUILD_SIDE_CAPT
+static const std::string ConnectionName = "FBW_SIMBRIDGE_TERRONND_LEFT";
+static const std::string MetadataName = "FBW_SIMBRIDGE_TERRONND_METADATA_LEFT";
+static const std::string FrameDataName = "FBW_SIMBRIDGE_TERRONND_FRAME_DATA_LEFT";
+static const std::string LightPotentiometerName = "LIGHT POTENTIOMETER:94";
+static constexpr bool SendSimulatorData = true;
+#elif BUILD_SIDE_FO
+static const std::string ConnectionName = "FBW_SIMBRIDGE_TERRONND_RIGHT";
+static const std::string MetadataName = "FBW_SIMBRIDGE_TERRONND_METADATA_RIGHT";
+static const std::string FrameDataName = "FBW_SIMBRIDGE_TERRONND_FRAME_DATA_RIGHT";
+static const std::string LightPotentiometerName = "LIGHT POTENTIOMETER:95";
+static constexpr bool SendSimulatorData = false;
+#else
+static const std::string ConnectionName = "";
+static const std::string MetadataName = "";
+static const std::string FrameDataName = "";
+static const std::string LightPotentiometerName = "";
+static constexpr bool SendSimulatorData = false;
+#error "Unknown side to compile"
+#endif
+
+#ifdef BUILD_A32NX
+static constexpr TerrainRenderingMode RenderingMode = TerrainRenderingMode::ArcMode;
+
+// macro to create the full SimVar name
+#define FBW_LVAR_NAME(NAME) "A32NX_" NAME
+
+#elif BUILD_A380X
+static constexpr TerrainRenderingMode RenderingMode = TerrainRenderingMode::VerticalMode;
+
+// TODO change prefix as soon as we switched
+// macro to create the full SimVar name
+#define FBW_LVAR_NAME(NAME) "A32NX_" NAME
+
+#else
+static constexpr TerrainRenderingMode RenderingMode = TerrainRenderingMode::ArcMode;
+#define FBW_LVAR_NAME(NAME) ""
+#error "Unknown aircraft build type"
+#endif
