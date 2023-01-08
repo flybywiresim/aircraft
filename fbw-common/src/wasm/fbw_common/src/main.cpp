bf073a330245c57e09ebab87b1d51f97c2ddc5f6
Sven Czarnian <devel@svcz.de>
2023-01-08 21:12:05 +0100

    update the rendering

--- fbw-common/src/wasm/fbw_common/src/main.cpp
+++ fbw-common/src/wasm/fbw_common/src/main.cpp
@@ -38,6 +38,7 @@ terronnd_gauge_callback(FsContext ctx, int service_id, void* pData) {
     }
     case PANEL_SERVICE_PRE_DRAW: {
       const bool retval = simconnect.update();
+
       if (simconnect.receivedFrameData()) {
         if (decodedImage != 0) {
           nvgDeleteImage(renderContext[ctx], decodedImage);
@@ -45,8 +46,12 @@ terronnd_gauge_callback(FsContext ctx, int service_id, void* pData) {
         }
 
         // decode the frame
-        decodedImage =
-            nvgCreateImageMem(renderContext[ctx], 0, (std::uint8_t*)simconnect.frameData().data(), simconnect.metadata().frameByteCount);
+        if (decodedImage == 0) {
+          decodedImage = nvgCreateImageRGBA(renderContext[ctx], simconnect.metadata().imageWidth, simconnect.metadata().imageHeight, 0,
+                                            (std::uint8_t*)simconnect.frameData().data());
+        } else {
+          nvgUpdateImage(renderContext[ctx], decodedImage, (std::uint8_t*)simconnect.frameData().data());
+        }
         if (decodedImage == 0) {
           std::cout << "TERR ON ND: Failed to load image: " << stbi_failure_reason() << std::endl;
         }
@@ -60,15 +65,16 @@ terronnd_gauge_callback(FsContext ctx, int service_id, void* pData) {
       nvgBeginFrame(context, pDrawData->winWidth, pDrawData->winHeight, ratio);
       {
         // fill the background
-        nvgFillColor(context, nvgRGB(4, 4, 5));
-        nvgBeginPath(context);
-        nvgRect(context, 0.0f, 0.0f, static_cast<float>(pDrawData->winWidth), static_cast<float>(pDrawData->winHeight));
-        nvgFill(context);
-        if (decodedImage != 0) {
+        if (decodedImage == 0) {
+          nvgFillColor(context, nvgRGBA(4, 4, 5, 255));
+          nvgBeginPath(context);
+          nvgRect(context, 0.0f, 0.0f, static_cast<float>(pDrawData->winWidth), static_cast<float>(pDrawData->winHeight));
+          nvgFill(context);
+        } else {
           // draw the image
+          nvgBeginPath(context);
           NVGpaint imagePaint = nvgImagePattern(context, 0.0f, 0.0f, simconnect.metadata().imageWidth, simconnect.metadata().imageHeight,
                                                 0.0, decodedImage, 1.0);
-          nvgBeginPath(context);
           nvgRect(context, 0.0f, 0.0f, simconnect.metadata().imageWidth, simconnect.metadata().imageHeight);
           nvgFillPaint(context, imagePaint);
           nvgFill(context);
