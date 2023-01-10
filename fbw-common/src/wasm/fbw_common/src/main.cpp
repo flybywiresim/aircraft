317c20be4904423b04b667c66006e56b54056864
Sven Czarnian <devel@svcz.de>
2023-01-10 22:08:07 +0100

    use the maprenderer class

--- fbw-common/src/wasm/fbw_common/src/main.cpp
+++ fbw-common/src/wasm/fbw_common/src/main.cpp
@@ -10,11 +10,10 @@
 #include "config.h"
 #include "interface/SimConnectInterface.h"
 #include "main.h"
+#include "maprenderer.h"
 
-std::vector<std::uint8_t> imageBuffer;
 SimConnectInterface simconnect;
-std::map<FsContext, NVGcontext*> renderContext;
-int decodedImage = 0;
+MapRenderer renderer;
 
 #if BUILD_SIDE_CAPT
 __attribute__((export_name("terronnd_left_gauge_callback")))
@@ -30,57 +29,19 @@ terronnd_gauge_callback(FsContext ctx, int service_id, void* pData) {
       return simconnect.connect();
     };
     case PANEL_SERVICE_POST_INSTALL: {
-      NVGparams params;
-      params.userPtr = ctx;
-      params.edgeAntiAlias = false;
-      renderContext[ctx] = nvgCreateInternal(&params);
+      renderer.initialize(ctx);
       break;
     }
     case PANEL_SERVICE_PRE_DRAW: {
       const bool retval = simconnect.update();
 
       if (simconnect.receivedFrameData()) {
-        if (decodedImage != 0) {
-          nvgDeleteImage(renderContext[ctx], decodedImage);
-          decodedImage = 0;
-        }
-
-        // decode the frame
-        if (decodedImage == 0) {
-          decodedImage = nvgCreateImageRGBA(renderContext[ctx], simconnect.metadata().imageWidth, simconnect.metadata().imageHeight, 0,
-                                            (std::uint8_t*)simconnect.frameData().data());
-        } else {
-          nvgUpdateImage(renderContext[ctx], decodedImage, (std::uint8_t*)simconnect.frameData().data());
-        }
-        if (decodedImage == 0) {
-          std::cout << "TERR ON ND: Failed to load image: " << stbi_failure_reason() << std::endl;
-        }
+        renderer.newMap(ctx, simconnect.frameData(), simconnect.metadata().frameByteCount);
         simconnect.processedFrame();
       }
 
-      sGaugeDrawData* pDrawData = (sGaugeDrawData*)pData;
-      NVGcontext* context = renderContext[ctx];
-
-      const float ratio = pDrawData->fbWidth / pDrawData->fbHeight;
-      nvgBeginFrame(context, pDrawData->winWidth, pDrawData->winHeight, ratio);
-      {
-        // fill the background
-        if (decodedImage == 0) {
-          nvgFillColor(context, nvgRGBA(4, 4, 5, 255));
-          nvgBeginPath(context);
-          nvgRect(context, 0.0f, 0.0f, static_cast<float>(pDrawData->winWidth), static_cast<float>(pDrawData->winHeight));
-          nvgFill(context);
-        } else {
-          // draw the image
-          nvgBeginPath(context);
-          NVGpaint imagePaint = nvgImagePattern(context, 0.0f, 0.0f, simconnect.metadata().imageWidth, simconnect.metadata().imageHeight,
-                                                0.0, decodedImage, 1.0);
-          nvgRect(context, 0.0f, 0.0f, simconnect.metadata().imageWidth, simconnect.metadata().imageHeight);
-          nvgFillPaint(context, imagePaint);
-          nvgFill(context);
-        }
-      }
-      nvgEndFrame(context);
+      renderer.update(ctx, simconnect.currentNdMode(), simconnect.terrainMapActive());
+      renderer.render((sGaugeDrawData*)pData, ctx);
 
       return retval;
     };
@@ -88,11 +49,7 @@ terronnd_gauge_callback(FsContext ctx, int service_id, void* pData) {
     case PANEL_SERVICE_PRE_KILL: {
       // disconnect sim connect
       simconnect.disconnect();
-      if (decodedImage != 0) {
-        nvgDeleteImage(renderContext[ctx], decodedImage);
-      }
-      nvgDeleteInternal(renderContext[ctx]);
-      renderContext.erase(ctx);
+      renderer.destroy(ctx);
       break;
     }
   }
