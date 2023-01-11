eac4ccb2fa40de360ff594e00cf611b337e7b12e
Mischa Binder <mischa.binder@outlook.de>
2023-01-11 18:05:58 +0100

    Added new function to fetch flightplan data from requestId

--- src/instruments/src/EFB/SimbriefApi/simbriefParser.ts
+++ src/instruments/src/EFB/SimbriefApi/simbriefParser.ts
@@ -24,6 +24,125 @@ export const getSimbriefFlightplans = (): Promise<ISimbriefFlightplan[]> => (
         })
 );
 
+export const getSimbriefDataNew = (requestId: number): Promise<ISimbriefData> => (
+    fetch(`${simbriefApiUrlV2.toString()}/flightplans/${requestId}`, getRequestData)
+        .then((res) => {
+            if (!res.ok) {
+                return res.json().then((json) => Promise.reject(new Error(json.message)));
+            }
+            return res.json().then((json) => simbriefDataParserNew(json));
+        })
+);
+
+const simbriefDataParserNew = (simbriefJson: any): ISimbriefData => {
+    const {
+        params,
+        general,
+        origin,
+        destination,
+        alternate,
+        aircraft,
+        fuel,
+        times,
+        weights,
+        text,
+        files,
+    } = simbriefJson;
+
+    return {
+        airline: general.icao_airline,
+        flightNumber: general.flight_number,
+        aircraftReg: aircraft.reg,
+        cruiseAltitude: general.initial_altitude,
+        costIndex: general.costindex,
+        route: general.route,
+        files: { loadsheet: files.pdf.link ? files.directory + files.pdf.link : undefined },
+        origin: {
+            iata: origin.iata_code,
+            runway: origin.plan_rwy,
+            icao: origin.icao_code,
+            name: origin.name,
+            posLat: origin.pos_lat,
+            posLong: origin.pos_long,
+            metar: origin.metar,
+        },
+        destination: {
+            iata: destination.iata_code,
+            runway: destination.plan_rwy,
+            icao: destination.icao_code,
+            name: destination.name,
+            posLat: destination.pos_lat,
+            posLong: destination.pos_long,
+            metar: destination.metar,
+        },
+        distance: `${general.air_distance}nm`,
+        flightETAInSeconds: times.est_time_enroute,
+        weights: {
+            cargo: weights.cargo,
+            estLandingWeight: weights.est_ldw,
+            estTakeOffWeight: weights.est_tow,
+            estZeroFuelWeight: weights.est_zfw,
+            maxLandingWeight: weights.max_ldw,
+            maxTakeOffWeight: weights.max_tow,
+            maxZeroFuelWeight: weights.max_zfw,
+            passengerCount: weights.pax_count_actual,
+            bagCount: weights.bag_count_actual,
+            passengerWeight: weights.pax_weight,
+            bagWeight: weights.bag_weight,
+            payload: weights.payload,
+            freight: weights.freight_added,
+        },
+        fuel: {
+            avgFuelFlow: fuel.avg_fuel_flow,
+            contingency: fuel.contingency,
+            enrouteBurn: fuel.enroute_burn,
+            etops: fuel.etops,
+            extra: fuel.extra,
+            maxTanks: fuel.max_tanks,
+            minTakeOff: fuel.min_takeoff,
+            planLanding: fuel.plan_landing,
+            planRamp: fuel.plan_ramp,
+            planTakeOff: fuel.plan_takeoff,
+            reserve: fuel.reserve,
+            taxi: fuel.taxi,
+        },
+        units: params.units,
+        alternate: {
+            icao: alternate[0].icao_code,
+            iata: alternate[0].iata_code,
+            burn: alternate[0].burn,
+        },
+        times: {
+            contFuelTime: times.contfuel_time,
+            destTimezone: times.dest_timezone,
+            endurance: times.endurance,
+            estBlock: times.est_block,
+            estIn: times.est_in,
+            estOff: times.est_off,
+            estOn: times.est_on,
+            estOut: times.est_out,
+            estTimeEnroute: times.est_time_enroute,
+            etopsFuelTime: times.etopsfuel_time,
+            extraFuelTime: times.extrafuel_time,
+            origTimeZone: times.orig_timezone,
+            reserveTime: times.reserve_time,
+            schedBlock: times.sched_block,
+            schedIn: times.sched_in,
+            schedOff: times.sched_off,
+            schedOn: times.sched_on,
+            schedOut: times.sched_out,
+            schedTimeEnroute: times.sched_time_enroute,
+            taxiIn: times.taxi_in,
+            taxiOut: times.taxi_out,
+        },
+        weather: {
+            avgWindDir: general.avg_wind_dir,
+            avgWindSpeed: general.avg_wind_spd,
+        },
+        text: text.plan_html.replace(/^<div [^>]+>/, '').replace(/<\/div>$/, ''),
+    };
+};
+
 export const getSimbriefData = (simbriefUserId: string): Promise<ISimbriefData> => {
     simbriefApiParams.append('userid', simbriefUserId);
     simbriefApiParams.append('json', '1');
