3abcdc39a31e08af4c0efc09628a482a5b650877
Mischa Binder <mischa.binder@outlook.de>
2023-01-12 16:27:01 +0100

    implemented more needed simvars into publisher

--- fbw-a32nx/src/systems/instruments/src/Clock2/shared/ClockSimvarPublisher.tsx
+++ fbw-a32nx/src/systems/instruments/src/Clock2/shared/ClockSimvarPublisher.tsx
@@ -3,20 +3,35 @@ import { EventBus, SimVarDefinition, SimVarValueType, SimVarPublisher } from 'ms
 export interface ClockSimvars {
     ltsTest: number;
     dcEssIsPowered: boolean;
+    dcHot1IsPowered: boolean;
     absTime: number;
+    currentUTC: number;
+    dayOfMonth: number;
+    monthOfYear: number;
+    year: number;
 }
 
 export enum ClockVars {
     ltsTest = 'L:A32NX_OVHD_INTLT_ANN',
     dcEssIsPowered = 'L:A32NX_ELEC_DC_ESS_BUS_IS_POWERED',
+    dcHot1IsPowered = 'L:A32NX_ELEC_DC_HOT_1_BUS_IS_POWERED',
     absTime = 'E:ABSOLUTE TIME',
+    currentUTC = 'E:ZULU TIME',
+    dayOfMonth = 'E:ZULU DAY OF MONTH',
+    monthOfYear = 'E:ZULU MONTH OF YEAR',
+    year = 'E:ZULU YEAR',
 }
 
 export class ClockSimvarPublisher extends SimVarPublisher<ClockSimvars> {
     private static simvars = new Map<keyof ClockSimvars, SimVarDefinition>([
         ['ltsTest', { name: ClockVars.ltsTest, type: SimVarValueType.Number }],
         ['dcEssIsPowered', { name: ClockVars.dcEssIsPowered, type: SimVarValueType.Bool }],
+        ['dcHot1IsPowered', { name: ClockVars.dcHot1IsPowered, type: SimVarValueType.Bool }],
         ['absTime', { name: ClockVars.absTime, type: SimVarValueType.Number }],
+        ['currentUTC', { name: ClockVars.currentUTC, type: SimVarValueType.Number }],
+        ['dayOfMonth', { name: ClockVars.dayOfMonth, type: SimVarValueType.Number }],
+        ['monthOfYear', { name: ClockVars.monthOfYear, type: SimVarValueType.Number }],
+        ['year', { name: ClockVars.year, type: SimVarValueType.Number }],
     ])
 
     public constructor(bus: EventBus) {
