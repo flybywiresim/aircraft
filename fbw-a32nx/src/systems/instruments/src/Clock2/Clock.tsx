3fcdda3ec2c18a576bcb56c67316e18e38bb9e4c
Mischa Binder <mischa.binder@outlook.de>
2023-01-12 13:05:30 +0100

    outsourced ltsTest to root component

--- fbw-a32nx/src/systems/instruments/src/Clock2/Clock.tsx
+++ fbw-a32nx/src/systems/instruments/src/Clock2/Clock.tsx
@@ -1,4 +1,5 @@
-import { ComponentProps, DisplayComponent, EventBus, FSComponent, VNode } from 'msfssdk';
+import { ClockSimvars } from 'instruments/src/Clock2/shared/ClockSimvarPublisher';
+import { ComponentProps, DisplayComponent, EventBus, FSComponent, Subject, VNode } from 'msfssdk';
 import { Chrono } from './Components/Chrono';
 
 import './style.scss';
@@ -8,11 +9,22 @@ interface ClockProps extends ComponentProps {
 }
 
 export class ClockRoot extends DisplayComponent<ClockProps> {
+    private readonly ltsTest = Subject.create(false);
+
+    public onAfterRender(node: VNode): void {
+        super.onAfterRender(node);
+
+        const sub = this.props.bus.getSubscriber<ClockSimvars>();
+        sub.on('ltsTest').whenChanged().handle((ltsTest) => {
+            this.ltsTest.set(ltsTest === 0);
+        });
+    }
+
     render(): VNode {
         return (
             <svg version="1.1" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
                 <g class="day on">
-                    <Chrono bus={this.props.bus} />
+                    <Chrono bus={this.props.bus} ltsTest={this.ltsTest} />
                 </g>
             </svg>
         );
