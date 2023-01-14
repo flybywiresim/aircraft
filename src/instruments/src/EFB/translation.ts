dc81f48c7ec980a4cf2f4c6d9f17c3c117b3f18b
Frank Kopp <frank@familie-kopp.de>
2023-01-14 02:51:21 +0100

    feat: Add aircraft version check and user message when using old version (#7655)

--- src/instruments/src/EFB/translation.ts
+++ src/instruments/src/EFB/translation.ts
@@ -153,6 +153,17 @@ if (process.env.VITE_BUILD) {
     watchLanguageChanges();
 }
 
+const placeholderReplace = (translation: string, replacements: Record<string, string>[]): string => {
+    let result = translation;
+    replacements.forEach((replacement: Record<string, string>) => {
+        // Localazy uses $<key> as placeholder - $key will be replaced with the value of key
+        const searchValue = `$${Object.keys(replacement)[0]}`;
+        const replaceValue = Object.values(replacement)[0].toString();
+        result = result.replace(searchValue, replaceValue);
+    });
+    return result;
+};
+
 /**
  * Returns localized string in the currently configured language when provided with
  * correct identifier key.
@@ -160,14 +171,26 @@ if (process.env.VITE_BUILD) {
  * find the key there.
  * If the key is not available in the default language the key itself will be returned.
  *
+ * If a replacement list is provided it will replace the placeholders in the string with the
+ * key as placeholder-text to be search and the value as the string to be put in place.
+ *
+ * Placeholders are defined as follows: $key
+ *
+ * E.g. "Hello $name" with {name: "John"} will return "Hello John"
+ *
  * Note: Currently all language files are imported and contain all keys so this is redundant
  * but still implemented for future changes.
  * @param key String identifier key
+ * @param replacements list of Records of key value pairs to replace in the string
  * @return translated string in the current language if available, or default
  *         language, or key string
  */
-export function t(key: string): string {
-    return currentLanguageMap.get(key) || defaultLanguage.get(key) || key;
+export function t(key: string, replacements?: Record<string, string>[]): string {
+    const translation = currentLanguageMap.get(key) || defaultLanguage.get(key) || key;
+    if (replacements) {
+        return placeholderReplace(translation, replacements);
+    }
+    return translation;
 }
 
 // Workaround after simvar hook changes - only required on FlyPadPage.tsx from flypad settings
