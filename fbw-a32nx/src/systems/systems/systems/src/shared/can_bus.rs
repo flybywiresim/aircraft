e643166ab9472aa55b524df450cee69059f9c512
Sven Czarnian <devel@svcz.de>
2023-01-17 21:05:58 +0100

    add some functions to handle data inside the bus

--- fbw-a32nx/src/systems/systems/systems/src/shared/can_bus.rs
+++ fbw-a32nx/src/systems/systems/systems/src/shared/can_bus.rs
@@ -8,15 +8,18 @@ use crate::{
 use std::collections::VecDeque;
 use std::vec::Vec;
 
+const TRANSMISSION_BUFFER_SIZE: usize = 12;
+
 pub struct CanBus<const N: usize> {
     attached_systems: [u8; N],
     next_transmitting_system: usize,
-    transmission_buffers: Vec<VecDeque<f64>>,
+    transmission_buffers: Vec<VecDeque<Arinc825Word<f64>>>,
     availability_id: VariableIdentifier,
     available: bool,
     databus_id: VariableIdentifier,
-    received_message: Arinc825Word<f32>,
-    next_output_message: f64,
+    last_received_message: Arinc825Word<f64>,
+    received_message: Arinc825Word<f64>,
+    next_output_message: Arinc825Word<f64>,
     next_output_message_valid: bool,
 }
 
@@ -29,11 +32,18 @@ impl<const N: usize> CanBus<N> {
             availability_id: context.get_identifier(format!("{}_AVAIL", bus_name)),
             available: false,
             databus_id: context.get_identifier(bus_name.to_owned()),
+            last_received_message: Arinc825Word::new(
+                0.0,
+                LogicalCommunicationChannel::CanBaseFrameMigrationChannel,
+            ),
             received_message: Arinc825Word::new(
                 0.0,
                 LogicalCommunicationChannel::CanBaseFrameMigrationChannel,
             ),
-            next_output_message: 0.0,
+            next_output_message: Arinc825Word::new(
+                0.0,
+                LogicalCommunicationChannel::CanBaseFrameMigrationChannel,
+            ),
             next_output_message_valid: false,
         }
     }
@@ -46,6 +56,7 @@ impl<const N: usize> CanBus<N> {
                 end_idx = N - 1;
             }
 
+            // search the next sendable message
             while idx != end_idx {
                 if idx >= N {
                     idx = 0;
@@ -53,13 +64,54 @@ impl<const N: usize> CanBus<N> {
 
                 let message = self.transmission_buffers[idx].get(0);
                 if message.is_some() {
-                    self.next_output_message =
-                        self.transmission_buffers[idx].pop_front().unwrap().into();
+                    self.next_output_message = self.transmission_buffers[idx].pop_front().unwrap();
                     self.next_output_message_valid = true;
                     break;
                 } else {
                     idx += 1;
                 }
+            }
+
+            // start the next time from the next system
+            self.next_transmitting_system = (self.next_transmitting_system + 1) % N;
+        }
+    }
+
+    pub fn new_message_received(&self, function_id: u8) -> bool {
+        if self.last_received_message.status() != self.received_message.status()
+            || self.last_received_message.value() != self.received_message.value()
+        {
+            self.received_message.client_function_id() != function_id
+        } else {
+            false
+        }
+    }
+
+    pub fn received_message(&self) -> Arinc825Word<f64> {
+        self.received_message
+    }
+
+    pub fn send_message(&mut self, message: Arinc825Word<f64>) -> bool {
+        for (i, id) in self.attached_systems.iter().enumerate() {
+            if *id == message.client_function_id() {
+                // detected an buffer overrun
+                if self.transmission_buffers[i].len() >= TRANSMISSION_BUFFER_SIZE {
+                    return false;
+                }
+
+                self.transmission_buffers[i].push_back(message);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    pub fn reset_buffer(&mut self, function_id: u8) {
+        for (i, id) in self.attached_systems.iter().enumerate() {
+            if *id == function_id {
+                self.transmission_buffers[i] = VecDeque::new();
+                break;
             }
         }
     }
@@ -67,6 +119,7 @@ impl<const N: usize> CanBus<N> {
 
 impl<const N: usize> SimulationElement for CanBus<N> {
     fn read(&mut self, reader: &mut SimulatorReader) {
+        self.last_received_message = self.received_message;
         self.received_message = reader.read_arinc825(&self.databus_id);
         let availability: f64 = reader.read(&self.availability_id);
         self.available = availability != 0.0;
@@ -74,7 +127,11 @@ impl<const N: usize> SimulationElement for CanBus<N> {
 
     fn write(&self, writer: &mut SimulatorWriter) {
         if self.next_output_message_valid {
-            writer.write(&self.databus_id, self.next_output_message);
+            writer.write_arinc825(
+                &self.databus_id,
+                self.next_output_message.value(),
+                self.next_output_message.status(),
+            );
         }
         writer.write(&self.availability_id, 1.0);
     }
