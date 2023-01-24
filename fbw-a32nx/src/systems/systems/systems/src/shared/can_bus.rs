ca6da4f266cf4d97f0d6721fe89ee44d2cbbe9ee
Sven Czarnian <devel@svcz.de>
2023-01-24 18:00:24 +0100

    fix issues triggered by the unit tests

--- fbw-a32nx/src/systems/systems/systems/src/shared/can_bus.rs
+++ fbw-a32nx/src/systems/systems/systems/src/shared/can_bus.rs
@@ -1,5 +1,5 @@
 use crate::{
-    shared::arinc825::{Arinc825Word, LogicalCommunicationChannel},
+    shared::arinc825::Arinc825Word,
     simulation::{
         InitContext, Read, SimulationElement, SimulatorReader, SimulatorWriter, VariableIdentifier,
         Write,
@@ -11,19 +11,17 @@ use std::vec::Vec;
 const TRANSMISSION_BUFFER_SIZE: usize = 12;
 
 pub struct CanBus<const N: usize> {
-    initialize: bool,
     attached_systems: [u8; N],
     next_transmitting_system: usize,
     transmission_buffers: Vec<VecDeque<Arinc825Word<f64>>>,
     message_received_by_systems_ids: Vec<VariableIdentifier>,
-    message_received_by_systems: Vec<bool>,
-    update_message_received_flag: Vec<bool>,
+    // first bool is the received-state and the second bool describes a dirty flag to skip read-calls, if needed
+    message_received_by_systems: Vec<[bool; 2]>,
     availability_id: VariableIdentifier,
     available: bool,
     failure_indication_id: VariableIdentifier,
     failure_indication: bool,
     databus_id: VariableIdentifier,
-    last_received_message: Arinc825Word<f64>,
     received_message: Arinc825Word<f64>,
     next_output_message: Arinc825Word<f64>,
     next_output_message_valid: bool,
@@ -32,41 +30,30 @@ pub struct CanBus<const N: usize> {
 impl<const N: usize> CanBus<N> {
     pub fn new(context: &mut InitContext, bus_name: &str, systems: [u8; N]) -> Self {
         Self {
-            initialize: true,
             attached_systems: systems,
             next_transmitting_system: 0,
             transmission_buffers: (1..=N).map(|_| VecDeque::new()).collect(),
             message_received_by_systems_ids: (0..=N - 1)
                 .map(|id| context.get_identifier(format!("{}_{}_RECEIVED", bus_name, systems[id])))
                 .collect(),
-            message_received_by_systems: (1..=N).map(|_| true).collect(),
-            update_message_received_flag: (1..=N).map(|_| true).collect(),
+            message_received_by_systems: (1..=N).map(|_| [true, true]).collect(),
             availability_id: context.get_identifier(format!("{}_AVAIL", bus_name)),
             available: false,
             failure_indication_id: context.get_identifier(format!("{}_FAILURE", bus_name)),
             failure_indication: false,
             databus_id: context.get_identifier(bus_name.to_owned()),
-            last_received_message: Arinc825Word::new(
-                0.0,
-                LogicalCommunicationChannel::CanBaseFrameMigrationChannel,
-            ),
-            received_message: Arinc825Word::new(
-                0.0,
-                LogicalCommunicationChannel::CanBaseFrameMigrationChannel,
-            ),
-            next_output_message: Arinc825Word::new(
-                0.0,
-                LogicalCommunicationChannel::CanBaseFrameMigrationChannel,
-            ),
-            next_output_message_valid: false,
+            received_message: Arinc825Word::new_with_status(0.0, 0x04000000),
+            next_output_message: Arinc825Word::new_with_status(0.0, 0x04000000),
+            next_output_message_valid: true,
         }
     }
 
     pub fn update(&mut self) {
         self.next_output_message_valid = false;
-        self.update_message_received_flag
+
+        self.message_received_by_systems
             .iter_mut()
-            .for_each(|update| *update = false);
+            .for_each(|received| received[1] = false);
 
         if self.available && !self.failure_indication {
             let mut bus_busy = false;
@@ -75,7 +62,7 @@ impl<const N: usize> CanBus<N> {
             self.message_received_by_systems
                 .iter()
                 .for_each(|received| {
-                    if !received {
+                    if !received[0] {
                         bus_busy = true;
                     }
                 });
@@ -86,44 +73,37 @@ impl<const N: usize> CanBus<N> {
                     let idx = (self.next_transmitting_system + x) % N;
                     let message = self.transmission_buffers[idx as usize].get(0);
                     if message.is_some() {
+                        // reset the received flags to release the bus for the next transmission
+                        self.message_received_by_systems
+                            .iter_mut()
+                            .for_each(|received| *received = [false, true]);
+                        self.message_received_by_systems[idx as usize][0] = true;
+
                         self.next_output_message =
                             self.transmission_buffers[idx as usize].pop_front().unwrap();
                         self.next_output_message_valid = true;
+
                         break;
                     }
                 }
 
-                // start the next time from the next system
-                if self.next_output_message_valid {
-                    // reset the received flags to release the bus for the next transmission
-                    self.message_received_by_systems
-                        .iter_mut()
-                        .for_each(|received| *received = false);
-                }
                 self.next_transmitting_system = (self.next_transmitting_system + 1) % N;
             }
         } else {
-            for i in 0..N - 1 {
-                self.update_message_received_flag[i] = true;
-                self.message_received_by_systems[i] = true;
-            }
-        }
-
-        if self.initialize {
-            self.next_output_message = Arinc825Word::new_with_status(0.0, 0x04000000);
-            self.next_output_message_valid = true;
-            self.initialize = false;
+            self.message_received_by_systems
+                .iter_mut()
+                .for_each(|received| *received = [true, true]);
         }
     }
 
     pub fn new_message_received(&self, function_id: u8) -> bool {
-        if self.last_received_message.status() != self.received_message.status()
-            || self.last_received_message.value() != self.received_message.value()
-        {
-            self.received_message.client_function_id() != function_id
-        } else {
-            false
+        for (i, id) in self.attached_systems.iter().enumerate() {
+            if *id == function_id {
+                return !self.message_received_by_systems[i][0];
+            }
         }
+
+        false
     }
 
     pub fn received_message(&mut self, function_id: u8) -> Arinc825Word<f64> {
@@ -136,8 +116,7 @@ impl<const N: usize> CanBus<N> {
         }
 
         if system_idx < N {
-            self.message_received_by_systems[system_idx] = true;
-            self.update_message_received_flag[system_idx] = true;
+            self.message_received_by_systems[system_idx] = [true, true];
         }
 
         self.received_message
@@ -171,15 +150,16 @@ impl<const N: usize> CanBus<N> {
 
 impl<const N: usize> SimulationElement for CanBus<N> {
     fn read(&mut self, reader: &mut SimulatorReader) {
-        self.last_received_message = self.received_message;
         self.received_message = reader.read_arinc825(&self.databus_id);
 
         let availability: f64 = reader.read(&self.availability_id);
         self.available = availability != 0.0;
 
         for (i, id) in self.message_received_by_systems_ids.iter().enumerate() {
-            let message_received: f64 = reader.read(id);
-            self.message_received_by_systems[i] = message_received != 0.0;
+            if !self.message_received_by_systems[i][1] {
+                let message_received: f64 = reader.read(id);
+                self.message_received_by_systems[i][0] = message_received != 0.0;
+            }
         }
 
         let failure: f64 = reader.read(&self.failure_indication_id);
@@ -187,6 +167,13 @@ impl<const N: usize> SimulationElement for CanBus<N> {
     }
 
     fn write(&self, writer: &mut SimulatorWriter) {
+        // set the availability flag
+        if self.failure_indication {
+            writer.write(&self.availability_id, 0.0);
+        } else {
+            writer.write(&self.availability_id, 1.0);
+        }
+
         if self.next_output_message_valid {
             writer.write_arinc825(
                 &self.databus_id,
@@ -195,15 +182,16 @@ impl<const N: usize> SimulationElement for CanBus<N> {
             );
         }
 
-        // set the availability flag
-        if self.failure_indication {
-            writer.write(&self.availability_id, 0.0);
-        } else {
-            writer.write(&self.availability_id, 1.0);
-        }
-
         // write the current status of the message received state per system
         for (i, id) in self.message_received_by_systems_ids.iter().enumerate() {
+            if self.message_received_by_systems[i][0] {
+                writer.write(id, 1.0);
+            } else {
+                writer.write(id, 0.0);
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
