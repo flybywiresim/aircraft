401d134b46ae20d9134cf826062ba436a7866e9b
Benjamin Dupont <benjozorkfr@gmail.com>
2023-01-28 21:35:57 -0500

    refactor: remove old instrument sim build scripts

--- fbw-a32nx/src/systems/instruments/buildSrc/igniter/tasks.mjs
+++ fbw-a32nx/src/systems/instruments/buildSrc/igniter/tasks.mjs
@@ -3,13 +3,6 @@ import { join } from 'path';
 import { ExecTask } from '@flybywiresim/igniter';
 import { Directories } from '../directories.mjs';
 
-export function getInputs() {
-    const baseInstruments = fs.readdirSync(join(Directories.instruments, 'src'), { withFileTypes: true })
-        .filter((d) => d.isDirectory() && fs.existsSync(join(Directories.instruments, 'src', d.name, 'config.json')));
-
-    return baseInstruments.map(({ name }) => ({ path: name, name }));
-}
-
 export function getInstrumentsIgniterTasks() {
     const baseInstruments = fs.readdirSync(join(Directories.instruments, 'src'), { withFileTypes: true })
         .filter((d) => d.isDirectory() && fs.existsSync(join(Directories.instruments, 'src', d.name, 'config.json')));
