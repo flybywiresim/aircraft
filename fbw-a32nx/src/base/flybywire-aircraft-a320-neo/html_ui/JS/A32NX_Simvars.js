5b57431bb2de31557ec3b1939d112471b4f67471
Saschl <sascharudolf46@gmail.com>
2023-02-08 12:50:51 +0100

    externalize simvvars override where possible

---
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/JS/A32NX_Simvars.js
@@ -0,0 +1,91 @@
+const latlonaltRegEx = new RegExp(/latlonalt/i);
+const latlonaltpbhRegex = new RegExp(/latlonaltpbh/i);
+const pbhRegex = new RegExp(/pbh/i);
+const pid_structRegex = new RegExp(/pid_struct/i);
+const xyzRegex = new RegExp(/xyz/i);
+const stringRegex = new RegExp(/string/i);
+const boolRegex = new RegExp(/boolean|bool/i);
+const numberRegex = new RegExp(/number/i);
+const defaultSource = '';
+
+
+SimVar.GetSimVarValue = (name, unit, dataSource = defaultSource) => {
+    try {
+        if (simvar) {
+            let output;
+            const registeredID = SimVar.GetRegisteredId(name, unit, dataSource);
+
+            if (registeredID >= 0) {
+                if (numberRegex.test(unit)) {
+                    output = simvar.getValueReg(registeredID);
+                } else if (stringRegex.test(unit)) {
+                    output = simvar.getValueReg_String(registeredID);
+                } else if (latlonaltRegEx.test(unit)) {
+                    output = new LatLongAlt(simvar.getValue_LatLongAlt(name, dataSource));
+                } else if (latlonaltpbhRegex.test(unit)) {
+                    output = new LatLongAltPBH(simvar.getValue_LatLongAltPBH(name, dataSource));
+                } else if (pbhRegex.test(unit)) {
+                    output = new PitchBankHeading(simvar.getValue_PBH(name, dataSource));
+                } else if (pid_structRegex.test(unit)) {
+                    output = new PID_STRUCT(simvar.getValue_PID_STRUCT(name, dataSource));
+                } else if (xyzRegex.test(unit)) {
+                    output = new XYZ(simvar.getValue_XYZ(name, dataSource));
+                } else {
+                    output = simvar.getValueReg(registeredID);
+                }
+            }
+            return output;
+        }
+
+        console.warn(`SimVar handler is not defined (${name})`);
+    } catch (error) {
+        console.warn('ERROR ', error, ` GetSimVarValue ${name} unit : ${unit}`);
+        return null;
+    }
+    return null;
+};
+SimVar.SetSimVarValue = (name, unit, value, dataSource = defaultSource) => {
+    if (value == undefined) {
+        console.warn(`${name} : Trying to set a null value`);
+        return Promise.resolve();
+    }
+    try {
+        // @ts-ignore
+        if (simvar) {
+            const regID = SimVar.GetRegisteredId(name, unit, dataSource);
+            if (regID >= 0) {
+                if (stringRegex.test(unit)) {
+                    return Coherent.call('setValueReg_String', regID, value);
+                }
+                if (boolRegex.test(unit)) {
+                    return Coherent.call('setValueReg_Bool', regID, !!value);
+                }
+                if (numberRegex.test(unit)) {
+                    return Coherent.call('setValueReg_Number', regID, value);
+                }
+                if (latlonaltRegEx.test(unit)) {
+                    return Coherent.call('setValue_LatLongAlt', name, value, dataSource);
+                }
+                if (latlonaltpbhRegex.test(unit)) {
+                    return Coherent.call('setValue_LatLongAltPBH', name, value, dataSource);
+                }
+                if (pbhRegex.test(unit)) {
+                    return Coherent.call('setValue_PBH', name, value, dataSource);
+                }
+                if (pid_structRegex.test(unit)) {
+                    return Coherent.call('setValue_PID_STRUCT', name, value, dataSource);
+                }
+                if (xyzRegex.test(unit)) {
+                    return Coherent.call('setValue_XYZ', name, value, dataSource);
+                }
+
+                return Coherent.call('setValueReg_Number', regID, value);
+            }
+        } else {
+            console.warn('SimVar handler is not defined');
+        }
+    } catch (error) {
+        console.warn(`error SetSimVarValue ${error}`);
+    }
+    return Promise.resolve();
+};
