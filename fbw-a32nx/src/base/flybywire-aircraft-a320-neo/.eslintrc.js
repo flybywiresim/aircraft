9a634a3837df44c2fac5987e482b6b4b703a964c
Alex McBean <al.mcbean@outlook.com>
2023-02-11 16:59:01 +0000

    ESLint Fix - Removed eslintignore as it was causing issues with eslintrc - Migrated eslintignore properties to eslintrc - Created custom eslintrc for items flybywire-aircraft-a320-neo

---
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/.eslintrc.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    rules: {
+        'strict': 'off',
+        'no-unused-vars': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        'no-underscore-dangle': 'warn',
+        'camelcase': 'warn',
+    },
+};
