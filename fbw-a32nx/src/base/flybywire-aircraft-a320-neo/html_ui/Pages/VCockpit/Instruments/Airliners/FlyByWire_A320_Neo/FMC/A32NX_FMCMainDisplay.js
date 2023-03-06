a3ef033a90587764069d03ac84d29c5f0222defc
Yekouri <Trashskillz@gmail.com>
2023-03-06 01:40:50 +0100

    Fixed comment typo

--- fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -1645,7 +1645,7 @@ class FMCMainDisplay extends BaseAirliners {
 
     shouldTransmitMinimums() {
         const phase = getFlightPhaseManager().phase;
-        //Only transmit when in Cruise or later phase and within 2250NM of destination
+        //Only transmit when in Cruise or later phase and within 250NM of destination
         return (phase > FmgcFlightPhase.Cruise || (phase === FmgcFlightPhase.Cruise && this.flightPlanManager.getDistanceToDestination(FlightPlans.Active) < 250));
     }
 
