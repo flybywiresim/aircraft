b54138309a8d22cf37442ededabf0458c61699b2
Maverickwoe <Maverickwoe@users.noreply.github.com>
2023-03-08 16:16:32 +0100

    using factored components for PerHour new card for Timer

--- fbw-a32nx/src/systems/instruments/src/EFB/Failures/FailureGenerators/PerHourFailureGenerators.tsx
+++ fbw-a32nx/src/systems/instruments/src/EFB/Failures/FailureGenerators/PerHourFailureGenerators.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useSimVar } from '@instruments/common/simVars';
-import { activateRandomFailure, basicData, failureGeneratorCommonFunction, FailurePhases, findGeneratorFailures, flatten } from 'instruments/src/EFB/Failures/RandomFailureGen';
+import {
+    activateRandomFailure, basicData, FailureGeneratorCardTemplate, failureGeneratorCommonFunction,
+    FailureGeneratorFailureSetting, FailurePhases, findGeneratorFailures, flatten,
+} from 'instruments/src/EFB/Failures/RandomFailureGen';
 import { usePersistentProperty } from '@instruments/common/persistence';
-import { SimpleInput } from 'instruments/src/EFB/UtilComponents/Form/SimpleInput/SimpleInput';
-import { Trash } from 'react-bootstrap-icons';
 
 const numberOfSettingsPerGenerator = 2;
 const uniqueGenPrefix = 'E';
@@ -30,83 +31,14 @@ const eraseGenerator :(genID : number, generatorSettings : any) => void = (genID
 };
 
 const failureGeneratorCardPerHour : (genID : number, generatorSettings : any) => JSX.Element = (genID : number, generatorSettings : any) => {
-    const settings = generatorSettings.settingsPerHour;
-    const colorArmMode :string[] = [];
-    for (let i = 0; i < 4; i++) {
-        colorArmMode.push(settings[genID * numberOfSettingsPerGenerator + 0] === i ? 'bg-theme-highlight' : 'bg-theme-accent');
-    }
-    return (
-        <div className="flex flex-col flex-1 py-2 px-2 my-2 text-center rounded-md border-2 border-solid border-theme-accent mx-x">
-            <div className="flex flex-row justify-between item-center">
-                <div className="mr-4 align-left">
-                    <h2>
-                        {`${uniqueGenPrefix}${genID.toString()} : Chance per hour`}
-                    </h2>
-                </div>
-                <div className="flex flex-col text-center">
-                    <h2>Rearming</h2>
-                    <div className="flex flex-row">
-                        <button
-                            type="button"
-                            onClick={() => setNewSetting(0, generatorSettings, genID, 0)}
-                            className={`py-2 px-2 mx-0 text-center border-r-2 border-solid border-theme-highlight rounded-l-md hover:bg-theme-highlight ${colorArmMode[0]}`}
-                        >
-                            <h2>OFF</h2>
-                        </button>
-                        <button
-                            type="button"
-                            onClick={() => setNewSetting(1, generatorSettings, genID, 0)}
-                            className={`py-2 px-2 mx-0 text-center border-r-2 border-solid border-theme-highlight hover:bg-theme-highlight ${colorArmMode[1]}`}
-                        >
-                            <h2>Once</h2>
-                        </button>
-                        <button
-                            type="button"
-                            onClick={() => setNewSetting(2, generatorSettings, genID, 0)}
-                            className={`py-2 px-2 mx-0 text-center border-r-2 border-solid border-theme-highlight hover:bg-theme-highlight ${colorArmMode[2]}`}
-                        >
-                            <h2>Take-Off</h2>
-                        </button>
-                        <button
-                            type="button"
-                            onClick={() => setNewSetting(3, generatorSettings, genID, 0)}
-                            className={`py-2 px-2 mx-0 text-center rounded-r-md hover:bg-theme-highlight ${colorArmMode[3]}`}
-                        >
-                            <h2>Always</h2>
-                        </button>
-                    </div>
-                </div>
-                <button
-                    type="button"
-                    onClick={() => eraseGenerator(genID, generatorSettings)}
-                    className="flex-none mr-4 w-10 h-10 rounded-md bg-theme-accent hover:bg-utility-red"
-                >
-                    <Trash size={26} />
-                </button>
-            </div>
-            <div className="flex flex-row justify-between">
-                <div className="flex flex-col justify-between p-2 text-left border-r-2 border-r-theme-accent">
-                    <div className="break-keep">Failure per hour:</div>
-                    <div className="flex flex-row items-center">
-                        <SimpleInput
-                            className="my-2 w-40 font-mono"
-                            fontSizeClassName="text-2xl"
-                            number
-                            min={0}
-                            max={60}
-                            value={settings[genID * numberOfSettingsPerGenerator + 1]}
-                            onBlur={(x: string) => {
-                                if (!Number.isNaN(parseFloat(x) || parseFloat(x) === 0)) {
-                                    setNewSetting(parseFloat(x), generatorSettings, genID, 1);
-                                }
-                            }}
-                        />
-                        <div className="ml-2">per hour</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+    const settings = generatorSettings.settingsTakeOff;
+    const settingTable = [FailureGeneratorFailureSetting('Failure per hour:', 40, '/hour', 0, 60,
+        settings[genID * numberOfSettingsPerGenerator + 1], 1, true,
+        setNewSetting, generatorSettings, genID, 1),
+    ];
+    return FailureGeneratorCardTemplate(genID, generatorSettings, 'Chance per hour',
+        uniqueGenPrefix, numberOfSettingsPerGenerator,
+        setNewSetting, eraseGenerator, settingTable);
 };
 
 export const failureGeneratorPerHour = (generatorFailuresGetters : Map<number, string>) => {
--- fbw-a32nx/src/systems/instruments/src/EFB/Failures/FailureGenerators/TimerFailureGenerator.tsx
+++ fbw-a32nx/src/systems/instruments/src/EFB/Failures/FailureGenerators/TimerFailureGenerator.tsx
@@ -1,58 +1,72 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useSimVar } from '@instruments/common/simVars';
-import { activateRandomFailure, basicData, failureGeneratorCommonFunction, FailurePhases, findGeneratorFailures, flatten } from 'instruments/src/EFB/Failures/RandomFailureGen';
+import {
+    activateRandomFailure, basicData, FailureGeneratorCardTemplate, failureGeneratorCommonFunction, FailureGeneratorFailureSetting,
+    FailurePhases, findGeneratorFailures, flatten,
+} from 'instruments/src/EFB/Failures/RandomFailureGen';
 import { usePersistentProperty } from '@instruments/common/persistence';
 
 const numberOfSettingsPerGenerator = 2;
 const uniqueGenPrefix = 'F';
+const failureGeneratorArmed :boolean[] = [];
+const failureTime:number[] = [];
 
 export const FailureGeneratorCardsTimer : (generatorSettings: any) => JSX.Element[] = (generatorSettings : any) => {
     const htmlReturn : JSX.Element[] = [];
-    const nbGenerator = Math.floor(generatorSettings.settingsTimer.length / numberOfSettingsPerGenerator);
-    for (let i = 0; i < nbGenerator; i++) {
-        htmlReturn.push(failureGeneratorCardTimer(i));
+    const setting = generatorSettings.settingsTimer;
+    if (setting) {
+        const nbGenerator = Math.floor(setting.length / numberOfSettingsPerGenerator);
+        for (let i = 0; i < nbGenerator; i++) {
+            htmlReturn.push(failureGeneratorCardTimer(i, generatorSettings));
+        }
     }
     return htmlReturn;
 };
 
-const failureGeneratorCardTimer : (genID : number) => JSX.Element = (genID : number) => (
-    <button
-        type="button"
-        className="flex-1 py-2 px-2 mr-4 text-center rounded-md bg-theme-accent blue"
-    >
-        {`${uniqueGenPrefix}${genID.toString()}`}
-    </button>
-);
+const eraseGenerator :(genID : number, generatorSettings : any) => void = (genID : number, generatorSettings : any) => {
+    const settings : number[] = generatorSettings.settingsTimer;
+    settings.splice(genID * numberOfSettingsPerGenerator, numberOfSettingsPerGenerator);
+    generatorSettings.setSettingTimer(flatten(settings));
+    // arming
+    failureGeneratorArmed.splice(genID * numberOfSettingsPerGenerator, numberOfSettingsPerGenerator);
+};
+
+const failureGeneratorCardTimer : (genID : number, generatorSettings : any) => JSX.Element = (genID : number, generatorSettings : any) => {
+    const settings = generatorSettings.settingsTakeOff;
+    const settingTable = [FailureGeneratorFailureSetting('Delay after arming:', 40, 'second', 0, 60,
+        settings[genID * numberOfSettingsPerGenerator + 1], 1, true,
+        setNewSetting, generatorSettings, genID, 1),
+    ];
+    return FailureGeneratorCardTemplate(genID, generatorSettings, 'Timer',
+        uniqueGenPrefix, numberOfSettingsPerGenerator,
+        setNewSetting, eraseGenerator, settingTable);
+};
 
 export const failureGeneratorTimer = (generatorFailuresGetters : Map<number, string>) => {
     const [absoluteTime5s] = useSimVar('E:ABSOLUTE TIME', 'seconds', 5000);
     const [absoluteTime500ms] = useSimVar('E:ABSOLUTE TIME', 'seconds', 500);
     const { maxFailuresAtOnce, totalActiveFailures, allFailures, activate, activeFailures } = failureGeneratorCommonFunction();
     const [failureGeneratorSetting, setFailureGeneratorSetting] = usePersistentProperty('EFB_FAILURE_GENERATOR_SETTING_TIMER', '0,60');
-    const [failureGeneratorArmedTimer, setFailureGeneratorArmedTimer] = useState<boolean[]>([false, false]);
     const settingsTimer : number[] = useMemo<number[]>(() => failureGeneratorSetting.split(',').map(((it) => parseFloat(it))), [failureGeneratorSetting]);
     const nbGeneratorTimer = useMemo(() => Math.floor(settingsTimer.length / numberOfSettingsPerGenerator), [settingsTimer]);
     // Failure Specific memos
-    const [failureTime, setFailureTime] = useState<number[]>([-1, -1]);
     const { failureFlightPhase } = basicData();
 
     useEffect(() => {
         if (totalActiveFailures < maxFailuresAtOnce) {
-            const tempFailureGeneratorArmedTimerBased : boolean[] = Array.from(failureGeneratorArmedTimer);
             const tempSettings : number[] = Array.from(settingsTimer);
             let change = false;
             for (let i = 0; i < nbGeneratorTimer; i++) {
-                if (failureGeneratorArmedTimer[i] && absoluteTime5s > failureTime[i]) {
+                if (failureGeneratorArmed[i] && absoluteTime5s > failureTime[i]) {
                     activateRandomFailure(findGeneratorFailures(allFailures, generatorFailuresGetters, uniqueGenPrefix + i.toString()),
                         activate, activeFailures, uniqueGenPrefix + i.toString());
                     console.info('Time based failure triggered');
-                    tempFailureGeneratorArmedTimerBased[i] = false;
+                    failureGeneratorArmed[i] = false;
                     change = true;
                     if (tempSettings[i * numberOfSettingsPerGenerator + 0] === 1) tempSettings[i * numberOfSettingsPerGenerator + 0] = 0;
                 }
             }
             if (change) {
-                setFailureGeneratorArmedTimer(tempFailureGeneratorArmedTimerBased);
                 setFailureGeneratorSetting(flatten(tempSettings));
             }
         }
@@ -60,29 +74,33 @@ export const failureGeneratorTimer = (generatorFailuresGetters : Map<number, str
 
     useEffect(() => {
         // failureSettings once per start of takeoff
-        const tempFailureGeneratorArmed = Array.from(failureGeneratorArmedTimer);
-        const tempFailureTime = Array.from(failureTime);
-        let changed = false;
         for (let i = 0; i < nbGeneratorTimer; i++) {
-            if (!failureGeneratorArmedTimer[i] && (settingsTimer[i * numberOfSettingsPerGenerator + 0] === 1
+            if (!failureGeneratorArmed[i] && (settingsTimer[i * numberOfSettingsPerGenerator + 0] === 1
                     || (failureFlightPhase === FailurePhases.TAKEOFF && settingsTimer[i * numberOfSettingsPerGenerator + 0] === 2)
                     || settingsTimer[i * numberOfSettingsPerGenerator + 0] === 3)) {
-                tempFailureGeneratorArmed[i] = true;
-                tempFailureTime[i] = settingsTimer[i * numberOfSettingsPerGenerator + 1] + absoluteTime5s;
+                failureGeneratorArmed[i] = true;
+                failureTime[i] = settingsTimer[i * numberOfSettingsPerGenerator + 1] + absoluteTime5s;
                 console.info('Timer based failure armed at %.1f s', settingsTimer[i * numberOfSettingsPerGenerator + 1]);
-                changed = true;
             }
-        }
-        if (changed) {
-            setFailureTime(tempFailureTime);
-            setFailureGeneratorArmedTimer(tempFailureGeneratorArmed);
         }
     }, [absoluteTime500ms]);
 
     useEffect(() => {
         const generatorNumber = Math.floor(failureGeneratorSetting.split(',').length / numberOfSettingsPerGenerator);
-        const tempArmed : boolean[] = [];
-        for (let i = 0; i < generatorNumber; i++) tempArmed.push(false);
-        setFailureGeneratorArmedTimer(tempArmed);
+        for (let i = 0; i < generatorNumber; i++) failureGeneratorArmed[i] = false;
     }, []);
+};
+
+function setNewSetting(newSetting: number, generatorSettings : any, genID : number, settingIndex : number) {
+    const settings = generatorSettings.settingsTimer;
+    settings[genID * numberOfSettingsPerGenerator + settingIndex] = newSetting;
+    generatorSettings.setSettingTimer(flatten(settings));
+}
+
+export const failureGeneratorAddTimer = (generatorsSettings : any) => {
+    const additionalSetting = [0, 300];
+    if (generatorsSettings.settingsTimer === undefined || generatorsSettings.settingsTimer.length % numberOfSettingsPerGenerator !== 0 || generatorsSettings.settingsTimer.length === 0) {
+        // console.warn('Undefined generator setting, resetting');
+        generatorsSettings.setSettingTimer(flatten(additionalSetting));
+    } else generatorsSettings.setSettingTimer(flatten(generatorsSettings.settingsTimer.concat(additionalSetting)));
 };
