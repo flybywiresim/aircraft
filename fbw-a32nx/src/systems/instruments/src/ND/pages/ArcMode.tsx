f9acda6fa672c0e95bdff1e7582a4a0a88b948a6
Julian Sebline <juliansebline@hotmail.com>
2023-03-13 21:13:14 +0100

    Add of A32NX prefix to a variable

--- fbw-a32nx/src/systems/instruments/src/ND/pages/ArcMode.tsx
+++ fbw-a32nx/src/systems/instruments/src/ND/pages/ArcMode.tsx
@@ -34,7 +34,7 @@ export const ArcMode: React.FC<ArcModeProps> = ({ symbols, adirsAlign, rangeSett
     const [tcasMode] = useSimVar('L:A32NX_SWITCH_TCAS_Position', 'number');
     const [selectedHeading] = useSimVar('L:A32NX_FCU_HEADING_SELECTED', 'degrees');
     const [lsCourse] = useSimVar('L:A32NX_FM_LS_COURSE', 'number');
-    const [lsDisplayed] = useSimVar(`L:BTN_LS_${side === 'L' ? 1 : 2}_FILTER_ACTIVE`, 'bool'); // TODO rename simvar
+    const [lsDisplayed] = useSimVar(`L:A32NX_BTN_LS_${side === 'L' ? 1 : 2}_FILTER_ACTIVE`, 'bool'); // TODO rename simvar
     const [fmaLatMode] = useSimVar('L:A32NX_FMA_LATERAL_MODE', 'enum', 200);
     const [armedLateralBitmask] = useSimVar('L:A32NX_FMA_LATERAL_ARMED', 'enum', 200);
 
