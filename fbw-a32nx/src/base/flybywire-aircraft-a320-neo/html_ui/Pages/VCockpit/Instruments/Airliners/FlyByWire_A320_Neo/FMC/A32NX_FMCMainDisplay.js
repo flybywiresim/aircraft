520d492302851495cd0033212e8bcfbc7866b82b
Markus Olesen Mohr <Trashskillz@gmail.com>
2023-03-19 11:09:42 +0100

    feat(fwc): A32nx minimums exposed by fmgc (#7830)
    * Added conditional logic to set BnrArincSimVar for minimums
    
    * Fixed a typo in the condition
    
    * use same logic as approach msg on ND
    
    * Fixed comment typo
    
    * Added logic to only have one NO active for decision height
    
    * fixed missing line after merge conflict

--- fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/FMC/A32NX_FMCMainDisplay.js
@@ -215,6 +215,8 @@ class FMCMainDisplay extends BaseAirliners {
         this.arincLandingElevation = FmArinc429OutputWord.empty("LANDING_ELEVATION");
         this.arincDestinationLatitude = FmArinc429OutputWord.empty("DEST_LAT");
         this.arincDestinationLongitude = FmArinc429OutputWord.empty("DEST_LONG");
+        this.arincMDA = FmArinc429OutputWord.empty("MDA");
+        this.arincDH = FmArinc429OutputWord.empty("DH");
         this.arincThrustReductionAltitude = FmArinc429OutputWord.empty("THR_RED_ALT");
         this.arincAccelerationAltitude = FmArinc429OutputWord.empty("ACC_ALT");
         this.arincEoAccelerationAltitude = FmArinc429OutputWord.empty("EO_ACC_ALT");
@@ -230,6 +232,8 @@ class FMCMainDisplay extends BaseAirliners {
             this.arincLandingElevation,
             this.arincDestinationLatitude,
             this.arincDestinationLongitude,
+            this.arincMDA,
+            this.arincDH,
             this.arincThrustReductionAltitude,
             this.arincAccelerationAltitude,
             this.arincEoAccelerationAltitude,
@@ -238,7 +242,6 @@ class FMCMainDisplay extends BaseAirliners {
             this.arincMissedEoAccelerationAltitude,
         ];
     }
-
     Init() {
         super.Init();
         this.initVariables();
@@ -681,6 +684,7 @@ class FMCMainDisplay extends BaseAirliners {
             this.efisSymbols.update(_deltaTime);
         }
 
+        this.updateMinimums();
         this.arincBusOutputs.forEach((word) => word.writeToSimVarIfDirty());
     }
 
@@ -1714,6 +1718,21 @@ class FMCMainDisplay extends BaseAirliners {
 
             this.arincDestinationLongitude.setBnrValue(longitude ? longitude : 0, ssm, 18, 180, -180);
         }
+    }
+
+    updateMinimums() {
+        const inRange = this.shouldTransmitMinimums();
+        const MDAssm = inRange && this.perfApprMDA ? Arinc429Word.SignStatusMatrix.NormalOperation : Arinc429Word.SignStatusMatrix.NoComputedData;
+        const DHssm = inRange && this.perfApprDH ? Arinc429Word.SignStatusMatrix.NormalOperation : Arinc429Word.SignStatusMatrix.NoComputedData
+
+        this.arincMDA(this.perfApprMDA ? this.perfApprMDA : 0, MDAssm, 17, 131072, 0);
+        this.arincDH(this.perfApprDH ? this.perfApprDH : 0, DHssm, 16, 8192, 0);
+    }
+
+    shouldTransmitMinimums() {
+        const phase = getFlightPhaseManager().phase;
+        //Only transmit when in Cruise or later phase and within 250NM of destination
+        return (phase > FmgcFlightPhase.Cruise || (phase === FmgcFlightPhase.Cruise && this.flightPlanManager.getDistanceToDestination(FlightPlans.Active) < 250));
     }
 
     getClbManagedSpeedFromCostIndex() {
