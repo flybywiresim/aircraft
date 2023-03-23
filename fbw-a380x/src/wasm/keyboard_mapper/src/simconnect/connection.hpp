a395645516c46d8e5b5dd9403a2004a0a1eac546
Sven Czarnian <devel@svcz.de>
2023-03-23 21:09:08 +0100

    change it to an header-only library

--- fbw-a380x/src/wasm/keyboard_mapper/src/simconnect/connection.hpp
+++ fbw-a380x/src/wasm/keyboard_mapper/src/simconnect/connection.hpp
@@ -34,15 +34,91 @@ class Connection {
   std::map<std::uint32_t, std::shared_ptr<InputEventGroupBase>> _inputEventGroups;
   std::list<std::shared_ptr<LVarObjectBase>> _lvarObjects;
 
-  void updateLVarObjects();
-  void processSimObjectData(const SIMCONNECT_RECV_SIMOBJECT_DATA* data);
-  void processClientData(const SIMCONNECT_RECV_CLIENT_DATA* data);
-  void processDispatchMessage(SIMCONNECT_RECV* pData);
+  void updateLVarObjects() {
+    std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+
+    for (auto& object : this->_lvarObjects) {
+      if (object->updateRequired(now)) {
+        object->update(now);
+      }
+    }
+  }
+
+  void processSimObjectData(const SIMCONNECT_RECV_SIMOBJECT_DATA* data) {
+    auto object = this->_simObjects.find(data->dwRequestID);
+    if (object != this->_simObjects.end()) {
+      object->second->receivedData((void*)&data->dwData);
+    } else {
+      std::cout << "TERR ON ND: Unknown request ID in SimConnect connection: " << std::to_string(data->dwRequestID) << std::endl;
+    }
+  }
+
+  void processClientData(const SIMCONNECT_RECV_CLIENT_DATA* data) {
+    auto area = this->_clientDataAreas.find(data->dwRequestID);
+    if (area != this->_clientDataAreas.end()) {
+      area->second->receivedData((void*)&data->dwData);
+    } else {
+      std::cout << "TERR ON ND: Unknown request ID in SimConnect connection: " << std::to_string(data->dwRequestID) << std::endl;
+    }
+  }
+
+  void processSimEvent(const SIMCONNECT_RECV_EVENT* data) {
+    std::cout << "TERR ON ND: sim event: " << std::to_string(data->uGroupID) << std::endl;
+    auto group = this->_inputEventGroups.find(data->uGroupID);
+    if (group != this->_inputEventGroups.end()) {
+      if (!group->second->processEvent(data->uEventID)) {
+        std::cout << "TERR ON ND: Unknown event id" << std::endl;
+      }
+    } else {
+      std::cout << "TERR ON ND: Unknown group id" << std::endl;
+    }
+  }
+
+  void processDispatchMessage(SIMCONNECT_RECV* pData) {
+    switch (static_cast<SIMCONNECT_RECV_ID>(pData->dwID)) {
+      case SIMCONNECT_RECV_ID_OPEN:
+        std::cout << "TERR ON ND: SimConnect connection established" << std::endl;
+        break;
+      case SIMCONNECT_RECV_ID_QUIT:
+        std::cout << "TERR ON ND: Received SimConnect connection quit message" << std::endl;
+        this->disconnect();
+        break;
+      case SIMCONNECT_RECV_ID_CLIENT_DATA:
+        this->processClientData(static_cast<SIMCONNECT_RECV_CLIENT_DATA*>(pData));
+        break;
+      case SIMCONNECT_RECV_ID_SIMOBJECT_DATA:
+        this->processSimObjectData(static_cast<SIMCONNECT_RECV_SIMOBJECT_DATA*>(pData));
+        break;
+      case SIMCONNECT_RECV_ID_EVENT:
+        this->processSimEvent(static_cast<SIMCONNECT_RECV_EVENT*>(pData));
+        break;
+      case SIMCONNECT_RECV_ID_EXCEPTION:
+        std::cout << "TERR ON ND: Exception in SimConnect connection" << std::endl;
+        std::cout << "TERR ON ND: Exception: " << std::to_string(static_cast<SIMCONNECT_RECV_EXCEPTION*>(pData)->dwException) << std::endl;
+        std::cout << "TERR ON ND: Size: " << std::to_string(static_cast<SIMCONNECT_RECV_EXCEPTION*>(pData)->dwSize) << std::endl;
+        std::cout << "TERR ON ND: Send ID: " << std::to_string(static_cast<SIMCONNECT_RECV_EXCEPTION*>(pData)->dwSendID) << std::endl;
+        std::cout << "TERR ON ND: Index: " << std::to_string(static_cast<SIMCONNECT_RECV_EXCEPTION*>(pData)->dwIndex) << std::endl;
+        break;
+      default:
+        std::cout << "TERR ON ND: unknown message " << std::to_string(pData->dwID) << std::endl;
+        break;
+    }
+  }
 
  public:
-  Connection();
+  Connection()
+      : _connection(0),
+        _lastSimObjectId(0),
+        _lastClientDataId(0),
+        _lastClientDataDefinitionId(0),
+        _lastInputEventGroupId(0),
+        _simObjects(),
+        _clientDataAreas(),
+        _inputEventGroups(),
+        _lvarObjects() {}
   Connection(const Connection&) = delete;
-  ~Connection();
+
+  ~Connection() { this->disconnect(); }
 
   Connection& operator=(const Connection&) = delete;
 
@@ -53,18 +129,57 @@ class Connection {
    * @return true if the connection is established
    * @return false if the connection failed
    */
-  bool connect(const std::string& connectionName);
+  bool connect(const std::string& connectionName) {
+    if (this->_connection != 0) {
+      return true;
+    }
+
+    HRESULT result;
+    result = SimConnect_Open(&this->_connection, connectionName.c_str(), nullptr, 0, 0, 0);
+    return SUCCEEDED(result);
+  }
+
   /**
    * @brief Disconnects from the simconnect server
    */
-  void disconnect();
+  void disconnect() {
+    if (this->_connection != 0) {
+      SimConnect_Close(this->_connection);
+
+      this->_connection = 0;
+      this->_lastSimObjectId = 0;
+      this->_lastClientDataId = 0;
+      this->_lastClientDataDefinitionId = 0;
+      this->_lastInputEventGroupId = 0;
+      this->_simObjects.clear();
+      this->_clientDataAreas.clear();
+      this->_lvarObjects.clear();
+      this->_inputEventGroups.clear();
+    }
+  }
+
   /**
    * @brief Processes the buffered data that is received during to readData calls
    * The registered areas and objects are automatically update
    * @return true if data processing was possible
    * @return false if something failed
    */
-  bool readData();
+  bool readData() {
+    if (this->_connection == 0) {
+      return false;
+    }
+
+    DWORD cbData;
+    SIMCONNECT_RECV* pData;
+
+    while (SUCCEEDED(SimConnect_GetNextDispatch(this->_connection, &pData, &cbData))) {
+      this->processDispatchMessage(pData);
+    }
+
+    this->updateLVarObjects();
+
+    return true;
+  }
 
   /**
    * @brief Creates an object that handles simulator data
