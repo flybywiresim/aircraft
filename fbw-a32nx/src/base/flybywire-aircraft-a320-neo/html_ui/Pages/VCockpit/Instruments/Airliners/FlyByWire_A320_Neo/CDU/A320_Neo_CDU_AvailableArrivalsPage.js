d4117885c79f68da60394624c989f43e6e2a5d5f
Benjamin Dupont <benjozorkfr@gmail.com>
2023-03-26 09:38:51 -0400

    remove calls to non-existent functions

--- fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_AvailableArrivalsPage.js
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_AvailableArrivalsPage.js
@@ -280,7 +280,6 @@ class CDUAvailableArrivalsPage {
                 mcdu.insertTemporaryFlightPlan(() => {
                     mcdu.updateTowerHeadwind();
                     mcdu.updateConstraints();
-                    CDUPerformancePage.UpdateThrRedAccFromDestination(mcdu);
                     CDUFlightPlanPage.ShowPage(mcdu, 0, forPlan);
                 });
             };
@@ -416,7 +415,6 @@ class CDUAvailableArrivalsPage {
                 mcdu.insertTemporaryFlightPlan(() => {
                     mcdu.updateTowerHeadwind();
                     mcdu.updateConstraints();
-                    CDUPerformancePage.UpdateThrRedAccFromDestination(mcdu);
                     CDUAvailableArrivalsPage.ShowPage(mcdu, airport, 0, true, forPlan, inAlternate);
                 });
             };
