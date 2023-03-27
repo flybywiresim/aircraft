582592413835a34d8ba5ff3b5adef62c6989ae82
Benjamin Dupont <benjozorkfr@gmail.com>
2023-03-26 23:02:53 -0400

    remove msfssdk fork

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,17 +2,6 @@
 
 set -ex
 
-cd fbw-common/msfs-avionics-mirror/src/msfstypes
-npm pack
-cd ../sdk
-rm -rf node_modules
-rm -rf build
-npm ci
-npm run build
-cp package.json build/
-cd build
-npm pack
-
 cd /external
 rm -rf node_modules
 npm i
