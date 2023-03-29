d7529e60ba6debc42169e273dbe70fe51a0f4ce0
Maximilian Reuter <maximilian.a.reuter@gmail.com>
2023-03-29 04:03:19 +0200

    Moved igniter logic into A32NX_FADEC.js, enabled FADEC GND PWR Button functionality Added Igniter switching on startup Autostart now only uses one Igniter Crank mode also powers FADEC

--- fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/A32NX_FADEC.js
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/A32NX_FADEC.js
@@ -3,6 +3,7 @@ class A32NX_FADEC {
         this.engine = engine;
         this.fadecTimer = -1;
         this.dcEssPoweredInPreviousUpdate = false;
+        this.lastActiveIgniterAutostart = 0 // 0 = A, 1 = B
     }
 
     init() {
@@ -11,15 +12,22 @@ class A32NX_FADEC {
 
     update(deltaTime) {
         const dcEssIsPowered = this.isDcEssPowered();
-        const masterState = SimVar.GetSimVarValue("FUELSYSTEM VALVE SWITCH:" + (this.engine), "Bool");
-        const ignitionState = SimVar.GetSimVarValue("L:XMLVAR_ENG_MODE_SEL", "Enum") == 2;
+        const ignitionState = SimVar.GetSimVarValue("L:XMLVAR_ENG_MODE_SEL", "Enum") === 2;
+        const engineState = SimVar.GetSimVarValue(`L:A32NX_ENGINE_STATE:${this.engine}`,"Number");
+        const n2Percent = SimVar.GetSimVarValue(`L:A32NX_ENGINE_N2:${this.engine}`,"Percent");
 
-        if ((this.dcEssPoweredInPreviousUpdate != dcEssIsPowered && dcEssIsPowered == 1) || this.lastMasterState != masterState) {
+        if ((this.dcEssPoweredInPreviousUpdate !== dcEssIsPowered && dcEssIsPowered === 1) ||
+            (this.lastEngineState !== engineState && engineState === 4) ) {
             this.fadecTimer = 5 * 60;
         }
+        if ((this.lastEngineState === 2 || this.lastEngineState === 3) && engineState !== 2 && engineState !== 3){
+            this.lastActiveIgniterAutostart ^= 1 // toggles Igniter
+        }
 
-        if (this.lastIgnitionState != ignitionState && !ignitionState) {
-            this.fadecTimer = 30;
+        this.igniting = ignitionState && (engineState === 2 || engineState === 3) && n2Percent > 18 && n2Percent < 55;
+
+        if (this.lastIgnitionState !== ignitionState && !ignitionState) {
+            this.fadecTimer = Math.max(30,this.fadecTimer);
         }
         this.fadecTimer -= deltaTime / 1000;
         this.updateSimVars();
@@ -27,19 +35,24 @@ class A32NX_FADEC {
     }
 
     updateSimVars() {
-        this.lastMasterState = SimVar.GetSimVarValue("FUELSYSTEM VALVE SWITCH:" + (this.engine), "Bool");
-        this.lastIgnitionState = SimVar.GetSimVarValue("L:XMLVAR_ENG_MODE_SEL", "Enum") == 2;
-        SimVar.SetSimVarValue("L:A32NX_FADEC_POWERED_ENG" + this.engine, "Bool", this.isPowered() ? 1 : 0);
+        this.lastIgnitionState = SimVar.GetSimVarValue("L:XMLVAR_ENG_MODE_SEL", "Enum") === 2;
+        this.lastEngineState = SimVar.GetSimVarValue(`L:A32NX_ENGINE_STATE:${this.engine}`,"Number");
+        SimVar.SetSimVarValue(`L:A32NX_FADEC_POWERED_ENG${this.engine}`, "Bool", this.isPowered() ? 1 : 0);
+        SimVar.SetSimVarValue(`L:A32NX_FADEC_IGNITER_A_ACTIVE_ENG${this.engine}`, "Bool", this.igniting && this.lastActiveIgniterAutostart === 0 ? 1 : 0);
+        SimVar.SetSimVarValue(`L:A32NX_FADEC_IGNITER_B_ACTIVE_ENG${this.engine}`, "Bool", this.igniting && this.lastActiveIgniterAutostart === 1 ? 1 : 0);
     }
 
     isPowered() {
-        if (SimVar.GetSimVarValue("L:A32NX_FIRE_BUTTON_ENG" + this.engine, "Bool") == 1) {
+        if (SimVar.GetSimVarValue(`L:A32NX_FIRE_BUTTON_ENG${this.engine}`, "Bool") === 1) {
             return false;
         }
-        if (SimVar.GetSimVarValue("TURB ENG N2:" + this.engine, "Percent") > 15) {
+        if (SimVar.GetSimVarValue(`TURB ENG N2:${this.engine}`, "Percent") > 15) {
             return true;
         }
-        if (SimVar.GetSimVarValue("L:XMLVAR_ENG_MODE_SEL", "Enum") == 2) {
+        if (SimVar.GetSimVarValue("L:XMLVAR_ENG_MODE_SEL", "Enum") !== 1) {
+            return true;
+        }
+        if (SimVar.GetSimVarValue(`L:A32NX_OVHD_FADEC_${this.engine}`, "Bool")) {
             return true;
         }
         if (this.fadecTimer > 0) {
