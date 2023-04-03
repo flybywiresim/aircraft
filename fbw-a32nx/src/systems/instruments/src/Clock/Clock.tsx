c9c77657028c28ede786de7a632337feeabf217e
DESKTOP-7L17AJC\FinalLight <TufanTunca90@gmail.com>
2023-04-04 00:15:32 +0200

    edited overhead emis ambient, and clock turning only on bus power.

--- fbw-a32nx/src/systems/instruments/src/Clock/Clock.tsx
+++ fbw-a32nx/src/systems/instruments/src/Clock/Clock.tsx
@@ -11,42 +11,41 @@ interface ClockProps extends ComponentProps {
 }
 
 export class ClockRoot extends DisplayComponent<ClockProps> {
-    private readonly gElementRef = FSComponent.createRef<SVGGElement>();
+  private readonly gElementRef = FSComponent.createRef<SVGGElement>();
 
-    onAfterRender(node: VNode): void {
-        super.onAfterRender(node);
+  onAfterRender(node: VNode): void {
+    super.onAfterRender(node);
 
-        const sub = this.props.bus.getSubscriber<ClockSimvars>();
-        sub.on('dcEssIsPowered').whenChanged().handle((dccEssIsPowered) => {
-            if (dccEssIsPowered) {
-                this.gElementRef.instance.classList.add('on');
-                this.gElementRef.instance.classList.remove('off');
-            } else {
-                this.gElementRef.instance.classList.add('off');
-                this.gElementRef.instance.classList.remove('on');
-            }
-        });
+    const sub = this.props.bus.getSubscriber<ClockSimvars>();
+    sub.on("dcEssIsPowered").whenChanged().handle((dccEssIsPowered) => {
+      const svgElement = this.gElementRef.instance.closest("svg");
+      if (dccEssIsPowered) {
+        svgElement.style.opacity = "1";
+      } else {
+        svgElement.style.opacity = "0";
+      }
+    });
 
-        sub.on('timeOfDay').whenChanged().handle((timeOfDay) => {
-            if (timeOfDay === 1 || timeOfDay === 2) {
-                this.gElementRef.instance.classList.add('day');
-                this.gElementRef.instance.classList.remove('night');
-            } else {
-                this.gElementRef.instance.classList.add('night');
-                this.gElementRef.instance.classList.remove('day');
-            }
-        });
-    }
+    sub.on("timeOfDay").whenChanged().handle((timeOfDay) => {
+      if (timeOfDay === 1 || timeOfDay === 2) {
+        this.gElementRef.instance.classList.add("day");
+        this.gElementRef.instance.classList.remove("night");
+      } else {
+        this.gElementRef.instance.classList.add("night");
+        this.gElementRef.instance.classList.remove("day");
+      }
+    });
+  }
 
-    render(): VNode {
-        return (
-            <svg version="1.1" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
-                <g ref={this.gElementRef}>
-                    <Chrono bus={this.props.bus} />
-                    <Clock bus={this.props.bus} />
-                    <ElapsedTime bus={this.props.bus} />
-                </g>
-            </svg>
-        );
-    }
+  render(): VNode {
+    return (
+      <svg version="1.1" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+        <g ref={this.gElementRef}>
+          <Chrono bus={this.props.bus} />
+          <Clock bus={this.props.bus} />
+          <ElapsedTime bus={this.props.bus} />
+        </g>
+      </svg>
+    );
+  }
 }
