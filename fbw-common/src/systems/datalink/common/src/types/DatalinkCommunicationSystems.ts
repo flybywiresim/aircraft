9af3970b335acdcd104a0eb0d4962959544c043f
Sven Czarnian <devel@svcz.de>
2023-05-07 16:43:45 +0200

    use enums instead of strings and integers

---
+++ fbw-common/src/systems/datalink/common/src/types/DatalinkCommunicationSystems.ts
@@ -0,0 +1,9 @@
+//  Copyright (c) 2022 FlyByWire Simulations
+//  SPDX-License-Identifier: GPL-3.0
+
+export enum DatalinkCommunicationSystems {
+    None = 0,
+    VHF = 1,
+    HF = 2,
+    SATCOM = 3,
+}
