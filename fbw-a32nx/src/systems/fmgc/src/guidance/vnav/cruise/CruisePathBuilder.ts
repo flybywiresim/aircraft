c75d4fa054107cba5032eb887c07289baee0c5d8
Andreas Guther <andreas@guther.family>
2023-05-10 22:42:50 +0200

    feat: vertical navigation v1 (#7080)
    * feat: vertical navigation v1 #7080
    
    * fix(nd): vnav uses groundSpeed
    
    * Fix vertical angle missing on leg
    
    * Start using VNAV speed predictions
    
    * Trigger path recomputation for speed limit edits
    
    * Use INIT FUEL PRED for dest EFOB estimate
    
    * Start moving profile building out of VnavDriver
    
    * Change profile references
    
    * Fix intercept checkpoint not showing up
    
    * Fix continue descent not showing up
    
    * Fix FCU alt arrow not showing up
    
    * Use proper dep/dest elevations
    
    * Fix rebase issue
    
    * Intercept MCDU with descent profile
    
    * Fix tactical path constraint handling edge case
    
    * Fix level segments violating alt constraint
    
    * Fix update rate of yoyo
    
    * Fix descent SPD LIM PWP missing
    
    * Fix speed change dot missing
    
    * Re-add EXP CLB prediction
    
    * Refactor to use variables from observer
    
    * Fix level off arrow in selected modes
    
    * Fix tactical predictions in HDG
    
    * Fix SPD LIM interpolation
    
    * Predict approach path in tactical profiles
    
    * Consider discontinuities in speed predictions
    
    * Use new thr red/acc alt variables
    
    * Add comments about configuration options
    
    * Fix alt constraint violation
    
    * Add changelog entry
    
    * Rename PopUp to PopUpDialog
    
    * Remove unused comment
    
    * Remove old file
    
    * Consolidate fundamental prediction code
    
    * Fix missing callback after rebase
    
    * Fix build after removed file
    
    * Remove unused import
    
    * Fix my own code review comments
    
    * Implement first round of review suggestions
    
    * Implement more code review suggestions
    
    * Use subjects for everything
    
    * Fix typos
    
    * Fix CSS issues again
    
    * Refactor step altitude implementation
    
    * Fix step altitude bugs
    
    * Ignore steps before T/C and after T/D
    
    * Use AP's pressure alt variable
    
    ---------
    
    Co-authored-by: BBK <22713769+BlueberryKing@users.noreply.github.com>
    Co-authored-by: Michael Corcoran <tracer@outlook.co.nz>
    Co-authored-by: Benedict Etzel <developer@beheh.de>

---
+++ fbw-a32nx/src/systems/fmgc/src/guidance/vnav/cruise/CruisePathBuilder.ts
@@ -0,0 +1,228 @@
+import { VerticalProfileComputationParametersObserver } from '@fmgc/guidance/vnav/VerticalProfileComputationParameters';
+import { VnavConfig } from '@fmgc/guidance/vnav/VnavConfig';
+import { ClimbStrategy } from '@fmgc/guidance/vnav/climb/ClimbStrategy';
+import { DescentStrategy } from '@fmgc/guidance/vnav/descent/DescentStrategy';
+import { ManagedSpeedType, SpeedProfile } from '@fmgc/guidance/vnav/climb/SpeedProfile';
+import { EngineModel } from '@fmgc/guidance/vnav/EngineModel';
+import { WindComponent } from '@fmgc/guidance/vnav/wind';
+import { TemporaryCheckpointSequence } from '@fmgc/guidance/vnav/profile/TemporaryCheckpointSequence';
+import { HeadwindProfile } from '@fmgc/guidance/vnav/wind/HeadwindProfile';
+import { Predictions, StepResults } from '../Predictions';
+import { GeographicCruiseStep, MaxSpeedConstraint, NavGeometryProfile, VerticalCheckpoint, VerticalCheckpointReason } from '../profile/NavGeometryProfile';
+import { AtmosphericConditions } from '../AtmosphericConditions';
+
+export class CruisePathBuilder {
+    constructor(private computationParametersObserver: VerticalProfileComputationParametersObserver,
+        private atmosphericConditions: AtmosphericConditions) { }
+
+    computeCruisePath(
+        profile: NavGeometryProfile,
+        startOfCruise: VerticalCheckpoint,
+        targetDistanceFromStart: NauticalMiles,
+        stepClimbStrategy: ClimbStrategy,
+        stepDescentStrategy: DescentStrategy,
+        speedProfile: SpeedProfile,
+        windProfile: HeadwindProfile,
+    ): TemporaryCheckpointSequence {
+        const sequence = new TemporaryCheckpointSequence(startOfCruise);
+
+        for (const step of profile.cruiseSteps) {
+            if (step.isIgnored) {
+                continue;
+            }
+
+            // If the step is too close to T/D
+            if (step.distanceFromStart < startOfCruise.distanceFromStart || step.distanceFromStart > targetDistanceFromStart) {
+                this.ignoreCruiseStep(profile, step);
+                continue;
+            }
+
+            // See if there are any speed constraints before the step
+            for (const speedConstraint of profile.maxClimbSpeedConstraints) {
+                if (speedConstraint.distanceFromStart > step.distanceFromStart) {
+                    continue;
+                }
+
+                this.addSegmentToSpeedConstraint(sequence, speedConstraint, speedProfile, windProfile);
+            }
+
+            const { distanceFromStart, altitude, remainingFuelOnBoard } = sequence.lastCheckpoint;
+
+            const speed = speedProfile.getTarget(distanceFromStart, altitude, ManagedSpeedType.Cruise);
+            const headwind = windProfile.getHeadwindComponent(distanceFromStart, altitude);
+            const segmentToStep = this.computeCruiseSegment(altitude, step.distanceFromStart - distanceFromStart, remainingFuelOnBoard, speed, headwind);
+            sequence.addCheckpointFromStep(segmentToStep, VerticalCheckpointReason.AtmosphericConditions);
+
+            const addingStepSuccessful = this.tryAddStepFromLastCheckpoint(sequence, step, stepClimbStrategy, stepDescentStrategy, targetDistanceFromStart);
+            if (!addingStepSuccessful) {
+                this.ignoreCruiseStep(profile, step);
+                continue;
+            }
+        }
+
+        // Once again, we check if there are any speed constraints before the T/D
+        for (const speedConstraint of profile.maxClimbSpeedConstraints) {
+            // If speed constraint does not lie along the remaining cruise track
+            if (speedConstraint.distanceFromStart > targetDistanceFromStart) {
+                continue;
+            }
+
+            this.addSegmentToSpeedConstraint(sequence, speedConstraint, speedProfile, windProfile);
+        }
+
+        if (sequence.lastCheckpoint.distanceFromStart >= targetDistanceFromStart) {
+            return sequence;
+        }
+
+        const speedTarget = speedProfile.getTarget(
+            sequence.lastCheckpoint.distanceFromStart,
+            sequence.lastCheckpoint.altitude,
+            ManagedSpeedType.Cruise,
+        );
+
+        if (speedTarget - sequence.lastCheckpoint.speed > 1) {
+            const accelerationStep = this.levelAccelerationStep(
+                sequence.lastCheckpoint.distanceFromStart,
+                sequence.lastCheckpoint.speed,
+                speedTarget,
+                windProfile.getHeadwindComponent(sequence.lastCheckpoint.distanceFromStart, sequence.lastCheckpoint.distanceFromStart),
+            );
+
+            sequence.addCheckpointFromStep(accelerationStep, VerticalCheckpointReason.AtmosphericConditions);
+        }
+
+        if (VnavConfig.DEBUG_PROFILE && targetDistanceFromStart < sequence.lastCheckpoint.distanceFromStart) {
+            console.warn('[FMS/VNAV] An acceleration step in the cruise took us past T/D. This is not implemented properly yet. Blame BBK');
+        }
+
+        const step = this.computeCruiseSegment(
+            sequence.lastCheckpoint.altitude,
+            targetDistanceFromStart - sequence.lastCheckpoint.distanceFromStart,
+            startOfCruise.remainingFuelOnBoard,
+            speedTarget,
+            windProfile.getHeadwindComponent(sequence.lastCheckpoint.distanceFromStart, sequence.lastCheckpoint.altitude),
+        );
+
+        sequence.addCheckpointFromStep(step, VerticalCheckpointReason.AtmosphericConditions);
+
+        return sequence;
+    }
+
+    private addSegmentToSpeedConstraint(sequence: TemporaryCheckpointSequence, speedConstraint: MaxSpeedConstraint, speedProfile: SpeedProfile, windProfile: HeadwindProfile) {
+        const { distanceFromStart, altitude, remainingFuelOnBoard } = sequence.lastCheckpoint;
+
+        if (speedConstraint.distanceFromStart < distanceFromStart) {
+            return;
+        }
+
+        const speed = speedProfile.getTarget(distanceFromStart, altitude, ManagedSpeedType.Cruise);
+        const segmentResult = this.computeCruiseSegment(
+            altitude,
+            speedConstraint.distanceFromStart - distanceFromStart,
+            remainingFuelOnBoard,
+            speed,
+            windProfile.getHeadwindComponent(distanceFromStart, altitude),
+        );
+
+        sequence.addCheckpointFromStep(segmentResult, VerticalCheckpointReason.SpeedConstraint);
+    }
+
+    /**
+     * Compute a cruise step segment and add it unless it is too close to T/D.
+     * @param sequence
+     * @param step
+     * @param stepClimbStrategy
+     * @param stepDescentStrategy
+     * @param maxDistance
+     * @returns
+     */
+    private tryAddStepFromLastCheckpoint(
+        sequence: TemporaryCheckpointSequence, step: GeographicCruiseStep, stepClimbStrategy: ClimbStrategy, stepDescentStrategy: DescentStrategy, maxDistance: NauticalMiles,
+    ) {
+        const { managedCruiseSpeed, managedCruiseSpeedMach } = this.computationParametersObserver.get();
+        const { altitude, distanceFromStart, remainingFuelOnBoard } = sequence.lastCheckpoint;
+
+        const isClimbVsDescent = step.toAltitude > altitude;
+        // Instead of just atmospheric conditions, the last checkpoint is now a step climb point
+        if (sequence.lastCheckpoint.reason === VerticalCheckpointReason.AtmosphericConditions) {
+            sequence.lastCheckpoint.reason = isClimbVsDescent
+                ? VerticalCheckpointReason.StepClimb
+                : VerticalCheckpointReason.StepDescent;
+        }
+
+        const stepResults = isClimbVsDescent
+            ? stepClimbStrategy.predictToAltitude(altitude, step.toAltitude, managedCruiseSpeed, managedCruiseSpeedMach, remainingFuelOnBoard, WindComponent.zero())
+            : stepDescentStrategy.predictToAltitude(altitude, step.toAltitude, managedCruiseSpeed, managedCruiseSpeed, remainingFuelOnBoard, WindComponent.zero());
+
+        // If the step end is closer than 50 NM to T/D, the step is ignored.
+        if (distanceFromStart + stepResults.distanceTraveled - maxDistance > -50) {
+            return false;
+        }
+
+        sequence.addCheckpointFromStep(stepResults, isClimbVsDescent ? VerticalCheckpointReason.TopOfStepClimb : VerticalCheckpointReason.BottomOfStepDescent);
+
+        return true;
+    }
+
+    private computeCruiseSegment(altitude: Feet, distance: NauticalMiles, remainingFuelOnBoard: number, speed: Knots, headwind: WindComponent): StepResults {
+        const { zeroFuelWeight, managedCruiseSpeedMach, tropoPause } = this.computationParametersObserver.get();
+
+        return Predictions.levelFlightStep(
+            altitude,
+            distance,
+            speed,
+            managedCruiseSpeedMach,
+            zeroFuelWeight,
+            remainingFuelOnBoard,
+            headwind.value,
+            this.atmosphericConditions.isaDeviation,
+            tropoPause,
+        );
+    }
+
+    private levelAccelerationStep(remainingFuelOnBoard: number, speed: Knots, finalSpeed: Knots, headwind: WindComponent): StepResults {
+        const { zeroFuelWeight, cruiseAltitude, managedCruiseSpeedMach, tropoPause } = this.computationParametersObserver.get();
+
+        return Predictions.speedChangeStep(
+            0,
+            cruiseAltitude,
+            speed,
+            finalSpeed,
+            managedCruiseSpeedMach,
+            managedCruiseSpeedMach,
+            this.getClimbThrustN1Limit(this.atmosphericConditions, cruiseAltitude, speed),
+            zeroFuelWeight,
+            remainingFuelOnBoard,
+            headwind.value,
+            this.atmosphericConditions.isaDeviation,
+            tropoPause,
+        );
+    }
+
+    getFinalCruiseAltitude(cruiseSteps: GeographicCruiseStep[]): Feet {
+        const { cruiseAltitude } = this.computationParametersObserver.get();
+
+        let altitude = cruiseAltitude;
+        for (let i = 0; i < cruiseSteps.length; i++) {
+            const step = cruiseSteps[i];
+            if (!step.isIgnored) {
+                altitude = step.toAltitude;
+            }
+        }
+
+        return altitude;
+    }
+
+    private getClimbThrustN1Limit(atmosphericConditions: AtmosphericConditions, altitude: Feet, speed: Knots) {
+        // This Mach number is the Mach number for the predicted climb speed, not the Mach to use after crossover altitude.
+        const climbSpeedMach = atmosphericConditions.computeMachFromCas(altitude, speed);
+        const estimatedTat = atmosphericConditions.totalAirTemperatureFromMach(altitude, climbSpeedMach);
+
+        return EngineModel.tableInterpolation(EngineModel.maxClimbThrustTableLeap, estimatedTat, altitude);
+    }
+
+    private ignoreCruiseStep(profile: NavGeometryProfile, step: GeographicCruiseStep) {
+        step.isIgnored = true;
+        profile.ignoreCruiseStep(step.waypointIndex);
+    }
+}
