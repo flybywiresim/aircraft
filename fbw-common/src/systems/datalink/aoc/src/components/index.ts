70fc5f06e770776d97118dc3aca4a58f6f9d6437
Sven Czarnian <devel@svcz.de>
2023-05-12 18:32:11 +0200

    export the OFP handler

--- fbw-common/src/systems/datalink/aoc/src/components/index.ts
+++ fbw-common/src/systems/datalink/aoc/src/components/index.ts
@@ -1,5 +1,6 @@
 //  Copyright (c) 2023 FlyByWire Simulations
 //  SPDX-License-Identifier: GPL-3.0
 
+export * from './OperationalFlightPlan';
 export * from './OutOffOnIn';
 export * from './Sensors';
