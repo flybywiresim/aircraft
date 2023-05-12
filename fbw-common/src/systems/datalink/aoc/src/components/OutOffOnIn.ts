c99548f6e85924f6ba0f9c3dcb378d95ecf046b1
Sven Czarnian <devel@svcz.de>
2023-05-12 08:39:40 +0200

    introduce the structure for the OOOI system

---
+++ fbw-common/src/systems/datalink/aoc/src/components/OutOffOnIn.ts
@@ -0,0 +1,88 @@
+//  Copyright (c) 2023 FlyByWire Simulations
+//  SPDX-License-Identifier: GPL-3.0
+
+import { OutOffOnInMessage, OooiState, AtsuTimestamp } from '@datalink/common';
+import { Sensors } from './Sensors';
+import { DigitalInputs } from '../DigitalInputs';
+
+class OooiStateMachine {
+    public CurrentState: OooiState = OooiState.Unknown;
+
+    private standStillTimestamp: AtsuTimestamp = null;
+
+    constructor(private digitalInputs: DigitalInputs, private sensors: Sensors) { }
+
+    public powerDown(): void {
+        /* ensure that power-down calls during sim startup are not misinterpreted */
+        if (this.CurrentState !== OooiState.Unknown) this.CurrentState = OooiState.InGate;
+    }
+
+    public update(): void {
+        switch (this.CurrentState) {
+        case OooiState.Unknown:
+            const coldAndDark = SimVar.GetSimVarValue('A32NX_COLD_AND_DARK_SPAWN', 'Bool');
+            if (coldAndDark) {
+                this.CurrentState = OooiState.InGate;
+            } else {
+                this.CurrentState = OooiState.OutGate;
+            }
+            break;
+        case OooiState.OutGate:
+            if (this.sensors.NoseGearDown === false) this.CurrentState = OooiState.OffGround;
+            break;
+        case OooiState.OffGround:
+            if (this.sensors.NoseGearDown === true) this.CurrentState = OooiState.OnGround;
+            break;
+        case OooiState.OnGround:
+            if (this.sensors.GroundSpeed.isNormalOperation() === true && this.sensors.GroundSpeed.value === 0) {
+                if (this.standStillTimestamp !== null) {
+                    const current = AtsuTimestamp.fromClock(this.digitalInputs.UtcClock);
+                    const difference = AtsuTimestamp.difference(current, this.standStillTimestamp);
+
+                    /* ensure that we are standing still for the last 30 seconds */
+                    if (difference >= 30) {
+                        this.CurrentState = OooiState.InGate;
+                    }
+                } else {
+                    this.standStillTimestamp = AtsuTimestamp.fromClock(this.digitalInputs.UtcClock);
+                }
+            } else {
+                this.standStillTimestamp = null;
+            }
+            break;
+        case OooiState.InGate:
+            if (this.sensors.ParkingBrakeSet === false) {
+                /* check if the aircraft moving */
+                if (this.sensors.GroundSpeed.isNormalOperation() === true && this.sensors.GroundSpeed.value !== 0) {
+                    this.CurrentState = OooiState.OutGate;
+                }
+            }
+            break;
+        default:
+            this.CurrentState = OooiState.Unknown;
+            break;
+        }
+    }
+}
+
+export class OutOffOnIn {
+    private stateMachine: OooiStateMachine = null;
+
+    private lastMessage: OutOffOnInMessage = null;
+
+    private currentMessage: OutOffOnInMessage = null;
+
+    constructor(digitalInputs: DigitalInputs, private sensors: Sensors) {
+        this.stateMachine = new OooiStateMachine(digitalInputs, this.sensors);
+    }
+
+    public powerDown(): void {
+        this.stateMachine.powerDown();
+        this.lastMessage = null;
+        this.currentMessage = null;
+    }
+
+    public update(): void {
+        this.stateMachine.update();
+    }
+}
--- fbw-common/src/systems/datalink/aoc/src/components/index.ts
+++ fbw-common/src/systems/datalink/aoc/src/components/index.ts
@@ -1,4 +1,5 @@
 //  Copyright (c) 2023 FlyByWire Simulations
 //  SPDX-License-Identifier: GPL-3.0
 
+export * from './OutOffOnIn';
 export * from './Sensors';
