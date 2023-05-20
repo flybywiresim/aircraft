d1aab5585a7f2c3cf16aa703a627858ec1682ee1
Florian Gross <flogross89@gmail.com>
2023-05-21 01:32:37 +0300

    change Button's handling of disabled prop

--- fbw-a380x/src/systems/instruments/src/MFD/pages/common/Button.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/common/Button.tsx
@@ -1,4 +1,4 @@
-﻿import { ComponentProps, DisplayComponent, FSComponent, Subscribable, VNode } from '@microsoft/msfs-sdk';
+﻿import { ComponentProps, DisplayComponent, FSComponent, Subscribable, Subscription, VNode } from '@microsoft/msfs-sdk';
 import './style.scss';
 
 interface ButtonProps extends ComponentProps {
@@ -7,20 +7,34 @@ interface ButtonProps extends ComponentProps {
     onClick: () => void;
 }
 export class Button extends DisplayComponent<ButtonProps> {
-    private spanRef = FSComponent.createRef<HTMLSpanElement>();
+    // Make sure to collect all subscriptions here, otherwise page navigation doesn't work.
+    private subs = [] as Subscription[];
+
+    private buttonRef = FSComponent.createRef<HTMLSpanElement>();
+
+    clickHandler(): void {
+        if (this.props.disabled !== undefined && this.props.disabled.get() === false) {
+            this.props.onClick();
+        }
+    }
 
     onAfterRender(node: VNode): void {
         super.onAfterRender(node);
 
-        if (this.props.disabled === undefined || this.props.disabled.get() === false) {
-            this.spanRef.instance.addEventListener('click', () => this.props.onClick());
-        }
+        this.buttonRef.instance.addEventListener('click', () => this.clickHandler());
+    }
+
+    public destroy(): void {
+        // Destroy all subscriptions to remove all references to this instance.
+        this.subs.forEach((x) => x.destroy());
+
+        super.destroy();
     }
 
     render(): VNode {
         return (
             <span
-                ref={this.spanRef}
+                ref={this.buttonRef}
                 class={`MFDButton${(this.props.disabled !== undefined && this.props.disabled.get() === true) ? ' disabled' : ''}`}
                 style={`align-items: center; ${(this.props.disabled !== undefined && this.props.disabled.get() === true) ? 'color: grey; ' : ''} ${this.props.containerStyle}`}
             >
