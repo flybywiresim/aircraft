430cc8835e37eae34f200b6d716ea9eaea7dfba3
Maximilian Reuter <maximilian.a.reuter@gmail.com>
2023-05-24 23:57:53 +0200

    fix(ISIS): Diamond on the LS scales no longer leaves the scales and deviation is now the same for the PFD

--- fbw-a32nx/src/systems/instruments/src/ISIS/LandingSystem.tsx
+++ fbw-a32nx/src/systems/instruments/src/ISIS/LandingSystem.tsx
@@ -7,23 +7,28 @@ type DeviationIndicatorProps = {
     available: boolean
 }
 
-const DeviationIndicator: React.FC<DeviationIndicatorProps> = ({ deviation, available }) => (
-    <>
-        <rect x={0} y={0} width={200} height={20} fill="black" />
-        <circle className="StrokeWhite NoFill" cx={10} cy={10} r={7} />
-        <circle className="StrokeWhite NoFill" cx={55} cy={10} r={7} />
-        <line x1={100} x2={100} y1={2} y2={18} strokeWidth={4} stroke="white" />
-        <circle className="StrokeWhite NoFill" cx={145} cy={10} r={7} />
-        <circle className="StrokeWhite NoFill" cx={190} cy={10} r={7} />
-        { available
-            && (
-                <g transform={`translate(${(deviation * 50).toFixed(5)} 0)`}>
+const DeviationIndicator: React.FC<DeviationIndicatorProps> = ({ deviation, available }) => {
+    const maxDeviation = 1.778; // the maximum position the diamond can be while still fully inside scale
+
+    const dots = Math.sign(deviation) * Math.min(Math.abs(deviation / 0.4), maxDeviation);
+
+    return (
+        <>
+            <rect x={0} y={0} width={200} height={20} fill="black" />
+            <circle className="StrokeWhite NoFill" cx={10} cy={10} r={7} />
+            <circle className="StrokeWhite NoFill" cx={55} cy={10} r={7} />
+            <line x1={100} x2={100} y1={2} y2={18} strokeWidth={4} stroke="white" />
+            <circle className="StrokeWhite NoFill" cx={145} cy={10} r={7} />
+            <circle className="StrokeWhite NoFill" cx={190} cy={10} r={7} />
+            { available
+            && (// assumes 0.4 deg of deviation per dot and 45px distance between dots
+                <g transform={`translate(${(dots * 45).toFixed(5)} 0)`}>
                     <path className="FillMagenta" d="M80,10 L100,2 L120,10 L100,18z" />
                 </g>
             )}
-    </>
-);
-
+        </>
+    );
+};
 export const LandingSystem: React.FC = () => {
     const [gsDeviation] = useSimVar('L:A32NX_RADIO_RECEIVER_GS_DEVIATION', 'number');
     const [gsAvailable] = useSimVar('L:A32NX_RADIO_RECEIVER_GS_IS_VALID', 'number');
@@ -36,7 +41,7 @@ export const LandingSystem: React.FC = () => {
         lsActive && (
             <g id="LandingSystem">
                 <g transform="translate(156 380)">
-                    <DeviationIndicator deviation={lsDeviation} available={lsAvailable} />
+                    <DeviationIndicator deviation={lsDeviation / 2} available={lsAvailable} />
                 </g>
                 <g transform="translate(356 166) rotate(90 0 0)">
                     <DeviationIndicator deviation={gsDeviation} available={gsAvailable} />
