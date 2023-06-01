cbe22060ad9992ac4995b39578f6d159df551fe4
Michael Corcoran <tracer@outlook.co.nz>
2023-06-02 11:33:38 +1200

    fix(nd): plan label alignment

--- fbw-a32nx/src/systems/instruments/src/NDv2/pages/plan/PlanModeUnderlay.tsx
+++ fbw-a32nx/src/systems/instruments/src/NDv2/pages/plan/PlanModeUnderlay.tsx
@@ -22,16 +22,16 @@ export class PlanModeUnderlay extends DisplayComponent<PlanModeUnderlayProps> {
                     <text x={310} y={474} class="Cyan" font-size={22}>{this.props.mapRange.map((range) => range / 2)}</text>
                     <text x={212} y={556} class="Cyan" font-size={22}>{this.props.mapRange.map((range) => range / 1)}</text>
 
-                    <text x={384} y={170} class="White" font-size={25} text-anchor="middle" alignmentBaseline="central">N</text>
+                    <text x={384} y={170} class="White" font-size={25} text-anchor="middle" alignment-baseline="central">N</text>
                     <path d="M384,141.5 L390,151 L378,151 L384,141.5" fill="white" stroke="none" />
 
-                    <text x={598} y={384} class="White" font-size={25} text-anchor="middle" alignmentBaseline="central">E</text>
+                    <text x={598} y={384} class="White" font-size={25} text-anchor="middle" alignment-baseline="central">E</text>
                     <path d="M626.2,384 L617,390 L617,378 L626.5,384" fill="white" stroke="none" />
 
-                    <text x={384} y={598} class="White" font-size={25} text-anchor="middle" alignmentBaseline="central">S</text>
+                    <text x={384} y={598} class="White" font-size={25} text-anchor="middle" alignment-baseline="central">S</text>
                     <path d="M384,626.5 L390,617 L378,617 L384,626.5" fill="white" stroke="none" />
 
-                    <text x={170} y={384} class="White" font-size={25} text-anchor="middle" alignmentBaseline="central">W</text>
+                    <text x={170} y={384} class="White" font-size={25} text-anchor="middle" alignment-baseline="central">W</text>
                     <path d="M141.5,384 L151,390 L151,378 L141.5,384" fill="white" stroke="none" />
                 </g>
             </>
