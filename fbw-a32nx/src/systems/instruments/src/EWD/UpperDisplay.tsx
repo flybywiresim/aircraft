f862deb4db29cd6faf819c30d3f9cc7c761aa3c5
lukecologne <lukasmichels00@gmail.com>
2023-06-19 16:15:53 +0200

    feat(sd/ewd): improve visual accuracy for static components (#8053)
    * fix(ewd/sd): change some line colors to grey, add round linecap
    
    * feat(sd-bleed): pack and bleed boxes now grey, increased stroke of bleed
    
    * feat(sd-elec): use color definitions, make boxes grey
    
    * feat(sd-fuel): make wing shape, FOB box and fuel used lines grey
    
    * feat(sd-apu): boxes and separator in grey
    
    * feat(sd-door): aircraft shape in grey
    
    * feat(sd-sts): vertical separator is now grey
    
    * feat(ewd): make N2 starting background normal grey
    
    * chore: remove unused light and dark grey color definitions
    
    * feat(nd): change FM and WXR/TCAS message boxes to grey

--- fbw-a32nx/src/systems/instruments/src/EWD/UpperDisplay.tsx
+++ fbw-a32nx/src/systems/instruments/src/EWD/UpperDisplay.tsx
@@ -53,8 +53,8 @@ export class UpperDisplay extends DisplayComponent<UpperDisplayProps> {
                     <N2 bus={this.props.bus} engine={2} x={493} y={0} />
                     <text class="Large Center" x={386} y={33}>N2</text>
                     <text class="Medium Center Cyan" x={385} y={53}>%</text>
-                    <line class="Separator" x1={311} y1={37} x2={343} y2={28} strokeLinecap="round" />
-                    <line class="Separator" x1={424} y1={28} x2={456} y2={37} strokeLinecap="round" />
+                    <line class="Separator" x1={311} y1={37} x2={343} y2={28} />
+                    <line class="Separator" x1={424} y1={28} x2={456} y2={37} />
                 </Layer>
 
                 <Layer x={0} y={380}>
@@ -65,8 +65,8 @@ export class UpperDisplay extends DisplayComponent<UpperDisplayProps> {
                         {this.usingMetric.map((m) => (m ? 'KG' : 'LBS'))}
                         /H
                     </text>
-                    <line class="Separator" x1={311} y1={-11} x2={343} y2={-20} strokeLinecap="round" />
-                    <line class="Separator" x1={424} y1={-20} x2={456} y2={-11} strokeLinecap="round" />
+                    <line class="Separator" x1={311} y1={-11} x2={343} y2={-20} />
+                    <line class="Separator" x1={424} y1={-20} x2={456} y2={-11} />
                 </Layer>
 
                 <FOB bus={this.props.bus} x={40} y={490} metric={this.usingMetric} />
