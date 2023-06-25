2387e38ed655dce63a90545be8f6030625466200
Saschl <sascharudolf46@gmail.com>
2023-06-25 21:26:44 +0200

    fix: margins

--- fbw-a32nx/src/systems/instruments/src/NDv2/ND.tsx
+++ fbw-a32nx/src/systems/instruments/src/NDv2/ND.tsx
@@ -422,9 +422,9 @@ class SpeedIndicator extends DisplayComponent<{ bus: EventBus }> {
         return (
             <Layer x={2} y={25}>
                 <text x={0} y={0} class="White FontSmallest">GS</text>
-                <text ref={this.groundSpeedRef} x={80} y={0} class="Green FontIntermediate EndAlign" />
+                <text ref={this.groundSpeedRef} x={89} y={0} class="Green FontIntermediate EndAlign" />
                 <text x={95} y={0} class="White FontSmallest">TAS</text>
-                <text ref={this.trueAirSpeedRef} x={190} y={0} class="Green FontIntermediate EndAlign" />
+                <text ref={this.trueAirSpeedRef} x={201} y={0} class="Green FontIntermediate EndAlign" />
             </Layer>
         );
     }
