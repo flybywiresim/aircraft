84ab674ef1e382356cc1fb0a5c32f11b4abf3d06
Robert Xing <73909958+robertxing2004@users.noreply.github.com>
2023-07-14 00:07:12 -0400

    fix(sd): line thickness and fuel quantity position adjustments

--- fbw-a32nx/src/systems/instruments/src/SD/Pages/Fuel/Fuel.tsx
+++ fbw-a32nx/src/systems/instruments/src/SD/Pages/Fuel/Fuel.tsx
@@ -81,7 +81,7 @@ export const FuelPage = () => {
                 <Pump x={180} y={215} onBus="DC_2" pumpNumber={5} />
 
                 {/* Quantities */}
-                <text className="TankQuantity" x={74} y={285}>{fuelInTanksForDisplay(tankLeftOuter, unit, fuelWeightPerGallon)}</text>
+                <text className="TankQuantity" x={80} y={285}>{fuelInTanksForDisplay(tankLeftOuter, unit, fuelWeightPerGallon)}</text>
                 <text className="TankQuantity" x={190} y={285}>{fuelInTanksForDisplay(tankLeftInner, unit, fuelWeightPerGallon)}</text>
 
                 { leftOuterInnerValve ? <Triangle x={77} y={319} colour="Green" fill={0} orientation={90} /> : null }
@@ -146,7 +146,7 @@ export const FuelPage = () => {
 
                 {/* Quantities */}
                 <text className="TankQuantity" x={472} y={285}>{fuelInTanksForDisplay(tankRightInner, unit, fuelWeightPerGallon)}</text>
-                <text className="TankQuantity" x={579} y={285}>{fuelInTanksForDisplay(tankRightOuter, unit, fuelWeightPerGallon)}</text>
+                <text className="TankQuantity" x={580} y={285}>{fuelInTanksForDisplay(tankRightOuter, unit, fuelWeightPerGallon)}</text>
                 {rightOuterInnerValve && <Triangle x={522} y={319} colour="Green" fill={0} orientation={-90} />}
 
                 <text className="UnitTemp" x="510" y="355">°C</text>
@@ -236,7 +236,7 @@ const FOB = ({ unit }:FOBProps) => {
 const Wings = () => (
     <>
         {/* Bottom line */}
-        <path className="ThickShape" d="M 15, 255 l 0, 80 l 570, 0 l 0,-80" strokeLinecap="round" />
+        <path className="ThickShape" d="M 15, 255 l 0, 75 l 570, 0 l 0,-75" strokeLinecap="round" />
 
         {/* Top line */}
         <path className="ThickShape" d="M 585, 255 l -124.2, -21.6" />
@@ -246,10 +246,10 @@ const Wings = () => (
         <path className="ThickShape" d="M 355, 215 l 29.9, 5.2" />
 
         {/* Tank lines */}
-        <path className="ThickShape" d="M 80,  244 L 80,  335" />
-        <path className="ThickShape" d="M 245, 215 L 230, 335" />
-        <path className="ThickShape" d="M 355, 215 L 370, 335" />
-        <path className="ThickShape" d="M 520, 244 L 520, 335" />
+        <path className="ThickShape" d="M 85,  243 L 85,  330" />
+        <path className="ThickShape" d="M 245, 215 L 230, 330" />
+        <path className="ThickShape" d="M 355, 215 L 370, 330" />
+        <path className="ThickShape" d="M 515, 243 L 515, 330" />
     </>
 );
 
