425429acec1cf4ca4860a4a06ba353d76f82cd89
Saschl <sascharudolf46@gmail.com>
2023-10-01 20:35:00 +0200

    revert changes and refactor release

--- fbw-a32nx/src/systems/instruments/buildSrc/igniter/tasks.mjs
+++ fbw-a32nx/src/systems/instruments/buildSrc/igniter/tasks.mjs
@@ -7,7 +7,7 @@ export function getInstrumentsIgniterTasks() {
     const baseInstruments = fs.readdirSync(join(Directories.instruments, 'src'), { withFileTypes: true })
         .filter((d) => d.isDirectory() && fs.existsSync(join(Directories.instruments, 'src', d.name, 'config.json')));
 
-    return baseInstruments.map((instrument) => new TaskOfTasks(`${instrument.name}`, [new ExecTask(
+    return baseInstruments.map((instrument) => new ExecTask(
         instrument.name,
         `cd fbw-a32nx && mach build -f ${instrument.name}`,
         [
@@ -15,6 +15,6 @@ export function getInstrumentsIgniterTasks() {
             'fbw-a32nx/src/systems/instruments/src/Common',
             join('fbw-a32nx/out/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/A32NX', instrument.name),
         ],
-    ),
-    new ExecTask(`${instrument.name}_tsc`, `tsc -noemit -p ${join(Directories.instruments, 'src', instrument.name, 'tsconfig.json')} 1>&2`)]));
+    ));
+    // new ExecTask(`${instrument.name}_tsc`, `tsc -noemit -p ${join(Directories.instruments, 'src', instrument.name, 'tsconfig.json')} 1>&2`)]));
 }
