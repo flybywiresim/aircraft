8b050c2119106627da3bf48669a9797623f3b625
Michael Corcoran <tracer@outlook.co.nz>
2023-11-07 20:57:01 +1300

    chore: bring in a380 instruments @2dc636d7
    Build scripts and imports have been adjusted for the monorepo
    environment.
    
    Co-authored-by: Sven Czarnian <devel@svcz.de>
    Co-authored-by: Benjamin Dupont <benjozorkfr@gmail.com>
    Co-authored-by: Richard Pilbery <email@pilbery.co.uk>
    Co-authored-by: Benedict Etzel <developer@beheh.de>
    Co-authored-by: svengcz <64070348+svengcz@users.noreply.github.com>
    Co-authored-by: BBK <22713769+BlueberryKing@users.noreply.github.com>
    Co-authored-by: ErickSharp <titaninmc@gmail.com>
    Co-authored-by: 2hwk <reasonstouninstall@tutanota.com>
    Co-authored-by: Nufflee <zifcani@gmail.com>
    Co-authored-by: davydecorps <38904654+crocket63@users.noreply.github.com>
    Co-authored-by: Crock <38904654+crocket63@users.noreply.github.com>
    Co-authored-by: Taz5150 <roldan5150@gmail.com>
    Co-authored-by: crocket63 <38904654+crocket63@users.noreply.github.com>

---
+++ fbw-a380x/src/systems/instruments/src/RMP/Pages/NavPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const NavPage = () => (
+    <g>
+        <text x={823} y={512} fontSize={80} textAnchor="middle" fontFamily="RMP-11" fill="white">NAV</text>
+    </g>
+);
