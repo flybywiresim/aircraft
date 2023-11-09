d0a61755b2f1b3d4cf39bedec87e906beb8ff962
Chaoz <maximilian.a.reuter@gmail.com>
2023-11-09 10:22:33 +0100

    fix(EFB): ASU now correctly reenables after disabling due to moving once stopped (#8293)

--- fbw-a32nx/src/systems/instruments/src/EFB/Ground/Pages/Services/A320_251N/A320Services.tsx
+++ fbw-a32nx/src/systems/instruments/src/EFB/Ground/Pages/Services/A320_251N/A320Services.tsx
@@ -523,6 +523,7 @@ export const A320Services: React.FC = () => {
             dispatch(setBoarding3DoorButtonState(ServiceButtonState.INACTIVE));
             dispatch(setServiceDoorButtonState(ServiceButtonState.INACTIVE));
             dispatch(setCateringButtonState(ServiceButtonState.INACTIVE));
+            dispatch(setAsuButtonState(ServiceButtonState.INACTIVE));
         }
     }, [groundServicesAvailable]);
 
