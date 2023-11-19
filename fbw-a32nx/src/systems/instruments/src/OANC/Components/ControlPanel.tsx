2ff18b2758dc09abbd669458a22aafc5ce19460d
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2023-11-18 22:27:28 -0500

    feat: start control panel tabs

--- fbw-a32nx/src/systems/instruments/src/OANC/Components/ControlPanel.tsx
+++ fbw-a32nx/src/systems/instruments/src/OANC/Components/ControlPanel.tsx
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-import { FSComponent, DisplayComponent, VNode, Subscribable, Subscription, MapSubject } from '@microsoft/msfs-sdk';
+import { FSComponent, DisplayComponent, VNode, Subscribable, Subscription, MapSubject, Subject } from '@microsoft/msfs-sdk';
 
 import './ControlPanel.scss';
 
@@ -15,6 +15,8 @@ export class ControlPanel extends DisplayComponent<ControlPanelProps> {
 
     private readonly style = MapSubject.create<string, string>();
 
+    private readonly activeTabIndex = Subject.create<1 | 2 | 3>(2);
+
     onAfterRender() {
         this.subscriptions.push(
             this.props.isVisible.sub((it) => this.style.setValue('visibility', it ? 'visible' : 'hidden'), true),
@@ -23,8 +25,59 @@ export class ControlPanel extends DisplayComponent<ControlPanelProps> {
 
     render(): VNode | null {
         return (
-            <div class="oanc-control-panel" style={this.style}>
-                bruh
+            <div class="oanc-control-panel-container" style={this.style}>
+                <div class="oanc-control-panel-tabs" data-active-tab-index={this.activeTabIndex}>
+                    <div class="oanc-control-panel-tabs-dummy" />
+                    <ControlPanelTabButton
+                        text="MAP DATA"
+                        isSelected={this.activeTabIndex.map((it) => it === 1)}
+                        onSelected={() => this.activeTabIndex.set(1)}
+                    />
+                    <ControlPanelTabButton
+                        text="ARPT SEL"
+                        isSelected={this.activeTabIndex.map((it) => it === 2)}
+                        onSelected={() => this.activeTabIndex.set(2)}
+                    />
+                    <ControlPanelTabButton
+                        text="STATUS"
+                        isSelected={this.activeTabIndex.map((it) => it === 3)}
+                        onSelected={() => this.activeTabIndex.set(3)}
+                    />
+                </div>
+
+                <div class="oanc-control-panel">
+                    bruh
+                </div>
+            </div>
+        );
+    }
+}
+
+interface ControlPanelTabButtonProps {
+    text: string,
+
+    isSelected: Subscribable<boolean>,
+
+    onSelected: () => void,
+}
+
+class ControlPanelTabButton extends DisplayComponent<ControlPanelTabButtonProps> {
+    private readonly root = FSComponent.createRef<HTMLDivElement>();
+
+    onAfterRender(node: VNode) {
+        this.root.instance.addEventListener('click', this.props.onSelected);
+    }
+
+    render(): VNode | null {
+        return (
+            <div
+                ref={this.root}
+                class={{
+                    'oanc-control-panel-tab-button': true,
+                    'oanc-control-panel-tab-button-selected': this.props.isSelected,
+                }}
+            >
+                {this.props.text}
             </div>
         );
     }
