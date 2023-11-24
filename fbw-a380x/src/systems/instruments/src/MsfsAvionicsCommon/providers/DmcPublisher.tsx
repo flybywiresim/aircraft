b587a4ac116c3fe1839562e222ddf8434f18d62d
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2023-11-24 18:13:52 -0500

    refactor: copy MsfsAvionicsCommon files to fbw-a380x/

---
+++ fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/providers/DmcPublisher.tsx
@@ -0,0 +1,83 @@
+// Copyright (c) 2021-2023 FlyByWire Simulations
+//
+// SPDX-License-Identifier: GPL-3.0
+
+import { EventBus, SimVarDefinition, SimVarPublisher, SimVarValueType, Subject } from '@microsoft/msfs-sdk';
+import { Arinc429WordData, Arinc429RegisterSubject } from '@flybywiresim/fbw-sdk';
+import { AdirsSimVars } from '../SimVarTypes';
+
+export interface DmcLogicEvents {
+    trueRefActive: boolean,
+    heading: Arinc429WordData,
+    track: Arinc429WordData,
+}
+
+export interface DmcDiscreteInputEvents {
+    trueRefPushButton: boolean,
+}
+
+export type DmcEvents = DmcLogicEvents & DmcDiscreteInputEvents;
+
+export class DmcPublisher extends SimVarPublisher<DmcDiscreteInputEvents> {
+    private readonly irMaintWord = Arinc429RegisterSubject.createEmpty();
+
+    private readonly magHeading = Arinc429RegisterSubject.createEmpty();
+
+    private readonly trueHeading = Arinc429RegisterSubject.createEmpty();
+
+    private readonly magTrack = Arinc429RegisterSubject.createEmpty();
+
+    private readonly trueTrack = Arinc429RegisterSubject.createEmpty();
+
+    private readonly trueRefPb = Subject.create(false);
+
+    private readonly trueRefActive = Subject.create(false);
+
+    constructor(private eventBus: EventBus) {
+        const simVars = new Map<keyof DmcDiscreteInputEvents, SimVarDefinition>([
+            // FIXME, per-side
+            ['trueRefPushButton', { name: 'L:A32NX_PUSH_TRUE_REF', type: SimVarValueType.Bool }],
+        ]);
+        super(simVars, eventBus);
+    }
+
+    init(): void {
+        const pub = this.eventBus.getPublisher<DmcLogicEvents>();
+
+        this.trueRefActive.sub((v) => {
+            pub.pub('trueRefActive', v);
+            this.handleHeading();
+        }, true);
+
+        const sub = this.eventBus.getSubscriber<AdirsSimVars & DmcDiscreteInputEvents>();
+
+        this.irMaintWord.sub(this.handleTrueRef.bind(this));
+        this.trueRefPb.sub(this.handleTrueRef.bind(this), true);
+
+        this.magHeading.sub(this.handleHeading.bind(this));
+        this.magTrack.sub(this.handleHeading.bind(this));
+        this.trueHeading.sub(this.handleHeading.bind(this));
+        this.trueTrack.sub(this.handleHeading.bind(this));
+
+        sub.on('irMaintWordRaw').handle((v) => this.irMaintWord.setWord(v));
+        sub.on('trueRefPushButton').whenChanged().handle((v) => this.trueRefPb.set(v));
+        sub.on('magHeadingRaw').handle((v) => this.magHeading.setWord(v));
+        sub.on('magTrackRaw').handle((v) => this.magTrack.setWord(v));
+        sub.on('trueHeadingRaw').handle((v) => this.trueHeading.setWord(v));
+        sub.on('trueTrackRaw').handle((v) => this.trueTrack.setWord(v));
+    }
+
+    private handleTrueRef(): void {
+        // true ref is active when the PB is pressed or the ADIRU is at an extreme latitude
+        // and the ADIRU must not be in ATT reversion mode
+        const trueRequested = this.irMaintWord.get().bitValueOr(15, false) || this.trueRefPb.get();
+        this.trueRefActive.set(trueRequested && !this.irMaintWord.get().bitValueOr(2, false));
+    }
+
+    private handleHeading(): void {
+        const pub = this.eventBus.getPublisher<DmcLogicEvents>();
+
+        pub.pub('heading', this.trueRefActive.get() ? this.trueHeading.get() : this.magHeading.get());
+        pub.pub('track', this.trueRefActive.get() ? this.trueTrack.get() : this.magTrack.get());
+    }
+}
---
+++ fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/providers/FcuBusPublisher.ts
@@ -0,0 +1,29 @@
+// Copyright (c) 2021-2023 FlyByWire Simulations
+//
+// SPDX-License-Identifier: GPL-3.0
+
+import { EventBus, SimVarPublisher, SimVarValueType } from '@microsoft/msfs-sdk';
+import { EfisNdMode, EfisOption, EfisSide, NavAidMode } from '@flybywiresim/fbw-sdk';
+
+export interface FcuSimVars {
+    ndRangeSetting: number,
+    ndMode: EfisNdMode,
+    option: EfisOption,
+    navaidMode1: NavAidMode,
+    navaidMode2: NavAidMode,
+    /** State of the LS pushbutton on the EFIS control panel. */
+    efisLsActive: boolean,
+}
+
+export class FcuBusPublisher extends SimVarPublisher<FcuSimVars> {
+    constructor(bus: EventBus, efisSide: EfisSide) {
+        super(new Map([
+            ['ndRangeSetting', { name: `L:A32NX_EFIS_${efisSide}_ND_RANGE`, type: SimVarValueType.Enum }],
+            ['ndMode', { name: `L:A32NX_EFIS_${efisSide}_ND_MODE`, type: SimVarValueType.Enum }],
+            ['option', { name: `L:A32NX_EFIS_${efisSide}_OPTION`, type: SimVarValueType.Enum }],
+            ['navaidMode1', { name: `L:A32NX_EFIS_${efisSide}_NAVAID_1_MODE`, type: SimVarValueType.Enum }],
+            ['navaidMode2', { name: `L:A32NX_EFIS_${efisSide}_NAVAID_2_MODE`, type: SimVarValueType.Enum }],
+            ['efisLsActive', { name: `L:BTN_LS_${efisSide === 'L' ? 1 : 2}_FILTER_ACTIVE`, type: SimVarValueType.Bool }],
+        ]), bus);
+    }
+}
