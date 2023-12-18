97b09b096892a9d1cc47212cb6cb9381e6ee4a39
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2023-12-17 22:33:48 -0500

    update data storage typings

--- fbw-common/src/typings/fs-base-ui/html_ui/JS/dataStorage.d.ts
+++ fbw-common/src/typings/fs-base-ui/html_ui/JS/dataStorage.d.ts
@@ -7,12 +7,12 @@ declare global {
     }
 
     interface DataStorageSearchData {
-        key: any;
-        data: any;
+        key: string;
+        data: string;
     }
 
     function OnDataStorageReady(): void;
-    function GetDataStorage(): DataStorage | void;
+    function GetDataStorage(): DataStorage;
     function GetStoredData(key: string): any;
     function SearchStoredData(key: string): any;
     function SetStoredData(key: string, data: any): any;
