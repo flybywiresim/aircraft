74e5e89afe0eaecb30649a0c864b0dc6f1fb5e51
lukecologne <lukasmichels00@gmail.com>
2023-12-29 20:39:40 +0100

    feat(a380-sd): improve reservoir indications and fail flags

--- fbw-a380x/src/systems/instruments/src/SD/Pages/Hyd/elements/Reservoir.tsx
+++ fbw-a380x/src/systems/instruments/src/SD/Pages/Hyd/elements/Reservoir.tsx
@@ -13,27 +13,24 @@ type ReservoirProps = {
 export const Reservoir = ({ x, y, side }: ReservoirProps) => {
     const height = 166;
     const width = 19;
+    // Total reservoir capacity is 120L.
+    // Reservoir capacity is displayed up to 70L.
+    const reservoirCapacityInLiters = 70;
+    const litersToPixels = (liters: number) => liters * height / reservoirCapacityInLiters;
+
+    // TODO should come from the HSMU and be computed from hyd temp, LEHGS filling etc.
+    const normalFilling = 45;
     const fallbackNormalFilling = 40;
     const normalFillingFault = false;
-
-    const normalFillingRange = 50;
 
     const isLeftSide = side === 'GREEN';
 
     const [levelGallon] = useSimVar(`L:A32NX_HYD_${side}_RESERVOIR_LEVEL`, 'gallon', 1000);
-    const level = levelGallon * LITERS_PER_GALLON;
+    const level = Math.min(levelGallon * LITERS_PER_GALLON, 70);
     const [lowLevel] = useSimVar(`L:A32NX_HYD_${side}_RESERVOIR_LEVEL_IS_LOW`, 'boolean', 1000);
-
-    // TODO: Figure out
-    const reservoirCapacityInLiters = 50;
-    const litersToPixels = (liters: number) => liters * height / reservoirCapacityInLiters;
 
     return (
         <g transform={`translate(${x} ${y})`}>
-            <path d={`M 0, 0 v ${height} h 17 m 2 -36 v -56 m 0 -37 v -37`} className='White NoFill SW2 LineRound' />
-            <path d='m 9 -37 v 202' className={`${lowLevel ? 'Amber' : 'Green'} SW4`} />
-            <path d={`m 9 ${isLeftSide ? -12 : -13} h ${isLeftSide ? 157 : -155} v -66`} className={`${lowLevel ? 'Amber' : 'Green'} NoFill SW4`} />
-
             <rect
                 x={0.5}
                 y={height - litersToPixels(level)}
@@ -41,45 +38,81 @@ export const Reservoir = ({ x, y, side }: ReservoirProps) => {
                 width={width - 1}
                 className={`${lowLevel ? 'Amber' : 'Green'} Fill`}
             />
+
+            <path d={`M 0, 0 v ${height} h ${width} v -${height}`} className='White NoFill SW2 LineRound' />
+            <path d='m 9 -37 v 202' className={`${lowLevel ? 'Amber' : 'Green'} SW4`} />
+            <path d={`m 9 ${isLeftSide ? -12 : -13} h ${isLeftSide ? 157 : -155} v -66`} className={`${lowLevel ? 'Amber' : 'Green'} NoFill SW4`} />
+
             <rect
                 x={width + 1}
-                y={height - litersToPixels(normalFillingRange) - 17}
+                y={height - litersToPixels(normalFilling) - 16}
+                width={7}
+                height={15}
+                className='Background SW3'
+            />
+            <rect
+                x={width + 1}
+                y={height - litersToPixels(normalFilling) - 16}
                 width={7}
                 height={15}
                 className={normalFillingFault ? 'White Fill' : 'Green Fill'}
             />
             <rect
                 x={width + 1}
-                y={height - litersToPixels(normalFillingRange)}
+                y={height - litersToPixels(normalFilling) + 1}
+                width={7}
+                height={15}
+                className='Background SW3'
+            />
+            <rect
+                x={width + 1}
+                y={height - litersToPixels(normalFilling) + 1}
                 width={7}
                 height={15}
                 className={normalFillingFault ? 'White Fill' : 'Green Fill'}
             />
             {normalFillingFault
                 && <path className='Amber SW2' d={`M ${width - 0.5} ${height - litersToPixels(fallbackNormalFilling) - 4} l 8 8 m 0 -8 l -8 8`} />}
-            <rect x={width} y={height - 34} width={6} height={34} className='Amber Fill' />
 
-            <ReservoirFailIndications x={isLeftSide ? 64 : -64} y={height - 80} side={side} />
+            <rect
+                x={width}
+                y={height - 33}
+                width={6}
+                height={33}
+                className='Background SW3'
+            />
+            <rect
+                x={width}
+                y={height - 33}
+                width={6}
+                height={33}
+                className='Amber Fill'
+            />
+
+            <ReservoirFailIndications side={side} />
         </g>
     );
 };
 
 type ReservoirFailIndicationsProps = {
-    x: number;
-    y: number;
     side: 'GREEN' | 'YELLOW';
 }
-const ReservoirFailIndications = ({ x, y, side }: ReservoirFailIndicationsProps) => {
+const ReservoirFailIndications = ({ side }: ReservoirFailIndicationsProps) => {
+    const isLeftSide = side === 'GREEN';
+
     const [lowAirPress] = useSimVar(`L:A32NX_HYD_${side}_RESERVOIR_AIR_PRESSURE_IS_LOW`, 'boolean', 1000);
     const [ovht] = useSimVar(`L:A32NX_HYD_${side}_RESERVOIR_OVHT`, 'boolean', 1000);
+    const tempHi = false;
 
     return (
-        <g className='hyd-reservoir-fail'>
-            {lowAirPress && <text x={x} y={y} textAnchor='middle' className='Amber F19'>AIR</text>}
-            {lowAirPress && <text x={x} y={y + 1 * 21} textAnchor='middle' className='Amber F19'>PRESS</text>}
-            {lowAirPress && <text x={x} y={y + 2 * 21} textAnchor='middle' className='Amber F19'>LOW</text>}
-            {false && <text x={x} y={y + 66} textAnchor='middle' className='Amber F19'>TEMP HI</text>}
-            {ovht && <text x={x} y={y + 66} textAnchor='middle' className='Amber F19'>OVHT</text>}
+        <g>
+            <g className={lowAirPress ? '' : 'Hide'}>
+                <text x={isLeftSide ? 70 : -50} y={72} className='Amber F24 MiddleAlign'>AIR</text>
+                <text x={isLeftSide ? 70 : -50} y={96} className='Amber F24 MiddleAlign'>PRESS</text>
+                <text x={isLeftSide ? 70 : -50} y={120} className='Amber F24 MiddleAlign'>LOW</text>
+            </g>
+            <text x={isLeftSide ? 32 : -114} y={155} className={tempHi ? 'Amber F24 ' : 'Hide'}>TEMP HI</text>
+            <text x={isLeftSide ? 32 : -70} y={155} className={ovht ? 'Amber F24' : 'Hide'}>OVHT</text>
         </g>
     );
 };
