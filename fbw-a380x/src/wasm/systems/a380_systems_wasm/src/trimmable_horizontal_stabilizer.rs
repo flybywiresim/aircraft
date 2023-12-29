eb7f03b142e34d2c3be1a70563864a9b836908b5
Crocket63 <davydecorps@gmail.com>
2023-12-29 14:41:20 +0100

    Added trim input from keyboard events

--- fbw-a380x/src/wasm/systems/a380_systems_wasm/src/trimmable_horizontal_stabilizer.rs
+++ fbw-a380x/src/wasm/systems/a380_systems_wasm/src/trimmable_horizontal_stabilizer.rs
@@ -1,9 +1,9 @@
 use std::error::Error;
 
-use systems_wasm::aspects::{MsfsAspectBuilder, ObjectWrite, VariablesToObject};
+use systems_wasm::aspects::{
+    EventToVariableMapping, MsfsAspectBuilder, ObjectWrite, VariablesToObject,
+};
 use systems_wasm::{set_data_on_sim_object, Variable};
-
-use systems::shared::to_bool;
 
 use msfs::sim_connect;
 use msfs::{sim_connect::SimConnect, sim_connect::SIMCONNECT_OBJECT_ID_USER};
@@ -11,6 +11,24 @@ use msfs::{sim_connect::SimConnect, sim_connect::SIMCONNECT_OBJECT_ID_USER};
 pub(super) fn trimmable_horizontal_stabilizer(
     builder: &mut MsfsAspectBuilder,
 ) -> Result<(), Box<dyn Error>> {
+    const THS_EVENT_INCREMENTS: f64 = 0.05;
+    builder.event_to_variable(
+        "ELEV_TRIM_UP",
+        EventToVariableMapping::CurrentValueToValue(|current_value| {
+            (current_value + THS_EVENT_INCREMENTS).min(45.)
+        }),
+        Variable::named("DEV_FINAL_THS_ANGLE"),
+        |options| options.mask(),
+    )?;
+    builder.event_to_variable(
+        "ELEV_TRIM_DN",
+        EventToVariableMapping::CurrentValueToValue(|current_value| {
+            (current_value - THS_EVENT_INCREMENTS).max(-45.)
+        }),
+        Variable::named("DEV_FINAL_THS_ANGLE"),
+        |options| options.mask(),
+    )?;
+
     builder.variables_to_object(Box::new(PitchTrimSimOutput { elevator_trim: 0. }));
 
     Ok(())
@@ -24,17 +42,14 @@ struct PitchTrimSimOutput {
 }
 impl VariablesToObject for PitchTrimSimOutput {
     fn variables(&self) -> Vec<Variable> {
-        vec![
-            Variable::aspect("HYD_FINAL_THS_DEFLECTION"),
-            Variable::named("FLIGHT_CONTROLS_TRACKING_MODE"),
-        ]
+        vec![Variable::named("DEV_FINAL_THS_ANGLE")]
     }
 
     fn write(&mut self, values: Vec<f64>) -> ObjectWrite {
         self.elevator_trim = values[0];
 
         //Not writing control feedback when in tracking mode
-        ObjectWrite::on(!to_bool(values[1]))
+        ObjectWrite::ToSim
     }
 
     set_data_on_sim_object!();
