a66611541ab9ee1eb0755ec81567c8a35131014d
Crocket63 <davydecorps@gmail.com>
2024-01-03 12:58:26 +0100

    remove dev files

--- scripts/dev-env/run.sh
+++ scripts/dev-env/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#IMAGE="ghcr.io/flybywiresim/dev-env@sha256:2cf75753022112a79d4a512caecfbdf601e12b01b382263cf78f90635cdea6bf"
-IMAGE="saschl/dev-env:latest-arm"
+IMAGE="ghcr.io/flybywiresim/dev-env@sha256:2cf75753022112a79d4a512caecfbdf601e12b01b382263cf78f90635cdea6bf"
 
 # only set `-it` if there is a tty
 if [ -t 0 ] && [ -t 1 ];
