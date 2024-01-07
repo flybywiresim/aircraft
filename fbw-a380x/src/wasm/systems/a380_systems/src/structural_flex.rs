2933d086141537d8c934d8fefa036eb4086f1111
Crocket63 <davydecorps@gmail.com>
2024-01-07 22:34:09 +0100

    First adaptation

--- fbw-a380x/src/wasm/systems/a380_systems/src/structural_flex.rs
+++ fbw-a380x/src/wasm/systems/a380_systems/src/structural_flex.rs
@@ -423,6 +423,7 @@ mod tests {
     use crate::systems::simulation::{Aircraft, SimulationElement, SimulationElementVisitor};
     use std::time::Duration;
 
+    use systems::simulation::test::ReadByName;
     use uom::si::{angle::degree, velocity::knot};
 
     use ntest::assert_about_eq;
@@ -1249,5 +1250,55 @@ mod tests {
             test_bed.left_wing_lift_per_node().sum() + test_bed.right_wing_lift_per_node().sum()
                 >= test_bed.current_total_lift().get::<newton>() * 0.99
         );
+    }
+
+    #[test]
+    fn util_to_compute_xml_offsets_from_desired_angles_in_flight() {
+        let mut test_bed = WingFlexTestBed::new().with_nominal_weight().in_1g_flight();
+
+        test_bed = test_bed.run_waiting_for(Duration::from_secs(2));
+
+        let inboard_angle: Angle = test_bed.read_by_name("WING_FLEX_LEFT_INBOARD");
+        let inboard_mid_angle: Angle = test_bed.read_by_name("WING_FLEX_LEFT_INBOARD_MID");
+        let outboard_mid_angle: Angle = test_bed.read_by_name("WING_FLEX_LEFT_OUTBOARD_MID");
+        let outboard_angle: Angle = test_bed.read_by_name("WING_FLEX_LEFT_OUTBOARD");
+
+        println!(
+            "ANGLES => /O/--{:.1}--{:.1}--{:.1}--{:.1}",
+            inboard_angle.get::<degree>(),
+            inboard_mid_angle.get::<degree>(),
+            outboard_mid_angle.get::<degree>(),
+            outboard_angle.get::<degree>()
+        );
+
+        //Input formula from XML
+        let animation_position_inboard = inboard_angle.get::<degree>() * 23.25 + 53.78;
+        let animation_position_inboard_mid = inboard_mid_angle.get::<degree>() * 16.125 + 66.5;
+        let animation_position_outboard_mid = outboard_mid_angle.get::<degree>() * 29.41 + 31.9;
+        let animation_position_outboard = outboard_angle.get::<degree>() * 29.41 + 50.;
+
+        println!(
+            "ANIMATIONS => /O/--{:.1}--{:.1}--{:.1}--{:.1}",
+            animation_position_inboard,
+            animation_position_inboard_mid,
+            animation_position_outboard_mid,
+            animation_position_outboard
+        );
+
+        assert!(animation_position_inboard >= 62.2 && animation_position_inboard <= 62.3);
+        assert!(animation_position_inboard_mid >= 75.8 && animation_position_inboard_mid <= 76.2);
+        assert!(animation_position_outboard_mid >= 53.1 && animation_position_outboard_mid <= 53.3);
+        assert!(animation_position_outboard >= 55. && animation_position_outboard <= 57.);
+
+        // // One percent precision check on total lift value
+        // assert!(
+        //     test_bed.left_wing_lift_per_node().sum() + test_bed.right_wing_lift_per_node().sum()
+        //         <= test_bed.current_total_lift().get::<newton>() * 1.01
+        // );
+
+        // assert!(
+        //     test_bed.left_wing_lift_per_node().sum() + test_bed.right_wing_lift_per_node().sum()
+        //         >= test_bed.current_total_lift().get::<newton>() * 0.99
+        // );
     }
 }
