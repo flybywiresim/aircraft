81a32e7f41beaf9519c7faca390e439a6540c069
Maximilian Reuter <maximilian.a.reuter@gmail.com>
2024-01-12 17:47:45 +0100

    refactored previous solution to be more isolated

--- fbw-common/src/wasm/systems/systems/src/hydraulic/pushback.rs
+++ fbw-common/src/wasm/systems/systems/src/hydraulic/pushback.rs
@@ -3,8 +3,7 @@ use uom::si::{angle::radian, f64::*};
 use crate::{
     shared::{low_pass_filter::LowPassFilter, DelayedFalseLogicGate},
     simulation::{
-        InitContext, Read, SimulationElement, SimulatorReader, SimulatorWriter, UpdateContext,
-        VariableIdentifier, Write,
+        InitContext, Read, SimulationElement, SimulatorReader, UpdateContext, VariableIdentifier,
     },
 };
 use std::time::Duration;
@@ -12,11 +11,6 @@ use std::time::Duration;
 use super::nose_steering::Pushback;
 
 pub struct PushbackTug {
-    nw_strg_disc_memo_id: VariableIdentifier,
-
-    gsx_pin_state_id: VariableIdentifier,
-    gsx_pin_inserted: bool,
-
     state_id: VariableIdentifier,
     steer_angle_id: VariableIdentifier,
 
@@ -42,9 +36,6 @@ impl PushbackTug {
 
     pub fn new(context: &mut InitContext) -> Self {
         Self {
-            nw_strg_disc_memo_id: context.get_identifier("HYD_NW_STRG_DISC_ECAM_MEMO".to_owned()),
-            gsx_pin_state_id: context.get_identifier("FSDT_GSX_BYPASS_PIN".to_owned()),
-            gsx_pin_inserted: false,
             state_id: context.get_identifier("PUSHBACK STATE".to_owned()),
             steer_angle_id: context.get_identifier("PUSHBACK ANGLE".to_owned()),
 
@@ -76,7 +67,7 @@ impl PushbackTug {
 }
 impl Pushback for PushbackTug {
     fn is_nose_wheel_steering_pin_inserted(&self) -> bool {
-        self.nose_wheel_steering_pin_inserted.output() || self.gsx_pin_inserted
+        self.nose_wheel_steering_pin_inserted.output()
     }
 
     fn steering_angle(&self) -> Angle {
@@ -88,14 +79,5 @@ impl SimulationElement for PushbackTug {
         self.state = reader.read(&self.state_id);
 
         self.steering_angle_raw = Angle::new::<radian>(reader.read(&self.steer_angle_id));
-
-        self.gsx_pin_inserted = reader.read(&self.gsx_pin_state_id);
-    }
-
-    fn write(&self, writer: &mut SimulatorWriter) {
-        writer.write(
-            &self.nw_strg_disc_memo_id,
-            self.is_nose_wheel_steering_pin_inserted(),
-        );
     }
 }
