b3b4205601597adad7d52f15aafb641e9eaee165
lukecologne <lukasmichels00@gmail.com>
2024-01-14 00:40:16 +0100

    chore: ' to " in jsx

--- fbw-a380x/src/systems/instruments/src/SD/Pages/Doors/DoorPage.tsx
+++ fbw-a380x/src/systems/instruments/src/SD/Pages/Doors/DoorPage.tsx
@@ -17,42 +17,42 @@ export const DoorPage = () => {
 
     return (
         <>
-            <PageTitle showMore={false} x={5} y={28}>DOOR</PageTitle>
-            <text x='599' y='28' className='EcamPageTitle'>OXYGEN</text>
+            <PageTitle x={5} y={28}>DOOR</PageTitle>
+            <text x="599" y="28" className="F36 White TextUnderline">OXYGEN</text>
             <Oxygen x={634} y={108} active={sdacActive} onGround={onGround} />
 
-            <path className='White SW3 StrokeRound' d='M567,2 l 0,659' />
+            <path className="White SW3 StrokeRound" d="M567,2 l 0,659" />
 
-            <text x={285} y={159} className='White F22 MiddleAlign LS1'>MAIN</text>
+            <text x={285} y={159} className="White F22 MiddleAlign LS1">MAIN</text>
             <A380XDoors windowLeft={windowLeft === 0} windowRight={windowRight === 0} />
-            <text x={285} y={216} className='White F22 MiddleAlign LS1'>UPPER</text>
+            <text x={285} y={216} className="White F22 MiddleAlign LS1">UPPER</text>
 
             {/* Cabin Doors */}
-            <CabinDoor x={183} y={201} side='L' mainOrUpper='MAIN' doorNumber={1} engineRunning={engineRunning} />
-            <CabinDoor x={183} y={314} side='L' mainOrUpper='MAIN' doorNumber={2} engineRunning={engineRunning} />
-            <CabinDoor x={183} y={402} side='L' mainOrUpper='MAIN' doorNumber={3} engineRunning={engineRunning} />
-            <CabinDoor x={183} y={477} side='L' mainOrUpper='MAIN' doorNumber={4} engineRunning={engineRunning} />
-            <CabinDoor x={183} y={621} side='L' mainOrUpper='MAIN' doorNumber={5} engineRunning={engineRunning} />
+            <CabinDoor x={183} y={201} side="L" mainOrUpper="MAIN" doorNumber={1} engineRunning={engineRunning} />
+            <CabinDoor x={183} y={314} side="L" mainOrUpper="MAIN" doorNumber={2} engineRunning={engineRunning} />
+            <CabinDoor x={183} y={402} side="L" mainOrUpper="MAIN" doorNumber={3} engineRunning={engineRunning} />
+            <CabinDoor x={183} y={477} side="L" mainOrUpper="MAIN" doorNumber={4} engineRunning={engineRunning} />
+            <CabinDoor x={183} y={621} side="L" mainOrUpper="MAIN" doorNumber={5} engineRunning={engineRunning} />
 
-            <CabinDoor x={368} y={201} side='R' mainOrUpper='MAIN' doorNumber={1} engineRunning={engineRunning} />
-            <CabinDoor x={368} y={314} side='R' mainOrUpper='MAIN' doorNumber={2} engineRunning={engineRunning} />
-            <CabinDoor x={368} y={402} side='R' mainOrUpper='MAIN' doorNumber={3} engineRunning={engineRunning} />
-            <CabinDoor x={368} y={477} side='R' mainOrUpper='MAIN' doorNumber={4} engineRunning={engineRunning} />
-            <CabinDoor x={368} y={621} side='R' mainOrUpper='MAIN' doorNumber={5} engineRunning={engineRunning} />
+            <CabinDoor x={368} y={201} side="R" mainOrUpper="MAIN" doorNumber={1} engineRunning={engineRunning} />
+            <CabinDoor x={368} y={314} side="R" mainOrUpper="MAIN" doorNumber={2} engineRunning={engineRunning} />
+            <CabinDoor x={368} y={402} side="R" mainOrUpper="MAIN" doorNumber={3} engineRunning={engineRunning} />
+            <CabinDoor x={368} y={477} side="R" mainOrUpper="MAIN" doorNumber={4} engineRunning={engineRunning} />
+            <CabinDoor x={368} y={621} side="R" mainOrUpper="MAIN" doorNumber={5} engineRunning={engineRunning} />
 
-            <CabinDoor x={239} y={350} side='L' mainOrUpper='UPPER' doorNumber={1} engineRunning={engineRunning} />
-            <CabinDoor x={239} y={440} side='L' mainOrUpper='UPPER' doorNumber={2} engineRunning={engineRunning} />
-            <CabinDoor x={239} y={556} side='L' mainOrUpper='UPPER' doorNumber={3} engineRunning={engineRunning} />
+            <CabinDoor x={239} y={350} side="L" mainOrUpper="UPPER" doorNumber={1} engineRunning={engineRunning} />
+            <CabinDoor x={239} y={440} side="L" mainOrUpper="UPPER" doorNumber={2} engineRunning={engineRunning} />
+            <CabinDoor x={239} y={556} side="L" mainOrUpper="UPPER" doorNumber={3} engineRunning={engineRunning} />
 
-            <CabinDoor x={310} y={350} side='R' mainOrUpper='UPPER' doorNumber={1} engineRunning={engineRunning} />
-            <CabinDoor x={310} y={440} side='R' mainOrUpper='UPPER' doorNumber={2} engineRunning={engineRunning} />
-            <CabinDoor x={310} y={556} side='R' mainOrUpper='UPPER' doorNumber={3} engineRunning={engineRunning} />
+            <CabinDoor x={310} y={350} side="R" mainOrUpper="UPPER" doorNumber={1} engineRunning={engineRunning} />
+            <CabinDoor x={310} y={440} side="R" mainOrUpper="UPPER" doorNumber={2} engineRunning={engineRunning} />
+            <CabinDoor x={310} y={556} side="R" mainOrUpper="UPPER" doorNumber={3} engineRunning={engineRunning} />
 
             {/* Cargo Doors */}
-            <CargoDoor x={222} y={165} label='AVNCS' width={27} height={20} engineRunning={engineRunning} />
-            <CargoDoor x={359} y={250} label='FWD CARGO' width={26} height={46} engineRunning={engineRunning} />
-            <CargoDoor x={359} y={515} label='AFT CARGO' width={26} height={40} engineRunning={engineRunning} />
-            <CargoDoor x={359} y={590} label='BULK' width={26} height={26} engineRunning={engineRunning} />
+            <CargoDoor x={222} y={165} label="AVNCS" width={27} height={20} engineRunning={engineRunning} />
+            <CargoDoor x={359} y={250} label="FWD CARGO" width={26} height={46} engineRunning={engineRunning} />
+            <CargoDoor x={359} y={515} label="AFT CARGO" width={26} height={40} engineRunning={engineRunning} />
+            <CargoDoor x={359} y={590} label="BULK" width={26} height={26} engineRunning={engineRunning} />
         </>
     );
 };
--- fbw-a380x/src/systems/instruments/src/SD/Pages/Doors/elements/CargoDoor.tsx
+++ fbw-a380x/src/systems/instruments/src/SD/Pages/Doors/elements/CargoDoor.tsx
@@ -15,8 +15,8 @@ const CargoDoor: React.FC<Position & CargoDoorProps> = ({ x, y, label, width, he
 
     return (
         <g id={`${label}-door`} transform={`translate(${x} ${y})`}>
-            <rect x={0} y={0} width={width} height={height} rx='4' className={!validSDAC || doorOpen ? 'Amber SW2' : 'Green SW2'} />
-            <rect x={3} y={4} width={width - 6} height={height - 8} rx='4' className={!validSDAC || doorOpen ? 'Amber SW2 AmberFill' : 'Green SW2 NoFill'} />
+            <rect x={0} y={0} width={width} height={height} rx="4" className={!validSDAC || doorOpen ? 'Amber SW2' : 'Green SW2'} />
+            <rect x={3} y={4} width={width - 6} height={height - 8} rx="4" className={!validSDAC || doorOpen ? 'Amber SW2 AmberFill' : 'Green SW2 NoFill'} />
             <text x={label === 'AVNCS' ? -160 : 35} y={label === 'AVNCS' ? 18 : 20} className={`${!validSDAC ? 'White' : 'AmberFill'} F24`}>{cargoDoorMessage}</text>
         </g>
     );
