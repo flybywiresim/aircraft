c41259726b86193d7f371072f1d94ec961a437a2
Frank Kopp <frank@familie-kopp.de>
2024-01-16 17:34:52 +0100

    Added systems-host from A32NX to A380X

---
+++ fbw-a380x/src/systems/systems-host/build.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2021-2023 FlyByWire Simulations
+//
+// SPDX-License-Identifier: GPL-3.0
+
+'use strict';
+
+const esbuild = require('esbuild');
+const path = require('path');
+const { createModuleBuild } = require('#build-utils');
+
+const outFile = 'fbw-a380x/out/flybywire-aircraft-a380-842/html_ui/Pages/VCockpit/Instruments/A380X/SystemsHost/index.js';
+
+// process.env.FBW_TYPECHECK = "1";
+
+esbuild.build(createModuleBuild('fbw-a380x', undefined, path.join(__dirname, './index.ts'), outFile, __dirname));
