9702d797254833967010678e9511ab1021b5d395
Frank Kopp <frank@familie-kopp.de>
2024-01-17 21:07:21 +0100

    Removed commented part in build script

--- fbw-a380x/src/systems/systems-host/build.js
+++ fbw-a380x/src/systems/systems-host/build.js
@@ -10,6 +10,4 @@ const { createModuleBuild } = require('#build-utils');
 
 const outFile = 'fbw-a380x/out/flybywire-aircraft-a380-842/html_ui/Pages/VCockpit/Instruments/A380X/SystemsHost/index.js';
 
-// process.env.FBW_TYPECHECK = "1";
-
 esbuild.build(createModuleBuild('fbw-a380x', undefined, path.join(__dirname, './index.ts'), outFile, __dirname));
