799be7ee2537cfd78306895396b8072120ba2e29
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2024-02-02 21:14:18 -0500

    fix: import madness 3

--- fbw-common/src/typings/fs-base-ui/html_ui/JS/Avionics.d.ts
+++ fbw-common/src/typings/fs-base-ui/html_ui/JS/Avionics.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../../units.d.ts" />
+/// <reference path="../../../types.d.ts" />
 /// <reference path="./Types.d.ts" />
 
 declare global {
--- fbw-common/src/typings/index.d.ts
+++ fbw-common/src/typings/index.d.ts
@@ -3,5 +3,5 @@ export * from './asobo-vcockpits-instruments-a320-neo';
 export * from './asobo-vcockpits-instruments-airliners';
 export * from './flybywire-vcockpits-instruments';
 export * from './fs-base-ui';
+export * from './fmgc';
 export * from './types';
-export * from './units';
