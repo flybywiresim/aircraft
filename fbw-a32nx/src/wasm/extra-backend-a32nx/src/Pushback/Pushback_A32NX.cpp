02ad95a507784e851910ea9ef00ade401d3a03db
Frank Kopp <frank@familie-kopp.de>
2024-02-03 18:14:29 +0100

    Tuned the A32NX pushback movements in wind

--- fbw-a32nx/src/wasm/extra-backend-a32nx/src/Pushback/Pushback_A32NX.cpp
+++ fbw-a32nx/src/wasm/extra-backend-a32nx/src/Pushback/Pushback_A32NX.cpp
@@ -5,10 +5,13 @@
 
 FLOAT64 Pushback_A32NX::calculateCounterRotAccel(const FLOAT64 inertiaSpeed, AircraftVariablePtr& windVelBodyZ) const {
   FLOAT64 movementCounterRotAccel = 0.0;
+  // the calculation below has been determined by trial and error
+  // Tuned up to a wind from the front or back of 39kts or 20m/s
+  const FLOAT64 speedInfluence = 1 + std::abs(inertiaSpeed / SPEED_FACTOR);
   if (inertiaSpeed > 0) {
-    movementCounterRotAccel = (windVelBodyZ->get() / 2000.0) - 0.5;
+    movementCounterRotAccel = speedInfluence * ((windVelBodyZ->get() / 2000.0) - 0.5);
   } else if (inertiaSpeed < 0) {
-    movementCounterRotAccel = (windVelBodyZ->get() / 2000.0) + 1.0;
+    movementCounterRotAccel = speedInfluence * ((windVelBodyZ->get() / 2000.0) + 1.0);
   }
   return movementCounterRotAccel;
 }
