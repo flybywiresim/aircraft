e877b6e96e7adf4873dbc9e4bcc0c7932b4d3c8e
Florian Gross <63071941+flogross89@users.noreply.github.com>
2024-02-12 01:06:45 +0100

    implement QNH pre-selection

--- fbw-a380x/src/base/flybywire-aircraft-a380-842/html_ui/Pages/VCockpit/Instruments/LegacyA380X/FCU/A380_FCU.js
+++ fbw-a380x/src/base/flybywire-aircraft-a380-842/html_ui/Pages/VCockpit/Instruments/LegacyA380X/FCU/A380_FCU.js
@@ -1305,6 +1305,8 @@ class A320_Neo_FCU_Pressure extends A320_Neo_FCU_Component {
         this.standardElem = this.getDivElement('Standard');
         this.textQFE = this.getTextElement('QFE');
         this.textQNH = this.getTextElement('QNH');
+        this.textPreSelBaro = this.getTextElement('PreSelBaroValue');
+        this.currentPreSelValue = null;
         this.refresh('QFE', true, 0, 0, true);
     }
 
@@ -1315,24 +1317,49 @@ class A320_Neo_FCU_Pressure extends A320_Neo_FCU_Component {
     }
 
     refresh(_mode, _isHGUnit, _value, _lightsTest, _force = false) {
-        if ((_mode != this.currentMode) || (_isHGUnit != this.isHGUnit) || (_value != this.currentValue) || (_lightsTest !== this.lightsTest) || _force) {
+        let preSelValue = SimVar.GetSimVarValue('L:A380X_BARO_PRESELECTED', 'number');
+        // Conversion of baro selection SimVar. I tried using a standard altimeter, didn't work.
+        if (preSelValue === 0) {
+            preSelValue = _isHGUnit ? 29.92 : 1013.25;
+            SimVar.SetSimVarValue('L:A380X_BARO_PRESELECTED', 'number', preSelValue);
+        }
+        if (preSelValue < 800 && !_isHGUnit) {
+            preSelValue = preSelValue / 0.02953;
+            SimVar.SetSimVarValue('L:A380X_BARO_PRESELECTED', 'number', preSelValue);
+        } else if (preSelValue > 800 && _isHGUnit) {
+            preSelValue = Math.round(preSelValue * 0.02953 / 100) * 100;
+            SimVar.SetSimVarValue('L:A380X_BARO_PRESELECTED', 'number', preSelValue);
+        }
+
+        if ((_mode != this.currentMode) || (_isHGUnit != this.isHGUnit) || (_value != this.currentValue) || (preSelValue != this.currentPreSelValue) || (_lightsTest !== this.lightsTest) || _force) {
             const wasStd = this.currentMode == 'STD' && _mode != 'STD';
             this.currentMode = _mode;
             this.isHGUnit = _isHGUnit;
             this.currentValue = _value;
+            this.currentPreSelValue = preSelValue;
             this.lightsTest = _lightsTest;
             if (this.lightsTest) {
                 this.standardElem.style.display = 'none';
                 this.selectedElem.style.display = 'block';
                 this.setTextElementActive(this.textQFE, true, true);
                 this.setTextElementActive(this.textQNH, true, true);
+                this.setTextElementActive(this.textPreSelBaro, true, true);
                 this.textValueContent = '88.88';
+                this.textPreSelBaro.textContent = '88.88';
                 return;
             }
             if (this.currentMode == 'STD') {
                 this.standardElem.style.display = 'block';
                 this.selectedElem.style.display = 'none';
                 SimVar.SetSimVarValue('KOHLSMAN SETTING STD', 'Bool', 1);
+                const hPa = SimVar.GetSimVarValue('L:XMLVAR_Baro_Selector_HPA_1', 'boolean');
+                const preSelRender = Math.round(Math.max(!hPa ? (preSelValue * 100) : preSelValue, 0));
+                this.textPreSelBaro.textContent = !hPa ? `${preSelRender.toFixed(0).substring(0, 2)}.${preSelRender.toFixed(0).substring(2)}` : preSelRender.toFixed(0).padStart(4, '0');
+                if (Math.abs(preSelValue - 1013) < 0.5 || Math.abs(preSelValue - 29.92) < 0.005) {
+                    this.setTextElementActive(this.textPreSelBaro, false, true);
+                } else {
+                    this.setTextElementActive(this.textPreSelBaro, true, true);
+                }
             } else {
                 this.standardElem.style.display = 'none';
                 this.selectedElem.style.display = 'block';
@@ -1340,6 +1367,7 @@ class A320_Neo_FCU_Pressure extends A320_Neo_FCU_Component {
                 const isQFE = (this.currentMode == 'QFE');
                 this.setTextElementActive(this.textQFE, isQFE, true);
                 this.setTextElementActive(this.textQNH, !isQFE, true);
+                this.setTextElementActive(this.textPreSelBaro, false, true);
                 let value = Math.round(Math.max(this.isHGUnit ? (this.currentValue * 100) : this.currentValue, 0));
                 if (!wasStd) {
                     value = value.toString().padStart(4, '0');
