c05abf6189aef361b98500554bafe4b3e09638f5
lukecologne <lukasmichels00@gmail.com>
2024-03-07 23:41:36 +0100

    fix(adirs): adapt test cases

--- fbw-common/src/wasm/systems/systems/src/navigation/adirs.rs
+++ fbw-common/src/wasm/systems/systems/src/navigation/adirs.rs
@@ -3595,7 +3595,7 @@ mod tests {
                     .normal_value()
                     .unwrap()
                     .get::<percent>(),
-                (acc / g - test_bed.pitch(adiru_number).normal_value().unwrap().cos())
+                (acc / g + test_bed.pitch(adiru_number).normal_value().unwrap().sin())
                     .get::<ratio>()
             );
         }
@@ -3620,7 +3620,7 @@ mod tests {
                     .unwrap()
                     .get::<percent>(),
                 (acc / g
-                    + test_bed.pitch(adiru_number).normal_value().unwrap().cos()
+                    - test_bed.pitch(adiru_number).normal_value().unwrap().cos()
                         * test_bed.roll(adiru_number).normal_value().unwrap().sin())
                 .get::<ratio>()
             );
