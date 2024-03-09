eb8fa89028f36661af399c403ef56c779f5bba44
Frank Kopp <frank@familie-kopp.de>
2024-03-09 01:53:12 +0100

    Prepared A32NX fadec migration (wip)

--- fbw-a32nx/src/wasm/fadec_a32nx/src/Fadec/EngineControlA32Nx.cpp
+++ fbw-a32nx/src/wasm/fadec_a32nx/src/Fadec/EngineControlA32Nx.cpp
@@ -10,5 +10,41 @@ void EngineControl_A32NX::initialize(MsfsHandler* msfsHandler) {
   LOG_INFO("Fadec::EngineControl_A32NX::initialize() - initialized");
 }
 
-void EngineControl_A32NX::update() {}
-void EngineControl_A32NX::shutdown() {}
+void EngineControl_A32NX::update() {
+  profilerUpdate.start();
+
+  // Get ATC ID from sim to be able to load and store fuel levels
+  // If not yet available, request it from sim and return early
+  // If available initialize the engine control data
+  if (atcId.empty()) {
+    simData.atcIdDataPtr->requestUpdateFromSim(msfsHandlerPtr->getTimeStamp(), msfsHandlerPtr->getTickCounter());
+    if (simData.atcIdDataPtr->hasChanged()) {
+      atcId = simData.atcIdDataPtr->data().atcID;
+      LOG_INFO("Fadec::EngineControl_A32NX::update() - received ATC ID: " + atcId);
+      initializeEngineControlData();
+    }
+    return;
+  }
+
+  // TODO: Implement update logic
+
+  profilerUpdate.stop();
+  if (msfsHandlerPtr->getTickCounter() % 100 == 0) {
+    profilerUpdate.print();
+  }
+}
+
+void EngineControl_A32NX::shutdown() {
+  LOG_INFO("Fadec::EngineControl_A32NX::shutdown()");
+  // TODO: Implement shutdown if required
+}
+
+// =============================================================================
+// PRIVATE
+// =============================================================================
+
+void EngineControl_A32NX::initializeEngineControlData() {
+
+  // TODO Fuel level initialization
+
+}
--- fbw-a32nx/src/wasm/fadec_a32nx/src/Fadec/EngineControlA32Nx.h
+++ fbw-a32nx/src/wasm/fadec_a32nx/src/Fadec/EngineControlA32Nx.h
@@ -17,12 +17,40 @@ class EngineControl_A32NX {
   // FADEC simulation data
   FadecSimData_A32NX simData{};
 
+  // ATC ID for the aircraft used to load and store the fuel levels
+  std::string atcId{};
+
+
+
+  // additional constants
+  static constexpr double LBS_TO_KGS = 0.4535934;
+  static constexpr double KGS_TO_LBS = 1 / 0.4535934;
+  static constexpr double FUEL_THRESHOLD = 661;  // lbs/sec
+
+  // DEBUG
+  SimpleProfiler profilerUpdate{"Fadec::EngineControl_A32NX::update()", 100};
+  SimpleProfiler profilerGenerateParameters{"Fadec::EngineControl_A32NX::generateIdleParameters()", 100};
+  SimpleProfiler profilerEngineStateMachine{"Fadec::EngineControl_A32NX::engineStateMachine()", 100};
+  SimpleProfiler profilerEngineStartProcedure{"Fadec::EngineControl_A32NX::engineStartProcedure()", 100};
+  SimpleProfiler profilerEngineShutdownProcedure{"Fadec::EngineControl_A32NX::engineShutdownProcedure()", 100};
+  SimpleProfiler profilerUpdateFF{"Fadec::EngineControl_A32NX::updateFF()", 100};
+  SimpleProfiler profilerUpdatePrimaryParameters{"Fadec::EngineControl_A32NX::updatePrimaryParameters()", 100};
+  SimpleProfiler profilerUpdateEGT{"Fadec::EngineControl_A32NX::updateEGT()", 100};
+  SimpleProfiler profilerUpdateFuel{"Fadec::EngineControl_A32NX::updateFuel()", 100};
+  SimpleProfiler profilerUpdateThrustLimits{"Fadec::EngineControl_A32NX::updateThrustLimits()", 100};
+  SimpleProfiler profilerUpdateOil{"Fadec::EngineControl_A32NX::updateOil()", 100};
 
  public:
   void initialize(MsfsHandler* msfsHandler);
   void update();
   void shutdown();
 
+ private:
+  /**
+   * @brief Initialize the FADEC and Fuel model
+   * This is done after we have retrieved the ATC ID so we can load the fuel levels
+   */
+  void initializeEngineControlData();
 };
 
 #endif  // FLYBYWIRE_AIRCRAFT_ENGINECONTROLA32NX_H
