e89dcdc5f8d608fabf192291d033253d8f227571
BBK <22713769+BlueberryKing@users.noreply.github.com>
2024-03-24 19:51:48 +0100

    Disable SEC fpln functionality for now

--- fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_SecFplnMain.js
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_SecFplnMain.js
@@ -9,30 +9,34 @@ class CDUSecFplnMain {
         }
 
         mcdu.onLeftInput[0] = () => {
+            return;
             mcdu.flightPlanService.flightPlanManager.copy(Fmgc.FlightPlanIndex.Active, Fmgc.FlightPlanIndex.FirstSecondary);
             CDUFlightPlanPage.ShowPage(mcdu, 0, Fmgc.FlightPlanIndex.FirstSecondary);
         };
 
         mcdu.onLeftInput[1] = () => {
+            return;
             CDUFlightPlanPage.ShowPage(mcdu, 0, Fmgc.FlightPlanIndex.FirstSecondary);
         };
 
         mcdu.onLeftInput[2] = () => {
+            return;
             mcdu.flightPlanService.secondaryReset(1);
         };
 
         mcdu.onLeftInput[5] = () => {
+            return;
             mcdu.flightPlanService.flightPlanManager.swap(Fmgc.FlightPlanIndex.FirstSecondary, Fmgc.FlightPlanIndex.Active);
         };
 
         mcdu.setTemplate([
             ["SEC INDEX"],
             [""],
-            ["{COPY ACTIVE", "INIT>[color]inop"],
+            ["{COPY ACTIVE[color]inop", "INIT>[color]inop"],
             [""],
-            ["<SEC F-PLN", "PERF>[color]inop"],
+            ["<SEC F-PLN[color]inop", "PERF>[color]inop"],
             [""],
-            ["{DELETE SEC"],
+            ["{DELETE SEC[color]inop"],
             [""],
             ["*ACTIVATE SEC[color]inop"],
             [""],
