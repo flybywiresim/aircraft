56265ab16a195e8d1132917209cbb587144e7829
BBK <22713769+BlueberryKing@users.noreply.github.com>
2024-04-02 19:18:04 +0200

    Re-add licence identifier

--- fbw-a32nx/src/systems/instruments/src/SD/Pages/Status/elements/StatusDisplay.tsx
+++ fbw-a32nx/src/systems/instruments/src/SD/Pages/Status/elements/StatusDisplay.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 FlyByWire Simulations
+// SPDX-License-Identifier: GPL-3.0
+
 import EWDMessages from '@instruments/common/EWDMessages';
 import { useSimVar } from '@flybywiresim/fbw-sdk';
 import React from 'react';
