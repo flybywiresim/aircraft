d1585f0b36ca112ff0b933733215d8c6d031c5fb
Frank Kopp <frank@familie-kopp.de>
2024-04-12 00:28:12 +0200

    Fixed FLX-CLB thrust limit transition

--- fbw-a32nx/src/wasm/fadec_a32nx/src/Fadec/EngineControlA32NX.cpp
+++ fbw-a32nx/src/wasm/fadec_a32nx/src/Fadec/EngineControlA32NX.cpp
@@ -1003,25 +1003,26 @@ void EngineControl_A32NX::updateThrustLimits(double                  simulationT
   flex                 = flex_to + (flex_ga - flex_to) * machFactorLow;
 
   // adaption of CLB due to FLX limit if necessary ------------------------------------------------------------------
-  bool         isFlexActive    = false;
   const double thrustLimitType = simData.thrustLimitType->get();
   if ((prevThrustLimitType != 3 && thrustLimitType == 3) || (prevFlexTemperature == 0 && flexTemp > 0)) {
     isFlexActive = true;
+    std::cout << "Flex Active" << std::endl;
   } else if ((flexTemp == 0) || (thrustLimitType == 4)) {
     isFlexActive = false;
+    std::cout << "Flex Inactive" << std::endl;
   }
 
-  double transitionStartTime = 0;
-  double transitionFactor    = 0;
   if (isFlexActive && !isTransitionActive && thrustLimitType == 1) {
     isTransitionActive  = true;
     transitionStartTime = simulationTime;
     transitionFactor    = 0.2;
+    std::cout << "Transition Active" << std::endl;
     // transitionFactor = (clb - flex) / transitionTime;
   } else if (!isFlexActive) {
     isTransitionActive  = false;
     transitionStartTime = 0;
     transitionFactor    = 0;
+    std::cout << "Transition Inactive" << std::endl;
   }
 
   double deltaThrust = 0;
