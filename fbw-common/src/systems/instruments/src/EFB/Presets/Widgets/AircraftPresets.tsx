155cce39eda2b34e52b413f9b872f8eb8bf32fc8
Frank Kopp <frank@familie-kopp.de>
2024-04-27 19:19:02 +0200

    Fix lint fix rebase

--- fbw-common/src/systems/instruments/src/EFB/Presets/Widgets/AircraftPresets.tsx
+++ fbw-common/src/systems/instruments/src/EFB/Presets/Widgets/AircraftPresets.tsx
@@ -75,22 +75,22 @@ export const AircraftPresets = () => {
   }, [loadPresetVar]);
 
   return (
-    <div className="mt-4 h-content-section-reduced space-y-4 rounded-lg border-2 border-theme-accent p-4">
-      <div className="flex h-20 flex-row items-center justify-center space-x-2 rounded-md border-2 border-theme-accent p-2">
+    <div className="h-content-section-reduced border-theme-accent mt-4 space-y-4 rounded-lg border-2 p-4">
+      <div className="border-theme-accent flex h-20 flex-row items-center justify-center space-x-2 rounded-md border-2 p-2">
         {loadPresetVar ? (
           <>
-            <div className="h-full w-full content-center justify-center overflow-hidden rounded-md bg-theme-accent">
+            <div className="bg-theme-accent h-full w-full content-center justify-center overflow-hidden rounded-md">
               <span className="h-1/2 pl-3 pt-1 text-xl">
                 {t('Presets.AircraftStates.CurrentProcedureStep')} : {' '} {currentStepDescription}
               </span>
               <div
-                className="h-1/2 bg-theme-highlight"
+                className="bg-theme-highlight h-1/2"
                 style={{ width: `${loadPresetProgress * 100}%`, transition: 'width 0.1s ease' }}
               />
             </div>
 
             <div
-              className="flex h-full items-center rounded-md border-2 border-theme-highlight bg-theme-highlight px-4 text-theme-body transition duration-100 hover:bg-theme-body hover:text-theme-highlight"
+              className="border-theme-highlight bg-theme-highlight text-theme-body hover:bg-theme-body hover:text-theme-highlight flex h-full items-center rounded-md border-2 px-4 transition duration-100"
               onClick={() => handleCancel()}
             >
               {t('Presets.AircraftStates.Cancel')}
@@ -109,7 +109,7 @@ export const AircraftPresets = () => {
         {AircraftPresetsList.map(({ index, name }) => (
           <div
             key={index}
-            className={`flex h-24 items-center justify-center rounded-md border-2 border-theme-accent bg-theme-accent text-theme-text transition duration-100 hover:bg-theme-highlight hover:text-theme-body ${(!simOnGround || (loadPresetVar && loadPresetVar !== index)) && 'pointer-events-none opacity-50'}`}
+            className={`border-theme-accent bg-theme-accent text-theme-text hover:bg-theme-highlight hover:text-theme-body flex h-24 items-center justify-center rounded-md border-2 transition duration-100 ${(!simOnGround || (loadPresetVar && loadPresetVar !== index)) && 'pointer-events-none opacity-50'}`}
             onClick={() => handleLoadPreset(index)}
           >
             {name}
@@ -117,7 +117,7 @@ export const AircraftPresets = () => {
         ))}
       </ScrollableContainer>
 
-      <div className="mt-14 rounded-md border-2 border-theme-accent px-4 py-1">
+      <div className="border-theme-accent mt-14 rounded-md border-2 px-4 py-1">
         <div className="flex h-10 flex-row items-center">
           <div className="pr-3">
             {t('Presets.AircraftStates.ExpediteLoading')}
