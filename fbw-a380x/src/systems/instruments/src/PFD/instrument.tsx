f134667b9f5e9b81e3616b66d50d3db16835a808
Florian Gross <63071941+flogross89@users.noreply.github.com>
2024-05-05 02:07:07 +0200

    lint + lint-fixes

--- fbw-a380x/src/systems/instruments/src/PFD/instrument.tsx
+++ fbw-a380x/src/systems/instruments/src/PFD/instrument.tsx
@@ -13,92 +13,92 @@ import './style.scss';
 import { RopRowOansPublisher, TawsPublisher } from '@flybywiresim/msfs-avionics-common';
 
 class A380X_PFD extends BaseInstrument {
-    private readonly bus = new ArincEventBus();
+  private readonly bus = new ArincEventBus();
 
-    private readonly backplane = new InstrumentBackplane();
+  private readonly backplane = new InstrumentBackplane();
 
-    private readonly clock = new Clock(this.bus);
+  private readonly clock = new Clock(this.bus);
 
-    private readonly hEventPublisher= new HEventPublisher(this.bus);
+  private readonly hEventPublisher = new HEventPublisher(this.bus);
 
-    private readonly simVarPublisher = new PFDSimvarPublisher(this.bus);
+  private readonly simVarPublisher = new PFDSimvarPublisher(this.bus);
 
-    private readonly arincProvider = new ArincValueProvider(this.bus);
+  private readonly arincProvider = new ArincValueProvider(this.bus);
 
-    private readonly simplaneValueProvider = new SimplaneValueProvider(this.bus);
+  private readonly simplaneValueProvider = new SimplaneValueProvider(this.bus);
 
-    private readonly adirsValueProvider = new AdirsValueProvider(this.bus, this.simVarPublisher);
+  private readonly adirsValueProvider = new AdirsValueProvider(this.bus, this.simVarPublisher);
 
-    private readonly dmcPublisher = new DmcPublisher(this.bus);
+  private readonly dmcPublisher = new DmcPublisher(this.bus);
 
-    private readonly fmsDataPublisher: FmsDataPublisher;
+  private readonly fmsDataPublisher: FmsDataPublisher;
 
-    private readonly ropRowOansPublisher = new RopRowOansPublisher(this.bus);
+  private readonly ropRowOansPublisher = new RopRowOansPublisher(this.bus);
 
-    private readonly tawsPublisher = new TawsPublisher(this.bus);
+  private readonly tawsPublisher = new TawsPublisher(this.bus);
 
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        const side: EfisSide = getDisplayIndex() === 1 ? 'L' : 'R';
-        const stateSubject = Subject.create<'L' | 'R'>(side);
-        this.fmsDataPublisher = new FmsDataPublisher(this.bus, stateSubject);
+    const side: EfisSide = getDisplayIndex() === 1 ? 'L' : 'R';
+    const stateSubject = Subject.create<'L' | 'R'>(side);
+    this.fmsDataPublisher = new FmsDataPublisher(this.bus, stateSubject);
 
-        this.backplane.addInstrument('Clock', this.clock);
-        this.backplane.addPublisher('HEvent', this.hEventPublisher);
-        this.backplane.addPublisher('PfdSimVars', this.simVarPublisher);
-        this.backplane.addInstrument('ArincProvider', this.arincProvider);
-        this.backplane.addInstrument('Simplane', this.simplaneValueProvider);
-        this.backplane.addInstrument('AdirsProvider', this.adirsValueProvider);
-        this.backplane.addPublisher('DmcPublisher', this.dmcPublisher);
-        this.backplane.addPublisher('FmsDataPublisher', this.fmsDataPublisher);
-        this.backplane.addPublisher('RopRowOansPublisher', this.ropRowOansPublisher);
-        this.backplane.addPublisher('TawsPublisher', this.tawsPublisher);
-    }
+    this.backplane.addInstrument('Clock', this.clock);
+    this.backplane.addPublisher('HEvent', this.hEventPublisher);
+    this.backplane.addPublisher('PfdSimVars', this.simVarPublisher);
+    this.backplane.addInstrument('ArincProvider', this.arincProvider);
+    this.backplane.addInstrument('Simplane', this.simplaneValueProvider);
+    this.backplane.addInstrument('AdirsProvider', this.adirsValueProvider);
+    this.backplane.addPublisher('DmcPublisher', this.dmcPublisher);
+    this.backplane.addPublisher('FmsDataPublisher', this.fmsDataPublisher);
+    this.backplane.addPublisher('RopRowOansPublisher', this.ropRowOansPublisher);
+    this.backplane.addPublisher('TawsPublisher', this.tawsPublisher);
+  }
 
-    get templateID(): string {
-        return 'A380X_PFD';
-    }
+  get templateID(): string {
+    return 'A380X_PFD';
+  }
 
-    public getDeltaTime() {
-        return this.deltaTime;
-    }
+  public getDeltaTime() {
+    return this.deltaTime;
+  }
 
-    public onInteractionEvent(args: string[]): void {
-        this.hEventPublisher.dispatchHEvent(args[0]);
-    }
+  public onInteractionEvent(args: string[]): void {
+    this.hEventPublisher.dispatchHEvent(args[0]);
+  }
 
-    public connectedCallback(): void {
-        super.connectedCallback();
+  public connectedCallback(): void {
+    super.connectedCallback();
 
-        this.backplane.init();
+    this.backplane.init();
 
-        this.bus.getSubscriber<DmcEvents>().on('trueRefActive').handle(console.log);
+    this.bus.getSubscriber<DmcEvents>().on('trueRefActive').handle(console.log);
 
-        FSComponent.render(<PFDComponent bus={this.bus} instrument={this} />, document.getElementById('PFD_CONTENT'));
+    FSComponent.render(<PFDComponent bus={this.bus} instrument={this} />, document.getElementById('PFD_CONTENT'));
 
-        // Remove "instrument didn't load" text
-        document.getElementById('PFD_CONTENT').querySelector(':scope > h1').remove();
-    }
+    // Remove "instrument didn't load" text
+    document.getElementById('PFD_CONTENT').querySelector(':scope > h1').remove();
+  }
 
-    /**
+  /**
    * A callback called when the instrument gets a frame update.
    */
-    public Update(): void {
-        super.Update();
+  public Update(): void {
+    super.Update();
 
-        this.backplane.onUpdate();
-    }
+    this.backplane.onUpdate();
+  }
 
-    // FIXME remove. This does not belong in the PFD, and in any case we should use GameStateProvider as it has workarounds for issues with onFlightStart
-    protected onFlightStart() {
-        super.onFlightStart();
-        if (SimVar.GetSimVarValue('L:A32NX_IS_READY', 'number') !== 1) {
-            // set ready signal that JS code is initialized and flight is actually started
-            // -> user pressed 'READY TO FLY' button
-            SimVar.SetSimVarValue('L:A32NX_IS_READY', 'number', 1);
-        }
+  // FIXME remove. This does not belong in the PFD, and in any case we should use GameStateProvider as it has workarounds for issues with onFlightStart
+  protected onFlightStart() {
+    super.onFlightStart();
+    if (SimVar.GetSimVarValue('L:A32NX_IS_READY', 'number') !== 1) {
+      // set ready signal that JS code is initialized and flight is actually started
+      // -> user pressed 'READY TO FLY' button
+      SimVar.SetSimVarValue('L:A32NX_IS_READY', 'number', 1);
     }
+  }
 }
 
 registerInstrument('a380x-pfd', A380X_PFD);
