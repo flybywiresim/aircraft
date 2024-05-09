7549e0235ddcb5c06434efd9e79c33452cff1a99
2hwk <reasonstouninstall@tutanota.com>
2024-05-09 22:47:48 +0800

    refactor: pause at tod armed indicator simvar

--- fbw-a32nx/src/systems/fmgc/src/guidance/vnav/descent/TodGuidance.ts
+++ fbw-a32nx/src/systems/fmgc/src/guidance/vnav/descent/TodGuidance.ts
@@ -47,120 +47,68 @@ export class TodGuidance {
 
   update(deltaTime: number) {
     this.updateTdReached(deltaTime);
-    this.updateTdPause(deltaTime);
+    if (NXDataStore.get('PAUSE_AT_TOD', 'DISABLED') === 'ENABLED') {
+      this.updateTdPause(deltaTime);
+    }
   }
-  /***
-    reset flags on turnaround
 
-    t/d is armed is
-    (
-      pause at t/d enabled
-      and not back off timer > 0
-      and !paused at t/d
-      and CLB/CRZ
-      and A/P managed
-    )
-
-    if t/d is armed:
-      if t/d < config t/d distance:
-          PAUSE
-          back off timer +10s
-      elif current alt > transition alt:
-          if a/p engaged but not a/p active:
-            PAUSE
-            back off timer +10s
-          a/p engaged = a/p active
-
-    decrease back off timer until 0
-   */
-
-  /***
-    if pause at t/d enabled and not back off timer > 0:
-      if !paused at t/d and CLB/CRZ and A/P managed:
-        t/d is armed
-        if t/d < config t/d distance:
-            PAUSE
-            back off timer +10s
-        elif current alt > transition alt:
-            if ap engaged but not a/p active:
-              PAUSE
-              back off timer +10s
-            a/p engaged is a/p active
-      else:
-        t/d is not armed
-
-      reset flags on turnaround
-    else:
-      t/d is not armed
-
-    decrease back off timer until 0
-   */
   updateTdPause(deltaTime: number) {
-    if (this.cooldown <= 0 && NXDataStore.get('PAUSE_AT_TOD', 'DISABLED') === 'ENABLED') {
-      // Only watching if T/D pause untriggered + between flight phase CLB and CRZ
-      if (
-        !this.tdPaused &&
-        this.observer.get().flightPhase >= FmgcFlightPhase.Climb &&
-        this.observer.get().flightPhase <= FmgcFlightPhase.Cruise &&
-        Simplane.getAutoPilotAirspeedManaged()
-      ) {
-        // Pause at T/D is in an armed state
-        if (!this.tdArmed) {
-          this.tdArmed = true;
-          SimVar.SetSimVarValue('L:A32NX_PAUSE_AT_TOD_ARMED', 'bool', true);
-        }
+    // Only armed if all conditions met
+    this.tdArmed =
+      this.cooldown <= 0 &&
+      !this.tdPaused &&
+      this.observer.get().flightPhase >= FmgcFlightPhase.Climb &&
+      this.observer.get().flightPhase <= FmgcFlightPhase.Cruise &&
+      Simplane.getAutoPilotAirspeedManaged();
 
-        // Check T/D pause first
-        if (
-          (this.aircraftToDescentProfileRelation.distanceToTopOfDescent() ?? Number.POSITIVE_INFINITY) <
-          parseFloat(NXDataStore.get('PAUSE_AT_TOD_DISTANCE', '10'))
-        ) {
-          this.tdPaused = true;
+    if (SimVar.GetSimVarValue('L:A32NX_PAUSE_AT_TOD_ARMED', 'bool') !== this.tdArmed)
+      SimVar.SetSimVarValue('L:A32NX_PAUSE_AT_TOD_ARMED', 'bool', this.tdArmed);
+
+    if (this.tdArmed) {
+      // Check T/D pause first
+      if (
+        (this.aircraftToDescentProfileRelation.distanceToTopOfDescent() ?? Number.POSITIVE_INFINITY) <
+        parseFloat(NXDataStore.get('PAUSE_AT_TOD_DISTANCE', '10'))
+      ) {
+        this.tdPaused = true;
+        this.showPausePopup(
+          'TOP OF DESCENT',
+          `Paused before the calculated top of descent. System Time was ${new Date().toLocaleTimeString()}.`,
+        );
+        // Check A/P mode reversion
+      } else if (
+        // Only guard A/P above transitional altitude
+        this.atmosphericConditions.currentAltitude
+          ? this.atmosphericConditions.currentAltitude > this.observer.get().originTransitionAltitude
+          : false
+      ) {
+        const apActive =
+          SimVar.GetSimVarValue('L:A32NX_AUTOPILOT_ACTIVE', 'boolean') &&
+          SimVar.GetSimVarValue('L:A32NX_FMA_LATERAL_MODE', 'Enum') === LateralMode.NAV;
+
+        if (this.apEngaged && !apActive) {
           this.showPausePopup(
-            'TOP OF DESCENT',
-            `Paused before the calculated top of descent. System Time was ${new Date().toLocaleTimeString()}.`,
+            'AP PROTECTION',
+            `Autopilot or lateral guidance disengaged before the calculated top of descent. System Time was ${new Date().toLocaleTimeString()}.`,
           );
-          // Check AP mode reversion
-        } else if (
-          // Only guard AP above transitional altitude
-          this.atmosphericConditions.currentAltitude
-            ? this.atmosphericConditions.currentAltitude > this.observer.get().originTransitionAltitude
-            : false
-        ) {
-          const apActive =
-            SimVar.GetSimVarValue('L:A32NX_AUTOPILOT_ACTIVE', 'boolean') &&
-            SimVar.GetSimVarValue('L:A32NX_FMA_LATERAL_MODE', 'Enum') === LateralMode.NAV;
-
-          if (this.apEngaged && !apActive) {
-            this.showPausePopup(
-              'AP PROTECTION',
-              `Autopilot or lateral guidance disengaged before the calculated top of descent. System Time was ${new Date().toLocaleTimeString()}.`,
-            );
-          }
-
-          if (this.apEngaged !== apActive) {
-            this.apEngaged = apActive;
-          }
-          // Else T/D pause is not triggered but armed
         }
-      } else if (this.tdArmed) {
-        this.tdArmed = false;
-        SimVar.SetSimVarValue('L:A32NX_PAUSE_AT_TOD_ARMED', 'bool', false);
-      }
 
-      // Reset flags on turnaround
-      if (
-        this.observer.get().flightPhase === FmgcFlightPhase.Done ||
-        this.observer.get().flightPhase === FmgcFlightPhase.Preflight
-      ) {
-        this.tdPaused = false;
-        this.apEngaged = false;
+        if (this.apEngaged !== apActive) {
+          this.apEngaged = apActive;
+        }
+        // Else T/D pause is not triggered but armed
       }
-    } else if (this.tdArmed) {
-      this.tdArmed = false;
-      SimVar.SetSimVarValue('L:A32NX_PAUSE_AT_TOD_ARMED', 'bool', false);
     }
 
+    // Reset flags on turnaround
+    if (
+      this.observer.get().flightPhase === FmgcFlightPhase.Done ||
+      this.observer.get().flightPhase === FmgcFlightPhase.Preflight
+    ) {
+      this.tdPaused = false;
+      this.apEngaged = false;
+    }
+    // Iterate backoff timer
     if (this.cooldown > 0) {
       this.cooldown = Math.max(0, this.cooldown - deltaTime);
     }
