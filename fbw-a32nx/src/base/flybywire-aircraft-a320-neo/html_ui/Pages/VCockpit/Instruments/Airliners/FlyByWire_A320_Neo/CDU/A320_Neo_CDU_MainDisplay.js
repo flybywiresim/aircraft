697467cec581f6f7b2b05ba4b82dcdcf36aedca1
rahulrathnakumar <rahulrathnakumar.ae@gmail.com>
2024-05-27 14:52:18 -0700

    fix: small edit to condition

--- fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_MainDisplay.js
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_MainDisplay.js
@@ -553,7 +553,7 @@ class A320_Neo_CDU_MainDisplay extends FMCMainDisplay {
         }
 
         if (!this.aocTimes.on) {
-            if (this.aocTimes.off && !this.isOnGround() && this.FmgcFlightPhases != FmgcFlightPhases.TAKEOFF) {
+            if (this.aocTimes.off && this.isOnGround() && this.FmgcFlightPhases != FmgcFlightPhases.TAKEOFF) {
                 // On: remains blank until Landing time
                 this.aocTimes.on = Math.floor(SimVar.GetGlobalVarValue("ZULU TIME", "seconds"));
             }
