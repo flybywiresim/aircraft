09cef9912a89b1042275df9606532ffddf646857
rahulrathnakumar <rahulrathnakumar.ae@gmail.com>
2024-05-27 13:42:46 -0700

    cleanup: removed comments.

--- fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_MainDisplay.js
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_MainDisplay.js
@@ -535,7 +535,6 @@ class A320_Neo_CDU_MainDisplay extends FMCMainDisplay {
             this.sendUpdate();
         }
     }
-    // this function seems to be the place to make changes
     checkAocTimes() {
         if (!this.aocTimes.off) {
             if (this.flightPhaseManager.phase === FmgcFlightPhases.TAKEOFF && !this.isOnGround()) {
@@ -554,20 +553,6 @@ class A320_Neo_CDU_MainDisplay extends FMCMainDisplay {
         }
 
         if (!this.aocTimes.on) {
-            /*
-            this.isOnGround() works as follows (from flightphase.ts):
-
-            export function isOnGround(): boolean {
-            return (
-                SimVar.GetSimVarValue('L:A32NX_LGCIU_1_NOSE_GEAR_COMPRESSED', 'bool') ||
-                SimVar.GetSimVarValue('L:A32NX_LGCIU_2_NOSE_GEAR_COMPRESSED', 'bool')
-            );
-            }
-            The nose gear compression seems to stay on right after takeoff. Not going to modify this for now.
-            Instead, I can add another condition such that it doesn't trigger at the same time as aocTimes.out.
-            Temporarily using fmgc flight phase. Need to test for edge cases.
-
-            */
             if (this.aocTimes.off && !this.isOnGround() && !this.FmgcFlightPhases == FmgcFlightPhases.TAKEOFF) {
                 // On: remains blank until Landing time
                 this.aocTimes.on = Math.floor(SimVar.GetGlobalVarValue("ZULU TIME", "seconds"));
