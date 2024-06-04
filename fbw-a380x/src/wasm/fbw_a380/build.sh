2882c22e2fa7808a8702b158e8dfa7ab85ab5094
Saschl <19493808+Saschl@users.noreply.github.com>
2024-06-04 19:01:49 +0200

    chore: adapt rust build to SU15 (#8627)
    * chore: update rust and adapt to SU15
    
    * revert rust update as it is contained in another PR
    
    * update linker flags
    
    * remove --stack-guard-page
    
    Does not work with standard llvm, seems to be a custom option by asobo that only works with the wasm-ld.exe from the msfs-sdk
    
    * enable bulk memory for rust
    
    * add missing flags in fbw builds
    
    ---------
    
    Co-authored-by: 2hwk <15316958+2hwk@users.noreply.github.com>

--- fbw-a380x/src/wasm/fbw_a380/build.sh
+++ fbw-a380x/src/wasm/fbw_a380/build.sh
@@ -80,6 +80,11 @@ clang++ \
   -fno-exceptions \
   -fms-extensions \
   -fvisibility=hidden \
+  -fdata-sections \
+  -fno-stack-protector \
+  -fstack-size-section \
+  -mbulk-memory \
+  -Werror=return-type \
   -I "${MSFS_SDK}/WASM/include" \
   -I "${MSFS_SDK}/SimConnect SDK/include" \
   -I "${COMMON_DIR}/fbw_common/src" \
