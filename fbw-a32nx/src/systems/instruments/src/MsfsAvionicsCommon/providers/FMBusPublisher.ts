78bbcddd8297337fec9e192e5f10890494c8f81d
Michael Corcoran <tracer@outlook.co.nz>
2024-06-06 21:43:23 +1200

    feat(nd): backbeam loc

--- fbw-a32nx/src/systems/instruments/src/MsfsAvionicsCommon/providers/FMBusPublisher.ts
+++ fbw-a32nx/src/systems/instruments/src/MsfsAvionicsCommon/providers/FMBusPublisher.ts
@@ -12,6 +12,12 @@ export interface FMBusEvents {
   'fm.1.tuning_discrete_word': number;
 
   'fm.2.tuning_discrete_word': number;
+
+  // FIXME get the backbeam state from the FG when implemented
+  /** Whether FM1 localiser course is a backbeam. */
+  'fm.1.backbeam': boolean;
+  /** Whether FM2 localiser course is a backbeam. */
+  'fm.2.backbeam': boolean;
 }
 
 export class FMBusPublisher extends SimVarPublisher<FMBusEvents> {
@@ -22,6 +28,8 @@ export class FMBusPublisher extends SimVarPublisher<FMBusEvents> {
         ['fm.2.healthy_discrete', { name: 'L:A32NX_FM2_HEALTHY_DISCRETE', type: SimVarValueType.Bool }],
         ['fm.1.tuning_discrete_word', { name: 'L:A32NX_FM1_NAV_DISCRETE', type: SimVarValueType.Number }],
         ['fm.2.tuning_discrete_word', { name: 'L:A32NX_FM2_NAV_DISCRETE', type: SimVarValueType.Number }],
+        ['fm.1.backbeam', { name: 'L:A32NX_FM1_BACKBEAM_SELECTED', type: SimVarValueType.Bool }],
+        ['fm.2.backbeam', { name: 'L:A32NX_FM2_BACKBEAM_SELECTED', type: SimVarValueType.Bool }],
       ]),
       bus,
     );
