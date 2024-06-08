cb0a14ba1e747b6c0da7a114abfeba6b901dab5e
BBK <22713769+BlueberryKing@users.noreply.github.com>
2024-06-08 10:26:57 +0200

    Treat HX legs equally

--- fbw-a32nx/src/systems/fmgc/src/flightplanning/new/plans/FlightPlan.ts
+++ fbw-a32nx/src/systems/fmgc/src/flightplanning/new/plans/FlightPlan.ts
@@ -219,10 +219,9 @@ export class FlightPlan<P extends FlightPlanPerformanceData = FlightPlanPerforma
    * @param discontinuityIndex
    */
   private cleanUpAfterDiscontinuity(discontinuityIndex: number) {
-    // Find next XF/HM leg
+    // Find next XF/HX leg
     const xFLegIndexInPlan = this.allLegs.findIndex(
-      (it, index) =>
-        index > discontinuityIndex && it.isDiscontinuity === false && (it.isXF() || it.type === LegType.HM),
+      (it, index) => index > discontinuityIndex && it.isDiscontinuity === false && (it.isXF() || it.isHX()),
     );
 
     if (xFLegIndexInPlan !== -1) {
