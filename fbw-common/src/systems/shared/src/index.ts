5bd23bd003e0ff29026d5d2a1f92188fed92e445
Michael Corcoran <tracer@outlook.co.nz>
2024-06-27 18:25:19 +1200

    feat(a380x): rmp part 1 (#8686)
    * fix: remove a32nx and asobo cruft
    
    * feat(a380x): rmp part 1
    
    * [MFD/RMP] transponder control events
    
    * fix: radio tests
    
    * fix: re-add missing component
    
    * fix: integs, ann lt test, and knob push speed
    
    * fix(rmp): audio controls
    
    * feat(rmp): reception not selected messages
    
    * fix(rmp): smaller font when non-validated squawk
    
    * feat: radio nav audio and refactor of audio
    
    * doc: document lvars and input events
    
    * fix: undo erroneous changes to dead code (rebase error?)
    
    ---------
    
    Co-authored-by: Florian Gross <63071941+flogross89@users.noreply.github.com>

--- fbw-common/src/systems/shared/src/index.ts
+++ fbw-common/src/systems/shared/src/index.ts
@@ -13,6 +13,8 @@ export * from './FmMessages';
 export * from './GenericDataListenerSync';
 export * from './MathUtils';
 export * from './PathVector';
+export * from './RadioTypes';
+export * from './RadioUtils';
 export * from './RunwayUtils';
 export * from './UpdateThrottler';
 export * from './UniversalConfigProvider';
