b26498bf53b411fb3e0c407cf46a7eff98a7240e
Florian Gross <63071941+flogross89@users.noreply.github.com>
2024-07-01 14:49:22 +0300

    fix spoiler deflection

--- fbw-a380x/src/systems/instruments/src/SD/Pages/Fctl/elements/Spoiler.tsx
+++ fbw-a380x/src/systems/instruments/src/SD/Pages/Fctl/elements/Spoiler.tsx
@@ -24,7 +24,7 @@ export function deflectionToYOffset(deflection: number, maxDeflection: number): 
 
 export const Spoiler: FC<SpoilerProps> = ({ x, y, side, position, onGround }) => {
     const [spoilerDeflection]: [number, (v: number) => void] = useSimVar(`L:A32NX_HYD_SPOILER_${position}_${side}_DEFLECTION`, 'number', 100);
-    const maxDeflection = position >= 3 ? 50 : 30;
+    const maxDeflection = position >= 3 ? 50 : 35;
 
     const deflectionYVal = deflectionToYOffset(spoilerDeflection * 50, maxDeflection);
 
