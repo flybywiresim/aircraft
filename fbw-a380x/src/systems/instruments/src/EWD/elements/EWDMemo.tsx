231cf18e55d4ce8fe6a93531da3842dd0a620ec0
Florian Gross <63071941+flogross89@users.noreply.github.com>
2024-07-02 19:41:17 +0300

    use a32nx fwc for ECAM

--- fbw-a380x/src/systems/instruments/src/EWD/elements/EWDMemo.tsx
+++ fbw-a380x/src/systems/instruments/src/EWD/elements/EWDMemo.tsx
@@ -1,5 +1,5 @@
-import FormattedFwcText from '@instruments/common/EWDMessageParser';
-import EWDMessages from '@instruments/common/EWDMessages';
+import FormattedFwcText from '../../Common/EWDMessageParser';
+import EWDMessages from '../../Common/EWDMessages';
 import { useSimVar } from '@instruments/common/simVars';
 import React from 'react';
 
@@ -9,17 +9,18 @@ interface EWDMemoProps {
     x: number,
     y: number,
     active: boolean,
+    side: 'LEFT' | 'RIGHT',
 }
 
-export const EWDMemo: React.FC<EWDMemoProps> = ({ x, y, active }) => {
-    const [line1] = useSimVar('L:A380X_EWD_RIGHT_LINE_1', 'number', 500);
-    const [line2] = useSimVar('L:A380X_EWD_RIGHT_LINE_2', 'number', 500);
-    const [line3] = useSimVar('L:A380X_EWD_RIGHT_LINE_3', 'number', 500);
-    const [line4] = useSimVar('L:A380X_EWD_RIGHT_LINE_4', 'number', 500);
-    const [line5] = useSimVar('L:A380X_EWD_RIGHT_LINE_5', 'number', 500);
-    const [line6] = useSimVar('L:A380X_EWD_RIGHT_LINE_6', 'number', 500);
-    const [line7] = useSimVar('L:A380X_EWD_RIGHT_LINE_7', 'number', 500);
-    const [line8] = useSimVar('L:A380X_EWD_RIGHT_LINE_8', 'number', 500);
+export const EWDMemo: React.FC<EWDMemoProps> = ({ x, y, active, side }) => {
+    const [line1] = useSimVar(`L:A32NX_EWD_LOWER_${side}_LINE_1`, 'number', 500);
+    const [line2] = useSimVar(`L:A32NX_EWD_LOWER_${side}_LINE_2`, 'number', 500);
+    const [line3] = useSimVar(`L:A32NX_EWD_LOWER_${side}_LINE_3`, 'number', 500);
+    const [line4] = useSimVar(`L:A32NX_EWD_LOWER_${side}_LINE_4`, 'number', 500);
+    const [line5] = useSimVar(`L:A32NX_EWD_LOWER_${side}_LINE_5`, 'number', 500);
+    const [line6] = useSimVar(`L:A32NX_EWD_LOWER_${side}_LINE_6`, 'number', 500);
+    const [line7] = useSimVar(`L:A32NX_EWD_LOWER_${side}_LINE_7`, 'number', 500);
+    const [line8] = useSimVar(`L:A32NX_EWD_LOWER_${side}_LINE_8`, 'number', 500);
     const message = [
         EWDMessages[padEWDCode(line1)],
         EWDMessages[padEWDCode(line2)],
@@ -31,10 +32,10 @@ export const EWDMemo: React.FC<EWDMemoProps> = ({ x, y, active }) => {
         EWDMessages[padEWDCode(line8)],
     ].join('\r');
 
-    const numMemos = [line1, line2, line3, line4, line5, line6, line7, line8].filter(Boolean).length;
+    const numMemos = side === 'LEFT' ? 0 : [line1, line2, line3, line4, line5, line6, line7, line8].filter(Boolean).length;
 
     return (
-        <g id="EWDMemo" className={active ? 'Show' : 'Hide'}>
+        <g id={`EWDMemo${side}`} className={active ? 'Show' : 'Hide'}>
 
             <path className={numMemos > 0 ? 'WhiteLine' : 'Hide'} d={`M ${x - 10},${y - 23} l 0,${6 + (numMemos * 30)}`} />
             <FormattedFwcText x={x} y={y} message={message} />
