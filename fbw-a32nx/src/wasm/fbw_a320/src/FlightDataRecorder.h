bf8995bb75b40120614ce501d9b6e1704e1596ca
Lukas Michels <lukasmichels00@gmail.com>
2024-07-26 19:36:10 +0200

    feat(fdr): add FMGCs to FDR

--- fbw-a32nx/src/wasm/fbw_a320/src/FlightDataRecorder.h
+++ fbw-a32nx/src/wasm/fbw_a320/src/FlightDataRecorder.h
@@ -4,16 +4,17 @@
 
 #include "AdditionalData.h"
 #include "EngineData.h"
+#include "FmgcComputer_types.h"
 #include "zfstream.h"
 
 class FlightDataRecorder {
  public:
   // IMPORTANT: this constant needs to increased with every interface change
-  const uint64_t INTERFACE_VERSION = 25;
+  const uint64_t INTERFACE_VERSION = 26;
 
   void initialize();
 
-  void update(const EngineData& engineData, const AdditionalData& additionalData);
+  void update(const EngineData& engineData, const AdditionalData& additionalData, const fmgc_outputs& fmgc1, const fmgc_outputs& fmgc2);
 
   void terminate();
 
@@ -31,4 +32,6 @@ class FlightDataRecorder {
   std::string getFlightDataRecorderFilename();
 
   void cleanUpFlightDataRecorderFiles();
+
+  void writeFmgc(const fmgc_outputs& fmgc);
 };
