38e54ffefa56b24f1a9803b09c10f544b4cbd02c
Crocket63 <davydecorps@gmail.com>
2024-08-02 19:07:54 +0200

    Code added for door plus xml

--- fbw-a380x/src/wasm/systems/a380_systems/src/hydraulic/mod.rs
+++ fbw-a380x/src/wasm/systems/a380_systems/src/hydraulic/mod.rs
@@ -66,6 +66,7 @@ mod engine_pump_disc;
 use engine_pump_disc::EnginePumpDisconnectionClutch;
 pub mod autobrakes;
 use autobrakes::A380AutobrakeController;
+mod gear_secondary_doors;
 
 #[cfg(test)]
 use systems::hydraulic::PressureSwitchState;
@@ -1588,6 +1589,7 @@ pub(super) struct A380Hydraulic {
     epump_auto_logic: A380ElectricPumpAutoLogic,
 
     tilting_gears: A380TiltingGears,
+    aux_gear_doors: A380AuxiliaryGearDoorSet,
 }
 impl A380Hydraulic {
     const FLAP_FPPU_TO_SURFACE_ANGLE_BREAKPTS: [f64; 12] = [
@@ -1910,6 +1912,8 @@ impl A380Hydraulic {
             ),
 
             tilting_gears: A380TiltingGearsFactory::new_a380_tilt_assembly(context),
+
+            aux_gear_doors: A380AuxiliaryGearDoorSet::new(context),
         }
     }
 
@@ -1935,6 +1939,7 @@ impl A380Hydraulic {
             engines[0],
             engines[1],
             adirs,
+            lgcius,
         );
 
         for cur_time_step in self.core_hydraulic_updater {
@@ -2200,6 +2205,7 @@ impl A380Hydraulic {
         engine1: &impl Engine,
         engine2: &impl Engine,
         adirs: &impl AdirsMeasurementOutputs,
+        lgcius: &LandingGearControlInterfaceUnitSet,
     ) {
         self.gear_system_gravity_extension_controller
             .update(context);
@@ -2290,6 +2296,14 @@ impl A380Hydraulic {
             &self.bypass_pin,
             overhead_panel,
         );
+
+        self.aux_gear_doors.update(
+            context,
+            lgcius.active_lgciu(),
+            self.yellow_circuit
+                .system_section()
+                .is_pressure_switch_pressurised(),
+        )
     }
 
     // For each hydraulic loop retrieves volumes from and to each actuator and pass it to the loops
@@ -2933,6 +2947,8 @@ impl SimulationElement for A380Hydraulic {
 
         self.tilting_gears.accept(visitor);
 
+        self.aux_gear_doors.accept(visitor);
+
         visitor.visit(self);
     }
 }
@@ -3143,7 +3159,7 @@ impl HydraulicCircuitController for A380HydraulicCircuitController {
 
 use std::fmt::Display;
 
-use self::autobrakes::A380AutobrakePanel;
+use self::{autobrakes::A380AutobrakePanel, gear_secondary_doors::A380AuxiliaryGearDoorSet};
 #[derive(Clone, Copy, PartialEq, Debug)]
 enum A380EngineDrivenPumpId {
     Edp1a,
