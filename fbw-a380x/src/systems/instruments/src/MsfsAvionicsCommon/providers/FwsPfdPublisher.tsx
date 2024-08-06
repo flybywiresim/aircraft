889885fbbf3ef15cf28b9fded4df314d1cdd4a64
Florian Gross <63071941+flogross89@users.noreply.github.com>
2024-08-06 02:16:45 +0200

    update sounds, introduce indexed simvars

--- fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/providers/FwsPfdPublisher.tsx
+++ fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/providers/FwsPfdPublisher.tsx
@@ -1,50 +1,30 @@
-import { EventBus, SimVarDefinition, SimVarValueType } from '@microsoft/msfs-sdk';
-import { UpdatableSimVarPublisher } from '../UpdatableSimVarPublisher';
+import { EventBus, IndexedEventType, PublishPacer, SimVarDefinition, SimVarPublisher, SimVarPublisherEntry, SimVarValueType } from '@microsoft/msfs-sdk';
 
-export interface FwsPfdSimvars {
-  memoLine1: number;
-  memoLine2: number;
-  memoLine3: number;
-  limitationsLine1: number;
-  limitationsLine2: number;
-  limitationsLine3: number;
-  limitationsLine4: number;
-  limitationsLine5: number;
-  limitationsLine6: number;
-  limitationsLine7: number;
-  limitationsLine8: number;
+interface FwsPfdBaseSimvars {
+  memo_line: number;
+  limitations_line: number;
 }
 
-export enum FwsPfdVars {
-  memoLine1 = 'L:A32NX_PFD_MEMO_LINE_1',
-  memoLine2 = 'L:A32NX_PFD_MEMO_LINE_2',
-  memoLine3 = 'L:A32NX_PFD_MEMO_LINE_3',
-  limitationsLine1 = 'L:A32NX_PFD_LIMITATIONS_LINE_1',
-  limitationsLine2 = 'L:A32NX_PFD_LIMITATIONS_LINE_2',
-  limitationsLine3 = 'L:A32NX_PFD_LIMITATIONS_LINE_3',
-  limitationsLine4 = 'L:A32NX_PFD_LIMITATIONS_LINE_4',
-  limitationsLine5 = 'L:A32NX_PFD_LIMITATIONS_LINE_5',
-  limitationsLine6 = 'L:A32NX_PFD_LIMITATIONS_LINE_6',
-  limitationsLine7 = 'L:A32NX_PFD_LIMITATIONS_LINE_7',
-  limitationsLine8 = 'L:A32NX_PFD_LIMITATIONS_LINE_8',
+enum FwsPfdVars {
+  memoLineX = 'L:A32NX_PFD_MEMO_LINE_#index#',
+  limitationsLineX = 'L:A32NX_PFD_LIMITATIONS_LINE_#index#', // 1 to 8
 }
 
-export class FwsPfdSimvarPublisher extends UpdatableSimVarPublisher<FwsPfdSimvars> {
-  private static simvars = new Map<keyof FwsPfdSimvars, SimVarDefinition>([
-    ['memoLine1', { name: FwsPfdVars.memoLine1, type: SimVarValueType.Number }],
-    ['memoLine2', { name: FwsPfdVars.memoLine2, type: SimVarValueType.Number }],
-    ['memoLine3', { name: FwsPfdVars.memoLine3, type: SimVarValueType.Number }],
-    ['limitationsLine1', { name: FwsPfdVars.limitationsLine1, type: SimVarValueType.Number }],
-    ['limitationsLine2', { name: FwsPfdVars.limitationsLine2, type: SimVarValueType.Number }],
-    ['limitationsLine3', { name: FwsPfdVars.limitationsLine3, type: SimVarValueType.Number }],
-    ['limitationsLine4', { name: FwsPfdVars.limitationsLine4, type: SimVarValueType.Number }],
-    ['limitationsLine5', { name: FwsPfdVars.limitationsLine5, type: SimVarValueType.Number }],
-    ['limitationsLine6', { name: FwsPfdVars.limitationsLine6, type: SimVarValueType.Number }],
-    ['limitationsLine7', { name: FwsPfdVars.limitationsLine7, type: SimVarValueType.Number }],
-    ['limitationsLine8', { name: FwsPfdVars.limitationsLine8, type: SimVarValueType.Number }],
-  ]);
+type IndexedTopics = 'memo_line' | 'limitations_line';
 
-  public constructor(bus: EventBus) {
-    super(FwsPfdSimvarPublisher.simvars, bus);
+type FwsPfdIndexedEvents = {
+  [P in keyof Pick<FwsPfdBaseSimvars, IndexedTopics> as IndexedEventType<P>]: FwsPfdBaseSimvars[P];
+};
+
+export interface FwsPfdSimvars extends FwsPfdBaseSimvars, FwsPfdIndexedEvents {}
+
+export class FwsPfdSimvarPublisher extends SimVarPublisher<FwsPfdSimvars> {
+  constructor(bus: EventBus, pacer?: PublishPacer<FwsPfdSimvars>) {
+  const simvars: [keyof FwsPfdSimvars, SimVarPublisherEntry<any>][] = [
+    ['memo_line', { name: FwsPfdVars.memoLineX, type: SimVarValueType.Number, indexed: true }],
+    ['limitations_line', { name: FwsPfdVars.limitationsLineX, type: SimVarValueType.Number, indexed: true }],
+  ];
+
+    super(new Map(simvars), bus, pacer);
   }
 }
