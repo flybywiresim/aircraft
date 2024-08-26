1b957ba5b54b7f0933a2ea3d907494c2b124cf7e
Florian Gross <63071941+flogross89@users.noreply.github.com>
2024-08-26 20:18:56 +0200

    code re-use from a32nx SD

--- fbw-a380x/src/systems/instruments/src/SD/Pages/Wheel/elements/Wheel.tsx
+++ fbw-a380x/src/systems/instruments/src/SD/Pages/Wheel/elements/Wheel.tsx
@@ -10,12 +10,15 @@ interface WheelProps {
     moreActive: boolean,
 }
 
+const roundTemperature = (rawTemp: number): number => Math.min(995, Math.max(0, Math.round(rawTemp / 5) * 5));
+
+const maxStaleness = 300;
+
 export const Wheel: FC<WheelProps> = ({ x, y, number, isLeftSide, hasBrake, moreActive }) => {
     const negativeSign = isLeftSide ? '-' : '';
     const rightNegativeSign = !isLeftSide ? '-' : '';
 
-    const [brakeTemp] = useSimVar(`L:A32NX_REPORTED_BRAKE_TEMPERATURE_${number}`, 'celsius', 2000);
-    const brakeTempRoundedTo5 = Math.max(0, Math.ceil((brakeTemp as number) / 5) * 5).toFixed(0);
+    const [brakeTemp] = useSimVar(`L:A32NX_REPORTED_BRAKE_TEMPERATURE_${number}`, 'celsius', maxStaleness);
 
     return (
         <g id={`wheel-${number ?? 'nose'}`} transform={`translate(${x} ${y})`}>
@@ -34,7 +37,7 @@ export const Wheel: FC<WheelProps> = ({ x, y, number, isLeftSide, hasBrake, more
                         d={`m ${negativeSign}15,18 v -36 M ${negativeSign}21,18 v -36 M ${negativeSign}27,18 v -36 M ${negativeSign}33,18 v -36`}
                     />
                     <path className='BackgroundFill' d={`m ${isLeftSide ? -18 : 70},-13 h -50 v -24 h 50 z`} />
-                    <text className='F26 Green EndAlign' x={isLeftSide ? -16 : 72} y={-16}>{brakeTempRoundedTo5}</text>
+                    <text className='F26 Green EndAlign' x={isLeftSide ? -16 : 72} y={-16}>{roundTemperature(brakeTemp)}</text>
                 </>
             )}
             <text className='F22 Green EndAlign' x={isLeftSide ? -16 : 65} y={34} visibility={moreActive ? 'visible' : 'hidden'}>220</text>
