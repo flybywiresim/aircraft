b29db3013157c71951f8e1e66f27d6e0d5bf029b
Michael Corcoran <tracer@outlook.co.nz>
2024-08-28 21:02:16 +1200

    fix(a380x/sd): isa delta

--- fbw-a380x/src/systems/instruments/src/SD/StatusArea.tsx
+++ fbw-a380x/src/systems/instruments/src/SD/StatusArea.tsx
@@ -14,11 +14,13 @@ export const StatusArea = () => {
         return airDataSwitchingKnob === ADIRS_3_TO_CAPTAIN ? 3 : 1;
     };
 
+    const [isaVisible, setIsaVisible] = useState(false);
+
     const airDataReferenceSource = getStatusAirDataReferenceSource();
-    const sat = useArinc429Var(`L:A32NX_ADIRS_ADR_${airDataReferenceSource}_STATIC_AIR_TEMPERATURE`);
-    const tat = useArinc429Var(`L:A32NX_ADIRS_ADR_${airDataReferenceSource}_TOTAL_AIR_TEMPERATURE`);
-    // TODO: Currently this value will always be displayed but it should have more underlying logic tied to it as it relates to SAT
-    const isa = useArinc429Var(`L:A32NX_ADIRS_ADR_${airDataReferenceSource}_INTERNATIONAL_STANDARD_ATMOSPHERE_DELTA`);
+    const sat = useArinc429Var(`L:A32NX_ADIRS_ADR_${airDataReferenceSource}_STATIC_AIR_TEMPERATURE`, 6000);
+    const tat = useArinc429Var(`L:A32NX_ADIRS_ADR_${airDataReferenceSource}_TOTAL_AIR_TEMPERATURE`, 6000);
+    const zp = useArinc429Var(`L:A32NX_ADIRS_ADR_${airDataReferenceSource}_ALTITUDE`, 6000);
+    const isa = sat.valueOr(0) + Math.min(36089, zp.valueOr(0)) / 500 - 15;
 
     const getValuePrefix = (value: number) => (value >= 0 ? '+' : '');
 
@@ -65,6 +67,15 @@ export const StatusArea = () => {
         return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secondsLeft.toString().padStart(2, '0')}`;
     };
 
+    useEffect(() => {
+      const baroMode = SimVar.GetSimVarValue('L:XMLVAR_Baro1_Mode', 'number');
+      const isInStdMode = baroMode !== 0 && baroMode !== 1;
+      const isaShouldBeVisible = isInStdMode && zp.isNormalOperation() && sat.isNormalOperation();
+      setIsaVisible(isaShouldBeVisible);
+    }, [isa, sat, zp]);
+
+    const isaPrefix = isa > 0 ? '+' : '';
+
     return (
         <Layer x={0} y={0}>
             {/* Frame */}
@@ -77,20 +88,28 @@ export const StatusArea = () => {
 
             {/* Temps */}
             <text x={34} y={696} className='F26 White LS1'>TAT</text>
-            <text x={34} y={725} className='F26 White LS1'>SAT</text>
-            <text x={34} y={754} className='F26 White LS1'>ISA</text>
             <text x={158} y={696} className={`F25 ${tat.isNormalOperation() ? 'Green' : 'Amber'} EndAlign`}>
                 {tat.isNormalOperation() ? getValuePrefix(tat.value) + tat.value.toFixed(0) : 'XX'}
             </text>
+            <text x={185} y={696} className='F26 Cyan'>&#176;C</text>
+
+            <text x={34} y={725} className='F26 White LS1'>SAT</text>
             <text x={158} y={725} className={`F25 ${sat.isNormalOperation() ? 'Green' : 'Amber'} EndAlign`}>
                 {sat.isNormalOperation() ? getValuePrefix(sat.value) + sat.value.toFixed(0) : 'XX'}
             </text>
-            <text x={158} y={754} className={`F25 ${isa.isNormalOperation() ? 'Green' : 'Amber'} EndAlign`}>
-                {isa.isNormalOperation() ? getValuePrefix(isa.value) + isa.value.toFixed(0) : 'XX'}
-            </text>
-            <text x={185} y={696} className='F26 Cyan'>&#176;C</text>
             <text x={185} y={725} className='F26 Cyan'>&#176;C</text>
-            <text x={185} y={754} className='F26 Cyan'>&#176;C</text>
+
+            {
+              isaVisible ?
+                <>
+                  <text x={34} y={754} className='F26 White LS1'>ISA</text>
+                  <text x={158} y={754} className={'F25 Green EndAlign'}>
+                      {getValuePrefix(isa) + isa.toFixed(0)}
+                  </text>
+                  <text x={185} y={754} className='F26 Cyan'>&#176;C</text>
+                </>
+                : null
+            }
 
             {/* G Load Indication */}
             {gLoadIsAbnormal && (
