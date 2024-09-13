4eb4c99a3cdf4d6a9355e48fa7116099058116ba
Florian Gross <63071941+flogross89@users.noreply.github.com>
2024-09-13 21:20:50 +0200

    fix ND WASM fix

--- fbw-common/src/wasm/terronnd/src/navigationdisplay/displaybase.cpp
+++ fbw-common/src/wasm/terronnd/src/navigationdisplay/displaybase.cpp
@@ -1,13 +1,21 @@
 #include "display.h"
 
+#ifdef A380X
+#define INSTRUMENT_BG_COLOR nvgRGBA(0, 0, 0, 255)
+#endif
+
+#ifndef A380X
+#define INSTRUMENT_BG_COLOR nvgRGBA(4, 4, 4, 255)
+#endif
+
 using namespace navigationdisplay;
 
 DisplayBase::DisplayBase(DisplaySide side, FsContext context)
     : _side(side), _configuration(), _frameBufferSize(0), _nanovgImage(0), _context(nullptr), _thresholds(nullptr), _frameData(nullptr) {
   NVGparams params;
-  params.userPtr = context;
+  params.userPtr       = context;
   params.edgeAntiAlias = false;
-  this->_context = nvgCreateInternal(&params);
+  this->_context       = nvgCreateInternal(&params);
 }
 
 DisplaySide DisplayBase::side() const {
@@ -36,7 +44,12 @@ void DisplayBase::render(sGaugeDrawData* pDrawData) {
   nvgBeginFrame(this->_context, static_cast<float>(pDrawData->winWidth), static_cast<float>(pDrawData->winHeight), ratio);
   {
     if (this->_configuration.powered) {
-      if ((this->_nanovgImage != 0 && !helper::Math::almostEqual(this->_configuration.potentiometer, 0.0f))) {
+      if ((this->_nanovgImage == 0 || helper::Math::almostEqual(this->_configuration.potentiometer, 0.0f))) {
+        nvgFillColor(this->_context, INSTRUMENT_BG_COLOR);
+        nvgBeginPath(this->_context);
+        nvgRect(this->_context, 0.0f, 0.0f, static_cast<float>(pDrawData->winWidth), static_cast<float>(pDrawData->winHeight));
+        nvgFill(this->_context);
+      } else {
         // draw the image
         nvgBeginPath(this->_context);
         NVGpaint imagePaint =
@@ -46,6 +59,11 @@ void DisplayBase::render(sGaugeDrawData* pDrawData) {
         nvgFillPaint(this->_context, imagePaint);
         nvgFill(this->_context);
       }
+    } else {
+      nvgFillColor(this->_context, nvgRGBA(0, 0, 0, 255));
+      nvgBeginPath(this->_context);
+      nvgRect(this->_context, 0.0f, 0.0f, static_cast<float>(pDrawData->winWidth), static_cast<float>(pDrawData->winHeight));
+      nvgFill(this->_context);
     }
   }
   nvgEndFrame(this->_context);
