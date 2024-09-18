4694f873baff7ab87e34cbb6ac021f1734834f14
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2024-09-18 02:32:29 +0100

    feat(a380/mfd): add automatic perf page switching depending on fms phase (#8907)
    * feat(mfd): add automatic perf switching based on phase
    
    * only change active perf page when the phase is the same as the selected page

--- fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/MfdFmsPerf.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/MfdFmsPerf.tsx
@@ -45,6 +45,8 @@ export class MfdFmsPerf extends FmsPage<MfdFmsPerfProps> {
 
   private managedSpeedActive = Subject.create<boolean>(false);
 
+  private previousFmsFlightPhase: FmgcFlightPhase | null = null;
+
   // Subjects
   private crzFl = Subject.create<number | null>(null);
 
@@ -594,6 +596,24 @@ export class MfdFmsPerf extends FmsPage<MfdFmsPerfProps> {
             this.activateApprButton.instance.style.visibility = 'hidden';
           }
         }
+
+        if (this.previousFmsFlightPhase) {
+          const isSamePhase = this.flightPhasesSelectedPageIndex.get() + 1 === this.previousFmsFlightPhase;
+          if (isSamePhase) {
+            switch (val) {
+              case FmgcFlightPhase.Takeoff:
+              case FmgcFlightPhase.Climb:
+              case FmgcFlightPhase.Cruise:
+              case FmgcFlightPhase.Descent:
+              case FmgcFlightPhase.Approach:
+              case FmgcFlightPhase.GoAround: {
+                this.flightPhasesSelectedPageIndex.set(val - 1);
+                break;
+              }
+            }
+          }
+        }
+        this.previousFmsFlightPhase = val;
       }, true),
     );
 
