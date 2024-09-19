a2cb3dfe0f98ad41b9758671dfd8345fe6d58efc
Michael Corcoran <tracer@outlook.co.nz>
2024-09-19 21:42:30 +1200

    feat: add approach name function for a380x

--- fbw-a380x/src/systems/instruments/src/MFD/shared/utils.ts
+++ fbw-a380x/src/systems/instruments/src/MFD/shared/utils.ts
@@ -1,3 +1,8 @@
+// Copyright (c) 2023-2024 FlyByWire Simulations
+// SPDX-License-Identifier: GPL-3.0
+
+import { Approach, ApproachType } from '@flybywiresim/fbw-sdk';
+
 export function secondsToHHmmString(seconds: number) {
   const minutesTotal = seconds / 60;
   const hours = Math.abs(Math.floor(minutesTotal / 60))
@@ -9,4 +14,35 @@ export function secondsToHHmmString(seconds: number) {
     .toString()
     .padStart(2, '0');
   return `${hours}:${minutes}`;
+}
+
+const approachTypeNames: Record<ApproachType, string> = {
+  [ApproachType.Ils]: 'ILS',
+  [ApproachType.Gls]: 'GLS',
+  [ApproachType.Igs]: 'IGS',
+  [ApproachType.Loc]: 'LOC',
+  [ApproachType.LocBackcourse]: 'BAC',
+  [ApproachType.Lda]: 'LDA',
+  [ApproachType.Sdf]: 'SDF',
+  [ApproachType.Gps]: 'GPS',
+  [ApproachType.Rnav]: 'RNV',
+  [ApproachType.Vor]: 'VOR',
+  [ApproachType.VorDme]: 'VOR',
+  [ApproachType.Vortac]: 'VOR',
+  [ApproachType.Ndb]: 'NDB',
+  [ApproachType.NdbDme]: 'NDB',
+  [ApproachType.Fms]: 'RNAV',
+  [ApproachType.Mls]: 'MLS', // not actually supported
+  [ApproachType.MlsTypeA]: 'MLS', // not actually supported
+  [ApproachType.MlsTypeBC]: 'MLS', // not actually supported
+  [ApproachType.Tacan]: 'TAC', // not actually supported
+  [ApproachType.Unknown]: '',
+};
+
+export function getApproachName(approach: Approach): string {
+  // we don't need to worry about circling approaches as they aren't available, so we can always expect a runway ident
+  // FIXME add (RNP) suffix for RNP-AR missed approaches (even on non-RNAV approaches)
+  const approachSuffix = approach.suffix ? `-${approach.suffix}` : '';
+  const arSuffix = approach.authorisationRequired ? ' (RNP)' : '';
+  return `${approachTypeNames[approach.type]}${approach.runwayIdent?.substring(4)}${approachSuffix}${arSuffix}`;
 }
