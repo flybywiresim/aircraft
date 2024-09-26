ba20ff7d8cf7e93d0d1320430e285bc6c15edd83
2hwk <reasonstouninstall@tutanota.com>
2024-09-26 11:09:01 +0800

    fix: 6-> 8

--- fbw-a380x/src/wasm/systems/a380_systems/src/payload/mod.rs
+++ fbw-a380x/src/wasm/systems/a380_systems/src/payload/mod.rs
@@ -245,7 +245,7 @@ impl A380Payload {
                 [6, 7, 8, 9, 10, 11, 12, 13, 0, 1, 2, 3, 4, 5],
             ),
             BoardingAgent::new(
-                context.get_identifier("INTERACTIVE POINT OPEN:6".to_owned()),
+                context.get_identifier("INTERACTIVE POINT OPEN:8".to_owned()),
                 [9, 8, 7, 6, 13, 12, 11, 10, 5, 4, 3, 2, 1, 0],
             ),
             BoardingAgent::new(
