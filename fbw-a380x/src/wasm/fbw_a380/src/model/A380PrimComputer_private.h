d9e66126c978dad129d6ab6d74f2044b1da50ad8
Andreas Guther <andreas@guther.family>
2024-10-03 23:06:20 +0200

    feat(ap): Tuning of A380X Autoland #8999

--- fbw-a380x/src/wasm/fbw_a380/src/model/A380PrimComputer_private.h
+++ fbw-a380x/src/wasm/fbw_a380/src/model/A380PrimComputer_private.h
@@ -1,5 +1,5 @@
-#ifndef RTW_HEADER_A380PrimComputer_private_h_
-#define RTW_HEADER_A380PrimComputer_private_h_
+#ifndef A380PrimComputer_private_h_
+#define A380PrimComputer_private_h_
 #include "rtwtypes.h"
 #include "A380PrimComputer_types.h"
 #endif
