1b1f7d71b18a6379307e099fee266561a72c5c8a
2hwk <reasonstouninstall@tutanota.com>
2024-10-06 14:35:56 +0800

    fix(payload): correct boarding agent for U1L and M2L, added agents for possible quad stair boarding option

--- fbw-a380x/src/wasm/systems/a380_systems/src/payload/mod.rs
+++ fbw-a380x/src/wasm/systems/a380_systems/src/payload/mod.rs
@@ -238,11 +238,19 @@ impl A380Payload {
             ),
             BoardingAgent::new(
                 context.get_identifier("INTERACTIVE POINT OPEN:2".to_owned()),
-                [10, 11, 12, 13, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                [2, 3, 4, 5, 6, 7, 8, 9, 13, 12, 11, 10, 1, 0],
+            ),
+            BoardingAgent::new(
+                context.get_identifier("INTERACTIVE POINT OPEN:6".to_owned()),
+                [6, 7, 8, 9, 10, 11, 12, 13, 0, 1, 2, 3, 4, 5],
+            ),
+            BoardingAgent::new(
+                context.get_identifier("INTERACTIVE POINT OPEN:6".to_owned()),
+                [9, 8, 7, 6, 13, 12, 11, 10, 5, 4, 3, 2, 1, 0],
             ),
             BoardingAgent::new(
                 context.get_identifier("INTERACTIVE POINT OPEN:10".to_owned()),
-                [2, 3, 4, 5, 6, 7, 8, 9, 13, 12, 11, 10, 1, 0],
+                [10, 11, 12, 13, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             ),
         ];
 
