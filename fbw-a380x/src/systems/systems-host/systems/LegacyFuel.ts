53c3882ffff934066a2a42a1a2170db3c1ffce31
Maximilian Reuter <maximilian.a.reuter@gmail.com>
2024-10-26 11:50:53 +0200

    added more comment

--- fbw-a380x/src/systems/systems-host/systems/LegacyFuel.ts
+++ fbw-a380x/src/systems/systems-host/systems/LegacyFuel.ts
@@ -18,6 +18,8 @@ enum ValveState {
 /**
  * This is needed to initialize the desired fuel L:Var on load to sync with the fuel quantity as per the fuel state management per ATC ID
  * This is a temporary solution until all fuel state related ops are contained in the same module.
+ *
+ * It also now deals with managing the MSFS fuelsystem
  */
 
 /* TODO: remove this file after proper FQMS is implemented in Rust */
