6fb0a623a7a057f7d59fcf084c1b69134bd780b4
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2024-10-29 00:23:20 +0000

    add initial table in mfd-position-monitor

--- fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/POSITION/MfdFmsPositionMonitor.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/POSITION/MfdFmsPositionMonitor.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { ClockEvents, FSComponent, Subject, VNode } from '@microsoft/msfs-sdk';
 import { AbstractMfdPageProps } from 'instruments/src/MFD/MFD';
 import { FmsPage } from '../../common/FmsPage';
@@ -7,6 +8,8 @@ import { Footer } from '../../common/Footer';
 import { InputField } from '../../common/InputField';
 import { RnpFormat } from '../../common/DataEntryFormats';
 import './MfdFmsPositionMonitor.scss';
+import { distanceTo } from 'msfs-geo';
+import { Button } from '../../common/Button';
 
 interface MfdFmsPositionMonitorPageProps extends AbstractMfdPageProps {}
 
@@ -27,6 +30,8 @@ export class MfdFmsPositionMonitor extends FmsPage<MfdFmsPositionMonitorPageProp
 
   private readonly ir1Position = Subject.create('');
 
+  private readonly ir1PositionDeviation = Subject.create('');
+
   private readonly ir2LatitudeRegister = Arinc429Register.empty();
 
   private readonly ir2LongitudeRegister = Arinc429Register.empty();
@@ -34,6 +39,8 @@ export class MfdFmsPositionMonitor extends FmsPage<MfdFmsPositionMonitorPageProp
   private readonly ir2Coordinates: Coordinates = { lat: 0, long: 0 };
 
   private readonly ir2Position = Subject.create('');
+
+  private readonly ir2PositionDeviation = Subject.create('');
 
   private readonly ir3LatitudeRegister = Arinc429Register.empty();
 
@@ -43,7 +50,15 @@ export class MfdFmsPositionMonitor extends FmsPage<MfdFmsPositionMonitorPageProp
 
   private readonly ir3Position = Subject.create('');
 
+  private readonly ir3PositionDeviation = Subject.create('');
+
   private readonly fmPosition = Subject.create('');
+
+  private readonly positionFrozen = Subject.create(false);
+
+  private readonly positionFrozenLabel = this.positionFrozen.map(v => v? 'UNFREEZE POSITION *' : 'FREEZE POSITION *');
+
+  private readonly positionFrozenText = Subject.create('');
 
   public onAfterRender(node: VNode): void {
     super.onAfterRender(node);
@@ -65,47 +80,64 @@ export class MfdFmsPositionMonitor extends FmsPage<MfdFmsPositionMonitorPageProp
 
     const navigation = this.props.fmcService.master.navigation;
     const rnp = navigation.getRnp();
+    this.fmsAccuracy.set(navigation.isAcurracyHigh() ? 'HIGH' : 'LO');
     this.fmsEpu.set(navigation.getEpe() != Infinity ? navigation.getEpe().toFixed(2) : '--.-');
     this.fmsRnp.set(rnp);
     this.rnpEnteredByPilot.set(navigation.isRnpManual());
-    this.fmPosition.set(
-      coordinateToString(this.props.fmcService.master.navigation.getPpos() ?? { lat: 0, long: 0 }, false),
-    );
-    this.fmsAccuracy.set(navigation.isAcurracyHigh() ? 'HIGH' : 'LO');
-    this.ir1LatitudeRegister.setFromSimVar('L:A32NX_ADIRS_IR_1_LATITUDE');
-    this.ir1LongitudeRegister.setFromSimVar('L:A32NX_ADIRS_IR_1_LONGITUDE');
-    this.ir1Position.set(
-      this.computeIrPositionString(this.ir1LatitudeRegister, this.ir1LongitudeRegister, this.ir1Coordinates),
-    );
-    this.ir2LatitudeRegister.setFromSimVar('L:A32NX_ADIRS_IR_2_LATITUDE');
-    this.ir2LongitudeRegister.setFromSimVar('L:A32NX_ADIRS_IR_2_LONGITUDE');
-    this.ir2Position.set(
-      this.computeIrPositionString(this.ir2LatitudeRegister, this.ir2LongitudeRegister, this.ir2Coordinates),
-    );
-    this.ir3LatitudeRegister.setFromSimVar('L:A32NX_ADIRS_IR_3_LATITUDE');
-    this.ir3LongitudeRegister.setFromSimVar('L:A32NX_ADIRS_IR_3_LONGITUDE');
-    this.ir3Position.set(
-      this.computeIrPositionString(this.ir3LatitudeRegister, this.ir3LongitudeRegister, this.ir3Coordinates),
-    );
+
+    if (!this.positionFrozen.get()) {
+      const fmCoordinates = this.props.fmcService.master.navigation.getPpos();
+      const fmPositionAvailable = fmCoordinates != null
+      this.fmPosition.set(
+        fmPositionAvailable? 
+        coordinateToString(this.props.fmcService.master.navigation.getPpos()!, false) : '--°--.--/---°--.-',
+      );
+      this.fillIrData(1, this.ir1LatitudeRegister, this.ir1LongitudeRegister, this.ir1Coordinates, fmCoordinates, this.ir1Position, this.ir1PositionDeviation);
+      this.fillIrData(2, this.ir2LatitudeRegister, this.ir2LongitudeRegister, this.ir2Coordinates, fmCoordinates, this.ir2Position, this.ir2PositionDeviation);
+      this.fillIrData(3, this.ir3LatitudeRegister, this.ir3LongitudeRegister, this.ir3Coordinates, fmCoordinates, this.ir3Position, this.ir3PositionDeviation);
+    }
   }
 
-  private computeIrPositionString(
+  private fillIrData(
+    irIndex : number,
     latitude: Arinc429Register,
     longitude: Arinc429Register,
     coordinates: Coordinates,
-  ): string {
+    fmPosition : Coordinates | null,
+    irPositionSubject : Subject<string>,
+    irFmPositionDeviationSubject : Subject<string>
+  ): void {
+
+    latitude.setFromSimVar(`L:A32NX_ADIRS_IR_${irIndex}_LATITUDE`);
+    longitude.setFromSimVar(`L:A32NX_ADIRS_IR_${irIndex}_LONGITUDE`)
     if (
       !latitude.isNormalOperation() ||
       latitude.isNoComputedData() ||
       !longitude.isNormalOperation() ||
       longitude.isNoComputedData()
     ) {
-      return '--°--.--/---°--.-';
+      irPositionSubject.set('--°--.--/---°--.-');
+      irFmPositionDeviationSubject.set('-.-')
     }
     coordinates.lat = latitude.value;
     coordinates.long = longitude.value;
 
-    return coordinateToString(coordinates, false);
+    if(fmPosition) {
+    irPositionSubject.set(coordinateToString(coordinates, false));
+     irFmPositionDeviationSubject.set(distanceTo(coordinates, fmPosition).toFixed(1))
+    }
+  }
+
+  private setPositionFrozen() {
+    const frozen = this.positionFrozen.get();
+    this.positionFrozenText.set(frozen? "POSITION FROZEN AT " + this.getUtcFormatString() : "");
+    this.positionFrozen.set(!frozen);
+  }
+
+  private getUtcFormatString() : string {
+    const utcTime = SimVar.GetGlobalVarValue('ZULU TIME', 'seconds');
+    const date = new Date(utcTime * 1000);
+      return `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
   }
 
   render(): VNode {
@@ -114,7 +146,8 @@ export class MfdFmsPositionMonitor extends FmsPage<MfdFmsPositionMonitorPageProp
         {super.render()}
         {/* begin page content */}
         <div class="mfd-page-container">
-          <div class="pos-monitor-header">
+          <div class="mfd-pos-monitor-header"></div>
+          <div class="mfd-pos-top-row">
             <div class="mfd-label-value-container">
               <span class="mfd-label mfd-spacing-right">ACCURACY</span>
               <span class="mfd-value">{this.fmsAccuracy}</span>
@@ -127,7 +160,7 @@ export class MfdFmsPositionMonitor extends FmsPage<MfdFmsPositionMonitorPageProp
               <span class="mfd-label-unit mfd-unit-trailing">NM</span>
             </div>
           </div>
-          <div class="pos-monitor-header">
+          <div class="mfd-pos-top-row">
             <div class="mfd-label-value-container">
               <span class="mfd-value mfd-spacing-right">GPS PRIMARY</span>
             </div>
@@ -146,6 +179,38 @@ export class MfdFmsPositionMonitor extends FmsPage<MfdFmsPositionMonitorPageProp
                 interactionMode={this.props.mfd.interactionMode}
               />
             </div>
+          </div>
+          <div class="fc mfd-pos-monitor-table">
+          <div class="mfd-pos-table-row">
+          <div class="mfd-label-value-container">
+              <span class="mfd-label mfd-spacing-right">FMS1</span>
+              <span class="mfd-value">{this.fmPosition}</span>
+            </div>
+          </div>
+          <div class="mfd-pos-table-row">
+          <div class="mfd-label-value-container">
+              <span class="mfd-label mfd-spacing-right">RADIO</span>
+              <span class="mfd-value">{this.fmPosition}</span>
+            </div>
+          </div>
+          <div class="mfd-pos-table-row">
+          <div class="mfd-label-value-container">
+              <span class="mfd-label mfd-spacing-right">MIXIRS</span>
+              <span class="mfd-value">{this.fmPosition}</span>
+            </div>
+            <div span>{this.positionFrozenText}</div>
+            </div>
+            <div class="mfd-pos-table-row">
+            <div class="mfd-label-value-container">
+              <span class="mfd-label mfd-spacing-right">GPIRS</span>
+              <span class="mfd-value">{this.fmPosition}</span>
+            </div>
+            <Button
+                label="FREEZE <br /> POSITION *"
+                onClick={() => this.setPositionFrozen()}
+                buttonStyle="width: 150px;"
+              />
+          </div>
           </div>
           <Footer bus={this.props.bus} mfd={this.props.mfd} fmcService={this.props.fmcService} />;
         </div>
