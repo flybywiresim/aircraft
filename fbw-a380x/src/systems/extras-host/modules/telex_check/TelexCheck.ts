6b2e16d985e87d0f77416ef54b77b5e0fc9f5931
Saschl <sascharudolf46@gmail.com>
2024-10-31 16:53:42 +0100

    fix: add missing telex check for a380

---
+++ fbw-a380x/src/systems/extras-host/modules/telex_check/TelexCheck.ts
@@ -0,0 +1,30 @@
+// If the consent is not set, show telex page
+
+// Copyright (c) 2022 FlyByWire Simulations
+// SPDX-License-Identifier: GPL-3.0
+
+import { NXDataStore, PopUpDialog } from '@flybywiresim/fbw-sdk';
+
+export class TelexCheck {
+  public xmlConfig: Document;
+
+  constructor() {
+    console.log('TelexCheck: Created');
+  }
+
+  // TODO: Replace with new function for commbus implementation to sync VFS markup config to WASM
+  public startPublish(): void {
+    const onlineFeaturesStatus = NXDataStore.get('CONFIG_ONLINE_FEATURES_STATUS', 'UNKNOWN');
+    const popup = new PopUpDialog();
+
+    if (onlineFeaturesStatus === 'UNKNOWN') {
+      popup.showPopUp(
+        'TELEX CONFIGURATION',
+        'You have not yet configured the telex option. Telex enables free text and live map. If enabled, aircraft position data is published for the duration of the flight. Messages are public and not moderated. USE AT YOUR OWN RISK. To learn more about telex and the features it enables, please go to https://docs.flybywiresim.com/telex. Would you like to enable telex?',
+        'small',
+        () => NXDataStore.set('CONFIG_ONLINE_FEATURES_STATUS', 'ENABLED'),
+        () => NXDataStore.set('CONFIG_ONLINE_FEATURES_STATUS', 'DISABLED'),
+      );
+    }
+  }
+}
