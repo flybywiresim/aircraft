8e99336e5f6e6fbb83a2e66f4c3335fea805b935
sschiphorst <67174146+sschiphorst@users.noreply.github.com>
2024-11-01 17:48:54 +1100

    Update FbwAircraftSentryClient.ts
    Change error reporting popup to be plane agnostic

--- fbw-common/src/systems/shared/src/FbwAircraftSentryClient.ts
+++ fbw-common/src/systems/shared/src/FbwAircraftSentryClient.ts
@@ -161,8 +161,8 @@ export class FbwAircraftSentryClient {
     const popup = new PopUpDialog();
 
     return new Promise<boolean>((resolve) => {
-      popup.showPopUp(
-        'A32NX - ERROR REPORTING',
+      popup.showPopUp( /
+        'AIRCRAFT - ERROR REPORTING',
         'Are you willing to help FlyByWire Simulations by enabling anonymous reporting of errors that may occur in the future? ' +
           'This is 100% optional and we will never collect your personal data, but it will help us diagnose issues quickly.',
         'normal',
