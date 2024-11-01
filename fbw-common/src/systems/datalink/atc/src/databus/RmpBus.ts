30d061c64569dca769c2ae28e23cfdb95eb2a08e
Sven Czarnian <devel@svcz.de>
2024-11-01 21:58:04 +0100

    introduce the message bus to the RMP

---
+++ fbw-common/src/systems/datalink/atc/src/databus/RmpBus.ts
@@ -0,0 +1,7 @@
+//  Copyright (c) 2024 FlyByWire Simulations
+//  SPDX-License-Identifier: GPL-3.0
+
+export interface AtcRmpBus {
+  // sends the "contact" or "monitor" frequency in Hz
+  atcLoadFrequency: number;
+}
