8f531559597dee1e9fc5abcbfc2d99c16838d534
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2024-11-01 19:56:06 +0000

    add onside & offside Labels. Store waypoint in mfd

--- fbw-a380x/src/systems/instruments/src/MFD/MFD.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/MFD.tsx
@@ -16,7 +16,7 @@ import {
   Subscription,
   VNode,
 } from '@microsoft/msfs-sdk';
-import { DatabaseItem, Waypoint } from '@flybywiresim/fbw-sdk';
+import { DatabaseItem, Fix, Waypoint } from '@flybywiresim/fbw-sdk';
 
 import { MouseCursor } from 'instruments/src/MsfsAvionicsCommon/UiWidgets/MouseCursor';
 
@@ -61,6 +61,10 @@ export interface MfdDisplayInterface {
   interactionMode: Subscribable<InteractionMode>;
 
   openMessageList(): void;
+
+  get positionMonitorFix(): Fix | null;
+
+  set positionMonitorFix(fix: Fix | null);
 }
 
 export class MfdComponent
@@ -125,6 +129,16 @@ export class MfdComponent
   private readonly duplicateNamesOpened = Subject.create<boolean>(false);
 
   private readonly duplicateNamesRef = FSComponent.createRef<MfdFmsFplnDuplicateNames>();
+
+  private posMonitorFix: Fix | null = null;
+
+  get positionMonitorFix(): Fix | null {
+    return this.posMonitorFix;
+  }
+
+  set positionMonitorFix(fix: Fix | null) {
+    this.posMonitorFix = fix;
+  }
 
   // Necessary to enable mouse interaction
   get isInteractive(): boolean {
