c0714116c24ef8b51dea21290c4bc03d2d8d1280
Sven Czarnian <devel@svcz.de>
2024-11-02 11:59:41 +0100

    import the SDK

--- fbw-common/src/systems/datalink/atc/src/databus/RmpBus.ts
+++ fbw-common/src/systems/datalink/atc/src/databus/RmpBus.ts
@@ -1,7 +1,7 @@
 //  Copyright (c) 2024 FlyByWire Simulations
 //  SPDX-License-Identifier: GPL-3.0
 
-import { Arinc429Register } from 'shared/src';
+import { Arinc429Register } from '@flybywiresim/fbw-sdk';
 
 export interface AtcRmpBus {
   // sends the "contact" or "monitor" frequency in Hz
