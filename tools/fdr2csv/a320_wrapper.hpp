8237fbb0a04635c24c3a57039249b6ba2254366c
Lukas Michels <lukasmichels00@gmail.com>
2024-11-02 14:36:07 +0100

    feat(fdr2csv): refactor to unify fdr2csv, fix conversion bug

---
+++ tools/fdr2csv/a320_wrapper.hpp
@@ -0,0 +1,7 @@
+#include "../../fbw-a32nx/src/wasm/fbw_a320/src/model/AutopilotLaws_types.h"
+#include "../../fbw-a32nx/src/wasm/fbw_a320/src/model/AutopilotStateMachine_types.h"
+#include "../../fbw-a32nx/src/wasm/fbw_a320/src/model/Autothrust_types.h"
+#include "../../fbw-a32nx/src/wasm/fbw_a320/src/model/ElacComputer_types.h"
+#include "../../fbw-a32nx/src/wasm/fbw_a320/src/model/FacComputer_types.h"
+#include "../../fbw-a32nx/src/wasm/fbw_a320/src/model/SecComputer_types.h"
+#include "../../fbw-a32nx/src/wasm/fbw_a320/src/recording/RecordingDataTypes.h"
