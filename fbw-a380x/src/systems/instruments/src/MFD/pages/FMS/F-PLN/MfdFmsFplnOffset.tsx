7ce2917c7b5e95a247928ed81f52a4f16531a2bf
Valerie <slightlyclueless47@gmail.com>
2024-11-03 14:02:30 +0000

    Yeah don't try and set the two dropdowns to the same waypoint index

--- fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/F-PLN/MfdFmsFplnOffset.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/F-PLN/MfdFmsFplnOffset.tsx
@@ -17,17 +17,15 @@ export class MfdFmsFplnOffset extends FmsPage<MfdFmsFplnOffsetProps> {
 
   private availableWaypointsToLegIndex: number[] = [];
 
-  private selectedWaypointIndex = Subject.create<number | null>(null);
+  private selectedStartWaypointIndex = Subject.create<number | null>(null);
+
+  private selectedEndWaypointIndex = Subject.create<number | null>(null);
 
   private manualWptIdent: string | null = '';
 
   private utcEta = Subject.create<string>('--:--');
 
   private distToWpt = Subject.create<string>('---');
-
-  private offsetStartpWpt = Subject.create<string>('');
-
-  private offsetEndpWpt = Subject.create<string>('');
 
   protected onNewData(): void {
     // Use active FPLN for building the list (page only works for active anyways)
@@ -56,13 +54,13 @@ export class MfdFmsFplnOffset extends FmsPage<MfdFmsFplnOffsetProps> {
       if (revWpt) {
         const selectedLegIndex = this.availableWaypoints.getArray().findIndex((it) => it === revWpt.ident);
         if (selectedLegIndex !== -1) {
-          this.selectedWaypointIndex.set(selectedLegIndex);
+          this.selectedStartWaypointIndex.set(selectedLegIndex);
         }
       }
 
       // Manual waypoint was entered. In this case, force dropdown field to display wpt ident without selecting it
       if (this.manualWptIdent) {
-        this.selectedWaypointIndex.set(null);
+        this.selectedStartWaypointIndex.set(null);
         this.dropdownMenuRef.instance.forceLabel(this.manualWptIdent);
       }
 
@@ -80,27 +78,43 @@ export class MfdFmsFplnOffset extends FmsPage<MfdFmsFplnOffsetProps> {
         {super.render()}
         {/* begin page content */}
         <div class="fr">
-          <div class="mfd-fms-fpln-labeled-box-container" style="flex-grow: 1;">
-            <span class="mfd-label">START WPT</span>
-            <div class="mfd-fms-offset-start-dropdown-div">
+          <div class="mfd-fms-fpln-offset-grid">
+            <div>
+              <span class="mfd-label">START WPT</span>
+            </div>
+            <div>
+              <span class="mfd-lebal">END WPT</span>
+            </div>
+            <div style="grid-row-start: span 2; border-left: 1px solid lightgrey; margin-right: 10px;" />
+            <div>
+              <span class="mfd-lable">INTERCEPT ANGLE</span>
+            </div>
+            <div style="margin-top: 15px;">
               <DropdownMenu
                 ref={this.dropdownMenuRef}
                 idPrefix={`${this.props.mfd.uiService.captOrFo}_MFD_offsetStartDropdown`}
-                selectedIndex={this.selectedWaypointIndex}
+                selectedIndex={this.selectedStartWaypointIndex}
                 values={this.availableWaypoints}
                 freeTextAllowed
+                containerStyle="width: 175px;"
+                alignLabels="flex-start"
+                numberOfDigitsForInputField={7}
+                tmpyActive={this.tmpyActive}
                 hEventConsumer={this.props.mfd.hEventConsumer}
                 interactionMode={this.props.mfd.interactionMode}
               />
             </div>
-            <span class="mfd-label">END WPT</span>
-            <div class="mfd-fms-offset-end-dropdown=div">
+            <div style="margin-top: 15px;">
               <DropdownMenu
                 ref={this.dropdownMenuRef}
                 idPrefix={`${this.props.mfd.uiService.captOrFo}_MFD_offsetEndDropdown`}
-                selectedIndex={this.selectedWaypointIndex}
+                selectedIndex={this.selectedEndWaypointIndex}
                 values={this.availableWaypoints}
                 freeTextAllowed
+                containerStyle="width: 175px;"
+                alignLabels="flex-start"
+                numberOfDigitsForInputField={7}
+                tmpyActive={this.tmpyActive}
                 hEventConsumer={this.props.mfd.hEventConsumer}
                 interactionMode={this.props.mfd.interactionMode}
               />
