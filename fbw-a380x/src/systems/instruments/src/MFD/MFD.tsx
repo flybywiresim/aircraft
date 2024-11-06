dea63155485b9694110bde2b1b0528cb52b8396e
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2024-11-06 00:04:53 +0000

    address api & mfd comments by tracer

--- fbw-a380x/src/systems/instruments/src/MFD/MFD.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/MFD.tsx
@@ -81,6 +81,8 @@ export class MfdComponent
     return this.#uiService;
   }
 
+  public positionMonitorFix: Fix | null = null;
+
   public readonly hEventConsumer = this.props.bus.getSubscriber<InternalKccuKeyEvent>().on('kccuKeyEvent');
 
   public readonly interactionMode = Subject.create<InteractionMode>(InteractionMode.Touchscreen);
@@ -129,16 +131,6 @@ export class MfdComponent
   private readonly duplicateNamesOpened = Subject.create<boolean>(false);
 
   private readonly duplicateNamesRef = FSComponent.createRef<MfdFmsFplnDuplicateNames>();
-
-  private posMonitorFix: Fix | null = null;
-
-  get positionMonitorFix(): Fix | null {
-    return this.posMonitorFix;
-  }
-
-  set positionMonitorFix(fix: Fix | null) {
-    this.posMonitorFix = fix;
-  }
 
   // Necessary to enable mouse interaction
   get isInteractive(): boolean {
