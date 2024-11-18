e0722e92631ecf1ca171f0e8828dfbcd135b11f3
Lukas Michels <lukasmichels00@gmail.com>
2024-11-18 20:00:42 +0100

    feat: add clock and flashing utils

---
+++ fbw-a32nx/src/systems/instruments/src/MsfsAvionicsCommon/FlashingElementUtils.tsx
@@ -0,0 +1,75 @@
+// Copyright (c) 2021-2023 FlyByWire Simulations
+//
+// SPDX-License-Identifier: GPL-3.0
+
+import {
+  FSComponent,
+  DisplayComponent,
+  VNode,
+  Subscribable,
+  MappedSubject,
+  ComponentProps,
+  EventBus,
+  ConsumerSubject,
+  Subject,
+  SetSubject,
+} from '@microsoft/msfs-sdk';
+import { ExtendedClockEvents } from './providers/ExtendedClockProvider';
+import { NXLogicTriggeredMonostableNode } from '@flybywiresim/fbw-sdk';
+
+export interface FlashProps extends ComponentProps {
+  bus: EventBus;
+  visibleClassName?: string;
+  hiddenClassName: string;
+  visible: Subscribable<boolean>;
+  flashing?: Subscribable<boolean>;
+  flashDuration: number;
+}
+
+export class FlashOneHertz extends DisplayComponent<FlashProps> {
+  private oneHertzClock = ConsumerSubject.create(null, false);
+
+  private flashingMtrig = new NXLogicTriggeredMonostableNode(this.props.flashDuration, true);
+
+  private flashingMtrigResult = Subject.create(false);
+
+  private visible = MappedSubject.create(
+    ([visible, oneHertzClock, flashingMtrig]) => visible && !(flashingMtrig && oneHertzClock),
+    this.props.visible,
+    this.oneHertzClock,
+    this.flashingMtrigResult,
+  );
+
+  private classList = SetSubject.create<string>();
+
+  onAfterRender(node: VNode): void {
+    super.onAfterRender(node);
+
+    const sub = this.props.bus.getSubscriber<ExtendedClockEvents>();
+
+    this.oneHertzClock.setConsumer(sub.on('oneHertzClock'));
+
+    sub.on('deltaTime').handle((dt) => {
+      const shouldFlash = this.props.flashing?.get() ?? true;
+      this.flashingMtrigResult.set(this.flashingMtrig.write(this.props.visible.get() && shouldFlash, dt));
+    });
+
+    this.visible.sub((vis) => {
+      if (vis) {
+        if (this.props.visibleClassName) {
+          this.classList.add(this.props.visibleClassName);
+        }
+        this.classList.delete(this.props.hiddenClassName);
+      } else {
+        this.classList.add(this.props.hiddenClassName);
+        if (this.props.visibleClassName) {
+          this.classList.delete(this.props.visibleClassName ?? '');
+        }
+      }
+    }, true);
+  }
+
+  render(): VNode {
+    return <g class={this.classList}>{this.props.children}</g>;
+  }
+}
---
+++ fbw-a32nx/src/systems/instruments/src/MsfsAvionicsCommon/providers/ExtendedClockProvider.ts
@@ -0,0 +1,51 @@
+// Copyright (c) 2021-2023 FlyByWire Simulations
+//
+// SPDX-License-Identifier: GPL-3.0
+
+import { ClockEvents, EventBus, Instrument } from '@microsoft/msfs-sdk';
+
+export interface ExtendedClockEvents {
+  monotonicTime: number;
+  deltaTime: number;
+  oneHertzClock: boolean;
+}
+
+export class ExtendedClockEventProvider implements Instrument {
+  private prevSimTime = 0;
+
+  private deltaTime = 0;
+
+  private monotonicTime = 0;
+
+  private oneHertzClock = 0;
+
+  constructor(private readonly bus: EventBus) {}
+
+  /** @inheritdoc */
+  public init(): void {
+    const publisher = this.bus.getPublisher<ExtendedClockEvents>();
+    const subscriber = this.bus.getSubscriber<ClockEvents>();
+
+    subscriber.on('simTime').handle((time) => {
+      this.deltaTime = time - this.prevSimTime;
+
+      this.monotonicTime += this.deltaTime;
+
+      this.oneHertzClock += this.deltaTime;
+      if (this.oneHertzClock > 1000) {
+        this.oneHertzClock = 0;
+      }
+
+      publisher.pub('deltaTime', this.deltaTime);
+      publisher.pub('monotonicTime', this.monotonicTime);
+      publisher.pub('oneHertzClock', this.oneHertzClock > 500);
+
+      this.prevSimTime = time;
+    });
+  }
+
+  /** @inheritdoc */
+  public onUpdate(): void {
+    // noop
+  }
+}
