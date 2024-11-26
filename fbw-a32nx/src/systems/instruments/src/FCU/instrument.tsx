6babf509feedc8b80e00e183e5b2a94675729b1c
Michael Corcoran <tracer@outlook.co.nz>
2024-11-26 22:50:27 +1300

    feat(fcu): backlight powered separately

--- fbw-a32nx/src/systems/instruments/src/FCU/instrument.tsx
+++ fbw-a32nx/src/systems/instruments/src/FCU/instrument.tsx
@@ -6,16 +6,21 @@ import { FCUSimvarPublisher } from './shared/FcuSimvarPublisher';
 import { FCUComponent } from './FCU';
 import { calculateSunAzimuthElevation, lerp, calculateAmbientBrightness } from './SunAngle';
 import { MsfsAircraftSystemEvents, MsfsAircraftSystemPublisher } from '@flybywiresim/fbw-sdk';
+import {
+  A32NXElectricalSystemEvents,
+  A32NXElectricalSystemPublisher,
+} from '../../../shared/src/publishers/A32NXElectricalSystemPublisher';
 
 // eslint-disable-next-line camelcase
 class A32NX_FCU extends BaseInstrument {
   private readonly bus = new EventBus();
   private readonly backplane = new InstrumentBackplane();
 
-  private sub = this.bus.getSubscriber<MsfsAircraftSystemEvents>();
+  private sub = this.bus.getSubscriber<A32NXElectricalSystemEvents & MsfsAircraftSystemEvents>();
 
   private readonly simVarPublisher = new FCUSimvarPublisher(this.bus);
   private readonly msfsAircraftSystemPublisher = new MsfsAircraftSystemPublisher(this.bus);
+  private readonly a32nxElecSystemPublisher = new A32NXElectricalSystemPublisher(this.bus);
 
   private simTime = new Date();
 
@@ -23,20 +28,25 @@ class A32NX_FCU extends BaseInstrument {
 
   private readonly ambientBrightness = Subject.create(0, (a, b) => Math.abs(a - b) < 0.01);
 
-  private readonly screenBrightness = ConsumerSubject.create(this.sub.on('msfs_light_potentiometer_87'), 0);
+  private readonly backlightBrightness = ConsumerSubject.create(this.sub.on('msfs_light_potentiometer_87'), 0);
+
+  private readonly isBacklightPowered = ConsumerSubject.create(this.sub.on('a32nx_elec_ac_1_bus_is_powered'), false);
 
   constructor() {
     super();
 
     this.backplane.addPublisher('SimVarPublisher', this.simVarPublisher);
     this.backplane.addPublisher('MsfsAircraftSystem', this.msfsAircraftSystemPublisher);
+    this.backplane.addPublisher('A32NXElectricalSystemPublisher', this.a32nxElecSystemPublisher);
 
     this.ambientBrightness.sub((ambientBrightness) => {
       SimVar.SetSimVarValue('L:A32NX_AMBIENT_BRIGHTNESS', 'number', ambientBrightness);
       this.updateDisplayBrightness();
     });
 
-    this.screenBrightness.sub(this.updateDisplayBrightness.bind(this), true);
+    const updateDisplayBrightness = this.updateDisplayBrightness.bind(this);
+    this.isBacklightPowered.sub(updateDisplayBrightness);
+    this.backlightBrightness.sub(updateDisplayBrightness, true);
   }
 
   get templateID(): string {
@@ -45,18 +55,19 @@ class A32NX_FCU extends BaseInstrument {
 
   private updateDisplayBrightness() {
     const ambientBrightness = this.ambientBrightness.get();
-    const screenBrightess = this.screenBrightness.get();
+    const backlightBrightness = this.isBacklightPowered.get() ? this.backlightBrightness.get() : 0;
 
-    const saturation = screenBrightess > 0 ? lerp(ambientBrightness * (1.05 - screenBrightess), 1, 0.6, 0, 100) : 0;
-    const luminosity = lerp(ambientBrightness * (1.05 - screenBrightess), 1, 0.6, 80, 55);
+    const saturation =
+      backlightBrightness > 0 ? lerp(ambientBrightness * (1.05 - backlightBrightness), 1, 0.6, 0, 100) : 0;
+    const luminosity = lerp(ambientBrightness * (1.05 - backlightBrightness), 1, 0.6, 80, 55);
     const colour = `hsl(31, ${saturation.toFixed(1)}%, ${luminosity.toFixed(1)}%)`;
     document.documentElement.style.setProperty('--main-display-colour', colour);
 
-    const textShadowOpacity = lerp(screenBrightess, 0, 1, 0, 0.3) * lerp(ambientBrightness, 0, 0.9, 1, 0);
+    const textShadowOpacity = lerp(backlightBrightness, 0, 1, 0, 0.3) * lerp(ambientBrightness, 0, 0.9, 1, 0);
     const textShadow = `rgba(207, 110, 0, ${textShadowOpacity.toFixed(2)})`;
     document.documentElement.style.setProperty('--main-text-shadow-colour', textShadow);
 
-    const backgroundOpacity = lerp(screenBrightess, 0, 1, 0, 0.3) * lerp(ambientBrightness, 0, 0.8, 1, 0.1);
+    const backgroundOpacity = lerp(backlightBrightness, 0, 1, 0, 0.3) * lerp(ambientBrightness, 0, 0.8, 1, 0.1);
     document.documentElement.style.setProperty('--main-background-opacity', backgroundOpacity.toString());
   }
 
