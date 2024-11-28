dfcc757d75218f506979f5c9ec9bbb0a9fdf9e02
Michael Corcoran <tracer@outlook.co.nz>
2024-11-28 22:00:51 +1300

    doc: address review comment

--- fbw-common/src/typings/fs-base-ui/html_ui/JS/common.d.ts
+++ fbw-common/src/typings/fs-base-ui/html_ui/JS/common.d.ts
@@ -868,7 +868,9 @@ declare global {
     let g_AtlasMgr: AltasElementsMgr;
 
     namespace InputBar {
+        /** MSFS2020 = MENU_VALID, MSFS2024 = KEY_MENU_SR_VALID */
         const MENU_BUTTON_A: 'MENU_VALID' | 'KEY_MENU_SR_VALID';
+        /** MSFS2020 = MENU_BACK, MSFS2024 = KEY_MENU_SR_BACK */
         const MENU_BUTTON_B: 'MENU_BACK' | 'KEY_MENU_SR_BACK';
         const MENU_BUTTON_X: 'MENU_VALI2';
         const MENU_BUTTON_Y: 'MENU_CANCEL';
