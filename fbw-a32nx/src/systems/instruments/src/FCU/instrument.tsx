d3d60f9889b7dbf0836fce2b872c70f34badb7d7
Michael Corcoran <tracer@outlook.co.nz>
2024-11-28 13:12:24 +1300

    fix(a32nx/fcu): the lcd display is now visible with the backlight off (#9549)
    * feat(shared): start msfs aircraft system publisher
    
    * feat(a32nx/fcu): lcd is visible with backlight off
    
    * docs: changelog
    
    * feat: shared a32nx elec publisher
    
    * feat(fcu): backlight powered separately
    
    * fix: fcu healthy rather than dc
    
    ---------
    
    Co-authored-by: lukecologne <lukasmichels00@gmail.com>

--- fbw-a32nx/src/systems/instruments/src/FCU/instrument.tsx
+++ fbw-a32nx/src/systems/instruments/src/FCU/instrument.tsx
@@ -1,47 +1,52 @@
-import { EventBus, FSComponent, Subject } from '@microsoft/msfs-sdk';
+// Copyright (c) 2024 FlyByWire Simulations
+// SPDX-License-Identifier: GPL-3.0
+
+import { ConsumerSubject, EventBus, FSComponent, InstrumentBackplane, Subject } from '@microsoft/msfs-sdk';
 import { FCUSimvarPublisher } from './shared/FcuSimvarPublisher';
 import { FCUComponent } from './FCU';
 import { calculateSunAzimuthElevation, lerp, calculateAmbientBrightness } from './SunAngle';
+import { MsfsAircraftSystemEvents, MsfsAircraftSystemPublisher } from '@flybywiresim/fbw-sdk';
+import {
+  A32NXElectricalSystemEvents,
+  A32NXElectricalSystemPublisher,
+} from '../../../shared/src/publishers/A32NXElectricalSystemPublisher';
 
 // eslint-disable-next-line camelcase
 class A32NX_FCU extends BaseInstrument {
-  private bus: EventBus;
+  private readonly bus = new EventBus();
+  private readonly backplane = new InstrumentBackplane();
 
-  private simVarPublisher: FCUSimvarPublisher;
+  private sub = this.bus.getSubscriber<A32NXElectricalSystemEvents & MsfsAircraftSystemEvents>();
 
-  /**
-   * "mainmenu" = 0
-   * "loading" = 1
-   * "briefing" = 2
-   * "ingame" = 3
-   */
-  private gameState = 0;
+  private readonly simVarPublisher = new FCUSimvarPublisher(this.bus);
+  private readonly msfsAircraftSystemPublisher = new MsfsAircraftSystemPublisher(this.bus);
+  private readonly a32nxElecSystemPublisher = new A32NXElectricalSystemPublisher(this.bus);
 
   private simTime = new Date();
 
   private solarParams = calculateSunAzimuthElevation(this.simTime, 0, 0);
 
-  private ambientBrightness = Subject.create(0, (a, b) => Math.abs(a - b) < 0.01);
+  private readonly ambientBrightness = Subject.create(0, (a, b) => Math.abs(a - b) < 0.01);
 
-  private screenBrightess = Subject.create(0, (a, b) => Math.abs(a - b) < 0.01);
+  private readonly backlightBrightness = ConsumerSubject.create(this.sub.on('msfs_light_potentiometer_87'), 0);
+
+  private readonly isBacklightPowered = ConsumerSubject.create(this.sub.on('a32nx_elec_ac_1_bus_is_powered'), false);
 
   constructor() {
     super();
-    this.bus = new EventBus();
-    this.simVarPublisher = new FCUSimvarPublisher(this.bus);
+
+    this.backplane.addPublisher('SimVarPublisher', this.simVarPublisher);
+    this.backplane.addPublisher('MsfsAircraftSystem', this.msfsAircraftSystemPublisher);
+    this.backplane.addPublisher('A32NXElectricalSystemPublisher', this.a32nxElecSystemPublisher);
 
     this.ambientBrightness.sub((ambientBrightness) => {
       SimVar.SetSimVarValue('L:A32NX_AMBIENT_BRIGHTNESS', 'number', ambientBrightness);
       this.updateDisplayBrightness();
     });
 
-    this.screenBrightess.sub(this.updateDisplayBrightness.bind(this), true);
-
-    // need to run this at high speed to avoid jumps when the knob is rotated
-    setInterval(() => {
-      const screenBrightess = SimVar.GetSimVarValue('A:LIGHT POTENTIOMETER:87', 'percent over 100');
-      this.screenBrightess.set(screenBrightess);
-    });
+    const updateDisplayBrightness = this.updateDisplayBrightness.bind(this);
+    this.isBacklightPowered.sub(updateDisplayBrightness);
+    this.backlightBrightness.sub(updateDisplayBrightness, true);
   }
 
   get templateID(): string {
@@ -50,18 +55,19 @@ class A32NX_FCU extends BaseInstrument {
 
   private updateDisplayBrightness() {
     const ambientBrightness = this.ambientBrightness.get();
-    const screenBrightess = this.screenBrightess.get();
+    const backlightBrightness = this.isBacklightPowered.get() ? this.backlightBrightness.get() : 0;
 
-    const saturation = lerp(ambientBrightness * (1.05 - screenBrightess), 1, 0.6, 10, 100);
-    const luminosity = lerp(ambientBrightness * (1.05 - screenBrightess), 1, 0.6, 80, 55);
+    const saturation =
+      backlightBrightness > 0 ? lerp(ambientBrightness * (1.05 - backlightBrightness), 1, 0.6, 0, 100) : 0;
+    const luminosity = lerp(ambientBrightness * (1.05 - backlightBrightness), 1, 0.6, 80, 55);
     const colour = `hsl(31, ${saturation.toFixed(1)}%, ${luminosity.toFixed(1)}%)`;
     document.documentElement.style.setProperty('--main-display-colour', colour);
 
-    const textShadowOpacity = lerp(screenBrightess, 0, 1, 0, 0.3) * lerp(ambientBrightness, 0, 0.9, 1, 0);
+    const textShadowOpacity = lerp(backlightBrightness, 0, 1, 0, 0.3) * lerp(ambientBrightness, 0, 0.9, 1, 0);
     const textShadow = `rgba(207, 110, 0, ${textShadowOpacity.toFixed(2)})`;
     document.documentElement.style.setProperty('--main-text-shadow-colour', textShadow);
 
-    const backgroundOpacity = lerp(screenBrightess, 0, 1, 0, 0.3) * lerp(ambientBrightness, 0, 0.8, 1, 0.1);
+    const backgroundOpacity = lerp(backlightBrightness, 0, 1, 0, 0.3) * lerp(ambientBrightness, 0, 0.8, 1, 0.1);
     document.documentElement.style.setProperty('--main-background-opacity', backgroundOpacity.toString());
   }
 
@@ -72,20 +78,14 @@ class A32NX_FCU extends BaseInstrument {
 
     // Remove "instrument didn't load" text
     document.getElementById('FCU_CONTENT').querySelector(':scope > h1').remove();
+
+    this.backplane.init();
   }
 
   public Update(): void {
     super.Update();
 
-    if (this.gameState !== 3) {
-      const gamestate = this.getGameState();
-      if (gamestate === 3) {
-        this.simVarPublisher.startPublish();
-      }
-      this.gameState = gamestate;
-    } else {
-      this.simVarPublisher.onUpdate();
-    }
+    this.backplane.onUpdate();
 
     const lat = SimVar.GetSimVarValue('PLANE LATITUDE', 'degrees');
     const lon = SimVar.GetSimVarValue('PLANE LONGITUDE', 'degrees');
---
+++ fbw-a32nx/src/systems/shared/src/publishers/A32NXElectricalSystemPublisher.ts
@@ -0,0 +1,111 @@
+import {
+  EventBus,
+  IndexedEventType,
+  PublishPacer,
+  SimVarPublisher,
+  SimVarPublisherEntry,
+  SimVarValueType,
+} from '@microsoft/msfs-sdk';
+
+interface A32NXElectricalSystemBaseEvents {
+  /** Whether AC bus 1 is currently connected to a supply. */
+  a32nx_elec_ac_1_bus_is_powered: boolean;
+  /** Whether AC bus 2 is currently connected to a supply. */
+  a32nx_elec_ac_2_bus_is_powered: boolean;
+  /** Whether AC ESS bus is currently connected to a supply. */
+  a32nx_elec_ac_ess_bus_is_powered: boolean;
+  /** Whether sheddable AC ESS bus is currently connected to a supply. */
+  a32nx_elec_ac_ess_shed_bus_is_powered: boolean;
+  /** Whether AC static inverter bus is currently connected to a supply. */
+  a32nx_elec_ac_stat_inv_bus_is_powered: boolean;
+  /** Whether AC ground service bus is currently connected to a supply. */
+  a32nx_elec_ac_gnd_flt_svc_is_powered: boolean;
+  /** Whether DC bus 1 is currently connected to a supply. */
+  a32nx_elec_dc_1_bus_is_powered: boolean;
+  /** Whether DC bus 2 is currently connected to a supply. */
+  a32nx_elec_dc_2_bus_is_powered: boolean;
+  /** Whether DC ESS bus is currently connected to a supply. */
+  a32nx_elec_dc_ess_bus_is_powered: boolean;
+  /** Whether sheddable DC ESS bus is currently connected to a supply. */
+  a32nx_elec_dc_ess_shed_bus_is_powered: boolean;
+  /** Whether DC common battery bus is currently connected to a supply. */
+  a32nx_elec_dc_batt_bus_is_powered: boolean;
+  /** Whether DC battery 1 hot bus is currently connected to a supply. */
+  a32nx_elec_dc_hot_1_bus_is_powered: boolean;
+  /** Whether DC battery 2 hot bus is currently connected to a supply. */
+  a32nx_elec_dc_hot_2_bus_is_powered: boolean;
+  /** Whether DC ground service bus bus is currently connected to a supply. */
+  a32nx_elec_dc_gnd_flt_svc_bus_is_powered: boolean;
+}
+
+type IndexedTopics = never;
+
+type A32NXElectricalSystemIndexedEvents = {
+  [P in keyof Pick<
+    A32NXElectricalSystemBaseEvents,
+    IndexedTopics
+  > as IndexedEventType<P>]: A32NXElectricalSystemBaseEvents[P];
+};
+
+/**
+ * Events for A32NX electrical system local vars.
+ * Event names are the same as the local var names (including a32nx_elec_ prefix).
+ */
+export interface A32NXElectricalSystemEvents
+  extends A32NXElectricalSystemBaseEvents,
+    A32NXElectricalSystemIndexedEvents {}
+
+/**
+ * Publisher for A32NX electrical system local vars.
+ */
+export class A32NXElectricalSystemPublisher extends SimVarPublisher<A32NXElectricalSystemEvents> {
+  /**
+   * Create a publisher.
+   * @param bus The EventBus to publish to
+   * @param pacer An optional pacer to use to control the rate of publishing
+   */
+  public constructor(bus: EventBus, pacer?: PublishPacer<A32NXElectricalSystemEvents>) {
+    const simvars = new Map<keyof A32NXElectricalSystemEvents, SimVarPublisherEntry<any>>([
+      ['a32nx_elec_ac_1_bus_is_powered', { name: 'L:A32NX_ELEC_AC_1_BUS_IS_POWERED', type: SimVarValueType.Bool }],
+      ['a32nx_elec_ac_2_bus_is_powered', { name: 'L:A32NX_ELEC_AC_2_BUS_IS_POWERED', type: SimVarValueType.Bool }],
+      ['a32nx_elec_ac_ess_bus_is_powered', { name: 'L:A32NX_ELEC_AC_ESS_BUS_IS_POWERED', type: SimVarValueType.Bool }],
+      [
+        'a32nx_elec_ac_ess_shed_bus_is_powered',
+        { name: 'L:A32NX_ELEC_AC_ESS_SHED_BUS_IS_POWERED', type: SimVarValueType.Bool },
+      ],
+      [
+        'a32nx_elec_ac_stat_inv_bus_is_powered',
+        { name: 'L:A32NX_ELEC_AC_STAT_INV_BUS_IS_POWERED', type: SimVarValueType.Bool },
+      ],
+      [
+        'a32nx_elec_ac_gnd_flt_svc_is_powered',
+        { name: 'L:A32NX_ELEC_AC_GND_FLT_SVC_BUS_IS_POWERED', type: SimVarValueType.Bool },
+      ],
+      ['a32nx_elec_dc_1_bus_is_powered', { name: 'L:A32NX_ELEC_DC_1_BUS_IS_POWERED', type: SimVarValueType.Bool }],
+      ['a32nx_elec_dc_2_bus_is_powered', { name: 'L:A32NX_ELEC_DC_2_BUS_IS_POWERED', type: SimVarValueType.Bool }],
+      ['a32nx_elec_dc_ess_bus_is_powered', { name: 'L:A32NX_ELEC_DC_ESS_BUS_IS_POWERED', type: SimVarValueType.Bool }],
+      [
+        'a32nx_elec_dc_ess_shed_bus_is_powered',
+        { name: 'L:A32NX_ELEC_DC_ESS_SHED_BUS_IS_POWERED', type: SimVarValueType.Bool },
+      ],
+      [
+        'a32nx_elec_dc_batt_bus_is_powered',
+        { name: 'L:A32NX_ELEC_DC_BATT_BUS_IS_POWERED', type: SimVarValueType.Bool },
+      ],
+      [
+        'a32nx_elec_dc_hot_1_bus_is_powered',
+        { name: 'L:A32NX_ELEC_DC_HOT_1_BUS_IS_POWERED', type: SimVarValueType.Bool },
+      ],
+      [
+        'a32nx_elec_dc_hot_2_bus_is_powered',
+        { name: 'L:A32NX_ELEC_DC_HOT_2_BUS_IS_POWERED', type: SimVarValueType.Bool },
+      ],
+      [
+        'a32nx_elec_dc_gnd_flt_svc_bus_is_powered',
+        { name: 'L:A32NX_ELEC_DC_GND_FLT_SVC_BUS_IS_POWERED', type: SimVarValueType.Bool },
+      ],
+    ]);
+
+    super(simvars, bus, pacer);
+  }
+}
