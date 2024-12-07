7d577f8298dbc86a3410f27676b6b7a22f6533e8
Fragtality <78614902+Fragtality@users.noreply.github.com>
2024-12-07 13:13:54 +0100

    Update fbw-common/src/systems/shared/src/publishers/Extras/GsxSimVarPublisher.ts
    Co-authored-by: Michael Corcoran <tracer@outlook.co.nz>

--- fbw-common/src/systems/shared/src/publishers/Extras/GsxSimVarPublisher.ts
+++ fbw-common/src/systems/shared/src/publishers/Extras/GsxSimVarPublisher.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2021-2024 FlyByWire Simulations
+// Copyright (c) 2024 FlyByWire Simulations
 // SPDX-License-Identifier: GPL-3.0
 
 import { EventBus, PublishPacer, SimVarDefinition, SimVarPublisher, SimVarValueType } from '@microsoft/msfs-sdk';
