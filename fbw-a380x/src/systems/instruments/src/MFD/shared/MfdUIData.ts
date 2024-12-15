a229326fc93d39df77d84cb2543afee059c4639b
Saschl <sascharudolf46@gmail.com>
2024-12-16 00:43:46 +0100

    fix: enable transition from done phase to preflight phase

---
+++ fbw-a380x/src/systems/instruments/src/MFD/shared/MfdUIData.ts
@@ -0,0 +1,8 @@
+import { ActiveUriInformation } from 'instruments/src/MFD/pages/common/MfdUiService';
+
+/**
+ * Mfd Events
+ */
+export interface MfdUIData {
+  mfd_active_uri: ActiveUriInformation;
+}
