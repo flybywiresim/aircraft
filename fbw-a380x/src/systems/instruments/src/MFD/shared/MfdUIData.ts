966948aff46eb6e82823ce92d6b171fc09b4d73a
Saschl <sascharudolf46@gmail.com>
2024-12-16 00:54:48 +0100

    cleanup

--- fbw-a380x/src/systems/instruments/src/MFD/shared/MfdUIData.ts
+++ fbw-a380x/src/systems/instruments/src/MFD/shared/MfdUIData.ts
@@ -1,8 +1,5 @@
 import { ActiveUriInformation } from 'instruments/src/MFD/pages/common/MfdUiService';
 
-/**
- * Mfd Events
- */
 export interface MfdUIData {
   mfd_active_uri: ActiveUriInformation;
 }
