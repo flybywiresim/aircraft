f7103df3423af91134c65a230264d3ea530bdcc6
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2024-12-19 00:03:41 +0000

    fix: also use loc arming

--- fbw-a380x/src/systems/instruments/src/FCU/Managers/LsManager.ts
+++ fbw-a380x/src/systems/instruments/src/FCU/Managers/LsManager.ts
@@ -1,15 +1,25 @@
 // Copyright (c) 2024 FlyByWire Simulations
 // SPDX-License-Identifier: GPL-3.0
-import { EventBus, Instrument } from '@microsoft/msfs-sdk';
+import { ConsumerSubject, EventBus, Instrument, MappedSubject, SubscribableMapFunctions } from '@microsoft/msfs-sdk';
 import { FcuEvents } from '../Publishers/FcuPublisher';
 
 export class LsManager implements Instrument {
   private readonly sub = this.bus.getSubscriber<FcuEvents>();
 
+  private readonly locActive = ConsumerSubject.create(this.sub.on('fcu_loc_mode_active'), false);
+
+  private readonly apprActive = ConsumerSubject.create(this.sub.on('fcu_approach_mode_active'), false);
+
+  private readonly locOrApprSelected = MappedSubject.create(
+    SubscribableMapFunctions.or(),
+    this.locActive,
+    this.apprActive,
+  );
+
   constructor(private readonly bus: EventBus) {}
 
   init(): void {
-    this.sub.on('fcu_appr_mode_active').handle((v) => {
+    this.locOrApprSelected.sub((v) => {
       if (v) {
         SimVar.SetSimVarValue('L:A380X_EFIS_L_LS_BUTTON_IS_ON', 'Bool', true);
         SimVar.SetSimVarValue('L:A380X_EFIS_R_LS_BUTTON_IS_ON', 'Bool', true);
