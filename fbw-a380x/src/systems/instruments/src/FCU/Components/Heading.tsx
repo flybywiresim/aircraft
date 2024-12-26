067800e3344003bf8b5e03b95aa9ad75aa498d4d
Heclak <46121009+heclak@users.noreply.github.com>
2024-12-26 09:38:16 +0800

    feat(a380x/fcu): add TRUE indication on FCU when TRUE/MAG pb is pushed
    * add logic for TRUE indicator to display when true north ref is selected on AFS CP
    * partial port of heading display to MSFS Avionics Framework
    
    Fixes #9519

--- fbw-a380x/src/systems/instruments/src/FCU/Components/Heading.tsx
+++ fbw-a380x/src/systems/instruments/src/FCU/Components/Heading.tsx
@@ -1,16 +1,58 @@
 //  Copyright (c) 2023-2024 FlyByWire Simulations
 //  SPDX-License-Identifier: GPL-3.0
 
-import { DisplayComponent, FSComponent, VNode } from '@microsoft/msfs-sdk';
+import {
+  ConsumerSubject,
+  DisplayComponent,
+  EventBus,
+  FSComponent,
+  MappedSubject,
+  SubscribableMapFunctions,
+  VNode,
+} from '@microsoft/msfs-sdk';
+import { FcuEvents } from 'instruments/src/FCU/Publishers/FcuPublisher';
+import { OverheadEvents } from 'instruments/src/MsfsAvionicsCommon/providers/OverheadPublisher';
 
-export interface HeadingProps {}
+export interface HeadingProps {
+  readonly bus: EventBus;
+}
 
 export class Heading extends DisplayComponent<HeadingProps> {
+  private readonly sub = this.props.bus.getSubscriber<FcuEvents & OverheadEvents>();
+
+  private readonly isLightTestActive = ConsumerSubject.create(this.sub.on('ovhd_ann_lt_test_active'), false);
+  private readonly trueRefActive = ConsumerSubject.create(this.sub.on('fcu_push_true_ref'), false);
+
+  private readonly trueIndicatorActive = MappedSubject.create(
+    ([isLightTest, trueRef]) => {
+      if (isLightTest) {
+        return true;
+      } else {
+        return trueRef;
+      }
+    },
+    this.isLightTestActive,
+    this.trueRefActive,
+  );
+
+  onAfterRender(node: VNode): void {
+    super.onAfterRender(node);
+  }
+
   render(): VNode | null {
     return (
       <div id="Heading">
         <svg width="100%" height="100%">
-          <text id="TRUE" class="Common Active " x="23%" y="20%">
+          <text
+            id="TRUE"
+            class={{
+              Common: true,
+              Active: this.trueIndicatorActive,
+              Inactive: this.trueIndicatorActive.map(SubscribableMapFunctions.not()),
+            }}
+            x="23%"
+            y="20%"
+          >
             TRUE
           </text>
           <text id="HDG" class="Common Active" x="48%" y="20%">
