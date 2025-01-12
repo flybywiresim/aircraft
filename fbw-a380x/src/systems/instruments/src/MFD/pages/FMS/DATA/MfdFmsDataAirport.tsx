3092c890be30c56a59c44a78f43fec15e554f63e
Bulent <eroglu.bulent98@gmail.com>
2025-01-12 15:24:10 +0000

    style: correct style

--- fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/DATA/MfdFmsDataAirport.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/DATA/MfdFmsDataAirport.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) 2024 FlyByWire Simulations
 // SPDX-License-Identifier: GPL-3.0
-
 import { ClockEvents, FSComponent, Subject, VNode } from '@microsoft/msfs-sdk';
 import { AbstractMfdPageProps } from 'instruments/src/MFD/MFD';
 import { Footer } from 'instruments/src/MFD/pages/common/Footer';
@@ -9,101 +8,176 @@ import { FmsPage } from 'instruments/src/MFD/pages/common/FmsPage';
 import { MfdSimvars } from 'instruments/src/MFD/shared/MFDSimvarPublisher';
 import { TopTabNavigator, TopTabNavigatorPage } from 'instruments/src/MFD/pages/common/TopTabNavigator';
 import { Button } from 'instruments/src/MFD/pages/common/Button';
-import { coordinateToString } from '@flybywiresim/fbw-sdk';
+import { coordinateToString, MathUtils } from '@flybywiresim/fbw-sdk';
 
-import './MfdFmsDataAirport.scss';
 import { InputField } from 'instruments/src/MFD/pages/common/InputField';
 import { AirportFormat } from 'instruments/src/MFD/pages/common/DataEntryFormats';
 import { loadAirport, loadAllRunways } from '@fmgc/flightplanning/DataLoading';
+import { IconButton } from 'instruments/src/MFD/pages/common/IconButton';
+
+import './MfdFmsDataAirport.scss';
 
 interface MfdFmsDataAirportProps extends AbstractMfdPageProps {}
 
 interface SimplifiedRunway {
   label: string;
   length: string;
+  lsIdent: string;
+  elevation: string;
+  course: string;
+  coords: string;
+  runwayIdent: string;
 }
 
 export class MfdFmsDataAirport extends FmsPage<MfdFmsDataAirportProps> {
-  private selectedPageIndex = Subject.create<number>(0);
+  private readonly selectedPageIndex = Subject.create<number>(0);
+  private readonly runwayButtonListRef = FSComponent.createRef<HTMLDivElement>();
 
-  private runwayButtonRef = FSComponent.createRef<HTMLDivElement>();
+  private readonly airportName = Subject.create('');
+  private readonly airportCoords = Subject.create('');
+  private readonly airportRunways = Subject.create<SimplifiedRunway[]>([]);
 
-  private navDatabase = Subject.create('');
+  private readonly airportIcao = Subject.create<string | null>(null);
 
-  private activeDatabase = Subject.create('');
+  private readonly runwayDataRef = FSComponent.createRef<HTMLDivElement>();
 
-  private secondDatabase = Subject.create('');
+  private readonly runwayIdent = Subject.create<string | null>(null);
+  private readonly runwayCoords = Subject.create<string | null>(null);
+  private readonly runwayElevation = Subject.create<string | null>(null);
+  private readonly runwayLength = Subject.create<string | null>(null);
+  private readonly runwayCourse = Subject.create<string | null>(null);
+  private readonly runwayLsIdent = Subject.create<string | null>(null);
 
-  private storedWaypoints = Subject.create('00');
+  private readonly selectedRunwayIndex = Subject.create<number | null>(null);
 
-  private storedRoutes = Subject.create('00');
+  private readonly disabledScrollLeft = Subject.create(true);
+  private readonly disabledScrollRight = Subject.create(false);
 
-  private storedNavaids = Subject.create('00');
+  private readonly currentIndex = Subject.create(0);
 
-  private storedRunways = Subject.create('00');
+  private currentIndexRef = FSComponent.createRef<HTMLSpanElement>();
+  private totalRunwaysRef = FSComponent.createRef<HTMLSpanElement>();
 
-  private airportName = Subject.create('');
-
-  private airportCoords = Subject.create('');
-
-  private airportRunways = Subject.create<SimplifiedRunway[]>([]);
-
-  private deleteStoredElementsDisabled = Subject.create(true);
-
-  private readonly isSwapConfirmVisible = Subject.create(false);
-
-  private airportIcao = Subject.create<string | null>(null);
+  private containerRef = FSComponent.createRef<HTMLDivElement>();
 
   private renderRunwayButtons(): void {
-    if (this.runwayButtonRef.getOrDefault()) {
-      const runwayButtons = this.airportRunways.get().forEach((element) => {
-        return <Button label={element.label} onClick={() => console.log('POS MONITOR')} />;
-      });
-      FSComponent.render(<>{runwayButtons}</>, this.runwayButtonRef.instance);
-    }
+    this.runwayButtonListRef.instance.innerHTML = '';
+    this.airportRunways.get().forEach((runway, index) => {
+      if (runway.label.includes('ILS')) {
+        runway.label = runway.label.replace('ILS', 'LS');
+      }
+
+      FSComponent.render(
+        <Button
+          label={runway.label}
+          onClick={() => this.selectedRunwayIndex.set(index)}
+          buttonStyle="width: 220px; margin-bottom: 5px; height: 40px;"
+        />,
+        this.runwayButtonListRef.instance,
+      );
+    });
   }
 
-  private async loadAirportRunways() {
-    const airportIcao = this.airportIcao.get();
+  private scrollRunwayButtons(direction: 'left' | 'right'): void {
+    const currentIndex = this.selectedRunwayIndex.get() ?? 0;
 
-    if (airportIcao) {
-      const airport = await loadAirport(airportIcao);
+    this.selectedRunwayIndex.set(currentIndex + (direction === 'left' ? -1 : 1));
+  }
+
+  private renderRunwayData(runwayData: SimplifiedRunway): void {
+    this.runwayIdent.set(runwayData.runwayIdent);
+    this.runwayCoords.set(runwayData.coords);
+    this.runwayElevation.set(runwayData.elevation);
+    this.runwayLength.set(runwayData.length);
+    this.runwayCourse.set(runwayData.course);
+    this.runwayLsIdent.set(runwayData.lsIdent);
+
+    this.runwayDataRef.instance.style.display = 'block';
+  }
+
+  private async loadAirportRunways(icao: string | null) {
+    if (icao) {
+      const airport = await loadAirport(icao);
 
       if (airport) {
         const runways = await loadAllRunways(airport);
 
-        const simplifiedRunways = runways.map((runway) => ({
-          label: `${runway.ident.substring(4).padEnd(3, ' ')} ${runway.length.toFixed(0).padStart(5, ' ')}M ${runway.lsIdent ? 'ILS' : ''}`,
-          length: runway.length.toFixed(0),
-        }));
+        const airportCoords = coordinateToString(airport.location.lat, airport.location.long, false);
+
+        const simplifiedRunways = runways.map((runway) => {
+          const length = MathUtils.round(runway.length, 10).toFixed(0).padStart(5, '\xa0');
+          // TODO: add alt to location coming from Airport types
+          // @ts-ignore
+          const elevation = MathUtils.round(runway.location.alt, 10).toFixed(0).padStart(5, '\xa0');
+
+          return {
+            label: `${runway.ident.substring(4).padEnd(3, ' ')} ${length}M ${runway.lsIdent ? 'ILS' : ''}`,
+            length,
+            lsIdent: runway.lsIdent ?? '\xa0',
+            elevation,
+            coords: coordinateToString(runway.location.lat, runway.location.long, false),
+            runwayIdent: runway.ident.substring(4).padEnd(3, ' '),
+            course: runway.bearing.toFixed(0).padStart(3, '0'),
+          };
+        });
 
         this.airportRunways.set(simplifiedRunways);
 
         if (airport.name && airport.location) {
           this.airportName.set(airport.name.toUpperCase());
-          this.airportCoords.set(coordinateToString(airport.location.lat, airport.location.long, false));
+          this.airportCoords.set(airportCoords);
         }
+
+        this.renderRunwayButtons();
       }
     }
   }
 
-  protected onNewData() {
-    this.loadAirportRunways();
+  private checkScrollButtons() {
+    const currentIndex = this.selectedRunwayIndex.get() ?? 0;
+    const totalRunways = this.airportRunways.get().length;
 
-    const storedElements = this.props.fmcService.master?.getDataManager()?.numberOfStoredElements();
-    if (storedElements) {
-      this.storedWaypoints.set(storedElements.waypoints.toFixed(0).padStart(2, '0'));
-      this.storedRoutes.set(storedElements.routes.toFixed(0).padStart(2, '0'));
-      this.storedNavaids.set(storedElements.navaids.toFixed(0).padStart(2, '0'));
-      this.storedRunways.set(storedElements.runways.toFixed(0).padStart(2, '0'));
-      this.deleteStoredElementsDisabled.set(storedElements.total === 0);
-    }
+    this.currentIndexRef.instance.innerHTML = (currentIndex + 1).toFixed(0);
+    this.totalRunwaysRef.instance.innerHTML = totalRunways.toFixed(0);
+
+    this.disabledScrollLeft.set(currentIndex === 0);
+    this.disabledScrollRight.set(currentIndex === totalRunways - 1);
   }
+
+  protected onNewData() {}
 
   public onAfterRender(node: VNode): void {
     super.onAfterRender(node);
 
-    this.renderRunwayButtons();
+    this.airportIcao.sub((icao: string | null) => {
+      if (icao && icao.length === 4) {
+        this.loadAirportRunways(icao);
+
+        // hide runway data and show runway buttons
+        this.runwayDataRef.instance.style.display = 'none';
+        this.runwayButtonListRef.instance.style.display = 'block';
+        this.containerRef.instance.style.border = '1px solid #444';
+      } else {
+        this.containerRef.instance.style.border = 'none';
+        this.runwayDataRef.instance.style.display = 'none';
+        this.runwayButtonListRef.instance.style.display = 'none';
+      }
+    });
+
+    this.selectedRunwayIndex.sub((index: number | null) => {
+      if (index === null) {
+        this.runwayDataRef.instance.style.display = 'none';
+        this.runwayButtonListRef.instance.style.display = 'block';
+      } else {
+        this.checkScrollButtons();
+        this.currentIndex.set(index + 1);
+
+        const runwayData = this.airportRunways.get()[index];
+
+        this.renderRunwayData(runwayData);
+        this.runwayButtonListRef.instance.style.display = 'none';
+      }
+    });
 
     this.subs.push(
       this.props.mfd.uiService.activeUri.sub((val) => {
@@ -134,7 +208,6 @@ export class MfdFmsDataAirport extends FmsPage<MfdFmsDataAirportProps> {
   }
 
   render(): VNode {
-    this.airportRunways.get();
     return (
       <>
         {super.render()}
@@ -145,14 +218,15 @@ export class MfdFmsDataAirport extends FmsPage<MfdFmsDataAirportProps> {
             selectedPageIndex={this.selectedPageIndex}
             pageChangeCallback={(val) => {
               this.selectedPageIndex.set(val);
-              this.isSwapConfirmVisible.set(false);
             }}
             selectedTabTextColor="white"
             tabBarSlantedEdgeAngle={25}
           >
             <TopTabNavigatorPage>
-              <div class="mfd-data-airport-input-container">
-                <div class="mfd-label">ARPT IDENT</div>
+              <div class="mfd-data-airport-input">
+                <div class="mfd-label" style={'position: relative; right: 90px;'}>
+                  ARPT IDENT
+                </div>
                 <div>
                   <InputField<string>
                     dataEntryFormat={new AirportFormat()}
@@ -166,17 +240,101 @@ export class MfdFmsDataAirport extends FmsPage<MfdFmsDataAirportProps> {
                     errorHandler={(e) => this.props.fmcService.master?.showFmsErrorMessage(e)}
                     hEventConsumer={this.props.mfd.hEventConsumer}
                     interactionMode={this.props.mfd.interactionMode}
+                    containerStyle="position: relative; right: 60px;"
                   />
                 </div>
               </div>
               {/* DATABASE ARPTs */}
-              <div class="mfd-data-airport-information-container">
-                <div class="mfd-value bigger">{this.airportName}</div>
-                <div class="mfd-value bigger mfd-data-airport-coords-label">{this.airportCoords}</div>
+              <div class="mfd-data-airport-info">
+                <div class="mfd-value bigger" style={'margin-bottom: 20px;'}>
+                  {this.airportName || '\u00A0'}
+                </div>
+                <div class="mfd-value bigger mfd-data-airport-coords">{this.airportCoords || '\u00A0'}</div>
               </div>
-              <div ref={this.runwayButtonRef} class="mfd-fms-fpln-awy-awy-container" />
+              <div class="mfd-data-airport-container" ref={this.containerRef}>
+                <div ref={this.runwayButtonListRef} class="mfd-data-airport-list" />
+                <div ref={this.runwayDataRef} style="display: none;">
+                  <div class="mfd-data-airport-runway">
+                    <div class="mfd-label mfd-data-airport-runway-field" data-label="RWY" style="min-width: 120px;">
+                      <div>
+                        <span class="mfd-value bigger">{this.runwayIdent}</span>
+                      </div>
+                    </div>
+
+                    <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; margin-top: -25px; margin-bottom: 20px;">
+                      <div>
+                        <span class="mfd-label" ref={this.currentIndexRef}></span>
+                        <span class="mfd-label">/</span>
+                        <span class="mfd-label" ref={this.totalRunwaysRef}></span>
+                      </div>
+                      <div class="mfd-data-airport-runway-scroll-buttons">
+                        <IconButton
+                          icon="double-left"
+                          onClick={() => this.scrollRunwayButtons('left')}
+                          disabled={this.disabledScrollLeft}
+                          containerStyle="width: 50px; height: 50px; margin-right: 5px;"
+                        />
+                        <IconButton
+                          icon="double-right"
+                          onClick={() => this.scrollRunwayButtons('right')}
+                          containerStyle="width: 50px; height: 50px;"
+                          disabled={this.disabledScrollRight}
+                        />
+                      </div>
+                    </div>
+
+                    <div
+                      class="mfd-label mfd-data-airport-runway-field"
+                      data-label="LAT/LONG"
+                      style="min-width: 120px;"
+                    >
+                      <div>
+                        <span class="mfd-value bigger">{this.runwayCoords}</span>
+                      </div>
+                    </div>
+                    <div
+                      class="mfd-label mfd-data-airport-runway-field"
+                      data-label="ELEVATION"
+                      style="min-width: 120px;"
+                    >
+                      <div>
+                        <span class="mfd-value bigger">{this.runwayElevation}</span>
+                        <span class="mfd-label-unit mfd-unit-trailing">{'FT'}</span>
+                      </div>
+                    </div>
+                    <div class="mfd-label mfd-data-airport-runway-field" data-label="LENGTH" style="min-width: 120px;">
+                      <div>
+                        <span class="mfd-value bigger"> {this.runwayLength}</span>
+                        <span class="mfd-label-unit mfd-unit-trailing">{'M'}</span>
+                      </div>
+                      <div class="mfd-label mfd-data-airport-runway-field-course">
+                        <span class="mfd-value bigger">{this.runwayCourse}</span>
+                        <span class="mfd-label-unit mfd-unit-trailing">°</span>
+                      </div>
+                    </div>
+                    <div
+                      class="mfd-label mfd-data-airport-runway-field"
+                      data-label="LS IDENT"
+                      style="min-width: 120px;"
+                    >
+                      <div>
+                        <span class="mfd-value bigger">{this.runwayLsIdent}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <Button
+                    label="RWY LIST"
+                    onClick={() => this.selectedRunwayIndex.set(null)}
+                    buttonStyle="width: 220px; margin-bottom: 10px; margin-left: 10px; height: 40px;"
+                  />
+                </div>
+              </div>
             </TopTabNavigatorPage>
-            <TopTabNavigatorPage>{/* FMS P/N */}</TopTabNavigatorPage>
+            <TopTabNavigatorPage>
+              <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+                <span class="mfd-label">NOT IMPLEMENTED</span>
+              </div>
+            </TopTabNavigatorPage>
           </TopTabNavigator>
           <div style="flex-grow: 1;" />
           {/* fill space vertically */}
--- fbw-a380x/src/systems/instruments/src/MFD/pages/common/IconButton.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/common/IconButton.tsx
@@ -11,7 +11,16 @@ import './style.scss';
 
 interface IconButtonProps extends ComponentProps {
   containerStyle?: string;
-  icon: 'double-up' | 'double-down' | 'ecl-single-up' | 'ecl-single-down' | 'ecl-check' | '' | null;
+  icon:
+    | 'double-up'
+    | 'double-down'
+    | 'double-left'
+    | 'double-right'
+    | 'ecl-single-up'
+    | 'ecl-single-down'
+    | 'ecl-check'
+    | ''
+    | null;
   disabled?: Subscribable<boolean>;
   onClick?: () => void;
 }
@@ -86,6 +95,22 @@ export class IconButton extends DisplayComponent<IconButtonProps> {
         {this.props.icon === 'double-down' && (
           <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg">
             <g ref={this.svgGroupRef} fill={this.fillColor} transform="rotate(180 17.5 17.5)">
+              <polygon points="0,17.5 17.5,0 35,17.5" />
+              <polygon points="0,35 17.5,17.5 35,35" />
+            </g>
+          </svg>
+        )}
+        {this.props.icon === 'double-left' && (
+          <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg">
+            <g ref={this.svgGroupRef} fill={this.fillColor} transform="rotate(270 17.5 17.5)">
+              <polygon points="0,17.5 17.5,0 35,17.5" />
+              <polygon points="0,35 17.5,17.5 35,35" />
+            </g>
+          </svg>
+        )}
+        {this.props.icon === 'double-right' && (
+          <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg">
+            <g ref={this.svgGroupRef} fill={this.fillColor} transform="rotate(90 17.5 17.5)">
               <polygon points="0,17.5 17.5,0 35,17.5" />
               <polygon points="0,35 17.5,17.5 35,35" />
             </g>
