aa56e42d8176720a313fb2129413ffafe2b04b15
juliansebline <juliansebline@hotmail.com>
2025-01-14 14:21:28 +0100

    Radio panels components files

--- fbw-a32nx/src/systems/instruments/src/RMP/Components/BaseRadioPanels.tsx
+++ fbw-a32nx/src/systems/instruments/src/RMP/Components/BaseRadioPanels.tsx
@@ -6,6 +6,7 @@ import React, { useState } from 'react';
 import { useSimVar, useInteractionSimVar, useInteractionEvent } from '@flybywiresim/fbw-sdk';
 import { TransceiverType } from './StandbyFrequency';
 import { VhfRadioPanel } from './VhfRadioPanel';
+import { HfRadioPanel } from './HfRadioPanel';
 import { NavRadioPanel } from './NavRadioPanel';
 import { RadioPanelDisplay } from './RadioPanelDisplay';
 
@@ -14,6 +15,8 @@ interface Props {
    * The RMP side (e.g. 'L' or 'R').
    */
   side: string;
+
+  receiver: number;
 }
 
 /**
@@ -33,7 +36,7 @@ export const RootRadioPanel = (props: Props) => {
   const powered = powerAvailable && panelSwitch;
 
   if (!powered) return <UnpoweredRadioPanel />;
-  return <PoweredRadioPanel side={props.side} />;
+  return <PoweredRadioPanel side={props.side} receiver={props.receiver} />;
 };
 
 /**
@@ -53,38 +56,56 @@ const UnpoweredRadioPanel = () => (
  * Renders appropriate mode sub-component (e.g. VhfRadioPanel).
  */
 const PoweredRadioPanel = (props: Props) => {
-  const [navTransceiverType, setNavTransceiverType] = useState(TransceiverType.RADIO_VHF);
+  const [navReceiverType, setNavReceiverType] = useState(TransceiverType.ILS);
 
   // Used to turn on the associated led
   const [panelMode, setPanelMode] = useSimVar(`L:A32NX_RMP_${props.side}_SELECTED_MODE`, 'Number', 250);
   // Used to determine (in the FGMC for instance) if the system is in NAV backup mode. L and R simvars have to be checked
-  const [navButtonPressed, setNavButton] = useSimVar(`L:A32NX_RMP_${props.side}_NAV_BUTTON_SELECTED`, 'boolean', 250);
+  const [navButtonPressed, setNavButton] = useSimVar(`L:A32NX_RMP_${props.side}_NAV_BACKUP_MODE`, 'boolean', 250);
   // Used to return to the selected VHF once NAV is pushed again
   const [previousPanelMode, setPreviousPanelMode] = useState(panelMode);
+  const [indexTransceiver, setIndexTransceiver] = useState(props.side === 'L' ? 1 : 2);
+  // FIXME: A32NX_RMP_LS_COURSE and A32NX_RMP_VOR_** once MMR has them
+  const [, setCourse] = useSimVar(
+   navReceiverType === TransceiverType.VOR ? `K:VOR${props.side === 'L' ? 1 : 2}_SET` : 'L:A32NX_RMP_LS_COURSE',
+   'number'
+ );
 
   // Hook radio management panel mode buttons to set panelMode SimVar.
   useInteractionEvent(`A32NX_RMP_${props.side}_VHF1_BUTTON_PRESSED`, () => {
     setPanelMode(1);
     setPreviousPanelMode(1);
-    setNavTransceiverType(TransceiverType.RADIO_VHF);
+    setIndexTransceiver(1);
   });
 
   useInteractionEvent(`A32NX_RMP_${props.side}_VHF2_BUTTON_PRESSED`, () => {
     setPanelMode(2);
     setPreviousPanelMode(2);
-    setNavTransceiverType(TransceiverType.RADIO_VHF);
+    setIndexTransceiver(2);
   });
 
   useInteractionEvent(`A32NX_RMP_${props.side}_VHF3_BUTTON_PRESSED`, () => {
     setPanelMode(3);
     setPreviousPanelMode(3);
-    setNavTransceiverType(TransceiverType.RADIO_VHF);
+    setIndexTransceiver(3);
+  });
+
+  useInteractionEvent(`A32NX_RMP_${props.side}_HF1_BUTTON_PRESSED`, () => {
+    setPanelMode(4);
+    setPreviousPanelMode(4);
+    setIndexTransceiver(1);
+  });
+
+  useInteractionEvent(`A32NX_RMP_${props.side}_HF2_BUTTON_PRESSED`, () => {
+    setPanelMode(5);
+    setPreviousPanelMode(5);
+    setIndexTransceiver(2);
   });
 
   useInteractionEvent(`A32NX_RMP_${props.side}_NAV_BUTTON_PRESSED`, () => {
     if (navButtonPressed) {
+      setCourse(-1);
       setPanelMode(previousPanelMode);
-      setNavTransceiverType(TransceiverType.RADIO_VHF);
     }
 
     setNavButton(!navButtonPressed);
@@ -92,47 +113,48 @@ const PoweredRadioPanel = (props: Props) => {
 
   useInteractionEvent(`A32NX_RMP_${props.side}_VOR_BUTTON_PRESSED`, () => {
     if (navButtonPressed) {
-      setPanelMode(6);
-      setNavTransceiverType(TransceiverType.VOR);
+      setPanelMode(7);
+      setNavReceiverType(TransceiverType.VOR);
     }
   });
 
   useInteractionEvent(`A32NX_RMP_${props.side}_ILS_BUTTON_PRESSED`, () => {
     if (navButtonPressed) {
-      setPanelMode(7);
-      setNavTransceiverType(TransceiverType.ILS);
-    }
-  });
-
-  useInteractionEvent(`A32NX_RMP_${props.side}_GLS_BUTTON_PRESSED`, () => {
-    if (navButtonPressed) {
       setPanelMode(8);
-      setNavTransceiverType(TransceiverType.GLS);
+      setNavReceiverType(TransceiverType.ILS);
     }
   });
 
-  useInteractionEvent(`A32NX_RMP_${props.side}_MLS_BUTTON_PRESSED`, () => {
-    if (navButtonPressed) {
-      setPanelMode(9);
-      setNavTransceiverType(TransceiverType.MLS);
-    }
-  });
+  /**
+   * MLS IMPLEMENTED IN THE XML BEHAVIOURS
+   * BUT DISABLED HERE SINCE THERE IS NOT ENOUGH REFERENCES
+   */
+  // useInteractionEvent(`A32NX_RMP_${props.side}_MLS_BUTTON_PRESSED`, () => {
+  //   if (navButtonPressed) {
+  //     setPanelMode(8);
+  //   }
+  // });
 
   useInteractionEvent(`A32NX_RMP_${props.side}_ADF_BUTTON_PRESSED`, () => {
     if (navButtonPressed) {
-      setPanelMode(10);
-      setNavTransceiverType(TransceiverType.ADF);
+      setPanelMode(11);
+      setNavReceiverType(TransceiverType.ADF);
     }
   });
 
   // This means we're in a VHF communications mode.
-  switch (navTransceiverType) {
-    case TransceiverType.RADIO_VHF:
-      return <VhfRadioPanel side={props.side} vhf={panelMode} />;
-    case TransceiverType.VOR:
-    case TransceiverType.ILS:
-    case TransceiverType.ADF:
-      return <NavRadioPanel side={props.side} transceiver={navTransceiverType} />;
+  switch (panelMode) {
+    case 1:
+    case 2:
+    case 3:
+      return <VhfRadioPanel side={props.side} vhf={indexTransceiver} />;
+    case 4:
+    case 5:
+      return <HfRadioPanel side={props.side} hf={indexTransceiver} />;
+    case 7:
+    case 8:
+    case 11:
+      return <NavRadioPanel side={props.side} receiver={navReceiverType} />;
     default:
       // If we reach this block, something's gone wrong. We'll just render a broken panel.
       return (
--- fbw-a32nx/src/systems/instruments/src/RMP/Components/NavRadioPanel.tsx
+++ fbw-a32nx/src/systems/instruments/src/RMP/Components/NavRadioPanel.tsx
@@ -16,9 +16,9 @@ interface Props {
   side: string;
 
   /**
-   * The NAV transceiver  (VOR, ILS, ADF).
+   * The NAV receiver  (VOR, ILS, ADF).
    */
-  transceiver: number;
+  receiver: number;
 }
 
 enum Mode {
@@ -33,12 +33,12 @@ enum Mode {
 
 /**
  *
- * @param transceiver The transceiver type (VOR, ADF or ILS)
- * @param side Side on which the transceiver is (L or R)
+ * @param receiver The receiver type (VOR, ADF or ILS)
+ * @param side Side on which the receiver is (L or R)
  * @returns a tuble simvar
  */
-const useActiveFrequency = (transceiver: number, side: string) => {
-  switch (transceiver) {
+const useActiveFrequency = (receiver: number, side: string) => {
+  switch (receiver) {
     case TransceiverType.VOR:
       return useSimVar(`L:A32NX_RMP_${side}_SAVED_ACTIVE_FREQUENCY_VOR`, 'number');
     case TransceiverType.ADF:
@@ -50,12 +50,12 @@ const useActiveFrequency = (transceiver: number, side: string) => {
 
 /**
  *
- * @param transceiver The transceiver type (VOR, ADF or ILS)
- * @param side Side on which the transceiver is (L or R)
+ * @param receiver The receiver type (VOR, ADF or ILS)
+ * @param side Side on which the receiver is (L or R)
  * @returns a tuble simvar
  */
-const useStandbyFrequency = (transceiver: number, side: string) => {
-  switch (transceiver) {
+const useStandbyFrequency = (receiver: number, side: string) => {
+  switch (receiver) {
     case TransceiverType.VOR:
       return useSimVar(`L:A32NX_RMP_${side}_SAVED_STANDBY_FREQUENCY_VOR`, 'number');
     case TransceiverType.ADF:
@@ -67,12 +67,12 @@ const useStandbyFrequency = (transceiver: number, side: string) => {
 
 /**
  *
- * @param transceiver The transceiver type (VOR, ADF or ILS)
- * @param side Side on which the transceiver is (L or R)
+ * @param receiver The receiver type (VOR, ADF or ILS)
+ * @param side Side on which the receiver is (L or R)
  * @returns a tuble simvar
  */
-const useCourse = (transceiver: number, side: string) => {
-  switch (transceiver) {
+const useCourse = (receiver: number, side: string) => {
+  switch (receiver) {
     case TransceiverType.VOR:
       return useSimVar(`L:A32NX_RMP_${side}_SAVED_COURSE_VOR`, 'number');
     default:
@@ -80,16 +80,16 @@ const useCourse = (transceiver: number, side: string) => {
   }
 };
 
-const setActiveFrequencySimVar = (transceiver: number, index: number, frequency: number) => {
-  if (transceiver === TransceiverType.ADF) {
+const setActiveFrequencySimVar = (receiver: number, index: number, frequency: number) => {
+  if (receiver === TransceiverType.ADF) {
     SimVar.SetSimVarValue(
       `K:ADF${index === 1 ? '' : index}_ACTIVE_SET`,
       'Frequency ADF BCD32',
       Avionics.Utils.make_adf_bcd32(frequency),
     );
+  } else {
+    SimVar.SetSimVarValue(`K:NAV${index}_RADIO_SET_HZ`, 'Hz', frequency);
   }
-
-  SimVar.SetSimVarValue(`K:NAV${index}_RADIO_SET_HZ`, 'Hz', frequency);
 };
 
 /**
@@ -101,48 +101,71 @@ export const NavRadioPanel = (props: Props) => {
   let standbyWindow: JSX.Element;
 
   let index = props.side === 'L' ? 1 : 2;
-  if (props.transceiver === TransceiverType.ILS) {
+  if (props.receiver === TransceiverType.ILS) {
     index = 3; // Both RMPs manage the same ILS
   }
 
   const [mode, setMode] = useState(Mode.FREQUENCY);
 
-  const [activeFrequency, setActiveFrequencySaved] = useActiveFrequency(props.transceiver, props.side);
-  const [standbyFrequency, setStandbyFrequencySaved] = useStandbyFrequency(props.transceiver, props.side);
-  const [course, setCourseSaved] = useCourse(props.transceiver, props.side);
-
-  const [, setCourse] = useSimVar(
-    props.transceiver === TransceiverType.VOR ? `K:VOR${index}_SET` : 'L:A32NX_FM_LS_COURSE',
-    'number',
-    100,
-  );
+  const [activeFrequency, setActiveFrequencySaved] = useActiveFrequency(props.receiver, props.side);
+  const [standbyFrequency, setStandbyFrequencySaved] = useStandbyFrequency(props.receiver, props.side);
+  const [course, setCourseSaved] = useCourse(props.receiver, props.side);
 
   const [APPR] = useSimVar('L:A32NX_FCU_APPR_MODE_ACTIVE', 'bool');
   const [AP] = useSimVar('L:A32NX_AUTOPILOT_ACTIVE', 'bool');
   const [RA1] = useSimVar('L:A32NX_RA_1_RADIO_ALTITUDE', 'number');
   const [RA2] = useSimVar('L:A32NX_RA_2_RADIO_ALTITUDE', 'number');
+   // FIXME: Remove A32NX_RMP_LS_COURSE and K:VOR${index}_SET once MMR implements them
+  const [, setCourse] = useSimVar(
+    props.receiver === TransceiverType.VOR ? `K:VOR${index}_SET` : 'L:A32NX_RMP_LS_COURSE',
+    'number',
+    100,
+  );
 
   useInteractionEvent(`A32NX_RMP_${props.side}_TRANSFER_BUTTON_PRESSED`, () => {
+    // FIXME: Remove this logic once MMR is implemented
     // Inhibit RMP tuning if below 700 RA, APPR engaged, at least one AP/FD engaged (FCOM compliant)
     if ((RA1 >= 700 && RA2 >= 700) || !APPR || !AP) {
       if (mode === Mode.FREQUENCY) {
-        if (props.transceiver !== TransceiverType.ADF) {
+        if (props.receiver !== TransceiverType.ADF) {
           setMode(Mode.COURSE);
         }
 
-        // FCOM compliant: Both RMPs must be in nav backup mode in order to tune the ILS
+        // FCOM compliant: If ILS, the frequency can be tuned via the RMP only if both RMPs are in nav backup mode.
         if (
-          props.transceiver !== TransceiverType.ILS ||
-          (SimVar.GetSimVarValue('L:A32NX_RMP_L_NAV_BUTTON_SELECTED', 'Bool') &&
-            SimVar.GetSimVarValue('L:A32NX_RMP_R_NAV_BUTTON_SELECTED', 'Bool'))
+          props.receiver !== TransceiverType.ILS ||
+          (SimVar.GetSimVarValue('L:A32NX_RMP_NAV_BACKUP_MODE', 'Bool') === true)
         ) {
-          setActiveFrequencySimVar(props.transceiver, index, standbyFrequency);
+          setActiveFrequencySimVar(props.receiver, index, standbyFrequency);
         }
         setActiveFrequencySaved(standbyFrequency);
       } else {
         setCourse(course);
         setMode(Mode.FREQUENCY);
       }
+    }
+
+    // This effect to display frequency mode instead of course mode when switching between receivers.
+    // After few debug sessions, it was noticed standbyFrequency was the first valuable sign of switch.
+    // I could have listened props.receiver but this caused flickering (very fast display of previous frequency) due to sequential render
+    useEffect(() => {
+      // Performance purpose. Could set Frequency everytime but setMode fires a render
+      if (mode === Mode.COURSE) {
+        setMode(Mode.FREQUENCY);
+      }
+    }, [standbyFrequency]);
+
+    if (mode === Mode.FREQUENCY) {
+      standbyWindow = (
+        <StandbyFrequency
+          side={props.side}
+          value={standbyFrequency}
+          setValue={setStandbyFrequencySaved}
+          transceiver={props.receiver}
+        />
+      );
+    } else {
+      standbyWindow = <StandbyCourse side={props.side} value={course} setValue={setCourseSaved} />;
     }
   });
 
@@ -162,7 +185,7 @@ export const NavRadioPanel = (props: Props) => {
         side={props.side}
         value={standbyFrequency}
         setValue={setStandbyFrequencySaved}
-        transceiver={props.transceiver}
+        transceiver={props.receiver}
       />
     );
   } else {
