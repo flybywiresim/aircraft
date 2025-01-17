29f57597aa1751906e7338bc4e02d9d8e4a1be5e
Michael Corcoran <tracer@outlook.co.nz>
2025-01-17 22:56:47 +1300

    fix(efb): make 1 sub and clean up sub

--- fbw-common/src/systems/instruments/src/EFB/TroubleshootingContext.tsx
+++ fbw-common/src/systems/instruments/src/EFB/TroubleshootingContext.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0
 
 import { EventBus } from '@microsoft/msfs-sdk';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { TroubleshootingEvents } from '../../../shared/src/Troubleshooting';
 
 const TroubleshootingContext = React.createContext<string[]>(undefined as any);
@@ -14,10 +14,19 @@ interface TroubleshootingContextProps {
 export const TroubleshootingContextProvider: React.FC<TroubleshootingContextProps> = ({ eventBus, children }) => {
   const [errorLog, setErrorLog] = useState([] as string[]);
 
-  eventBus
-    .getSubscriber<TroubleshootingEvents>()
-    .on('troubleshooting_log_error')
-    .handle((err) => setErrorLog([`${new Date().toISOString()}: ${err}`, ...errorLog]));
+  useEffect(() => {
+    let log = [];
+    const sub = eventBus
+      .getSubscriber<TroubleshootingEvents>()
+      .on('troubleshooting_log_error')
+      .handle((err) => {
+        log = [`${new Date().toISOString()}: ${err}`, ...log];
+        setErrorLog(log);
+      });
+    return () => {
+      sub.destroy();
+    };
+  }, []);
 
   return <TroubleshootingContext.Provider value={errorLog}>{children}</TroubleshootingContext.Provider>;
 };
