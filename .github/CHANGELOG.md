bb69b21911c81f7fe25312b2b933be02d84487d5
Michael Corcoran <tracer@outlook.co.nz>
2025-01-18 03:51:03 +1300

    feat(atsu): msfs taf source

--- .github/CHANGELOG.md
+++ .github/CHANGELOG.md
@@ -125,7 +125,7 @@
 1. [A380X/MFD] Add airport data page into the MFD (DATA > AIRPORT) - @bulenteroglu (senolitam)
 1. [A380X/EFB] Adds PRIM/SEC/FCDC failures to EFB - @flogross89 (floridude)
 1. [A380X/PFD] Fix precision of pitch trim indicator - @flogross89 (floridude)
-
+1. [ATSU] Add MSFS as TAF source in MSFS2024 - @tracernz (Mike)
 
 ## 0.12.0
 
--- fbw-common/src/systems/datalink/router/src/msfs/MsfsConnector.ts
+++ fbw-common/src/systems/datalink/router/src/msfs/MsfsConnector.ts
@@ -3,6 +3,7 @@
 
 import { Metar } from '@microsoft/msfs-sdk';
 import { AtsuStatusCodes, WeatherMessage } from '../../../common/src';
+import { isMsfs2024 } from 'shared/src';
 
 export class MsfsConnector {
   public static async receiveMsfsMetar(icao: string, message: WeatherMessage): Promise<AtsuStatusCodes> {
@@ -18,5 +19,24 @@ export class MsfsConnector {
       message.Reports.push({ airport: icao, report: 'NO METAR AVAILABLE' });
       return AtsuStatusCodes.Ok;
     }
+  }
+
+  public static async receiveMsfsTaf(icao: string, message: WeatherMessage): Promise<AtsuStatusCodes> {
+    if (isMsfs2024()) {
+      try {
+        const taf: { tafString: string; icao: string } = await Coherent.call('GET_TAF_BY_IDENT', icao);
+        let report = taf.tafString;
+        if (!report || taf.icao !== icao) {
+          report = 'NO TAF AVAILABLE';
+        }
+        message.Reports.push({ airport: icao, report });
+        return AtsuStatusCodes.Ok;
+      } catch {
+        message.Reports.push({ airport: icao, report: 'NO TAF AVAILABLE' });
+        return AtsuStatusCodes.Ok;
+      }
+    }
+    message.Reports.push({ airport: icao, report: 'NO TAF AVAILABLE' });
+    return AtsuStatusCodes.Ok;
   }
 }
