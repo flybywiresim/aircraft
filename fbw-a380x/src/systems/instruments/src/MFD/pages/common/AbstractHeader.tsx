b95c1d823066dd53e447e78c6e375617a9df0282
jet2code <airb95@gmail.com>
2025-01-17 12:40:54 -0500

    Set atcCallsign string in header when it is null

--- fbw-a380x/src/systems/instruments/src/MFD/pages/common/AbstractHeader.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/common/AbstractHeader.tsx
@@ -14,7 +14,7 @@ import { MfdUiService } from 'instruments/src/MFD/pages/common/MfdUiService';
 
 interface AbstractMfdHeaderProps {
   activeFmsSource: Subscribable<'FMS 1' | 'FMS 2' | 'FMS 1-C' | 'FMS 2-C'>;
-  callsign: Subscribable<string>;
+  callsign: Subscribable<string | null>;
   uiService: MfdUiService;
   mfd: DisplayInterface & MfdDisplayInterface;
 }
@@ -108,7 +108,9 @@ export abstract class AbstractHeader extends DisplayComponent<AbstractMfdHeaderP
           hEventConsumer={this.props.mfd.hEventConsumer}
           interactionMode={this.props.mfd.interactionMode}
         />
-        <span class="mfd-header-callsign">{this.props.callsign}</span>
+        <span class="mfd-header-callsign">
+          {this.props.callsign.map((cs) => (cs !== null ? this.props.callsign : '----------'))}
+        </span>
       </div>
     );
   }
