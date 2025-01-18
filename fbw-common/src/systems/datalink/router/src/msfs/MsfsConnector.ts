471b1ef01a9eb8c1fcac8f92147fcffff64024a8
Michael Corcoran <tracer@outlook.co.nz>
2025-01-18 15:35:57 +1300

    refactor: simplify logic

--- fbw-common/src/systems/datalink/router/src/msfs/MsfsConnector.ts
+++ fbw-common/src/systems/datalink/router/src/msfs/MsfsConnector.ts
@@ -22,21 +22,18 @@ export class MsfsConnector {
   }
 
   public static async receiveMsfsTaf(icao: string, message: WeatherMessage): Promise<AtsuStatusCodes> {
+    let report: string | undefined;
     if (isMsfs2024()) {
       try {
         const taf: { tafString: string; icao: string } = await Coherent.call('GET_TAF_BY_IDENT', icao);
-        let report = taf.tafString;
-        if (!report || taf.icao !== icao) {
-          report = 'NO TAF AVAILABLE';
+        if (taf.tafString && taf.icao === icao) {
+          report = taf.tafString;
         }
-        message.Reports.push({ airport: icao, report });
-        return AtsuStatusCodes.Ok;
-      } catch {
-        message.Reports.push({ airport: icao, report: 'NO TAF AVAILABLE' });
-        return AtsuStatusCodes.Ok;
+      } catch (e) {
+        console.log('receiveMsfsTaf: Failed', e);
       }
     }
-    message.Reports.push({ airport: icao, report: 'NO TAF AVAILABLE' });
+    message.Reports.push({ airport: icao, report: report ?? 'NO TAF AVAILABLE' });
     return AtsuStatusCodes.Ok;
   }
 }
