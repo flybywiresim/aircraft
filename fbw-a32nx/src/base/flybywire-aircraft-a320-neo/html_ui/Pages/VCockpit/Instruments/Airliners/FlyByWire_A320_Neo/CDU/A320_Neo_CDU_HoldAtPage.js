880fe4d6ca8f9d75c6595a43bda1fa4157f8075e
Michael Corcoran <tracer@outlook.co.nz>
2025-01-23 17:44:56 +1300

    fix(a32nx): look for more holds

--- fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_HoldAtPage.js
+++ fbw-a32nx/src/base/flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/CDU/A320_Neo_CDU_HoldAtPage.js
@@ -57,54 +57,66 @@ class CDUHoldAtPage {
                 modifiedHold = {};
 
                 const fix = waypoint.terminationWaypoint();
-                if (fix && fix.area === 1 /* WaypointArea.Terminal */ && fix.airportIdent && fix.airportIdent.length === 4) {
-                    Fmgc.NavigationDatabaseService.activeDatabase.getHolds(fix.ident, fix.airportIdent).then((holds) => {
-                        // Pick a hold based on altitude suitability and inbound course
-                        // Missing in the navdata is the duplicate indicator that would help us pick the right area/airspace type.
-                        holds
-                            .filter((v) => v.waypoint.databaseId === fix.databaseId)
-                            .sort((a, b) => {
-                                let ret = CDUHoldAtPage.holdVerticalDistanceFromAlt(alt, a) - CDUHoldAtPage.holdVerticalDistanceFromAlt(alt, b);
-                                if (ret === 0) {
-                                    ret = Math.abs(a.magneticCourse - inboundMagneticCourse) - Math.abs(b.magneticCourse - inboundMagneticCourse);
-                                }
-                                return ret;
-                            });
-
-                        if (holds[0]) {
-                            defaultHold = {
-                                inboundMagneticCourse: holds[0].magneticCourse,
-                                turnDirection: holds[0].turnDirection,
-                                time: holds[0].lengthTime ? holds[0].lengthTime : undefined,
-                                distance: holds[0].length ? holds[0].length : undefined,
-                                type: HoldType.Database,
-                            };
-                        }
-
-                        CDUHoldAtPage.addOrEditManualHold(
-                            mcdu,
-                            waypointIndexFP,
-                            // eslint-disable-next-line prefer-object-spread
-                            Object.assign({}, defaultHold),
-                            modifiedHold,
-                            defaultHold,
-                            forPlan,
-                            inAlternate,
-                        );
-                    }).catch(() => {
-                        CDUHoldAtPage.addOrEditManualHold(
-                            mcdu,
-                            waypointIndexFP,
-                            // eslint-disable-next-line prefer-object-spread
-                            Object.assign({}, defaultHold),
-                            modifiedHold,
-                            defaultHold,
-                            forPlan,
-                            inAlternate,
-                        );
-                    });
-                    return;
+                const promises = [];
+                // Due to the way MSFS stores holds, we have to try all these possibilities.
+                if (targetPlan.originAirport) {
+                    promises.push(Fmgc.NavigationDatabaseService.activeDatabase.getHolds(fix.ident, targetPlan.originAirport.ident));
                 }
+                if (targetPlan.destinationAirport) {
+                    promises.push(Fmgc.NavigationDatabaseService.activeDatabase.getHolds(fix.ident, targetPlan.destinationAirport.ident));
+                }
+                if (fix && fix.area === 1 /* WaypointArea.Terminal */ && fix.airportIdent && fix.airportIdent.length > 0) {
+                    promises.push(Fmgc.NavigationDatabaseService.activeDatabase.getHolds(fix.ident, fix.airportIdent));
+                }
+                Promise.all(promises).then((resolvedPromises) => {
+                    // Pick a hold based on altitude suitability and inbound course
+                    // Missing in the navdata is the duplicate indicator that would help us pick the right area/airspace type.
+                    const holds = resolvedPromises
+                        .reduce((allHolds, holds) => {
+                            allHolds.push(...holds); return allHolds;
+                        })
+                        .filter((v) => v.waypoint.databaseId === fix.databaseId)
+                        .sort((a, b) => {
+                            let ret = CDUHoldAtPage.holdVerticalDistanceFromAlt(alt, a) - CDUHoldAtPage.holdVerticalDistanceFromAlt(alt, b);
+                            if (ret === 0) {
+                                ret = Math.abs(a.magneticCourse - inboundMagneticCourse) - Math.abs(b.magneticCourse - inboundMagneticCourse);
+                            }
+                            return ret;
+                        });
+
+                    if (holds[0]) {
+                        defaultHold = {
+                            inboundMagneticCourse: holds[0].magneticCourse,
+                            turnDirection: holds[0].turnDirection,
+                            time: holds[0].lengthTime ? holds[0].lengthTime : undefined,
+                            distance: holds[0].length ? holds[0].length : undefined,
+                            type: HoldType.Database,
+                        };
+                    }
+
+                    CDUHoldAtPage.addOrEditManualHold(
+                        mcdu,
+                        waypointIndexFP,
+                        // eslint-disable-next-line prefer-object-spread
+                        Object.assign({}, defaultHold),
+                        modifiedHold,
+                        defaultHold,
+                        forPlan,
+                        inAlternate,
+                    );
+                }).catch(() => {
+                    CDUHoldAtPage.addOrEditManualHold(
+                        mcdu,
+                        waypointIndexFP,
+                        // eslint-disable-next-line prefer-object-spread
+                        Object.assign({}, defaultHold),
+                        modifiedHold,
+                        defaultHold,
+                        forPlan,
+                        inAlternate,
+                    );
+                });
+                return;
             }
 
             CDUHoldAtPage.addOrEditManualHold(
