41babf643e7b46522feb9b0b669268e9b796b40f
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2025-01-26 17:45:46 -0500

    chore: fix after merge

--- fbw-a380x/src/systems/instruments/src/MFD/pages/common/FlightPlanFooter.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/common/FlightPlanFooter.tsx
@@ -1,5 +1,5 @@
 import { DisplayComponent, EventBus, FSComponent, VNode } from '@microsoft/msfs-sdk';
-import { Button } from './Button';
+import { Button } from 'instruments/src/MsfsAvionicsCommon/UiWidgets/Button';
 import { ObservableFlightPlanManager } from '@fmgc/flightplanning/ObservableFlightPlanManager';
 import { FmcServiceInterface } from '../../FMC/FmcServiceInterface';
 import { FmsDisplayInterface } from '@fmgc/flightplanning/interface/FmsDisplayInterface';
