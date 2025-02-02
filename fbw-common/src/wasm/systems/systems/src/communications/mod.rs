c986441f641b4a9bbfe3afb585eea36b19c2d748
juliansebline <juliansebline@hotmail.com>
2025-02-02 12:37:25 +0100

    Removal of Option(rmp)

--- fbw-common/src/wasm/systems/systems/src/communications/mod.rs
+++ fbw-common/src/wasm/systems/systems/src/communications/mod.rs
@@ -14,8 +14,8 @@ use self::radio_management_panel::RadioManagementPanel;
 pub struct Communications {
     amu: AudioManagementUnit,
 
-    rmp_cpt: Option<RadioManagementPanel>,
-    rmp_fo: Option<RadioManagementPanel>,
+    rmp_cpt: RadioManagementPanel,
+    rmp_fo: RadioManagementPanel,
 
     sel_light_id: VariableIdentifier,
     nav_backup_mode_id: VariableIdentifier,
@@ -28,8 +28,8 @@ impl Communications {
     pub fn new(context: &mut InitContext) -> Self {
         Self {
             amu: AudioManagementUnit::new(context),
-            rmp_cpt: Some(RadioManagementPanel::new_cpt(context)),
-            rmp_fo: Some(RadioManagementPanel::new_fo(context)),
+            rmp_cpt: RadioManagementPanel::new_cpt(context),
+            rmp_fo: RadioManagementPanel::new_fo(context),
 
             sel_light_id: context.get_identifier("RMP_SEL_LIGHT_ON".to_owned()),
             nav_backup_mode_id: context.get_identifier("RMP_NAV_BACKUP_MODE".to_owned()),
@@ -42,13 +42,11 @@ impl Communications {
     pub fn update(&mut self, context: &UpdateContext) {
         self.amu.update(context);
 
-        if self.rmp_cpt.as_ref().unwrap().is_powered() && self.rmp_fo.as_ref().unwrap().is_powered()
-        {
-            self.sel_light = self.rmp_cpt.as_ref().unwrap().is_abnormal_mode()
-                || self.rmp_fo.as_ref().unwrap().is_abnormal_mode();
+        if self.rmp_cpt.is_powered() && self.rmp_fo.is_powered() {
+            self.sel_light = self.rmp_cpt.is_abnormal_mode() || self.rmp_fo.is_abnormal_mode();
 
-            self.nav_backup_mode = self.rmp_cpt.as_ref().unwrap().is_nav_backup_mode()
-                && self.rmp_fo.as_ref().unwrap().is_nav_backup_mode()
+            self.nav_backup_mode =
+                self.rmp_cpt.is_nav_backup_mode() && self.rmp_fo.is_nav_backup_mode()
         } else {
             self.sel_light = false;
             self.nav_backup_mode = false;
@@ -58,8 +56,8 @@ impl Communications {
 
 impl SimulationElement for Communications {
     fn accept<T: SimulationElementVisitor>(&mut self, visitor: &mut T) {
-        self.rmp_cpt.as_mut().unwrap().accept(visitor);
-        self.rmp_fo.as_mut().unwrap().accept(visitor);
+        self.rmp_cpt.accept(visitor);
+        self.rmp_fo.accept(visitor);
 
         self.amu.accept(visitor);
 
