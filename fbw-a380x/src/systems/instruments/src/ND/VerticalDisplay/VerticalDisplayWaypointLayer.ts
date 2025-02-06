c06aac1b24321229d56ab33c7cb6bb93ed5913dc
Florian Gross <63071941+flogross89@users.noreply.github.com>
2025-02-06 19:29:25 +0200

    correct airport shape

--- fbw-a380x/src/systems/instruments/src/ND/VerticalDisplay/VerticalDisplayWaypointLayer.ts
+++ fbw-a380x/src/systems/instruments/src/ND/VerticalDisplay/VerticalDisplayWaypointLayer.ts
@@ -26,7 +26,9 @@ export class VerticalDisplayWaypointLayer implements VerticalDisplayMapLayer<NdS
       const rx = VerticalDisplayCanvasMap.distanceToX(symbol.distanceFromAirplane, vdRange);
       const ry = VerticalDisplayCanvasMap.altToY(symbol.predictedAltitude, verticalRange);
 
-      if (BitFlags.isAny(symbol.type, NdSymbolTypeFlags.FixInfo | NdSymbolTypeFlags.FlightPlan)) {
+      if (BitFlags.isAny(symbol.type, NdSymbolTypeFlags.Airport | NdSymbolTypeFlags.Runway)) {
+        this.paintAirport(false, context, rx, ry, symbol);
+      } else if (BitFlags.isAny(symbol.type, NdSymbolTypeFlags.FixInfo | NdSymbolTypeFlags.FlightPlan)) {
         this.paintFlightPlanWaypoint(false, context, rx, ry, symbol, verticalRange);
       } else {
         this.paintWaypoint(false, context, rx, ry, symbol);
@@ -43,12 +45,30 @@ export class VerticalDisplayWaypointLayer implements VerticalDisplayMapLayer<NdS
       const rx = VerticalDisplayCanvasMap.distanceToX(symbol.distanceFromAirplane, vdRange);
       const ry = VerticalDisplayCanvasMap.altToY(symbol.predictedAltitude, verticalRange);
 
-      if (BitFlags.isAny(symbol.type, NdSymbolTypeFlags.FixInfo | NdSymbolTypeFlags.FlightPlan)) {
+      if (BitFlags.isAny(symbol.type, NdSymbolTypeFlags.Airport | NdSymbolTypeFlags.Runway)) {
+        this.paintAirport(true, context, rx, ry, symbol);
+      } else if (BitFlags.isAny(symbol.type, NdSymbolTypeFlags.FixInfo | NdSymbolTypeFlags.FlightPlan)) {
         this.paintFlightPlanWaypoint(true, context, rx, ry, symbol, verticalRange);
       } else {
         this.paintWaypoint(true, context, rx, ry, symbol);
       }
     }
+  }
+
+  private paintAirport(
+    isColorLayer: boolean,
+    context: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    symbol: NdSymbol,
+  ) {
+    const mainColor = symbol.type & NdSymbolTypeFlags.FlightPlan ? '#fff' : '#ff94ff';
+
+    this.paintAirportShape(context, x, y, isColorLayer ? mainColor : '#000', isColorLayer ? 1.75 : 3.25);
+
+    context.font = '21px Ecam';
+
+    VerticalDisplayPaintUtils.paintText(isColorLayer, context, x + 13, y + 18, symbol.ident, mainColor);
   }
 
   private paintWaypoint(
@@ -155,6 +175,27 @@ export class VerticalDisplayWaypointLayer implements VerticalDisplayMapLayer<NdS
     context.font = '21px Ecam';
 
     VerticalDisplayPaintUtils.paintText(isColorLayer, context, x + 15, y + 17, symbol.ident, mainColor);
+  }
+
+  private paintAirportShape(context: CanvasRenderingContext2D, x: number, y: number, color: string, lineWidth: number) {
+    context.strokeStyle = color;
+    context.lineWidth = lineWidth;
+
+    context.translate(x, y);
+    context.beginPath();
+    context.moveTo(-13, 0);
+    context.lineTo(13, 0);
+    context.moveTo(0, -13);
+    context.lineTo(0, 13);
+    context.rotate(45 * MathUtils.DEGREES_TO_RADIANS);
+    context.moveTo(0, -13);
+    context.lineTo(0, 13);
+    context.rotate(90 * MathUtils.DEGREES_TO_RADIANS);
+    context.moveTo(0, -13);
+    context.lineTo(0, 13);
+    context.closePath();
+    context.stroke();
+    context.resetTransform();
   }
 
   private paintWaypointShape(
