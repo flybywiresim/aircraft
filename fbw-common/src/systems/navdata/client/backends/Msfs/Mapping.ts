76d4bc7920bd790cf072444ff8b276c61e19f8fa
BBK <22713769+BlueberryKing@users.noreply.github.com>
2025-02-19 21:09:40 +0100

    Remove references to MSFS airport magvar

--- fbw-common/src/systems/navdata/client/backends/Msfs/Mapping.ts
+++ fbw-common/src/systems/navdata/client/backends/Msfs/Mapping.ts
@@ -179,7 +179,7 @@ export class MsfsMapping {
       .filter((icao) => icao.charAt(0) === 'V');
     const navaids = await this.cache.getFacilities(Array.from(new Set(navaidIcaos)), LoadType.Vor);
 
-    const magVar = msAirport.magvar ?? Facilities.getMagVar(msAirport.lat, msAirport.lon);
+    const magVar = Facilities.getMagVar(msAirport.lat, msAirport.lon);
 
     msAirport.runways.forEach((msRunway) => {
       const gradient =
@@ -260,7 +260,7 @@ export class MsfsMapping {
 
     const icaoCode = this.getIcaoCodeFromAirport(msAirport);
 
-    const magVar = msAirport.magvar ?? Facilities.getMagVar(msAirport.lat, msAirport.lon);
+    const magVar = Facilities.getMagVar(msAirport.lat, msAirport.lon);
 
     msAirport.runways.forEach((msRunway) => {
       const gradient =
