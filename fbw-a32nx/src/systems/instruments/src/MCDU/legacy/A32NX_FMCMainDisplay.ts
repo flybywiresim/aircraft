f6e7995993c1376ba99c20305fa28b435e2728c2
BBK <22713769+BlueberryKing@users.noreply.github.com>
2025-03-08 19:19:03 +0100

    Compute capture condition in the FMS

--- fbw-a32nx/src/systems/instruments/src/MCDU/legacy/A32NX_FMCMainDisplay.ts
+++ fbw-a32nx/src/systems/instruments/src/MCDU/legacy/A32NX_FMCMainDisplay.ts
@@ -5343,6 +5343,10 @@ export abstract class FMCMainDisplay implements FmsDataInterface, FmsDisplayInte
     return this.navDbIdent;
   }
 
+  public disengageNavMode() {
+    this.guidanceController.disengageNavMode();
+  }
+
   // ---------------------------
   // CDUMainDisplay Types
   // ---------------------------
