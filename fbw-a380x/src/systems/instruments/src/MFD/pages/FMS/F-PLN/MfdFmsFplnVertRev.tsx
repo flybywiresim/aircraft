6d8b1e380796af3fca94a3f9004202349283f265
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2025-03-26 22:49:58 +0000

    chore: make input error handling mandatory. Fix lint

--- fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/F-PLN/MfdFmsFplnVertRev.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/F-PLN/MfdFmsFplnVertRev.tsx
@@ -452,8 +452,7 @@ export class MfdFmsFplnVertRev extends FmsPage<MfdFmsFplnVertRevProps> {
         }
         this.stepAltsIgnored[line].set(cruiseSteps[i].isIgnored);
         this.stepAltsAboveMaxFl[line].set(
-          cruiseSteps[i].toAltitude >
-            (this.props.fmcService.master?.getRecMaxAltitude() ?? maxCertifiedAlt),
+          cruiseSteps[i].toAltitude > (this.props.fmcService.master?.getRecMaxAltitude() ?? maxCertifiedAlt),
         );
 
         if (this.stepAltsIgnored[line].get()) {
@@ -697,7 +696,7 @@ export class MfdFmsFplnVertRev extends FmsPage<MfdFmsFplnVertRevProps> {
         .map((l) => (l.isDiscontinuity === false && l.cruiseStep ? l.cruiseStep : null))
         .filter((it) => it !== null);
       const isValid = MfdFmsFplnVertRev.checkStepInsertionRules(crzFl, cruiseSteps, legIndex, altitude);
-      if (altitude > (this.props.fmcService.master?.getRecMaxAltitude()  ?? maxCertifiedAlt)) {
+      if (altitude > (this.props.fmcService.master?.getRecMaxAltitude() ?? maxCertifiedAlt)) {
         this.props.fmcService.master?.addMessageToQueue(NXSystemMessages.stepAboveMaxFl, undefined, undefined);
       }
 
@@ -1143,7 +1142,9 @@ export class MfdFmsFplnVertRev extends FmsPage<MfdFmsFplnVertRevProps> {
                                   onModified={(newAltitude) =>
                                     this.handleCruiseStepFlightLevelModified(newAltitude, li)
                                   }
-                                  errorHandler={(e) => this.props.fmcService.master?.showFmsErrorMessage(e.type, e.details)}
+                                  errorHandler={(e) =>
+                                    this.props.fmcService.master?.showFmsErrorMessage(e.type, e.details)
+                                  }
                                   hEventConsumer={this.props.mfd.hEventConsumer}
                                   interactionMode={this.props.mfd.interactionMode}
                                 />
