934bb1843e4f3264bbc9267b295a8314e23c4fd8
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2025-04-09 21:16:05 +0100

    feat(fms): add min dest efob scratchpad message

--- fbw-a380x/src/systems/instruments/src/MFD/FMC/FlightManagementComputer.ts
+++ fbw-a380x/src/systems/instruments/src/MFD/FMC/FlightManagementComputer.ts
@@ -1008,6 +1008,8 @@ export class FlightManagementComputer implements FmcInterface {
         this.acInterface.toSpeedsChecks();
         this.acInterface.checkForStepClimb();
         this.acInterface.checkTooSteepPath();
+        this.acInterface.checkDestEfobBelowMin();
+        this.acInterface.checkDestEfobBelowMinScratchPadMessage(dt);
 
         const toFlaps = this.fmgc.getTakeoffFlapsSetting();
         if (toFlaps) {
