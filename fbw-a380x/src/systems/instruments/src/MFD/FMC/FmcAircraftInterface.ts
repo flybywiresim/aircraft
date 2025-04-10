7255868bb6ae42076a1fb053c93e8c1150fc8261
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2025-04-10 23:42:49 +0100

    fix: delta time for nxLogicConfirmNode

--- fbw-a380x/src/systems/instruments/src/MFD/FMC/FmcAircraftInterface.ts
+++ fbw-a380x/src/systems/instruments/src/MFD/FMC/FmcAircraftInterface.ts
@@ -2009,7 +2009,7 @@ export class FmcAircraftInterface {
       if (minFuelAtDestination !== null) {
         const isBelowMin = this.fmgc.data.destEfobBelowMin.get();
         if (isBelowMin) {
-          this.fmgc.data.destEfobBelowMin.set(destEfob - minFuelAtDestination >= 0.3);
+          this.fmgc.data.destEfobBelowMin.set(destEfob - minFuelAtDestination <= 0.3);
         } else {
           this.fmgc.data.destEfobBelowMin.set(destEfob < minFuelAtDestination);
         }
