cc4c680bd96e52983f46cd56f1cb32974563eb17
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2025-04-12 22:23:26 -0400

    chore: that's a bit excessive

--- fbw-a32nx/src/systems/extras-host/index.ts
+++ fbw-a32nx/src/systems/extras-host/index.ts
@@ -1,15 +1,3 @@
-// Copyright (c) 2021-2024 FlyByWire Simulations
-//
-// SPDX-License-Identifier: GPL-3.0
-
-// Copyright (c) 2021-2024 FlyByWire Simulations
-//
-// SPDX-License-Identifier: GPL-3.0
-
-// Copyright (c) 2021-2024 FlyByWire Simulations
-//
-// SPDX-License-Identifier: GPL-3.0
-
 // Copyright (c) 2021-2025 FlyByWire Simulations
 //
 // SPDX-License-Identifier: GPL-3.0
