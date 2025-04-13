b2c2d3f482a9998cf8dd13944417917ec265b4d0
Jonny23787 <Jonny.leske@outlook.com>
2025-04-13 16:36:31 +0930

    add aural warning

--- fbw-a380x/src/systems/systems-host/systems/FlightWarningSystem/FwsCore.ts
+++ fbw-a380x/src/systems/systems-host/systems/FlightWarningSystem/FwsCore.ts
@@ -853,6 +853,8 @@ export class FwsCore {
 
   public readonly flapsLeverNotZeroWarning = Subject.create(false);
 
+  public readonly flapsLeverNotZeroAural = Subject.create(false);
+
   public readonly speedBrakeCommand5sConfirm = new NXLogicConfirmNode(5, true);
 
   public readonly speedBrakeCommand50sConfirm = new NXLogicConfirmNode(50, true);
@@ -3641,6 +3643,7 @@ export class FwsCore {
         this.flightPhase.get() === 8 &&
         !this.slatFlapSelectionS0F0,
     );
+    this.flapsLeverNotZeroAural.set(this.flapsLeverNotZeroWarning.get());
 
     // spd brk still out
     this.speedBrakeCommand5sConfirm.write(this.speedBrakeCommand.get(), deltaTime);
