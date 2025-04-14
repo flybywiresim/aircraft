f7e5e6be2e08669e89152c91c504a19dae92b832
Lukas Michels <lukasmichels00@gmail.com>
2025-04-14 23:00:06 +0200

    fix(a380x/sd): improve BLEED page visuals

--- fbw-a380x/src/systems/instruments/src/SD/Pages/Bleed/elements/BleedApu.tsx
+++ fbw-a380x/src/systems/instruments/src/SD/Pages/Bleed/elements/BleedApu.tsx
@@ -12,19 +12,41 @@ const BleedApu: FC<BleedApuProps> = ({ isPack1Operative, isPack2Operative }) => 
   const [apuMasterSwitchPbIsOn] = useSimVar('L:A32NX_OVHD_APU_MASTER_SW_PB_IS_ON', 'bool', 500);
 
   // The bleed valve and isolation valve operate "simulatneously" according to the FCOM
+  // Not fully correct, according to other sources, APU bleed valve is closed on ground without any bleed users,
+  // while the isolation valve is then open
   const apuIsolationValveOpen = apuBleedAirValveOpen;
 
+  const x = 360;
+  const y = 602;
+
   return (
-    <g id="ApuBleed">
-      <Valve x={352} y={446} radius={19} css="Green SW2" position={apuIsolationValveOpen ? 'V' : 'H'} sdacDatum />
+    <g id="ApuBleed" style={{ transform: `translate3d(${x}px, ${y}px, 0px)` }}>
+      <Valve
+        x={0}
+        y={-150}
+        radius={19.5}
+        css="Green SW2 NoFill"
+        position={apuIsolationValveOpen ? 'V' : 'H'}
+        sdacDatum
+      />
+
+      {/* Why would this depend on the packs? */}
       <path
         className={`${apuIsolationValveOpen && (isPack1Operative || isPack2Operative) ? 'Show' : 'Hide'} Line Green NoFill`}
-        d={`M${352},${325} l 0,201`}
+        d="M0,-69 l 0,-61"
       />
+
       <g className={apuMasterSwitchPbIsOn ? 'Show' : 'Hide'}>
-        <Valve x={352} y={546} radius={19} css="Green SW2" position={apuBleedAirValveOpen ? 'V' : 'H'} sdacDatum />
-        <path className="Green SW2" d="M 352,565 l 0,30" />
-        <text x={352} y={610} className="White F23 MiddleAlign">
+        <Valve
+          x={0}
+          y={-49}
+          radius={19.5}
+          css="Green SW2 NoFill"
+          position={apuBleedAirValveOpen ? 'V' : 'H'}
+          sdacDatum
+        />
+        <path className="Green SW2" d="M 0,0 l 0,-30" />
+        <text x={1} y={16} className="White F23 MiddleAlign">
           APU
         </text>
       </g>
