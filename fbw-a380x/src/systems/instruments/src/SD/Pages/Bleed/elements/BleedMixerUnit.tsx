bc23c2ee9f3643bcdc3f2f2ad9bc7bbc97a2e195
Lukas Michels <lukasmichels00@gmail.com>
2025-04-14 23:00:06 +0200

    fix(a380x/sd): improve BLEED page visuals

--- fbw-a380x/src/systems/instruments/src/SD/Pages/Bleed/elements/BleedMixerUnit.tsx
+++ fbw-a380x/src/systems/instruments/src/SD/Pages/Bleed/elements/BleedMixerUnit.tsx
@@ -18,56 +18,58 @@ const BleedMixerUnit: FC<BleedMixerUnitProps> = ({ x, y, _sdacDatum }) => {
   const [ramInletOpen] = useSimVar('L:A32NX_OVHD_COND_RAM_AIR_PB_IS_ON', 'bool', 100);
 
   return (
-    <g id="MixerUnit">
-      <path className="Grey SW2" d={`M ${x},${y} l -228,0 l 0,16 l 102,0`} />
-      <path className="Grey SW2" d={`M ${x},${y} l 210,0 l 0,16 l -100,0`} />
-      <path className="Grey SW2" d={`M ${x - 74},${y + 16} l 133,0`} />
+    <g id="MixerUnit" style={{ transform: `translate3d(${x}px, ${y}px, 0px)` }}>
+      <path className="Grey SW2 NoFill" d="M 105,14 h -105 v -14 h 441 v 14 h -98 m -54,0 h -130" />
 
       <GaugeMarkerComponent
         value={ramInletOpen ? 1 : 2}
-        x={x - 80}
-        y={y + 16}
+        x={152}
+        y={14}
         min={0}
         max={2}
         radius={44}
         startAngle={180}
         endAngle={270}
-        className="Green Line"
+        className="Green SW3 LineRound"
         indicator
+        halfIndicator
         multiplierOuter={1}
+        multiplierInner={0.07}
       />
-      <circle className="Green SW2 BackgroundFill" cx={x - 80} cy={y + 16} r={3} />
+      <circle className="Green SW2 NoFill" cx={152} cy={14} r={4} />
 
       <GaugeMarkerComponent
         value={ramInletOpen ? 1 : 2}
-        x={x + 105}
-        y={y + 16}
+        x={337}
+        y={15}
         min={0}
         max={2}
         radius={44}
         startAngle={180}
         endAngle={270}
-        className="Green Line"
+        className="Green SW3 LineRound"
         indicator
+        halfIndicator
         multiplierOuter={1}
+        multiplierInner={0.07}
       />
-      <circle className="Green SW2 BackgroundFill" cx={x + 104} cy={y + 16} r={3} />
+      <circle className="Green SW2 NoFill" cx={337} cy={15} r={4} />
 
       <Triangle
-        x={x - 116}
-        y={y - 22}
+        x={114}
+        y={-23}
         colour={airSuppliedToCabinAndCockpit ? 'Green' : 'Amber'}
         fill={0}
         orientation={0}
-        scale={1.2}
+        scale={1.3}
       />
       <Triangle
-        x={x + 98}
-        y={y - 22}
+        x={328}
+        y={-23}
         colour={airSuppliedToCabinAndCockpit ? 'Green' : 'Amber'}
         fill={0}
         orientation={0}
-        scale={1.2}
+        scale={1.3}
       />
     </g>
   );
