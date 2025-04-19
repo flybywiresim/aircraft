ed5411933c0849d2ac514c48fc5836f667c6cc83
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2025-04-19 23:46:17 +0100

    fix: typos. Disable degraded & redunc loss in case of fault

--- fbw-a380x/src/systems/systems-host/systems/FlightWarningSystem/FwsCore.ts
+++ fbw-a380x/src/systems/systems-host/systems/FlightWarningSystem/FwsCore.ts
@@ -3125,7 +3125,7 @@ export class FwsCore {
 
     this.fdac2Channel1Failure.set(SimVar.GetSimVarValue('L:A32NX_COND_FDAC_2_CHANNEL_1_FAILURE', 'bool'));
 
-    this.pack2Ctl1Fault.set(this.acESSBusPowered.get() && this.fdac2Channel2Failure.get());
+    this.pack2Ctl1Fault.set(this.acESSBusPowered.get() && this.fdac2Channel1Failure.get());
 
     this.fdac2Channel2Failure.set(SimVar.GetSimVarValue('L:A32NX_COND_FDAC_2_CHANNEL_2_FAILURE', 'bool'));
 
@@ -3193,14 +3193,16 @@ export class FwsCore {
     }
 
     this.oneTcsAppFailed.set(
-      this.cpiomBTcsAppDiscreteWord1.isFailureWarning() ||
-        this.cpiomBTcsAppDiscreteWord2.isFailureWarning() ||
-        this.cpiomBTcsAppDiscreteWord3.isFailureWarning() ||
-        this.cpiomBTcsAppDiscreteWord4.isFailureWarning(),
+      !this.tempCtlFault.get() &&
+        (this.cpiomBTcsAppDiscreteWord1.isFailureWarning() ||
+          this.cpiomBTcsAppDiscreteWord2.isFailureWarning() ||
+          this.cpiomBTcsAppDiscreteWord3.isFailureWarning() ||
+          this.cpiomBTcsAppDiscreteWord4.isFailureWarning()),
     );
 
     this.tempCtrDegraded.set(
-      this.cpiomBTcsAppDiscreteWord1.isFailureWarning() &&
+      !this.tempCtlFault.get() &&
+        this.cpiomBTcsAppDiscreteWord1.isFailureWarning() &&
         this.cpiomBTcsAppDiscreteWord2.isFailureWarning() &&
         this.cpiomBTcsAppDiscreteWord3.isFailureWarning() &&
         this.cpiomBTcsAppDiscreteWord4.isFailureWarning(),
@@ -3319,8 +3321,8 @@ export class FwsCore {
     this.vcmAftChannel1Failure.set(SimVar.GetSimVarValue('L:A32NX_VENT_AFT_VCM_CHANNEL_1_FAILURE', 'bool'));
     this.vcmAftChannel2Failure.set(SimVar.GetSimVarValue('L:A32NX_VENT_AFT_VCM_CHANNEL_2_FAILURE', 'bool'));
 
-    this.aftVentCtl1Fault.set(this.vcmFwdChannel1Failure.get() && this.dc2BusPowered.get());
-    this.aftVentCtl2Fault.set(this.vcmFwdChannel2Failure.get() && this.dcESSBusPowered.get());
+    this.aftVentCtl1Fault.set(this.vcmAftChannel1Failure.get() && this.dc2BusPowered.get());
+    this.aftVentCtl2Fault.set(this.vcmAftChannel2Failure.get() && this.dcESSBusPowered.get());
 
     this.fwdVentCtrDegraded.set(
       !this.cpiomBVcsAppDiscreteWord1.isNormalOperation() && !this.cpiomBVcsAppDiscreteWord3.isNormalOperation(),
