9cc123eeb99ff8f9e74615adb82e0e5e5f8c67b3
Jonny23787 <Jonny.leske@outlook.com>
2025-04-26 21:03:40 +0930

    Basic procedure implementation

--- fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/EcamMessages/AbnormalSensed/ata46-49-52-56.tsx
+++ fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/EcamMessages/AbnormalSensed/ata46-49-52-56.tsx
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2024 FlyByWire Simulations
 // SPDX-License-Identifier: GPL-3.0
 
-import { AbnormalProcedure } from 'instruments/src/MsfsAvionicsCommon/EcamMessages';
+import { AbnormalProcedure, ChecklistLineStyle } from 'instruments/src/MsfsAvionicsCommon/EcamMessages';
 
 // Convention for IDs:
 // First two digits: ATA chapter
@@ -17,4 +17,52 @@ import { AbnormalProcedure } from 'instruments/src/MsfsAvionicsCommon/EcamMessag
 //    9 for ABN non-sensed procedures
 
 /** All abnormal sensed procedures (alerts, via ECL) should be here. */
-export const EcamAbnormalSensedAta46495256: { [n: number]: AbnormalProcedure } = {};
+export const EcamAbnormalSensedAta46495256: { [n: number]: AbnormalProcedure } = {
+  // ATA 52 DOOR
+  520800008: {
+    title: '\x1b<4m\x1b4mDOOR\x1bm CKPT SLIDING WINDOW NOT CLOSED',
+    sensed: true,
+    items: [
+      {
+        name: 'CKPT SLIDING WINDOW',
+        labelNotCompleted: 'CLOSE',
+        sensed: true,
+      },
+    ],
+  },
+  520800017: {
+    title: '\x1b<4m\x1b4mDOOR\x1bm MAIN 1L NOT CLOSED',
+    sensed: true,
+    items: [
+      {
+        name: 'PACK 1+2 & CAB PRESS NOT AVAIL',
+        sensed: true,
+        style: ChecklistLineStyle.Amber,
+      },
+      {
+        name: 'IF ABNORM CAB V/S :',
+        sensed: false,
+        condition: true,
+      },
+      {
+        name: 'MAX FL : 100/MEA-MORA',
+        sensed: false,
+      },
+      {
+        name: 'CABIN CREW',
+        labelNotCompleted: 'ADVISE',
+        sensed: false,
+      },
+      {
+        name: 'ATC',
+        labelNotCompleted: 'NOTIFY',
+        sensed: false,
+      },
+      {
+        name: 'DECEND TO FL 100/MEA-MORA',
+        labelNotCompleted: 'INITIATE',
+        sensed: false,
+      },
+    ],
+  },
+};
