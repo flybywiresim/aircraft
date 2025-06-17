2afa90e0bea45a2614b681220a32831aaadc6368
lucasiq <lucas.jarrosson@gmail.com>
2025-06-18 00:22:37 +0200

    fix(A32X/MCDU): use NXUnits on arrival airport runway lengths

--- fbw-a32nx/src/systems/instruments/src/MCDU/legacy_pages/A320_Neo_CDU_AvailableArrivalsPage.ts
+++ fbw-a32nx/src/systems/instruments/src/MCDU/legacy_pages/A320_Neo_CDU_AvailableArrivalsPage.ts
@@ -23,6 +23,7 @@ import {
   ApproachType,
   ApproachUtils,
   Arrival,
+  NXUnits,
   Runway,
   RunwayUtils,
 } from '@flybywiresim/fbw-sdk';
@@ -221,7 +222,7 @@ export class CDUAvailableArrivalsPage {
             rows[2 * i] = [
               `{${color}}${!isSelected ? '{' : '{sp}'}${ApproachUtils.shortApproachName(approachOrRunway)}{end}`,
               '',
-              `{sp}{sp}{sp}${runwayLength}{small}M{end}[color]${color}`,
+              `{sp}{sp}{sp}${NXUnits.mToUser(runwayLength).toFixed(0).padStart(6, '\xa0')}{small}${NXUnits.userDistanceUnit().padEnd(2)}{end}{end}[color]${color}`,
             ];
             rows[2 * i + 1] = [`{${color}}{sp}{sp}{sp}${runwayCourse}${ilsText}{end}`];
           }
@@ -262,7 +263,7 @@ export class CDUAvailableArrivalsPage {
           rows[2 * i] = [
             `{${color}}${!isSelected ? '{' : '{sp}'}${RunwayUtils.runwayString(approachOrRunway.ident)}{end}`,
             '',
-            `{sp}{sp}{sp}${runwayLength}{small}M{end}[color]${color}`,
+            `{sp}{sp}{sp}${NXUnits.mToUser(runwayLength).toFixed(0).padStart(6, '\xa0')}{small}${NXUnits.userDistanceUnit().padEnd(2)}{end}{end}[color]${color}`,
           ];
           rows[2 * i + 1] = ['{sp}{sp}{sp}{sp}' + runwayCourse + '[color]cyan'];
 
