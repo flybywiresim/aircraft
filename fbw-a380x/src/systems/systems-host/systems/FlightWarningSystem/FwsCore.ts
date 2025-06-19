374717d01b0e00dc70dd8fd42fd5ab229e7edc86
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2025-06-19 17:54:48 +0100

    fix: hyd sys lo pressure flap lvr 1 check

--- fbw-a380x/src/systems/systems-host/systems/FlightWarningSystem/FwsCore.ts
+++ fbw-a380x/src/systems/systems-host/systems/FlightWarningSystem/FwsCore.ts
@@ -971,8 +971,6 @@ export class FwsCore {
 
   public readonly groundSpoilerNotArmedWarning = Subject.create(false);
 
-  public readonly taxiInFlap0Check = new NXLogicConfirmNode(60, false);
-
   /* FUEL */
 
   public readonly engine1ValueSwitch = ConsumerSubject.create(this.sub.on('fuel_valve_switch_1'), false);
@@ -3841,9 +3839,6 @@ export class FwsCore {
         !this.toConfigPulseNode.read() &&
         (phase3 || this.toConfigCheckedInPhase2Or3),
     );
-
-    // taxi in flap 0 one minute check
-    this.taxiInFlap0Check.write(this.slatFlapSelectionS0F0 && this.flightPhase.get() == 11, deltaTime);
 
     this.flapsMcduDisagree.set(
       (flapsMcduPos1Disagree || flapsMcduPos2Disagree || flapsMcduPos3Disagree) &&
