8b747f67b26018642d0e7ec9991aa0c74547d54d
Michael Corcoran <tracer@outlook.co.nz>
2025-06-22 02:15:50 +1200

    feat(fms): compute gps primary status

--- fbw-a32nx/src/systems/fmgc/src/components/fms-messages/FmsMessages.ts
+++ fbw-a32nx/src/systems/fmgc/src/components/fms-messages/FmsMessages.ts
@@ -25,6 +25,7 @@ import { MapPartlyDisplayedLeft, MapPartlyDisplayedRight } from './MapPartlyDisp
 import { Navigation } from '@fmgc/navigation/Navigation';
 import { GuidanceController } from '@fmgc/guidance/GuidanceController';
 import { TooSteepPathAhead } from '@fmgc/components/fms-messages/TooSteepPathAhead';
+import { EventBus } from '@microsoft/msfs-sdk';
 
 /**
  * This class manages Type II messages sent from the FMGC.
@@ -47,8 +48,8 @@ export class FmsMessages implements FmgcComponent {
   };
 
   private messageSelectors: FMMessageSelector[] = [
-    new GpsPrimary(),
-    new GpsPrimaryLost(),
+    new GpsPrimary(this.bus),
+    new GpsPrimaryLost(this.bus),
     new MapPartlyDisplayedLeft(),
     new MapPartlyDisplayedRight(),
     new TurnAreaExceedanceLeft(),
@@ -66,6 +67,8 @@ export class FmsMessages implements FmgcComponent {
     new StepDeleted(),
     new TooSteepPathAhead(),
   ];
+
+  constructor(private readonly bus: EventBus) {}
 
   init(navigation: Navigation, guidanceController: GuidanceController, flightPlanService: FlightPlanService): void {
     for (const selector of this.messageSelectors) {
--- fbw-a32nx/src/systems/fmgc/src/components/index.ts
+++ fbw-a32nx/src/systems/fmgc/src/components/index.ts
@@ -9,16 +9,19 @@ import { FmgcComponent } from './FmgcComponent';
 import { FmsMessages } from './fms-messages';
 import { Navigation } from '../navigation/Navigation';
 import { GuidanceController } from '../guidance/GuidanceController';
+import { EventBus } from '@microsoft/msfs-sdk';
 
-const fmsMessages = new FmsMessages();
-
-const components: FmgcComponent[] = [fmsMessages, new ReadySignal(), new FcuSync()];
+const components: FmgcComponent[] = [];
+let fmsMessages: FmsMessages;
 
 export function initComponents(
+  bus: EventBus,
   navigation: Navigation,
   guidanceController: GuidanceController,
   flightPlanService: FlightPlanService,
 ): void {
+  fmsMessages = new FmsMessages(bus);
+  components.push(fmsMessages, new ReadySignal(), new FcuSync());
   components.forEach((component) => component.init(navigation, guidanceController, flightPlanService));
 }
 
@@ -27,5 +30,5 @@ export function updateComponents(deltaTime: number): void {
 }
 
 export function recallMessageById(id: number) {
-  fmsMessages.recallId(id);
+  fmsMessages?.recallId(id);
 }
