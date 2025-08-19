55dfb561c47b7e91c4aa370b7572aa166db301ee
Jonny23787 <Jonny.leske@outlook.com>
2025-08-19 16:57:53 +0930

    remove unneeded :

--- fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/EcamMessages/AbnormalSensed/ata21-22-23.ts
+++ fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/EcamMessages/AbnormalSensed/ata21-22-23.ts
@@ -1146,7 +1146,7 @@ export const EcamAbnormalSensedAta212223: { [n: number]: AbnormalProcedure } = {
         labelNotCompleted: 'ALERT',
       },
       {
-        name: 'BEFORE OPENING ANY CABIN DOOR:',
+        name: 'BEFORE OPENING ANY CABIN DOOR',
         sensed: false,
         condition: true,
       },
