5bb4dfd682f88f30e9f8b5274246de3dfd104275
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2025-08-22 22:27:16 +0100

    fix: blank lines due to common checklist items

--- fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/EcamMessages/AbnormalSensed/ata31-32-33.ts
+++ fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/EcamMessages/AbnormalSensed/ata31-32-33.ts
@@ -6,9 +6,8 @@ import {
   ChecklistLineStyle,
   DeferredProcedure,
   DeferredProcedureType,
-  FMS_PRED_UNRELIABLE_CHECKLIST_ITEM,
-  STEER_ENDURANCE_LIMITED_CHECKLIST_ITEM,
 } from 'instruments/src/MsfsAvionicsCommon/EcamMessages';
+import { FMS_PRED_UNRELIABLE_CHECKLIST_ITEM, STEER_ENDURANCE_LIMITED_CHECKLIST_ITEM } from '../CommonChecklistItems';
 
 // Convention for IDs:
 // First two digits: ATA chapter
--- fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/EcamMessages/index.ts
+++ fbw-a380x/src/systems/instruments/src/MsfsAvionicsCommon/EcamMessages/index.ts
@@ -805,34 +805,3 @@ export enum WdSpecialLine {
   Empty,
   SeparationLine,
 }
-
-export const FMS_PRED_UNRELIABLE_CHECKLIST_ITEM: ChecklistSpecialItem = {
-  name: 'FMS PRED UNRELIABLE', // TODO Replace with FMS PRED UNRELIABLE WITHOUT ACCURATE FMS FUEL PENALTY INSERTION once multiple lines supported
-  sensed: false,
-  style: ChecklistLineStyle.ChecklistCondition,
-};
-
-export const SLATS_SLOW_CHECKLIST_ITEM: ChecklistSpecialItem = {
-  name: 'SLATS SLOW',
-  sensed: false,
-};
-
-export const FLAPS_SLOW_CHECKLIST_ITEM: ChecklistSpecialItem = {
-  name: 'FLAPS SLOW',
-  sensed: false,
-};
-
-export const LDG_PERF_AFFECTED_CHECKLIST_ITEM: ChecklistSpecialItem = {
-  name: 'LDG PERF AFFECTED',
-  sensed: false,
-};
-
-export const FUEL_CONSUMPT_INCRSD_CHECKLIST_ITEM: ChecklistSpecialItem = {
-  name: 'FUEL CONSUMPT INCRSD',
-  sensed: false,
-};
-
-export const STEER_ENDURANCE_LIMITED_CHECKLIST_ITEM: ChecklistSpecialItem = {
-  name: 'FOR TAXI: STEER ENDURANCE LIMITED',
-  sensed: false,
-};
