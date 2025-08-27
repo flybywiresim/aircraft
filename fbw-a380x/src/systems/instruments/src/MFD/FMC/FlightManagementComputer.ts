3389269cf1a2351a700e5f6ec72bc77b551a46b3
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2025-08-27 18:38:59 +0100

    fix: inverted max FL condition. Use sub for takeoff flap setting

--- fbw-a380x/src/systems/instruments/src/MFD/FMC/FlightManagementComputer.ts
+++ fbw-a380x/src/systems/instruments/src/MFD/FMC/FlightManagementComputer.ts
@@ -1060,11 +1060,6 @@ export class FlightManagementComputer implements FmcInterface {
         this.acInterface.checkDestEfobBelowMinScratchPadMessage(throttledDt);
         this.acInterface.checkEngineOut(throttledDt);
 
-        const toFlaps = this.fmgc.getTakeoffFlapsSetting();
-        if (toFlaps) {
-          this.acInterface.setTakeoffFlaps(toFlaps);
-        }
-
         const thsFor = this.fmgc.data.takeoffThsFor.get();
         if (thsFor) {
           this.acInterface.setTakeoffTrim(thsFor);
