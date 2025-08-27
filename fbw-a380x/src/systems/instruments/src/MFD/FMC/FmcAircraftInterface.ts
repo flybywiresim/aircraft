c983646f4b7658fed281efdc07ab1ea5ca4348a6
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2025-08-27 18:38:59 +0100

    fix: inverted max FL condition. Use sub for takeoff flap setting

--- fbw-a380x/src/systems/instruments/src/MFD/FMC/FmcAircraftInterface.ts
+++ fbw-a380x/src/systems/instruments/src/MFD/FMC/FmcAircraftInterface.ts
@@ -295,6 +295,9 @@ export class FmcAircraftInterface {
           0,
         ),
       ),
+      this.fmgc.data.takeoffFlapsSetting.sub((v) => {
+        this.setTakeoffFlaps(v);
+      }),
     );
 
     const pub = this.bus.getPublisher<FmsData>();
@@ -581,7 +584,7 @@ export class FmcAircraftInterface {
    * Set the takeoff flap config
    * @param {0 | 1 | 2 | 3 | null} flaps
    */
-  setTakeoffFlaps(flaps: FlapConf) {
+  setTakeoffFlaps(flaps: FlapConf | null) {
     this.arincDiscreteWord2.setBitValue(13, flaps === 0);
     this.arincDiscreteWord2.setBitValue(14, flaps === 1);
     this.arincDiscreteWord2.setBitValue(15, flaps === 2);
@@ -1775,7 +1778,7 @@ export class FmcAircraftInterface {
     }
 
     this.flightPlanService.active.setPerformanceData('cruiseFlightLevel', fl);
-    if (fl > (this.fmc.getRecMaxFlightLevel() ?? -Infinity)) {
+    if (fl > (this.fmc.getRecMaxFlightLevel() ?? Infinity)) {
       this.fmc.addMessageToQueue(NXSystemMessages.crzFlAboveMaxFL, undefined, undefined);
     }
     this.onUpdateCruiseLevel(fl);
