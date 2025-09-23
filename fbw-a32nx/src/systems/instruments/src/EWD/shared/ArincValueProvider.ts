12e695eda384887b9b9a9bb087878a03f0893cff
eagle941 <8973725+Eagle941@users.noreply.github.com>
2025-09-23 07:47:08 +0100

    chore(sfcc): added switch of SFCC in EWD input data

--- fbw-a32nx/src/systems/instruments/src/EWD/shared/ArincValueProvider.ts
+++ fbw-a32nx/src/systems/instruments/src/EWD/shared/ArincValueProvider.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-import { EventBus } from '@microsoft/msfs-sdk';
+import { EventBus, Publisher } from '@microsoft/msfs-sdk';
 import { Arinc429Word } from '@flybywiresim/fbw-sdk';
 import { EwdSimvars } from './EwdSimvarPublisher';
 
@@ -11,15 +11,22 @@ export interface Arinc429Values {
   slatsFlapsStatus: Arinc429Word;
   slatsPosition: Arinc429Word;
   flapsPosition: Arinc429Word;
+  sfccToUse: number;
 }
 export class ArincValueProvider {
   private sat = new Arinc429Word(0);
+
+  private sfcc1SlatsFlapsStatus = new Arinc429Word(0);
+
+  private sfcc2SlatsFlapsStatus = new Arinc429Word(0);
 
   private slatsFlapsStatus = new Arinc429Word(0);
 
   private slatsPosition = new Arinc429Word(0);
 
   private flapsPosition = new Arinc429Word(0);
+
+  private sfccToUse = 0;
 
   constructor(private readonly bus: EventBus) {}
 
@@ -32,19 +39,76 @@ export class ArincValueProvider {
       publisher.pub('sat', this.sat);
     });
 
-    subscriber.on('slatsFlapsStatusRaw').handle((w) => {
+    subscriber.on('sfcc1SlatsFlapsStatusRaw').handle((w) => {
       this.slatsFlapsStatus = new Arinc429Word(w);
-      publisher.pub('slatsFlapsStatus', this.slatsFlapsStatus);
+      this.sfcc1SlatsFlapsStatus = new Arinc429Word(w);
+      this.determineSfccToUse(publisher);
+      if (this.sfccToUse === 1) {
+        publisher.pub('slatsFlapsStatus', this.slatsFlapsStatus);
+      } else if (this.sfccToUse === 0) {
+        publisher.pub('slatsFlapsStatus', new Arinc429Word(0));
+      }
     });
 
-    subscriber.on('slatsPositionRaw').handle((w) => {
+    subscriber.on('sfcc1SlatsPositionRaw').handle((w) => {
       this.slatsPosition = new Arinc429Word(w);
-      publisher.pub('slatsPosition', this.slatsPosition);
+      if (this.sfccToUse === 1) {
+        publisher.pub('slatsPosition', this.slatsPosition);
+      } else if (this.sfccToUse === 0) {
+        publisher.pub('slatsPosition', new Arinc429Word(0));
+      }
     });
 
-    subscriber.on('flapsPositionRaw').handle((w) => {
+    subscriber.on('sfcc1FlapsPositionRaw').handle((w) => {
       this.flapsPosition = new Arinc429Word(w);
-      publisher.pub('flapsPosition', this.flapsPosition);
+      if (this.sfccToUse === 1) {
+        publisher.pub('flapsPosition', this.flapsPosition);
+      } else if (this.sfccToUse === 0) {
+        publisher.pub('flapsPosition', new Arinc429Word(0));
+      }
     });
+
+    subscriber.on('sfcc2SlatsFlapsStatusRaw').handle((w) => {
+      this.slatsFlapsStatus = new Arinc429Word(w);
+      this.sfcc2SlatsFlapsStatus = new Arinc429Word(w);
+      this.determineSfccToUse(publisher);
+      if (this.sfccToUse === 2) {
+        publisher.pub('slatsFlapsStatus', this.slatsFlapsStatus);
+      } else if (this.sfccToUse === 0) {
+        publisher.pub('slatsFlapsStatus', new Arinc429Word(0));
+      }
+    });
+
+    subscriber.on('sfcc2SlatsPositionRaw').handle((w) => {
+      this.slatsPosition = new Arinc429Word(w);
+      if (this.sfccToUse === 2) {
+        publisher.pub('slatsPosition', this.slatsPosition);
+      } else if (this.sfccToUse === 0) {
+        publisher.pub('slatsPosition', new Arinc429Word(0));
+      }
+    });
+
+    subscriber.on('sfcc2FlapsPositionRaw').handle((w) => {
+      this.flapsPosition = new Arinc429Word(w);
+      if (this.sfccToUse === 2) {
+        publisher.pub('flapsPosition', this.flapsPosition);
+      } else if (this.sfccToUse === 0) {
+        publisher.pub('flapsPosition', new Arinc429Word(0));
+      }
+    });
+  }
+
+  private determineSfccToUse(publisher: Publisher<Arinc429Values>) {
+    const sfcc1Valid = !this.sfcc1SlatsFlapsStatus.isFailureWarning();
+    const sfcc2Valid = !this.sfcc2SlatsFlapsStatus.isFailureWarning();
+    if (sfcc1Valid) {
+      this.sfccToUse = 1;
+    } else if (sfcc2Valid) {
+      this.sfccToUse = 2;
+    } else {
+      this.sfccToUse = 0;
+    }
+
+    publisher.pub('sfccToUse', this.sfccToUse);
   }
 }
