461841cd26416db8246bf28f55efa477deb90425
eagle941 <8973725+Eagle941@users.noreply.github.com>
2025-10-05 20:13:43 +0100

    chore: changed visibility of SlatFlapControlComputerMisc to pub(super)

--- fbw-a32nx/src/wasm/systems/a320_systems/src/hydraulic/sfcc/utils.rs
+++ fbw-a32nx/src/wasm/systems/a320_systems/src/hydraulic/sfcc/utils.rs
@@ -3,39 +3,42 @@ use uom::{
     ConstZero,
 };
 
-pub struct SlatFlapControlComputerMisc {}
+pub(super) struct SlatFlapControlComputerMisc {}
 impl SlatFlapControlComputerMisc {
     const POSITIONING_THRESHOLD_DEGREE: f64 = 6.69;
     const ENLARGED_TARGET_THRESHOLD_DEGREE: f64 = 0.8;
     const TARGET_THRESHOLD_DEGREE: f64 = 0.18;
 
-    pub fn in_positioning_threshold_range(position: Angle, target_position: Angle) -> bool {
+    pub(super) fn in_positioning_threshold_range(position: Angle, target_position: Angle) -> bool {
         let tolerance = Angle::new::<degree>(Self::POSITIONING_THRESHOLD_DEGREE);
         position > (target_position - tolerance) && position < (target_position + tolerance)
     }
 
-    pub fn in_target_threshold_range(position: Angle, target_position: Angle) -> bool {
+    pub(super) fn in_target_threshold_range(position: Angle, target_position: Angle) -> bool {
         let tolerance = Angle::new::<degree>(Self::TARGET_THRESHOLD_DEGREE);
         position > (target_position - tolerance) && position < (target_position + tolerance)
     }
 
-    pub fn below_enlarged_target_range(position: Angle, target_position: Angle) -> bool {
+    pub(super) fn below_enlarged_target_range(position: Angle, target_position: Angle) -> bool {
         let tolerance = Angle::new::<degree>(Self::ENLARGED_TARGET_THRESHOLD_DEGREE);
         position < target_position - tolerance
     }
 
-    pub fn in_enlarged_target_range(position: Angle, target_position: Angle) -> bool {
+    pub(super) fn in_enlarged_target_range(position: Angle, target_position: Angle) -> bool {
         let tolerance = Angle::new::<degree>(Self::ENLARGED_TARGET_THRESHOLD_DEGREE);
         position > (target_position - tolerance) && position < (target_position + tolerance)
     }
 
-    pub fn in_or_above_enlarged_target_range(position: Angle, target_position: Angle) -> bool {
+    pub(super) fn in_or_above_enlarged_target_range(
+        position: Angle,
+        target_position: Angle,
+    ) -> bool {
         let tolerance = Angle::new::<degree>(Self::ENLARGED_TARGET_THRESHOLD_DEGREE);
         Self::in_enlarged_target_range(position, target_position)
             || position >= target_position + tolerance
     }
 
-    pub fn between_0_1f_enlarged_target_range(position: Angle) -> bool {
+    pub(super) fn between_0_1f_enlarged_target_range(position: Angle) -> bool {
         let tolerance = Angle::new::<degree>(Self::ENLARGED_TARGET_THRESHOLD_DEGREE);
         position > (Angle::ZERO + tolerance)
             && position < (Angle::new::<degree>(120.21) - tolerance)
