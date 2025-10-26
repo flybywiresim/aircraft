fcd4a4eea2148ed070ec8814cf83da7429e784d5
Lukas Michels <lukasmichels00@gmail.com>
2025-10-26 14:09:13 +0100

    fix: fix bug in rate limiter, fix init condition if none was provided

--- fbw-common/src/wasm/systems/systems/src/shared/rate_lmiter.rs
+++ fbw-common/src/wasm/systems/systems/src/shared/rate_lmiter.rs
@@ -1,4 +1,4 @@
-use std::ops::AddAssign;
+use std::ops::Add;
 use std::ops::Mul;
 use std::ops::Sub;
 use std::time::Duration;
@@ -8,18 +8,22 @@ use std::time::Duration;
 /// y(k) = y(k-1) + min(max(x(k) - y(k-1), falling_max_rate), rising_max_rate) with the rates being in the units of T per second
 pub struct RateLimiter<T>
 where
-    T: PartialOrd + AddAssign<T> + Sub<Output = T> + Mul<f64, Output = T> + Copy,
+    T: PartialOrd + Add<T, Output = T> + Sub<Output = T> + Mul<f64, Output = T> + Copy,
 {
     rising_max_rate: T,
     falling_max_rate: T,
-    limited_output: T,
+    limited_output: Option<T>,
 }
 impl<T> RateLimiter<T>
 where
-    T: PartialOrd + AddAssign<T> + Sub<Output = T> + Mul<f64, Output = T> + Copy + Default,
+    T: PartialOrd + Add<T, Output = T> + Sub<Output = T> + Mul<f64, Output = T> + Copy + Default,
 {
     pub fn new(rising_max_rate: T, falling_max_rate: T) -> Self {
-        Self::new_with_init_value(rising_max_rate, falling_max_rate, T::default())
+        Self {
+            rising_max_rate,
+            falling_max_rate,
+            limited_output: None,
+        }
     }
 
     pub fn new_symmetrical(max_rate: T) -> Self {
@@ -27,37 +31,33 @@ where
     }
 
     pub fn new_with_init_value(rising_max_rate: T, falling_max_rate: T, init_value: T) -> Self {
-        debug_assert!(
-            falling_max_rate <= T::default(),
-            "Falling max rate must be negative or zero"
-        );
-        debug_assert!(
-            rising_max_rate >= T::default(),
-            "Rising max rate must be positive or zero"
-        );
         Self {
             rising_max_rate,
             falling_max_rate,
-            limited_output: init_value,
+            limited_output: Some(init_value),
         }
     }
 
     pub fn update(&mut self, time_delta: Duration, new_input: T) -> T {
+        if self.limited_output.is_none() {
+            self.limited_output = Some(new_input);
+            return new_input;
+        }
         // We need to do this stupid shit because rust doesnt implement Ord for floats.
         // If anyone has a better way to solve this, please let me know.
-        let mut clamped_delta = new_input - self.limited_output;
+        let mut clamped_delta = new_input - self.limited_output.unwrap();
         if clamped_delta > self.rising_max_rate * time_delta.as_secs_f64() {
-            clamped_delta = self.rising_max_rate;
+            clamped_delta = self.rising_max_rate * time_delta.as_secs_f64();
         } else if clamped_delta < self.falling_max_rate * time_delta.as_secs_f64() {
-            clamped_delta = self.falling_max_rate;
+            clamped_delta = self.falling_max_rate * time_delta.as_secs_f64();
         }
-        self.limited_output += clamped_delta;
+        self.limited_output = Some(self.limited_output.unwrap() + clamped_delta);
 
-        self.limited_output
+        self.limited_output.unwrap()
     }
 
     pub fn output(&self) -> T {
-        self.limited_output
+        self.limited_output.unwrap_or_default()
     }
 
     pub fn set_max_rate(&mut self, rising_max_rate: T, falling_max_rate: T) {
@@ -66,7 +66,7 @@ where
     }
 
     pub fn reset(&mut self, reset_value: T) {
-        self.limited_output = reset_value;
+        self.limited_output = Some(reset_value);
     }
 }
 
@@ -95,12 +95,12 @@ mod tests {
     fn rate_limiter_step_test() {
         let mut rate_limiter = RateLimiter::<f64>::new(1., -0.5);
 
-        rate_limiter.update(Duration::from_secs_f64(0.5), 1.);
+        rate_limiter.update(Duration::from_secs_f64(0.1), 1.);
         assert!(rate_limiter.output() > 0.);
 
-        assert_about_eq!(rate_limiter.update(Duration::from_secs_f64(0.5), 1.), 1.);
+        assert_about_eq!(rate_limiter.update(Duration::from_secs_f64(0.1), 1.), 0.2);
 
-        rate_limiter.update(Duration::from_secs_f64(1.), 0.);
-        assert_about_eq!(rate_limiter.output(), 0.5);
+        rate_limiter.update(Duration::from_secs_f64(0.2), 0.);
+        assert_about_eq!(rate_limiter.output(), 0.1);
     }
 }
