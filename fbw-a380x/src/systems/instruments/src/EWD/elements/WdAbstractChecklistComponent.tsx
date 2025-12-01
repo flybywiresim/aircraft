e872ac332a12487705937066267e8b47b05554a7
floridude <63071941+flogross89@users.noreply.github.com>
2025-12-01 17:31:00 +0200

    feat(a380x/fws): Add FCDC, SLAT/FLAP SYS, FWS+FCDC, FWS+CPIOM faults (#10332)
    * add abn proc: FCDC, SLAT SYS/CTL, FLAP SYS/CTL, FWS+FCDC, FWS+CPIOM FAULTS
    
    * add procedure logic for FCDC 1+2, SLAT/FLAP SYS faults
    
    * add EWD fallbacks for FWS, CPIOM faults
    
    * ad INFO, LIMITATIONS, INOP
    
    * use CPIOM_x_AVAIL simvar
    
    * fix fallback displays
    
    * add four lines to WD, make items smaller
    
    * comments pt 1
    
    * EWD visual fixes
    
    * refactor CPIOM C & other fault to separate component for different layout with more lines
    
    * review comments
    
    * refactor ADR/IR publishers
    
    * consider NCD for IR
    
    * fix FCDC 1+2 FAULT items
    
    * display FWS 1+2 FALUT & FCDC 1 + 2 FAULT also when CPIOM C not failed
    
    * add EWD extra dep for ECAM message defs
    
    * improve fallback from new references
    
    * three more
    
    * fix abnormal proc display condition
    
    * fix startup condition
    
    * also resume subs if FWS are failed

--- fbw-a380x/src/systems/instruments/src/EWD/elements/WdAbstractChecklistComponent.tsx
+++ fbw-a380x/src/systems/instruments/src/EWD/elements/WdAbstractChecklistComponent.tsx
@@ -10,6 +10,7 @@ import {
   VNode,
 } from '@microsoft/msfs-sdk';
 import { EwdSimvars } from 'instruments/src/EWD/shared/EwdSimvarPublisher';
+import { FcdcSimvars } from '../../MsfsAvionicsCommon/providers/FcdcPublisher';
 import { FwsEwdEvents } from '../../MsfsAvionicsCommon/providers/FwsEwdPublisher';
 import {
   ChecklistLineStyle,
@@ -17,18 +18,24 @@ import {
   WdLineData,
   WdSpecialLine,
 } from 'instruments/src/MsfsAvionicsCommon/EcamMessages';
+import { DestroyableComponent } from 'instruments/src/MsfsAvionicsCommon/DestroyableComponent';
 import { FormattedFwcText } from 'instruments/src/EWD/elements/FormattedFwcText';
 import { EclSoftKeys } from 'instruments/src/EWD/elements/EclClickspots';
+import { AdrBusEvents, CpiomData, IrBusEvents } from '@flybywiresim/fbw-sdk';
+import { CpiomEwdAvailabilityChecker } from '../EWD';
 
 interface WdAbstractChecklistComponentProps {
   bus: EventBus;
   visible: Subscribable<boolean>;
   abnormal: boolean;
   fwsAvail?: Subscribable<boolean>;
+  cpiomAvailChecker?: CpiomEwdAvailabilityChecker;
 }
 
-export class WdAbstractChecklistComponent extends DisplayComponent<WdAbstractChecklistComponentProps> {
-  protected readonly sub = this.props.bus.getSubscriber<ClockEvents & EwdSimvars & FwsEwdEvents>();
+export class WdAbstractChecklistComponent extends DestroyableComponent<WdAbstractChecklistComponentProps> {
+  protected readonly sub = this.props.bus.getSubscriber<
+    AdrBusEvents & ClockEvents & CpiomData & EwdSimvars & FcdcSimvars & FwsEwdEvents & IrBusEvents
+  >();
 
   protected readonly lineData: WdLineData[] = [];
 
@@ -104,7 +111,7 @@ export class WdAbstractChecklistComponent extends DisplayComponent<WdAbstractChe
     this.showFromLine.sub(() => this.updateChecklists());
   }
 
-  // 17 lines
+  // 18 lines
   render() {
     return (
       <div class="ProceduresContainer" style={{ display: this.props.visible.map((it) => (it ? 'flex' : 'none')) }}>
