c69168eb2c0227ba2cd4764c2cd50718d5de990a
alexr4339 <83085903+alexr4339@users.noreply.github.com>
2025-12-09 15:24:27 +0100

    Change button color from #f5ca4f to #ffd200

--- fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/F-PLN/MfdFmsFplnArr.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/F-PLN/MfdFmsFplnArr.tsx
@@ -599,7 +599,7 @@ export class MfdFmsFplnArr extends FmsPage<MfdFmsFplnArrProps> {
                 this.props.fmcService.master?.resetRevisedWaypoint();
                 this.props.mfd.uiService.navigateTo(`fms/${this.props.mfd.uiService.activeUri.get().category}/f-pln`);
               }}
-              buttonStyle="color: #f5ca4f;"
+              buttonStyle="color: #ffd200;"
             />
           </div>
         </div>
