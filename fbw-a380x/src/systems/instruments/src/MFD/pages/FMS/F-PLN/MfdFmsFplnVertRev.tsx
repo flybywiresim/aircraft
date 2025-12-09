cd0d0e5b296e4b71e81fae172fab3a01f31df9ed
alexr4339 <83085903+alexr4339@users.noreply.github.com>
2025-12-09 15:33:59 +0100

    Change button color from #f5ca4f to #ffd200

--- fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/F-PLN/MfdFmsFplnVertRev.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/F-PLN/MfdFmsFplnVertRev.tsx
@@ -1270,7 +1270,7 @@ export class MfdFmsFplnVertRev extends FmsPage<MfdFmsFplnVertRevProps> {
                       `fms/${this.props.mfd.uiService.activeUri.get().category}/f-pln`,
                     );
                   }}
-                  buttonStyle="color: #f5ca4f;"
+                  buttonStyle="color: #ffd200;"
                 />
               </div>
             </div>
