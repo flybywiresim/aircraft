489a4adf9b9975311646ed33b18a1791d958eaa8
Saschl <sascharudolf46@gmail.com>
2025-12-22 21:21:18 +0100

    rename loc keys in efb

--- fbw-common/src/systems/instruments/src/EFB/Settings/Pages/AtsuAocPage.tsx
+++ fbw-common/src/systems/instruments/src/EFB/Settings/Pages/AtsuAocPage.tsx
@@ -111,10 +111,10 @@ export const AtsuAocPage = () => {
   }
 
   const acarsProviderButtons = [
-    { name: t('Settings.AtsuAoc.HoppieProviderNone'), setting: 'NONE' },
-    { name: t('Settings.AtsuAoc.HoppieProviderHoppie'), setting: 'HOPPIE' },
-    { name: t('Settings.AtsuAoc.HoppieProviderBatc'), setting: 'BATC' },
-    { name: t('Settings.AtsuAoc.HoppieProviderSAI'), setting: 'SAI' },
+    { name: t('Settings.AtsuAoc.AcarsProviderNone'), setting: 'NONE' },
+    { name: t('Settings.AtsuAoc.AcarsProviderHoppie'), setting: 'HOPPIE' },
+    { name: t('Settings.AtsuAoc.AcarsProviderBatc'), setting: 'BATC' },
+    { name: t('Settings.AtsuAoc.AcarsProviderSAI'), setting: 'SAI' },
   ] as const;
 
   const { showModal } = useModals();
