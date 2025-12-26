35e3a939dcfa55d264de8efc5e2efef4ed1895f5
Lukas Michels <lukasmichels00@gmail.com>
2025-12-26 16:14:55 +0100

    feat(adiru): various improvements

---
+++ fbw-a32nx/src/wasm/systems/a320_systems_wasm/src/navigation.rs
@@ -0,0 +1,48 @@
+use std::error::Error;
+use systems::shared::to_bool;
+use systems_wasm::aspects::{ExecuteOn, MsfsAspectBuilder};
+use systems_wasm::Variable;
+
+#[derive(Clone, Copy, PartialEq)]
+enum AlignTime {
+    Realistic = 0,
+    Instant = 1,
+    Fast = 2,
+}
+
+impl From<f64> for AlignTime {
+    fn from(value: f64) -> Self {
+        match value as u8 {
+            1 => AlignTime::Instant,
+            2 => AlignTime::Fast,
+            _ => AlignTime::Realistic,
+        }
+    }
+}
+
+pub(super) fn navigation(builder: &mut MsfsAspectBuilder) -> Result<(), Box<dyn Error>> {
+    builder.map(
+        ExecuteOn::PreTick,
+        Variable::named("CONFIG_ADIRS_IR_ALIGN_TIME"),
+        |value| (AlignTime::from(value) == AlignTime::Fast).into(),
+        Variable::aspect("ADIRS_IR_FAST_ALIGN_MODE"),
+    );
+
+    builder.map_many(
+        ExecuteOn::PreTick,
+        vec![
+            Variable::named("CONFIG_ADIRS_IR_ALIGN_TIME"),
+            Variable::named("CONFIG_ADIRS_IR_INSTANT_ALIGN"),
+            Variable::named("AIRCRAFT_PRESET_QUICK_MODE"),
+        ],
+        |values| {
+            (AlignTime::from(values[0]) == AlignTime::Instant
+                || to_bool(values[1])
+                || to_bool(values[2]))
+            .into()
+        },
+        Variable::aspect("ADIRS_IR_INSTANT_ALIGN"),
+    );
+
+    Ok(())
+}
