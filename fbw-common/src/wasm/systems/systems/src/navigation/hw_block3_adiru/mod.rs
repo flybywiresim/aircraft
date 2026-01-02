440e397ded3f29dcb8ab2b9edde8de4090403db0
Lukas Michels <lukasmichels00@gmail.com>
2026-01-02 02:03:40 +0100

    test(adiru): fix tests compilation

--- fbw-common/src/wasm/systems/systems/src/navigation/hw_block3_adiru/mod.rs
+++ fbw-common/src/wasm/systems/systems/src/navigation/hw_block3_adiru/mod.rs
@@ -42,6 +42,48 @@ impl From<f64> for AlignTime {
     }
 }
 
+trait NormaliseAngleExt {
+    fn normalised(self) -> Angle;
+    #[allow(dead_code)]
+    fn normalised_180(self) -> Angle;
+}
+
+impl NormaliseAngleExt for Angle {
+    /// Create a new angle by normalizing the value into the range of
+    /// [0, 2π) rad.
+    #[inline]
+    fn normalised(self) -> Angle {
+        if self < Angle::FULL_TURN && self >= Angle::default() {
+            self
+        } else {
+            let v = self % Angle::FULL_TURN;
+
+            if v >= Angle::default() {
+                v
+            } else {
+                v + Angle::FULL_TURN
+            }
+        }
+    }
+
+    /// Create a new angle by normalizing the value into the range of
+    /// [-π, π) rad.
+    #[inline]
+    fn normalised_180(self) -> Angle {
+        if self < Angle::HALF_TURN && self >= -Angle::HALF_TURN {
+            self
+        } else {
+            let v = self % Angle::FULL_TURN;
+
+            if v < Angle::HALF_TURN {
+                v
+            } else {
+                v - Angle::FULL_TURN
+            }
+        }
+    }
+}
+
 pub struct AirDataInertialReferenceSystem {
     pub adiru_1: AirDataInertialReferenceUnit,
     pub adiru_2: AirDataInertialReferenceUnit,
