6c80b546658b968a56121e68bfd5ad37ea9ece13
Saschl <sascharudolf46@gmail.com>
2026-02-05 15:54:04 +0100

    add null check and also clear timeout

--- fbw-common/src/systems/datalink/atc/src/ATC.ts
+++ fbw-common/src/systems/datalink/atc/src/ATC.ts
@@ -51,6 +51,8 @@ export class Atc {
 
   private handoverInterval: number = null;
 
+  private handoverTimeout: number = null;
+
   private handoverOngoing = false;
 
   private currentAtc = '';
@@ -538,15 +540,22 @@ export class Atc {
       this.handoverInterval = null;
     }
 
+    if (this.handoverTimeout != null) {
+      clearTimeout(this.handoverTimeout);
+      this.handoverTimeout = null;
+    }
+
     return new Promise((resolve, _reject) => {
       // add an interval to check if all messages are answered or sent to ATC
       this.handoverInterval = window.setInterval(() => {
         if (!this.mailboxBus.openMessagesForStation(this.currentAtc)) {
-          clearInterval(this.handoverInterval);
-          this.handoverInterval = null;
+          if (this.handoverInterval !== null) {
+            clearInterval(this.handoverInterval);
+            this.handoverInterval = null;
+          }
 
           // add a timer to ensure that the last transmission is already received to avoid ATC software warnings
-          setTimeout(() => {
+          this.handoverTimeout = window.setTimeout(() => {
             if (this.currentAtc !== '') {
               this.logoffWithoutReset().then((code) => {
                 if (code !== AtsuStatusCodes.Ok) {
