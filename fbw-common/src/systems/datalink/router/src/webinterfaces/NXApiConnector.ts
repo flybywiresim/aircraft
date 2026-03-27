24dc56b42e2bc94c2172161ccc5538460860560e
Saschl <sascharudolf46@gmail.com>
2026-03-27 13:15:21 +0100

    fix parameter issue

--- fbw-common/src/systems/datalink/router/src/webinterfaces/NXApiConnector.ts
+++ fbw-common/src/systems/datalink/router/src/webinterfaces/NXApiConnector.ts
@@ -68,7 +68,7 @@ export class NXApiConnector {
     }
 
     if (source === 'SAI') {
-      return SayIntentionsConnector.receiveAtis(icao, type, message);
+      return SayIntentionsConnector.receiveAtis(icao, message);
     }
 
     await Atis.get(icao, ConfigWeatherMap[source])
