d90e48c13ccb07428f4cb7fb3e5ec5dc951741a3
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2026-04-20 20:39:49 +0100

    fix: vectors() not vectors

--- fbw-a32nx/src/systems/fmgc/src/flightplanning/plans/BaseFlightPlan.ts
+++ fbw-a32nx/src/systems/fmgc/src/flightplanning/plans/BaseFlightPlan.ts
@@ -3071,7 +3071,7 @@ export abstract class BaseFlightPlan<P extends FlightPlanPerformanceData = Fligh
     for (let i = this.activeLegIndex; i < this.allLegs.length; i++) {
       const nextLeg = this.maybeElementAt(i + 1);
       // Handle case of end of flightplan or discontinuity outside of a manual leg.
-      if (isLeg(this.activeLeg) && !this.activeLeg.isVectors && (!nextLeg || nextLeg.isDiscontinuity)) {
+      if (isLeg(this.activeLeg) && !this.activeLeg.isVectors() && (!nextLeg || nextLeg.isDiscontinuity)) {
         return i;
       }
     }
