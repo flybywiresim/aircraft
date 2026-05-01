c7edf293b744b0c0073cf3a6c58648e98cc50744
eagle941 <8973725+Eagle941@users.noreply.github.com>
2026-05-01 20:45:01 +0100

    Added unit tests to ValveBlock

--- fbw-common/src/wasm/systems/systems/src/hydraulic/valve_block.rs
+++ fbw-common/src/wasm/systems/systems/src/hydraulic/valve_block.rs
@@ -6,13 +6,15 @@ use crate::{
     simulation::UpdateContext,
 };
 
-use uom::si::{angular_velocity::radian_per_second, f64::*, pressure::psi};
+use uom::si::{angular_velocity::radian_per_second, f64::*, pressure::psi, ratio::ratio};
+use uom::typenum::P2;
 use uom::ConstZero;
 
 pub struct ValveBlock {
     current_max_speed: LowPassFilter<AngularVelocity>,
     full_pressure_max_speed: AngularVelocity,
     circuit_target_pressure: Pressure,
+    minimum_pressure: Pressure,
 
     speed: AngularVelocity,
 }
@@ -36,6 +38,7 @@ impl ValveBlock {
             ),
             full_pressure_max_speed,
             circuit_target_pressure,
+            minimum_pressure: Pressure::new::<psi>(Self::BRAKE_PRESSURE_MIN_TO_ALLOW_MOVEMENT_PSI),
             speed: AngularVelocity::ZERO,
         }
     }
@@ -48,14 +51,14 @@ impl ValveBlock {
     ) {
         self.update_current_max_speed(context, sfcc, pressure.pressure_downstream_priority_valve());
 
-        self.update_speed_and_position(sfcc);
+        self.update_speed(sfcc);
     }
 
     pub fn get_speed(&self) -> AngularVelocity {
         self.speed
     }
 
-    fn update_speed_and_position(&mut self, sfcc: &impl ValveBlockController) {
+    fn update_speed(&mut self, sfcc: &impl ValveBlockController) {
         let max_speed = self.current_max_speed.output();
 
         let sfcc_pob = sfcc.get_pob_status();
@@ -123,20 +126,305 @@ impl ValveBlock {
     }
 
     fn max_speed_factor_from_pressure(&self, current_pressure: Pressure) -> f64 {
-        let min_pressure = Pressure::new::<psi>(Self::BRAKE_PRESSURE_MIN_TO_ALLOW_MOVEMENT_PSI);
-        let press_corrected = current_pressure - min_pressure;
-        if current_pressure > min_pressure {
-            (0.0004 * press_corrected * press_corrected
-                / (self.circuit_target_pressure - min_pressure))
-                .get::<psi>()
-                .clamp(0., 1.)
-        } else {
-            0.
-        }
+        // max(Pressure.ZERO) is needed to clamp the minimum correction factor to 0 when the pressure
+        // is less than the minimum threshold.
+        let press_corrected = (current_pressure - self.minimum_pressure).max(Pressure::ZERO);
+        let target_excess_pressure = self.circuit_target_pressure - self.minimum_pressure;
+        (press_corrected.powi(P2::new()) / target_excess_pressure.powi(P2::new())).get::<ratio>()
     }
 
     #[cfg(test)]
     pub fn get_full_pressure_max_speed(&self) -> AngularVelocity {
         self.full_pressure_max_speed
+    }
+}
+
+#[cfg(test)]
+mod valve_block_tests {
+    use uom::si::{angular_velocity::revolution_per_minute, pressure::bar};
+
+    use crate::{
+        assert_gt_lt,
+        simulation::{
+            test::{SimulationTestBed, TestBed},
+            Aircraft, SimulationElement, SimulationElementVisitor,
+        },
+    };
+
+    use super::*;
+
+    const MAX_SPEED_RPM: f64 = 391.1 / 2.0;
+    const TARGET_PRESSURE_BAR: f64 = 206.0;
+
+    #[derive(Default)]
+    struct TestHydraulicSection {
+        pressure: Pressure,
+    }
+    impl TestHydraulicSection {
+        fn set_pressure(&mut self, pressure: Pressure) {
+            self.pressure = pressure;
+        }
+    }
+    impl SectionPressure for TestHydraulicSection {
+        fn pressure(&self) -> Pressure {
+            self.pressure
+        }
+
+        fn pressure_downstream_leak_valve(&self) -> Pressure {
+            self.pressure
+        }
+
+        fn pressure_downstream_priority_valve(&self) -> Pressure {
+            self.pressure
+        }
+
+        fn is_pressure_switch_pressurised(&self) -> bool {
+            self.pressure.get::<psi>() > 1700.
+        }
+    }
+
+    struct TestSfcc {
+        pob_solenoid: SolenoidStatus,
+        retract_solenoid: SolenoidStatus,
+        extend_solenoid: SolenoidStatus,
+    }
+    impl TestSfcc {
+        pub fn new() -> Self {
+            TestSfcc {
+                pob_solenoid: SolenoidStatus::DeEnergised,
+                retract_solenoid: SolenoidStatus::DeEnergised,
+                extend_solenoid: SolenoidStatus::DeEnergised,
+            }
+        }
+    }
+    impl ValveBlockController for TestSfcc {
+        fn get_pob_status(&self) -> SolenoidStatus {
+            self.pob_solenoid
+        }
+
+        fn get_retract_status(&self) -> SolenoidStatus {
+            self.retract_solenoid
+        }
+
+        fn get_extend_status(&self) -> SolenoidStatus {
+            self.extend_solenoid
+        }
+    }
+
+    struct TestAircraft {
+        hydraulic_circuit: TestHydraulicSection,
+        sfcc: TestSfcc,
+        valve_block: ValveBlock,
+    }
+    impl TestAircraft {
+        fn new() -> Self {
+            let max_speed = AngularVelocity::new::<revolution_per_minute>(MAX_SPEED_RPM);
+            let target_hydraulic_pressure = Pressure::new::<bar>(TARGET_PRESSURE_BAR);
+            Self {
+                hydraulic_circuit: TestHydraulicSection::default(),
+                sfcc: TestSfcc::new(),
+                valve_block: ValveBlock::new(max_speed, target_hydraulic_pressure),
+            }
+        }
+
+        fn set_pob_solenoid(&mut self, solenoid_status: SolenoidStatus) {
+            self.sfcc.pob_solenoid = solenoid_status;
+        }
+
+        fn set_retract_solenoid(&mut self, solenoid_status: SolenoidStatus) {
+            self.sfcc.retract_solenoid = solenoid_status;
+        }
+
+        fn set_extend_solenoid(&mut self, solenoid_status: SolenoidStatus) {
+            self.sfcc.extend_solenoid = solenoid_status;
+        }
+
+        fn set_current_pressure(&mut self, pressure: Pressure) {
+            self.hydraulic_circuit.set_pressure(pressure);
+        }
+    }
+    impl Aircraft for TestAircraft {
+        fn update_after_power_distribution(&mut self, context: &UpdateContext) {
+            self.valve_block
+                .update(context, &self.sfcc, &self.hydraulic_circuit);
+        }
+    }
+    impl SimulationElement for TestAircraft {
+        fn accept<T: SimulationElementVisitor>(&mut self, visitor: &mut T) {
+            visitor.visit(self);
+        }
+    }
+
+    struct ValveBlockTestBed {
+        test_bed: SimulationTestBed<TestAircraft>,
+    }
+    impl ValveBlockTestBed {
+        fn new() -> Self {
+            Self {
+                test_bed: SimulationTestBed::new(|_| TestAircraft::new()),
+            }
+        }
+
+        fn run_waiting_for(mut self, delta: Duration) -> Self {
+            self.test_bed.run_multiple_frames(delta);
+            self
+        }
+
+        fn max_speed_factor_from_pressure(&self, pressure: Pressure) -> f64 {
+            self.query(|a| a.valve_block.max_speed_factor_from_pressure(pressure))
+        }
+
+        fn get_minimum_pressure(&self) -> Pressure {
+            self.query(|a| a.valve_block.minimum_pressure)
+        }
+
+        fn get_speed(&self) -> AngularVelocity {
+            self.query(|a| a.valve_block.get_speed())
+        }
+
+        fn set_pob_solenoid(mut self, solenoid_status: SolenoidStatus) -> Self {
+            self.command(|a| a.set_pob_solenoid(solenoid_status));
+            self
+        }
+
+        fn set_extend_solenoid(mut self, solenoid_status: SolenoidStatus) -> Self {
+            self.command(|a| a.set_extend_solenoid(solenoid_status));
+            self
+        }
+
+        fn set_retract_solenoid(mut self, solenoid_status: SolenoidStatus) -> Self {
+            self.command(|a| a.set_retract_solenoid(solenoid_status));
+            self
+        }
+
+        fn set_hyd_pressure(mut self, pressure: Pressure) -> Self {
+            self.command(|a| a.set_current_pressure(pressure));
+            self
+        }
+    }
+    impl TestBed for ValveBlockTestBed {
+        type Aircraft = TestAircraft;
+
+        fn test_bed(&self) -> &SimulationTestBed<TestAircraft> {
+            &self.test_bed
+        }
+
+        fn test_bed_mut(&mut self) -> &mut SimulationTestBed<TestAircraft> {
+            &mut self.test_bed
+        }
+    }
+
+    fn test_bed() -> ValveBlockTestBed {
+        ValveBlockTestBed::new()
+    }
+
+    #[test]
+    fn test_speed_factor() {
+        let valve_block = test_bed();
+
+        let current_pressure = Pressure::ZERO;
+        let speed_factor = valve_block.max_speed_factor_from_pressure(current_pressure);
+        assert_eq!(speed_factor, 0.0);
+
+        let current_pressure = valve_block.get_minimum_pressure();
+        let speed_factor = valve_block.max_speed_factor_from_pressure(current_pressure);
+        assert_eq!(speed_factor, 0.0);
+
+        let current_pressure = Pressure::new::<bar>(TARGET_PRESSURE_BAR / 2.0);
+        let speed_factor = valve_block.max_speed_factor_from_pressure(current_pressure);
+        assert!(speed_factor > 0.0);
+        assert!(speed_factor < 1.0);
+
+        let current_pressure = Pressure::new::<bar>(TARGET_PRESSURE_BAR);
+        let speed_factor = valve_block.max_speed_factor_from_pressure(current_pressure);
+        assert_eq!(speed_factor, 1.0);
+
+        let current_pressure = Pressure::new::<bar>(209.0);
+        let speed_factor = valve_block.max_speed_factor_from_pressure(current_pressure);
+        assert!(speed_factor > 1.0);
+    }
+
+    #[test]
+    fn test_pob_solenoid() {
+        let test_bed = test_bed();
+
+        let test_bed = test_bed
+            .set_hyd_pressure(Pressure::ZERO)
+            .run_waiting_for(Duration::from_millis(2000));
+        assert_eq!(test_bed.get_speed(), AngularVelocity::ZERO);
+
+        let test_bed = test_bed
+            .set_hyd_pressure(Pressure::new::<bar>(TARGET_PRESSURE_BAR))
+            .run_waiting_for(Duration::from_millis(2000));
+        assert_eq!(test_bed.get_speed(), AngularVelocity::ZERO);
+
+        let test_bed = test_bed
+            .set_hyd_pressure(Pressure::new::<bar>(TARGET_PRESSURE_BAR))
+            .set_pob_solenoid(SolenoidStatus::DeEnergised)
+            .set_extend_solenoid(SolenoidStatus::Energised)
+            .set_retract_solenoid(SolenoidStatus::DeEnergised)
+            .run_waiting_for(Duration::from_millis(2000));
+        assert_eq!(test_bed.get_speed(), AngularVelocity::ZERO);
+
+        let test_bed = test_bed
+            .set_hyd_pressure(Pressure::new::<bar>(TARGET_PRESSURE_BAR))
+            .set_pob_solenoid(SolenoidStatus::DeEnergised)
+            .set_extend_solenoid(SolenoidStatus::DeEnergised)
+            .set_retract_solenoid(SolenoidStatus::Energised)
+            .run_waiting_for(Duration::from_millis(2000));
+        assert_eq!(test_bed.get_speed(), AngularVelocity::ZERO);
+    }
+
+    #[test]
+    fn test_minimum_pressure() {
+        let test_bed = test_bed();
+
+        let minimum_pressure = test_bed.get_minimum_pressure();
+
+        let test_bed = test_bed
+            .set_hyd_pressure(minimum_pressure)
+            .set_pob_solenoid(SolenoidStatus::Energised)
+            .set_extend_solenoid(SolenoidStatus::Energised)
+            .set_retract_solenoid(SolenoidStatus::DeEnergised)
+            .run_waiting_for(Duration::from_millis(2000));
+        assert_eq!(test_bed.get_speed(), AngularVelocity::ZERO);
+
+        let test_bed = test_bed
+            .set_hyd_pressure(minimum_pressure)
+            .set_pob_solenoid(SolenoidStatus::Energised)
+            .set_extend_solenoid(SolenoidStatus::DeEnergised)
+            .set_retract_solenoid(SolenoidStatus::Energised)
+            .run_waiting_for(Duration::from_millis(2000));
+        assert_eq!(test_bed.get_speed(), AngularVelocity::ZERO);
+    }
+
+    #[test]
+    fn test_max_speed() {
+        let max_speed = AngularVelocity::new::<revolution_per_minute>(MAX_SPEED_RPM);
+        let test_bed = test_bed();
+
+        let test_bed = test_bed
+            .set_hyd_pressure(Pressure::new::<bar>(TARGET_PRESSURE_BAR))
+            .set_pob_solenoid(SolenoidStatus::Energised)
+            .set_extend_solenoid(SolenoidStatus::Energised)
+            .set_retract_solenoid(SolenoidStatus::DeEnergised)
+            .run_waiting_for(Duration::from_millis(2000));
+        assert_gt_lt!(
+            test_bed.get_speed(),
+            max_speed,
+            AngularVelocity::new::<radian_per_second>(0.01)
+        );
+
+        let test_bed = test_bed
+            .set_hyd_pressure(Pressure::new::<bar>(TARGET_PRESSURE_BAR))
+            .set_pob_solenoid(SolenoidStatus::Energised)
+            .set_extend_solenoid(SolenoidStatus::DeEnergised)
+            .set_retract_solenoid(SolenoidStatus::Energised)
+            .run_waiting_for(Duration::from_millis(2000));
+        assert_gt_lt!(
+            test_bed.get_speed(),
+            -max_speed,
+            AngularVelocity::new::<radian_per_second>(0.01)
+        );
     }
 }
