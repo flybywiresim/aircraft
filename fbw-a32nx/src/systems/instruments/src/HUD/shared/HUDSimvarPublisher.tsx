c44ec63086b38e461c7828da6be41b37779f9a04
Julien KEMLER <julien.kemler@gmail.com>
2026-05-08 18:37:53 +0200

    Revert "."
    This reverts commit 4c5fd4a71144ef2c62437f775ae893e56c33cd70.

--- fbw-a32nx/src/systems/instruments/src/HUD/shared/HUDSimvarPublisher.tsx
+++ fbw-a32nx/src/systems/instruments/src/HUD/shared/HUDSimvarPublisher.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) 2021-2023 FlyByWire Simulations
 //
-//
 // SPDX-License-Identifier: GPL-3.0
 
 import { BasePublisher, EventBus, SimVarDefinition, SimVarValueType } from '@microsoft/msfs-sdk';
