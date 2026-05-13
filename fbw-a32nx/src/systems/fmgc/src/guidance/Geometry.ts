f56b7b0aca47e04078c01d380ceebcfc937f0719
BBK <22713769+BlueberryKing@users.noreply.github.com>
2026-05-13 19:04:53 +0200

    Only compute FROM leg on SEC

--- fbw-a32nx/src/systems/fmgc/src/guidance/Geometry.ts
+++ fbw-a32nx/src/systems/fmgc/src/guidance/Geometry.ts
@@ -183,7 +183,7 @@ export class Geometry {
       // When we activate a SEC plan, it's possible that we're putting a FROM leg into the active plan that we've never
       // actually flown. This breaks the geometry if the active leg depends on the FROm leg having been computed before
       const leg = this.legs.get(i);
-      if (i === fromLegIndex && (plan.index === FlightPlanIndex.Active || leg.isComputed)) {
+      if (i === fromLegIndex && (plan.index !== FlightPlanIndex.FirstSecondary || leg.isComputed)) {
         continue;
       }
 
