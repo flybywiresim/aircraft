fe7d5687baed3f67320f5fafa45673a9b6c78850
Florian Scheuner <github@florian-scheuner.de>
2026-05-13 20:49:57 +0300

    ci: branch split (fs2020) (#10646)
    * ci: branch split (fs2020)
    
    * ci: change pre-release ids
    
    * ci: change branch for checklist fix
    
    * ci: fix permissions
    
    * ci: pull_request_target runs from default branch
    
    * chore: remove unused scripts
    
    * ci: adapt source identifier
    
    * build: update metadata
    
    * chore: rename workflow files
    
    because apparently to github -> same filename must be same workflow name even if it is running from another branch and even if that branch is on another repo

--- scripts/install-source_a32nx.js
+++ scripts/install-source_a32nx.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const fs = require('fs-extra');
 
-const source = process.env.BUILD_DIR_NAME ? `external/a32nx/${process.env.BUILD_DIR_NAME}` : 'external/a32nx';
+const source = process.env.BUILD_DIR_NAME
+  ? `external/msfs2020/a32nx/${process.env.BUILD_DIR_NAME}`
+  : 'external/msfs2020/a32nx';
 console.log(`installManifest source is: ${source}`);
 
 const installManifest = fs.readJSONSync('./fbw-a32nx/out/flybywire-aircraft-a320-neo/install.json');
