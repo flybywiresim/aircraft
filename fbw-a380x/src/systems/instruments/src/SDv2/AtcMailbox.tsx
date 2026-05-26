4bdee24a23faef349b9efc32f6013fe1894024d8
heclak <46121009+heclak@users.noreply.github.com>
2026-05-26 08:55:35 +0800

    feat(a380x/mailbox): initial message handling implementation
    - refactor MailboxMessageBlock

--- fbw-a380x/src/systems/instruments/src/SDv2/AtcMailbox.tsx
+++ fbw-a380x/src/systems/instruments/src/SDv2/AtcMailbox.tsx
@@ -1,15 +1,7 @@
 //  Copyright (c) 2024 FlyByWire Simulations
 //  SPDX-License-Identifier: GPL-3.0
 
-import {
-  ArraySubject,
-  DisplayComponent,
-  EventBus,
-  FSComponent,
-  Subject,
-  Subscribable,
-  VNode,
-} from '@microsoft/msfs-sdk';
+import { ArraySubject, DisplayComponent, EventBus, FSComponent, MapSubject, Subject, VNode } from '@microsoft/msfs-sdk';
 import { Button } from '../MsfsAvionicsCommon/UiWidgets/Button';
 import { IconButton } from '../MsfsAvionicsCommon/UiWidgets/IconButton';
 import { MouseCursor } from '../MsfsAvionicsCommon/UiWidgets/MouseCursor';
@@ -20,19 +12,20 @@ import {
   DclMessage,
   MailboxStatusMessage,
   AtsuMessageSerializationFormat,
+  CpdlcMessageMonitoringState,
 } from '@datalink/common';
 
 import './style.scss';
 
 export class MailboxMessageBlock {
-  public readonly messages: ArraySubject<CpdlcMessage> = ArraySubject.create();
-  public readonly timestamp: number = 0;
-  public readonly response: Subject<number> = Subject.create<number>(-1);
-  public readonly statusMessage: MailboxStatusMessage = MailboxStatusMessage.NoMessage;
-  public readonly messageVisible: Subscribable<boolean> = Subject.create<boolean>(false);
-  public readonly automaticCloseTimeout: Subject<number> = Subject.create<number>(-1);
-  public readonly semanticResponseIncomplete: Subscribable<boolean> = Subject.create<boolean>(false);
-  public readonly reachEndOfMessage: Subscribable<boolean> = Subject.create<boolean>(false);
+  public messages: CpdlcMessage[] = [];
+  public timestamp: number = 0;
+  public response: number = -1;
+  public statusMessage: MailboxStatusMessage = MailboxStatusMessage.NoMessage;
+  public messageVisible: Subject<boolean> = Subject.create<boolean>(false);
+  public automaticCloseTimeout: Subject<number> = Subject.create<number>(-1);
+  public semanticResponseIncomplete: boolean = false;
+  public reachEndOfMessage: boolean = false;
 
   constructor() {
     this.timestamp = new Date().getTime();
@@ -54,15 +47,23 @@ export class AtcMailbox extends DisplayComponent<AtcMailboxProps> {
 
   private onMouseMoveHandler = this.onMouseMove.bind(this);
 
+  private readonly publisher = this.props.bus.getPublisher<AtsuMailboxMessages>();
   private readonly subs = this.props.bus.getSubscriber<AtsuMailboxMessages>();
 
-  private readonly messageRef = FSComponent.createRef<HTMLSpanElement>();
+  private messages: MapSubject<number, MailboxMessageBlock> = MapSubject.create<number, MailboxMessageBlock>();
+
+  private messageIndex: Subject<number> = Subject.create<number>(-1);
+  private messageReadComplete: Subject<boolean> = Subject.create(false);
+  private visibleMessageSemanticResponseIncomplete: Subject<boolean> = Subject.create<boolean>(false);
+  private visibleMessages: ArraySubject<CpdlcMessage> = ArraySubject.create();
+  private visibleMessageStatus: Subject<MailboxStatusMessage> = Subject.create<MailboxStatusMessage>(
+    MailboxStatusMessage.NoMessage,
+  );
+  private response: Subject<number> = Subject.create<number>(-1);
 
   private mailboxText = Subject.create<string>(
     'DEPART REQUEST\n FBW123\n FROM:WSSS GATE:A\n TO:YSSY ATIS:S\n A/C TYPE:A388',
   );
-
-  private messageArray = ArraySubject.create<MailboxMessageBlock>([]);
 
   private handleIncomingMessages(cpdlcMessages: CpdlcMessage[]): void {
     console.log('message received');
@@ -75,6 +76,48 @@ export class AtcMailbox extends DisplayComponent<AtcMailboxProps> {
       console.log(enhancedMessages[0].serialize(AtsuMessageSerializationFormat.Mailbox));
       this.mailboxText.set(enhancedMessages[0].serialize(AtsuMessageSerializationFormat.Mailbox));
     });
+
+    if (enhancedMessages.length !== 0) {
+      // check if incoming message already exist in messages
+      const messageBlock = this.messages.getValue(enhancedMessages[0].UniqueMessageID);
+
+      if (messageBlock !== undefined) {
+        // update the communication states and reponses of the existing message
+        // messageBlock.messages.set(enhancedMessages);
+      } else {
+        // store new message in messages
+        const message = new MailboxMessageBlock();
+        message.messages = enhancedMessages;
+        if (enhancedMessages[0].MessageMonitoring === CpdlcMessageMonitoringState.Monitoring) {
+          message.statusMessage = MailboxStatusMessage.Monitoring;
+        } else if (enhancedMessages[0].MessageMonitoring === CpdlcMessageMonitoringState.Cancelled) {
+          message.statusMessage = MailboxStatusMessage.MonitoringCancelled;
+        }
+        this.messages.setValue(enhancedMessages[0].UniqueMessageID, message);
+      }
+
+      // TODO implement semantic response section
+
+      // display message
+      if (this.messages.size === 1) {
+        const message = this.messages.getValue(enhancedMessages[0].UniqueMessageID);
+        if (message) {
+          message.messageVisible.set(true);
+          this.messages.setValue(enhancedMessages[0].UniqueMessageID, message);
+          this.publisher.pub('visibleMessage', message.messages[0].UniqueMessageID, true, false);
+        }
+      }
+    }
+  }
+
+  private sendMessage(uid: number) {
+    this.publisher.pub('downlinkTransmit', uid, true, false);
+  }
+
+  private sortedMessageArray(messages: MapSubject<number, MailboxMessageBlock>) {
+    const arrMessages = Array.from(messages.get().values());
+    arrMessages.sort((a, b) => a.timestamp - b.timestamp);
+    return arrMessages;
   }
 
   public onAfterRender(node: VNode): void {
@@ -84,6 +127,26 @@ export class AtcMailbox extends DisplayComponent<AtcMailboxProps> {
 
     this.subs.on('cpdlcMessages').handle((messages: CpdlcMessage[]) => this.handleIncomingMessages(messages));
     this.subs.on('dclMessages').handle((messages: DclMessage[]) => this.handleIncomingMessages(messages));
+
+    this.messages.sub((messages) => {
+      if (messages.size === 0) return;
+
+      // find the first visible message
+      const arrMessages = this.sortedMessageArray(this.messages);
+      this.messageIndex.set(arrMessages.findIndex((element) => element.messageVisible));
+
+      if (this.messageIndex.get() !== -1) {
+        this.response.set(arrMessages[this.messageIndex.get()].response);
+        this.visibleMessages.set(arrMessages[this.messageIndex.get()].messages);
+        this.messageReadComplete.set(arrMessages[this.messageIndex.get()].reachEndOfMessage);
+        this.visibleMessageStatus.set(arrMessages[this.messageIndex.get()].statusMessage);
+        this.visibleMessageSemanticResponseIncomplete.set(
+          arrMessages[this.messageIndex.get()].semanticResponseIncomplete,
+        );
+      }
+
+      // TODO: check for priority messages
+    });
   }
 
   destroy(): void {
@@ -131,7 +194,7 @@ export class AtcMailbox extends DisplayComponent<AtcMailboxProps> {
           </div>
         </div>
         <div class="atc-mailbox-right-layout">
-          <Button label="CLOSE" onClick={() => {}} buttonStyle="height: 50px; justify-content: flex-end;"></Button>
+          <Button label="SEND" onClick={() => {}} buttonStyle="height: 50px; justify-content: flex-end;"></Button>
           <Button label="STANBY" onClick={() => {}} buttonStyle="height: 50px; justify-content: flex-end;"></Button>
           <Button label="UNABLE" onClick={() => {}} buttonStyle="height: 50px; justify-content: flex-end;"></Button>
           <Button label="LOAD SEC3" onClick={() => {}} buttonStyle="height: 50px; justify-content: flex-end;"></Button>
