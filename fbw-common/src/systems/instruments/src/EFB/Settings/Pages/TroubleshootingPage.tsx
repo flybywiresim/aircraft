bd54c0bc864fdbe0e36f0493cd16a2cd867ccc6a
Saschl <sascharudolf46@gmail.com>
2026-05-31 13:56:16 +0200

    fix lint

--- fbw-common/src/systems/instruments/src/EFB/Settings/Pages/TroubleshootingPage.tsx
+++ fbw-common/src/systems/instruments/src/EFB/Settings/Pages/TroubleshootingPage.tsx
@@ -10,25 +10,13 @@ import { useTroubleshooting } from '../../TroubleshootingContext';
 import { AiracCycleFormatter, FacilityLoader } from '@microsoft/msfs-sdk';
 import { AircraftGithubVersionChecker, BuildInfo } from '../../../../../shared/src/AircraftGithubVersionChecker';
 import { isMsfs2024 } from '../../../../../shared/src/MsfsDetect';
-import { usePersistentSetting, useSimVar } from '@flybywiresim/fbw-sdk-react';
 
 export const TroubleshootingPage = () => {
   const errorLog = useTroubleshooting();
   const [navDates, setNavDates] = useState('');
   const [naviInstalled, setNaviInstalled] = useState(false);
   const [buildInfo, setBuildInfo] = useState<BuildInfo | undefined>(undefined);
-  const [acarsProvider] = usePersistentSetting('ACARS_PROVIDER');
-  const [acarsState] = useSimVar('L:A32NX_ACARS_ACTIVE', 'boolean', 1000);
   const { mismatches: fileHashMismatches } = useAppSelector((state) => state.fileHashes);
-
-  const acarsStateInfo = () => {
-    if (acarsProvider === 'NONE') {
-      return 'Disabled';
-    } else if (acarsProvider === 'BATC') {
-      return acarsState ? 'Connected' : 'Disconnected from BATC, check if the BATC Client is running';
-    }
-    return acarsState ? 'Connected' : 'Disconnected, check your UserID';
-  };
 
   useEffect(() => {
     fetch('/VFS/scenery/fs-base-jep/scenery/world/airaccycle.bgl', { method: 'HEAD' }).then((r) =>
