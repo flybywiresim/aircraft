609009f541b0e15a01672387977fddd6e62c0c84
Saschl <sascharudolf46@gmail.com>
2026-06-10 22:02:04 +0200

    only write when boarding is active

--- fbw-a32nx/src/wasm/systems/a320_systems_wasm/src/payload.rs
+++ fbw-a32nx/src/wasm/systems/a320_systems_wasm/src/payload.rs
@@ -1,10 +1,7 @@
 use std::error::Error;
 
-use msfs::sim_connect;
-use msfs::{sim_connect::SimConnect, sim_connect::SIMCONNECT_OBJECT_ID_USER};
-
-use systems_wasm::aspects::{ExecuteOn, MsfsAspectBuilder, ObjectWrite, VariablesToObject};
-use systems_wasm::{set_data_on_sim_object, Variable};
+use systems_wasm::aspects::{ExecuteOn, MsfsAspectBuilder};
+use systems_wasm::Variable;
 
 pub(super) fn payload(builder: &mut MsfsAspectBuilder) -> Result<(), Box<dyn Error>> {
     for i in 1..=8 {
@@ -13,15 +10,15 @@ pub(super) fn payload(builder: &mut MsfsAspectBuilder) -> Result<(), Box<dyn Err
             Variable::aspect(&format!("PAYLOAD_STATION_{i}_REQ")),
         );
 
-        builder.map_many(
+        builder.map_many_if(
             ExecuteOn::PostTick,
             vec![
                 Variable::aspect(&format!("PAYLOAD_STATION_{i}_REQ")),
-                //  Variable::named("BOARDING_STARTED_BY_USR"),
-                //  Variable::named("FSDT_GSX_BOARDING_STATE"),
+                Variable::named("BOARDING_STARTED_BY_USR"),
+                Variable::named("FSDT_GSX_BOARDING_STATE"),
             ],
             |values| values[0],
-            //|values| values[1] > 0. || values[2] >= 4. && values[2] < 6.,
+            |values| values[1] > 0. || values[2] >= 4. && values[2] < 6.,
             Variable::aircraft("PAYLOAD STATION WEIGHT", "POUNDS", i),
         );
     }
