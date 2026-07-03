a81bbf26614fd3d66401f4b7981152a41a2badcd
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2026-07-03 21:11:58 +0100

    fix: ga legs appear to be allowed

--- fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/F-PLN/MfdFmsFplnVertRev.tsx
+++ fbw-a380x/src/systems/instruments/src/MFD/pages/FMS/F-PLN/MfdFmsFplnVertRev.tsx
@@ -242,7 +242,7 @@ export class MfdFmsFplnVertRev extends FmsPage<MfdFmsFplnVertRevProps> {
     this.availableWaypointsToLegIndex = [];
     this.availableWaypointsIdents = [];
     if (activeLegIndex !== undefined && plan !== null) {
-      for (let i = activeLegIndex; i < plan.firstMissedApproachLegIndex; i++) {
+      for (let i = activeLegIndex; i < plan.legCount; i++) {
         const leg = plan.maybeElementAt(i);
         if (isLeg(leg) && leg.isXF()) {
           this.availableWaypointsToLegIndex.push(i);
@@ -283,11 +283,7 @@ export class MfdFmsFplnVertRev extends FmsPage<MfdFmsFplnVertRevProps> {
     leg: ReadonlyFlightPlanLeg,
     flightPlan: ReadonlyFlightPlan,
   ): boolean {
-    return (
-      isConstraintRevisionAllowed(leg) &&
-      legIndex >= flightPlan.activeLegIndex &&
-      legIndex < flightPlan.firstMissedApproachLegIndex
-    );
+    return isConstraintRevisionAllowed(leg) && legIndex >= flightPlan.activeLegIndex;
   }
 
   private updateConstraints() {
