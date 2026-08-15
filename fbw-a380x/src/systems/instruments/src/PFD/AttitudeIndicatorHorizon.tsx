e5e3c17f26ce152755414889ea55484955db27d5
Lukas Michels <lukasmichels00@gmail.com>
2026-08-15 13:43:04 +0200

    refactor(a380x/pfd): pause hdg bug transform when not visible

--- fbw-a380x/src/systems/instruments/src/PFD/AttitudeIndicatorHorizon.tsx
+++ fbw-a380x/src/systems/instruments/src/PFD/AttitudeIndicatorHorizon.tsx
@@ -81,6 +81,14 @@ class HeadingBug extends DisplayComponent<{ bus: EventBus; isCaptainSide: boolea
     this.trkFpaModeActive.sub((trkFpaModeActive) => {
       this.selectedHdgTrk.setConsumer(this.sub.on(trkFpaModeActive ? 'prim_selected_track' : 'prim_selected_heading'));
     }, true);
+
+    this.visible.sub((visible) => {
+      if (visible) {
+        this.transform.resume();
+      } else {
+        this.transform.pause();
+      }
+    }, true);
   }
 
   render(): VNode {
