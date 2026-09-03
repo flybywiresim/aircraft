c84846f68bf5252d030fa601755b0c89e114b06d
donstim <70166617+donstim@users.noreply.github.com>
2026-09-03 12:36:16 -0700

    Update a32nx_takeoff.ts
    Fix temperature correction for CONF 2 (using runway length -pressure altitude/15 for CONF 2 and runway length - pressure altitude/12 for CONF 1 and 3.

--- fbw-a32nx/src/systems/shared/src/performance/a32nx_takeoff.ts
+++ fbw-a32nx/src/systems/shared/src/performance/a32nx_takeoff.ts
@@ -3443,7 +3443,11 @@ export class A320251NTakeoffPerformanceCalculator implements TakeoffPerformanceC
 
     const tempFactors = A320251NTakeoffPerformanceCalculator.runwayTemperatureFactor[conf];
 
-    const runwayAltFactor = runwayLength - pressureAlt / 12;
+    if (conf = 2) {
+      const runwayAltFactor = runwayLength - pressureAlt / 15;
+    } else {
+      const runwayAltFactor = runwayLength - pressureAlt / 12;
+    }
     let weightDelta = 1000 * (runwayAltFactor * tempFactors[0] + tempFactors[1]) * (Math.min(temp, tRef) - isaTemp);
     if (temp > tRef) {
       weightDelta += 1000 * (runwayAltFactor * tempFactors[2] + tempFactors[3]) * (Math.min(temp, tMax) - tRef);
