2b00b6bee06f04cc74b09b1282ea70728626ddee
donstim <70166617+donstim@users.noreply.github.com>
2026-09-03 13:28:53 -0700

    Update a32nx_takeoff.ts

--- fbw-a32nx/src/systems/shared/src/performance/a32nx_takeoff.ts
+++ fbw-a32nx/src/systems/shared/src/performance/a32nx_takeoff.ts
@@ -3443,10 +3443,10 @@ export class A320251NTakeoffPerformanceCalculator implements TakeoffPerformanceC
 
     const tempFactors = A320251NTakeoffPerformanceCalculator.runwayTemperatureFactor[conf];
 
-    if (conf = 2) {
-      runwayAltFactor = runwayLength - pressureAlt / 15;
+    if (conf == 2) {
+      const runwayAltFactor = runwayLength - pressureAlt / 15;
     } else {
-      runwayAltFactor = runwayLength - pressureAlt / 12;
+      const runwayAltFactor = runwayLength - pressureAlt / 12;
     }
     let weightDelta = 1000 * (runwayAltFactor * tempFactors[0] + tempFactors[1]) * (Math.min(temp, tRef) - isaTemp);
     if (temp > tRef) {
