f078d62f8af22adcd509779f88826a06db9170f8
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2026-09-17 00:02:23 +0100

    chore: simply `getRemainingAlignTime`

--- fbw-a380x/src/systems/shared/src/AdirsUtils.ts
+++ fbw-a380x/src/systems/shared/src/AdirsUtils.ts
@@ -9,27 +9,10 @@ import { Arinc429WordData } from '@flybywiresim/fbw-sdk';
  */
 export function getRemainingAlignTime(v: Arinc429WordData): number | null {
   if (!v.isInvalid()) {
-    if (v.bitValue(16) && v.bitValue(17) && v.bitValue(18)) {
-      return 7;
-    }
-    if (v.bitValue(17) && v.bitValue(18)) {
-      return 6;
-    }
-    if (v.bitValue(16) && v.bitValue(18)) {
-      return 5;
-    }
-    if (v.bitValue(18)) {
-      return 4;
-    }
-    if (v.bitValue(16) && v.bitValue(17)) {
-      return 3;
-    }
-    if (v.bitValue(17)) {
-      return 2;
-    }
-    if (v.bitValue(16)) {
-      return 1;
-    }
+    const firstBitMinute = v.bitValue(16) ? 0 : 1;
+    const secondBitMinute = v.bitValue(17) ? 2 : 0;
+    const thirdBitMinute = v.bitValue(18) ? 4 : 0;
+    return firstBitMinute + secondBitMinute + thirdBitMinute;
   }
   return null;
 }
