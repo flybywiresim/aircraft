de5903f864e7d1943875f463c52d07e3f0b8054d
BravoMike99 <119708186+BravoMike99@users.noreply.github.com>
2026-09-18 18:30:25 +0100

    fix(fms): split max cert level by wind uplink per A/C
    (cherry picked from commit 74005e763fd5fc80744885f6f8170c68da430322)

--- fbw-a32nx/src/systems/instruments/src/MCDU/legacy/A32NX_FMCMainDisplay.ts
+++ fbw-a32nx/src/systems/instruments/src/MCDU/legacy/A32NX_FMCMainDisplay.ts
@@ -5615,6 +5615,7 @@ export abstract class FMCMainDisplay implements FmsDataInterface, FmsDisplayInte
           plan,
           this.flightPhaseManager.phase,
           FpmConfigs.A320_HONEYWELL_H4,
+          this.maximumRecommendedCruiseFlightLevel,
         );
 
         this.addMessageToQueue(
--- fbw-a380x/src/systems/shared/src/PerformanceConstants.ts
+++ fbw-a380x/src/systems/shared/src/PerformanceConstants.ts
@@ -20,6 +20,8 @@ export const VloGravityGearExtension = 220; // kt
 export const MloGravityGearExtension = 0.48; // mach
 
 export const maxCertifiedAlt = 43_000; // feet
+/** The maximum certified flight level */
+export const maxCertifiedFlightLevel = maxCertifiedAlt / 100;
 
 export const defaultTropopauseAlt = 36_090; // feet
 
